2b2e80a6aaacef9d76c75110d197769416bc904c
Matt Robenolt <matt@ydekproductions.com>
2011-12-05 18:41:18 -0500

    Adding some testing for createFromText and the reading the passthrough res.sentry object.

--- tests.js
+++ tests.js
@@ -5,13 +5,23 @@ var connect = require('connect'),
 var options = {
     key: 'testing123',
     servers: ['http://sentry.dev:9000/store/'],
-};
+},
+client = new raven.Client(options);
 
-raven.patch_global(options);
+client.createFromText('Testing!!!', function(result){});
 
-connect(function(req, res){
-    idontexist['what'];
-}, raven_middleware(options)).listen(3000);
+raven.patchGlobal(options);
+
+function handle_request(req, res) {
+    throw new Error('broke');
+}
+
+connect(function connect(req, res){
+    handle_request(req, res);
+}, raven_middleware(client), function(err, req, res, next) {
+    res.statusCode = 500;
+    res.end(JSON.stringify(res.sentry));
+}).listen(3000);
 
 // testing the patch_global method
-ffdasfsd.fdasfd;
+//ffdasfsd.fdasfd;
