2eeb259547b82b3567eb01a7b22ee196a0d76c36
Matt Robenolt <matt@ydekproductions.com>
2012-03-16 16:03:27 -0400

    Bootstrapping a test runner

---
+++ test/test_something.js
@@ -0,0 +1,8 @@
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      [1,2,3].indexOf(5).should.equal(-1);
+      [1,2,3].indexOf(0).should.equal(-1);
+    })
+  })
+})
