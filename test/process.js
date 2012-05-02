964a1655ac1443e1718ae2d9f821252321a76e82
bkonkle <brandon@konkle.us>
2012-05-02 16:40:17 -0600

    Fix the tests to account for the project ID insertion in the url

--- test/process.js
+++ test/process.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     module("Raven.process");
-        
+
     function parseAuthHeader(header) {
         var values = {};
         $.each(header.slice(7).split(', '), function(i, value) {
@@ -9,29 +9,29 @@ $(document).ready(function() {
         });
         return values;
     };
-    
+
     var message = "Once upon a midnight dreary",
         fileurl = 'http://edgarallen.poe/nevermore/',
-        lineno = 12;    
-    
-    test("should correctly base64 encode the data", function() {
+        lineno = 12;
+
+    test("should correctly capture the data", function() {
         Raven.process(message, fileurl, lineno, undefined, timestamp);
-        var decoded_data = JSON.parse(ajax_calls[0].data);
-        
-        equal(decoded_data['culprit'], fileurl);
-        equal(decoded_data['message'], message + " at " + lineno);
-        equal(decoded_data['logger'], "javascript");
-        equal(decoded_data['project'], 1);
-        equal(decoded_data['site'], null);
+        var data = JSON.parse(ajax_calls[0].data);
+
+        equal(data['culprit'], fileurl);
+        equal(data['message'], message + " at " + lineno);
+        equal(data['logger'], "javascript");
+        equal(data['project'], 1);
+        equal(data['site'], null);
     });
-    
+
     test("should correctly generate Sentry headers", function() {
         Raven.process(message, fileurl, lineno, undefined, timestamp);
         var values = parseAuthHeader(ajax_calls[0].headers['X-Sentry-Auth']);
-        
+
         equal(values.sentry_key, 'e89652ec30b94d9db6ea6f28580ab499',
               "sentry_key should match the public key");
-        
+
         // import hmac, base64, hashlib
         // message = base64.b64encode('{"message":"Once upon a midnight dreary at 12","culprit":"http://edgarallen.poe/nevermore/","sentry.interfaces.Stacktrace":{"frames":[{"filename":"http://edgarallen.poe/nevermore/","lineno":12}]},"sentry.interfaces.Exception":{"value":"Once upon a midnight dreary"},"project":1,"logger":"javascript"}')
         // hmac.new('77ec8c99a8854256aa68ccb91dd9119d', '1328155597571 %s' % message, hashlib.sha1).hexdigest()
@@ -42,13 +42,13 @@ $(document).ready(function() {
 	test("should hit an external url for signature if desired", function() {
 		Raven.config({"signatureUrl": "/api/sign-error/"});
 		Raven.process(message, fileurl, lineno, undefined, timestamp);
-		
+
 		// The first Ajax call in this case should be to the signature URL
 		equal(ajax_calls[0].url, '/api/sign-error/');
 		equal(ajax_calls[0].data.timestamp, timestamp);
 		notEqual(ajax_calls[1].headers['X-Sentry-Auth'].indexOf('dummy-signature'), -1);
 	});
-	
+
 	test("should omit 'at' if there is no line number provided", function() {
         Raven.process('ManuallyThrownError');
 
