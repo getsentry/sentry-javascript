eb27a16ed165c12df9b8a8bb8820d5d6c2613854
David Cramer <dcramer@gmail.com>
2012-05-01 20:12:27 -0700

    Add captureMessage

--- src/raven.js
+++ src/raven.js
@@ -133,6 +133,10 @@
         self.process(e, fileurl, lineno, traceback);
     };
 
+    Raven.captureMessage = function(msg) {
+        self.process(msg);
+    };
+
     Raven.chromeTraceback = function(e) {
         /*
          * First line is simply the repeated message:
