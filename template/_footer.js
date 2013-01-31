f1e796076afa93d9f8a4e0b44434cd20695f3e29
Matt Robenolt <matt@ydekproductions.com>
2013-01-31 11:54:37 -0800

    Make Raven compatible with CommonJS

--- template/_footer.js
+++ template/_footer.js
@@ -1,4 +1,9 @@
-// Expose Raven to the world
-window.Raven = Raven;
 
-})(window);
+// Expose Raven to the world
+if (typeof define === 'function' && define.amd) {
+    define(function() { return Raven; });
+} else {
+    window.Raven = Raven;
+}
+
+})(this);
