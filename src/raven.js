024da478688952c7d3db72ca738c6346155487a9
Matt Robenolt <matt@ydekproductions.com>
2013-02-07 22:04:36 -0800

    A little more reliable check for JSON support

--- src/raven.js
+++ src/raven.js
@@ -4,7 +4,7 @@
 // If there is no JSON, we no-op the core features of Raven
 // since JSON is required to encode the payload
 var _Raven = window.Raven,
-    hasJSON = !isUndefined(window.JSON),
+    hasJSON = !!(window.JSON && window.JSON.stringify),
     globalServer,
     globalUser,
     globalKey,
