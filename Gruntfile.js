8bc0a6d62334cabe30c97bc499c202ab2b5ec0c3
Matt Robenolt <matt@ydekproductions.com>
2013-10-20 10:55:47 -0700

    No jshint yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 
 };
