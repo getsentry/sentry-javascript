ed833ab66dca06ded63595141f8091b46341e525
Matt Robenolt <matt@ydekproductions.com>
2014-01-03 00:13:33 -0800

    2014!

--- template/_copyright.js
+++ template/_copyright.js
@@ -4,7 +4,7 @@
  * Includes TraceKit
  * https://github.com/getsentry/TraceKit
  *
- * Copyright 2013 Matt Robenolt and other contributors
+ * Copyright <%= grunt.template.today('yyyy') %> Matt Robenolt and other contributors
  * Released under the BSD license
  * https://github.com/getsentry/raven-js/blob/master/LICENSE
  *
