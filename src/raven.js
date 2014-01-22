e0bb8eff12a83d2f22a2e81a93c84771772653e9
frekw <fredrik@warnsberg.se>
2014-01-22 13:20:12 +0100

    Split V8Transport out into its own file.

--- src/raven.js
+++ src/raven.js
@@ -47,66 +47,6 @@ var HTTPGetTransport = {
     }
 };
 
-var V8Transport = {
-    setup: function(dsn, triggerEvent){
-        if(!this.hasCORS() && window.console && console.error){
-            console.error('This browser lacks support for CORS. Falling back to the default transport');
-            delete dsn.pass;
-            HTTPGetTransport.setup(dsn);
-            return HTTPGetTransport;
-        }
-
-        if(!dsn.pass)
-            throw new RavenConfigError('The https+post V8 transport needs the private key to be set in the DSN.');
-
-        this.triggerEvent = triggerEvent;
-        this.dsn = dsn;
-        return this;
-    },
-
-    hasCORS: function(){
-        return 'withCredentials' in new XMLHttpRequest();
-    },
-
-    getAuthString: function(){
-        if (this.cachedAuth) return this.cachedAuth;
-
-        var qs = [
-            'sentry_version=4',
-            'sentry_client=raven-js/' + Raven.VERSION,
-            'sentry_key=' + this.dsn.user,
-            'sentry_secret=' + this.dsn.pass
-        ];
-
-        return this.cachedAuth = 'Sentry ' + qs.join(',');
-    },
-
-    send: function(data, endpoint){
-        var xhr = new XMLHttpRequest(),
-            triggerEvent = this.triggerEvent;
-
-
-        xhr.open('POST', endpoint, true);
-        xhr.setRequestHeader('X-Sentry-Auth', this.getAuthString());
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
-        xhr.onload = function success() {
-            triggerEvent('success', {
-                data: data,
-                src: endpoint
-            });
-        };
-        xhr.onerror = xhr.onabort = function failure() {
-            triggerEvent('failure', {
-                data: data,
-                src: endpoint
-            });
-        };
-
-        xhr.send(JSON.stringify(data));
-    }
-};
-
 // First, check for JSON support
 // If there is no JSON, we no-op the core features of Raven
 // since JSON is required to encode the payload
