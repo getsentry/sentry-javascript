d8b7babc913325d8d7d079657ede5dd37be57c9f
Steve Lamb <steve@stevelamb.io>
2014-04-28 12:46:47 -0700

    removed dist changes

--- template/_footer.js
+++ template/_footer.js
@@ -1,9 +1,13 @@
 // Expose Raven to the world
-window.Raven = Raven;
 
 // AMD
 if (typeof define === 'function' && define.amd) {
     define('raven', [], function() { return Raven; });
 }
-
+else if (typeof module == 'object') {
+    module.exports = Raven;
+}
+else {
+    window.Raven = Raven;
+}
 })(this);
