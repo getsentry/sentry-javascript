2bcc39a738fc133a74ccbc4063f8ffd60b9c91f8
Marsel Zaripov <marszaripov@gmail.com>
2014-07-26 06:56:58 +0400

    Add ability to not configure and/or not install Raven in angular plugin
    By default it will configure and install Raven

--- plugins/angular.js
+++ plugins/angular.js
@@ -11,8 +11,6 @@ if (!angular) {
     return;
 }
 
-var isRavenInstalled = false;
-
 function ngRavenProvider($provide) {
     $provide.decorator('$exceptionHandler', [
         'RavenConfig', '$delegate',
@@ -24,10 +22,12 @@ function ngRavenExceptionHandler(RavenConfig, $delegate) {
     if (!RavenConfig)
         throw new Error('RavenConfig must be set before using this');
 
-    if (!isRavenInstalled) {
-        Raven.config(RavenConfig.dsn, RavenConfig.config).install();
-        isRavenInstalled = true;
-    }
+    if (!RavenConfig.notConfig)
+        Raven.config(RavenConfig.dsn, RavenConfig.config);
+
+    if (!RavenConfig.notInstall)
+        Raven.install();
+
     return function angularExceptionHandler(ex, cause) {
         $delegate(ex, cause);
         Raven.captureException(ex, {extra: {cause: cause}});
