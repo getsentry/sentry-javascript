44be7407fa77fe515fddf8447241edb094414013
Clay Walker <clayzermk1@gmail.com>
2014-09-16 10:36:51 -0700

    Added AMD global for plugins per UMDJS and feedback in #261. Added support for strict CommonJS per UMDJS.

--- template/_footer.js
+++ template/_footer.js
@@ -1,10 +1,15 @@
 // Expose Raven to the world
 if (typeof define === 'function' && define.amd) {
     // AMD
-    define('raven', [], function() { return Raven; });
+    define('raven', function(Raven) {
+      return (window.Raven = Raven);
+    });
 } else if (isObject(module)) {
-    // CommonJS
+    // browserify
     module.exports = Raven;
+} else if (isObject(exports)) {
+    // CommonJS
+    exports = Raven;
 } else {
     // Everything else
     window.Raven = Raven;
