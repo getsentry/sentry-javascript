e29930b13760aee7bdbbd20f533c9e96cb477211
Clay Walker <clayzermk1@gmail.com>
2014-09-15 17:01:59 -0700

    Added support for CommonJS/browserify.

--- template/_footer.js
+++ template/_footer.js
@@ -1,9 +1,13 @@
 // Expose Raven to the world
-window.Raven = Raven;
-
-// AMD
 if (typeof define === 'function' && define.amd) {
+    // AMD
     define('raven', [], function() { return Raven; });
+} else if (typeof module === 'object') {
+    // CommonJS
+    module.exports = Raven;
+} else {
+    // Everything else
+    window.Raven = Raven;
 }
 
 })(window);
