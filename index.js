4dba3d30bc9dc49ab61b9ef3a288ad40c147d603
Matt Robenolt <matt@ydekproductions.com>
2015-02-10 16:26:45 -0800

    Export parsers
    Fixes #111

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports.middleware = {
 // friendly alias for "raven.middleware.express"
 module.exports.middleware.express = module.exports.middleware.connect;
 module.exports.transports = require('./lib/transports');
+module.exports.parsers = require('./lib/parsers');
 
 // To infinity and beyond
 Error.stackTraceLimit = Infinity;
