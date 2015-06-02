0c2c392a90a2bbb8b607df29cee19ac4a220f504
stambata <stamen.peev@softwaregroup-bg.com>
2015-06-02 14:16:54 +0300

    Update raven.utils.js

--- test/raven.utils.js
+++ test/raven.utils.js
@@ -97,6 +97,23 @@ describe('raven.utils', function () {
         lol: 1
       }], callback);
     });
+
+    it('should extract context from last stack line', function(done){
+      var parseStack = raven.utils.parseStack;
+      var callback = function(frames) {
+        var frame = frames.pop();
+        frame.pre_context.should.be.an.instanceOf(Array);
+        frame.context_line.should.be.type('string');
+        frame.context_line.should.endWith('undeclared_function();');
+        frame.post_context.should.be.an.instanceOf(Array);
+        done();
+      };
+      try {
+        undeclared_function();
+      } catch(e) {
+        parseStack(e, callback);
+      }
+    });
   });
 
   describe('#getCulprit()', function () {
