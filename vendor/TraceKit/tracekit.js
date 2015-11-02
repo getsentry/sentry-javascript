32965d97e29dbfb6ffbbf825a788ae6abfbdcc27
Matt Robenolt <matt@ydekproductions.com>
2015-11-02 11:11:37 -0800

    TraceKit.wrap isn't used or needed

--- vendor/TraceKit/tracekit.js
+++ vendor/TraceKit/tracekit.js
@@ -15,24 +15,6 @@ var TraceKit = {
 var _slice = [].slice;
 var UNKNOWN_FUNCTION = '?';
 
-/**
- * TraceKit.wrap: Wrap any function in a TraceKit reporter
- * Example: func = TraceKit.wrap(func);
- *
- * @param {Function} func Function to be wrapped
- * @return {Function} The wrapped func
- */
-TraceKit.wrap = function traceKitWrapper(func) {
-    function wrapped() {
-        try {
-            return func.apply(this, arguments);
-        } catch (e) {
-            TraceKit.report(e);
-            throw e;
-        }
-    }
-    return wrapped;
-};
 
 function getLocationHref() {
     if (typeof document === 'undefined')
