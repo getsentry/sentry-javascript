1be00c431d54d6c7381af1caed3ef946e64897cd
Lewis Ellis <lewis@getsentry.com>
2016-11-29 18:16:39 -0800

    Tracekit window defensiveness

--- vendor/TraceKit/tracekit.js
+++ vendor/TraceKit/tracekit.js
@@ -10,6 +10,11 @@ var TraceKit = {
     debug: false
 };
 
+var _window = typeof window !== 'undefined' ? window
+            : typeof global !== 'undefined' ? global
+            : typeof self !== 'undefined' ? self
+            : {};
+
 // global reference to slice
 var _slice = [].slice;
 var UNKNOWN_FUNCTION = '?';
@@ -188,8 +193,8 @@ TraceKit.report = (function reportModuleWrapper() {
         if (_onErrorHandlerInstalled) {
             return;
         }
-        _oldOnerrorHandler = window.onerror;
-        window.onerror = traceKitWindowOnError;
+        _oldOnerrorHandler = _window.onerror;
+        _window.onerror = traceKitWindowOnError;
         _onErrorHandlerInstalled = true;
     }
 
@@ -198,7 +203,7 @@ TraceKit.report = (function reportModuleWrapper() {
         if (!_onErrorHandlerInstalled) {
             return;
         }
-        window.onerror = _oldOnerrorHandler;
+        _window.onerror = _oldOnerrorHandler;
         _onErrorHandlerInstalled = false;
         _oldOnerrorHandler = undefined;
     }
