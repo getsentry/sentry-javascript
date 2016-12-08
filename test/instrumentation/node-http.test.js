6e140160638fb13b6fbcb1b6fcc3b56798a87d49
Lewis Ellis <lewis@getsentry.com>
2016-12-07 20:09:27 -0800

    Drop 0.10 and 0.12 from instrumentation tests, make instr tests work on 4.x

--- test/instrumentation/node-http.test.js
+++ test/instrumentation/node-http.test.js
@@ -3,10 +3,13 @@ var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 
+// running on pre-4.0 is hard, somewhat different test scheme
+if (process.version < 'v4') process.exit(0);
+
 var nodeRoot = process.argv[2];
 var testRoot = path.join(nodeRoot, 'test/parallel');
 var testFiles = fs.readdirSync(testRoot).filter(function (filename) {
-  return filename.startsWith('test-http');
+  return filename.indexOf('test-http') === 0;
 });
 
 var failedTests = [];
@@ -26,8 +29,12 @@ testFiles.forEach(function (filename) {
 
 console.log('Finished, failures: ' + failedTests.length + ', successes: ' + numSuccesses);
 
+// pipeline-flood is a harness issue with how it expects argvs
+// header response splitting is something about becoming more strict on allowed chars in headers
+// not worried about either one; they also fail without our instrumentation
 var knownFailures = [
-  'test-http-pipeline-flood.js'
+  'test-http-pipeline-flood.js',
+  'test-http-header-response-splitting.js'
 ];
 
 var didPass = failedTests.every(function (filename) {
