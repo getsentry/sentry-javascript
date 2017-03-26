640ae7cbde2998c6784d440bed16688fb1df8777
Ben Vinegar <ben@benv.ca>
2017-03-26 09:57:29 -0700

    Allow manager.js to run from project root

--- test/manual/manager.js
+++ test/manual/manager.js
@@ -1,10 +1,15 @@
 'use strict';
+
+var path = require('path');
 var child_process = require('child_process');
 
 var child;
+
+var serverPath = path.join(__dirname, 'express-patient.js');
+
 function startChild() {
   console.log('starting child');
-  child = child_process.spawn('node', ['express-patient.js']);
+  child = child_process.spawn('node', [serverPath]);
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
   child.on('exit', function () {
