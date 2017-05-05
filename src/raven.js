5d9a3aae25a6b14e3a46642f2e5bf2e3c99a316f
Lewis Ellis <lewis@getsentry.com>
2017-05-05 12:02:16 -0700

    3.15.0

--- src/raven.js
+++ src/raven.js
@@ -91,7 +91,7 @@ Raven.prototype = {
     // webpack (using a build step causes webpack #1617). Grunt verifies that
     // this value matches package.json during build.
     //   See: https://github.com/getsentry/raven-js/issues/465
-    VERSION: '3.14.2',
+    VERSION: '3.15.0',
 
     debug: false,
 
