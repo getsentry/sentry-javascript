cadda13d4853b9074a09dc9a17a80756952d2ac9
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-06-27 15:18:24 +0200

    Fix android react native release builds stacktrace

--- plugins/react-native.js
+++ plugins/react-native.js
@@ -33,9 +33,13 @@ var ASYNC_STORAGE_KEY = '--raven-js-global-error-payload--';
  * Strip device-specific IDs from React Native file:// paths
  */
 function normalizeUrl(url, pathStripRe) {
-    return url
-        .replace(/^file\:\/\//, '')
-        .replace(pathStripRe, '');
+  if (url.indexOf('/') !== -1) {
+      return url
+          .replace(/^file\:\/\//, '')
+          .replace(pathStripRe, '');
+  } else {
+      return '/' + url;
+  }
 }
 
 /**
