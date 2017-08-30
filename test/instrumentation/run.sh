ed646d5b16d5d3c1305d3dc9d1ab1ec4cdbd9db5
Kamil Ogórek <kamil.ogorek@gmail.com>
2017-08-30 14:06:52 +0200

    Remove node archive after extracting

--- test/instrumentation/run.sh
+++ test/instrumentation/run.sh
@@ -7,5 +7,6 @@ if [ ! -d $nodeRoot ]; then
   url="https://codeload.github.com/nodejs/node/tar.gz/$version"
   curl $url -o "$version.tar.gz"
   tar -xf "$version.tar.gz"
+  rm "$version.tar.gz"
 fi
 exec node http.test.js `pwd`/$nodeRoot
