c7971b7ab6db9a3c67bcef644187db70cd88f189
Kamil Ogórek <kamil.ogorek@gmail.com>
2017-09-08 14:13:24 +0200

    Add test for maxReqQueueCount

---
+++ test/raven.transports.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var transports = require('../lib/transports');
+
+describe.only('transports', function() {
+  it('should emit error when requests queued over the limit', function(done) {
+    var http = transports.http;
+    var _cachedAgent = http.options.agent;
+
+    http.options.agent = {
+      requests: {
+        'foo:1234': Array.from({length: 10}).fill('req')
+      }
+    };
+
+    http.send({
+      dsn: {
+        host: 'foo',
+        port: 1234
+      },
+      maxReqQueueCount: 5,
+      emit: function(event, body) {
+        event.should.equal('error');
+        body.message.should.equal('client req queue is full..');
+        http.options.agent = _cachedAgent;
+        done();
+      }
+    });
+  });
+});
