26830228c020b20bf73f3747229eede00acedd82
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-11-14 16:33:50 +0100

    fix: Browser tests

--- packages/browser/__tests__/index.ts
+++ packages/browser/__tests__/index.ts
@@ -1,39 +1,49 @@
 /// <reference types="node" />
 /// <reference types="jest" />
-/// <reference types="puppeteer" />
+import * as Sentry from '@sentry/core';
+import * as fs from 'fs';
 import * as http from 'http';
 import * as path from 'path';
-import * as fs from 'fs';
-import * as Sentry from '@sentry/core';
-import { Browser } from '../browser';
 import * as Puppeteer from 'puppeteer';
+import { Browser } from '../browser';
 
 describe('Browser Interface', () => {
   test('sending a message', async done => {
-    expect.assertions(1);
+    let countExcpects = 0;
+    const expectedAssertions = 2;
+    expect.assertions(expectedAssertions);
 
-    let server = http
-      .createServer(function(request, response) {
+    const server = http
+      .createServer((request, response) => {
         if (request.url === '/index.html') {
           response.writeHead(200, { 'Content-Type': 'text/html' });
-          let contents = fs.readFileSync(path.join(__dirname, './index.html'));
+          const contents = fs.readFileSync(path.join(__dirname, './index.html'));
           response.write(contents);
         }
         if (request.url === '/dist/bundle.js') {
           response.writeHead(200, { 'Content-Type': 'text/javscript' });
-          let contents = fs.readFileSync(path.join(__dirname, '../dist/bundle.js'));
+          const contents = fs.readFileSync(path.join(__dirname, '../dist/bundle.js'));
           response.write(contents);
         }
         response.end();
       })
       .listen(8999);
 
-    let browser = await Puppeteer.launch({ headless: true });
+    const browser = await Puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+
     page.on('request', async request => {
-      if (request.resourceType === 'XHR') {
-        let data = JSON.parse(<any>request.postData);
-        expect(data.message).toBe('PICKLE RIIIICK!');
+      // @ts-ignore
+      if (request.resourceType === 'xhr') {
+        const data = JSON.parse(request.postData as any);
+        if (data.exception) {
+          expect(data.exception).not.toBeUndefined();
+        } else {
+          expect(data.message).toBe('PICKLE RIIIICK!');
+        }
+        countExcpects++;
+      }
+      if (countExcpects === expectedAssertions) {
         await browser.close();
         done();
       }
