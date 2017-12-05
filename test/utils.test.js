f3150c174d829ca4f520b4a75b0dd5d74b4c60c1
Sherwood Callaway <shcallaway@gmail.com>
2017-12-05 08:10:27 -0800

    Move assertion from ErrorEvent test to Error test

--- test/utils.test.js
+++ test/utils.test.js
@@ -89,7 +89,6 @@ describe('utils', function() {
     describe('isErrorEvent', function() {
       it('should work as advertised', function() {
         assert.isFalse(isErrorEvent(new Error()));
-        assert.isFalse(isError(new ErrorEvent('')));
         assert.isTrue(isErrorEvent(new ErrorEvent('')));
       });
     });
@@ -121,6 +120,11 @@ describe('utils', function() {
     it('should work as advertised', function() {
       assert.isTrue(isError(new Error()));
       assert.isTrue(isError(new ReferenceError()));
+
+      if (supportsErrorEvent()) {
+        assert.isFalse(isError(new ErrorEvent('')));
+      }
+
       assert.isTrue(isError(new RavenConfigError()));
       assert.isTrue(isError(testErrorFromDifferentContext(fromContext)));
       assert.isTrue(isError(testErrorFromDifferentContext(domException)));
