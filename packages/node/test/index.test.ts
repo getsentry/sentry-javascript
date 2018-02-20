6b16e744310ec78e921fe89fc1147c277c087a51
Daniel Griesser <daniel.griesser.86@gmail.com>
2018-02-20 13:24:33 +0100

    feat: Use jest for node test

--- packages/node/test/index.test.ts
+++ packages/node/test/index.test.ts
@@ -5,13 +5,17 @@ import { SentryNode } from '../src/index';
 
 describe('Test', () => {
   beforeEach(async () => {
-    await Sentry.create('https://53039209a22b4ec1bcc296a3c9fdecd6@sentry.io/4291')
+    await Sentry.create(
+      'https://53039209a22b4ec1bcc296a3c9fdecd6@sentry.io/4291',
+    )
       .use(SentryNode)
       .install()
       .then(client => client.setContext({ extra: { abc: 'def' } }));
   });
 
   it('works', async () => {
-    expect(await Sentry.getSharedClient().getContext()).to.deep.equal({ extra: { abc: 'def' } });
+    expect(await Sentry.getSharedClient().getContext()).to.deep.equal({
+      extra: { abc: 'def' },
+    });
   });
 });
