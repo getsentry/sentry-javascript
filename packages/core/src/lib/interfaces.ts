adfde816cce686dbb96e8b55a833123d3d2d9023
Jan Michael Auer <jan.auer@sentry.io>
2018-03-05 10:59:49 +0100

    feat(core): Implement the full event interface

--- packages/core/src/lib/interfaces.ts
+++ packages/core/src/lib/interfaces.ts
@@ -30,9 +30,75 @@ export interface Context {
   user?: User;
 }
 
+export interface SdkInfo {
+  version?: string;
+  name?: string;
+  integrations?: string[];
+}
+
+export interface StackFrame {
+  filename?: string;
+  function?: string;
+  module?: string;
+  platform?: string;
+  lineno?: number;
+  colno?: number;
+  abs_path?: string;
+  context_line?: string;
+  pre_context?: string;
+  post_context?: string;
+  in_app?: boolean;
+  vars?: { [name: string]: any };
+}
+
+export interface Stacktrace {
+  frames?: StackFrame[];
+  frames_omitted?: [number, number];
+}
+
+export interface Thread {
+  id?: number;
+  name?: string;
+  stacktrace?: Stacktrace;
+  crashed?: boolean;
+  current?: boolean;
+}
+
+export interface SentryException {
+  type?: string;
+  value?: string;
+  module?: string;
+  thread_id?: number;
+  stacktrace?: Stacktrace;
+}
+
+export interface Request {
+  url?: string;
+  method?: string;
+  data?: any;
+  query_string?: string;
+  cookies?: { [key: string]: string };
+  env?: { [key: string]: string };
+}
+
 // TODO: Add missing fields
 export interface SentryEvent extends Context {
+  event_id?: string;
   message?: string;
+  timestamp?: number;
+  level?: Severity;
+  platform?: string;
+  logger?: string;
+  server?: string;
+  release?: string;
+  dist?: string;
+  environment?: string;
+  sdk?: SdkInfo;
+  request?: Request;
+  modules?: { [key: string]: string };
+  fingerprint?: string[];
+  exception?: SentryException[];
+  stacktrace?: Stacktrace;
   breadcrumbs?: Breadcrumb[];
 }
 
