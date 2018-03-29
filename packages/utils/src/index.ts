aef481c8cc204e78e82d5d342ca0f8a2ecabbcc1
Jan Michael Auer <jan.auer@sentry.io>
2018-03-29 15:29:47 +0200

    ref(utils): Flatten source code structure

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,4 @@
-export { forget, filterAsync } from './lib/async';
-export { mkdirp, mkdirpSync } from './lib/fs';
-export { clone, deserialize, serialize } from './lib/object';
-export { Store } from './lib/store';
+export { forget, filterAsync } from './async';
+export { mkdirp, mkdirpSync } from './fs';
+export { clone, deserialize, serialize } from './object';
+export { Store } from './store';
