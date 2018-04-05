b478429449ea2eea3267e5d1b2ce2abbb8e1d8b0
kumavis <kumavis@users.noreply.github.com>
2018-04-05 03:41:06 -0700

    feat: add flag for unhandled promise rejections (#1278)

--- src/raven.js
+++ src/raven.js
@@ -426,7 +426,11 @@ Raven.prototype = {
    */
   _promiseRejectionHandler: function(event) {
     this._logDebug('debug', 'Raven caught unhandled promise rejection:', event);
-    this.captureException(event.reason);
+    this.captureException(event.reason, {
+      extra: {
+        unhandledPromiseRejection: true
+      }
+    });
   },
 
   /**
