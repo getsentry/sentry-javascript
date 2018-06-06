05b22e5fd1e189700904ad71cc88e87ff552b9fb
HazA <daniel.griesser.86@gmail.com>
2018-06-06 15:50:38 +0200

    meta: Remove old stuff

--- karma/karma.unit.config.js
+++
@@ -1,9 +0,0 @@
-var commonConfig = require('./karma.config');
-
-module.exports = function(config) {
-  var testConfig = Object.assign({}, commonConfig, {
-    logLevel: config.LOG_INFO,
-    files: ['build/raven.test.js']
-  });
-  config.set(testConfig);
-};
