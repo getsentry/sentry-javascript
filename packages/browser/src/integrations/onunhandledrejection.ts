2673f62db2a3506a43fc2b93febabdb3e3041a7a
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-06-08 13:07:46 +0200

    Browser integrations

---
+++ packages/browser/src/integrations/onunhandledrejection.ts
@@ -0,0 +1,32 @@
+import { captureException } from '@sentry/shim';
+import { Integration } from '@sentry/types';
+
+/** onunhandledrejection is not standardized, thus not available on Window type */
+interface PromisifiedWindow extends Window {
+  onunhandledrejection?(event: PromiseRejectionEvent): void;
+}
+
+/** TODO: Change to safe window access, window||global||self||{} */
+const _window: PromisifiedWindow = window;
+
+/** Global Promise Rejection handler */
+export class OnUnhandledRejection implements Integration {
+  /**
+   * @inheritDoc
+   */
+  public name: string = 'OnUnhandledRejection';
+  /**
+   * @inheritDoc
+   */
+  public handler(event: PromiseRejectionEvent): void {
+    captureException(event.reason);
+  }
+  /**
+   * @inheritDoc
+   */
+  public install(): void {
+    _window.addEventListener('unhandledrejection', this.handler.bind(
+      this,
+    ) as EventListener);
+  }
+}
