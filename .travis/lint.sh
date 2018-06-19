28a368e42ef4ae8ac927570945c67b780536a268
HazA <daniel.griesser.86@gmail.com>
2018-06-19 14:49:50 +0200

    feat: Add lint for raven
    [force ci]

--- .travis/lint.sh
+++ .travis/lint.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 set -e
 
+# Run @sentry/*
 yarn && yarn build && yarn lint
+
+# Run raven-node
+if [[ ("$RAVEN_NODE_CHANGES" = "true" || "$TRAVIS_PULL_REQUEST" = "false" ) ]]; then
+  cd packages/raven-node
+  npm install
+  npm run lint
+  cd ../..
+fi
+
+# Run raven-js
+if [[ ("$RAVEN_JS_CHANGES" = "true" || "$TRAVIS_PULL_REQUEST" = "false" ) ]]; then
+  cd packages/raven-js
+  npm install
+  npm run lint
+  cd ../..
+fi
