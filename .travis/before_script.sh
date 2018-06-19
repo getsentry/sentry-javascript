4bb8bfbb0cfa05ad28485e5c70bb8b1dc42cef50
HazA <daniel.griesser.86@gmail.com>
2018-06-19 15:09:52 +0200

    fix: Source before
    [force ci]

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -4,21 +4,21 @@ set -e
 CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
 
 if [ -n "$(grep 'raven-js' <<< "$CHANGES")" ]; then
-  export RAVEN_JS_CHANGES=true
+  RAVEN_JS_CHANGES=true
 fi
 
 if [ -n "$(grep 'raven-node' <<< "$CHANGES")" ]; then
-  export RAVEN_NODE_CHANGES=true
+  RAVEN_NODE_CHANGES=true
 fi
 
 FORCE=$(git log --format=%B --no-merges -n 1)
 
 if [ -n "$(grep '\[force ci\]' <<< "$FORCE")" ]; then
-  export RAVEN_JS_CHANGES=true
-  export RAVEN_NODE_CHANGES=true
+  RAVEN_JS_CHANGES=true
+  RAVEN_NODE_CHANGES=true
 fi
 
 NODE_VERSION=$(node -v);
 if  [ ${NODE_VERSION:1:1} > 5 ]; then
-  export PACKAGES=true
+  PACKAGES=true
 fi
