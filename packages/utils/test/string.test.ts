e3d862d496e3e400c2ec8e354b04f9535baa730b
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-06-21 14:42:48 +0200

    test: Fix string utils tests

--- packages/utils/test/string.test.ts
+++ packages/utils/test/string.test.ts
@@ -1,10 +1,12 @@
 import { truncate } from '../src';
 
 describe('truncate()', () => {
-  expect(truncate('lolol', 3)).toEqual('lol\u2026');
-  expect(truncate('lolol', 10)).toEqual('lolol');
-  expect(truncate('lol', 3)).toEqual('lol');
-  expect(truncate(new Array(1000).join('f'), 0)).toEqual(
-    new Array(1000).join('f'),
-  );
+  test('it works as expected', () => {
+    expect(truncate('lolol', 3)).toEqual('lol\u2026');
+    expect(truncate('lolol', 10)).toEqual('lolol');
+    expect(truncate('lol', 3)).toEqual('lol');
+    expect(truncate(new Array(1000).join('f'), 0)).toEqual(
+      new Array(1000).join('f'),
+    );
+  });
 });
