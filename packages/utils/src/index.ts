d29f44c811e3ac103840b1673c424667d9f39bee
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-06-22 11:30:35 +0200

    test: Core and browser transport tests

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,7 +3,7 @@ export { mkdirp, mkdirpSync } from './fs';
 export { clone, deserialize, fill, serialize, urlEncode } from './object';
 export { Store } from './store';
 export { isError, isErrorEvent, isDOMError, isDOMException } from './is';
-export { truncate, uuid4 } from './string';
+export { truncate } from './string';
 export {
   supportsDOMError,
   supportsDOMException,
@@ -11,4 +11,4 @@ export {
   supportsFetch,
   supportsReferrerPolicy,
 } from './supports';
-export { getGlobalObject } from './misc';
+export { getGlobalObject, uuid4 } from './misc';
