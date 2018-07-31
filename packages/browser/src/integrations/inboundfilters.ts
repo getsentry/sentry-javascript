cad96288e08d04fb836e99722f7c2d6d74a1cb40
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-07-31 15:04:16 +0200

    integration: Fix inboundFilter methods

--- packages/browser/src/integrations/inboundfilters.ts
+++ packages/browser/src/integrations/inboundfilters.ts
@@ -38,49 +38,48 @@ export class InboundFilters implements Integration {
   }
 
   /** JSDoc */
-  private configureOptions(options: BrowserOptions): void {
-    if (options.ignoreErrors) {
-      // TODO: Afair people wanted an option to disable defaults. Should we do it?
-      this.ignoreErrors = joinRegExp([...DEFAULT_IGNORE_ERRORS, ...options.ignoreErrors]);
-    }
-    if (options.blacklistUrls) {
-      this.blacklistUrls = joinRegExp([...options.blacklistUrls]);
-    }
-    if (options.whitelistUrls) {
-      this.whitelistUrls = joinRegExp([...options.whitelistUrls]);
-    }
-  }
-
-  /** JSDoc */
-  private shouldDropEvent(event: SentryEvent): boolean {
+  public shouldDropEvent(event: SentryEvent): boolean {
     if (this.isIgnoredError(event)) {
-      logger.warn(`Event dropped due to being ignored.\n  Event: ${event.event_id}`);
+      logger.warn(`Event dropped due to being matched by \`ignoreErrors\` option.\n  Event: ${event.event_id}`);
       return true;
     }
     if (this.isBlacklistedUrl(event)) {
-      logger.warn(`Event dropped due to being blacklisted.\n  Event: ${event.event_id}`);
+      logger.warn(`Event dropped due to being matched by \`blacklistUrls\` option.\n  Event: ${event.event_id}`);
       return true;
     }
     if (!this.isWhitelistedUrl(event)) {
-      logger.warn(`Event dropped due to not being whitelisted.\n  Event: ${event.event_id}`);
+      logger.warn(`Event dropped due to not being matched by \`whitelistUrls\` option.\n  Event: ${event.event_id}`);
       return true;
     }
     return false;
   }
 
   /** JSDoc */
-  private isIgnoredError(event: SentryEvent): boolean {
+  public isIgnoredError(event: SentryEvent): boolean {
     return this.getPossibleEventMessages(event).some(message => this.ignoreErrors.test(message));
   }
 
   /** JSDoc */
-  private isWhitelistedUrl(event: SentryEvent): boolean {
+  public isWhitelistedUrl(event: SentryEvent): boolean {
     return this.whitelistUrls.test(this.getEventFilterUrl(event));
   }
 
   /** JSDoc */
-  private isBlacklistedUrl(event: SentryEvent): boolean {
+  public isBlacklistedUrl(event: SentryEvent): boolean {
     return this.blacklistUrls.test(this.getEventFilterUrl(event));
+  }
+
+  /** JSDoc */
+  private configureOptions(options: BrowserOptions): void {
+    if (options.ignoreErrors && options.ignoreErrors.length) {
+      this.ignoreErrors = joinRegExp([...DEFAULT_IGNORE_ERRORS, ...options.ignoreErrors]);
+    }
+    if (options.blacklistUrls && options.blacklistUrls.length) {
+      this.blacklistUrls = joinRegExp([...options.blacklistUrls]);
+    }
+    if (options.whitelistUrls && options.whitelistUrls.length) {
+      this.whitelistUrls = joinRegExp([...options.whitelistUrls]);
+    }
   }
 
   /** JSDoc */
