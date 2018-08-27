c72f6b9ac12f37c1b5f098586a743149dadee51e
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-08-27 16:01:21 +0200

    misc: Update license template for raven-js

--- packages/raven-js/template/_copyright.js
+++ packages/raven-js/template/_copyright.js
@@ -4,8 +4,8 @@
  * Includes TraceKit
  * https://github.com/getsentry/TraceKit
  *
- * Copyright <%= grunt.template.today('yyyy') %> Matt Robenolt and other contributors
- * Released under the BSD license
- * https://github.com/getsentry/raven-js/blob/master/LICENSE
+ * Copyright (c) <%= grunt.template.today('yyyy') %> Sentry (https://sentry.io) and individual contributors.
+ * All rights reserved.
+ * https://github.com/getsentry/sentry-javascript/blob/master/packages/raven-js/LICENSE
  *
  */
