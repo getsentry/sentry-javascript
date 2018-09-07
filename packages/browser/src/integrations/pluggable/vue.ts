8758a9462d3e19eda3fde61b072089b80144e8df
HazA <daniel.griesser.86@gmail.com>
2018-09-07 11:18:26 +0200

    feat: Add integrations to sdkinfo

--- packages/browser/src/integrations/pluggable/vue.ts
+++ packages/browser/src/integrations/pluggable/vue.ts
@@ -1,5 +1,5 @@
 import { getCurrentHub, Scope } from '@sentry/hub';
-import { Integration } from '@sentry/types';
+import { Integration, SentryEvent } from '@sentry/types';
 import { isPlainObject, isUndefined } from '@sentry/utils/is';
 import { getGlobalObject } from '@sentry/utils/misc';
 
@@ -74,6 +74,17 @@ export class Vue implements Integration {
         getCurrentHub().configureScope((scope: Scope) => {
           Object.keys(metadata).forEach(key => {
             scope.setExtra(key, metadata[key]);
+          });
+
+          scope.addEventProcessor(async (event: SentryEvent) => {
+            if (event.sdk) {
+              const integrations = event.sdk.integrations || [];
+              event.sdk = {
+                ...event.sdk,
+                integrations: [...integrations, 'vue'],
+              };
+            }
+            return event;
           });
         });
 
