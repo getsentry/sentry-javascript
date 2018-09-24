8cdb7f8231dcfb2dc19a27fbcb1694a5c205883a
HazA <daniel.griesser.86@gmail.com>
2018-09-24 14:53:08 +0200

    ref: Add ReportDialogOptions

--- packages/browser/src/client.ts
+++ packages/browser/src/client.ts
@@ -4,6 +4,31 @@ import { getGlobalObject } from '@sentry/utils/misc';
 import { BrowserBackend, BrowserOptions } from './backend';
 
 /**
+ * All properties the report dialog supports
+ */
+export interface ReportDialogOptions {
+  [key: string]: any;
+  eventId?: string;
+  dsn?: DsnLike;
+  user?: {
+    email?: string;
+    name?: string;
+  };
+  lang?: string;
+  title?: string;
+  subtitle?: string;
+  subtitle2?: string;
+  labelName?: string;
+  labelEmail?: string;
+  labelComments?: string;
+  labelClose?: string;
+  labelSubmit?: string;
+  errorGeneric?: string;
+  errorFormEntry?: string;
+  successMessage?: string;
+}
+
+/**
  * The Sentry Browser SDK Client.
  *
  * @see BrowserOptions for documentation on configuration options.
@@ -28,27 +53,7 @@ export class BrowserClient extends BaseClient<BrowserBackend, BrowserOptions> {
   }
 
   /** JSDoc */
-  public showReportDialog(options: {
-    [key: string]: any;
-    eventId?: string;
-    dsn?: DsnLike;
-    user?: {
-      email?: string;
-      name?: string;
-    };
-    lang?: string;
-    title?: string;
-    subtitle?: string;
-    subtitle2?: string;
-    labelName?: string;
-    labelEmail?: string;
-    labelComments?: string;
-    labelClose?: string;
-    labelSubmit?: string;
-    errorGeneric?: string;
-    errorFormEntry?: string;
-    successMessage?: string;
-  }): void {
+  public showReportDialog(options: ReportDialogOptions = {}): void {
     // doesn't work without a document (React Native)
     const document = (getGlobalObject() as Window).document;
     if (!document) {
