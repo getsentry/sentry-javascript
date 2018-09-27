46a6a6d67eefde2b9e68d84f818d79032ed97d8f
HazA <daniel.griesser.86@gmail.com>
2018-09-27 09:50:24 +0200

    meta: Move all script into scripts folder, Lint json output

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,11 +1,23 @@
 import { exec } from 'child_process';
 import { danger, message, schedule, warn } from 'danger';
 import { promisify } from 'util';
+import { resolve } from 'path';
+// import tslint from 'danger-plugin-tslint';
+
+const packages = ['browser', 'core', 'hub', 'minimal', 'node', 'types', 'utils'];
 
 export default async () => {
   if (!danger.github) {
     return;
   }
+
+  packages.map(packageName => {
+    console.log(resolve(__dirname, 'packages', packageName, 'lint-results.json'));
+  });
+
+  // tslint({
+  //   lintResultsJsonPath: resolve(__dirname, 'lint-results.json'),
+  // });
 
   const hasChangelog = danger.git.modified_files.indexOf('CHANGELOG.md') !== -1;
   const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes('#trivial');
--- scripts/danger.sh
+++ scripts/danger.sh
@@ -4,4 +4,5 @@ set -e
 yarn
 # We have to build it first, so that TypeScript Types are recognized correctly
 yarn build
+yarn lint:json
 yarn danger ci
