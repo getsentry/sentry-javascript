59f2af20f649fe65af70854e5aabd0e95bc35732
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-10-06 10:35:53 +0200

    misc: Update changelog

--- packages/browser/test/karma/karma.saucelabs.config.js
+++ packages/browser/test/karma/karma.saucelabs.config.js
@@ -94,6 +94,8 @@ module.exports = function(config) {
     browserNoActivityTimeout: 600000,
     captureTimeout: 600000,
     sauceLabs: {
+      // NOTE: To run tests locally, change `startConnect` to `true` and use command:
+      // $ SAUCE_USERNAME=sentryio SAUCE_ACCESS_KEY=<key> yarn test:saucelabs
       startConnect: false,
       // Just something "random" so we don't have to provide additional ENV var when running locally
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER || Math.ceil(Math.random() * 1337),
