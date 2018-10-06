a4715b75d7e77075d07159199cbef792a8965ad9
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-10-06 10:24:21 +0200

    feat: Introduce stringify and debugger options in Debug integration

--- packages/core/src/integrations/pluggable/debug.ts
+++ packages/core/src/integrations/pluggable/debug.ts
@@ -1,5 +1,11 @@
 import { configureScope } from '@sentry/minimal';
-import { Integration, SentryEvent } from '@sentry/types';
+import { Integration, SentryEvent, SentryEventHint } from '@sentry/types';
+
+/** JSDoc */
+interface DebugOptions {
+  stringify?: boolean;
+  debugger?: boolean;
+}
 
 /** JSDoc */
 export class Debug implements Integration {
@@ -8,14 +14,44 @@ export class Debug implements Integration {
    */
   public name: string = 'Debug';
 
+  /** JSDoc */
+  private readonly options: DebugOptions;
+
+  /**
+   * @inheritDoc
+   */
+  public constructor(options?: DebugOptions) {
+    this.options = {
+      debugger: false,
+      stringify: false,
+      ...options,
+    };
+  }
+
   /**
    * @inheritDoc
    */
   public install(): void {
     configureScope(scope => {
-      scope.addEventProcessor(async (event: SentryEvent) => {
-        // tslint:disable-next-line:no-console
-        console.log(event);
+      scope.addEventProcessor(async (event: SentryEvent, hint?: SentryEventHint) => {
+        // tslint:disable:no-console
+        // tslint:disable:no-debugger
+
+        if (this.options.debugger) {
+          debugger;
+        }
+
+        if (this.options.stringify) {
+          console.log(JSON.stringify(event, null, 2));
+          if (hint) {
+            console.log(JSON.stringify(hint, null, 2));
+          }
+        } else {
+          console.log(event);
+          if (hint) {
+            console.log(hint);
+          }
+        }
         return event;
       });
     });
