9dc8a17adba8bdc931fa7972d61801a4407d9200
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-11-28 14:52:46 +0100

    test: Add integration test for blacklistUrls config

--- packages/browser/test/integration/test.js
+++ packages/browser/test/integration/test.js
@@ -101,6 +101,56 @@ for (var idx in frames) {
             }
           );
         });
+
+        it('should allow to ignore specific urls', function(done) {
+          var iframe = this.iframe;
+
+          iframeExecute(
+            iframe,
+            done,
+            function() {
+              /**
+               * We always filter on the caller, not the cause of the error
+               *
+               * > foo.js file called a function in bar.js
+               * > bar.js file called a function in baz.js
+               * > baz.js threw an error
+               *
+               * foo.js is blacklisted in the `init` call (init.js), thus we filter it
+               * */
+              var urlWithBlacklistedUrl = new Error('filter');
+              urlWithBlacklistedUrl.stack =
+                'Error: bar\n' +
+                ' at baz(http://localhost:5000/baz.js:2:9)\n' +
+                ' at bar(http://localhost:5000/bar.js:2:3)\n' +
+                ' at http://localhost:5000/foo.js:7:19';
+
+              /**
+               * > foo-pass.js file called a function in bar-pass.js
+               * > bar-pass.js file called a function in baz-pass.js
+               * > baz-pass.js threw an error
+               *
+               * foo-pass.js is *not* blacklisted in the `init` call (init.js), thus we don't filter it
+               * */
+              var urlWithoutBlacklistedUrl = new Error('pass');
+              urlWithoutBlacklistedUrl.stack =
+                'Error: bar\n' +
+                ' at baz(http://localhost:5000/baz-pass.js:2:9)\n' +
+                ' at bar(http://localhost:5000/bar-pass.js:2:3)\n' +
+                ' at http://localhost:5000/foo-pass.js:7:19';
+
+              Sentry.captureException(urlWithBlacklistedUrl);
+              Sentry.captureException(urlWithoutBlacklistedUrl);
+            },
+            function(sentryData) {
+              if (debounceAssertEventCount(sentryData, 1, done)) {
+                assert.equal(sentryData[0].exception.values[0].type, 'Error');
+                assert.equal(sentryData[0].exception.values[0].value, 'pass');
+                done();
+              }
+            }
+          );
+        });
       });
 
       describe('API', function() {
