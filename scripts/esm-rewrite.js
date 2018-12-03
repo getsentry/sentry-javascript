2a8e2f8810cdb0f9451a8544b437371dae58c63c
cromefire <tim.langhorst@outlook.de>
2018-12-03 18:29:02 +0100

    improved and documented rewrite regex
    Signed-off-by: cromefire <tim.langhorst@outlook.de>

--- scripts/esm-rewrite.js
+++ scripts/esm-rewrite.js
@@ -1,9 +1,35 @@
 const { relative } = require('path');
 const replace = require('replace-in-file');
+
+/**
+ * capture group 1
+ * `import/export [...] from [...]`
+ */
+const inExport = '(import|export)';
+/**
+ * capture group 2
+ * Either `import {x, y, z} from [...]`, `import xyz from [...]` or `import * as xyz from [...]`
+ */
+const thing = '({[a-zA-Z0-9,\\s]*}| *(\\* as )?[a-zA-Z] *)';
+/**
+ * capture group 4
+ * packages with esm modules
+ */
+const esmPkgs = `(${['browser', 'core', 'hub', 'minimal', 'types', 'utils'].join('|')})`;
+/**
+ * `from '@sentry/pkg/dist/xyz';` > `from '@sentry/pkg/xyz';`
+ */
+const removeDist = '(/dist)?';
+/**
+ * capture group 6
+ * `from '@sentry/pkg/xyz';` > `from '@sentry/pkg/esm/xyz';`, if not already imported from esm
+ */
+const subPath = '(/(?!esm)[a-zA-Z0-9/]+)?';
+
 const options = {
   files: `${process.cwd()}/esm/**/*.{js,d.ts}`,
-  from: /import {([a-zA-Z0-9,\s]*)} from '@sentry\/(browser|core|hub|minimal|types|utils)(\/dist)?(\/(?!esm)[a-zA-Z0-9/]+)?';/gm,
-  to: "import {$1} from '@sentry/$2/esm$4';",
+  from: new RegExp(`${inExport} ${thing} from '@sentry/${esmPkgs}${removeDist}${subPath}';`, 'gm'),
+  to: "$1 $2 from '@sentry/$4/esm$6';",
 };
 
 const changes = replace.sync(options);
