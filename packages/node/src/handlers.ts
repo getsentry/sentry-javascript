c1c32b12e28144fdf94fa1a37198c842d842250f
Forrest Middleton <forrest@dealr.cloud>
2018-12-06 17:46:30 -0700

    Enable additional context to be sent as part of the error.

--- packages/node/src/handlers.ts
+++ packages/node/src/handlers.ts
@@ -201,6 +201,10 @@ export function parseRequest(
     };
   }
 
+  if (req.extraContext && typeof req.extraContext === 'object') {
+    event.extra = req.extraContext;
+  }
+
   if (options.transaction) {
     const transaction = extractTransaction(req, options.transaction);
     if (transaction) {
