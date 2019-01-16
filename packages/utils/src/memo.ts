1d8eae19610c461f372580a6ae9596ba1df90866
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-01-16 12:09:43 +0100

    fix: Decycling of objects (#1839)
    * fix: Decycling of objects
    
    * fix: Add changelog
    
    * fix: CodeReview
    
    * fix: Use isPrimitive

---
+++ packages/utils/src/memo.ts
@@ -0,0 +1,55 @@
+// tslint:disable:no-unsafe-any
+/**
+ * Memo class used for decycle json objects. Uses WeakSet if available otherwise array.
+ */
+export class Memo {
+  /** Determines if WeakSet is available */
+  private readonly hasWeakSet: boolean;
+  /** Either WeakSet or Array */
+  private readonly inner: any;
+
+  public constructor() {
+    // tslint:disable-next-line
+    this.hasWeakSet = typeof WeakSet === 'function';
+    this.inner = this.hasWeakSet ? new WeakSet() : [];
+  }
+
+  /**
+   * Sets obj to remember.
+   * @param obj Object to remember
+   */
+  public memoize(obj: any): boolean {
+    if (this.hasWeakSet) {
+      if (this.inner.has(obj)) {
+        return true;
+      }
+      this.inner.add(obj);
+      return false;
+    } else {
+      for (const value of this.inner) {
+        if (value === obj) {
+          return true;
+        }
+      }
+      this.inner.push(obj);
+      return false;
+    }
+  }
+
+  /**
+   * Removes object from internal storage.
+   * @param obj Object to forget
+   */
+  public unmemoize(obj: any): void {
+    if (this.hasWeakSet) {
+      this.inner.delete(obj);
+    } else {
+      for (let i = 0; i < this.inner.length; i++) {
+        if (this.inner[i] === obj) {
+          this.inner.splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+}
--- packages/utils/test/object.test.ts
+++ packages/utils/test/object.test.ts
@@ -53,7 +53,7 @@ describe('serialize()', () => {
       expect(serialize(obj)).toEqual(
         JSON.stringify({
           name: 'Alice',
-          child: { name: 'Bob', self: '[Circular ~.child]' },
+          child: { name: 'Bob', self: '[Circular ~]' },
         }),
       );
     });
@@ -65,7 +65,7 @@ describe('serialize()', () => {
       expect(serialize(obj)).toEqual(
         JSON.stringify({
           name: 'Alice',
-          child: { name: 'Bob', identity: { self: '[Circular ~.child]' } },
+          child: { name: 'Bob', identity: { self: '[Circular ~]' } },
         }),
       );
     });
@@ -94,10 +94,7 @@ describe('serialize()', () => {
       expect(serialize(obj)).toEqual(
         JSON.stringify({
           name: 'Alice',
-          children: [
-            { name: 'Bob', self: '[Circular ~.children.0]' },
-            { name: 'Eve', self: '[Circular ~.children.1]' },
-          ],
+          children: [{ name: 'Bob', self: '[Circular ~]' }, { name: 'Eve', self: '[Circular ~]' }],
         }),
       );
     });
@@ -310,7 +307,7 @@ describe('safeNormalize()', () => {
       obj.child.self = obj.child;
       expect(safeNormalize(obj)).toEqual({
         name: 'Alice',
-        child: { name: 'Bob', self: '[Circular ~.child]' },
+        child: { name: 'Bob', self: '[Circular ~]' },
       });
     });
 
@@ -320,7 +317,7 @@ describe('safeNormalize()', () => {
       obj.child.identity = { self: obj.child };
       expect(safeNormalize(obj)).toEqual({
         name: 'Alice',
-        child: { name: 'Bob', identity: { self: '[Circular ~.child]' } },
+        child: { name: 'Bob', identity: { self: '[Circular ~]' } },
       });
     });
 
@@ -345,7 +342,7 @@ describe('safeNormalize()', () => {
       obj.children[1].self = obj.children[1];
       expect(safeNormalize(obj)).toEqual({
         name: 'Alice',
-        children: [{ name: 'Bob', self: '[Circular ~.children.0]' }, { name: 'Eve', self: '[Circular ~.children.1]' }],
+        children: [{ name: 'Bob', self: '[Circular ~]' }, { name: 'Eve', self: '[Circular ~]' }],
       });
     });
 
