30909d56b57761bab4d7b63c83d4ccfb1997bc33
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-01-21 12:51:30 +0100

    fix: safeNormalize and serialize for Promise rejections (#1841)
    * fix: safeNormalize and serialize
    
    * fix: Ensure max of 300 chars

--- packages/browser/src/integrations/globalhandlers.ts
+++ packages/browser/src/integrations/globalhandlers.ts
@@ -1,6 +1,8 @@
 import { getCurrentHub } from '@sentry/core';
 import { Integration, SentryEvent } from '@sentry/types';
 import { logger } from '@sentry/utils/logger';
+import { safeNormalize, serialize } from '@sentry/utils/object';
+import { truncate } from '@sentry/utils/string';
 import { addExceptionTypeValue, eventFromStacktrace } from '../parsers';
 import {
   installGlobalHandler,
@@ -110,7 +112,10 @@ export class GlobalHandlers implements Integration {
       },
     };
 
-    const fallbackValue = typeof stacktrace.original !== 'undefined' ? `${stacktrace.original}` : '';
+    const fallbackValue =
+      typeof stacktrace.original !== 'undefined'
+        ? `${truncate(serialize(safeNormalize(stacktrace.original)), 300)}`
+        : '';
     const fallbackType = stacktrace.mechanism === 'onunhandledrejection' ? 'UnhandledRejection' : 'Error';
 
     // This makes sure we have type/value in every exception
