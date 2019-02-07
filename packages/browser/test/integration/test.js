1d16944016ead45d4d0b7561dc0354f956572082
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-02-07 10:14:41 +0100

    feat: Add test for callback order

--- packages/browser/test/integration/test.js
+++ packages/browser/test/integration/test.js
@@ -1658,6 +1658,40 @@ for (var idx in frames) {
           );
         });
       });
+
+      // LOADER SPECIFIC TESTS ---------------------------------------------------------------------------------------
+      if (IS_ASYNC_LOADER) {
+        describe('Loader Specific Tests', function() {
+          it('should add breadcrumb from onLoad Callback to undefined error', function(done) {
+            var iframe = this.iframe;
+
+            iframeExecute(
+              iframe,
+              done,
+              function() {
+                Sentry.onLoad(function() {
+                  Sentry.addBreadcrumb({
+                    category: 'auth',
+                    message: 'testing loader',
+                    level: 'error',
+                  });
+                });
+                undefinedMethod(); //trigger error
+              },
+              function(sentryData) {
+                if (debounceAssertEventCount(sentryData, 1, done)) {
+                  var sentryData = iframe.contentWindow.sentryData[0];
+                  assert.ok(sentryData.breadcrumbs);
+                  assert.lengthOf(sentryData.breadcrumbs, 1);
+                  assert.equal(sentryData.breadcrumbs[0].message, 'testing loader');
+                  done();
+                }
+              }
+            );
+          });
+        });
+      }
+      // -------------------------------------------------------------------------------------------------------------
     });
   })();
 }
