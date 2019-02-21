08c075d66296ec196829c6d44714ceda472ae93d
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-02-21 17:05:55 +0100

    ref: Remove lsmod dependency

--- packages/integrations/src/modules.ts
+++ packages/integrations/src/modules.ts
@@ -1,8 +1,60 @@
 import { addGlobalEventProcessor, getCurrentHub } from '@sentry/core';
 import { Integration } from '@sentry/types';
-import * as lsmod from 'lsmod';
+import { existsSync, readFileSync } from 'fs';
+import { dirname, join } from 'path';
 
 let moduleCache: { [key: string]: string };
+
+/** Extract information about package.json modules */
+function collectModules(): {
+  [name: string]: string;
+} {
+  const mainPaths = (require.main && require.main.paths) || [];
+  const paths = require.cache ? Object.keys(require.cache as {}) : [];
+  const infos: {
+    [name: string]: string;
+  } = {};
+  const seen: {
+    [path: string]: boolean;
+  } = {};
+
+  paths.forEach(path => {
+    let dir = path;
+
+    /** Traverse directories upward in the search of package.json file */
+    const updir = (): void | (() => void) => {
+      const orig = dir;
+      dir = dirname(orig);
+
+      if (!dir || orig === dir || seen[orig]) {
+        return undefined;
+      } else if (mainPaths.indexOf(dir) < 0) {
+        return updir();
+      }
+
+      const pkgfile = join(orig, 'package.json');
+      seen[orig] = true;
+
+      if (!existsSync(pkgfile)) {
+        return updir();
+      }
+
+      try {
+        const info = JSON.parse(readFileSync(pkgfile, 'utf8')) as {
+          name: string;
+          version: string;
+        };
+        infos[info.name] = info.version;
+      } catch (_oO) {
+        // no-empty
+      }
+    };
+
+    updir();
+  });
+
+  return infos;
+}
 
 /** Add node modules / packages to the event */
 export class Modules implements Integration {
@@ -34,7 +86,7 @@ export class Modules implements Integration {
   private getModules(): { [key: string]: string } {
     if (!moduleCache) {
       // tslint:disable-next-line:no-unsafe-any
-      moduleCache = lsmod();
+      moduleCache = collectModules();
     }
     return moduleCache;
   }
