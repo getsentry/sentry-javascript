2bac71862836336e4ff1b7fba7090b57c65b3953
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-02-21 08:44:00 +0100

    feat: uuid4 split for spanId

--- packages/opentracing/src/spancontext.ts
+++ packages/opentracing/src/spancontext.ts
@@ -5,13 +5,13 @@ import { uuid4 } from '@sentry/utils/misc';
 export class SpanContext {
   public constructor(
     public readonly traceId?: string,
-    public readonly spanId: string = uuid4(), // private readonly parentId: string,
+    public readonly spanId: string = uuid4().substring(16), // private readonly parentId: string,
   ) {}
 
   /**
    * Returns debug version of the span.
    */
   public toString(): string {
-    return `TraceId: ${this.traceId} SpanId: ${this.spanId}`;
+    return `${(this.traceId && `trace-id:${this.traceId};`) || ''}span-id:${this.spanId}`;
   }
 }
