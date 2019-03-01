79d2babe2b8c1f2f9fcc00e56828c032afeafad8
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-03-01 13:50:45 +0100

    feat: Remove rollup plugins

--- packages/core/src/sdk.ts
+++ packages/core/src/sdk.ts
@@ -11,13 +11,10 @@ export type ClientClass<F extends Client, O extends Options> = new (options: O) 
  *
  * @param clientClass The client class to instanciate.
  * @param options Options to pass to the client.
- * @returns The installed and bound client instance.
  */
 export function initAndBind<F extends Client, O extends Options>(clientClass: ClientClass<F, O>, options: O): void {
   if (options.debug === true) {
     logger.enable();
   }
-
-  const client = new clientClass(options);
-  getCurrentHub().bindClient(client);
+  getCurrentHub().bindClient(new clientClass(options));
 }
