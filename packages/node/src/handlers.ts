681838dfbb69222eb75da343d1b2f1c7c252de12
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-03-19 11:08:34 +0100

    feat: Use generic for getClient on hub

--- packages/node/src/handlers.ts
+++ packages/node/src/handlers.ts
@@ -291,12 +291,20 @@ export function errorHandler(): (
  */
 export function defaultOnFatalError(error: Error): void {
   console.error(error && error.stack ? error.stack : error);
-  const options = (getCurrentHub().getClient() as NodeClient).getOptions();
+  const client = getCurrentHub().getClient<NodeClient>();
+
+  if (client === undefined) {
+    logger.warn('No NodeClient was defined, we are exiting the process now.');
+    global.process.exit(1);
+    return;
+  }
+
+  const options = client.getOptions();
   const timeout =
     (options && options.shutdownTimeout && options.shutdownTimeout > 0 && options.shutdownTimeout) ||
     DEFAULT_SHUTDOWN_TIMEOUT;
   forget(
-    (getCurrentHub().getClient() as NodeClient).close(timeout).then((result: boolean) => {
+    client.close(timeout).then((result: boolean) => {
       if (!result) {
         logger.warn('We reached the timeout for emptying the request buffer, still exiting now!');
       }
--- packages/node/test/onunhandledrejection.test.ts
+++ packages/node/test/onunhandledrejection.test.ts
@@ -23,14 +23,16 @@ describe('unhandled promises', () => {
     const captureException = jest.spyOn(Hub.prototype, 'captureException');
     const setUser = jest.spyOn(Scope.prototype, 'setUser');
     const setExtra = jest.spyOn(Scope.prototype, 'setExtra');
-    const setTag = jest.spyOn(Scope.prototype, 'setTag');
+    const setExtras = jest.spyOn(Scope.prototype, 'setExtras');
+    const setTags = jest.spyOn(Scope.prototype, 'setTags');
 
     integration.sendUnhandledPromise('bla', promise);
 
     expect(captureException.mock.calls[0][0]).toBe('bla');
     expect(setUser.mock.calls[0][0]).toEqual({ id: 1 });
     expect(setExtra.mock.calls[0]).toEqual(['unhandledPromiseRejection', true]);
-    expect(setExtra.mock.calls[1]).toEqual(['extra', '1']);
-    expect(setTag.mock.calls[0]).toEqual(['tag', '2']);
+
+    expect(setExtras.mock.calls[0]).toEqual([{ extra: '1' }]);
+    expect(setTags.mock.calls[0]).toEqual([{ tag: '2' }]);
   });
 });
