396da53a43e78b948a6805b3b4a7e8654b2629fe
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-03-27 08:33:20 +0100

    fix: Disable Tests in node 6 for integrations

--- packages/integrations/rollup.config.js
+++ packages/integrations/rollup.config.js
@@ -43,15 +43,6 @@ const plugins = [
   commonjs(),
 ];
 
-function toPascalCase(string) {
-  return `${string}`
-    .replace(new RegExp(/[-_]+/, 'g'), ' ')
-    .replace(new RegExp(/[^\w\s]/, 'g'), '')
-    .replace(new RegExp(/\s+(.)(\w+)/, 'g'), ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`)
-    .replace(new RegExp(/\s/, 'g'), '')
-    .replace(new RegExp(/\w/), s => s.toUpperCase());
-}
-
 function mergeIntoSentry() {
   return `
   __window.Sentry = __window.Sentry || {};
