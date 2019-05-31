dc35419c263d01ead06b9f805c38bf67acd508ed
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-05-31 14:12:44 +0200

    build: Dont mangle internal properties in integrations (#2095)

--- packages/browser/rollup.config.js
+++ packages/browser/rollup.config.js
@@ -11,7 +11,7 @@ const commitHash = require('child_process')
 const terserInstance = terser({
   mangle: {
     // captureExceptions and captureMessage are public API methods and they don't need to be listed here
-    // as mangler doesn't touch user-facing thing, however sentryWrapepd is not, and it would be mangled into a minified version.
+    // as mangler doesn't touch user-facing thing, however sentryWrapped is not, and it would be mangled into a minified version.
     // We need those full names to correctly detect our internal frames for stripping.
     // I listed all of them here just for the clarity sake, as they are all used in the frames manipulation process.
     reserved: ['captureException', 'captureMessage', 'sentryWrapped'],
