0b1c050840d9898d4d80a49571e121941fca3abe
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-07-05 10:28:39 +0200

    feat: SessionDuration integration (#2150)

---
+++ packages/integrations/src/sessiontiming.ts
@@ -0,0 +1,46 @@
+import { Event, EventProcessor, Hub, Integration } from '@sentry/types';
+
+/** This function adds duration since Sentry was initialized till the time event was sent */
+export class SessionTiming implements Integration {
+  /**
+   * @inheritDoc
+   */
+  public name: string = SessionTiming.id;
+  /**
+   * @inheritDoc
+   */
+  public static id: string = 'SessionTiming';
+
+  /** Exact time Client was initialized expressed in milliseconds since Unix Epoch. */
+  protected readonly _startTime: number = Date.now();
+
+  /**
+   * @inheritDoc
+   */
+  public setupOnce(addGlobalEventProcessor: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void {
+    addGlobalEventProcessor(event => {
+      const self = getCurrentHub().getIntegration(SessionTiming);
+      if (self) {
+        return self.process(event);
+      }
+      return event;
+    });
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public process(event: Event): Event {
+    const now = Date.now();
+
+    return {
+      ...event,
+      extra: {
+        ...event.extra,
+        ['session:start']: this._startTime,
+        ['session:duration']: now - this._startTime,
+        ['session:end']: now,
+      },
+    };
+  }
+}
