da053d5fad8c1546c2791b87206220296cad978f
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-10-02 13:12:04 +0200

    Remove filterAsync method

--- packages/utils/src/async.ts
+++ packages/utils/src/async.ts
@@ -8,20 +8,3 @@ export function forget(promise: Promise<any>): void {
     console.error(e);
   });
 }
-
-/**
- * Helper to filter an array with asynchronous callbacks.
- *
- * @param array An array containing items to filter.
- * @param predicate An async predicate evaluated on every item.
- * @param thisArg Optional value passed as "this" into the callback.
- * @returns An array containing only values where the callback returned true.
- */
-export async function filterAsync<T>(
-  array: T[],
-  predicate: (item: T) => Promise<boolean> | boolean,
-  thisArg?: any,
-): Promise<T[]> {
-  const verdicts = await Promise.all(array.map(predicate, thisArg));
-  return array.filter((_, index) => verdicts[index]);
-}
