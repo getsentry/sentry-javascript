8d92ce32bab604aedf49a043962b101c8185651a
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-12-05 16:05:04 +0100

    ci: Let build for externals fail

--- scripts/danger.sh
+++ scripts/danger.sh
@@ -4,7 +4,12 @@ set -e
 yarn
 # We have to build it first, so that TypeScript Types are recognized correctly
 yarn build
-yarn lint:json
-yarn test
-yarn codecov
-yarn danger ci || true # for external PRs danger will fail because of token
+if [[ ! -z $DANGER_GITHUB_API_TOKEN ]]; then
+  yarn lint:json
+  yarn test
+  yarn codecov
+  yarn danger ci || true # for external PRs danger will fail because of token
+else
+  yarn lint:json
+  yarn test
+fi
