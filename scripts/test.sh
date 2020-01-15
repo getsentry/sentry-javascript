f5e458c551ab26b6dc814f0ffacc8bd143891507
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-01-15 10:01:31 +0100

    fix: travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ if [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 6 ]]; then
   yarn build --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/apm" --ignore="@sentry/node"
   yarn build:es5 --scope="@sentry/apm"
   yarn build --scope="@sentry/node"
-  yarn test --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/apm" # latest version of karma doesn't run on node 6
+  yarn test --ignore="@sentry/browser" --ignore="@sentry/integrations" # latest version of karma doesn't run on node 6
 else
   yarn install
   yarn build
