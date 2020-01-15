4ef64d774a6ad8e8423f02c4385633538aaadd61
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-01-15 09:54:00 +0100

    fix: travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ set -e
 # We need this check to skip engines check for typescript-tslint-plugin package
 if [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 6 ]]; then
   yarn install --ignore-engines
+  yarn build:es5 --scope="@sentry/apm"
   yarn build --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/apm"
   yarn test --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/apm" # latest version of karma doesn't run on node 6
 else
