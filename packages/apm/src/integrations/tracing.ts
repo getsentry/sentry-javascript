3de0425b57a3b27e71dbb0c9acaf9462538b5d0b
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-02-04 13:01:12 +0100

    Update packages/apm/src/integrations/tracing.ts
    Co-Authored-By: Kamil Ogórek <kamil.ogorek@gmail.com>

--- packages/apm/src/integrations/tracing.ts
+++ packages/apm/src/integrations/tracing.ts
@@ -195,7 +195,7 @@ export class Tracing implements Integration {
       });
     }
 
-    // This EventProcessor makes sure the the transaction is not longer than maxTransactionDuration
+    // This EventProcessor makes sure that the transaction is not longer than maxTransactionDuration
     addGlobalEventProcessor((event: Event) => {
       const self = getCurrentHub().getIntegration(Tracing);
       if (!self) {
