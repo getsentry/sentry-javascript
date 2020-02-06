3360babea314d7a7623087b97afcdbb0ade47e1e
David Cramer <dcramer@gmail.com>
2020-02-06 08:44:21 -0800

    Add support for checkoutEveryNms and checkoutEveryNth

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
 # sentry-rrweb
 
 This integration is a WIP.
+
+## Pre-Requisites
+
+For the sentry-rrweb integration to work, you must have the [Sentry browser SDK package](https://www.npmjs.com/package/@sentry/browser) and the [rrweb package](https://www.npmjs.com/package/rrweb) installed.
+
+## Installation
+
+To install the stable version:
+
+with npm:
+
+```shell
+npm install --save @sentry/rrweb
+```
+
+with yarn:
+
+```shell
+yarn add @sentry/rrweb
+```
+
+## Setup
+
+To set up the integration add the following to your Sentry initialization:
+
+```javascript
+import * as Sentry from '@sentry/browser';
+import SentryRRWeb from '@sentry/rrweb';
+
+Sentry.init({
+  dsn: '__DSN__',
+  integrations: [
+    new SentryRRWeb({
+      // ...options
+    })
+  ]
+  // ...
+});
+```
+
+Several options are supported and passable via the integration constructor:
+
+```javascript
+import * as Sentry from '@sentry/browser';
+import SentryRRWeb from '@sentry/rrweb';
+
+Sentry.init({
+  dsn: '__DSN__',
+  integrations: [
+    new SentryRRWeb({
+      // default is empty
+      checkoutEveryNth: 100,
+      // default is 5 minutes
+      checkoutEveryNms: 15 * 60 * 1000
+    })
+  ]
+  // ...
+});
+```
+
+See the rrweb documentation for advice on configuring these values.
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as rrweb from 'rrweb';
 import * as Sentry from '@sentry/browser';
 import { Dsn, Event } from '@sentry/types';
 
-type RrwebEvent = {
+type RRWebEvent = {
   type: rrweb.EventType;
   data: {};
   timestamp: number;
@@ -12,25 +12,43 @@ type RrwebEvent = {
 export default class SentryRRWeb {
   public readonly name: string = SentryRRWeb.id;
   public static id: string = 'SentryRRWeb';
-  public events: Array<RrwebEvent> = [];
 
-  attachmentUrlFromDsn(dsn: Dsn, eventId: string) {
+  public events: Array<RRWebEvent> = [];
+  private readonly checkoutEveryNms: number;
+  private readonly checkoutEveryNth: number;
+
+  public constructor(
+    options: { checkoutEveryNms?: number; checkoutEveryNth?: number } = {}
+  ) {
+    // default checkout time of 5 minutes
+    this.checkoutEveryNms = options.checkoutEveryNms || 5 * 60 * 1000;
+    this.checkoutEveryNth = options.checkoutEveryNth;
+  }
+
+  public attachmentUrlFromDsn(dsn: Dsn, eventId: string) {
     const { host, path, projectId, port, protocol, user } = dsn;
     return `${protocol}://${host}${port !== '' ? `:${port}` : ''}${
       path !== '' ? `/${path}` : ''
     }/api/${projectId}/events/${eventId}/attachments/?sentry_key=${user}&sentry_version=7&sentry_client=rrweb`;
   }
 
-  setupOnce() {
+  public setupOnce() {
     rrweb.record({
-      emit(event: RrwebEvent, isCheckout?: boolean) {
+      checkoutEveryNms: this.checkoutEveryNms,
+      checkoutEveryNth: this.checkoutEveryNth,
+      emit(event: RRWebEvent, isCheckout?: boolean) {
         const self = Sentry.getCurrentHub().getIntegration(SentryRRWeb);
-        self.events.push(event);
+        if (isCheckout) {
+          self.events = [event];
+        } else {
+          self.events.push(event);
+        }
       }
     });
 
     Sentry.addGlobalEventProcessor((event: Event) => {
       const self = Sentry.getCurrentHub().getIntegration(SentryRRWeb);
+      if (!self) return;
       try {
         // short circuit if theres no events to replay
         if (!self.events.length) return;
