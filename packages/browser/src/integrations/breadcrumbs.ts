94234478cfbff41abedd4e4cb86762398011350b
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-02-24 16:00:33 +0100

    ref: Mark transactions as event.transaction in breadcrumbs

--- packages/browser/src/integrations/breadcrumbs.ts
+++ packages/browser/src/integrations/breadcrumbs.ts
@@ -317,7 +317,7 @@ function addSentryBreadcrumb(serializedData: string): void {
     const event = JSON.parse(serializedData);
     getCurrentHub().addBreadcrumb(
       {
-        category: 'sentry',
+        category: `sentry.${event.transaction ? 'transaction' : 'event'}`,
         event_id: event.event_id,
         level: event.level || Severity.fromString('error'),
         message: getEventDescription(event),
