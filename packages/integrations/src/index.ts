cf060911b147e27e1b799e9b1582d3b0a8b81220
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-02-26 11:53:50 +0100

    Revert "feat(integrations): Add scrubbing sensitive data integration (#2422)" (#2454)
    This reverts commit 9154a39a1ef22ed547ea71c3b46531cfca1af076.

--- packages/integrations/src/index.ts
+++ packages/integrations/src/index.ts
@@ -6,7 +6,6 @@ export { Ember } from './ember';
 export { ExtraErrorData } from './extraerrordata';
 export { ReportingObserver } from './reportingobserver';
 export { RewriteFrames } from './rewriteframes';
-export { ScrubData } from './scrubdata';
 export { SessionTiming } from './sessiontiming';
 export { Transaction } from './transaction';
 export { Vue } from './vue';
