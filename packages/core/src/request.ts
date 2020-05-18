33602dfba17436ac48e3a9413467e06349a7365f
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-05-18 14:31:58 +0200

    Update packages/core/src/request.ts
    Co-authored-by: Abhijeet Prasad <AbhiPrasad@users.noreply.github.com>

--- packages/core/src/request.ts
+++ packages/core/src/request.ts
@@ -35,7 +35,7 @@ export function eventToSentryRequest(event: Event, api: API): SentryRequest {
   if (useEnvelope) {
     const envelopeHeaders = JSON.stringify({
       event_id: event.event_id,
-      // We need to add * 1000 since we devide it by 1000 by default but JS works with ms precision
+      // We need to add * 1000 since we divide it by 1000 by default but JS works with ms precision
       // The reason we use timestampWithMs here is that all clocks across the SDK use the same clock
       sent_at: new Date(timestampWithMs() * 1000).toISOString(),
     });
