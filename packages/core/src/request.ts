e5825d0bd06f75fe897b57040fdb97d6da9658f0
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-05-18 13:13:48 +0200

    fix: sent_at for envelope headers

--- packages/core/src/request.ts
+++ packages/core/src/request.ts
@@ -1,5 +1,6 @@
 import { getCurrentHub } from '@sentry/hub';
 import { Event } from '@sentry/types';
+import { timestampWithMs } from '@sentry/utils';
 
 import { API } from './api';
 
@@ -34,7 +35,9 @@ export function eventToSentryRequest(event: Event, api: API): SentryRequest {
   if (useEnvelope) {
     const envelopeHeaders = JSON.stringify({
       event_id: event.event_id,
-      sent_at: new Date().toISOString(),
+      // We need to add * 1000 since we devide it by 1000 by default but JS works with ms precision
+      // The reason we use timestampWithMs here is that all clocks across the SDK use the same clock
+      sent_at: new Date(timestampWithMs() * 1000).toISOString(),
     });
     const itemHeaders = JSON.stringify({
       type: event.type,
