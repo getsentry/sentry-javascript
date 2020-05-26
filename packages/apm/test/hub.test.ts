b077373234abe0b37096bc1b0e421604ae7b6f32
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-05-26 14:05:06 +0200

    feat: Add additional tests

--- packages/apm/test/hub.test.ts
+++ packages/apm/test/hub.test.ts
@@ -1,5 +1,6 @@
 import { BrowserClient } from '@sentry/browser';
 import { Hub, Scope } from '@sentry/hub';
+import { Span, Transaction } from '@sentry/types';
 
 import { addExtensionMethods } from '../src/hubextensions';
 
@@ -36,14 +37,22 @@ describe('Hub', () => {
       });
     });
 
-    describe('start', () => {
-      test('simple', () => {
+    describe('startSpan', () => {
+      test('simple standalone Span', () => {
         const hub = new Hub(new BrowserClient());
         const span = hub.startSpan({}) as any;
         expect(span.spanId).toBeTruthy();
       });
 
-      test('transaction inherits trace_id from span on scope', () => {
+      test('simple standalone Transaction', () => {
+        const hub = new Hub(new BrowserClient({ tracesSampleRate: 1 }));
+        const transaction = hub.startSpan({ name: 'transaction' }) as Transaction;
+        expect(transaction.spanId).toBeTruthy();
+        // tslint:disable-next-line: no-unbound-method
+        expect(transaction.setName).toBeTruthy();
+      });
+
+      test('Transaction inherits trace_id from span on scope', () => {
         const myScope = new Scope();
         const hub = new Hub(new BrowserClient(), myScope);
         const parentSpan = hub.startSpan({}) as any;
@@ -52,6 +61,24 @@ describe('Hub', () => {
         });
         const span = hub.startSpan({ name: 'test' }) as any;
         expect(span.trace_id).toEqual(parentSpan.trace_id);
+      });
+
+      test('create a child if there is a Span already on the scope', () => {
+        const myScope = new Scope();
+        const hub = new Hub(new BrowserClient({ tracesSampleRate: 1 }), myScope);
+        const transaction = hub.startSpan({ name: 'transaction' }) as Transaction;
+        hub.configureScope(scope => {
+          scope.setSpan(transaction);
+        });
+        const span = hub.startSpan({}) as Span;
+        expect(span.traceId).toEqual(transaction.traceId);
+        expect(span.parentSpanId).toEqual(transaction.spanId);
+        hub.configureScope(scope => {
+          scope.setSpan(span);
+        });
+        const span2 = hub.startSpan({}) as Span;
+        expect(span2.traceId).toEqual(span.traceId);
+        expect(span2.parentSpanId).toEqual(span.spanId);
       });
     });
   });
