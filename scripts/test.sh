51747917b4889394c50f2866377132f47487138b
Abhijeet Prasad <aprasad@sentry.io>
2020-06-08 14:41:45 -0400

    cd: ignore react on node 6

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,7 @@ if [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 6 ]]; then
   yarn install --ignore-engines
   yarn build
   nvm use 6
-  yarn test --ignore="@sentry/browser" --ignore="@sentry/integrations" # latest version of karma doesn't run on node 6
+  yarn test --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/react" # latest version of karma doesn't run on node 6
 else
   yarn install
   yarn build
