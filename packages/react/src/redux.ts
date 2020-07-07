4d7ecae74bb44afa90897f5525fe3daebe4dee09
Jennarong Muengtaweepongsa <ldrwarlock@gmail.com>
2020-07-07 18:08:33 +0700

    ref: Handle all ts errors

--- packages/react/src/redux.ts
+++ packages/react/src/redux.ts
@@ -8,32 +8,34 @@ export interface SentryMiddlewareOptions {
    * Use this to remove any private data before sending it to Sentry.
    * Return null to not attach the state.
    */
-  stateTransformer?(state: object | undefined): object | null;
+  // tslint:disable-next-line: no-null-undefined-union
+  stateTransformer(state: object | undefined): object | null | undefined;
   /**
    * Transforms the action before sending it as a breadcrumb.
    * Use this to remove any private data before sending it to Sentry.
    * Return null to not send the breadcrumb.
    */
-  actionTransformer?(action: Redux.Action): Redux.Action | null;
+  // tslint:disable-next-line: no-null-undefined-union
+  actionTransformer(action: Redux.Action): Redux.Action | null | undefined;
   /**
    * Category of the breadcrumb sent by actions. Default is 'redux.action'
    */
-  actionBreadcrumbCategory?: string;
+  actionBreadcrumbCategory: string;
   /**
    * Type of the breadcrumb sent by actions. Default is 'info'
    */
-  actionBreadcrumbType?: string;
+  actionBreadcrumbType: string;
   /**
    * The extra key to pass the state to. Default is 'redux.state'
    */
-  stateExtraKey?: string;
+  stateExtraKey: string;
   /**
    * Called on every state update, configure the Sentry Scope with the redux state.
    */
   configureScopeWithState?(scope: Sentry.Scope, state: object | undefined): void;
 }
 
-const defaultOptions = {
+const defaultOptions: SentryMiddlewareOptions = {
   actionBreadcrumbCategory: 'redux.action',
   actionBreadcrumbType: 'info',
   actionTransformer: action => action,
@@ -41,7 +43,7 @@ const defaultOptions = {
   stateTransformer: state => state,
 };
 
-function createReduxEnhancer(enhancerOptions: SentryMiddlewareOptions = defaultOptions): Redux.StoreEnhancer {
+function createReduxEnhancer(enhancerOptions?: Partial<SentryMiddlewareOptions>): Redux.StoreEnhancer {
   return next => (reducer, initialState) => {
     const options = {
       ...defaultOptions,
@@ -49,14 +51,17 @@ function createReduxEnhancer(enhancerOptions: SentryMiddlewareOptions = defaultO
     };
 
     const sentryReducer: Redux.Reducer<any, any> = (state, action) => {
-      const newState = reducer(state, action);
+      // tslint:disable-next-line: no-unsafe-any
+      const newState: any = reducer(state, action);
 
       Sentry.configureScope(scope => {
         /* Action breadcrumbs */
+        // tslint:disable-next-line: no-unsafe-any
         const transformedAction = options.actionTransformer ? options.actionTransformer(action) : action;
         if (typeof transformedAction !== 'undefined' && transformedAction !== null) {
           scope.addBreadcrumb({
             category: options.actionBreadcrumbCategory,
+            // tslint:disable-next-line: no-unsafe-any
             data: transformedAction,
             type: options.actionBreadcrumbType,
           });
@@ -73,6 +78,7 @@ function createReduxEnhancer(enhancerOptions: SentryMiddlewareOptions = defaultO
         /* Allow user to configure scope with latest state */
         const { configureScopeWithState } = options;
         if (typeof configureScopeWithState === 'function') {
+          // tslint:disable-next-line: no-unsafe-any
           configureScopeWithState(scope, newState);
         }
       });
