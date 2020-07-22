2b7134d6b561a493da8a4f0607890141717db814
k-fish <kevan.fisher@sentry.io>
2020-07-22 09:30:09 -0700

    Ignore @sentry/ember in build phase as well as node > 10 is required

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,12 +6,12 @@ source ~/.nvm/nvm.sh
 if [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 6 ]]; then
   nvm use 8
   yarn install --ignore-engines --ignore-scripts
-  yarn build
+  yarn build --ignore="@sentry/ember"
   nvm use 6
   yarn test --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/react" --ignore="@sentry/ember" --ignore="@sentry/tracing" # latest version of karma doesn't run on node 6
 elif [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 8 ]]; then
   yarn install --ignore-engines --ignore-scripts
-  yarn build
+  yarn build --ignore="@sentry/ember"
   yarn test --ignore="@sentry/tracing" --ignore="@sentry/react" --ignore="@sentry/ember"
 else
   yarn install
