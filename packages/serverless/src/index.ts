ea94635adc8c74edf0f9e5f23b57e163b4fb18b7
Dolly Chawla <dolly.chawla@calsoftinc.com>
2020-08-12 22:36:24 +0530

    changes : 1. Unnecessary code has been removed and tested 2. updated the version of serverless to 5.21.0 3. Changed the license to 'MIT' 4. description, version, license in package.json have been changes 5. All the versions in package.json have been updated to current latest release 6. setTimeout code has been removed after implementing flush()

--- packages/serverless/src/index.ts
+++ packages/serverless/src/index.ts
@@ -41,16 +41,3 @@ import * as ServerlessIntegrations from './integrations';
 export { ServerlessIntegrations as Integrations };
 export { init } from './sdk';
 export { SDK_NAME, SDK_VERSION } from './version';
-// import { getMainCarrier } from '@sentry/hub';
-// import * as domain from 'domain';
-
-// We need to patch domain on the global __SENTRY__ object to make it work for serverless
-// if we don't do this, browser bundlers will have troubles resolving require('domain')
-// const carrier = getMainCarrier();
-// if (carrier.__SENTRY__) {
-//   carrier.__SENTRY__.extensions = carrier.__SENTRY__.extensions || {};
-//   if (!carrier.__SENTRY__.extensions.domain) {
-//     // @ts-ignore
-//     carrier.__SENTRY__.extensions.domain = domain;
-//   }
-// }
