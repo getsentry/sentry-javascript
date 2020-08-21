605e3de7c0ede33cd07d70ae8611b6f617d76e3f
Abhijeet Prasad <aprasad@sentry.io>
2020-08-21 14:07:45 -0400

    feat: Make @sentry/tracing required

--- packages/gatsby/gatsby-browser.js
+++ packages/gatsby/gatsby-browser.js
@@ -1,31 +1,14 @@
 exports.onClientEntry = function(_, pluginParams) {
-  require.ensure(['@sentry/react'], function(require) {
+  require.ensure(['@sentry/react', '@sentry/tracing'], function(require) {
     const Sentry = require('@sentry/react');
-
-    let TracingIntegration = undefined;
-    let BrowserTracingIntegration = undefined;
-    try {
-      BrowserTracingIntegration = require('@sentry/tracing').Integrations.BrowserTracing;
-    } catch (_) {
-      /* no-empty */
-    }
-    try {
-      /** @deprecated Remove when @sentry/apm is no longer used */
-      TracingIntegration = require('@sentry/apm').Integrations.Tracing;
-    } catch (_) {
-      /* no-empty */
-    }
-
+    const Tracing = require('@sentry/tracing');
     const tracesSampleRate = pluginParams.tracesSampleRate !== undefined ? pluginParams.tracesSampleRate : 0;
     const integrations = [...(pluginParams.integrations || [])];
 
-    if (tracesSampleRate) {
-      if (BrowserTracingIntegration) {
-        integrations.push(new BrowserTracingIntegration());
-      } else if (TracingIntegration) {
-        integrations.push(new TracingIntegration());
-      }
+    if (tracesSampleRate && !integrations.some(ele => ele.id === 'BrowserTracing')) {
+      integrations.push(new Tracing.Integrations.BrowserTracing(pluginParams.browserTracingOptions));
     }
+    console.log(pluginParams);
 
     Sentry.init({
       environment: process.env.NODE_ENV || 'development',
@@ -33,10 +16,16 @@ exports.onClientEntry = function(_, pluginParams) {
       release: __SENTRY_RELEASE__,
       // eslint-disable-next-line no-undef
       dsn: __SENTRY_DSN__,
+      beforeSend: event => {
+        console.log(event);
+        return null;
+      },
       ...pluginParams,
       tracesSampleRate,
       integrations,
     });
+
+    Tracing.addExtensionMethods();
 
     Sentry.addGlobalEventProcessor(event => {
       event.sdk = {
