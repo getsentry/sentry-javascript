f1001dc1ac95e0a23a289de3e809fad85007946f
k-fish <kevan.fisher@sentry.io>
2020-09-09 16:07:45 -0700

    fix(ember): Fix ember tests by switching ember-try to yarn
    Ember-try uses npm to modify the package.json to test different versions of Ember, switching to use yarn should fix the issues it had resolving on new releases

--- packages/ember/config/ember-try.js
+++ packages/ember/config/ember-try.js
@@ -4,6 +4,7 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.12',
