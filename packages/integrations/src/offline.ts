524ec334e705733dbba0f107662479484e8651e3
Piotr Konieczny <hello@piotr.cz>
2020-09-10 11:05:57 +0200

    Move event listeners setup from constructor to setupOnce

--- packages/integrations/src/offline.ts
+++ packages/integrations/src/offline.ts
@@ -51,6 +51,13 @@ export class Offline implements Integration {
     this.offlineEventStore = localForage.createInstance({
       name: 'sentry/offlineEventStore',
     });
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public setupOnce(addGlobalEventProcessor: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void {
+    this.hub = getCurrentHub();
 
     if ('addEventListener' in this.global) {
       this.global.addEventListener('online', () => {
@@ -59,13 +66,6 @@ export class Offline implements Integration {
         });
       });
     }
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public setupOnce(addGlobalEventProcessor: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void {
-    this.hub = getCurrentHub();
 
     addGlobalEventProcessor((event: Event) => {
       if (this.hub && this.hub.getIntegration(Offline)) {
