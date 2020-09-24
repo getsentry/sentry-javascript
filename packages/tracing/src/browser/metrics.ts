18e40609b16995fc2226dbb6810243865d387269
Alberto Leal <mail4alberto@gmail.com>
2020-09-24 10:18:59 -0400

    use getFID function

--- packages/tracing/src/browser/metrics.ts
+++ packages/tracing/src/browser/metrics.ts
@@ -152,46 +152,19 @@ export class MetricsInstrumentation {
 
   /** Starts tracking the First Input Delay on the current page. */
   private _trackFID(): void {
-    // Based on reference implementation from https://web.dev/fid/#measure-fid-in-javascript.
-    // Use a try/catch instead of feature detecting `first-input`
-    // support, since some browsers throw when using the new `type` option.
-    // https://bugs.webkit.org/show_bug.cgi?id=209216
-    try {
-      if (!PerformanceObserver.supportedEntryTypes.includes('first-input')) {
+    getFID((metric) => {
+      const entry = metric.entries.pop();
+
+      if (!entry) {
         return;
       }
 
-      const updateFID = (entry: PerformanceEventTiming, po: PerformanceObserver): void => {
-        // Only report FID if the page wasn't hidden prior to
-        // the entry being dispatched. This typically happens when a
-        // page is loaded in a background tab.
-        if (entry.startTime < this._firstHiddenTime) {
-          const fidValue = entry.processingStart - entry.startTime;
-          const timeOrigin = msToSec(performance.timeOrigin);
-          const startTime = msToSec(entry.startTime as number);
-          logger.log('[Measurements] Adding FID');
-          this._measurements['fid'] = { value: fidValue };
-          this._measurements['mark.fid'] = { value: timeOrigin + startTime };
-
-        }
-
-        po.disconnect();
-      };
-
-      // Create a PerformanceObserver that calls `updateFID` for each entry.
-      const po = new PerformanceObserver(entryList => {
-        entryList.getEntries().forEach(entry => updateFID(entry as PerformanceEventTiming, po));
-      });
-
-      // Observe entries of type `first-input`, including buffered entries,
-      // i.e. entries that occurred before calling `observe()` below.
-      po.observe({
-        buffered: true,
-        type: 'first-input',
-      });
-    } catch (e) {
-      // Do nothing if the browser doesn't support this API.
-    }
+      const timeOrigin = msToSec(performance.timeOrigin);
+      const startTime = msToSec(entry.startTime as number);
+      logger.log('[Measurements] Adding FID');
+      this._measurements['fid'] = { value: metric.value };
+      this._measurements['mark.fid'] = { value: timeOrigin + startTime };
+    });
   }
 }
 
