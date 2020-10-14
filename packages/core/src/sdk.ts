b66cfb70bfdacb9f30649cac96738683783f0004
Rodolfo Carvalho <rodolfo.carvalho@sentry.io>
2020-10-14 17:19:28 +0200

    fix: Typo instanciate => instantiate

--- packages/core/src/sdk.ts
+++ packages/core/src/sdk.ts
@@ -2,14 +2,14 @@ import { getCurrentHub } from '@sentry/hub';
 import { Client, Options } from '@sentry/types';
 import { logger } from '@sentry/utils';
 
-/** A class object that can instanciate Client objects. */
+/** A class object that can instantiate Client objects. */
 export type ClientClass<F extends Client, O extends Options> = new (options: O) => F;
 
 /**
  * Internal function to create a new SDK client instance. The client is
  * installed and then bound to the current scope.
  *
- * @param clientClass The client class to instanciate.
+ * @param clientClass The client class to instantiate.
  * @param options Options to pass to the client.
  */
 export function initAndBind<F extends Client, O extends Options>(clientClass: ClientClass<F, O>, options: O): void {
