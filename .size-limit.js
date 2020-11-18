ac8c9b5dff68b5738bf17be9dd6301e1aec4b141
Katie Byers <katie.byers@sentry.io>
2020-11-18 13:59:36 -0800

    chore(build): Increase limits on size check (#3055)

--- .size-limit.js
+++ .size-limit.js
@@ -3,24 +3,24 @@ module.exports = [
     name: '@sentry/browser - CDN Bundle (gzipped)',
     path: 'packages/browser/build/bundle.min.js',
     gzip: true,
-    limit: '18 KB',
+    limit: '21 KB',
   },
   {
     name: '@sentry/browser - Webpack',
     path: 'packages/browser/esm/index.js',
     import: '{ init }',
-    limit: '19 KB',
+    limit: '22 KB',
   },
   {
     name: '@sentry/react - Webpack',
     path: 'packages/react/esm/index.js',
     import: '{ init }',
-    limit: '19 KB',
+    limit: '22 KB',
   },
   {
     name: '@sentry/browser + @sentry/tracing - CDN Bundle (gzipped)',
     path: 'packages/tracing/build/bundle.tracing.min.js',
     gzip: true,
-    limit: '25 KB',
+    limit: '28 KB',
   },
 ];
