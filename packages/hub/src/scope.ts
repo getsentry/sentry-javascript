c28c84ecb0af19cc8b17f19b84c76b396c141958
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-11-23 13:04:59 +0100

    Revert "fix: Remove unused var"
    This reverts commit d059dffff386a4eddea33a9cea365d58d54abdc5.

--- packages/hub/src/scope.ts
+++ packages/hub/src/scope.ts
@@ -25,6 +25,9 @@ import { Session } from './session';
  * called by the client before an event will be sent.
  */
 export class Scope implements ScopeInterface {
+  /** Flag if notifiying is happening. */
+  protected _notifyingListeners: boolean = false;
+
   /** Callback for client to receive scope changes. */
   protected _scopeListeners: Array<(scope: Scope) => void> = [];
 
@@ -435,9 +438,13 @@ export class Scope implements ScopeInterface {
    * This will be called on every set call.
    */
   protected _notifyScopeListeners(): void {
-    this._scopeListeners.forEach(callback => {
-      callback(this);
-    });
+    if (!this._notifyingListeners) {
+      this._notifyingListeners = true;
+      this._scopeListeners.forEach(callback => {
+        callback(this);
+      });
+      this._notifyingListeners = false;
+    }
   }
 
   /**
