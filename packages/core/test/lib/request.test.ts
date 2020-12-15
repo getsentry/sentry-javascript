0afb3c76b66a6a52ced49d048700cad420672872
Katie Byers <lobsterkatie@gmail.com>
2020-12-15 08:13:45 -0800

    parameterize test, add variations

--- packages/core/test/lib/request.test.ts
+++ packages/core/test/lib/request.test.ts
@@ -1,4 +1,4 @@
-import { Event } from '@sentry/types';
+import { Event, TransactionSamplingMethod } from '@sentry/types';
 
 import { API } from '../../src/api';
 import { eventToSentryRequest } from '../../src/request';
@@ -16,25 +16,41 @@ describe('eventToSentryRequest', () => {
     user: { id: '1121', username: 'CharlieDog', ip_address: '11.21.20.12' },
   };
 
-  it('adds sampling information to transaction item header', () => {
-    event.tags = { __sentry_samplingMethod: 'client_rate', __sentry_sampleRate: '0.1121', dog: 'Charlie' };
+  [
+    { method: TransactionSamplingMethod.Rate, rate: '0.1121', dog: 'Charlie' },
+    { method: TransactionSamplingMethod.Sampler, rate: '0.1231', dog: 'Maisey' },
+    { method: TransactionSamplingMethod.Inheritance, dog: 'Cory' },
+    { method: TransactionSamplingMethod.Explicit, dog: 'Bodhi' },
 
-    const result = eventToSentryRequest(event as Event, api);
+    // this shouldn't ever happen (at least the method should always be included in tags), but good to know that things
+    // won't blow up if it does
+    { dog: 'Lucy' },
+  ].forEach(({ method, rate, dog }) => {
+    it(`adds transaction sampling information to item header - ${method}, ${rate}, ${dog}`, () => {
+      // TODO kmclb - once tag types are loosened, don't need to cast to string here
+      event.tags = { __sentry_samplingMethod: String(method), __sentry_sampleRate: String(rate), dog };
 
-    const [envelopeHeaderString, itemHeaderString, eventString] = result.body.split('\n');
+      const result = eventToSentryRequest(event as Event, api);
 
-    const envelope = {
-      envelopeHeader: JSON.parse(envelopeHeaderString),
-      itemHeader: JSON.parse(itemHeaderString),
-      event: JSON.parse(eventString),
-    };
+      const [envelopeHeaderString, itemHeaderString, eventString] = result.body.split('\n');
 
-    // the right stuff is added to the item header
-    expect(envelope.itemHeader).toEqual({ type: 'transaction', sample_rates: [{ id: 'client_rate', rate: '0.1121' }] });
+      const envelope = {
+        envelopeHeader: JSON.parse(envelopeHeaderString),
+        itemHeader: JSON.parse(itemHeaderString),
+        event: JSON.parse(eventString),
+      };
 
-    // show that it pops the right tags and leaves the rest alone
-    expect('__sentry_samplingMethod' in envelope.event.tags).toBe(false);
-    expect('__sentry_sampleRate' in envelope.event.tags).toBe(false);
-    expect('dog' in envelope.event.tags).toBe(true);
+      // the right stuff is added to the item header
+      expect(envelope.itemHeader).toEqual({
+        type: 'transaction',
+        // TODO kmclb - once tag types are loosened, don't need to cast to string here
+        sample_rates: [{ id: String(method), rate: String(rate) }],
+      });
+
+      // show that it pops the right tags and leaves the rest alone
+      expect('__sentry_samplingMethod' in envelope.event.tags).toBe(false);
+      expect('__sentry_sampleRate' in envelope.event.tags).toBe(false);
+      expect('dog' in envelope.event.tags).toBe(true);
+    });
   });
 });
