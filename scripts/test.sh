9f7d922462ffa877927d74512f26d892347f3030
Billy Vong <billyvg@users.noreply.github.com>
2021-01-07 16:40:25 -0800

    build(ci): Remove TravisCI (#3149)
    Travis sprung some surprise billing changes on us, so we are now prioritizing removing TravisCI completely.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,8 @@ set -e
 source ~/.nvm/nvm.sh
 
 # We need this check to skip engines check for typescript-tslint-plugin package
-if [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 6 ]]; then
+if [[ "$(cut -d. -f1 <<< "$NODE_VERSION")" -le 6 ]]; then
+  nvm install 8
   nvm use 8
   yarn install --ignore-engines --ignore-scripts
   # current versions of nock don't support node 6
@@ -12,10 +13,11 @@ if [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 6 ]]; then
   cd ../..
   # ember requires Node >= 10 to build
   yarn build --ignore="@sentry/ember" --ignore="@sentry/serverless" --ignore="@sentry/gatsby" --ignore="@sentry/react"
+  nvm install 6
   nvm use 6
   # browser can be tested only on Node >= v8 because Karma is not supporting anything older
   yarn test --ignore="@sentry/tracing" --ignore="@sentry/react" --ignore="@sentry/gatsby" --ignore="@sentry/ember" --ignore="@sentry-internal/eslint-plugin-sdk" --ignore="@sentry-internal/eslint-config-sdk" --ignore="@sentry/serverless" --ignore="@sentry/browser" --ignore="@sentry/integrations" 
-elif [[ "$(cut -d. -f1 <<< "$TRAVIS_NODE_VERSION")" -le 8 ]]; then
+elif [[ "$(cut -d. -f1 <<< "$NODE_VERSION")" -le 8 ]]; then
   yarn install --ignore-engines --ignore-scripts
   # ember requires Node >= 10 to build
   yarn build --ignore="@sentry/ember" --ignore="@sentry/serverless" --ignore="@sentry/gatsby" --ignore="@sentry/react"
