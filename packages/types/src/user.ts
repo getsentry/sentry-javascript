fc00b221f9bd300a08c08a9c1a9d425d7466f2a6
SeRi <seri@heise.de>
2021-01-21 00:46:51 +0100

    chore: User type improvments
    - JSDoc

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -1,8 +1,25 @@
 /** JSDoc */
-export interface User {
-  [key: string]: any;
+export interface User extends Record<string, unknown> {
+  /**
+   * The application-specific internal identifier for the user.
+   */
   id?: string;
-  ip_address?: string;
-  email?: string;
+
+  /**
+   * The username. Typically used as a better label than the internal id.
+   */
   username?: string;
+
+  /**
+   * An alternative, or addition, to the username.
+   * Sentry is aware of email addresses and can display things such as Gravatars and unlock messaging capabilities.
+   */
+  email?: string;
+
+  /**
+   * The user's IP address.
+   * If the user is unauthenticated, Sentry uses the IP address as a unique identifier for the user.
+   * Set to "{{auto}}" to let Sentry infer the IP address from the connection.
+   */
+  ip_address?: string | '{{auto}}';
 }
