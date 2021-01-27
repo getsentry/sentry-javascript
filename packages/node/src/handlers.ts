cdd6995762ec251f75e6346888eda62262431510
Katie Byers <lobsterkatie@gmail.com>
2021-01-26 20:08:25 -0800

    move `extractNodeRequestData` (and associated tests) from utils to node package, rename

--- packages/node/src/handlers.ts
+++ packages/node/src/handlers.ts
@@ -2,18 +2,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { captureException, getCurrentHub, startTransaction, withScope } from '@sentry/core';
 import { extractTraceparentData, Span } from '@sentry/tracing';
-import { Event, Transaction } from '@sentry/types';
-import {
-  extractNodeRequestData,
-  forget,
-  isPlainObject,
-  isString,
-  logger,
-  stripUrlQueryAndFragment,
-} from '@sentry/utils';
+import { Event, ExtractedNodeRequestData, Transaction } from '@sentry/types';
+import { forget, isPlainObject, isString, logger, normalize, stripUrlQueryAndFragment } from '@sentry/utils';
+import * as cookie from 'cookie';
 import * as domain from 'domain';
 import * as http from 'http';
 import * as os from 'os';
+import * as url from 'url';
 
 import { NodeClient } from './client';
 import { flush } from './sdk';
@@ -179,6 +174,97 @@ function extractUserData(
   return extractedUser;
 }
 
+/** Default request keys that'll be used to extract data from the request */
+const DEFAULT_REQUEST_KEYS = ['cookies', 'data', 'headers', 'method', 'query_string', 'url'];
+
+/**
+ * Normalizes data from the request object, accounting for framework differences.
+ *
+ * @param req The request object from which to extract data
+ * @param keys An optional array of keys to include in the normalized data. Defaults to DEFAULT_REQUEST_KEYS if not
+ * provided.
+ * @returns An object containing normalized request data
+ */
+export function extractRequestData(
+  req: { [key: string]: any },
+  keys: string[] = DEFAULT_REQUEST_KEYS,
+): ExtractedNodeRequestData {
+  const requestData: { [key: string]: any } = {};
+
+  // headers:
+  //   node, express: req.headers
+  //   koa: req.header
+  const headers = (req.headers || req.header || {}) as {
+    host?: string;
+    cookie?: string;
+  };
+  // method:
+  //   node, express, koa: req.method
+  const method = req.method;
+  // host:
+  //   express: req.hostname in > 4 and req.host in < 4
+  //   koa: req.host
+  //   node: req.headers.host
+  const host = req.hostname || req.host || headers.host || '<no host>';
+  // protocol:
+  //   node: <n/a>
+  //   express, koa: req.protocol
+  const protocol =
+    req.protocol === 'https' || req.secure || ((req.socket || {}) as { encrypted?: boolean }).encrypted
+      ? 'https'
+      : 'http';
+  // url (including path and query string):
+  //   node, express: req.originalUrl
+  //   koa: req.url
+  const originalUrl = (req.originalUrl || req.url || '') as string;
+  // absolute url
+  const absoluteUrl = `${protocol}://${host}${originalUrl}`;
+
+  keys.forEach(key => {
+    switch (key) {
+      case 'headers':
+        requestData.headers = headers;
+        break;
+      case 'method':
+        requestData.method = method;
+        break;
+      case 'url':
+        requestData.url = absoluteUrl;
+        break;
+      case 'cookies':
+        // cookies:
+        //   node, express, koa: req.headers.cookie
+        //   vercel, sails.js, express (w/ cookie middleware): req.cookies
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        requestData.cookies = req.cookies || cookie.parse(headers.cookie || '');
+        break;
+      case 'query_string':
+        // query string:
+        //   node: req.url (raw)
+        //   express, koa: req.query
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        requestData.query_string = url.parse(originalUrl || '', false).query;
+        break;
+      case 'data':
+        if (method === 'GET' || method === 'HEAD') {
+          break;
+        }
+        // body data:
+        //   node, express, koa: req.body
+        if (req.body !== undefined) {
+          requestData.data = isString(req.body) ? req.body : JSON.stringify(normalize(req.body));
+        }
+        break;
+      default:
+        if ({}.hasOwnProperty.call(req, key)) {
+          requestData[key] = (req as { [key: string]: any })[key];
+        }
+    }
+  });
+
+  return requestData;
+}
+
 /**
  * Options deciding what parts of the request to use when enhancing an event
  */
@@ -222,10 +308,10 @@ export function parseRequest(event: Event, req: ExpressRequest, options?: ParseR
   }
 
   if (options.request) {
-    // if the option value is `true`, use the default set of keys by not passing anything to `extractNodeRequestData()`
+    // if the option value is `true`, use the default set of keys by not passing anything to `extractRequestData()`
     const extractedRequestData = Array.isArray(options.request)
-      ? extractNodeRequestData(req, options.request)
-      : extractNodeRequestData(req);
+      ? extractRequestData(req, options.request)
+      : extractRequestData(req);
     event.request = {
       ...event.request,
       ...extractedRequestData,
--- packages/node/test/handlers.test.ts
+++ packages/node/test/handlers.test.ts
@@ -8,7 +8,7 @@ import * as net from 'net';
 
 import { Event, Request, User } from '../src';
 import { NodeClient } from '../src/client';
-import { ExpressRequest, parseRequest, tracingHandler } from '../src/handlers';
+import { ExpressRequest, extractRequestData, parseRequest, tracingHandler } from '../src/handlers';
 
 describe('parseRequest', () => {
   let mockReq: { [key: string]: any };
@@ -345,4 +345,179 @@ describe('tracingHandler', () => {
       done();
     });
   });
-}); // end describe('tracingHandler')
+});
+
+describe('extractRequestData()', () => {
+  describe('default behaviour', () => {
+    test('node', () => {
+      expect(
+        extractRequestData({
+          headers: { host: 'example.com' },
+          method: 'GET',
+          secure: true,
+          originalUrl: '/',
+        }),
+      ).toEqual({
+        cookies: {},
+        headers: {
+          host: 'example.com',
+        },
+        method: 'GET',
+        query_string: null,
+        url: 'https://example.com/',
+      });
+    });
+
+    test('degrades gracefully without request data', () => {
+      expect(extractRequestData({})).toEqual({
+        cookies: {},
+        headers: {},
+        method: undefined,
+        query_string: null,
+        url: 'http://<no host>',
+      });
+    });
+  });
+
+  describe('cookies', () => {
+    it('uses `req.cookies` if available', () => {
+      expect(
+        extractRequestData(
+          {
+            cookies: { foo: 'bar' },
+          },
+          ['cookies'],
+        ),
+      ).toEqual({
+        cookies: { foo: 'bar' },
+      });
+    });
+
+    it('parses the cookie header', () => {
+      expect(
+        extractRequestData(
+          {
+            headers: {
+              cookie: 'foo=bar;',
+            },
+          },
+          ['cookies'],
+        ),
+      ).toEqual({
+        cookies: { foo: 'bar' },
+      });
+    });
+
+    it('falls back if no cookies are defined', () => {
+      expect(extractRequestData({}, ['cookies'])).toEqual({
+        cookies: {},
+      });
+    });
+  });
+
+  describe('data', () => {
+    it('includes data from `req.body` if available', () => {
+      expect(
+        extractRequestData(
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: 'foo=bar',
+          },
+          ['data'],
+        ),
+      ).toEqual({
+        data: 'foo=bar',
+      });
+    });
+
+    it('encodes JSON body contents back to a string', () => {
+      expect(
+        extractRequestData(
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: { foo: 'bar' },
+          },
+          ['data'],
+        ),
+      ).toEqual({
+        data: '{"foo":"bar"}',
+      });
+    });
+  });
+
+  describe('query_string', () => {
+    it('parses the query parms from the url', () => {
+      expect(
+        extractRequestData(
+          {
+            headers: { host: 'example.com' },
+            secure: true,
+            originalUrl: '/?foo=bar',
+          },
+          ['query_string'],
+        ),
+      ).toEqual({
+        query_string: 'foo=bar',
+      });
+    });
+
+    it('gracefully degrades if url cannot be determined', () => {
+      expect(extractRequestData({}, ['query_string'])).toEqual({
+        query_string: null,
+      });
+    });
+  });
+
+  describe('url', () => {
+    test('express/koa', () => {
+      expect(
+        extractRequestData(
+          {
+            host: 'example.com',
+            protocol: 'https',
+            url: '/',
+          },
+          ['url'],
+        ),
+      ).toEqual({
+        url: 'https://example.com/',
+      });
+    });
+
+    test('node', () => {
+      expect(
+        extractRequestData(
+          {
+            headers: { host: 'example.com' },
+            secure: true,
+            originalUrl: '/',
+          },
+          ['url'],
+        ),
+      ).toEqual({
+        url: 'https://example.com/',
+      });
+    });
+  });
+
+  describe('custom key', () => {
+    it('includes the custom key if present', () => {
+      expect(
+        extractRequestData(
+          {
+            httpVersion: '1.1',
+          },
+          ['httpVersion'],
+        ),
+      ).toEqual({
+        httpVersion: '1.1',
+      });
+    });
+
+    it('gracefully degrades if the custom key is missing', () => {
+      expect(extractRequestData({}, ['httpVersion'])).toEqual({});
+    });
+  });
+});
