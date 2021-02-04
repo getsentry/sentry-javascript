8cb7a49c756ebfd0f16219a6f54a8de96576f7ac
Katie Byers <katie.byers@sentry.io>
2021-02-04 02:27:15 -0800

    ref(CI): Speed up testing in GHA (#3237)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 set -e
-source ~/.nvm/nvm.sh
 
-# We need this check to skip engines check for typescript-tslint-plugin package
-if [[ "$(cut -d. -f1 <<< "$NODE_VERSION")" -le 6 ]]; then
-  nvm install 8
-  nvm use 8
-  yarn install --ignore-engines --ignore-scripts
-  # current versions of nock don't support node 6
+# control which packages we test on each version of node
+if [[ "$(cut -d. -f1 <<<"$NODE_VERSION")" -le 6 ]]; then
+
+  # install legacy versions of packages whose current versions don't support node 6
+  # ignoring engines and scripts lets us get away with having incompatible things installed for packages we're not testing
   cd packages/node
-  yarn add --dev --ignore-engines nock@10.x
+  yarn add --dev --ignore-engines --ignore-scripts nock@10.x
   cd ../..
-  # ember requires Node >= 10 to build
-  yarn build --ignore="@sentry/ember" --ignore="@sentry/serverless" --ignore="@sentry/gatsby" --ignore="@sentry/react" --ignore="@sentry/wasm"
-  nvm install 6
-  nvm use 6
-  # browser can be tested only on Node >= v8 because Karma is not supporting anything older
-  yarn test --ignore="@sentry/tracing" --ignore="@sentry/react" --ignore="@sentry/wasm" --ignore="@sentry/gatsby" --ignore="@sentry/ember" --ignore="@sentry-internal/eslint-plugin-sdk" --ignore="@sentry-internal/eslint-config-sdk" --ignore="@sentry/serverless" --ignore="@sentry/browser" --ignore="@sentry/integrations" --ignore="@sentry/utils"
-elif [[ "$(cut -d. -f1 <<< "$NODE_VERSION")" -le 8 ]]; then
-  yarn install --ignore-engines --ignore-scripts
-  # ember requires Node >= 10 to build
-  yarn build --ignore="@sentry/ember" --ignore="@sentry/serverless" --ignore="@sentry/gatsby" --ignore="@sentry/react" --ignore="@sentry/wasm"
-  # serverless, tracing, ember and react work only on Node >= v10
-  yarn test --ignore="@sentry/tracing" --ignore="@sentry/react" --ignore="@sentry/wasm" --ignore="@sentry/gatsby" --ignore="@sentry/ember" --ignore="@sentry-internal/eslint-plugin-sdk" --ignore="@sentry-internal/eslint-config-sdk" --ignore="@sentry/serverless"
+  cd packages/tracing
+  yarn add --dev --ignore-engines --ignore-scripts jsdom@11.x
+  cd ../..
+
+  # only test against @sentry/node and its dependencies - node 6 is too old for anything else to work
+  yarn test --scope="@sentry/core" --scope="@sentry/hub" --scope="@sentry/minimal" --scope="@sentry/node" --scope="@sentry/utils" --scope="@sentry/tracing"
+
+elif [[ "$(cut -d. -f1 <<<"$NODE_VERSION")" -le 8 ]]; then
+
+  # install legacy versions of packages whose current versions don't support node 8
+  # ignoring engines and scripts lets us get away with having incompatible things installed for packages we're not testing
+  cd packages/tracing
+  yarn add --dev --ignore-engines --ignore-scripts jsdom@15.x
+  cd ../..
+
+  # ember tests happen separately, and the rest fail on node 8 for various syntax or dependency reasons
+  yarn test --ignore="@sentry/ember" --ignore="@sentry-internal/eslint-plugin-sdk" --ignore="@sentry/react" --ignore="@sentry/wasm" --ignore="@sentry/gatsby" --ignore="@sentry/serverless"
+
 else
-  yarn install
-  yarn build
-  yarn test
+  yarn test --ignore="@sentry/ember"
+
 fi
