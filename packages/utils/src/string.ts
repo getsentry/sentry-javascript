6124bdb052b186aa62a9ab1429cf756bdf43dcfe
Katie Byers <katie.byers@sentry.io>
2021-02-06 13:43:51 -0800

    ref(tracing): Prework for initial `tracestate` implementation (#3242)

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,3 +1,5 @@
+import { getGlobalObject } from './compat';
+import { SentryError } from './error';
 import { isRegExp, isString } from './is';
 
 /**
@@ -100,4 +102,96 @@ export function isMatchingPattern(value: string, pattern: RegExp | string): bool
     return value.indexOf(pattern) !== -1;
   }
   return false;
+}
+
+/**
+ * Convert a Unicode string to a base64 string.
+ *
+ * @param plaintext The string to base64-encode
+ * @throws SentryError (because using the logger creates a circular dependency)
+ * @returns A base64-encoded version of the string
+ */
+export function unicodeToBase64(plaintext: string): string {
+  const global = getGlobalObject();
+
+  // Cast to a string just in case we're given something else
+  const stringifiedInput = String(plaintext);
+  const errMsg = `Unable to convert to base64: ${
+    stringifiedInput.length > 256 ? `${stringifiedInput.slice(0, 256)}...` : stringifiedInput
+  }`;
+
+  // To account for the fact that different platforms use different character encodings natively, our `tracestate`
+  // spec calls for all jsonified data to be encoded in UTF-8 bytes before being passed to the base64 encoder.
+  try {
+    // browser
+    if ('btoa' in global) {
+      // encode using UTF-8
+      const bytes = new TextEncoder().encode(plaintext);
+
+      // decode using UTF-16 (JS's native encoding) since `btoa` requires string input
+      const bytesAsString = String.fromCharCode(...bytes);
+
+      return btoa(bytesAsString);
+    }
+
+    // Node
+    if ('Buffer' in global) {
+      // encode using UTF-8
+      const bytes = Buffer.from(plaintext, 'utf-8');
+
+      // unlike the browser, Node can go straight from bytes to base64
+      return bytes.toString('base64');
+    }
+  } catch (err) {
+    throw new SentryError(`${errMsg} Got error: ${err}`);
+  }
+
+  // we shouldn't ever get here, because one of `btoa` and `Buffer` should exist, but just in case...
+  throw new SentryError(errMsg);
+}
+
+/**
+ * Convert a base64 string to a Unicode string.
+ *
+ * @param base64String The string to decode
+ * @throws SentryError (because using the logger creates a circular dependency)
+ * @returns A Unicode string
+ */
+export function base64ToUnicode(base64String: string): string {
+  const globalObject = getGlobalObject();
+
+  // we cast to a string just in case we're given something else
+  const stringifiedInput = String(base64String);
+  const errMsg = `Unable to convert from base64: ${
+    stringifiedInput.length > 256 ? `${stringifiedInput.slice(0, 256)}...` : stringifiedInput
+  }`;
+
+  // To account for the fact that different platforms use different character encodings natively, our `tracestate` spec
+  // calls for all jsonified data to be encoded in UTF-8 bytes before being passed to the base64 encoder. So to reverse
+  // the process, decode from base64 to bytes, then feed those bytes to a UTF-8 decoder.
+  try {
+    // browser
+    if ('atob' in globalObject) {
+      // `atob` returns a string rather than bytes, so we first need to encode using the native encoding (UTF-16)
+      const bytesAsString = atob(base64String);
+      const bytes = [...bytesAsString].map(char => char.charCodeAt(0));
+
+      // decode using UTF-8 (cast the `bytes` arry to a Uint8Array just because that's the format `decode()` expects)
+      return new TextDecoder().decode(Uint8Array.from(bytes));
+    }
+
+    // Node
+    if ('Buffer' in globalObject) {
+      // unlike the browser, Node can go straight from base64 to bytes
+      const bytes = Buffer.from(base64String, 'base64');
+
+      // decode using UTF-8
+      return bytes.toString('utf-8');
+    }
+  } catch (err) {
+    throw new SentryError(`${errMsg} Got error: ${err}`);
+  }
+
+  // we shouldn't ever get here, because one of `atob` and `Buffer` should exist, but just in case...
+  throw new SentryError(errMsg);
 }
