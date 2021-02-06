02f1788ef99ae1950185f9ad5f385b97dd1ec2a2
Katie Byers <katie.byers@sentry.io>
2021-02-06 13:43:51 -0800

    ref(tracing): Prework for initial `tracestate` implementation (#3242)

--- packages/core/src/request.ts
+++ packages/core/src/request.ts
@@ -31,7 +31,106 @@ function enhanceEventWithSdkInfo(event: Event, sdkInfo?: SdkInfo): Event {
   return event;
 }
 
-/** Creates a SentryRequest from an event. */
+/**
+ * Create a SentryRequest from an error, message, or transaction event.
+ *
+ * @param event The event to send
+ * @param api Helper to provide the correct url for the request
+ * @returns SentryRequest representing the event
+ */
+export function eventToSentryRequest(event: Event, api: API): SentryRequest {
+  const sdkInfo = getSdkMetadataForEnvelopeHeader(api);
+  const eventWithSdkInfo = sdkInfo ? enhanceEventWithSdkInfo(event, api.metadata.sdk) : event;
+
+  if (event.type === 'transaction') {
+    return transactionToSentryRequest(eventWithSdkInfo, api);
+  }
+  return {
+    body: JSON.stringify(eventWithSdkInfo),
+    type: event.type || 'event',
+    url: api.getStoreEndpointWithUrlEncodedAuth(),
+  };
+}
+
+/**
+ * Create a SentryRequest from a transaction event.
+ *
+ * Since we don't need to manipulate envelopes nor store them, there is no exported concept of an Envelope with
+ * operations including serialization and deserialization. Instead, we only implement a minimal subset of the spec to
+ * serialize events inline here. See https://develop.sentry.dev/sdk/envelopes/.
+ *
+ * @param event The transaction event to send
+ * @param api Helper to provide the correct url for the request
+ * @returns SentryRequest in envelope form
+ */
+export function transactionToSentryRequest(event: Event, api: API): SentryRequest {
+  const sdkInfo = getSdkMetadataForEnvelopeHeader(api);
+
+  const { transactionSampling, ...metadata } = event.debug_meta || {};
+  const { method: samplingMethod, rate: sampleRate } = transactionSampling || {};
+  if (Object.keys(metadata).length === 0) {
+    delete event.debug_meta;
+  } else {
+    event.debug_meta = metadata;
+  }
+
+  const envelopeHeaders = JSON.stringify({
+    event_id: event.event_id,
+    sent_at: new Date().toISOString(),
+    ...(sdkInfo && { sdk: sdkInfo }),
+
+    // trace context for dynamic sampling on relay
+    trace: {
+      trace_id: event.contexts?.trace?.trace_id,
+      public_key: api.getDsn().publicKey,
+      environment: event.environment || null,
+      release: event.release || null,
+    },
+  });
+
+  const itemHeaders = JSON.stringify({
+    type: event.type,
+
+    // TODO: Right now, sampleRate will be undefined in the cases of inheritance and explicitly-set sampling decisions.
+    sample_rates: [{ id: samplingMethod, rate: sampleRate }],
+
+    // Note: `content_type` and `length` were left out on purpose. Here's a quick explanation of why, along with the
+    // value to use if we ever decide to put them back in.
+    //
+    // `content_type`:
+    // Assumed to be 'application/json' and not part of the current spec for transaction items. No point in bloating the
+    // request body with it.
+    //
+    // would be:
+    // content_type: 'application/json',
+    //
+    // `length`:
+    // Optional and equal to the number of bytes in req.Body encoded as UTF-8. Since the server can figure this out and
+    // would otherwise refuse events that report the length incorrectly, we decided not to send the length to avoid
+    // problems related to reporting the wrong size and to reduce request body size.
+    //
+    // would be:
+    // length: new TextEncoder().encode(req.body).length,
+  });
+
+  const req: SentryRequest = {
+    // The trailing newline is optional; leave it off to avoid sending unnecessary bytes.
+    // body: `${envelopeHeaders}\n${itemHeaders}\n${JSON.stringify(event)\n}`,
+    body: `${envelopeHeaders}\n${itemHeaders}\n${JSON.stringify(event)}`,
+    type: 'transaction',
+    url: api.getEnvelopeEndpointWithUrlEncodedAuth(),
+  };
+
+  return req;
+}
+
+/**
+ * Create a SentryRequest from a session event.
+ *
+ * @param event The session event to send
+ * @param api Helper to provide the correct url for the request
+ * @returns SentryRequest in envelope form
+ */
 export function sessionToSentryRequest(session: Session, api: API): SentryRequest {
   const sdkInfo = getSdkMetadataForEnvelopeHeader(api);
   const envelopeHeaders = JSON.stringify({
@@ -47,78 +146,4 @@ export function sessionToSentryRequest(session: Session, api: API): SentryReques
     type: 'session',
     url: api.getEnvelopeEndpointWithUrlEncodedAuth(),
   };
-}
-
-/** Creates a SentryRequest from an event. */
-export function eventToSentryRequest(event: Event, api: API): SentryRequest {
-  const sdkInfo = getSdkMetadataForEnvelopeHeader(api);
-  const eventType = event.type || 'event';
-  const useEnvelope = eventType === 'transaction';
-
-  const { transactionSampling, ...metadata } = event.debug_meta || {};
-  const { method: samplingMethod, rate: sampleRate } = transactionSampling || {};
-  if (Object.keys(metadata).length === 0) {
-    delete event.debug_meta;
-  } else {
-    event.debug_meta = metadata;
-  }
-
-  const req: SentryRequest = {
-    body: JSON.stringify(sdkInfo ? enhanceEventWithSdkInfo(event, api.metadata.sdk) : event),
-    type: eventType,
-    url: useEnvelope ? api.getEnvelopeEndpointWithUrlEncodedAuth() : api.getStoreEndpointWithUrlEncodedAuth(),
-  };
-
-  // https://develop.sentry.dev/sdk/envelopes/
-
-  // Since we don't need to manipulate envelopes nor store them, there is no
-  // exported concept of an Envelope with operations including serialization and
-  // deserialization. Instead, we only implement a minimal subset of the spec to
-  // serialize events inline here.
-  if (useEnvelope) {
-    const envelopeHeaders = JSON.stringify({
-      event_id: event.event_id,
-      sent_at: new Date().toISOString(),
-      ...(sdkInfo && { sdk: sdkInfo }),
-
-      // trace context for dynamic sampling on relay
-      trace: {
-        trace_id: event.contexts?.trace?.trace_id,
-        public_key: api.getDsn().publicKey,
-        environment: event.environment || null,
-        release: event.release || null,
-      },
-    });
-
-    const itemHeaders = JSON.stringify({
-      type: event.type,
-
-      // TODO: Right now, sampleRate may or may not be defined (it won't be in the cases of inheritance and
-      // explicitly-set sampling decisions). Are we good with that?
-      sample_rates: [{ id: samplingMethod, rate: sampleRate }],
-
-      // The content-type is assumed to be 'application/json' and not part of
-      // the current spec for transaction items, so we don't bloat the request
-      // body with it.
-      //
-      // content_type: 'application/json',
-      //
-      // The length is optional. It must be the number of bytes in req.Body
-      // encoded as UTF-8. Since the server can figure this out and would
-      // otherwise refuse events that report the length incorrectly, we decided
-      // not to send the length to avoid problems related to reporting the wrong
-      // size and to reduce request body size.
-      //
-      // length: new TextEncoder().encode(req.body).length,
-    });
-
-    // The trailing newline is optional. We intentionally don't send it to avoid
-    // sending unnecessary bytes.
-    //
-    // const envelope = `${envelopeHeaders}\n${itemHeaders}\n${req.body}\n`;
-    const envelope = `${envelopeHeaders}\n${itemHeaders}\n${req.body}`;
-    req.body = envelope;
-  }
-
-  return req;
 }
