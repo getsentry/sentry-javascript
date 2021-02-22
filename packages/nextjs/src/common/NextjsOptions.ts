56e6056a1e49875466357f280cfc5651579f6763
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-02-22 13:20:05 +0100

    feat: Add NextJS options

---
+++ packages/nextjs/src/common/NextjsOptions.ts
@@ -0,0 +1,7 @@
+import { BrowserOptions } from '@sentry/browser';
+import { NodeOptions } from '@sentry/node';
+import { Options } from '@sentry/types';
+
+export interface NextjsOptions extends Options, BrowserOptions, NodeOptions {
+  // TODO: options for NextJS
+}
