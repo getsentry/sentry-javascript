f62ecc7623641a9863c8f1ba47e70752892033e5
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-02-23 13:47:17 +0100

    feat: Add NextJS entry point

---
+++ packages/nextjs/src/index.ts
@@ -0,0 +1,36 @@
+export {
+  Breadcrumb,
+  Request,
+  SdkInfo,
+  Event,
+  Exception,
+  Response,
+  Severity,
+  StackFrame,
+  Stacktrace,
+  Status,
+  Thread,
+  User,
+} from '@sentry/types';
+
+export {
+  addGlobalEventProcessor,
+  addBreadcrumb,
+  captureException,
+  captureEvent,
+  captureMessage,
+  configureScope,
+  getHubFromCarrier,
+  getCurrentHub,
+  Hub,
+  Scope,
+  setContext,
+  setExtra,
+  setExtras,
+  setTag,
+  setTags,
+  setUser,
+  withScope,
+} from '@sentry/core';
+
+export { init, showReportDialog, flush, close } from './sdk';
