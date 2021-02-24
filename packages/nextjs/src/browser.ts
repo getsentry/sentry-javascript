57a7c94ea0ec5c80b17f602c3bd2982ea5be7768
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-02-24 15:45:56 +0100

    docs: Add docs to browser and node inits

--- packages/nextjs/src/browser.ts
+++ packages/nextjs/src/browser.ts
@@ -2,7 +2,7 @@ import * as SentryBrowser from '@sentry/browser';
 
 import { NextjsOptions } from './options';
 
-/** */
+/** Inits the Sentry NextJS SDK on the browser. */
 export function init(options: NextjsOptions): any {
   SentryBrowser.init(options);
 }
--- packages/nextjs/src/node.ts
+++ packages/nextjs/src/node.ts
@@ -2,7 +2,7 @@ import * as SentryNode from '@sentry/node';
 
 import { NextjsOptions } from './options';
 
-/** */
+/** Inits the Sentry NextJS SDK on node. */
 export function init(options: NextjsOptions): any {
   SentryNode.init(options);
 }
