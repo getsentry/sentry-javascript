92a1ba2a63d8d55e5c543783afcc37b866956e56
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-03-03 11:09:49 +0100

    fix: Check if object is undefined

--- packages/react/src/sdk.ts
+++ packages/react/src/sdk.ts
@@ -5,7 +5,7 @@ import { BrowserOptions, init as browserInit, SDK_VERSION } from '@sentry/browse
  */
 export function init(options: BrowserOptions): void {
   options._metadata = options._metadata || {};
-  if (!options._metadata.sdk) {
+  if (options._metadata.sdk === undefined) {
     options._metadata.sdk = {
       name: 'sentry.javascript.react',
       packages: [
