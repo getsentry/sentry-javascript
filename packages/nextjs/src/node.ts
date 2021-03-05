3db3cce1563a8e5d5bcd80a34a04d1be128c51ff
Daniel Griesser <daniel.griesser.86@gmail.com>
2021-03-05 11:47:48 +0100

    feat: Export ErrorBoundary

--- packages/nextjs/src/node.ts
+++ packages/nextjs/src/node.ts
@@ -6,6 +6,10 @@ import { NextjsOptions } from './utils/nextjsOptions';
 
 export * from '@sentry/node';
 
+// Here we want to make sure to only include what doesn't have browser specifics
+// because or SSR of next.js we can only use this.
+export { ErrorBoundary, withErrorBoundary } from '@sentry/react';
+
 /** Inits the Sentry NextJS SDK on node. */
 export function init(options: NextjsOptions): any {
   const metadataBuilder = new MetadataBuilder(options, ['nextjs', 'node']);
