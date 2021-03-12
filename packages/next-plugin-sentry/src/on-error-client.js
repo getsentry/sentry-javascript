e589d1097a9e35e64c71a0c61d242d8135d2353c
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-03-12 14:15:06 +0100

    ref: Remove unused args and code

--- packages/next-plugin-sentry/src/on-error-client.js
+++ packages/next-plugin-sentry/src/on-error-client.js
@@ -1,14 +1,7 @@
 import { withScope, captureException } from '@sentry/nextjs';
 
-export default async function onErrorClient({ err, errorInfo, renderErrorProps, data, version }) {
-  // TODO: Extract some useful metadata from the router and other arguments — Kamil
-
+export default async function onErrorClient({ err }) {
   withScope(scope => {
-    if (typeof errorInfo?.componentStack === 'string') {
-      scope.setContext('react', {
-        componentStack: errorInfo.componentStack.trim(),
-      });
-    }
     captureException(err);
   });
 }
