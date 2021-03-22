3d2ffe12dabad7e7712923f2dbb0aecca0ed0a7f
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-03-22 15:44:05 +0100

    fix: Reference correct variable

--- packages/next-plugin-sentry/src/on-error-client.js
+++ packages/next-plugin-sentry/src/on-error-client.js
@@ -3,7 +3,7 @@ import { withScope, captureException, captureMessage } from '@sentry/nextjs';
 export default async function onErrorClient({ err }) {
   withScope(scope => {
     if (err instanceof Error) {
-      captureException(toCapture);
+      captureException(err);
     } else {
       captureMessage(err.message);
     }
