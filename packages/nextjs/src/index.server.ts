db74d74d045c56d941af17fe242b534d8d56889f
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-07 11:37:29 +0200

    ref(nextjs): Remove `initDecider` (#3359)

--- packages/nextjs/src/index.server.ts
+++ packages/nextjs/src/index.server.ts
@@ -1,7 +1,6 @@
 import { RewriteFrames } from '@sentry/integrations';
 import { configureScope, init as nodeInit } from '@sentry/node';
 
-import { InitDecider } from './utils/initDecider';
 import { MetadataBuilder } from './utils/metadataBuilder';
 import { NextjsOptions } from './utils/nextjsOptions';
 
@@ -17,33 +16,25 @@ const SOURCEMAP_FILENAME_REGEX = /^.*\/.next\//;
 export function init(options: NextjsOptions): void {
   const metadataBuilder = new MetadataBuilder(options, ['nextjs', 'node']);
   metadataBuilder.addSdkMetadata();
-  const initDecider = new InitDecider(options);
-  if (initDecider.shouldInitSentry()) {
-    nodeInit({
-      ...options,
-      // TODO: handle use cases when users provide integrations
-      integrations: [
-        new RewriteFrames({
-          iteratee: frame => {
-            try {
-              if (frame.filename) {
-                frame.filename = frame.filename.replace(SOURCEMAP_FILENAME_REGEX, 'app:///_next/');
-              }
-            } catch {
-              //
+  nodeInit({
+    ...options,
+    // TODO: handle use cases when users provide integrations
+    integrations: [
+      new RewriteFrames({
+        iteratee: frame => {
+          try {
+            if (frame.filename) {
+              frame.filename = frame.filename.replace(SOURCEMAP_FILENAME_REGEX, 'app:///_next/');
             }
-            return frame;
-          },
-        }),
-      ],
-    });
-    configureScope(scope => {
-      scope.setTag('runtime', 'node');
-    });
-  } else {
-    // eslint-disable-next-line no-console
-    console.warn('[Sentry] Detected a non-production environment. Not initializing Sentry.');
-    // eslint-disable-next-line no-console
-    console.warn('[Sentry] To use Sentry also in development set `dev: true` in the options.');
-  }
+          } catch {
+            //
+          }
+          return frame;
+        },
+      }),
+    ],
+  });
+  configureScope(scope => {
+    scope.setTag('runtime', 'node');
+  });
 }
