36b67710fd089bc5f32338cb06688d3b483283fa
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-07 15:02:07 +0200

    test: Add serverIntegrations test

---
+++ packages/nextjs/test/serverIntegrations.test.ts
@@ -0,0 +1,54 @@
+import { RewriteFrames } from '@sentry/integrations';
+import { Integration } from '@sentry/types';
+
+import { getFinalServerIntegrations, REWRITE_FRAMES_INTEGRATION } from '../src/utils/serverIntegrations';
+
+interface IntegrationFunction {
+  (integrations: Integration[]): Integration[];
+}
+
+describe('integrations without RewriteFrames', () => {
+  test('as an array', () => {
+    const integrations: Integration[] = [];
+    // Should get a single integration: RewriteFrames
+    let finalIntegrations = getFinalServerIntegrations(integrations);
+    expect(Array.isArray(finalIntegrations)).toBeTruthy();
+    finalIntegrations = finalIntegrations as Integration[];
+    expect(finalIntegrations.length === 1).toBeTruthy();
+    expect(finalIntegrations[0]).toMatchObject(REWRITE_FRAMES_INTEGRATION);
+  });
+
+  test('as a function', () => {
+    const integrations: IntegrationFunction = (): Integration[] => {
+      return [];
+    };
+    // Should get a single integration: RewriteFrames
+    const integrationWrapper = getFinalServerIntegrations(integrations);
+    expect(typeof integrationWrapper === 'function').toBeTruthy();
+    const finalIntegrations = (integrationWrapper as IntegrationFunction)([]);
+    expect(finalIntegrations.length === 1).toBeTruthy();
+    expect(finalIntegrations[0]).toMatchObject(REWRITE_FRAMES_INTEGRATION);
+  });
+});
+
+describe('integrations with RewriteFrames', () => {
+  test('as an array', () => {
+    const rewriteFramesIntegration = new RewriteFrames();
+    const integrations = [rewriteFramesIntegration];
+    // Should get the same array (with no patches)
+    const finalIntegrations = getFinalServerIntegrations(integrations);
+    expect(finalIntegrations).toMatchObject(integrations);
+  });
+
+  test('as a function', () => {
+    const integrations = [new RewriteFrames()];
+    const integrationsFnc: IntegrationFunction = (_integrations: Integration[]): Integration[] => {
+      return integrations;
+    };
+    // Should get a function that returns the RewriteFramesIntegration
+    let finalIntegrations = getFinalServerIntegrations(integrationsFnc);
+    expect(typeof finalIntegrations === 'function').toBeTruthy();
+    finalIntegrations = finalIntegrations as IntegrationFunction;
+    expect(finalIntegrations([])).toMatchObject(integrations);
+  });
+});
