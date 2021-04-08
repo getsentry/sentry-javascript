b04423930e0a839ed9fb6ffccffd3bc1de348f39
Kamil Ogórek <kamil@sentry.io>
2021-04-08 13:34:52 +0200

    Update packages/tracing/src/transaction.ts

--- packages/tracing/src/transaction.ts
+++ packages/tracing/src/transaction.ts
@@ -132,7 +132,8 @@ export class Transaction extends SpanClass implements TransactionInterface {
       logger.log('[Measurements] Adding measurements to transaction', JSON.stringify(this._measurements, undefined, 2));
       transaction.measurements = this._measurements;
     }
-    logger.log(`[Tracing] finishing ${this.op} transaction - ${this.name}`);
+
+    logger.log(`[Tracing] Finishing ${this.op} transaction: ${this.name}.`);
 
     return this._hub.captureEvent(transaction);
   }
