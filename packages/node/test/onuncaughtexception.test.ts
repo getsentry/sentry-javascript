f27d084a032907b151966a34074c1c74915325f6
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-04-12 10:51:11 +0200

    Fixed process exiting at the end of test in onuncaughtexception tests

--- packages/node/test/onuncaughtexception.test.ts
+++ packages/node/test/onuncaughtexception.test.ts
@@ -1,8 +1,13 @@
 import { init, Integrations, NodeClient, Scope } from '../src';
 
 const dsn = 'https://53039209a22b4ec1bcc296a3c9fdecd6@sentry.io/4291';
+const mockedOnFatalError = jest.fn();
 
 describe('onuncaught exceptions', () => {
+  beforeEach(() => {
+    mockedOnFatalError.mockClear();
+  });
+
   afterEach(() => {
     jest.restoreAllMocks();
   });
@@ -13,13 +18,14 @@ describe('onuncaught exceptions', () => {
     const captureRequestSession = jest.spyOn(NodeClient.prototype, 'captureRequestSession');
     const setExtra = jest.spyOn(Scope.prototype, 'setExtra');
 
-    const integration = new Integrations.OnUncaughtException();
+    const integration = new Integrations.OnUncaughtException({ onFatalError: mockedOnFatalError });
 
     integration.handler({ message: 'message', name: 'name' });
 
     // Ensure when autoSessionTracking is enabled, captureRequestSession is called
     expect(captureRequestSession).toHaveBeenCalledTimes(1);
     expect(setExtra.mock.calls[0]).toEqual(['onUncaughtException', true]);
+    expect(mockedOnFatalError).toHaveBeenCalledTimes(1);
   });
 
   test('send onUncaughtException, when autoSessionTracking is enabled and no release exists', () => {
@@ -28,13 +34,14 @@ describe('onuncaught exceptions', () => {
     const captureRequestSession = jest.spyOn(NodeClient.prototype, 'captureRequestSession');
     const setExtra = jest.spyOn(Scope.prototype, 'setExtra');
 
-    const integration = new Integrations.OnUncaughtException();
+    const integration = new Integrations.OnUncaughtException({ onFatalError: mockedOnFatalError });
 
     integration.handler({ message: 'message', name: 'name' });
 
     // Ensure when autoSessionTracking is disabled, captureRequestSession is not called
     expect(captureRequestSession).toHaveBeenCalledTimes(0);
     expect(setExtra.mock.calls[0]).toEqual(['onUncaughtException', true]);
+    expect(mockedOnFatalError).toHaveBeenCalledTimes(1);
   });
 
   test('send onUncaughtException, when autoSessionTracking is disabled', () => {
@@ -43,12 +50,13 @@ describe('onuncaught exceptions', () => {
     const captureRequestSession = jest.spyOn(NodeClient.prototype, 'captureRequestSession');
     const setExtra = jest.spyOn(Scope.prototype, 'setExtra');
 
-    const integration = new Integrations.OnUncaughtException();
+    const integration = new Integrations.OnUncaughtException({ onFatalError: mockedOnFatalError });
 
     integration.handler({ message: 'message', name: 'name' });
 
     // Ensure when autoSessionTracking is disabled, captureRequestSession is not called
     expect(captureRequestSession).toHaveBeenCalledTimes(0);
     expect(setExtra.mock.calls[0]).toEqual(['onUncaughtException', true]);
+    expect(mockedOnFatalError).toHaveBeenCalledTimes(1);
   });
 });
