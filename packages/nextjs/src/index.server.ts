6fa9feb5244c6064e9fa9ea7ce2858ca28ff5307
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-13 12:23:52 +0200

    feat(nextjs): Add support for integrations on the server-side (#3375)

--- packages/nextjs/src/index.server.ts
+++ packages/nextjs/src/index.server.ts
@@ -1,8 +1,8 @@
-import { RewriteFrames } from '@sentry/integrations';
 import { configureScope, init as nodeInit } from '@sentry/node';
 
 import { MetadataBuilder } from './utils/metadataBuilder';
 import { NextjsOptions } from './utils/nextjsOptions';
+import { defaultRewriteFrames, getFinalServerIntegrations } from './utils/serverIntegrations';
 
 export * from '@sentry/node';
 
@@ -10,30 +10,17 @@ export * from '@sentry/node';
 // because or SSR of next.js we can only use this.
 export { ErrorBoundary, withErrorBoundary } from '@sentry/react';
 
-const SOURCEMAP_FILENAME_REGEX = /^.*\/.next\//;
-
 /** Inits the Sentry NextJS SDK on node. */
 export function init(options: NextjsOptions): void {
   const metadataBuilder = new MetadataBuilder(options, ['nextjs', 'node']);
   metadataBuilder.addSdkMetadata();
-  nodeInit({
-    ...options,
-    // TODO: handle use cases when users provide integrations
-    integrations: [
-      new RewriteFrames({
-        iteratee: frame => {
-          try {
-            if (frame.filename) {
-              frame.filename = frame.filename.replace(SOURCEMAP_FILENAME_REGEX, 'app:///_next/');
-            }
-          } catch {
-            //
-          }
-          return frame;
-        },
-      }),
-    ],
-  });
+  if (options.integrations) {
+    options.integrations = getFinalServerIntegrations(options.integrations);
+  } else {
+    options.integrations = [defaultRewriteFrames];
+  }
+
+  nodeInit(options);
   configureScope(scope => {
     scope.setTag('runtime', 'node');
   });
