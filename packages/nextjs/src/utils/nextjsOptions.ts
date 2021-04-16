d31a5c73fbbc948133539250261048734b43221a
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-16 16:40:00 +0200

    ref: Convert NextjsOptions to union type
    Instead of having an empty interface, set the type as the union of the interfaces it's made of.

--- packages/nextjs/src/utils/nextjsOptions.ts
+++ packages/nextjs/src/utils/nextjsOptions.ts
@@ -2,6 +2,4 @@ import { NodeOptions } from '@sentry/node';
 import { BrowserOptions } from '@sentry/react';
 import { Options } from '@sentry/types';
 
-export interface NextjsOptions extends Options, BrowserOptions, NodeOptions {
-  // Add Next.js specific options here
-}
+export type NextjsOptions = Options | BrowserOptions | NodeOptions;
