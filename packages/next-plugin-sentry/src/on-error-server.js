68ad73de4308a6ebd57c1dace0eb74d4e8a8613b
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-20 14:33:33 +0200

    ref: Check for undefined

--- packages/next-plugin-sentry/src/on-error-server.js
+++ packages/next-plugin-sentry/src/on-error-server.js
@@ -8,7 +8,7 @@ export default async function onErrorServer(err) {
   const sentryTimeout = serverRuntimeConfig.sentryTimeout || publicRuntimeConfig.sentryTimeout || 2000;
 
   withScope(scope => {
-    if (typeof err.req !== 'undefined') {
+    if (err.req) {
       scope.addEventProcessor(event => {
         return parseRequest(event, err.req, {
           // 'cookies' and 'query_string' use `dynamicRequire` which has a bug in SSR envs right now — Kamil
