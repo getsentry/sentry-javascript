f86785b2a272bb0e968385d71b408e791c34d41b
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-04-27 12:21:23 +0200

    Process request for captured error

--- packages/nextjs/src/utils/handlers.ts
+++ packages/nextjs/src/utils/handlers.ts
@@ -1,5 +1,7 @@
-import { captureException, flush } from '@sentry/node';
+import { captureException, flush, Handlers, withScope } from '@sentry/node';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+const { parseRequest } = Handlers;
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const withSentry = (handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) => {
@@ -7,11 +9,13 @@ export const withSentry = (handler: (req: NextApiRequest, res: NextApiResponse) 
   return async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       // TODO: Start Transaction
-      // TODO: Extract data from req
       return await handler(req, res); // Call Handler
       // TODO: Finish Transaction
     } catch (e) {
-      captureException(e);
+      withScope(scope => {
+        scope.addEventProcessor(event => parseRequest(event, req));
+        captureException(e);
+      });
       await flush(2000);
       throw e;
     }
