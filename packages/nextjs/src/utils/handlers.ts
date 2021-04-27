525edb436b39ca265ef4796a1d186fe90a27fb02
Daniel Griesser <daniel.griesser.86@gmail.com>
2021-04-27 09:23:28 +0200

    feat: Add flush

--- packages/nextjs/src/utils/handlers.ts
+++ packages/nextjs/src/utils/handlers.ts
@@ -1,4 +1,4 @@
-import { captureException } from '@sentry/node';
+import { captureException, flush } from '@sentry/node';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -6,9 +6,13 @@ export const withSentry = (handler: (req: NextApiRequest, res: NextApiResponse) 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   return async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-      return await handler(req, res);
+      // TODO: Start Transaction
+      // TODO: Extract data from req
+      return await handler(req, res); // Call Handler
+      // TODO: Finish Transaction
     } catch (e) {
       captureException(e);
+      await flush(2000);
     }
   };
 };
