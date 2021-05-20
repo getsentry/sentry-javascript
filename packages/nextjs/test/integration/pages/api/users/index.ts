d48c48ef950be3c1c57ee59c5c58c3a4642da1c1
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-05-20 11:37:58 +0200

    Example app using create-next-app and Sentry docs

---
+++ packages/nextjs/test/integration/pages/api/users/index.ts
@@ -0,0 +1,18 @@
+import { withSentry } from '@sentry/nextjs';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { sampleUserData } from '../../../utils/sample-data';
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  try {
+    if (!Array.isArray(sampleUserData)) {
+      throw new Error('Cannot find user data');
+    }
+
+    res.status(200).json(sampleUserData);
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: (err as Error).message });
+  }
+};
+
+export default withSentry(handler);
