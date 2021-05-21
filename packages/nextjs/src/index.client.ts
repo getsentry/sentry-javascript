29a72d843d045dc5692a4b67c57a3da0883fae40
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-05-21 18:01:40 +0200

    feat(nextjs): Automatic performance monitoring

--- packages/nextjs/src/index.client.ts
+++ packages/nextjs/src/index.client.ts
@@ -1,7 +1,10 @@
 import { configureScope, init as reactInit } from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
+import { nextRouterInstrumentation } from './performance/client';
 import { MetadataBuilder } from './utils/metadataBuilder';
 import { NextjsOptions } from './utils/nextjsOptions';
+import { addIntegration } from './utils/userIntegrations';
 
 export * from '@sentry/react';
 export { nextRouterInstrumentation } from './performance/client';
@@ -11,8 +14,21 @@ export function init(options: NextjsOptions): void {
   const metadataBuilder = new MetadataBuilder(options, ['nextjs', 'react']);
   metadataBuilder.addSdkMetadata();
   options.environment = options.environment || process.env.NODE_ENV;
+  addClientIntegrations(options);
   reactInit(options);
   configureScope(scope => {
     scope.setTag('runtime', 'browser');
   });
+}
+
+const defaultBrowserTracingIntegration = new Integrations.BrowserTracing({
+  routingInstrumentation: nextRouterInstrumentation,
+});
+
+function addClientIntegrations(options: NextjsOptions): void {
+  if (options.integrations) {
+    addIntegration(defaultBrowserTracingIntegration, options.integrations);
+  } else {
+    options.integrations = [defaultBrowserTracingIntegration];
+  }
 }
