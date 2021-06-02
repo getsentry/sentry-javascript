dfd4b635f6924ad10c8d41f6928a2e901d050a72
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-06-02 10:03:52 +0200

    s/undefine/undefined/g

--- packages/nextjs/test/integration/pages/crashed.tsx
+++ packages/nextjs/test/integration/pages/crashed.tsx
@@ -2,7 +2,7 @@ const CrashedPage = (): JSX.Element => {
   // Magic to naively trigger onerror to make session crashed and allow for SSR
   try {
     // @ts-ignore
-    if (typeof window !== 'undefine' && typeof window.onerror === 'function') {
+    if (typeof window !== 'undefined' && typeof window.onerror === 'function') {
       // Lovely oldschool browsers syntax with 5 arguments <3
       // @ts-ignore
       window.onerror(null, null, null, null, new Error('Crashed'));
