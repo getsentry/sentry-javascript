172e478084ec0d61b8f30efdda476e7e33e5237f
Abhijeet Prasad <aprasad@sentry.io>
2021-06-02 11:05:42 -0400

    fix: fix more floating promise eslint issues (#3640)
    For some reason eslint is passing on master, but fails
    after the yarn lock was updating when adding madge. This PR
    cherry-picks a commit fixing the lint errors from the
    abhi/circular-dep branch

--- packages/node/src/backend.ts
+++ packages/node/src/backend.ts
@@ -109,7 +109,7 @@ export class NodeBackend extends BaseBackend<NodeOptions> {
     return new SyncPromise<Event>(resolve => {
       if (this._options.attachStacktrace && hint && hint.syntheticException) {
         const stack = hint.syntheticException ? extractStackFromError(hint.syntheticException) : [];
-        parseStack(stack, this._options)
+        void parseStack(stack, this._options)
           .then(frames => {
             event.stacktrace = {
               frames: prepareFramesForEvent(frames),
