6d2feaa838d2736ddfaa528614f776a013f5c9c1
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-06-22 13:59:20 +0200

    Make it work on Node 6

--- packages/node/test/manual/release-health/runner.js
+++ packages/node/test/manual/release-health/runner.js
@@ -1,57 +1,6 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
-
-(async () => {
-  const scenariosDirs = ['session-aggregates', 'single-session'];
-  const scenarios = [];
-
-  for (const dir of scenariosDirs) {
-    const scenarioDir = path.resolve(__dirname, dir);
-    const filenames = await fs.readdir(scenarioDir);
-    const paths = filenames.map(filename => [filename, path.resolve(scenarioDir, filename)]);
-    scenarios.push(...paths);
-  }
-
-  const codes = await Promise.all(
-    scenarios.map(([filename, filepath]) => {
-      return new Promise(resolve => {
-        const scenarioProcess = spawn('/usr/bin/env', ['node', filepath]);
-        const output = [];
-        const errors = [];
-
-        scenarioProcess.stdout.on('data', data => {
-          output.push(data.toString());
-        });
-
-        scenarioProcess.stderr.on('data', data => {
-          errors.push(data.toString());
-        });
-
-        scenarioProcess.on('exit', code => {
-          if (code === 0) {
-            console.log(colorize(`PASSED: ${filename}`, 'green'));
-          } else {
-            console.log(colorize(`FAILED: ${filename}`, 'red'));
-
-            if (output.length) {
-              console.log(colorize(output.join('\n'), 'yellow'));
-            }
-            if (errors.length) {
-              console.log(colorize(errors.join('\n'), 'yellow'));
-            }
-          }
-
-          resolve(code);
-        });
-      });
-    }),
-  );
-
-  if (codes.some(code => code !== 0)) {
-    process.exit(1);
-  }
-})();
 
 const COLOR_RESET = '\x1b[0m';
 const COLORS = {
@@ -67,3 +16,52 @@ const colorize = (str, color) => {
 
   return `${COLORS[color]}${str}${COLOR_RESET}`;
 };
+
+const scenariosDirs = ['session-aggregates', 'single-session'];
+const scenarios = [];
+
+for (const dir of scenariosDirs) {
+  const scenarioDir = path.resolve(__dirname, dir);
+  const filenames = fs.readdirSync(scenarioDir);
+  const paths = filenames.map(filename => [filename, path.resolve(scenarioDir, filename)]);
+  scenarios.push(...paths);
+}
+
+const processes = scenarios.map(([filename, filepath]) => {
+  return new Promise(resolve => {
+    const scenarioProcess = spawn('/usr/bin/env', ['node', filepath]);
+    const output = [];
+    const errors = [];
+
+    scenarioProcess.stdout.on('data', data => {
+      output.push(data.toString());
+    });
+
+    scenarioProcess.stderr.on('data', data => {
+      errors.push(data.toString());
+    });
+
+    scenarioProcess.on('exit', code => {
+      if (code === 0) {
+        console.log(colorize(`PASSED: ${filename}`, 'green'));
+      } else {
+        console.log(colorize(`FAILED: ${filename}`, 'red'));
+
+        if (output.length) {
+          console.log(colorize(output.join('\n'), 'yellow'));
+        }
+        if (errors.length) {
+          console.log(colorize(errors.join('\n'), 'yellow'));
+        }
+      }
+
+      resolve(code);
+    });
+  });
+});
+
+Promise.all(processes).then(codes => {
+  if (codes.some(code => code !== 0)) {
+    process.exit(1);
+  }
+});
