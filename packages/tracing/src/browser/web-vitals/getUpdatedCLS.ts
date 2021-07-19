fa644311c84e1ae65383782d7466e5783ecef98e
Abhijeet Prasad <aprasad@sentry.io>
2021-07-19 09:24:18 -0400

    fix(web-vitals): Check for valid entry in updatedCLS

--- packages/tracing/src/browser/web-vitals/getUpdatedCLS.ts
+++ packages/tracing/src/browser/web-vitals/getUpdatedCLS.ts
@@ -43,7 +43,8 @@ export const getUpdatedCLS = (onReport: ReportHandler, reportAllChanges?: boolea
 
   const entryHandler = (entry: LayoutShift): void => {
     // Only count layout shifts without recent user input.
-    if (!entry.hadRecentInput) {
+    // TODO: Figure out why entry can be undefined
+    if (entry && !entry.hadRecentInput) {
       const firstSessionEntry = sessionEntries[0];
       const lastSessionEntry = sessionEntries[sessionEntries.length - 1];
 
