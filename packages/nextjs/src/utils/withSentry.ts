ff3817fbc8eb312c6b8967f8f1853fa422b670c9
Katie Byers <lobsterkatie@gmail.com>
2021-08-02 18:38:25 -0700

    remove unnecessary TODO

--- packages/nextjs/src/utils/withSentry.ts
+++ packages/nextjs/src/utils/withSentry.ts
@@ -98,8 +98,6 @@ type WrappedResponseEndMethod = AugmentedResponse['end'];
 
 function wrapEndMethod(origEnd: ResponseEndMethod): WrappedResponseEndMethod {
   return async function newEnd(this: AugmentedResponse, ...args: unknown[]) {
-    // TODO: if the handler errored, it will have popped us out of the domain, so all of our scope data will be missing
-
     const transaction = this.__sentryTransaction;
 
     if (transaction) {
