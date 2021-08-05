a5432cdde6c83deaaecce67da87fb4876e0ba696
Daniel Griesser <daniel.griesser.86@gmail.com>
2021-08-05 15:42:57 +0200

    ref: Still broken typedocs

--- typedoc.js
+++ typedoc.js
@@ -5,12 +5,19 @@ module.exports = {
   includes: './',
   exclude: [
     '**/test/**/*',
+    '**/tests/**/*',
     '**/*.js',
     '**/dist/**/*',
     '**/esm/**/*',
     '**/build/**/*',
+    '**/node_modules/**/*',
     '**/packages/typescript/**/*',
     '**/packages/eslint-*/**/*',
+
+    // These are special cased because I don't know how to resolve type issues
+    '**/packages/react/src/errorboundary.tsx',
+    '**/packages/react/src/profiler.tsx',
+    '**/packages/react/src/reactrouter.tsx',
   ],
   mode: 'modules',
   excludeExternals: true,
