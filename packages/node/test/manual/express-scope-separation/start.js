51275d28dbc3255828750b601be667b469f3cfa5
Onur Temizkan <onur@narval.co.uk>
2021-08-31 12:08:23 +0000

    fix(tests): Fix Express scope separation tests.

--- packages/node/test/manual/express-scope-separation/start.js
+++ packages/node/test/manual/express-scope-separation/start.js
@@ -32,22 +32,13 @@ Sentry.init({
   dsn: 'http://test@example.com/1337',
   transport: DummyTransport,
   beforeSend(event) {
-    if (event.message === 'Error: foo') {
-      assertTags(event.tags, {
-        global: 'wat',
-        foo: 'wat',
-      });
-    } else if (event.message === 'Error: bar') {
-      assertTags(event.tags, {
-        global: 'wat',
-        bar: 'wat',
-      });
-    } else if (event.message === 'Error: baz') {
-      assertTags(event.tags, {
-        global: 'wat',
-        baz: 'wat',
-      });
-    }
+    const errorValue = event.exception?.values?.[0].value;
+
+    assertTags(event.tags, {
+      global: 'wat',
+      [errorValue]: 'wat',
+    });
+
     return event;
   },
 });
