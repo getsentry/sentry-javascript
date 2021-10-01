694170c899f8b93d06346c3e6840df1d5bae9a9c
Katie Byers <lobsterkatie@gmail.com>
2021-09-30 22:19:35 -0700

    implement code review suggestions

--- packages/nextjs/test/integration_test_utils.sh
+++ packages/nextjs/test/integration_test_utils.sh
@@ -1,13 +1,12 @@
 function link_package() {
   local package_abs_path=$1
   local package_name=$2
-  local current_dir=$(pwd)
 
   echo "Setting up @sentry/${package_name} for linking"
-  cd $package_abs_path
+  pushd $package_abs_path
   yarn link
+  popd
 
-  cd $current_dir
   echo "Linking @sentry/$package_name"
   yarn link "@sentry/$package_name"
 
@@ -33,10 +32,9 @@ function linkplugin() {
       # the webpack plugin depends on `@sentry/cli`, so if we're using a linked version of the cli package, the plugin
       # needs to link to it, too
       if [[ $LINK_CLI ]]; then
-        local current_dir=$(pwd)
-        cd $WEBPACK_PLUGIN_REPO
-        link_package $CLI_REPO "cli"
-        cd $current_dir
+        pushd $WEBPACK_PLUGIN_REPO
+        link_cli
+        popd
       fi
     else
       echo "Can't link @sentry/wepack-plugin because WEBPACK_PLUGIN_REPO is not set."
