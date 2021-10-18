6e8c1859a43f3a4a047d146165a6b929b88b61cb
Katie Byers <lobsterkatie@gmail.com>
2021-10-18 15:45:17 -0700

    check for bundle existence before running tests

--- packages/wasm/test/integration.test.js
+++ packages/wasm/test/integration.test.js
@@ -1,5 +1,14 @@
 /* global page, window */
+const fs = require('fs');
+const path = require('path');
+
 const HOST = `http://localhost:${process.env.PORT}`;
+
+if (!fs.existsSync(path.resolve(__dirname, '../../browser/build/bundle.js'))) {
+  throw new Error(
+    'ERROR: No browser bundle found in `packages/browser/build/`. Please run `yarn build` in the browser package before running wasm tests.',
+  );
+}
 
 describe('Wasm', () => {
   it('captured exception should include modified frames and debug_meta attribute', async () => {
