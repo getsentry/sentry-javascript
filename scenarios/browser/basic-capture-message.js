bbe09d886b6330df8c4cf1ba64e8f751c1cb3db8
Abhijeet Prasad <aprasad@sentry.io>
2021-10-28 13:15:14 -0400

    Add capture exception/message

---
+++ scenarios/browser/basic-capture-message.js
@@ -0,0 +1,7 @@
+import { init, captureMessage } from "@sentry/browser";
+
+init({
+  dsn: "https://00000000000000000000000000000000@o000000.ingest.sentry.io/0000000",
+});
+
+captureMessage("this is a message");
