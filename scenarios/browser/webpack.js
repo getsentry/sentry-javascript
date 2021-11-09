e549d9dae8de99f21848d6056d28c09ef99f01e0
Abhijeet Prasad <aprasad@sentry.io>
2021-11-09 10:03:40 -0500

    validate scenario used

--- scenarios/browser/webpack.js
+++ scenarios/browser/webpack.js
@@ -1,16 +1,31 @@
 const path = require('path');
+const { readdirSync } = require('fs')
 
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const CURRENT_SCENARIO = 'basic';
+
+const hasCurrentScenario = () =>
+  readdirSync(__dirname, { withFileTypes: true })
+    .filter(dir => dir.isDirectory())
+    .find(dir => dir.name === CURRENT_SCENARIO)
+
+if (!hasCurrentScenario()) {
+  throw new Error(`Scenario "${CURRENT_SCENARIO}" does not exist`)
+}
+
+// eslint-disable-next-line no-console
+console.log(`Bundling scenario: ${CURRENT_SCENARIO}`)
+
 webpack(
   {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'perf-auto'),
+    mode: 'production',
+    entry: path.resolve(__dirname, CURRENT_SCENARIO),
     output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'dist', 'perf-auto'),
+      path: path.resolve(__dirname, 'dist', CURRENT_SCENARIO),
     },
     plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'static' }), new HtmlWebpackPlugin()],
   },
