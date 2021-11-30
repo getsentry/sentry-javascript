af5530125493fe6fc4879fa23d5ddee0793cc5fc
Abhijeet Prasad <aprasad@sentry.io>
2021-11-30 14:10:23 -0500

    feat(gatsby): Convert gatsby to use buildMetadata

--- packages/gatsby/src/sdk.ts
+++ packages/gatsby/src/sdk.ts
@@ -1,23 +1,15 @@
-import { init as reactInit, SDK_VERSION } from '@sentry/react';
+import { buildMetadata, init as reactInit } from '@sentry/react';
 
 import { getIntegrationsFromOptions } from './utils/integrations';
 import { GatsbyOptions } from './utils/types';
+
+const PACKAGE_NAME = 'gatsby';
 
 /**
  * Inits the Sentry Gatsby SDK.
  */
 export function init(options: GatsbyOptions): void {
-  options._metadata = options._metadata || {};
-  options._metadata.sdk = options._metadata.sdk || {
-    name: 'sentry.javascript.gatsby',
-    packages: [
-      {
-        name: 'npm:@sentry/gatsby',
-        version: SDK_VERSION,
-      },
-    ],
-    version: SDK_VERSION,
-  };
+  buildMetadata(options, PACKAGE_NAME, [PACKAGE_NAME]);
 
   const integrations = getIntegrationsFromOptions(options);
   reactInit({
