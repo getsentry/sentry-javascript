246ea110ee219f96d2d17e858d1aa73083532794
Abhijeet Prasad <aprasad@sentry.io>
2021-12-01 10:54:51 -0500

    woops

--- packages/tracing/test/testutils.ts
+++ packages/tracing/test/testutils.ts
@@ -42,3 +42,17 @@ export function getSymbolObjectKeyByName(obj: Record<string | symbol, any>, desc
 
   return matches[0] || undefined;
 }
+
+export const testOnlyIfNodeVersionAtLeast = (minVersion: number): jest.It => {
+  const currentNodeVersion = process.env.NODE_VERSION;
+
+  try {
+    if (Number(currentNodeVersion?.split('.')[0]) < minVersion) {
+      return it.skip;
+    }
+  } catch (oO) {
+    // we can't tell, so err on the side of running the test
+  }
+
+  return it;
+};
