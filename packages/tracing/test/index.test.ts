39b5a754d426f02a8d76760d9b91f597c3b4e2e5
Abhijeet Prasad <aprasad@sentry.io>
2021-12-01 10:35:46 -0500

    add tests for new integration exports

---
+++ packages/tracing/test/index.test.ts
@@ -0,0 +1,29 @@
+import { getCurrentHub } from '@sentry/hub';
+
+import { BrowserTracing, Integrations } from '../src';
+
+describe('index', () => {
+  describe('Integrations', () => {
+    it('is exported correctly', () => {
+      Object.keys(Integrations).forEach(key => {
+        expect(Integrations[key as keyof typeof Integrations].id).toStrictEqual(expect.any(String));
+      });
+    });
+
+    it('contains BrowserTracing', () => {
+      expect(Integrations.BrowserTracing).toEqual(BrowserTracing);
+    });
+  });
+
+  it('patches the global hub as a side effect', () => {
+    const hub = getCurrentHub();
+    const transaction = hub.startTransaction({ name: 'test', endTimestamp: 123 });
+    expect(transaction).toBeDefined();
+  });
+
+  it('patches the global hub as a side effect', () => {
+    const hub = getCurrentHub();
+    const transaction = hub.startTransaction({ name: 'test', endTimestamp: 123 });
+    expect(transaction).toBeDefined();
+  });
+});
