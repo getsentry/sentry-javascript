f17416648f719d8e0f138341793fce6099a9547a
Katie Byers <katie.byers@sentry.io>
2021-12-07 06:58:32 -0800

    chore(build): Fix missed `es5` to `cjs` conversions in build commands (#4243)
    Follow up to https://github.com/getsentry/sentry-javascript/pull/4178.
    
    This fixes two spots which were missed when converting the build command from `build:es5` to `build:cjs`.

--- packages/nextjs/vercel/install-sentry-from-branch.sh
+++ packages/nextjs/vercel/install-sentry-from-branch.sh
@@ -29,7 +29,7 @@ echo " "
 echo "BUILDING SDK"
 # We need to build es5 versions because `next.config.js` calls `require` on the SDK (to get `withSentryConfig`) and
 # therefore it looks for `dist/index.js`
-yarn build:es5
+yarn build:cjs
 # We need to build esm versions because that's what `next` actually uses when it builds the app
 yarn build:esm
 
