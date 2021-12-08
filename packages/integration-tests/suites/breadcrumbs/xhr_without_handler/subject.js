4beddbdd2eaefefdf1b9aa73b503ab1c031e0f72
Onur Temizkan <onur@narval.co.uk>
2021-12-08 08:03:30 +0000

    Replace tests with a simple fail case.

--- packages/integration-tests/suites/breadcrumbs/xhr_without_handler/subject.js
+++
@@ -1,9 +0,0 @@
-import { waitForXHR } from '../../../utils/browserHelpers.ts';
-
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/base/subjects/example.json');
-xhr.send();
-
-waitForXHR(xhr, function() {
-  Sentry.captureMessage('test');
-});
--- packages/integration-tests/suites/demo/tmp/test.ts
+++ packages/integration-tests/suites/demo/tmp/test.ts
@@ -8,10 +8,5 @@ sentryTest('should record an XMLHttpRequest with a handler', async ({ getLocalTe
 
   const eventData = await getSentryRequest(page, url);
 
-  expect(eventData.message).toBe('test');
-  expect(eventData.breadcrumbs).toHaveLength(1);
-  expect(eventData.breadcrumbs?.[0].category).toBe('xhr');
-  expect(eventData.breadcrumbs?.[0].type).toBe('http');
-  expect(eventData.breadcrumbs?.[0].data?.method).toBe('GET');
-  expect(eventData.breadcrumbs?.[0].data?.url).toBe('/base/subjects/example.json');
+  expect(eventData.message).toBe('2');
 });
