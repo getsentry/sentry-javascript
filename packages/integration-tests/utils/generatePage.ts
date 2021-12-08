96a844c6959aae601adef6269b4f9234ef8d9ff9
Onur Temizkan <onur@narval.co.uk>
2021-12-08 23:47:44 +0000

    Resolve 2nd level @sentry deps locally on webpack

--- packages/integration-tests/utils/generatePage.ts
+++ packages/integration-tests/utils/generatePage.ts
@@ -1,8 +1,34 @@
-import { existsSync, mkdirSync } from 'fs';
+import { Package } from '@sentry/types';
+import { existsSync, mkdirSync, promises } from 'fs';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import { isNotJunk } from 'junk';
+import path from 'path';
 import webpack from 'webpack';
 
 import webpackConfig from '../webpack.config';
+
+const PACKAGE_PATH = '../../packages';
+
+/**
+ * Generate webpack aliases based on packages in monorepo
+ * Example of an alias: '@sentry/serverless': 'path/to/sentry-javascript/packages/serverless',
+ */
+async function generateSentryAlias(): Promise<Record<string, string>> {
+  const dirents = (await promises.readdir(PACKAGE_PATH, { withFileTypes: true }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dir => dir.name);
+
+  return Object.fromEntries(
+    await Promise.all(
+      dirents.map(async d => {
+        const packageJSON: Package = JSON.parse(
+          await promises.readFile(path.resolve(PACKAGE_PATH, d, 'package.json'), { encoding: 'utf-8' }),
+        );
+        return [packageJSON['name'], path.resolve(PACKAGE_PATH, d)];
+      }),
+    ),
+  );
+}
 
 export async function generatePage(
   initialization: string,
@@ -14,8 +40,9 @@ export async function generatePage(
   const initializationPath = `${initialization}`;
   const subjectPath = `${subject}`;
   const templatePath = `${template}`;
-
   const bundlePath = `${localPath}/index.html`;
+
+  const alias = await generateSentryAlias();
 
   if (!existsSync(localPath)) {
     mkdirSync(localPath, { recursive: true });
@@ -25,6 +52,9 @@ export async function generatePage(
     await new Promise<void>((resolve, reject) => {
       const compiler = webpack(
         webpackConfig({
+          resolve: {
+            alias,
+          },
           entry: {
             initialization: initializationPath,
             subject: subjectPath,
