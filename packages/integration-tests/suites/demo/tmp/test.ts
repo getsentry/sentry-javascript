d4d82721e31576b83050497d94e2abdc2fc89ed6
Onur Temizkan <onur@narval.co.uk>
2021-12-08 23:48:26 +0000

    Fix sample test title.

--- packages/integration-tests/suites/demo/tmp/test.ts
+++ packages/integration-tests/suites/demo/tmp/test.ts
@@ -3,7 +3,7 @@ import { expect } from '@playwright/test';
 import { sentryTest } from '../../../utils/fixtures';
 import { getSentryRequest } from '../../../utils/helpers';
 
-sentryTest('should record an XMLHttpRequest with a handler', async ({ getLocalTestPath, page }) => {
+sentryTest('should fail', async ({ getLocalTestPath, page }) => {
   const url = await getLocalTestPath({ testDir: __dirname });
 
   const eventData = await getSentryRequest(page, url);
