e3f9b5d30e7b9856ecf93ebc1c0b542496a3eb6f
Armin Ronacher <armin.ronacher@active-4.com>
2021-12-15 11:31:40 +0100

    fix: remove presumed incorrect handleEvent test

--- packages/browser/test/unit/integrations/helpers.test.ts
+++ packages/browser/test/unit/integrations/helpers.test.ts
@@ -73,9 +73,6 @@ describe('internal wrap()', () => {
     expect(fn).toHaveProperty('__sentry_wrapped__');
     expect(fn.__sentry_wrapped__).toBe(wrapped);
 
-    expect(wrapped).toHaveProperty('__sentry__');
-    expect(wrapped.__sentry__).toBe(true);
-
     expect(wrapped).toHaveProperty('__sentry_original__');
     expect(wrapped.__sentry_original__).toBe(fn);
   });
@@ -118,27 +115,14 @@ describe('internal wrap()', () => {
     expect(fnArgB).toHaveProperty('__sentry_wrapped__');
   });
 
-  it('calls either `handleEvent` property if it exists or the original function', () => {
-    interface SinonEventSpy extends SinonSpy {
-      handleEvent: SinonSpy;
-    }
-
+  it('calls the original function', () => {
     const fn = spy();
-    const eventFn = spy() as SinonEventSpy;
-    eventFn.handleEvent = spy();
 
     wrap(fn)(123, 'Rick');
-    wrap(eventFn)(123, 'Morty');
 
     expect(fn.called).toBe(true);
     expect(fn.getCalls()[0].args[0]).toBe(123);
     expect(fn.getCalls()[0].args[1]).toBe('Rick');
-
-    expect(eventFn.handleEvent.called).toBe(true);
-    expect(eventFn.handleEvent.getCalls()[0].args[0]).toBe(123);
-    expect(eventFn.handleEvent.getCalls()[0].args[1]).toBe('Morty');
-
-    expect(eventFn.called).toBe(false);
   });
 
   it('preserves `this` context for all the calls', () => {
@@ -182,14 +166,11 @@ describe('internal wrap()', () => {
     const wrapped = wrap(fn);
 
     // Shouldn't show up in iteration
-    expect(Object.keys(fn)).toEqual(expect.not.arrayContaining(['__sentry__']));
     expect(Object.keys(fn)).toEqual(expect.not.arrayContaining(['__sentry_original__']));
     expect(Object.keys(fn)).toEqual(expect.not.arrayContaining(['__sentry_wrapped__']));
-    expect(Object.keys(wrapped)).toEqual(expect.not.arrayContaining(['__sentry__']));
     expect(Object.keys(wrapped)).toEqual(expect.not.arrayContaining(['__sentry_original__']));
     expect(Object.keys(wrapped)).toEqual(expect.not.arrayContaining(['__sentry_wrapped__']));
     // But should be accessible directly
-    expect(wrapped.__sentry__).toBe(true);
     expect(wrapped.__sentry_original__).toBe(fn);
     expect(fn.__sentry_wrapped__).toBe(wrapped);
   });
