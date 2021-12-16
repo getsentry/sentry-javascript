370458404ac2973c01965657e806ed2acdbab900
JonasBa <jonas@badalic.com>
2021-12-16 14:37:06 +0100

    fix(types): drop transactionsamplingmethod

--- packages/types/src/transactionsamplingmethod.ts
+++
@@ -1,9 +0,0 @@
-/** JSDoc
- * @deprecated Use string literals - if you require type casting, cast to TransactionSamplingMethod type
- */
-export enum TransactionSamplingMethod {
-  Explicit = 'explicitly_set',
-  Sampler = 'client_sampler',
-  Rate = 'client_rate',
-  Inheritance = 'inheritance',
-}
