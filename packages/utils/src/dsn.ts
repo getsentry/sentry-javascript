f85305a0abcfbb0f209e13a106a18e8a3213a5ea
JonasBa <jonas@badalic.com>
2021-12-17 07:23:43 +0100

    ref(utils): dsn

--- packages/utils/src/dsn.ts
+++ packages/utils/src/dsn.ts
@@ -9,6 +9,110 @@ const DSN_REGEX = /^(?:(\w+):)\/\/(?:(\w+)(?::(\w+))?@)([\w.-]+)(?::(\d+))?\/(.+
 /** Error message */
 const ERROR_MESSAGE = 'Invalid Dsn';
 
+function isValidProtocol(protocol?: string): protocol is DsnProtocol {
+  return protocol === 'http' || protocol === 'https';
+}
+
+/**
+ * Renders the string representation of this Dsn.
+ *
+ * By default, this will render the public representation without the password
+ * component. To get the deprecated private representation, set `withPassword`
+ * to true.
+ *
+ * @param withPassword When set to true, the password will be included.
+ */
+function dsntoString(dsn: Dsn, withPassword: boolean = false): string {
+  const { host, path, pass, port, projectId, protocol, publicKey } = dsn;
+  return (
+    `${protocol}://${publicKey}${withPassword && pass ? `:${pass}` : ''}` +
+    `@${host}${port ? `:${port}` : ''}/${path ? `${path}/` : path}${projectId}`
+  );
+}
+
+function dsnFromString(str: string): Dsn {
+  const match = DSN_REGEX.exec(str);
+
+  if (!match) {
+    throw new SentryError(ERROR_MESSAGE);
+  }
+
+  const [protocol, publicKey, pass = '', host, port = '', lastPath] = match.slice(1);
+  let path = '';
+  let projectId = lastPath;
+
+  const split = projectId.split('/');
+  if (split.length > 1) {
+    path = split.slice(0, -1).join('/');
+    projectId = split.pop() as string;
+  }
+
+  if (projectId) {
+    const projectMatch = projectId.match(/^\d+/);
+    if (projectMatch) {
+      projectId = projectMatch[0];
+    }
+  }
+
+  if (isValidProtocol(protocol)) {
+    return dsnFromComponents({ host, pass, path, projectId, port, protocol: protocol, publicKey });
+  }
+
+  throw new SentryError(ERROR_MESSAGE);
+}
+
+function dsnFromComponents(components: DsnComponents): Dsn {
+  // TODO this is for backwards compatibility, and can be removed in a future version
+  if ('user' in components && !('publicKey' in components)) {
+    components.publicKey = components.user;
+  }
+
+  return {
+    user: components.publicKey || '',
+    protocol: components.protocol,
+    publicKey: components.publicKey || '',
+    pass: components.pass || '',
+    host: components.host,
+    port: components.port || '',
+    path: components.path || '',
+    projectId: components.projectId,
+  };
+}
+
+function validateDsn(dsn: Dsn): boolean {
+  if (isDebugBuild()) {
+    const { port, projectId, protocol } = dsn;
+
+    ['protocol', 'publicKey', 'host', 'projectId'].forEach(component => {
+      if (!dsn[component]) {
+        throw new SentryError(`${ERROR_MESSAGE}: ${component} missing`);
+      }
+    });
+
+    if (!projectId.match(/^\d+$/)) {
+      throw new SentryError(`${ERROR_MESSAGE}: Invalid projectId ${projectId}`);
+    }
+
+    if (isValidProtocol(protocol)) {
+      throw new SentryError(`${ERROR_MESSAGE}: Invalid protocol ${protocol}`);
+    }
+
+    if (port && isNaN(parseInt(port, 10))) {
+      throw new SentryError(`${ERROR_MESSAGE}: Invalid port ${port}`);
+    }
+  }
+
+  return true;
+}
+
+function makeDsn(from: DsnLike): Dsn {
+  let dsn = typeof from === 'string' ? dsnFromString(from) : dsnFromComponents(from);
+
+  return {
+    ...dsn,
+    toString: () => dsntoString(dsn),
+  };
+}
 /** The Sentry Dsn, identifying a Sentry instance and project. */
 export class Dsn implements DsnComponents {
   /** Protocol used to connect to Sentry. */
@@ -29,99 +133,5 @@ export class Dsn implements DsnComponents {
   public projectId!: string;
 
   /** Creates a new Dsn component */
-  public constructor(from: DsnLike) {
-    if (typeof from === 'string') {
-      this._fromString(from);
-    } else {
-      this._fromComponents(from);
-    }
-
-    this._validate();
-  }
-
-  /**
-   * Renders the string representation of this Dsn.
-   *
-   * By default, this will render the public representation without the password
-   * component. To get the deprecated private representation, set `withPassword`
-   * to true.
-   *
-   * @param withPassword When set to true, the password will be included.
-   */
-  public toString(withPassword: boolean = false): string {
-    const { host, path, pass, port, projectId, protocol, publicKey } = this;
-    return (
-      `${protocol}://${publicKey}${withPassword && pass ? `:${pass}` : ''}` +
-      `@${host}${port ? `:${port}` : ''}/${path ? `${path}/` : path}${projectId}`
-    );
-  }
-
-  /** Parses a string into this Dsn. */
-  private _fromString(str: string): void {
-    const match = DSN_REGEX.exec(str);
-
-    if (!match) {
-      throw new SentryError(ERROR_MESSAGE);
-    }
-
-    const [protocol, publicKey, pass = '', host, port = '', lastPath] = match.slice(1);
-    let path = '';
-    let projectId = lastPath;
-
-    const split = projectId.split('/');
-    if (split.length > 1) {
-      path = split.slice(0, -1).join('/');
-      projectId = split.pop() as string;
-    }
-
-    if (projectId) {
-      const projectMatch = projectId.match(/^\d+/);
-      if (projectMatch) {
-        projectId = projectMatch[0];
-      }
-    }
-
-    this._fromComponents({ host, pass, path, projectId, port, protocol: protocol as DsnProtocol, publicKey });
-  }
-
-  /** Maps Dsn components into this instance. */
-  private _fromComponents(components: DsnComponents): void {
-    // TODO this is for backwards compatibility, and can be removed in a future version
-    if ('user' in components && !('publicKey' in components)) {
-      components.publicKey = components.user;
-    }
-    this.user = components.publicKey || '';
-
-    this.protocol = components.protocol;
-    this.publicKey = components.publicKey || '';
-    this.pass = components.pass || '';
-    this.host = components.host;
-    this.port = components.port || '';
-    this.path = components.path || '';
-    this.projectId = components.projectId;
-  }
-
-  /** Validates this Dsn and throws on error. */
-  private _validate(): void {
-    // we only validate in debug mode.  This will fail later anyways.
-    if (isDebugBuild()) {
-      ['protocol', 'publicKey', 'host', 'projectId'].forEach(component => {
-        if (!this[component as keyof DsnComponents]) {
-          throw new SentryError(`${ERROR_MESSAGE}: ${component} missing`);
-        }
-      });
-
-      if (!this.projectId.match(/^\d+$/)) {
-        throw new SentryError(`${ERROR_MESSAGE}: Invalid projectId ${this.projectId}`);
-      }
-
-      if (this.protocol !== 'http' && this.protocol !== 'https') {
-        throw new SentryError(`${ERROR_MESSAGE}: Invalid protocol ${this.protocol}`);
-      }
-
-      if (this.port && isNaN(parseInt(this.port, 10))) {
-        throw new SentryError(`${ERROR_MESSAGE}: Invalid port ${this.port}`);
-      }
-    }
-  }
+  public constructor(from: DsnLike) {}
 }
