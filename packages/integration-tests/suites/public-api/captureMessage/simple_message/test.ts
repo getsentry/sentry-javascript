01161b025308b5694aa10222219b57e9d439e645
Onur Temizkan <onur@narval.co.uk>
2021-12-22 22:10:52 +0000

    test(browser): Add integration tests for `captureMessage` (#4351)

---
+++ packages/integration-tests/suites/public-api/captureMessage/simple_message/test.ts
@@ -0,0 +1,13 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getSentryRequest } from '../../../../utils/helpers';
+
+sentryTest('should capture a simple message string', async ({ getLocalTestPath, page }) => {
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const eventData = await getSentryRequest(page, url);
+
+  expect(eventData.message).toBe('foo');
+  expect(eventData.level).toBe('info');
+});
