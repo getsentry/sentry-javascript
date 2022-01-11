321a3f6a5bb47e3c64d082643588397ae19da6c3
Onur Temizkan <onur@narval.co.uk>
2022-01-11 20:41:05 +0000

    test(tracing): Add tests for `pageload` spans.

---
+++ packages/integration-tests/suites/tracing/metrics/pageload-browser-spans/test.ts
@@ -0,0 +1,23 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getSentryTransactionRequest } from '../../../../utils/helpers';
+
+sentryTest('should add browser-related spans to pageload transaction', async ({ getLocalTestPath, page }) => {
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const eventData = await getSentryTransactionRequest(page, url);
+  const browserSpans = eventData.spans?.filter(({ op }) => op === 'browser');
+
+  // Spans `connect`, `cache` and `DNS` are not always inside `pageload` transaction.
+  expect(browserSpans?.length).toBeGreaterThanOrEqual(4);
+
+  ['domContentLoadedEvent', 'loadEvent', 'request', 'response'].forEach(eventDesc =>
+    expect(browserSpans).toContainEqual(
+      expect.objectContaining({
+        description: eventDesc,
+        parent_span_id: eventData.contexts?.trace.span_id,
+      }),
+    ),
+  );
+});
---
+++ packages/integration-tests/suites/tracing/metrics/pageload-resource-spans/test.ts
@@ -0,0 +1,27 @@
+import { expect, Route } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getSentryTransactionRequest } from '../../../../utils/helpers';
+
+sentryTest('should add resource spans to pageload transaction', async ({ getLocalTestPath, page }) => {
+  // Intercepting asset requests to avoid network-related flakiness and random retries (on Firefox).
+  await page.route('**/path/to/image.svg', (route: Route) => route.fulfill({ path: `${__dirname}/assets/image.svg` }));
+  await page.route('**/path/to/script.js', (route: Route) => route.fulfill({ path: `${__dirname}/assets/script.js` }));
+  await page.route('**/path/to/style.css', (route: Route) => route.fulfill({ path: `${__dirname}/assets/style.css` }));
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const eventData = await getSentryTransactionRequest(page, url);
+  const resourceSpans = eventData.spans?.filter(({ op }) => op?.startsWith('resource'));
+
+  expect(resourceSpans?.length).toBe(3);
+
+  ['resource.img', 'resource.script', 'resource.link'].forEach(op =>
+    expect(resourceSpans).toContainEqual(
+      expect.objectContaining({
+        op: op,
+        parent_span_id: eventData.contexts?.trace.span_id,
+      }),
+    ),
+  );
+});
