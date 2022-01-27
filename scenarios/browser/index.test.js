0693bd4d7a659e6661355cf845a352593de3ce17
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-01-27 10:42:26 +0100

    chore: print bundle size per scenario

--- scenarios/browser/index.test.js
+++ scenarios/browser/index.test.js
@@ -1,4 +1,4 @@
-const { promises } = require('fs');
+const { promises, statSync } = require('fs');
 const path = require('path');
 
 const runWebpack = require('./webpack');
@@ -8,13 +8,28 @@ function expectNotToExist(module, consumedModules) {
   expect(matching).not.toContain(module);
 }
 
+function getPathByScenario(scenario) {
+  return path.resolve(__dirname, 'dist', scenario, 'main.js');
+}
+
 async function getBundleAsString(scenario) {
-  const bundle = await promises.readFile(path.resolve(__dirname, 'dist', scenario, 'main.js'));
+  const pathname = getPathByScenario(scenario);
+  const bundle = await promises.readFile(pathname);
+
   return bundle.toString();
 }
 
+function getBundleSize(scenario) {
+  const bundleStats = statSync(getPathByScenario(scenario));
+  const rounded = (bundleStats.size / 1024).toFixed(2);
+  return `${rounded} Kb`;
+}
+
 // Example case ["scenario", ["list", "of", "unwelcome", "modules"], ["list", "of", "unwelcome", "methods"]]
-const cases = [['basic', ['minimal'], ['captureException']]];
+const cases = [
+  ['basic', ['minimal'], ['captureException']],
+  ['basic-capture-exception', ['minimal'], ['captureException']],
+];
 
 describe.each(cases)('scenario: "%s"', (scenario, unwelcomeModules, unwelcomeMethods) => {
   let consumedModules;
@@ -24,6 +39,9 @@ describe.each(cases)('scenario: "%s"', (scenario, unwelcomeModules, unwelcomeMet
     consumedModules = await runWebpack(scenario);
     bundleAsAString = await getBundleAsString(scenario);
   });
+
+  // Dummy test. Used to pretty print the bundle size per scenario.
+  test(`Bundle size: ${getBundleSize(scenario)}`, () => {});
 
   test.each(unwelcomeModules)(`module: "%s" should not be present`, async unwelcomeModule => {
     expectNotToExist(unwelcomeModule, consumedModules);
