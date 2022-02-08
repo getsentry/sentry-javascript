9ba1e0f7a5eee8b648e99ffffa93306bdc8423ad
Abhijeet Prasad <aprasad@sentry.io>
2022-02-08 17:52:47 -0500

    update gzip -> gzip + minified

--- .size-limit.js
+++ .size-limit.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    name: '@sentry/browser - CDN Bundle (gzipped)',
+    name: '@sentry/browser - CDN Bundle (gzipped + minified)',
     path: 'packages/browser/build/bundle.min.js',
     gzip: true,
     limit: '100 KB',
@@ -12,7 +12,7 @@ module.exports = [
     limit: '120 KB',
   },
   {
-    name: '@sentry/browser - Webpack (gzipped)',
+    name: '@sentry/browser - Webpack (gzipped + minified)',
     path: 'packages/browser/esm/index.js',
     import: '{ init }',
     gzip: true,
@@ -26,27 +26,27 @@ module.exports = [
     limit: '100 KB',
   },
   {
-    name: '@sentry/react - Webpack (gzipped)',
+    name: '@sentry/react - Webpack (gzipped + minified)',
     path: 'packages/react/esm/index.js',
     import: '{ init }',
     gzip: true,
     limit: '100 KB',
   },
   {
-    name: '@sentry/nextjs Client - Webpack (gzipped)',
+    name: '@sentry/nextjs Client - Webpack (gzipped + minified)',
     path: 'packages/nextjs/esm/index.client.js',
     import: '{ init }',
     gzip: true,
     limit: '100 KB',
   },
   {
-    name: '@sentry/browser + @sentry/tracing - CDN Bundle (gzipped)',
+    name: '@sentry/browser + @sentry/tracing - CDN Bundle (gzipped + minified)',
     path: 'packages/tracing/build/bundle.tracing.min.js',
     gzip: true,
     limit: '100 KB',
   },
   {
-    name: '@sentry/browser - ES6 CDN Bundle (gzipped)',
+    name: '@sentry/browser - ES6 CDN Bundle (gzipped + minified)',
     path: 'packages/browser/build/bundle.es6.min.js',
     gzip: true,
     limit: '100 KB',
