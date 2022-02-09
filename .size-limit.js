4bf257f05dd1e80c24c13830110c571b11f5245d
Abhijeet Prasad <aprasad@sentry.io>
2022-02-08 20:25:09 -0500

    label es5 and reorganize imports

--- .size-limit.js
+++ .size-limit.js
@@ -1,13 +1,25 @@
 module.exports = [
   {
-    name: '@sentry/browser - CDN Bundle (gzipped + minified)',
+    name: '@sentry/browser - ES5 CDN Bundle (gzipped + minified)',
     path: 'packages/browser/build/bundle.min.js',
     gzip: true,
     limit: '100 KB',
   },
   {
-    name: '@sentry/browser - CDN Bundle (minified)',
+    name: '@sentry/browser - ES5 CDN Bundle (minified)',
     path: 'packages/browser/build/bundle.min.js',
+    gzip: false,
+    limit: '120 KB',
+  },
+  {
+    name: '@sentry/browser - ES6 CDN Bundle (gzipped + minified)',
+    path: 'packages/browser/build/bundle.es6.min.js',
+    gzip: true,
+    limit: '100 KB',
+  },
+  {
+    name: '@sentry/browser - ES6 CDN Bundle (minified)',
+    path: 'packages/browser/build/bundle.es6.min.js',
     gzip: false,
     limit: '120 KB',
   },
@@ -33,6 +45,13 @@ module.exports = [
     limit: '100 KB',
   },
   {
+    name: '@sentry/react - Webpack (gzipped + minified)',
+    path: 'packages/react/esm/index.js',
+    import: '{ init }',
+    gzip: true,
+    limit: '100 KB',
+  },
+  {
     name: '@sentry/nextjs Client - Webpack (gzipped + minified)',
     path: 'packages/nextjs/esm/index.client.js',
     import: '{ init }',
@@ -40,21 +59,9 @@ module.exports = [
     limit: '100 KB',
   },
   {
-    name: '@sentry/browser + @sentry/tracing - CDN Bundle (gzipped + minified)',
+    name: '@sentry/browser + @sentry/tracing - ES5 CDN Bundle (gzipped + minified)',
     path: 'packages/tracing/build/bundle.tracing.min.js',
     gzip: true,
     limit: '100 KB',
-  },
-  {
-    name: '@sentry/browser - ES6 CDN Bundle (gzipped + minified)',
-    path: 'packages/browser/build/bundle.es6.min.js',
-    gzip: true,
-    limit: '100 KB',
-  },
-  {
-    name: '@sentry/browser - ES6 CDN Bundle (minified)',
-    path: 'packages/browser/build/bundle.es6.min.js',
-    gzip: false,
-    limit: '120 KB',
   },
 ];
