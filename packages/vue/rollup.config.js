2fc7ecef677d4fc3ed037cb87801a64b5d17eea2
Katie Byers <lobsterkatie@gmail.com>
2022-02-15 10:43:58 -0800

    update node-resolve plugin

--- packages/vue/rollup.config.js
+++ packages/vue/rollup.config.js
@@ -1,7 +1,7 @@
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import license from 'rollup-plugin-license';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 
