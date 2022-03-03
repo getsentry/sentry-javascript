48a454e0765d77698dd8fd2d26f00b0c7811ec08
Katie Byers <lobsterkatie@gmail.com>
2022-03-02 21:28:42 -0800

    remove unused exports from main rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -15,15 +15,6 @@ import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
-export const paths = {
-  '@sentry/browser': ['../browser/src'],
-  '@sentry/core': ['../core/src'],
-  '@sentry/hub': ['../hub/src'],
-  '@sentry/minimal': ['../minimal/src'],
-  '@sentry/types': ['../types/src'],
-  '@sentry/utils': ['../utils/src'],
-};
-
 /**
  * Create a plugin to add an identification banner to the top of stand-alone bundles.
  *
@@ -64,94 +55,61 @@ export const terserPlugin = terser({
   },
 });
 
-export const markAsBrowserBuild = replace({
-  // don't replace `__placeholder__` where it's followed immediately by a single `=` (to prevent ending up
-  // with something of the form `let "replacementValue" = "some assigned value"`, which would cause a
-  // syntax error)
-  preventAssignment: true,
-  // the replacement to make
-  values: {
-    __SENTRY_BROWSER_BUNDLE__: true,
-  },
-});
-
-const baseTSPluginOptions = {
-  tsconfig: 'tsconfig.esm.json',
-  tsconfigOverride: {
-    compilerOptions: {
-      declaration: false,
-      declarationMap: false,
-      paths,
-      baseUrl: '.',
-    },
-  },
-  include: ['*.ts+(|x)', '**/*.ts+(|x)', '../**/*.ts+(|x)'],
-};
-
-export const typescriptPluginES5 = typescript(
-  deepMerge(baseTSPluginOptions, {
-    tsconfigOverride: {
-      compilerOptions: {
-        target: 'es5',
-      },
-    },
-  }),
-);
-
-export const typescriptPluginES6 = typescript(
-  deepMerge(baseTSPluginOptions, {
-    tsconfigOverride: {
-      compilerOptions: {
-        target: 'es6',
-      },
-    },
-  }),
-);
-
-export const nodeResolvePlugin = resolve();
-
-export const baseBundleConfig = {
-  output: {
-    sourcemap: true,
-    strict: false,
-    esModule: false,
-  },
-  treeshake: 'smallest',
-};
-
-export const addOnBundleConfig = {
-  // These output settings are designed to mimic an IIFE. We don't use Rollup's `iife` format because we don't want to
-  // attach this code to a new global variable, but rather inject it into the existing SDK's `Integrations` object.
-  output: {
-    format: 'cjs',
-
-    // code to add before the CJS wrapper
-    banner: '(function (__window) {',
-
-    // code to add just inside the CJS wrapper, before any of the wrapped code
-    intro: 'var exports = {};',
-
-    // code to add after all of the wrapped code, but still inside the CJS wrapper
-    outro: () =>
-      [
-        '',
-        "  // Add this module's exports to the global `Sentry.Integrations`",
-        '  __window.Sentry = __window.Sentry || {};',
-        '  __window.Sentry.Integrations = __window.Sentry.Integrations || {};',
-        '  for (var key in exports) {',
-        '    if (Object.prototype.hasOwnProperty.call(exports, key)) {',
-        '      __window.Sentry.Integrations[key] = exports[key];',
-        '    }',
-        '  }',
-      ].join('\n'),
-
-    // code to add after the CJS wrapper
-    footer: '}(window));',
-  },
-};
-
 export function makeBaseBundleConfig(options) {
   const { input, isAddOn, jsVersion, outputFileBase } = options;
+
+  const baseTSPluginOptions = {
+    tsconfig: 'tsconfig.esm.json',
+    tsconfigOverride: {
+      compilerOptions: {
+        declaration: false,
+        declarationMap: false,
+        paths: {
+          '@sentry/browser': ['../browser/src'],
+          '@sentry/core': ['../core/src'],
+          '@sentry/hub': ['../hub/src'],
+          '@sentry/minimal': ['../minimal/src'],
+          '@sentry/types': ['../types/src'],
+          '@sentry/utils': ['../utils/src'],
+        },
+        baseUrl: '.',
+      },
+    },
+    include: ['*.ts+(|x)', '**/*.ts+(|x)', '../**/*.ts+(|x)'],
+  };
+
+  const typescriptPluginES5 = typescript(
+    deepMerge(baseTSPluginOptions, {
+      tsconfigOverride: {
+        compilerOptions: {
+          target: 'es5',
+        },
+      },
+    }),
+  );
+
+  const typescriptPluginES6 = typescript(
+    deepMerge(baseTSPluginOptions, {
+      tsconfigOverride: {
+        compilerOptions: {
+          target: 'es6',
+        },
+      },
+    }),
+  );
+
+  const nodeResolvePlugin = resolve();
+
+  const markAsBrowserBuild = replace({
+    // don't replace `__placeholder__` where it's followed immediately by a single `=` (to prevent ending up
+    // with something of the form `let "replacementValue" = "some assigned value"`, which would cause a
+    // syntax error)
+    preventAssignment: true,
+    // the replacement to make
+    values: {
+      __SENTRY_BROWSER_BUNDLE__: true,
+    },
+  });
 
   const standAloneBundleConfig = {
     output: {
