2d442db47df5c734681e2ad9e1ae88a4f54c20e9
Katie Byers <lobsterkatie@gmail.com>
2022-03-14 19:21:59 -0700

    use both ES5 and ES6 as values for js version env var

--- packages/integrations/scripts/buildBundles.sh
+++ packages/integrations/scripts/buildBundles.sh
@@ -1,20 +1,25 @@
 for filepath in ./src/*; do
-  file=$(basename $filepath)
+  for js_version in "ES5" "ES6"; do
 
-  # the index file is only there for the purposes of npm builds - for the CDN we create a separate bundle for each
-  # integration - so we can skip it here
-  if [[ $file == "index.ts" ]]; then
-    continue
-  fi
+    file=$(basename $filepath)
 
-  # run the build for each integration, pushing each build process into the background once it starts (that's what the
-  # trailing `&` does) so that we can start another one
-  echo -e "\nBuilding bundles for \`$file\`..."
-  INTEGRATION_FILE=$file yarn --silent rollup -c rollup.config.js 2>/dev/null && echo -e "\nFinished building bundles for \`$file\`." &
+    # the index file is only there for the purposes of npm builds - for the CDN we create a separate bundle for each
+    # integration - so we can skip it here
+    if [[ $file == "index.ts" ]]; then
+      continue
+    fi
 
+    # run the build for each integration, pushing each build process into the background once it starts (that's what the
+    # trailing `&` does) so that we can start another one
+    echo -e "Building $js_version bundles for \`$file\`..."
+    INTEGRATION_FILE=$file JS_VERSION=$js_version \
+      yarn --silent rollup -c rollup.config.js &&
+      echo -e "Finished building $js_version bundles for \`$file\`." &
+
+  done
 done
 
 # keep the process running until all backgrounded tasks have finished
 wait
 
-echo "Integration bundles built successfully"
+echo -e "\nIntegration bundles built successfully"
