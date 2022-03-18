54f658efefd577663cccc9d7075a114764b2d69c
Onur Temizkan <onur@narval.co.uk>
2022-03-18 21:38:35 +0000

    Remove stale babel and jest configurations.

--- packages/node-integration-tests/babel.config.js
+++
@@ -1,8 +0,0 @@
-// Jest still doesn't support ESM modules natively.
-module.exports = {
-  env: {
-    test: {
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
-};
--- packages/node-integration-tests/jest.config.js
+++ packages/node-integration-tests/jest.config.js
@@ -1,18 +1,9 @@
-const path = require('path');
-
-const sentryNodePath = path.join(path.dirname(__dirname), 'node');
-const sentryTracingPath = path.join(path.dirname(__dirname), 'tracing');
-
 const config = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testEnvironment: 'node',
   testMatch: ['**/test.ts'],
-  moduleNameMapper: {
-    '@sentry/node': sentryNodePath,
-    '@sentry/tracing': sentryTracingPath,
-  },
   moduleFileExtensions: ['js', 'ts'],
 };
 
