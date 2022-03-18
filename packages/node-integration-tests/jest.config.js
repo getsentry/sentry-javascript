d53e4c60048aa0c541c3cbcd7b52b5059abd22f3
Onur Temizkan <onur@narval.co.uk>
2022-03-18 15:22:48 +0000

    Use, TS source of packages instead of builds.

--- packages/node-integration-tests/jest.config.js
+++ packages/node-integration-tests/jest.config.js
@@ -1,16 +1,11 @@
 const path = require('path');
-const process = require('process');
 
-const useBuild = process.env['SENTRY_NODE_BUILD'];
-const useESM = useBuild === 'esm';
-
-const sentryNodePath = path.join(path.dirname(__dirname), 'node', useESM ? 'esm' : 'dist');
-const sentryTracingPath = path.join(path.dirname(__dirname), 'tracing', useESM ? 'esm' : 'dist');
+const sentryNodePath = path.join(path.dirname(__dirname), 'node');
+const sentryTracingPath = path.join(path.dirname(__dirname), 'tracing');
 
 const config = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
   },
   testEnvironment: 'node',
   testMatch: ['**/test.ts'],
