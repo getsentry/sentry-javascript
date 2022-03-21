76acf2fae5cff3e77546751b88aa6b5a292676a6
Abhijeet Prasad <aprasad@sentry.io>
2022-03-21 17:02:28 +0100

    feat(core): Introduce New Transports API (#4716)
    This PR introduces a functional transport based on what was discussed in https://github.com/getsentry/sentry-javascript/issues/4660.
    
    The transport is defined by an interface, which sets two basic functions, `send` and `flush`, which both interact directly with an internal `PromiseBuffer` data structure, a queue of requests that represents the data that needs to be sent to Sentry.
    
    ```ts
    interface NewTransport {
      // If `$` is set, we know that this is a new transport.
      // TODO(v7): Remove this as we will no longer have split between
      // old and new transports.
      $: boolean;
      send(request: Envelope, category: TransportCategory): PromiseLike<TransportResponse>;
      flush(timeout: number): PromiseLike<boolean>;
    }
    ```
    
    `send` relies on an externally defined `makeRequest` function (that is passed into a `makeTransport` constructor) to make a request, and then return a status based on it. It also updates internal rate-limits according to the response from `makeRequest`. The status will be also used by the client in the future for client outcomes (we will extract client outcomes from the transport -> client because it living in the transport is not the best pattern).
    
    `send` takes in an envelope, which means that for now, no errors will go through this transport when we update the client to use this new transport.
    
    `flush`, flushes the promise buffer, pretty much how it worked before.
    
    To make a custom transport (and how we'll be making fetch, xhr transports), you essentially define a `makeRequest` function.
    
    ```ts
    function createFetchTransport(options: FetchTransportOptions): NewTransport {
      function makeRequest(request: TransportRequest): PromiseLike<TransportMakeRequestResponse> {
        return fetch(options.url, options.fetchOptions).then(response => {
          return response.text().then(body => ({
            body,
            headers: {
              'x-sentry-rate-limits': response.headers.get('X-Sentry-Rate-Limits'),
              'retry-after': response.headers.get('Retry-After'),
            },
            reason: response.statusText,
            statusCode: response.status,
          }));
        });
      }
    
      return createTransport({ bufferSize: options.bufferSize }, makeRequest);
    }
    ```

---
+++ packages/core/test/lib/transports/base.test.ts
@@ -0,0 +1,372 @@
+import { EventEnvelope, EventItem } from '@sentry/types';
+import { createEnvelope, PromiseBuffer, resolvedSyncPromise, serializeEnvelope } from '@sentry/utils';
+
+import {
+  createTransport,
+  ERROR_TRANSPORT_CATEGORY,
+  NewTransport,
+  TRANSACTION_TRANSPORT_CATEGORY,
+  TransportMakeRequestResponse,
+  TransportResponse,
+} from '../../../src/transports/base';
+
+const ERROR_ENVELOPE = createEnvelope<EventEnvelope>({ event_id: 'aa3ff046696b4bc6b609ce6d28fde9e2', sent_at: '123' }, [
+  [{ type: 'event' }, { event_id: 'aa3ff046696b4bc6b609ce6d28fde9e2' }] as EventItem,
+]);
+
+const TRANSACTION_ENVELOPE = createEnvelope<EventEnvelope>(
+  { event_id: 'aa3ff046696b4bc6b609ce6d28fde9e2', sent_at: '123' },
+  [[{ type: 'transaction' }, { event_id: 'aa3ff046696b4bc6b609ce6d28fde9e2' }] as EventItem],
+);
+
+describe('createTransport', () => {
+  it('has $ property', () => {
+    const transport = createTransport({}, _ => resolvedSyncPromise({ statusCode: 200 }));
+    expect(transport.$).toBeDefined();
+  });
+
+  it('flushes the buffer', async () => {
+    const mockBuffer: PromiseBuffer<TransportResponse> = {
+      $: [],
+      add: jest.fn(),
+      drain: jest.fn(),
+    };
+    const transport = createTransport({}, _ => resolvedSyncPromise({ statusCode: 200 }), mockBuffer);
+    /* eslint-disable @typescript-eslint/unbound-method */
+    expect(mockBuffer.drain).toHaveBeenCalledTimes(0);
+    await transport.flush(1000);
+    expect(mockBuffer.drain).toHaveBeenCalledTimes(1);
+    expect(mockBuffer.drain).toHaveBeenLastCalledWith(1000);
+    /* eslint-enable @typescript-eslint/unbound-method */
+  });
+
+  describe('send', () => {
+    it('constructs a request to send to Sentry', async () => {
+      const transport = createTransport({}, req => {
+        expect(req.category).toEqual(ERROR_TRANSPORT_CATEGORY);
+        expect(req.body).toEqual(serializeEnvelope(ERROR_ENVELOPE));
+        return resolvedSyncPromise({ statusCode: 200, reason: 'OK' });
+      });
+      const res = await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+      expect(res.status).toBe('success');
+      expect(res.reason).toBe('OK');
+    });
+
+    it('returns an error if request failed', async () => {
+      const transport = createTransport({}, req => {
+        expect(req.category).toEqual(ERROR_TRANSPORT_CATEGORY);
+        expect(req.body).toEqual(serializeEnvelope(ERROR_ENVELOPE));
+        return resolvedSyncPromise({ statusCode: 400, reason: 'Bad Request' });
+      });
+      try {
+        await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+      } catch (res) {
+        expect(res.status).toBe('invalid');
+        expect(res.reason).toBe('Bad Request');
+      }
+    });
+
+    it('returns a default reason if reason not provided and request failed', async () => {
+      const transport = createTransport({}, req => {
+        expect(req.category).toEqual(TRANSACTION_TRANSPORT_CATEGORY);
+        expect(req.body).toEqual(serializeEnvelope(TRANSACTION_ENVELOPE));
+        return resolvedSyncPromise({ statusCode: 500 });
+      });
+      try {
+        await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+      } catch (res) {
+        expect(res.status).toBe('failed');
+        expect(res.reason).toBe('Unknown transport error');
+      }
+    });
+
+    describe('Rate-limiting', () => {
+      function setRateLimitTimes(): {
+        retryAfterSeconds: number;
+        beforeLimit: number;
+        withinLimit: number;
+        afterLimit: number;
+      } {
+        const retryAfterSeconds = 10;
+        const beforeLimit = Date.now();
+        const withinLimit = beforeLimit + (retryAfterSeconds / 2) * 1000;
+        const afterLimit = beforeLimit + retryAfterSeconds * 1000;
+        return { retryAfterSeconds, beforeLimit, withinLimit, afterLimit };
+      }
+
+      function createTestTransport(
+        initialTransportResponse: TransportMakeRequestResponse,
+      ): [NewTransport, (res: TransportMakeRequestResponse) => void] {
+        let transportResponse: TransportMakeRequestResponse = initialTransportResponse;
+
+        function setTransportResponse(res: TransportMakeRequestResponse) {
+          transportResponse = res;
+        }
+
+        const transport = createTransport({}, _ => {
+          return resolvedSyncPromise(transportResponse);
+        });
+
+        return [transport, setTransportResponse];
+      }
+
+      it('back-off using Retry-After header', async () => {
+        const { retryAfterSeconds, beforeLimit, withinLimit, afterLimit } = setRateLimitTimes();
+
+        jest
+          .spyOn(Date, 'now')
+          // 1st event - isRateLimited - FALSE
+          .mockImplementationOnce(() => beforeLimit)
+          // 1st event - updateRateLimits
+          .mockImplementationOnce(() => beforeLimit)
+          // 2nd event - isRateLimited - TRUE
+          .mockImplementationOnce(() => withinLimit)
+          // 3rd event - isRateLimited - FALSE
+          .mockImplementationOnce(() => afterLimit)
+          // 3rd event - updateRateLimits
+          .mockImplementationOnce(() => afterLimit);
+
+        const [transport, setTransportResponse] = createTestTransport({
+          headers: {
+            'x-sentry-rate-limits': null,
+            'retry-after': `${retryAfterSeconds}`,
+          },
+          statusCode: 429,
+        });
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        setTransportResponse({ statusCode: 200 });
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        const res = await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        expect(res.status).toBe('success');
+      });
+
+      it('back-off using X-Sentry-Rate-Limits with single category', async () => {
+        const { retryAfterSeconds, beforeLimit, withinLimit, afterLimit } = setRateLimitTimes();
+
+        jest
+          .spyOn(Date, 'now')
+          // 1st event - isRateLimited - FALSE
+          .mockImplementationOnce(() => beforeLimit)
+          // 1st event - updateRateLimits
+          .mockImplementationOnce(() => beforeLimit)
+          // 2nd event - isRateLimited - FALSE (different category)
+          .mockImplementationOnce(() => withinLimit)
+          // 3rd event - isRateLimited - TRUE
+          .mockImplementationOnce(() => withinLimit)
+          // 4th event - isRateLimited - FALSE
+          .mockImplementationOnce(() => afterLimit)
+          // 4th event - updateRateLimits
+          .mockImplementationOnce(() => afterLimit);
+
+        const [transport, setTransportResponse] = createTestTransport({
+          headers: {
+            'x-sentry-rate-limits': `${retryAfterSeconds}:error:scope`,
+            'retry-after': null,
+          },
+          statusCode: 429,
+        });
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        setTransportResponse({ statusCode: 200 });
+
+        try {
+          await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        const res = await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        expect(res.status).toBe('success');
+      });
+
+      it('back-off using X-Sentry-Rate-Limits with multiple categories', async () => {
+        const { retryAfterSeconds, beforeLimit, withinLimit, afterLimit } = setRateLimitTimes();
+
+        jest
+          .spyOn(Date, 'now')
+          // 1st event - isRateLimited - FALSE
+          .mockImplementationOnce(() => beforeLimit)
+          // 1st event - updateRateLimits
+          .mockImplementationOnce(() => beforeLimit)
+          // 2nd event - isRateLimited - TRUE (different category)
+          .mockImplementationOnce(() => withinLimit)
+          // 3rd event - isRateLimited - TRUE
+          .mockImplementationOnce(() => withinLimit)
+          // 4th event - isRateLimited - FALSE
+          .mockImplementationOnce(() => afterLimit)
+          // 4th event - updateRateLimits
+          .mockImplementationOnce(() => afterLimit);
+
+        const [transport, setTransportResponse] = createTestTransport({
+          headers: {
+            'x-sentry-rate-limits': `${retryAfterSeconds}:error;transaction:scope`,
+            'retry-after': null,
+          },
+          statusCode: 429,
+        });
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        try {
+          await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(
+            `Too many transaction requests, backing off until: ${new Date(afterLimit).toISOString()}`,
+          );
+        }
+
+        setTransportResponse({ statusCode: 200 });
+
+        const eventRes = await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        expect(eventRes.status).toBe('success');
+
+        const transactionRes = await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        expect(transactionRes.status).toBe('success');
+      });
+
+      it('back-off using X-Sentry-Rate-Limits with missing categories should lock them all', async () => {
+        const { retryAfterSeconds, beforeLimit, withinLimit, afterLimit } = setRateLimitTimes();
+
+        jest
+          .spyOn(Date, 'now')
+          // 1st event - isRateLimited - false
+          .mockImplementationOnce(() => beforeLimit)
+          // 1st event - updateRateLimits
+          .mockImplementationOnce(() => beforeLimit)
+          // 2nd event - isRateLimited - true (event category)
+          .mockImplementationOnce(() => withinLimit)
+          // 3rd event - isRateLimited - true (transaction category)
+          .mockImplementationOnce(() => withinLimit)
+          // 4th event - isRateLimited - false (event category)
+          .mockImplementationOnce(() => afterLimit)
+          // 4th event - updateRateLimits
+          .mockImplementationOnce(() => afterLimit)
+          // 5th event - isRateLimited - false (transaction category)
+          .mockImplementationOnce(() => afterLimit)
+          // 5th event - updateRateLimits
+          .mockImplementationOnce(() => afterLimit);
+
+        const [transport, setTransportResponse] = createTestTransport({
+          headers: {
+            'x-sentry-rate-limits': `${retryAfterSeconds}::scope`,
+            'retry-after': null,
+          },
+          statusCode: 429,
+        });
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        try {
+          await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(
+            `Too many transaction requests, backing off until: ${new Date(afterLimit).toISOString()}`,
+          );
+        }
+
+        setTransportResponse({ statusCode: 200 });
+
+        const eventRes = await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        expect(eventRes.status).toBe('success');
+
+        const transactionRes = await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        expect(transactionRes.status).toBe('success');
+      });
+
+      it('back-off using X-Sentry-Rate-Limits should also trigger for 200 responses', async () => {
+        const { retryAfterSeconds, beforeLimit, withinLimit, afterLimit } = setRateLimitTimes();
+
+        jest
+          .spyOn(Date, 'now')
+          // 1st event - isRateLimited - FALSE
+          .mockImplementationOnce(() => beforeLimit)
+          // 1st event - updateRateLimits
+          .mockImplementationOnce(() => beforeLimit)
+          // 2nd event - isRateLimited - TRUE
+          .mockImplementationOnce(() => withinLimit)
+          // 3rd event - isRateLimited - FALSE
+          .mockImplementationOnce(() => afterLimit)
+          // 3rd event - updateRateLimits
+          .mockImplementationOnce(() => afterLimit);
+
+        const [transport] = createTestTransport({
+          headers: {
+            'x-sentry-rate-limits': `${retryAfterSeconds}:error;transaction:scope`,
+            'retry-after': null,
+          },
+          statusCode: 200,
+        });
+
+        try {
+          await transport.send(ERROR_ENVELOPE, ERROR_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(`Too many error requests, backing off until: ${new Date(afterLimit).toISOString()}`);
+        }
+
+        try {
+          await transport.send(TRANSACTION_ENVELOPE, TRANSACTION_TRANSPORT_CATEGORY);
+        } catch (res) {
+          expect(res.status).toBe('rate_limit');
+          expect(res.reason).toBe(
+            `Too many transaction requests, backing off until: ${new Date(afterLimit).toISOString()}`,
+          );
+        }
+      });
+    });
+  });
+});
