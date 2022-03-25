9835b46aed9cdb95d5e10032cf50469f3080e929
Onur Temizkan <onur@narval.co.uk>
2022-03-25 18:53:59 +0000

    test(node): Add `withScope` integration tests.

---
+++ packages/node-integration-tests/suites/public-api/withScope/nested-scopes/scenario.ts
@@ -0,0 +1,25 @@
+import * as Sentry from '@sentry/node';
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  release: '1.0',
+});
+
+Sentry.setUser({ id: 'qux' });
+Sentry.captureMessage('root_before');
+
+Sentry.withScope(scope => {
+  scope.setTag('foo', false);
+  Sentry.captureMessage('outer_before');
+
+  Sentry.withScope(scope => {
+    scope.setTag('bar', 10);
+    scope.setUser(null);
+    Sentry.captureMessage('inner');
+  });
+
+  scope.setUser({ id: 'baz' });
+  Sentry.captureMessage('outer_after');
+});
+
+Sentry.captureMessage('root_after');
