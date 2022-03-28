05c8c2a61d14724e9d14f2eb67ea080cff93bee1
Abhijeet Prasad <aprasad@sentry.io>
2022-03-28 15:28:44 -0400

    console.log

--- packages/integration-tests/suites/new-transports/fetch-captureException/test.ts
+++ packages/integration-tests/suites/new-transports/fetch-captureException/test.ts
@@ -7,8 +7,6 @@ import { getFirstSentryEnvelopeRequest } from '../../../utils/helpers';
 sentryTest('should capture an error with the new fetch transport', async ({ getLocalTestPath, page }) => {
   const url = await getLocalTestPath({ testDir: __dirname });
 
-  page.on('console', arg => console.log(arg));
-
   const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
 
   expect(eventData.exception?.values).toHaveLength(1);
