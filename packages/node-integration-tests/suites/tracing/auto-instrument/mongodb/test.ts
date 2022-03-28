28aba8763d7074e103fa9a77a0f7137812653a2e
Onur Temizkan <onur@narval.co.uk>
2022-03-28 22:45:00 +0100

    Wrap before / after inside conditional describe.

--- packages/node-integration-tests/suites/tracing/auto-instrument/mongodb/test.ts
+++ packages/node-integration-tests/suites/tracing/auto-instrument/mongodb/test.ts
@@ -3,93 +3,95 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 
 import { assertSentryTransaction, getEnvelopeRequest, runServer } from '../../../../utils';
 
-let mongoServer: MongoMemoryServer;
-
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  process.env.MONGO_URL = mongoServer.getUri();
-}, 30000);
-
-afterAll(async () => {
-  await mongoServer.stop();
-});
-
 const NODE_VERSION = parseSemver(process.versions.node);
 
 // Skipping on Node versions below 12 as `mongo-memory-server` supports >= 12
-const conditionalTest = NODE_VERSION.major && NODE_VERSION.major < 12 ? test.skip : test;
+const conditionalTest = NODE_VERSION.major && NODE_VERSION.major < 12 ? describe.skip : describe;
 
-conditionalTest('should auto-instrument `mongodb` package.', async () => {
-  const url = await runServer(__dirname);
+conditionalTest('MongoDB Test', () => {
+  let mongoServer: MongoMemoryServer;
 
-  const envelope = await getEnvelopeRequest(url);
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    process.env.MONGO_URL = mongoServer.getUri();
+  }, 30000);
 
-  expect(envelope).toHaveLength(3);
+  afterAll(async () => {
+    await mongoServer.stop();
+  });
 
-  assertSentryTransaction(envelope[2], {
-    transaction: 'Test Transaction',
-    spans: [
-      {
-        data: {
-          collectionName: 'movies',
-          dbName: 'admin',
-          namespace: 'admin.movies',
-          doc: '{"title":"Rick and Morty"}',
+  test('should auto-instrument `mongodb` package.', async () => {
+    const url = await runServer(__dirname);
+
+    const envelope = await getEnvelopeRequest(url);
+
+    expect(envelope).toHaveLength(3);
+
+    assertSentryTransaction(envelope[2], {
+      transaction: 'Test Transaction',
+      spans: [
+        {
+          data: {
+            collectionName: 'movies',
+            dbName: 'admin',
+            namespace: 'admin.movies',
+            doc: '{"title":"Rick and Morty"}',
+          },
+          description: 'insertOne',
+          op: 'db',
         },
-        description: 'insertOne',
-        op: 'db',
-      },
-      {
-        data: {
-          collectionName: 'movies',
-          dbName: 'admin',
-          namespace: 'admin.movies',
-          query: '{"title":"Back to the Future"}',
+        {
+          data: {
+            collectionName: 'movies',
+            dbName: 'admin',
+            namespace: 'admin.movies',
+            query: '{"title":"Back to the Future"}',
+          },
+          description: 'findOne',
+          op: 'db',
         },
-        description: 'findOne',
-        op: 'db',
-      },
-      {
-        data: {
-          collectionName: 'movies',
-          dbName: 'admin',
-          namespace: 'admin.movies',
-          query: '{"title":"Back to the Future"}',
+        {
+          data: {
+            collectionName: 'movies',
+            dbName: 'admin',
+            namespace: 'admin.movies',
+            query: '{"title":"Back to the Future"}',
+          },
+          description: 'find',
+          op: 'db',
         },
-        description: 'find',
-        op: 'db',
-      },
-      {
-        data: {
-          collectionName: 'movies',
-          dbName: 'admin',
-          namespace: 'admin.movies',
-          filter: '{"title":"Back to the Future"}',
-          update: '{"$set":{"title":"South Park"}}',
+        {
+          data: {
+            collectionName: 'movies',
+            dbName: 'admin',
+            namespace: 'admin.movies',
+            filter: '{"title":"Back to the Future"}',
+            update: '{"$set":{"title":"South Park"}}',
+          },
+          description: 'updateOne',
+          op: 'db',
         },
-        description: 'updateOne',
-        op: 'db',
-      },
-      {
-        data: {
-          collectionName: 'movies',
-          dbName: 'admin',
-          namespace: 'admin.movies',
-          query: '{"title":"South Park"}',
+        {
+          data: {
+            collectionName: 'movies',
+            dbName: 'admin',
+            namespace: 'admin.movies',
+            query: '{"title":"South Park"}',
+          },
+          description: 'findOne',
+          op: 'db',
         },
-        description: 'findOne',
-        op: 'db',
-      },
-      {
-        data: {
-          collectionName: 'movies',
-          dbName: 'admin',
-          namespace: 'admin.movies',
-          query: '{"title":"South Park"}',
+        {
+          data: {
+            collectionName: 'movies',
+            dbName: 'admin',
+            namespace: 'admin.movies',
+            query: '{"title":"South Park"}',
+          },
+          description: 'find',
+          op: 'db',
         },
-        description: 'find',
-        op: 'db',
-      },
-    ],
+      ],
+    });
   });
 });
