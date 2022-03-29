62a5f44e991625365de1df508beda3813985ec8c
Onur Temizkan <onur@narval.co.uk>
2022-03-29 19:39:32 +0100

    test(node): Add basic integration tests for Express.

---
+++ packages/node-integration-tests/suites/express/handle-error/server.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/node';
+import express from 'express';
+
+const app = express();
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  release: '1.0',
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
+app.get('/test/express', async () => {
+  throw new Error('test_error');
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+export default app;
