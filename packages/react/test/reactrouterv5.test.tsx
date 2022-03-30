0e78d1b72d2c040823ac16db1821d3c5473945a3
Abhijeet Prasad <aprasad@sentry.io>
2022-03-30 08:11:18 -0400

    fix lint errors

--- packages/react/test/reactrouterv5.test.tsx
+++ packages/react/test/reactrouterv5.test.tsx
@@ -1,4 +1,4 @@
-import { render, act } from '@testing-library/react';
+import { act,render } from '@testing-library/react';
 import { createMemoryHistory } from 'history-4';
 import * as React from 'react';
 import { matchPath, Route, Router, Switch } from 'react-router-5';
