7b49f6585248ecb036d1c86dd6d6523bda9a2eb1
Lukas Stracke <lukas.stracke@sentry.io>
2022-03-31 16:58:15 +0200

    add central build dir to `@sentry/tracing`

--- .size-limit.js
+++ .size-limit.js
@@ -53,13 +53,13 @@ module.exports = [
   },
   {
     name: '@sentry/browser + @sentry/tracing - ES5 CDN Bundle (gzipped + minified)',
-    path: 'packages/tracing/build/bundle.tracing.min.js',
+    path: 'packages/tracing/build/bundles/bundle.tracing.min.js',
     gzip: true,
     limit: '100 KB',
   },
   {
     name: '@sentry/browser + @sentry/tracing - ES6 CDN Bundle (gzipped + minified)',
-    path: 'packages/tracing/build/bundle.tracing.es6.min.js',
+    path: 'packages/tracing/build/bundles/bundle.tracing.es6.min.js',
     gzip: true,
     limit: '100 KB',
   },
