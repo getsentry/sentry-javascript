80ab1be1bb6cf37d5f72b750c9bfc20acc144732
Lukas Stracke <lukas.stracke@sentry.io>
2022-03-31 11:08:52 +0200

    ref(build): convert `postbuild.sh` to node script
    add `postbuild.ts` node script which can be run with ts-node to replace the
    bash script `postbuild.sh`. This is done to conform with our decision to
    change all bash scripts to node s.t. they can be run on any platform (incl.
    Windows). Furthermore, it gives us the flexibility to make more adjustments
    that are harder to code/read in bash.
    
    * clone behaviour of `postbuild.sh`
    * add removal of `volta` and `scripts` properties in copied package.json
    * adjust calls to postbuild.sh to node script
    * adjust reference in comment to node script

---
+++ scripts/postbuild.ts
@@ -0,0 +1,46 @@
+/*
+  This script prepares the central `build` directory for NPM package creation.
+  It first copies all non-code files into the `build` directory, including `package.json` which
+  is edited to adjust entry point paths. These corrections are performed so that they align with
+  the directory structure inside `build`.
+*/
+
+import * as fs from 'fs';
+
+const BUILD_DIR = 'build';
+const ASSETS = ['README.md', 'LICENSE', 'package.json', '.npmignore'];
+const ENTRY_POINTS = ['main', 'module', 'types'];
+
+// check if build dirs exists
+if (!fs.existsSync(BUILD_DIR)) {
+  console.error(`Directory ${BUILD_DIR} DOES NOT exist`);
+  console.error("This script should only be executed after you've run `yarn build`.");
+  process.exit(1);
+}
+
+// copy non-code assets to build dir
+ASSETS.forEach(asset => {
+  if (!fs.existsSync(asset)) {
+    console.error(`Asset ${asset} does not exist.`);
+    process.exit(1);
+  }
+  fs.copyFileSync(asset, `${BUILD_DIR}/${asset}`);
+});
+
+// package.json modifications
+const packageJsonPath = `${process.cwd()}/${BUILD_DIR}/package.json`;
+const pkg: { [key: string]: string } = require(packageJsonPath);
+
+// modify entry points to point to correct paths (i.e. delete the build directory)
+ENTRY_POINTS.filter(entryPoint => !!pkg[entryPoint]).forEach(entryPoint => {
+  pkg[entryPoint] = pkg[entryPoint].replace(`${BUILD_DIR}/`, '');
+});
+
+// TODO decide if we want this:
+delete pkg.scripts;
+delete pkg.volta;
+
+// write modified package.json to file
+fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2));
+
+console.log('Successfully finishied postbuild commands');
