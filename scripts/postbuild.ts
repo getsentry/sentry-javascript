6aa4ff6b992b1d43340a066b93ae63c8a79b26a6
Lukas Stracke <lukas@stracke.co.at>
2022-04-01 09:21:42 +0200

    apply suggestions from code review
    * comment/spelling corrections
    * console output
    
    Co-authored-by: Katie Byers <lobsterkatie@gmail.com>

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,7 +1,7 @@
 /*
   This script prepares the central `build` directory for NPM package creation.
-  It first copies all non-code files into the `build` directory, including `package.json` which
-  is edited to adjust entry point paths. These corrections are performed so that they align with
+  It first copies all non-code files into the `build` directory, including `package.json`, which
+  is edited to adjust entry point paths. These corrections are performed so that the paths align with
   the directory structure inside `build`.
 */
 
@@ -13,7 +13,7 @@ const BUILD_DIR = 'build';
 const ASSETS = ['README.md', 'LICENSE', 'package.json', '.npmignore'];
 const ENTRY_POINTS = ['main', 'module', 'types'];
 
-// check if build dirs exists
+// check if build dir exists
 if (!fs.existsSync(BUILD_DIR)) {
   console.error(`Directory ${BUILD_DIR} DOES NOT exist`);
   console.error("This script should only be executed after you've run `yarn build`.");
@@ -33,7 +33,7 @@ ASSETS.forEach(asset => {
 const packageJsonPath = path.join(process.cwd(), BUILD_DIR, 'package.json');
 const pkg: { [key: string]: string } = require(packageJsonPath);
 
-// modify entry points to point to correct paths (i.e. delete the build directory)
+// modify entry points to point to correct paths (i.e. strip out the build directory)
 ENTRY_POINTS.filter(entryPoint => !!pkg[entryPoint]).forEach(entryPoint => {
   pkg[entryPoint] = pkg[entryPoint].replace(`${BUILD_DIR}/`, '');
 });
@@ -45,4 +45,4 @@ delete pkg.volta;
 // write modified package.json to file
 fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2));
 
-console.log('Successfully finished postbuild commands');
+console.log(`\nSuccessfully finished postbuild commands for ${pkg.name}`);
