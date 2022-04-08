d9936eb6cfd1655ea600ed30495cd5eabe2bc508
Lukas Stracke <lukas@stracke.co.at>
2022-04-08 17:12:20 +0200

    ref(build): Rename `dist` directories to `cjs` (#4900)
    rename the dist directories in all build dirs to cjs. Hence, also the tarballs' structure changes which is why this PR also introduces a breaking change.
    
    Additional change: cleanup `yarn clean` commands by removing no longer existing directories

--- packages/browser/test/package/test-code.js
+++ packages/browser/test/package/test-code.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const Sentry = require('../../build/npm/dist/index.js');
-const Integrations = require('../../../integrations/build/npm/dist/dedupe.js');
+const Sentry = require('../../build/npm/cjs/index.js');
+const Integrations = require('../../../integrations/build/npm/cjs/dedupe.js');
 
 // Init
 Sentry.init({
