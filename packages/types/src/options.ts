2c06ccc34f3e6ca5402f77b3f22d7f00346c8587
Abhijeet Prasad <aprasad@sentry.io>
2022-04-12 16:02:17 -0400

    feat(core): Introduce seperate client options

--- packages/types/src/options.ts
+++ packages/types/src/options.ts
@@ -1,4 +1,5 @@
 import { Breadcrumb, BreadcrumbHint } from './breadcrumb';
+import { DsnComponents } from './dsn';
 import { Event, EventHint } from './event';
 import { Integration } from './integration';
 import { CaptureContext } from './scope';
@@ -7,8 +8,7 @@ import { StackLineParser, StackParser } from './stacktrace';
 import { SamplingContext } from './transaction';
 import { Transport, TransportClass, TransportOptions } from './transport';
 
-/** Base configuration options for every SDK. */
-export interface Options {
+export interface ClientOptions {
   /**
    * Enable debug functionality in the SDK itself
    */
@@ -20,48 +20,26 @@ export interface Options {
    */
   enabled?: boolean;
 
+  /** Attaches stacktraces to pure capture message / log integrations */
+  attachStacktrace?: boolean;
+
+  /**
+   * A flag enabling Sessions Tracking feature.
+   * By default, Sessions Tracking is enabled.
+   */
+  autoSessionTracking?: boolean;
+
+  /**
+   * Send SDK Client Reports.
+   * By default, Client Reports are enabled.
+   */
+  sendClientReports?: boolean;
+
   /**
    * The Dsn used to connect to Sentry and identify the project. If omitted, the
    * SDK will not send any data to Sentry.
    */
-  dsn?: string;
-
-  /**
-   * If this is set to false, default integrations will not be added, otherwise this will internally be set to the
-   * recommended default integrations.
-   * TODO: We should consider changing this to `boolean | Integration[]`
-   */
-  defaultIntegrations?: false | Integration[];
-
-  /**
-   * List of integrations that should be installed after SDK was initialized.
-   * Accepts either a list of integrations or a function that receives
-   * default integrations and returns a new, updated list.
-   */
-  integrations?: Integration[] | ((integrations: Integration[]) => Integration[]);
-
-  /**
-   * A pattern for error messages which should not be sent to Sentry.
-   * By default, all errors will be sent.
-   */
-  ignoreErrors?: Array<string | RegExp>;
-
-  /**
-   * Transport object that should be used to send events to Sentry
-   */
-  transport?: TransportClass<Transport>;
-
-  /**
-   * Options for the default transport that the SDK uses.
-   */
-  transportOptions?: TransportOptions;
-
-  /**
-   * A URL to an envelope tunnel endpoint. An envelope tunnel is an HTTP endpoint
-   * that accepts Sentry envelopes for forwarding. This can be used to force data
-   * through a custom server independent of the type of data.
-   */
-  tunnel?: string;
+  dsn?: DsnComponents;
 
   /**
    * The release identifier used when uploading respective source maps. Specify
@@ -77,50 +55,25 @@ export interface Options {
   dist?: string;
 
   /**
-   * The maximum number of breadcrumbs sent with events. Defaults to 100.
-   * Values over 100 will be ignored and 100 used instead.
+   * List of integrations that should be installed after SDK was initialized.
    */
-  maxBreadcrumbs?: number;
-
-  /** A global sample rate to apply to all events (0 - 1). */
-  sampleRate?: number;
-
-  /** Attaches stacktraces to pure capture message / log integrations */
-  attachStacktrace?: boolean;
-
-  /** Maximum number of chars a single value can have before it will be truncated. */
-  maxValueLength?: number;
+  integrations: Integration[];
 
   /**
-   * Maximum number of levels that normalization algorithm will traverse in objects and arrays.
-   * Used when normalizing an event before sending, on all of the listed attributes:
-   * - `breadcrumbs.data`
-   * - `user`
-   * - `contexts`
-   * - `extra`
-   * Defaults to `3`. Set to `0` to disable.
+   * Transport object that should be used to send events to Sentry
    */
-  normalizeDepth?: number;
+  transport: TransportClass<Transport>;
 
   /**
-   * Maximum number of properties or elements that the normalization algorithm will output in any single array or object included in the normalized event.
-   * Used when normalizing an event before sending, on all of the listed attributes:
-   * - `breadcrumbs.data`
-   * - `user`
-   * - `contexts`
-   * - `extra`
-   * Defaults to `1000`
+   * A stack parser implementation
+   * By default, a stack parser is supplied for all supported platforms
    */
-  normalizeMaxBreadth?: number;
+  stackParser: StackParser;
 
   /**
-   * Controls how many milliseconds to wait before shutting down. The default is
-   * SDK-specific but typically around 2 seconds. Setting this too low can cause
-   * problems for sending events from command line applications. Setting it too
-   * high can cause the application to block for users with network connectivity
-   * problems.
+   * Options for the default transport that the SDK uses.
    */
-  shutdownTimeout?: number;
+  transportOptions?: TransportOptions;
 
   /**
    * Sample rate to determine trace sampling.
@@ -134,27 +87,65 @@ export interface Options {
   tracesSampleRate?: number;
 
   /**
-   * A flag enabling Sessions Tracking feature.
-   * By default, Sessions Tracking is enabled.
-   */
-  autoSessionTracking?: boolean;
-
-  /**
-   * Send SDK Client Reports.
-   * By default, Client Reports are enabled.
-   */
-  sendClientReports?: boolean;
-
-  /**
    * Initial data to populate scope.
    */
   initialScope?: CaptureContext;
 
   /**
-   * A stack parser implementation or an array of stack line parsers
-   * By default, a stack parser is supplied for all supported browsers
+   * The maximum number of breadcrumbs sent with events. Defaults to 100.
+   * Values over 100 will be ignored and 100 used instead.
    */
-  stackParser?: StackParser | StackLineParser[];
+  maxBreadcrumbs: number;
+
+  /** A global sample rate to apply to all events (0 - 1). */
+  sampleRate: number;
+
+  /** Maximum number of chars a single value can have before it will be truncated. */
+  maxValueLength: number;
+
+  /**
+   * Maximum number of levels that normalization algorithm will traverse in objects and arrays.
+   * Used when normalizing an event before sending, on all of the listed attributes:
+   * - `breadcrumbs.data`
+   * - `user`
+   * - `contexts`
+   * - `extra`
+   * Defaults to `3`. Set to `0` to disable.
+   */
+  normalizeDepth: number;
+
+  /**
+   * Maximum number of properties or elements that the normalization algorithm will output in any single array or object included in the normalized event.
+   * Used when normalizing an event before sending, on all of the listed attributes:
+   * - `breadcrumbs.data`
+   * - `user`
+   * - `contexts`
+   * - `extra`
+   * Defaults to `1000`
+   */
+  normalizeMaxBreadth: number;
+
+  /**
+   * Controls how many milliseconds to wait before shutting down. The default is
+   * SDK-specific but typically around 2 seconds. Setting this too low can cause
+   * problems for sending events from command line applications. Setting it too
+   * high can cause the application to block for users with network connectivity
+   * problems.
+   */
+  shutdownTimeout: number;
+
+  /**
+   * A pattern for error messages which should not be sent to Sentry.
+   * By default, all errors will be sent.
+   */
+  ignoreErrors?: Array<string | RegExp>;
+
+  /**
+   * A URL to an envelope tunnel endpoint. An envelope tunnel is an HTTP endpoint
+   * that accepts Sentry envelopes for forwarding. This can be used to force data
+   * through a custom server independent of the type of data.
+   */
+  tunnel?: string;
 
   /**
    * Set of metadata about the SDK that can be internally used to enhance envelopes and events,
@@ -209,4 +200,38 @@ export interface Options {
    * @returns The breadcrumb that will be added | null.
    */
   beforeBreadcrumb?: (breadcrumb: Breadcrumb, hint?: BreadcrumbHint) => Breadcrumb | null;
+}
+
+/** Base configuration options for every SDK. */
+export interface Options extends Omit<Partial<ClientOptions>, 'dsn' | 'integrations' | 'transport' | 'stackParser'> {
+  /**
+   * The Dsn used to connect to Sentry and identify the project. If omitted, the
+   * SDK will not send any data to Sentry.
+   */
+  dsn?: string;
+
+  /**
+   * If this is set to false, default integrations will not be added, otherwise this will internally be set to the
+   * recommended default integrations.
+   * TODO: We should consider changing this to `boolean | Integration[]`
+   */
+  defaultIntegrations?: false | Integration[];
+
+  /**
+   * List of integrations that should be installed after SDK was initialized.
+   * Accepts either a list of integrations or a function that receives
+   * default integrations and returns a new, updated list.
+   */
+  integrations?: Integration[] | ((integrations: Integration[]) => Integration[]);
+
+  /**
+   * Transport object that should be used to send events to Sentry
+   */
+  transport?: TransportClass<Transport>;
+
+  /**
+   * A stack parser implementation or an array of stack line parsers
+   * By default, a stack parser is supplied for all supported browsers
+   */
+  stackParser?: StackParser | StackLineParser[];
 }
