7f73eff8c7b60f3d0149af26b75f98096a4190e6
Lukas Stracke <lukas@stracke.co.at>
2022-04-12 10:12:48 +0200

    ref: Port functionality from `Backend` to `Client` (#4911)
    port all the functionality from the Backend classes to the Client classes. This includes:
    
    * Backend (interface)
    * BaseBackend
    * BrowserBackend
    * NodeBackend
    * TestBackend
    
    Additionally, fix the unit and integration tests in Core to spy on TestClient instead of TestBackend.

--- packages/browser/src/client.ts
+++ packages/browser/src/client.ts
@@ -1,17 +1,20 @@
-import { BaseClient, Scope, SDK_VERSION } from '@sentry/core';
-import { Event, EventHint } from '@sentry/types';
-import { getGlobalObject, logger } from '@sentry/utils';
+import { BaseClient, getEnvelopeEndpointWithUrlEncodedAuth, initAPIDetails, Scope, SDK_VERSION } from '@sentry/core';
+import { Event, EventHint, Severity, Transport, TransportOptions } from '@sentry/types';
+import { getGlobalObject, logger, supportsFetch } from '@sentry/utils';
 
 import { BrowserBackend, BrowserOptions } from './backend';
+import { eventFromException, eventFromMessage } from './eventbuilder';
 import { IS_DEBUG_BUILD } from './flags';
 import { injectReportDialog, ReportDialogOptions } from './helpers';
 import { Breadcrumbs } from './integrations';
+import { FetchTransport, makeNewFetchTransport, makeNewXHRTransport, XHRTransport } from './transports';
 
 /**
  * The Sentry Browser SDK Client.
  *
  * @see BrowserOptions for documentation on configuration options.
  * @see SentryClient for usage documentation.
+ * TODO(v7): remove BrowserBackend
  */
 export class BrowserClient extends BaseClient<BrowserBackend, BrowserOptions> {
   /**
@@ -32,6 +35,7 @@ export class BrowserClient extends BaseClient<BrowserBackend, BrowserOptions> {
       version: SDK_VERSION,
     };
 
+    // TODO(v7): remove BrowserBackend param
     super(BrowserBackend, options);
   }
 
@@ -61,6 +65,20 @@ export class BrowserClient extends BaseClient<BrowserBackend, BrowserOptions> {
   /**
    * @inheritDoc
    */
+  public eventFromException(exception: unknown, hint?: EventHint): PromiseLike<Event> {
+    return eventFromException(exception, hint, this._options.attachStacktrace);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public eventFromMessage(message: string, level: Severity = Severity.Info, hint?: EventHint): PromiseLike<Event> {
+    return eventFromMessage(message, level, hint, this._options.attachStacktrace);
+  }
+
+  /**
+   * @inheritDoc
+   */
   protected _prepareEvent(event: Event, scope?: Scope, hint?: EventHint): PromiseLike<Event | null> {
     event.platform = event.platform || 'javascript';
     return super._prepareEvent(event, scope, hint);
@@ -75,5 +93,41 @@ export class BrowserClient extends BaseClient<BrowserBackend, BrowserOptions> {
       integration.addSentryBreadcrumb(event);
     }
     super._sendEvent(event);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected _setupTransport(): Transport {
+    if (!this._options.dsn) {
+      // We return the noop transport here in case there is no Dsn.
+      return super._setupTransport();
+    }
+
+    const transportOptions: TransportOptions = {
+      ...this._options.transportOptions,
+      dsn: this._options.dsn,
+      tunnel: this._options.tunnel,
+      sendClientReports: this._options.sendClientReports,
+      _metadata: this._options._metadata,
+    };
+
+    const api = initAPIDetails(transportOptions.dsn, transportOptions._metadata, transportOptions.tunnel);
+    const url = getEnvelopeEndpointWithUrlEncodedAuth(api.dsn, api.tunnel);
+
+    if (this._options.transport) {
+      return new this._options.transport(transportOptions);
+    }
+    if (supportsFetch()) {
+      const requestOptions: RequestInit = { ...transportOptions.fetchParameters };
+      this._newTransport = makeNewFetchTransport({ requestOptions, url });
+      return new FetchTransport(transportOptions);
+    }
+
+    this._newTransport = makeNewXHRTransport({
+      url,
+      headers: transportOptions.headers,
+    });
+    return new XHRTransport(transportOptions);
   }
 }
