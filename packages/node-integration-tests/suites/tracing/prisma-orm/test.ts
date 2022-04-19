a92f781079cc6f24a42d509fd68ede1982b90729
Onur Temizkan <onur@narval.co.uk>
2022-04-19 14:24:33 +0300

    Skip test on Node 10.

--- packages/node-integration-tests/suites/tracing/prisma-orm/test.ts
+++ packages/node-integration-tests/suites/tracing/prisma-orm/test.ts
@@ -1,15 +1,17 @@
-import { assertSentryTransaction, getEnvelopeRequest, runServer } from '../../../utils';
+import { assertSentryTransaction, conditionalTest, getEnvelopeRequest, runServer } from '../../../utils';
 
-test('should instrument Prisma ORM client for tracing.', async () => {
-  const url = await runServer(__dirname);
-  const envelope = await getEnvelopeRequest(url);
+conditionalTest({ min: 12 })('Prisma ORM Integration', () => {
+  test('should instrument Prisma client for tracing.', async () => {
+    const url = await runServer(__dirname);
+    const envelope = await getEnvelopeRequest(url);
 
-  assertSentryTransaction(envelope[2], {
-    transaction: 'Test Transaction',
-    spans: [
-      { description: 'Action: create, Model: User', op: 'prisma' },
-      { description: 'Action: findMany, Model: User', op: 'prisma' },
-      { description: 'Action: deleteMany, Model: User', op: 'prisma' },
-    ],
+    assertSentryTransaction(envelope[2], {
+      transaction: 'Test Transaction',
+      spans: [
+        { description: 'Action: create, Model: User', op: 'prisma' },
+        { description: 'Action: findMany, Model: User', op: 'prisma' },
+        { description: 'Action: deleteMany, Model: User', op: 'prisma' },
+      ],
+    });
   });
 });
