025667131e0644fd62cfe959f28034c72ff05a01
Katie Byers <lobsterkatie@gmail.com>
2022-04-19 01:33:15 -0700

    add `const`-to-`var` plugin

--- rollup/plugins/npmPlugins.js
+++ rollup/plugins/npmPlugins.js
@@ -1,7 +1,9 @@
 /**
+ * Replace plugin docs: https://github.com/rollup/plugins/tree/master/packages/replace
  * Sucrase plugin docs: https://github.com/rollup/plugins/tree/master/packages/sucrase
  */
 
+import replace from '@rollup/plugin-replace';
 import sucrase from '@rollup/plugin-sucrase';
 
 /**
@@ -12,5 +14,31 @@ import sucrase from '@rollup/plugin-sucrase';
 export function makeSucrasePlugin() {
   return sucrase({
     transforms: ['typescript', 'jsx'],
+  });
+}
+
+/**
+ * Create a plugin to switch all instances of `const` to `var`, both to prevent problems when we shadow `global` and
+ * because it's fewer characters.
+ *
+ * Note that the generated plugin runs the replacement both before and after rollup does its code manipulation, to
+ * increase the chances that nothing is missed.
+ *
+ * TODO This is pretty brute-force-y. Perhaps we could switch to using a parser, the way we (will) do for both our jest
+ * transformer and the polyfill build script.
+ *
+ * @returns An instance of the `@rollup/plugin-replace` plugin
+ */
+export function makeConstToVarPlugin() {
+  return replace({
+    // TODO `preventAssignment` will default to true in version 5.x of the replace plugin, at which point we can get rid
+    // of this. (It actually makes no difference in this case whether it's true or false, since we never assign to
+    // `const`, but if we don't give it a value, it will spam with warnings.)
+    preventAssignment: true,
+    values: {
+      // Include a space at the end to guarantee we're not accidentally catching the beginning of the words "constant,"
+      // "constantly," etc.
+      'const ': 'var ',
+    },
   });
 }
