e05852064e77abcf434330c0d2936d5cf3f77481
Lukas Stracke <lukas.stracke@sentry.io>
2022-04-20 15:17:52 +0200

    adjust angular build dir and tarball structure to be more similar to other SDKs
    includes deletion of .npmignore and the  addition of `scripts/postbuild.ts` to
    make a few adjustments to folder structure and the shipped `package.json`

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -2,5 +2,7 @@ module.exports = {
   env: {
     browser: true,
   },
+  // ignore these because they're not covered by a `tsconfig`, which makes eslint throw an error
+  ignorePatterns: ['scripts/postbuild.ts'],
   extends: ['../../.eslintrc.js'],
 };
