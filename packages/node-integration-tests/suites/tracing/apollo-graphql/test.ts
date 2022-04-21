c80739f36d8c0d52ed2faa92656ff5e30d1cbed6
Onur Temizkan <onur@narval.co.uk>
2022-04-21 21:17:40 +0300

    Disable integration test on Node 10.

--- packages/node-integration-tests/suites/tracing/apollo-graphql/test.ts
+++ packages/node-integration-tests/suites/tracing/apollo-graphql/test.ts
@@ -1,31 +1,35 @@
-import { assertSentryTransaction, getEnvelopeRequest, runServer } from '../../../utils';
+import { assertSentryTransaction, conditionalTest, getEnvelopeRequest, runServer } from '../../../utils';
 
-test('should instrument GraphQL and Apollo Server.', async () => {
-  const url = await runServer(__dirname);
-  const envelope = await getEnvelopeRequest(url);
+// Node 10 is not supported by `graphql-js`
+// Ref: https://github.com/graphql/graphql-js/blob/main/package.json
+conditionalTest({ min: 12 })('GraphQL/Apollo Tests', () => {
+  test('should instrument GraphQL and Apollo Server.', async () => {
+    const url = await runServer(__dirname);
+    const envelope = await getEnvelopeRequest(url);
 
-  expect(envelope).toHaveLength(3);
+    expect(envelope).toHaveLength(3);
 
-  const transaction = envelope[2];
-  const parentSpanId = (transaction as any)?.contexts?.trace?.span_id;
-  const graphqlSpanId = (transaction as any)?.spans?.[0].span_id;
+    const transaction = envelope[2];
+    const parentSpanId = (transaction as any)?.contexts?.trace?.span_id;
+    const graphqlSpanId = (transaction as any)?.spans?.[0].span_id;
 
-  expect(parentSpanId).toBeDefined();
-  expect(graphqlSpanId).toBeDefined();
+    expect(parentSpanId).toBeDefined();
+    expect(graphqlSpanId).toBeDefined();
 
-  assertSentryTransaction(transaction, {
-    transaction: 'test_transaction',
-    spans: [
-      {
-        description: 'execute',
-        op: 'db.graphql',
-        parent_span_id: parentSpanId,
-      },
-      {
-        description: 'Query.hello',
-        op: 'db.graphql.apollo',
-        parent_span_id: graphqlSpanId,
-      },
-    ],
+    assertSentryTransaction(transaction, {
+      transaction: 'test_transaction',
+      spans: [
+        {
+          description: 'execute',
+          op: 'db.graphql',
+          parent_span_id: parentSpanId,
+        },
+        {
+          description: 'Query.hello',
+          op: 'db.graphql.apollo',
+          parent_span_id: graphqlSpanId,
+        },
+      ],
+    });
   });
 });
