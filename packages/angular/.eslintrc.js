b438540186c850579624abf14a94c97de9bc27c9
Lukas Stracke <lukas.stracke@sentry.io>
2022-04-21 10:38:58 +0200

    Revert "adjust angular build dir and tarball structure to be more similar to other SDKs"
    This reverts commit e05852064e77abcf434330c0d2936d5cf3f77481.

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -2,7 +2,5 @@ module.exports = {
   env: {
     browser: true,
   },
-  // ignore these because they're not covered by a `tsconfig`, which makes eslint throw an error
-  ignorePatterns: ['scripts/postbuild.ts'],
   extends: ['../../.eslintrc.js'],
 };
