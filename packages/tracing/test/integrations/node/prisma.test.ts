3963d1d5104c734ede1cbac69eada594a0160f72
Onur Temizkan <onur@narval.co.uk>
2022-04-21 19:05:18 +0300

    Add a simple unit test.

---
+++ packages/tracing/test/integrations/node/prisma.test.ts
@@ -0,0 +1,60 @@
+import { Hub, Scope } from '@sentry/hub';
+
+import { Prisma } from '../../../src/integrations/node/prisma';
+import { Span } from '../../../src/span';
+
+type PrismaMiddleware = (params: unknown, next: (params?: unknown) => Promise<unknown>) => Promise<unknown>;
+
+class PrismaClient {
+  constructor() {
+    this.middleware = undefined;
+  }
+
+  private middleware?: PrismaMiddleware;
+
+  public $use(cb: PrismaMiddleware) {
+    this.middleware = cb;
+  }
+
+  public user = {
+    create: () => this.middleware?.({ action: 'create', model: 'user' }, () => Promise.resolve('result')),
+  };
+}
+
+describe('setupOnce', () => {
+  const Client: PrismaClient = new PrismaClient();
+
+  let scope = new Scope();
+  let parentSpan: Span;
+  let childSpan: Span;
+
+  beforeAll(() => {
+    // @ts-ignore
+    new Prisma({ client: Client }).setupOnce(
+      () => undefined,
+      () => new Hub(undefined, scope),
+    );
+  });
+
+  beforeEach(() => {
+    scope = new Scope();
+    parentSpan = new Span();
+    childSpan = parentSpan.startChild();
+    jest.spyOn(scope, 'getSpan').mockReturnValueOnce(parentSpan);
+    jest.spyOn(parentSpan, 'startChild').mockReturnValueOnce(childSpan);
+    jest.spyOn(childSpan, 'finish');
+  });
+
+  it(`should add middleware with $use method correctly`, done => {
+    Client.user.create()?.then(res => {
+      expect(res).toBe('result');
+      expect(scope.getSpan).toBeCalled();
+      expect(parentSpan.startChild).toBeCalledWith({
+        description: 'user create',
+        op: 'db.prisma',
+      });
+      expect(childSpan.finish).toBeCalled();
+      done();
+    });
+  });
+});
