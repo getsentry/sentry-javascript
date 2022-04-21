ac1db2162b43a3768c60b2dabe66c1786b381146
Abhijeet Prasad <aprasad@sentry.io>
2022-04-21 15:59:40 -0400

    only add 1 event processor

--- packages/ember/tests/dummy/app/app.js
+++ packages/ember/tests/dummy/app/app.js
@@ -4,19 +4,7 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import * as Sentry from '@sentry/ember';
 
-import Ember from 'ember';
-
 Sentry.init();
-
-Sentry.addGlobalEventProcessor((event) => {
-  if (Ember.testing) {
-    if (!window._sentryTestEvents) {
-      window._sentryTestEvents = [];
-    }
-    window._sentryTestEvents.push(event);
-  }
-  return event;
-});
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
