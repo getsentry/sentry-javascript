fcdfe7de71953d36e32591b10a01139cd284280b
Lukas Stracke <lukas.stracke@sentry.io>
2022-04-22 09:42:19 +0200

    feat(angular): Log warning if SDK is used with an older Angular version than officially supported

--- packages/angular/src/sdk.ts
+++ packages/angular/src/sdk.ts
@@ -1,4 +1,8 @@
+import { VERSION } from '@angular/core';
 import { BrowserOptions, init as browserInit, SDK_VERSION } from '@sentry/browser';
+import { logger } from '@sentry/utils';
+import { ANGULAR_MINIMUM_VERSION } from './constants';
+import { IS_DEBUG_BUILD } from './flags';
 
 /**
  * Inits the Angular SDK
@@ -15,5 +19,20 @@ export function init(options: BrowserOptions): void {
     ],
     version: SDK_VERSION,
   };
+  checkAngularVersion();
   browserInit(options);
+}
+
+function checkAngularVersion(): void {
+  if (VERSION && VERSION.major) {
+    const major = parseInt(VERSION.major, 10);
+    if (major < ANGULAR_MINIMUM_VERSION) {
+      IS_DEBUG_BUILD &&
+        logger.warn(
+          `The Sentry SDK does not officially support Angular ${major}.`,
+          `This version of the Sentry SDK supports Angular ${ANGULAR_MINIMUM_VERSION} and above.`,
+          'Please consider upgrading your Angular version or downgrading the Sentry SDK.',
+        );
+    }
+  }
 }
