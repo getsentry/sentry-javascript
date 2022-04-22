3c46a1478d8b4f9ba15ff5c7fdb6f491838e6cfd
Katie Byers <lobsterkatie@gmail.com>
2022-04-22 08:10:38 -0700

    STASH changes to test ci script

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,15 +1,25 @@
-import { spawnSync } from 'child_process';
+import * as childProcess from 'child_process';
 import * as fs from 'fs';
-import { join } from 'path';
+import * as path from 'path';
 
-function run(cmd: string, cwd: string = '') {
-  const result = spawnSync(cmd, { shell: true, stdio: 'inherit', cwd: join(__dirname, '..', cwd || '') });
+// import * as jestConfig from '../jest/jest.config.js';
 
-  if (result.status !== 0) {
-    process.exit(result.status || undefined);
-  }
+/**
+ * Run the given shell command, piping the shell process's `stdin`, `stdout`, and `stderr` to that of the current process.
+ *
+ * @param cmd The command to run
+ * @param options Options (other than `stdio`) to pass to the underlying `execSync` command.
+ * @returns The child process's outpout on `stdout`
+ */
+function run(cmd: string, options?: childProcess.ExecSyncOptions) {
+  return childProcess.execSync(cmd, { stdio: 'inherit', ...options });
+
+  // if (result.status !== 0) {
+  //   process.exit(result.status || undefined);
+  // }
 }
 
+// const nodeMajorVersion = 8;
 const nodeMajorVersion = parseInt(process.version.split('.')[0].replace('v', ''), 10);
 
 // Ember tests require dependency changes for each set of tests, making them quite slow. To compensate for this, in CI
@@ -19,7 +29,7 @@ let ignorePackages = ['@sentry/ember'];
 // install legacy versions of third-party packages whose current versions don't support node 8 or 10, and skip testing
 // our own packages which don't support node 8 for various syntax or dependency reasons
 if (nodeMajorVersion <= 10) {
-  let legacyDependencies;
+  let legacyDependencies: string[];
 
   if (nodeMajorVersion === 8) {
     legacyDependencies = [
@@ -40,6 +50,36 @@ if (nodeMajorVersion <= 10) {
       '@sentry/nextjs',
       '@sentry/angular',
     ];
+
+    // The ts-jest config schema changed between v25 and v26. Because we're about to downgrade to v25, we have to adjust
+    // our config to match.
+
+    // Loading the existing jest config will error out unless it has an accompanying types file.
+    run('yarn tsc --allowJs --skipLibCheck --declaration --emitDeclarationOnly jest/jest.config.js');
+
+    // Because we're loading this partway through the script, we have to use `require`.
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const jestConfig = require('../jest/jest.config.js');
+
+    // We need to cast the new `astTransformers` value to `any` because it violates the ts-jest v26+ schema, on which
+    // the types file we generated above is based.
+    jestConfig.globals['ts-jest'].astTransformers = jestConfig.globals['ts-jest'].astTransformers.after as any;
+
+    // The rest of the fixes we need to make are more easily done on the stringified code
+    let code = `module.exports = ${JSON.stringify(jestConfig, null, 2)}`;
+
+    // When we imported the jest config above, all expressions it contained were evaluated. Specifically, the
+    // `process.cwd()` call was replaced with a hard-coded path, which will break as soon as the jest config needs to be
+    // used in a different directory than this one. Though it's a little brute-force-y, the easiest way to fix this is
+    // just to perform the substitution in reverse.
+    code = code.replace(`"rootDir": "${process.cwd()}"`, 'rootDir: process.cwd()');
+
+    // Finally, it seems older versions of ts-jest need the transformer to be in JS from the start (newer versions will
+    // transpile it on their own), so we need to both compile it and change the extension in the filename in our config.
+    run('yarn tsc --skipLibCheck jest/transformers/constReplacer.ts');
+    code = code.replace('constReplacer.ts', 'constReplacer.js');
+
+    fs.writeFileSync(path.resolve('jest/jest.config.js'), code);
 
     // This is a hack, to deal the fact that the browser-based tests fail under Node 8, because of a conflict buried
     // somewhere in the interaction between our current overall set of dependencies and the older versions of a small
