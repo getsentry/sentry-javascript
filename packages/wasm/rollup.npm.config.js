06106a6f93f783019f693cc2706804636e4d9f83
Katie Byers <lobsterkatie@gmail.com>
2022-04-26 21:41:36 -0700

    fix packages with bundles

--- packages/wasm/rollup.npm.config.js
+++ packages/wasm/rollup.npm.config.js
@@ -1,3 +1,8 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
 
-export default makeNPMConfigVariants(makeBaseNPMConfig());
+export default makeNPMConfigVariants(
+  makeBaseNPMConfig({
+    // packages with bundles have a different build directory structure
+    hasBundles: true,
+  }),
+);
