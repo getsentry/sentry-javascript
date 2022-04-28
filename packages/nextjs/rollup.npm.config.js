7f04d333deed87bbd43042a86993e8db41b650f5
Katie Byers <lobsterkatie@gmail.com>
2022-04-28 00:55:27 -0700

    feat(build): Add sucrase build foundations (#4992)
    This lays the initial groundwork for the new build process, by making the following additions:
    
    - `@rollup/plugin-sucrase`, which we will use to transpile TS, has been added as a dev dependency.
    - Functions have been added, similar to those which exist for CDN bundles, to generate the base rollup config and then turn it into a CJS version and an ESM version.
    - New `build:rollup` and `build:rollup:watch` commands have been added to each package's `package.json`.
    - A new `rollup.npm.config.js` file has been added to each package, generating a base config and modifying it as necessary given each package's specific needs.
    - A temporary set of CI jobs has been added, running tests against the sucrase build.
    
    Note that other than in tests, none of this code is yet being used - the actual switch from one build process to another will happen in a future PR.

---
+++ packages/nextjs/rollup.npm.config.js
@@ -0,0 +1,12 @@
+import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
+
+export default makeNPMConfigVariants(
+  makeBaseNPMConfig({
+    // We need to include `instrumentServer.ts` separately because it's only conditionally required, and so rollup
+    // doesn't automatically include it when calculating the module dependency tree.
+    entrypoints: ['src/index.server.ts', 'src/index.client.ts', 'src/utils/instrumentServer.ts'],
+    // prevent this nextjs code from ending up in our built package (this doesn't happen automatially because the name
+    // doesn't match an SDK dependency)
+    externals: ['next/router'],
+  }),
+);
