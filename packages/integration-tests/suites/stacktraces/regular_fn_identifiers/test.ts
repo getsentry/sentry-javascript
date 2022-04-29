e44bce89ae74d8699fd966b2e87e29a8b168b810
Onur Temizkan <onur@narval.co.uk>
2022-04-29 10:00:44 +0100

    Use new Transports API in stacktrace integration tests.

--- packages/integration-tests/suites/stacktraces/regular_fn_identifiers/test.ts
+++ packages/integration-tests/suites/stacktraces/regular_fn_identifiers/test.ts
@@ -1,14 +1,15 @@
 import { expect } from '@playwright/test';
+import { Event } from '@sentry/types';
 
 import { sentryTest } from '../../../utils/fixtures';
-import { getSentryRequest } from '../../../utils/helpers';
+import { getFirstSentryEnvelopeRequest } from '../../../utils/helpers';
 
 sentryTest(
   'should parse function identifiers correctly',
   async ({ getLocalTestPath, page, runInChromium, runInFirefox, runInWebkit }) => {
     const url = await getLocalTestPath({ testDir: __dirname });
 
-    const eventData = await getSentryRequest(page, url);
+    const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
     const frames = eventData.exception?.values?.[0].stacktrace?.frames;
 
     runInChromium(() => {
@@ -55,7 +56,7 @@ sentryTest(
 sentryTest('should not add any part of the function identifier inside filename', async ({ getLocalTestPath, page }) => {
   const url = await getLocalTestPath({ testDir: __dirname });
 
-  const eventData = await getSentryRequest(page, url);
+  const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
 
   expect(eventData.exception?.values?.[0].stacktrace?.frames).toMatchObject(
     Array(8).fill({ filename: expect.stringMatching(/^file:\/?/) }),
