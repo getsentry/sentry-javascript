a3f3974ea1b3b0965700415becb3efe3a7cbf1c3
Katie Byers <lobsterkatie@gmail.com>
2022-05-06 06:50:55 -0700

    add polyfills

---
+++ packages/utils/src/buildPolyfills/_interopNamespaceDefaultOnly.ts
@@ -0,0 +1,24 @@
+import { RequireResult } from './types';
+
+/**
+ * Wrap a module in an object, as the value under the key `default`.
+ *
+ * Adapted from Rollup (https://github.com/rollup/rollup)
+ *
+ * @param requireResult The result of calling `require` on a module
+ * @returns An object containing the key-value pair (`default`, `requireResult`)
+ */
+export function _interopNamespaceDefaultOnly(requireResult: RequireResult): RequireResult {
+  return {
+    __proto__: null,
+    default: requireResult,
+  };
+}
+
+// Rollup version
+// function _interopNamespaceDefaultOnly(e) {
+//   return {
+//     __proto__: null,
+//     'default': e
+//   };
+// }
