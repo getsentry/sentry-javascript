8310ca752dd008a8ef57eca11070e39dcb7028ac
Abhijeet Prasad <aprasad@sentry.io>
2022-05-12 14:58:23 -0400

    spelling

--- packages/utils/src/baggage.ts
+++ packages/utils/src/baggage.ts
@@ -8,11 +8,11 @@ export type BaggageObj = Partial<Record<AllowedBaggageKeys, string> & Record<str
  * It is expected that users interact with baggage using the helpers methods:
  * `createBaggage`, `getBaggageValue`, and `setBaggageValue`.
  *
- * Internally, the baggage data structure is a tuple of length 2, seperating baggage values
+ * Internally, the baggage data structure is a tuple of length 2, separating baggage values
  * based on if they are related to Sentry or not. If the baggage values are
  * set/used by sentry, they will be stored in an object to be easily accessed.
  * If they are not, they are kept as a string to be only accessed when serialized
- * at baggage propogation time.
+ * at baggage propagation time.
  */
 export type Baggage = [BaggageObj, string];
 
