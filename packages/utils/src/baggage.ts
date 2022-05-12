0a2811dba19be8275c8be32b4fd4f8ebd7d38187
Abhijeet Prasad <aprasad@sentry.io>
2022-05-12 11:50:15 -0400

    add tests

--- packages/utils/src/baggage.ts
+++ packages/utils/src/baggage.ts
@@ -1,7 +1,14 @@
 export type AllowedBaggageKeys = 'environment' | 'release'; // TODO: Add remaining allowed baggage keys | 'transaction' | 'userid' | 'usersegment';
-export type Baggage = Partial<Record<AllowedBaggageKeys, string> & Record<string, string>>;
+export type BaggageObj = Partial<Record<AllowedBaggageKeys, string> & Record<string, string>>;
+export type Baggage = [BaggageObj, string];
 
 export const BAGGAGE_HEADER_NAME = 'baggage';
+
+export const SENTRY_BAGGAGE_KEY_PREFIX = 'sentry-';
+
+export const SENTRY_BAGGAGE_KEY_PREFIX_REGEX = /^sentry-/;
+
+// baggage = sentry-environment=prod;my-info=true;
 
 /**
  * Max length of a serialized baggage string
@@ -11,44 +18,46 @@ export const BAGGAGE_HEADER_NAME = 'baggage';
 export const MAX_BAGGAGE_STRING_LENGTH = 8192;
 
 /** Create an instance of Baggage */
-export function createBaggage(initItems: Baggage): Baggage {
-  return { ...initItems };
+export function createBaggage(initItems: BaggageObj, baggageString: string = ''): Baggage {
+  return [{ ...initItems }, baggageString];
 }
 
 /** Add a value to baggage */
-export function getBaggageValue(baggage: Baggage, key: keyof Baggage): Baggage[keyof Baggage] {
-  return baggage[key];
+export function getBaggageValue(baggage: Baggage, key: keyof BaggageObj): BaggageObj[keyof BaggageObj] {
+  return baggage[0][key];
 }
 
 /** Add a value to baggage */
-export function setBaggageValue(baggage: Baggage, key: keyof Baggage, value: Baggage[keyof Baggage]): void {
-  baggage[key] = value;
+export function setBaggageValue(baggage: Baggage, key: keyof BaggageObj, value: BaggageObj[keyof BaggageObj]): void {
+  baggage[0][key] = value;
 }
-
-/** remove a value from baggage */
-// TODO: Is this even useful?
-// export function removeBaggageValue(baggage: Baggage, key: keyof Baggage): void {
-//   // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-//   delete baggage[key];
-// }
 
 /** Serialize a baggage object */
 export function serializeBaggage(baggage: Baggage): string {
-  return Object.keys(baggage).reduce((prev, key) => {
-    const newVal = `${prev};${key}=${baggage[key as keyof Baggage]}`;
+  return Object.keys(baggage[0]).reduce((prev, key) => {
+    const baggageEntry = `${SENTRY_BAGGAGE_KEY_PREFIX}${key}=${baggage[0][key as keyof BaggageObj]}`;
+    const newVal = prev === '' ? baggageEntry : `${prev},${baggageEntry}`;
     return newVal.length > MAX_BAGGAGE_STRING_LENGTH ? prev : newVal;
-  }, '');
+  }, baggage[1]);
 }
 
 /** Parse a baggage header to a string */
 export function parseBaggageString(baggageString: string): Baggage {
-  // TODO: Should we check validity with regex? How much should we check for malformed baggage keys?
-  // Perhaps we shouldn't worry about this being used in the frontend, so bundle size isn't that much of a concern
-  return baggageString.split(';').reduce((prevBaggage, curr) => {
-    const [key, val] = curr.split('=');
-    return {
-      ...prevBaggage,
-      [key]: val,
-    };
-  }, {} as Baggage);
+  return baggageString.split(',').reduce(
+    ([baggageObj, baggageString], curr) => {
+      const [key, val] = curr.split('=');
+      if (SENTRY_BAGGAGE_KEY_PREFIX_REGEX.test(key)) {
+        return [
+          {
+            ...baggageObj,
+            [key.split('-')[1]]: val,
+          },
+          baggageString,
+        ];
+      } else {
+        return [baggageObj, baggageString === '' ? curr : `${baggageString},${curr}`];
+      }
+    },
+    [{}, ''],
+  );
 }
