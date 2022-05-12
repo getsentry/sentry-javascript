1badac1745ef16517628d895a354d1bd9a1dfabd
Katie Byers <lobsterkatie@gmail.com>
2022-05-12 01:25:53 -0700

    ref(node): Improve Node manual test logging (#5088)
    This makes some small changes to the logs emitted by the manual tests in the node package, to make it a little cleaner and easier to see what's what.
    
    Included changes:
    - Use `colorize` function from release health tests in all test suites.
    - Capture and display console logs in express test.
    - Standardize success message across test suites.
    - Use whitespace to separate each suite's logs from the next.

--- packages/node/test/manual/release-health/runner.js
+++ packages/node/test/manual/release-health/runner.js
@@ -1,21 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
-
-const COLOR_RESET = '\x1b[0m';
-const COLORS = {
-  green: '\x1b[32m',
-  red: '\x1b[31m',
-  yellow: '\x1b[33m',
-};
-
-const colorize = (str, color) => {
-  if (!(color in COLORS)) {
-    throw new Error(`Unknown color. Available colors: ${Object.keys(COLORS).join(', ')}`);
-  }
-
-  return `${COLORS[color]}${str}${COLOR_RESET}`;
-};
+const { colorize } = require('../colorize');
 
 const scenariosDirs = ['session-aggregates', 'single-session'];
 const scenarios = [];
