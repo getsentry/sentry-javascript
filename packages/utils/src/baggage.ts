0068cb54cfe921021ea0cdb16d0edca51be96314
Abhijeet Prasad <aprasad@sentry.io>
2022-05-12 14:55:38 -0400

    description

--- packages/utils/src/baggage.ts
+++ packages/utils/src/baggage.ts
@@ -1,5 +1,19 @@
 export type AllowedBaggageKeys = 'environment' | 'release'; // TODO: Add remaining allowed baggage keys | 'transaction' | 'userid' | 'usersegment';
 export type BaggageObj = Partial<Record<AllowedBaggageKeys, string> & Record<string, string>>;
+
+/**
+ * The baggage data structure represents key,value pairs based on the baggage
+ * spec: https://www.w3.org/TR/baggage
+ *
+ * It is expected that users interact with baggage using the helpers methods:
+ * `createBaggage`, `getBaggageValue`, and `setBaggageValue`.
+ *
+ * Internally, the baggage data structure is a tuple of length 2, seperating baggage values
+ * based on if they are related to Sentry or not. If the baggage values are
+ * set/used by sentry, they will be stored in an object to be easily accessed.
+ * If they are not, they are kept as a string to be only accessed when serialized
+ * at baggage propogation time.
+ */
 export type Baggage = [BaggageObj, string];
 
 export const BAGGAGE_HEADER_NAME = 'baggage';
@@ -7,8 +21,6 @@ export const BAGGAGE_HEADER_NAME = 'baggage';
 export const SENTRY_BAGGAGE_KEY_PREFIX = 'sentry-';
 
 export const SENTRY_BAGGAGE_KEY_PREFIX_REGEX = /^sentry-/;
-
-// baggage = sentry-environment=prod;my-info=true;
 
 /**
  * Max length of a serialized baggage string
@@ -35,7 +47,8 @@ export function setBaggageValue(baggage: Baggage, key: keyof BaggageObj, value: 
 /** Serialize a baggage object */
 export function serializeBaggage(baggage: Baggage): string {
   return Object.keys(baggage[0]).reduce((prev, key) => {
-    const baggageEntry = `${SENTRY_BAGGAGE_KEY_PREFIX}${key}=${baggage[0][key as keyof BaggageObj]}`;
+    const val = baggage[0][key as keyof BaggageObj] as string;
+    const baggageEntry = `${SENTRY_BAGGAGE_KEY_PREFIX}${encodeURIComponent(key)}=${encodeURIComponent(val)}`;
     const newVal = prev === '' ? baggageEntry : `${prev},${baggageEntry}`;
     return newVal.length > MAX_BAGGAGE_STRING_LENGTH ? prev : newVal;
   }, baggage[1]);
@@ -47,10 +60,11 @@ export function parseBaggageString(baggageString: string): Baggage {
     ([baggageObj, baggageString], curr) => {
       const [key, val] = curr.split('=');
       if (SENTRY_BAGGAGE_KEY_PREFIX_REGEX.test(key)) {
+        const baggageKey = decodeURIComponent(key.split('-')[1]);
         return [
           {
             ...baggageObj,
-            [key.split('-')[1]]: val,
+            [baggageKey]: decodeURIComponent(val),
           },
           baggageString,
         ];
