79766c09304867ccc61bd7b0f1cf70e4f7e4e21d
Katie Byers <lobsterkatie@gmail.com>
2022-05-13 22:19:44 -0700

    adjust npm rollup config to become a package with bundles

--- packages/serverless/rollup.npm.config.js
+++ packages/serverless/rollup.npm.config.js
@@ -3,5 +3,7 @@ import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js'
 export default makeNPMConfigVariants(
   makeBaseNPMConfig({
     entrypoints: ['src/index.ts', 'src/awslambda-auto.ts'],
+    // packages with bundles have a different build directory structure
+    hasBundles: true,
   }),
 );
