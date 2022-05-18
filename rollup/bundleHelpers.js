450161cc0380bda2b05b334bb6f739c9021a9fd8
Katie Byers <lobsterkatie@gmail.com>
2022-05-17 20:41:34 -0700

    add option for arbitrary package-specific config

--- rollup/bundleHelpers.js
+++ rollup/bundleHelpers.js
@@ -21,7 +21,7 @@ import {
 import { mergePlugins } from './utils';
 
 export function makeBaseBundleConfig(options) {
-  const { bundleType, entrypoints, jsVersion, licenseTitle, outputFileBase } = options;
+  const { bundleType, entrypoints, jsVersion, licenseTitle, outputFileBase, packageSpecificConfig } = options;
 
   const nodeResolvePlugin = makeNodeResolvePlugin();
   const sucrasePlugin = makeSucrasePlugin();
@@ -113,7 +113,7 @@ export function makeBaseBundleConfig(options) {
     node: nodeBundleConfig,
   };
 
-  return deepMerge(sharedBundleConfig, bundleTypeConfigMap[bundleType], {
+  return deepMerge.all([sharedBundleConfig, bundleTypeConfigMap[bundleType], packageSpecificConfig || {}], {
     // Plugins have to be in the correct order or everything breaks, so when merging we have to manually re-order them
     customMerge: key => (key === 'plugins' ? mergePlugins : undefined),
   });
