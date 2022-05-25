c7c6a5b1dde69515e92dbbc250e2624a6769b0e0
Luca Forstner <luca.forstner@sentry.io>
2022-05-25 09:22:03 +0000

    fix(utils): Fix infinite recursion in `dropUndefinedKeys`

--- packages/utils/src/object.ts
+++ packages/utils/src/object.ts
@@ -4,6 +4,7 @@ import { WrappedFunction } from '@sentry/types';
 
 import { htmlTreeAsString } from './browser';
 import { isElement, isError, isEvent, isInstanceOf, isPlainObject, isPrimitive } from './is';
+import { memoBuilder, MemoFunc } from './memo';
 import { truncate } from './string';
 
 /**
@@ -207,18 +208,32 @@ export function extractExceptionKeysForMessage(exception: Record<string, unknown
  * Works recursively on objects and arrays.
  */
 export function dropUndefinedKeys<T>(val: T): T {
+  return _dropUndefinedKeys(val, memoBuilder());
+}
+
+function _dropUndefinedKeys<T>(val: T, memo: MemoFunc): T {
+  const [memoize] = memo;
+
   if (isPlainObject(val)) {
+    if (memoize(val)) {
+      return val;
+    }
     const rv: { [key: string]: any } = {};
     for (const key of Object.keys(val)) {
       if (typeof val[key] !== 'undefined') {
-        rv[key] = dropUndefinedKeys(val[key]);
+        rv[key] = _dropUndefinedKeys(val[key], memo);
       }
     }
     return rv as T;
   }
 
   if (Array.isArray(val)) {
-    return (val as any[]).map(dropUndefinedKeys) as any;
+    if (memoize(val)) {
+      return val;
+    }
+    return (val as any[]).map(item => {
+      return _dropUndefinedKeys(item, memo);
+    }) as any;
   }
 
   return val;
