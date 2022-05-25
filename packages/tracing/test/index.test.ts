e55c23bc552f9a81150e884e6b01f17fd51ab331
Luca Forstner <luca.forstner@sentry.io>
2022-05-25 14:56:19 +0000

    Revert "Skip browsertracing in test"
    This reverts commit e09921e10f8ef45450b3dd5547ff406d40fa7cb7.

--- packages/tracing/test/index.test.ts
+++ packages/tracing/test/index.test.ts
@@ -12,11 +12,6 @@ describe('index', () => {
   describe('Integrations', () => {
     it('is exported correctly', () => {
       Object.keys(Integrations).forEach(key => {
-        // Skip BrowserTracing because it doesn't have a static id field.
-        if (key !== 'BrowserTracing') {
-          return;
-        }
-
         expect(Integrations[key as keyof typeof Integrations].id).toStrictEqual(expect.any(String));
       });
     });
