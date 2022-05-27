37691cdcdfb4fe469fb3803c234ed0216b26411c
Luca Forstner <luca.forstner@sentry.io>
2022-05-27 14:24:22 +0000

    Add integration tests

---
+++ packages/integration-tests/suites/public-api/startTransaction/circular_data/test.ts
@@ -0,0 +1,26 @@
+import { expect } from '@playwright/test';
+import { Event } from '@sentry/types';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest } from '../../../../utils/helpers';
+
+sentryTest('should be able to handle circular data', async ({ getLocalTestPath, page }) => {
+  const url = await getLocalTestPath({ testDir: __dirname });
+  const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+
+  expect(eventData.type).toBe('transaction');
+  expect(eventData.transaction).toBe('circular_object_test_transaction');
+
+  expect(eventData.contexts).toMatchObject({
+    trace: {
+      data: { lays: { contains: { lays: { contains: '[Circular ~]' } } } },
+    },
+  });
+
+  expect(eventData?.spans?.[0]).toMatchObject({
+    data: { lays: { contains: '[Circular ~]' } },
+    op: 'circular_object_test_span',
+  });
+
+  await new Promise(resolve => setTimeout(resolve, 2000));
+});
