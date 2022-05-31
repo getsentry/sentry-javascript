6401ebab8dffb09ae39f56def1913840a5e65c2e
Luca Forstner <luca.forstner@sentry.io>
2022-05-31 13:39:50 +0000

    Add documentation

--- packages/types/src/globals.ts
+++ packages/types/src/globals.ts
@@ -1,3 +1,10 @@
+/*
+ * This file defines flags and constants that can be modified during compile time in order to facilitate tree shaking
+ * for users.
+ *
+ * "Magic strings" like `__SENTRY_DEBUG__` must be replaced with actual values or safe statements during our build process.
+ */
+
 declare global {
   const __DEBUG_BUILD__: boolean;
 }
