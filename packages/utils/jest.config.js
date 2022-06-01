d68d3e74444e4be4aabdc8d3223f87211e304710
Luca Forstner <luca.forstner@sentry.io>
2022-06-01 10:29:56 +0000

    Remove unnecessary jest config used for debugging

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -6,10 +6,4 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'ts-jest',
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.test.json',
-    },
-    __DEBUG_BUILD__: true,
-  },
 };
