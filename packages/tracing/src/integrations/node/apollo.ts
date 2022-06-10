cafd6cb7e529ebecfa5f96d634cffef42ab210ed
Onur Temizkan <onur@narval.co.uk>
2022-06-10 10:20:06 +0100

    feat(tracing): GraphQL and Apollo Integrations (#3953)

---
+++ packages/tracing/src/integrations/node/apollo.ts
@@ -0,0 +1,101 @@
+import { Hub } from '@sentry/hub';
+import { EventProcessor, Integration } from '@sentry/types';
+import { fill, isThenable, loadModule, logger } from '@sentry/utils';
+
+type ApolloResolverGroup = {
+  [key: string]: () => unknown;
+};
+
+type ApolloModelResolvers = {
+  [key: string]: ApolloResolverGroup;
+};
+
+/** Tracing integration for Apollo */
+export class Apollo implements Integration {
+  /**
+   * @inheritDoc
+   */
+  public static id: string = 'Apollo';
+
+  /**
+   * @inheritDoc
+   */
+  public name: string = Apollo.id;
+
+  /**
+   * @inheritDoc
+   */
+  public setupOnce(_: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void {
+    const pkg = loadModule<{
+      ApolloServerBase: {
+        prototype: {
+          constructSchema: () => unknown;
+        };
+      };
+    }>('apollo-server-core');
+
+    if (!pkg) {
+      logger.error('Apollo Integration was unable to require apollo-server-core package.');
+      return;
+    }
+
+    /**
+     * Iterate over resolvers of the ApolloServer instance before schemas are constructed.
+     */
+    fill(pkg.ApolloServerBase.prototype, 'constructSchema', function (orig: () => unknown) {
+      return function (this: { config: { resolvers: ApolloModelResolvers[] } }) {
+        const resolvers = Array.isArray(this.config.resolvers) ? this.config.resolvers : [this.config.resolvers];
+
+        this.config.resolvers = resolvers.map(model => {
+          Object.keys(model).forEach(resolverGroupName => {
+            Object.keys(model[resolverGroupName]).forEach(resolverName => {
+              if (typeof model[resolverGroupName][resolverName] !== 'function') {
+                return;
+              }
+
+              wrapResolver(model, resolverGroupName, resolverName, getCurrentHub);
+            });
+          });
+
+          return model;
+        });
+
+        return orig.call(this);
+      };
+    });
+  }
+}
+
+/**
+ * Wrap a single resolver which can be a parent of other resolvers and/or db operations.
+ */
+function wrapResolver(
+  model: ApolloModelResolvers,
+  resolverGroupName: string,
+  resolverName: string,
+  getCurrentHub: () => Hub,
+): void {
+  fill(model[resolverGroupName], resolverName, function (orig: () => unknown | Promise<unknown>) {
+    return function (this: unknown, ...args: unknown[]) {
+      const scope = getCurrentHub().getScope();
+      const parentSpan = scope?.getSpan();
+      const span = parentSpan?.startChild({
+        description: `${resolverGroupName}.${resolverName}`,
+        op: 'db.graphql.apollo',
+      });
+
+      const rv = orig.call(this, ...args);
+
+      if (isThenable(rv)) {
+        return rv.then((res: unknown) => {
+          span?.finish();
+          return res;
+        });
+      }
+
+      span?.finish();
+
+      return rv;
+    };
+  });
+}
---
+++ packages/tracing/src/integrations/node/graphql.ts
@@ -0,0 +1,59 @@
+import { Hub } from '@sentry/hub';
+import { EventProcessor, Integration } from '@sentry/types';
+import { fill, isThenable, loadModule, logger } from '@sentry/utils';
+
+/** Tracing integration for graphql package */
+export class GraphQL implements Integration {
+  /**
+   * @inheritDoc
+   */
+  public static id: string = 'GraphQL';
+
+  /**
+   * @inheritDoc
+   */
+  public name: string = GraphQL.id;
+
+  /**
+   * @inheritDoc
+   */
+  public setupOnce(_: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void {
+    const pkg = loadModule<{
+      [method: string]: (...args: unknown[]) => unknown;
+    }>('graphql/execution/execute.js');
+
+    if (!pkg) {
+      logger.error('GraphQL Integration was unable to require graphql/execution package.');
+      return;
+    }
+
+    fill(pkg, 'execute', function (orig: () => void | Promise<unknown>) {
+      return function (this: unknown, ...args: unknown[]) {
+        const scope = getCurrentHub().getScope();
+        const parentSpan = scope?.getSpan();
+
+        const span = parentSpan?.startChild({
+          description: 'execute',
+          op: 'db.graphql',
+        });
+
+        scope?.setSpan(span);
+
+        const rv = orig.call(this, ...args);
+
+        if (isThenable(rv)) {
+          return rv.then((res: unknown) => {
+            span?.finish();
+            scope?.setSpan(parentSpan);
+
+            return res;
+          });
+        }
+
+        span?.finish();
+        scope?.setSpan(parentSpan);
+        return rv;
+      };
+    });
+  }
+}
