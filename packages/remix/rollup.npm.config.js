45818f3348ce8dc30d22743b2093f5a2aebb1ce9
Onur Temizkan <onur@narval.co.uk>
2022-06-21 18:44:42 +0100

    feat(remix): Add Remix server SDK (#5269)
    Adds server side SDK for error tracking / performance tracing of Remix.
    
    - Uses Node SDK underneath.
    - For tracing, monkey-patches `createRequestHandler` from `@remix-run/server-runtime` which apparently is used by all server-side adapters of Remix.
    - `action` and `loader` functions are patched as parameters of `createRequestHandler`.
    
    Co-authored-by: Abhijeet Prasad <aprasad@sentry.io>

--- packages/remix/rollup.npm.config.js
+++ packages/remix/rollup.npm.config.js
@@ -2,7 +2,6 @@ import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js'
 
 export default makeNPMConfigVariants(
   makeBaseNPMConfig({
-    // Todo: Replace with -> ['src/index.server.ts', 'src/index.client.tsx'],
-    entrypoints: 'src/index.ts',
+    entrypoints: ['src/index.server.ts', 'src/index.client.tsx'],
   }),
 );
