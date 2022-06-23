362fae2a62f3ef1a0698a800f484f23ddcfd2bf7
Abhijeet Prasad <aprasad@sentry.io>
2022-06-23 16:21:12 -0400

    chore: Add eslint rule to guard against optional chaining in browser SDKs

--- packages/browser/.eslintrc.js
+++ packages/browser/.eslintrc.js
@@ -4,4 +4,14 @@ module.exports = {
   },
   ignorePatterns: ['test/integration/**', 'src/loader.js'],
   extends: ['../../.eslintrc.js'],
+  rules: {
+    // Disallow optional chaining in browser SDKs
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'OptionalMemberExpression',
+        message: 'Optional Chaining is disallowed in packages that could be used by Browser SDKs',
+      },
+    ],
+  },
 };
--- packages/hub/.eslintrc.js
+++ packages/hub/.eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
+  rules: {
+    // Disallow optional chaining in browser SDKs
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'OptionalMemberExpression',
+        message: 'Optional Chaining is disallowed in packages that could be used by Browser SDKs',
+      },
+    ],
+  },
 };
