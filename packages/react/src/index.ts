50409a7b108e52b1d1980363d3bd176d21b513ff
Matthew Peveler <matt@popsql.com>
2022-06-23 23:23:14 +0000

    feat(react): expose FallbackRender as top-level type
    Signed-off-by: Matthew Peveler <matt@popsql.com>

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,7 +2,7 @@ export * from '@sentry/browser';
 
 export { init } from './sdk';
 export { Profiler, withProfiler, useProfiler } from './profiler';
-export { ErrorBoundary, withErrorBoundary } from './errorboundary';
+export { ErrorBoundary, FallbackRender, withErrorBoundary } from './errorboundary';
 export { createReduxEnhancer } from './redux';
 export { reactRouterV3Instrumentation } from './reactrouterv3';
 export { reactRouterV4Instrumentation, reactRouterV5Instrumentation, withSentryRouting } from './reactrouter';
