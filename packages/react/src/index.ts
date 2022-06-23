3846c36b2677cfb3d07464f026a06e3bbfc95925
Matthew Peveler <matt@popsql.com>
2022-06-23 23:37:11 +0000

    Update packages/react/src/index.ts
    Co-authored-by: Abhijeet Prasad <devabhiprasad@gmail.com>

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,7 +2,8 @@ export * from '@sentry/browser';
 
 export { init } from './sdk';
 export { Profiler, withProfiler, useProfiler } from './profiler';
-export { ErrorBoundary, FallbackRender, withErrorBoundary } from './errorboundary';
+export type { FallbackRender } from './errorboundary';
+export { ErrorBoundary, withErrorBoundary } from './errorboundary';
 export { createReduxEnhancer } from './redux';
 export { reactRouterV3Instrumentation } from './reactrouterv3';
 export { reactRouterV4Instrumentation, reactRouterV5Instrumentation, withSentryRouting } from './reactrouter';
