3abfb2d07fd45398393330cf30cb6c0c8e8a415a
Matthew Peveler <matt.peveler@gmail.com>
2022-06-24 14:17:09 -0400

    feat(react): expose FallbackRender as top-level type (#5307)
    This PR exposes the `FallbackRender` type at the root level of `@sentry/react` package. Currently, to use the type and so get type info on the passed error object., a project I work on does:
    
    ```ts
    import { FallbackRender } from '@sentry/react/types/errorboundary.d';
    // v6: import { FallbackRender } from '@sentry/react/dist/errorboundary';
    
    export const ErrorOverlay: FallbackRender = (error) => { /* component code */ });
    ```
    
    which feels wrong, as reaching into the package internals is clearly asking for unannounced breaking changes (as seen going from v6->v7).
    
    Signed-off-by: Matthew Peveler <matt@popsql.com>
    
    Co-authored-by: Abhijeet Prasad <devabhiprasad@gmail.com>

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,6 +2,7 @@ export * from '@sentry/browser';
 
 export { init } from './sdk';
 export { Profiler, withProfiler, useProfiler } from './profiler';
+export type { FallbackRender } from './errorboundary';
 export { ErrorBoundary, withErrorBoundary } from './errorboundary';
 export { createReduxEnhancer } from './redux';
 export { reactRouterV3Instrumentation } from './reactrouterv3';
