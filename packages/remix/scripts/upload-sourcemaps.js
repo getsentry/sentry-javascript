e708c2a965c5f4ad0d535e2f48f21f0ba1fc8ed2
Onur Temizkan <onur@narval.co.uk>
2022-06-27 14:42:21 +0100

    Use `proposeVersion` if release is not provided.

--- packages/remix/scripts/upload-sourcemaps.js
+++ packages/remix/scripts/upload-sourcemaps.js
@@ -1,25 +1,22 @@
 #! /usr/bin/env node
-/* eslint-disable no-console */
 const SentryCli = require('@sentry/cli');
 const argv = require('yargs/yargs')(process.argv.slice(2))
-  .option('release', { type: 'string', describe: 'The release number', demandOption: true })
+  .option('release', { type: 'string', describe: 'The release number' })
   .option('urlPrefix', { type: 'string', describe: 'The url prefix for the sourcemaps' })
   .option('buildPath', { type: 'string', describe: 'The path to the build directory' })
-  .usage('Usage: $0 --release RELEASE [--urlPrefix URL_PREFIX] [--buildPath BUILD_PATH]').argv;
-
-const RELEASE = argv.release;
-
-if (!RELEASE) {
-  console.error('No release provided.');
-  process.exit(1);
-}
-
-const URL_PREFIX = argv.urlPrefix || '~/build/';
-const BUILD_PATH = argv.buildPath || 'public/build';
+  .usage(
+    'Usage: $0 [--release RELEASE] [--urlPrefix URL_PREFIX] [--buildPath BUILD_PATH] \n\n' +
+      'If you need a more advanced configuration, you can use `sentry-cli` instead.\n' +
+      'https://github.com/getsentry/sentry-cli',
+  ).argv;
 
 const sentry = new SentryCli();
 
 async function createRelease() {
+  const RELEASE = argv.release || (await sentry.releases.proposeVersion());
+  const URL_PREFIX = argv.urlPrefix || '~/build/';
+  const BUILD_PATH = argv.buildPath || 'public/build';
+
   await sentry.releases.new(RELEASE);
 
   await sentry.releases.uploadSourceMaps(RELEASE, {
