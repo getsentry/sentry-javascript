0dbbac61fbcc8b2d9636d5d9fdef5be9cd93fd41
Luca Forstner <luca.forstner@sentry.io>
2022-07-08 12:27:11 +0200

    feat(vue): Add transaction source to VueRouter instrumentation (#5381)

--- packages/vue/src/router.ts
+++ packages/vue/src/router.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { captureException } from '@sentry/browser';
-import { Transaction, TransactionContext } from '@sentry/types';
+import { Transaction, TransactionContext, TransactionSource } from '@sentry/types';
 
 export type VueRouterInstrumentation = <T extends Transaction>(
   startTransaction: (context: TransactionContext) => T | undefined,
@@ -8,14 +7,24 @@ export type VueRouterInstrumentation = <T extends Transaction>(
   startTransactionOnLocationChange?: boolean,
 ) => void;
 
-// This is not great, but kinda necessary to make it work with VueRouter@3 and VueRouter@4 at the same time.
-type Route = {
-  params: any;
-  query: any;
-  name?: any;
-  path: any;
-  matched: any[];
+// The following type is an intersection of the Route type from VueRouter v2, v3, and v4.
+// This is not great, but kinda necessary to make it work with all versions at the same time.
+export type Route = {
+  /** Unparameterized URL */
+  path: string;
+  /**
+   * Query params (keys map to null when there is no value associated, e.g. "?foo" and to an array when there are
+   * multiple query params that have the same key, e.g. "?foo&foo=bar")
+   */
+  query: Record<string, string | null | (string | null)[]>;
+  /** Route name (VueRouter provides a way to give routes individual names) */
+  name?: string | symbol | null | undefined;
+  /** Evaluated parameters */
+  params: Record<string, string | string[]>;
+  /** All the matched route objects as defined in VueRouter constructor */
+  matched: { path: string }[];
 };
+
 interface VueRouter {
   onError: (fn: (err: Error) => void) => void;
   beforeEach: (fn: (to: Route, from: Route, next: () => void) => void) => void;
@@ -39,8 +48,10 @@ export function vueRouterInstrumentation(router: VueRouter): VueRouterInstrument
       // https://router.vuejs.org/api/#router-start-location
       // https://next.router.vuejs.org/api/#start-location
 
-      // Vue2 - null
-      // Vue3 - undefined
+      // from.name:
+      // - Vue 2: null
+      // - Vue 3: undefined
+      // hence only '==' instead of '===', because `undefined == null` evaluates to `true`
       const isPageLoadNavigation = from.name == null && from.matched.length === 0;
 
       const tags = {
@@ -51,23 +62,38 @@ export function vueRouterInstrumentation(router: VueRouter): VueRouterInstrument
         query: to.query,
       };
 
+      // Determine a name for the routing transaction and where that name came from
+      let transactionName: string = to.path;
+      let transactionSource: TransactionSource = 'url';
+      if (to.name) {
+        transactionName = to.name.toString();
+        transactionSource = 'custom';
+      } else if (to.matched[0] && to.matched[0].path) {
+        transactionName = to.matched[0].path;
+        transactionSource = 'route';
+      }
+
       if (startTransactionOnPageLoad && isPageLoadNavigation) {
         startTransaction({
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          name: (to.name && to.name.toString()) || to.path,
+          name: transactionName,
           op: 'pageload',
           tags,
           data,
+          metadata: {
+            source: transactionSource,
+          },
         });
       }
 
       if (startTransactionOnLocationChange && !isPageLoadNavigation) {
         startTransaction({
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          name: (to.name && to.name.toString()) || (to.matched[0] && to.matched[0].path) || to.path,
+          name: transactionName,
           op: 'navigation',
           tags,
           data,
+          metadata: {
+            source: transactionSource,
+          },
         });
       }
 
