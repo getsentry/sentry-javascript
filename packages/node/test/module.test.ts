e9573b01f3db252b295d2c0da5377ed55e16182b
Tim Fish <tim@timfish.uk>
2022-07-27 23:42:54 +0100

    Restore filename

--- packages/node/test/module.test.ts
+++ packages/node/test/module.test.ts
@@ -1,15 +1,30 @@
 import { getModule } from '../src/module';
 
+function reaplaceFilename(fn: () => void, filename: string) {
+  const prevFilename = require.main?.filename;
+  if (require.main?.filename) {
+    require.main.filename = filename;
+  }
+
+  try {
+    fn();
+  } finally {
+    if (require.main && prevFilename) {
+      require.main.filename = prevFilename;
+    }
+  }
+}
+
 describe('getModule', () => {
   test('Windows', async () => {
-    (require.main as any) = { filename: 'C:\\Users\\Tim\\app.js' };
-
-    expect(getModule('C:\\Users\\users\\Tim\\Desktop\\node_modules\\module.js')).toEqual('module');
+    reaplaceFilename(() => {
+      expect(getModule('C:\\Users\\users\\Tim\\Desktop\\node_modules\\module.js')).toEqual('module');
+    }, 'C:\\Users\\Tim\\app.js');
   });
 
   test('POSIX', async () => {
-    (require.main as any) = { filename: '/Users/Tim/app.js' };
-
-    expect(getModule('/Users/users/Tim/Desktop/node_modules/module.js')).toEqual('module');
+    reaplaceFilename(() => {
+      expect(getModule('/Users/users/Tim/Desktop/node_modules/module.js')).toEqual('module');
+    }, '/Users/Tim/app.js');
   });
 });
