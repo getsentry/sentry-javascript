cb925e38f2f49cd621dccff14f434b275d9829ec
Onur Temizkan <onur@narval.co.uk>
2022-08-02 16:41:43 +0100

    fix(browser): Set `:` as a part of gecko protocol regex group. (#4153)
    Fixes: #4138
    
    The original issue about `file` keyword also happens for everything in that group, such as `http`, `blob` and so on. And the problem originates from : `.*` between that group and `:`. It seems that `:` follows those keywords without anything in between, as far as I have seen from the tests.
    
    Removing that from the regex solved the issue without breaking any tests other than `safari-extension` and `safari-web-extension`, which are special-cased in https://github.com/getsentry/sentry-javascript/blob/d2e0cc4d683364dc500a681631b5be7df40ffec6/packages/browser/src/stack-parsers.ts#L160, and adding those two to the matching group also solved the issue of an extra `:` coming from https://github.com/getsentry/sentry-javascript/blob/d2e0cc4d683364dc500a681631b5be7df40ffec6/packages/browser/src/stack-parsers.ts#L165.

---
+++ packages/integration-tests/suites/stacktraces/regular_fn_identifiers/test.ts
@@ -0,0 +1,68 @@
+import { expect } from '@playwright/test';
+import { Event } from '@sentry/types';
+
+import { sentryTest } from '../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest } from '../../../utils/helpers';
+
+sentryTest(
+  'should parse function identifiers correctly',
+  async ({ getLocalTestPath, page, runInChromium, runInFirefox, runInWebkit }) => {
+    const url = await getLocalTestPath({ testDir: __dirname });
+
+    const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+    const frames = eventData.exception?.values?.[0].stacktrace?.frames;
+
+    runInChromium(() => {
+      expect(frames).toMatchObject([
+        { function: '?' },
+        { function: '?' },
+        { function: 'qux' },
+        { function: '?' },
+        { function: '?' },
+        { function: 'foo' },
+        { function: 'bar' },
+        { function: 'Function.baz' },
+      ]);
+    });
+
+    runInFirefox(() => {
+      expect(frames).toMatchObject([
+        { function: '?' },
+        { function: '?' },
+        { function: 'qux' },
+        { function: 'qux/<' },
+        { function: 'qux/</<' },
+        { function: 'foo' },
+        { function: 'bar' },
+        { function: 'baz' },
+      ]);
+    });
+
+    runInWebkit(() => {
+      expect(frames).toMatchObject([
+        { function: 'global code' },
+        { function: '?' },
+        { function: 'qux' },
+        { function: '?' },
+        { function: '?' },
+        { function: 'foo' },
+        { function: 'bar' },
+        { function: 'baz' },
+      ]);
+    });
+  },
+);
+
+sentryTest(
+  'should not add any part of the function identifier to beginning of filename',
+  async ({ getLocalTestPath, page }) => {
+    const url = await getLocalTestPath({ testDir: __dirname });
+
+    const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+
+    expect(eventData.exception?.values?.[0].stacktrace?.frames).toMatchObject(
+      // specifically, we're trying to avoid values like `Blob@file://path/to/file` in frames with function names like `makeBlob`
+      Array(8).fill({ filename: expect.stringMatching(/^file:\/?/) }),
+    );
+  },
+);
