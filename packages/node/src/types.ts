4e33c466cad8fae524ca869a9cd722cc61f3e56c
Luca Forstner <luca.forstner@sentry.io>
2022-08-04 07:41:11 +0000

    feat(node): Add tracePropagationTargets option

--- packages/node/src/types.ts
+++ packages/node/src/types.ts
@@ -6,6 +6,19 @@ export interface BaseNodeOptions {
   /** Sets an optional server name (device name) */
   serverName?: string;
 
+  // We have this option seperately in both the node options and the browser options, so that we can have different JSDoc
+  // comments, since this has different behaviour in the Browser and Node SDKs.
+  /**
+   * List of strings/regex controlling to which outgoing requests
+   * the SDK will attach tracing headers.
+   *
+   * By default the SDK will attach those headers to all outgoing
+   * requests. If this option is provided, the SDK will match the
+   * request URL of outgoing requests against the items in this
+   * array, and only attach tracing headers if a match was found.
+   */
+  tracePropagationTargets?: (string | RegExp)[];
+
   /** Callback that is executed when a fatal global error occurs. */
   onFatalError?(error: Error): void;
 }
