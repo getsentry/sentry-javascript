ac107fc97f33a53207ebc8d37d292a3f627c4b52
Ash Anand <ash.anand@sentry.io>
2022-08-05 15:43:06 -0400

    Move comments

--- packages/react/src/profiler.tsx
+++ packages/react/src/profiler.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { getCurrentHub, Hub } from '@sentry/browser';
-import { Span, Transaction } from '@sentry/types';
-import { timestampWithMs } from '@sentry/utils';
+import {getCurrentHub, Hub} from '@sentry/browser';
+import {Span, Transaction} from '@sentry/types';
+import {timestampWithMs} from '@sentry/utils';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import * as React from 'react';
 
-import { REACT_MOUNT_OP, REACT_RENDER_OP, REACT_UPDATE_OP } from './constants';
+import {REACT_MOUNT_OP, REACT_RENDER_OP, REACT_UPDATE_OP} from './constants';
 
 export const UNKNOWN_COMPONENT = 'unknown';
 
@@ -21,7 +21,7 @@ export type ProfilerProps = {
   // If component updates should be displayed as spans. True by default.
   includeUpdates?: boolean;
   // props given to component being profiled.
-  updateProps: { [key: string]: unknown };
+  updateProps: {[key: string]: unknown};
 };
 
 /**
@@ -45,7 +45,7 @@ class Profiler extends React.Component<ProfilerProps> {
 
   public constructor(props: ProfilerProps) {
     super(props);
-    const { name, disabled = false } = this.props;
+    const {name, disabled = false} = this.props;
 
     if (disabled) {
       return;
@@ -67,11 +67,19 @@ class Profiler extends React.Component<ProfilerProps> {
     }
   }
 
-  public shouldComponentUpdate({ updateProps, includeUpdates = true }: ProfilerProps): boolean {
+  public shouldComponentUpdate({
+    updateProps,
+    includeUpdates = true,
+  }: ProfilerProps): boolean {
+    // Only generate an update span if includeUpdates is true, if there is a valid mountSpan,
+    // and if the updateProps have changed. It is ok to not do a deep equality check here as it is expensive.
+    // We are just trying to give baseline clues for further investigation.
     if (includeUpdates && this._mountSpan && updateProps !== this.props.updateProps) {
       // See what props haved changed between the previous props, and the current props. This is
       // set as data on the span. We just store the prop keys as the values could be potenially very large.
-      const changedProps = Object.keys(updateProps).filter(k => updateProps[k] !== this.props.updateProps[k]);
+      const changedProps = Object.keys(updateProps).filter(
+        k => updateProps[k] !== this.props.updateProps[k]
+      );
       if (changedProps.length > 0) {
         // The update span is a point in time span with 0 duration, just signifying that the component
         // has been updated.
@@ -91,9 +99,6 @@ class Profiler extends React.Component<ProfilerProps> {
   }
 
   public componentDidUpdate(): void {
-    // Only generate an update span if hasUpdateSpan is true, if there is a valid mountSpan,
-    // and if the updateProps have changed. It is ok to not do a deep equality check here as it is expensive.
-    // We are just trying to give baseline clues for further investigation.
     if (this._updateSpan) {
       this._updateSpan.finish();
       this._updateSpan = undefined;
@@ -103,7 +108,7 @@ class Profiler extends React.Component<ProfilerProps> {
   // If a component is unmounted, we can say it is no longer on the screen.
   // This means we can finish the span representing the component render.
   public componentWillUnmount(): void {
-    const { name, includeRender = true } = this.props;
+    const {name, includeRender = true} = this.props;
 
     if (this._mountSpan && includeRender) {
       // If we were able to obtain the spanId of the mount activity, we should set the
@@ -133,10 +138,13 @@ class Profiler extends React.Component<ProfilerProps> {
 function withProfiler<P extends Record<string, any>>(
   WrappedComponent: React.ComponentType<P>,
   // We do not want to have `updateProps` given in options, it is instead filled through the HOC.
-  options?: Pick<Partial<ProfilerProps>, Exclude<keyof ProfilerProps, 'updateProps'>>,
+  options?: Pick<Partial<ProfilerProps>, Exclude<keyof ProfilerProps, 'updateProps'>>
 ): React.FC<P> {
   const componentDisplayName =
-    (options && options.name) || WrappedComponent.displayName || WrappedComponent.name || UNKNOWN_COMPONENT;
+    (options && options.name) ||
+    WrappedComponent.displayName ||
+    WrappedComponent.name ||
+    UNKNOWN_COMPONENT;
 
   const Wrapped: React.FC<P> = (props: P) => (
     <Profiler {...options} name={componentDisplayName} updateProps={props}>
@@ -161,10 +169,10 @@ function withProfiler<P extends Record<string, any>>(
  */
 function useProfiler(
   name: string,
-  options: { disabled?: boolean; hasRenderSpan?: boolean } = {
+  options: {disabled?: boolean; hasRenderSpan?: boolean} = {
     disabled: false,
     hasRenderSpan: true,
-  },
+  }
 ): void {
   const [mountSpan] = React.useState(() => {
     if (options && options.disabled) {
@@ -202,10 +210,12 @@ function useProfiler(
   }, []);
 }
 
-export { withProfiler, Profiler, useProfiler };
+export {withProfiler, Profiler, useProfiler};
 
 /** Grabs active transaction off scope */
-export function getActiveTransaction<T extends Transaction>(hub: Hub = getCurrentHub()): T | undefined {
+export function getActiveTransaction<T extends Transaction>(
+  hub: Hub = getCurrentHub()
+): T | undefined {
   if (hub) {
     const scope = hub.getScope();
     if (scope) {
