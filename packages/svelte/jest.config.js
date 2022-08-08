3ca09c221b6abf63f9e5c742a60ed72d4eee5e7d
Lukas Stracke <lukas.stracke@sentry.io>
2022-08-08 15:19:09 +0200

    actually fix CI (pls)

--- packages/svelte/jest.config.js
+++ packages/svelte/jest.config.js
@@ -3,5 +3,4 @@ const baseConfig = require('../../jest/jest.config.js');
 module.exports = {
   ...baseConfig,
   testEnvironment: 'jsdom',
-  passWithNoTests: true, // TODO remove once we have something to test and tests
 };
