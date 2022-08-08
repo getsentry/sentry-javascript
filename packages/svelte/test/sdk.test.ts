cccaa889279592713585c28a855ea3b7f83e239d
Lukas Stracke <lukas.stracke@sentry.io>
2022-08-08 17:26:59 +0200

    feat(svelte): Add Basic Error and Performance instrumentation
    Wraps Browser SDK functionality

---
+++ packages/svelte/test/sdk.test.ts
@@ -0,0 +1,30 @@
+import { init as svelteInit } from '../src/sdk';
+import { init as browserInitRaw, SDK_VERSION } from '@sentry/browser';
+
+const browserInit = browserInitRaw as jest.Mock;
+jest.mock('@sentry/browser');
+
+describe('Initialize Svelte SDk', () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it('has the correct metadata', () => {
+    svelteInit({
+      dsn: 'https://public@dsn.ingest.sentry.io/1337',
+    });
+
+    const expectedMetadata = {
+      _metadata: {
+        sdk: {
+          name: 'sentry.javascript.svelte',
+          packages: [{ name: 'npm:@sentry/svelte', version: SDK_VERSION }],
+          version: SDK_VERSION,
+        },
+      },
+    };
+
+    expect(browserInit).toHaveBeenCalledTimes(1);
+    expect(browserInit).toHaveBeenCalledWith(expect.objectContaining(expectedMetadata));
+  });
+});
