ea2fa959f7fac30def749577bd1f978090aa7fab
Katie Byers <lobsterkatie@gmail.com>
2022-08-10 14:57:34 -0700

    suppress stack when `SentryError` isn't an error

--- packages/utils/src/error.ts
+++ packages/utils/src/error.ts
@@ -1,12 +1,17 @@
+import type { ConsoleLevel } from './logger';
+
 /** An error emitted by Sentry SDKs and related utilities. */
 export class SentryError extends Error {
   /** Display name of this error instance. */
   public name: string;
 
-  public constructor(public message: string) {
+  public logLevel: ConsoleLevel;
+
+  public constructor(public message: string, logLevel: ConsoleLevel = 'warn') {
     super(message);
 
     this.name = new.target.prototype.constructor.name;
     Object.setPrototypeOf(this, new.target.prototype);
+    this.logLevel = logLevel;
   }
 }
