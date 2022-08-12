dbd83406dcdd730491873e00226865ae4b4a7ed1
Kev <6111995+k-fish@users.noreply.github.com>
2022-08-12 13:59:11 -0400

    feat(tracing): Add long task collection (#5529)
    * rfc(perf): Add long task collection
    
    This adds long task collection to represent long running ui behaviour as spans in your frontend transactions. Can be disabled by passing an experimental flag for now.

---
+++ packages/integration-tests/suites/tracing/browsertracing/long-tasks-disabled/init.js
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+
+window.Sentry = Sentry;
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  integrations: [new Integrations.BrowserTracing({ _experiments: { enableLongTasks: false }, idleTimeout: 9000 })],
+  tracesSampleRate: 1,
+});
---
+++ packages/integration-tests/suites/tracing/browsertracing/long-tasks-enabled/test.ts
@@ -0,0 +1,36 @@
+import { expect, Route } from '@playwright/test';
+import { Event } from '@sentry/types';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest } from '../../../../utils/helpers';
+
+sentryTest('should capture long task.', async ({ browserName, getLocalTestPath, page }) => {
+  // Long tasks only work on chrome
+  if (browserName !== 'chromium') {
+    sentryTest.skip();
+  }
+
+  await page.route('**/path/to/script.js', (route: Route) => route.fulfill({ path: `${__dirname}/assets/script.js` }));
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+  const uiSpans = eventData.spans?.filter(({ op }) => op?.startsWith('ui'));
+
+  expect(uiSpans?.length).toBe(1);
+
+  const [firstUISpan] = uiSpans || [];
+  expect(firstUISpan).toEqual(
+    expect.objectContaining({
+      op: 'ui.long-task',
+      description: 'Long Task',
+      parent_span_id: eventData.contexts?.trace.span_id,
+    }),
+  );
+  const start = firstUISpan['start_timestamp'] ?? 0;
+  const end = firstUISpan['timestamp'] ?? 0;
+  const duration = end - start;
+
+  expect(duration).toBeGreaterThanOrEqual(0.1);
+  expect(duration).toBeLessThanOrEqual(0.15);
+});
