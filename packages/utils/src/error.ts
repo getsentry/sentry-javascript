c56310aaac996d6f7b1d325679d82c910a873ce4
Katie Byers <lobsterkatie@gmail.com>
2022-08-15 23:44:23 -0700

    add note about `SentryError` prototype

--- packages/utils/src/error.ts
+++ packages/utils/src/error.ts
@@ -11,6 +11,9 @@ export class SentryError extends Error {
     super(message);
 
     this.name = new.target.prototype.constructor.name;
+    // This sets the prototype to be `Error`, not `SentryError`. It's unclear why we do this, but commenting this line
+    // out causes various (seemingly totally unrelated) playwright tests consistently time out. FYI, this makes
+    // instances of `SentryError` fail `obj instanceof SentryError` checks.
     Object.setPrototypeOf(this, new.target.prototype);
     this.logLevel = logLevel;
   }
