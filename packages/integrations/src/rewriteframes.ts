c9e725b3eab1281332c245da5061a2c459d36a30
Lucas <lucas-zimerman1@hotmail.com>
2022-08-22 11:54:07 -0300

    review nits

--- packages/integrations/src/rewriteframes.ts
+++ packages/integrations/src/rewriteframes.ts
@@ -77,8 +77,8 @@ export class RewriteFrames implements Integration {
     if (isWindowsFrame || startsWithSlash) {
       const filename = isWindowsFrame
         ? frame.filename
-            .replace(/^[A-Z]:/, '') // remove Windows-style prefix
-            .replace(/\\/g, '/') // replace all `\\` instances with `/`
+          .replace(/^[A-Z]:/, '') // remove Windows-style prefix
+          .replace(/\\/g, '/') // replace all `\\` instances with `/`
         : frame.filename;
       const base = this._root ? relative(this._root, filename) : basename(filename);
       frame.filename = `${this._prefix}${base}`;
@@ -108,9 +108,9 @@ export class RewriteFrames implements Integration {
 
   /** JSDoc */
   private _processStacktrace(stacktrace?: Stacktrace): Stacktrace {
-      return {
-        ...stacktrace,
-        frames: stacktrace && stacktrace.frames && stacktrace.frames.map(f => this._iteratee(f)),
-      };
+    return {
+      ...stacktrace,
+      frames: stacktrace && stacktrace.frames && stacktrace.frames.map(f => this._iteratee(f)),
+    };
   }
 }
