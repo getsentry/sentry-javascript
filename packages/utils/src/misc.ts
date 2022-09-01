b6c8624caf3c1a70ee0e3085c7eab4198fbe30ab
Katie Byers <lobsterkatie@gmail.com>
2022-08-31 18:07:02 -0700

    add `arrayify` function

--- packages/utils/src/misc.ts
+++ packages/utils/src/misc.ts
@@ -200,3 +200,13 @@ export function checkOrSetAlreadyCaught(exception: unknown): boolean {
 
   return false;
 }
+
+/**
+ * Checks whether the given input is already an array, and if it isn't, wraps it in one.
+ *
+ * @param maybeArray Input to turn into an array, if necessary
+ * @returns The input, if already an array, or an array with the input as the only element, if not
+ */
+export function arrayify<T = unknown>(maybeArray: T | T[]): T[] {
+  return Array.isArray(maybeArray) ? maybeArray : [maybeArray];
+}
