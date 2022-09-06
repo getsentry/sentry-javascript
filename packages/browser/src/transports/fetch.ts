30619ab76a9a47b79ab0728f07bea66892f55a64
Luca Forstner <luca.forstner@sentry.io>
2022-09-06 09:22:56 +0000

    feat(browser): Use fetch `keepalive` flag

--- packages/browser/src/transports/fetch.ts
+++ packages/browser/src/transports/fetch.ts
@@ -17,6 +17,16 @@ export function makeFetchTransport(
       method: 'POST',
       referrerPolicy: 'origin',
       headers: options.headers,
+      // Outgoing requests are usually cancelled when navigating to a different page, causing a "TypeError: Failed to
+      // fetch" error and sending a "network_error" client-outcome - in Chrome, the request status shows "(cancelled)".
+      // The `keepalive` flag keeps outgoing requests alive, even when switching pages. We want this since we're
+      // frequently sending events right before the user is switching pages (eg. whenfinishing navigation transactions).
+      // Gotchas:
+      // - `keepalive` isn't supported by Firefox
+      // - As per spec (https://fetch.spec.whatwg.org/#http-network-or-cache-fetch), a request with `keepalive: true`
+      //   and a content length of > 64 kibibytes returns a network error. We will therefore only activate the flag wnen
+      //   we're below that limit.
+      keepalive: request.body.length <= 65536,
       ...options.fetchOptions,
     };
 
