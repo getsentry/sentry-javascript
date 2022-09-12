2cd90a6c81670e28e42224e16ed98543567b46ed
Billy Vong <billyvg@users.noreply.github.com>
2022-09-12 11:36:06 -0400

    feat: Change session life to 5 minutes (getsentry/sentry-replay#188)
    ...down from 15 minutes.
    
    Co-authored-by: Ryan Albrecht <ryan@ryanalbrecht.ca>

--- src/session/constants.ts
+++ src/session/constants.ts
@@ -3,7 +3,7 @@ export const REPLAY_EVENT_NAME = 'replay_event';
 export const RECORDING_EVENT_NAME = 'replay_recording';
 
 // The idle limit for a session
-export const SESSION_IDLE_DURATION = 900000; // 15 minutes
+export const SESSION_IDLE_DURATION = 300_000; // 5 minutes in ms
 
 // Grace period to keep a session when a user changes tabs or hides window
 export const VISIBILITY_CHANGE_TIMEOUT = SESSION_IDLE_DURATION;
