372b7989ecd5f647cee0df205db7405f850d24f9
Abhijeet Prasad <aprasad@sentry.io>
2022-09-12 14:52:05 +0200

    ref(tracing): Record transaction name changes

--- packages/tracing/test/transaction.test.ts
+++ packages/tracing/test/transaction.test.ts
@@ -24,6 +24,46 @@ describe('`Transaction` class', () => {
       expect(transaction.metadata.source).toEqual('custom');
     });
 
+    it('updates transaction metadata with correct variables needed', () => {
+      const transaction = new Transaction({ name: 'dogpark' });
+      expect(transaction.metadata.changes).toEqual([]);
+
+      transaction.name = 'ballpit';
+
+      expect(transaction.metadata.changes).toEqual([
+        {
+          source: 'custom',
+          timestamp: expect.any(Number),
+          propagations: 0,
+        },
+      ]);
+
+      transaction.metadata.propagations += 3;
+
+      expect(transaction.metadata.changes).toEqual([
+        {
+          source: 'custom',
+          timestamp: expect.any(Number),
+          propagations: 0,
+        },
+      ]);
+
+      transaction.name = 'playground';
+
+      expect(transaction.metadata.changes).toEqual([
+        {
+          source: 'custom',
+          timestamp: expect.any(Number),
+          propagations: 0,
+        },
+        {
+          source: 'custom',
+          timestamp: expect.any(Number),
+          propagations: 3,
+        },
+      ]);
+    });
+
     describe('`setName` method', () => {
       it("sets source to `'custom'` if no source provided", () => {
         const transaction = new Transaction({ name: 'dogpark' });
@@ -39,6 +79,46 @@ describe('`Transaction` class', () => {
 
         expect(transaction.name).toEqual('ballpit');
         expect(transaction.metadata.source).toEqual('route');
+      });
+
+      it('updates transaction metadata with correct variables needed', () => {
+        const transaction = new Transaction({ name: 'dogpark' });
+        expect(transaction.metadata.changes).toEqual([]);
+
+        transaction.name = 'ballpit';
+
+        expect(transaction.metadata.changes).toEqual([
+          {
+            source: 'custom',
+            timestamp: expect.any(Number),
+            propagations: 0,
+          },
+        ]);
+
+        transaction.metadata.propagations += 3;
+
+        expect(transaction.metadata.changes).toEqual([
+          {
+            source: 'custom',
+            timestamp: expect.any(Number),
+            propagations: 0,
+          },
+        ]);
+
+        transaction.setName('playground', 'task');
+
+        expect(transaction.metadata.changes).toEqual([
+          {
+            source: 'custom',
+            timestamp: expect.any(Number),
+            propagations: 0,
+          },
+          {
+            source: 'task',
+            timestamp: expect.any(Number),
+            propagations: 3,
+          },
+        ]);
       });
     });
   });
