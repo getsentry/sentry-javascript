00c530fff35067bd38c1b51357b268dbaf5f957c
Billy Vong <billyvg@users.noreply.github.com>
2022-09-16 10:27:59 -0400

    feat: Do not ignore ingest host on sentry.io (getsentry/sentry-replay#198)
    We want to see network requests made to our ingest endpoint when Replays
    SDK is used on our SaaS.
    
    Closes https://github.com/getsentry/sentry-replay/issues/196

--- src/util/isIngestHost.ts
+++ src/util/isIngestHost.ts
@@ -6,5 +6,11 @@ import { getCurrentHub } from '@sentry/core';
 export function isIngestHost(targetHost: string) {
   const { protocol, host } = getCurrentHub().getClient()?.getDsn() || {};
 
+  // XXX: Special case when this integration is used by Sentry on `sentry.io`
+  // We would like to capture network requests made to our ingest endpoints for debugging
+  if (window.location.host === 'sentry.io') {
+    return false;
+  }
+
   return targetHost.startsWith(`${protocol}://${host}`);
 }
