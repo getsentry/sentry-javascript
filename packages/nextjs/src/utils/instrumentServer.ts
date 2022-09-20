9582902e7585fc279852bbddf5efd2fa0dddbda8
Katie Byers <lobsterkatie@gmail.com>
2022-09-20 12:30:08 -0700

    handle `pathname` being passed in object

--- packages/nextjs/src/utils/instrumentServer.ts
+++ packages/nextjs/src/utils/instrumentServer.ts
@@ -317,14 +317,23 @@ function makeWrappedMethodForGettingParameterizedPath(
   origMethod: ApiPageEnsurer | PageComponentFinder,
 ): WrappedApiPageEnsurer | WrappedPageComponentFinder {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const wrappedMethod = async function (this: Server, parameterizedPath: string, ...args: any[]): Promise<any> {
+  const wrappedMethod = async function (
+    this: Server,
+    parameterizedPath:
+      | string // `ensureAPIPage`, `findPageComponents` before nextjs 12.2.6
+      | { pathname: string }, // `findPageComponents` from nextjs 12.2.6 onward
+    ...args: any[]
+  ): Promise<any> {
     const transaction = getActiveTransaction();
 
     // replace specific URL with parameterized version
     if (transaction && transaction.metadata.requestPath) {
       const origPath = transaction.metadata.requestPath;
-      const newName = transaction.name.replace(origPath, parameterizedPath);
-      transaction.setName(newName, 'route');
+      const newPath = typeof parameterizedPath === 'string' ? parameterizedPath : parameterizedPath.pathname;
+      if (newPath) {
+        const newName = transaction.name.replace(origPath, newPath);
+        transaction.setName(newName, 'route');
+      }
     }
 
     return origMethod.call(this, parameterizedPath, ...args);
