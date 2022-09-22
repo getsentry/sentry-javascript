62b1900351740001aa8d5801c1d93c5535eebe27
Tim Fish <tim@timfish.uk>
2022-09-22 15:18:22 +0100

    Fix broken logic and add flag so they can still be run sequentially

--- packages/integrations/scripts/buildBundles.ts
+++ packages/integrations/scripts/buildBundles.ts
@@ -2,13 +2,15 @@ import { spawn } from 'child_process';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
+const runParallel = process.argv.includes('--parallel');
+
 /** Gets a list of src filenames, one for each integration and excludes the index.ts */
 function getIntegrations(): string[] {
   const srcDir = join(__dirname, '..', 'src');
   const srcFiles = readdirSync(srcDir);
-  // The index file is only there for the purposes of npm builds (for the CDN we create a separate bundle for each
-  // integration)
-  return srcFiles.filter(file => file === 'index.ts');
+  // The index file is only there for the purposes of npm builds
+  // (for the CDN we create a separate bundle for each integration)
+  return srcFiles.filter(file => file !== 'index.ts');
 }
 
 /** Builds a bundle for a specific integration and JavaScript ES version */
@@ -28,18 +30,29 @@ async function buildBundle(integration: string, jsVersion: string): Promise<void
   });
 }
 
-// We're building a bundle for each integration and each JavaScript version.
-const tasks = getIntegrations().reduce(
-  (tasks, integration) => [...tasks, buildBundle(integration, 'ES5'), buildBundle(integration, 'ES6')],
-  [] as Promise<void>[],
-);
+if (runParallel) {
+  // We're building a bundle for each integration and each JavaScript version.
+  const tasks = getIntegrations().reduce(
+    (tasks, integration) => [...tasks, buildBundle(integration, 'ES5'), buildBundle(integration, 'ES6')],
+    [] as Promise<void>[],
+  );
 
-Promise.all(tasks)
-  // eslint-disable-next-line no-console
-  .then(_ => console.log('\nIntegration bundles built successfully'))
-  .catch(error => {
+  Promise.all(tasks)
     // eslint-disable-next-line no-console
-    console.error(error);
-    // Important to exit with a non-zero exit code, so that the build fails.
-    process.exit(1);
-  });
+    .then(_ => console.log('\nIntegration bundles built successfully'))
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      // Important to exit with a non-zero exit code, so that the build fails.
+      process.exit(1);
+    });
+} else {
+  void (async () => {
+    for (const integration of getIntegrations()) {
+      await buildBundle(integration, 'ES5');
+      await buildBundle(integration, 'ES6');
+    }
+    // eslint-disable-next-line no-console
+    console.log('\nIntegration bundles built successfully');
+  })();
+}
