ea0155ff77a82e400a832c63ef8c7388bfdc75cc
Luca Forstner <luca.forstner@sentry.io>
2022-09-26 08:43:30 +0000

    Rename verdaccio config folder

--- packages/e2e-tests/run.ts
+++ packages/e2e-tests/run.ts
@@ -11,20 +11,20 @@ const repositoryRoot = path.resolve(__dirname, '../..');
 childProcess.execSync('yarn build:npm', { encoding: 'utf8', cwd: repositoryRoot, stdio: 'inherit' });
 
 try {
-  // Stop Verdaccio Test Registry container if it was already running
+  // Stop test registry container (Verdaccio) if it was already running
   childProcess.execSync(`docker stop ${TEST_REGISTRY_CONTAINER_NAME}`, { stdio: 'ignore' });
   console.log('Stopped previously running test registry');
 } catch (e) {
   // Don't throw if container wasn't running
 }
 
-// Start Verdaccio
+// Start test registry (Verdaccio)
 childProcess.execSync(
-  `docker run --detach --rm --name verdaccio-e2e-test-registry -p 4873:4873 -v ${__dirname}/verdaccio/conf:/verdaccio/conf verdaccio/verdaccio:5.15.3`,
+  `docker run --detach --rm --name verdaccio-e2e-test-registry -p 4873:4873 -v ${__dirname}/verdaccio-config:/verdaccio/conf verdaccio/verdaccio:5.15.3`,
   { encoding: 'utf8', stdio: 'inherit' },
 );
 
-// Publish built packages to Verdaccio
+// Publish built packages to test registry
 const packageTarballPaths = glob.sync('packages/*/sentry-*.tgz', {
   cwd: repositoryRoot,
   absolute: true,
