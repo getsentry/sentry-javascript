fc14bbba1e126baeefcee2c400b433b1860edfd2
Luca Forstner <luca.forstner@sentry.io>
2022-09-26 08:16:24 +0000

    Clean up dependencies?

--- e2e-tests/run.ts
+++ e2e-tests/run.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import * as childProcess from 'child_process';
-import * as path from 'path';
 import * as glob from 'glob';
+import * as path from 'path';
 
 const TEST_REGISTRY_CONTAINER_NAME = 'verdaccio-e2e-test-registry';
 
