d7050517d83a3cc65743dd572fbc7474d4533899
Luca Forstner <luca.forstner@sentry.io>
2022-09-28 15:41:41 +0000

    Add Sentry

--- packages/e2e-tests/test-applications/create-react-app/src/App.tsx
+++ packages/e2e-tests/test-applications/create-react-app/src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import * as Sentry from '@sentry/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+        <button
+          onClick={() => {
+            Sentry.captureException(new Error('I am a captured error!'));
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          I am capturing an error
+        </button>
+      </div>
+    </Sentry.ErrorBoundary>
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
--- packages/e2e-tests/test-applications/create-react-app/src/index.tsx
+++ packages/e2e-tests/test-applications/create-react-app/src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+Sentry.init({
+  dsn: 'https://b887218a80114d26a9b1a51c5f88e0b4@o447951.ingest.sentry.io/6601807',
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
