908b2b9bd9359beb619eb43d89b19e2d03e94f46
Luca Forstner <luca.forstner@sentry.io>
2022-09-28 11:34:57 +0000

    Add logic

--- packages/e2e-tests/run.ts
+++ packages/e2e-tests/run.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import * as childProcess from 'child_process';
+import * as fs from 'fs';
+import * as glob from 'glob';
 import * as path from 'path';
 
 const repositoryRoot = path.resolve(__dirname, '../..');
@@ -11,10 +13,12 @@ const PUBLISH_PACKAGES_DOCKER_IMAGE_NAME = 'publish-packages';
 
 const publishScriptNodeVersion = process.env.E2E_TEST_PUBLISH_SCRIPT_NODE_VERSION;
 
+const DEFAULT_TEST_TIMEOUT_SECONDS = 60;
+
 // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#grouping-log-lines
 function groupCIOutput(groupTitle: string, fn: () => void): void {
   if (process.env.CI) {
-    console.log(`::group::{${groupTitle}}`);
+    console.log(`::group::${groupTitle}`);
     fn();
     console.log('::endgroup::');
   } else {
@@ -22,26 +26,54 @@ function groupCIOutput(groupTitle: string, fn: () => void): void {
   }
 }
 
+// https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
+function printCIErrorMessage(message: string): void {
+  if (process.env.CI) {
+    console.log(`::error::${message}`);
+  } else {
+    console.log(message);
+  }
+}
+
 groupCIOutput('Test Registry Setup', () => {
   try {
     // Stop test registry container (Verdaccio) if it was already running
-    childProcess.execSync(`docker stop ${TEST_REGISTRY_CONTAINER_NAME}`, { stdio: 'ignore' });
+    childProcess.spawnSync('docker', ['stop', TEST_REGISTRY_CONTAINER_NAME], { stdio: 'ignore' });
     console.log('Stopped previously running test registry');
   } catch (e) {
     // Don't throw if container wasn't running
   }
 
   // Start test registry (Verdaccio)
-  childProcess.execSync(
-    `docker run --detach --rm --name ${TEST_REGISTRY_CONTAINER_NAME} -p 4873:4873 -v ${__dirname}/verdaccio-config:/verdaccio/conf verdaccio/verdaccio:${VERDACCIO_VERSION}`,
+  childProcess.spawnSync(
+    'docker',
+    [
+      'run',
+      '--detach',
+      '--rm',
+      '--name',
+      TEST_REGISTRY_CONTAINER_NAME,
+      '-p',
+      '4873:4873',
+      '-v',
+      `${__dirname}/verdaccio-config:/verdaccio/conf`,
+      `verdaccio/verdaccio:${VERDACCIO_VERSION}`,
+    ],
     { encoding: 'utf8', stdio: 'inherit' },
   );
 
   // Build container image that is uploading our packages to fake registry with specific Node.js/npm version
-  childProcess.execSync(
-    `docker build --tag ${PUBLISH_PACKAGES_DOCKER_IMAGE_NAME} --file ./Dockerfile.publish-packages ${
-      publishScriptNodeVersion ? `--build-arg NODE_VERSION=${publishScriptNodeVersion}` : ''
-    } .`,
+  childProcess.spawnSync(
+    'docker',
+    [
+      'build',
+      '--tag',
+      PUBLISH_PACKAGES_DOCKER_IMAGE_NAME,
+      '--file',
+      './Dockerfile.publish-packages',
+      publishScriptNodeVersion ? `--build-arg NODE_VERSION=${publishScriptNodeVersion}` : undefined,
+      '.',
+    ].filter((arg): arg is string => arg !== undefined),
     {
       encoding: 'utf8',
       stdio: 'inherit',
@@ -49,8 +81,17 @@ groupCIOutput('Test Registry Setup', () => {
   );
 
   // Run container that uploads our packages to fake registry
-  childProcess.execSync(
-    `docker run --rm -v ${repositoryRoot}:/sentry-javascript --network host ${PUBLISH_PACKAGES_DOCKER_IMAGE_NAME}`,
+  childProcess.spawnSync(
+    'docker',
+    [
+      'run',
+      '--rm',
+      '-v',
+      `${repositoryRoot}:/sentry-javascript`,
+      '--network',
+      'host',
+      PUBLISH_PACKAGES_DOCKER_IMAGE_NAME,
+    ],
     {
       encoding: 'utf8',
       stdio: 'inherit',
@@ -60,10 +101,107 @@ groupCIOutput('Test Registry Setup', () => {
 
 groupCIOutput('Run E2E Test Suites', () => {
   // TODO: Run e2e tests here
+  const recipePaths = glob.sync(`${__dirname}/test-applications/*/test-recipe.json`, { absolute: true });
+
+  const recipeResults = recipePaths.map(recipePath => {
+    type Recipe = {
+      testApplicationName: string;
+      buildCommand?: string;
+      tests: {
+        testName: string;
+        testCommand: string;
+        timeoutSeconds?: number;
+      }[];
+    };
+
+    const recipe: Recipe = JSON.parse(fs.readFileSync(recipePath, 'utf-8'));
+
+    if (recipe.buildCommand) {
+      console.log(`Running E2E test build command for test application "${recipe.testApplicationName}"`);
+      const [buildCommand, ...buildCommandArgs] = recipe.buildCommand.split(' ');
+      childProcess.spawnSync(buildCommand, buildCommandArgs, {
+        cwd: path.dirname(recipePath),
+        encoding: 'utf8',
+        stdio: 'inherit',
+      });
+    }
+
+    type TestResult = {
+      testName: string;
+      result: 'PASS' | 'FAIL' | 'TIMEOUT';
+    };
+
+    const testResults: TestResult[] = recipe.tests.map(test => {
+      console.log(
+        `Running E2E test command for test application "${recipe.testApplicationName}", test "${test.testName}"`,
+      );
+
+      const [testCommand, ...testCommandArgs] = test.testCommand.split(' ');
+      const testProcessResult = childProcess.spawnSync(testCommand, testCommandArgs, {
+        cwd: path.dirname(recipePath),
+        timeout: (test.timeoutSeconds ?? DEFAULT_TEST_TIMEOUT_SECONDS) * 1000,
+        encoding: 'utf8',
+        stdio: 'pipe',
+      });
+
+      console.log(testProcessResult.stdout.replace(/^/gm, '[TEST OUTPUT] '));
+      console.log(testProcessResult.stderr.replace(/^/gm, '[TEST OUTPUT] '));
+
+      const error: undefined | (Error & { code?: string }) = testProcessResult.error;
+
+      if (error?.code === 'ETIMEDOUT') {
+        printCIErrorMessage(
+          `Test "${test.testName}" in test application "${recipe.testApplicationName}" (${path.dirname(
+            recipePath,
+          )}) timed out.`,
+        );
+        return {
+          testName: test.testName,
+          result: 'TIMEOUT',
+        };
+      } else if (testProcessResult.status !== 0) {
+        printCIErrorMessage(
+          `Test "${test.testName}" in test application "${recipe.testApplicationName}" (${path.dirname(
+            recipePath,
+          )}) failed.`,
+        );
+        return {
+          testName: test.testName,
+          result: 'FAIL',
+        };
+      } else {
+        console.log(
+          `Test "${test.testName}" in test application "${recipe.testApplicationName}" (${path.dirname(
+            recipePath,
+          )}) succeeded.`,
+        );
+        return {
+          testName: test.testName,
+          result: 'PASS',
+        };
+      }
+    });
+
+    return {
+      testApplicationName: recipe.testApplicationName,
+      testApplicationPath: recipePath,
+      testResults,
+    };
+  });
+
+  console.log('--------------------------------------');
+  console.log('Test Result Summary:');
+
+  recipeResults.forEach(recipeResult => {
+    console.log(`● ${recipeResult.testApplicationName} (${path.dirname(recipeResult.testApplicationPath)})`);
+    recipeResult.testResults.forEach(testResult => {
+      console.log(`  ● ${testResult.result.padEnd(7, ' ')} ${testResult.testName}`);
+    });
+  });
 });
 
 groupCIOutput('Cleanup', () => {
   // Stop test registry
-  childProcess.execSync(`docker stop ${TEST_REGISTRY_CONTAINER_NAME}`, { encoding: 'utf8', stdio: 'ignore' });
+  childProcess.spawnSync(`docker stop ${TEST_REGISTRY_CONTAINER_NAME}`, { encoding: 'utf8', stdio: 'ignore' });
   console.log('Successfully stopped test registry container'); // Output from command above is not good so we `ignore` it and emit our own
 });
---
+++ packages/e2e-tests/test-applications/temporary-app-2/timeout.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+  console.log('Bored :/');
+}, 6000);
