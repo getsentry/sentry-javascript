864033dd3864267fc035669b415c9ac107d0f083
Luca Forstner <luca.forstner@sentry.io>
2022-10-01 16:21:37 +0000

    Add check for transaction in integration test

--- packages/nextjs/test/integration/test/server/cjsApiEndpoints.js
+++ packages/nextjs/test/integration/test/server/cjsApiEndpoints.js
@@ -1,12 +1,55 @@
 const assert = require('assert');
 
 const { sleep } = require('../utils/common');
-const { getAsync, interceptEventRequest, interceptTracingRequest } = require('../utils/server');
+const { getAsync, interceptTracingRequest } = require('../utils/server');
 
 module.exports = async ({ url: urlBase, argv }) => {
-  const responseUnwrapped = await getAsync(`${urlBase}/api/withSentryAPI/unwrapped/cjsExport`);
+  const unwrappedRoute = '/api/withSentryAPI/unwrapped/cjsExport';
+  const interceptedUnwrappedRequest = interceptTracingRequest(
+    {
+      contexts: {
+        trace: {
+          op: 'http.server',
+          status: 'ok',
+          tags: { 'http.status_code': '200' },
+        },
+      },
+      transaction: `GET ${unwrappedRoute}`,
+      type: 'transaction',
+      request: {
+        url: `${urlBase}${unwrappedRoute}`,
+      },
+    },
+    argv,
+    'unwrapped CJS route',
+  );
+  const responseUnwrapped = await getAsync(`${urlBase}${unwrappedRoute}`);
   assert.equal(responseUnwrapped, '{"success":true}');
 
-  const responseWrapped = await getAsync(`${urlBase}/api/withSentryAPI/wrapped/cjsExport`);
+  const wrappedRoute = '/api/withSentryAPI/wrapped/cjsExport';
+  const interceptedWrappedRequest = interceptTracingRequest(
+    {
+      contexts: {
+        trace: {
+          op: 'http.server',
+          status: 'ok',
+          tags: { 'http.status_code': '200' },
+        },
+      },
+      transaction: `GET ${wrappedRoute}`,
+      type: 'transaction',
+      request: {
+        url: `${urlBase}${wrappedRoute}`,
+      },
+    },
+    argv,
+    'wrapped CJS route',
+  );
+  const responseWrapped = await getAsync(`${urlBase}${wrappedRoute}`);
   assert.equal(responseWrapped, '{"success":true}');
+
+  await sleep(250);
+
+  assert.ok(interceptedUnwrappedRequest.isDone(), 'Did not intercept unwrapped request');
+  assert.ok(interceptedWrappedRequest.isDone(), 'Did not intercept wrapped request');
 };
