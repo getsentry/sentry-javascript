d4b21ef92b12bc9cab830c31f609bea0cca272f8
Onur Temizkan <onur@narval.co.uk>
2022-10-06 11:22:17 +0100

    Flag `react-router` as `external` without making it a peer dep.

--- packages/remix/rollup.npm.config.js
+++ packages/remix/rollup.npm.config.js
@@ -4,6 +4,7 @@ export default makeNPMConfigVariants(
   makeBaseNPMConfig({
     entrypoints: ['src/index.server.ts', 'src/index.client.tsx'],
     packageSpecificConfig: {
+      external: ['react-router', 'react-router-dom'],
       output: {
         // make it so Rollup calms down about the fact that we're combining default and named exports
         exports: 'named',
