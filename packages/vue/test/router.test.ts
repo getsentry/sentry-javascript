2cea8e7191fc0b392ab6b2a87826ba4c406a79c1
Lukas Stracke <lukas.stracke@sentry.io>
2022-10-18 12:10:36 +0200

    fix linter error

--- packages/vue/test/router.test.ts
+++ packages/vue/test/router.test.ts
@@ -3,7 +3,6 @@ import { Transaction } from '@sentry/types';
 
 import { vueRouterInstrumentation } from '../src';
 import { Route } from '../src/router';
-
 import * as vueTracing from '../src/tracing';
 
 const captureExceptionSpy = jest.spyOn(SentryBrowser, 'captureException');
