76ce3cbced78669b31f29af2f6dbd512e88ce7e7
Katie Byers <lobsterkatie@gmail.com>
2022-10-19 00:51:11 -0700

    use GCP options in integration if available

--- packages/node/src/integrations/requestdata.ts
+++ packages/node/src/integrations/requestdata.ts
@@ -119,10 +119,11 @@ export class RequestData implements Integration {
       }
 
       // The Express request handler takes a similar `include` option to that which can be passed to this integration.
-      // If passed there, we store it in `sdkProcessingMetadata`. TODO(v8): Force express people to use this
+      // If passed there, we store it in `sdkProcessingMetadata`. TODO(v8): Force express and GCP people to use this
       // integration, so that all of this passing and conversion isn't necessary
       const addRequestDataOptions =
         sdkProcessingMetadata.requestDataOptionsFromExpressHandler ||
+        sdkProcessingMetadata.requestDataOptionsFromGCPWrapper ||
         convertReqDataIntegrationOptsToAddReqDataOpts(this._options);
 
       const processedEvent = this._addRequestData(event, req, addRequestDataOptions);
