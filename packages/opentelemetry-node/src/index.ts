acfc296a7ed187054b6c2692081e0c0b3a052ae9
Abhijeet Prasad <aprasad@sentry.io>
2022-10-24 11:05:50 +0200

    add span processor boilerplate

--- packages/opentelemetry-node/src/index.ts
+++ packages/opentelemetry-node/src/index.ts
@@ -1,6 +1,41 @@
+import { Context } from '@opentelemetry/api';
+import { Span as OtelSpan, SpanProcessor as OtelSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { getCurrentHub } from '@sentry/core';
+
 /**
- * Test function
+ * Converts OpenTelemetry Spans to Sentry Spans and sends them to Sentry via
+ * the Sentry SDK.
  */
-export function test(): void {
-  // no-op
+export class SentrySpanProcessor implements OtelSpanProcessor {
+  /**
+   * @inheritDoc
+   */
+  public onStart(span: OtelSpan, parentContext: Context): void {
+    // do something
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public onEnd(span: OtelSpan): void {
+    // do something
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public shutdown(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public async forceFlush(): Promise<void> {
+    const client = getCurrentHub().getClient();
+    if (client) {
+      return client.flush().then();
+    }
+    return Promise.resolve();
+  }
 }
