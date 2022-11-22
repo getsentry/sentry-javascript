daacd386f976b340be5ff353e7b31922e6d07940
Onur Temizkan <onur@narval.co.uk>
2022-11-22 16:29:12 +0000

    feat(react): Add tracing support for React Router 6.4 `createBrowserRouter`. (#6172)
    Adds tracing support for React Router 6.4's [`createBrowserRouter`](https://reactrouter.com/en/main/routers/create-browser-router#createbrowserrouter) (and seamlessly [`createMemoryRouter`](https://reactrouter.com/en/main/routers/create-memory-router#creatememoryrouter)).
    
    React Router 6.4 allows us to reach the router state itself, without having to use React hooks. So, the implementation is much simpler.

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -1,3 +1,5 @@
+// Disabling `no-explicit-any` for the whole file as `any` has became common requirement.
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Transaction, TransactionContext } from '@sentry/types';
 
 export type Action = 'PUSH' | 'REPLACE' | 'POP';
@@ -5,7 +7,6 @@ export type Action = 'PUSH' | 'REPLACE' | 'POP';
 export type Location = {
   pathname: string;
   action?: Action;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } & Record<string, any>;
 
 export type ReactRouterInstrumentation = <T extends Transaction>(
@@ -13,3 +14,226 @@ export type ReactRouterInstrumentation = <T extends Transaction>(
   startTransactionOnPageLoad?: boolean,
   startTransactionOnLocationChange?: boolean,
 ) => void;
+
+// React Router v6 Vendored Types
+export interface NonIndexRouteObject {
+  caseSensitive?: boolean;
+  children?: RouteObject[];
+  element?: React.ReactNode | null;
+  index?: any;
+  path?: string;
+}
+
+export interface IndexRouteObject {
+  caseSensitive?: boolean;
+  children?: undefined;
+  element?: React.ReactNode | null;
+  index: any;
+  path?: string;
+}
+
+// This type was originally just `type RouteObject = IndexRouteObject`, but this was changed
+// in https://github.com/remix-run/react-router/pull/9366, which was released with `6.4.2`
+// See https://github.com/remix-run/react-router/issues/9427 for a discussion on this.
+export type RouteObject = IndexRouteObject | NonIndexRouteObject;
+
+export type Params<Key extends string = string> = {
+  readonly [key in Key]: string | undefined;
+};
+
+export type UseRoutes = (routes: RouteObject[], locationArg?: Partial<Location> | string) => React.ReactElement | null;
+
+// https://github.com/remix-run/react-router/blob/9fa54d643134cd75a0335581a75db8100ed42828/packages/react-router/lib/router.ts#L114-L134
+export interface RouteMatch<ParamKey extends string = string> {
+  /**
+   * The names and values of dynamic parameters in the URL.
+   */
+  params: Params<ParamKey>;
+  /**
+   * The portion of the URL pathname that was matched.
+   */
+  pathname: string;
+  /**
+   * The portion of the URL pathname that was matched before child routes.
+   */
+  pathnameBase: string;
+  /**
+   * The route object that was used to match.
+   */
+  route: RouteObject;
+}
+
+export type UseEffect = (cb: () => void, deps: unknown[]) => void;
+export type UseLocation = () => Location;
+export type UseNavigationType = () => Action;
+
+// For both of these types, use `any` instead of `RouteObject[]` or `RouteMatch[]`.
+// Have to do this so we maintain backwards compatability between
+// react-router > 6.0.0 and >= 6.4.2.
+export type RouteObjectArrayAlias = any;
+export type RouteMatchAlias = any;
+export type CreateRoutesFromChildren = (children: JSX.Element[]) => RouteObjectArrayAlias;
+export type MatchRoutes = (routes: RouteObjectArrayAlias, location: Location) => RouteMatchAlias[] | null;
+
+// Types for react-router >= 6.4.2
+export type ShouldRevalidateFunction = (args: any) => boolean;
+
+interface DataFunctionArgs {
+  request: Request;
+  params: Params;
+}
+
+type LoaderFunctionArgs = DataFunctionArgs;
+type ActionFunctionArgs = DataFunctionArgs;
+
+export interface LoaderFunction {
+  (args: LoaderFunctionArgs): Promise<Response> | Response | Promise<any> | any;
+}
+export interface ActionFunction {
+  (args: ActionFunctionArgs): Promise<Response> | Response | Promise<any> | any;
+}
+declare type AgnosticBaseRouteObject = {
+  caseSensitive?: boolean;
+  path?: string;
+  id?: string;
+  loader?: LoaderFunction;
+  action?: ActionFunction;
+  hasErrorBoundary?: boolean;
+  shouldRevalidate?: ShouldRevalidateFunction;
+  handle?: any;
+};
+
+export declare type AgnosticIndexRouteObject = AgnosticBaseRouteObject & Record<string, any>;
+
+export declare type AgnosticNonIndexRouteObject = AgnosticBaseRouteObject & Record<string, any>;
+
+export declare type AgnosticDataIndexRouteObject = AgnosticIndexRouteObject & {
+  id: string;
+};
+
+export declare type AgnosticDataNonIndexRouteObject = AgnosticNonIndexRouteObject & {
+  children?: AgnosticDataRouteObject[];
+  id: string;
+};
+
+export interface AgnosticRouteMatch<
+  ParamKey extends string = string,
+  RouteObjectType extends AgnosticRouteObject = AgnosticRouteObject,
+> {
+  params: Params<ParamKey>;
+  pathname: string;
+  pathnameBase: string;
+  route: RouteObjectType;
+}
+
+export type AgnosticDataRouteMatch = AgnosticRouteMatch<string, AgnosticDataRouteObject>;
+
+interface UseMatchesMatch {
+  id: string;
+  pathname: string;
+  params: AgnosticRouteMatch['params'];
+  data: unknown;
+  handle: unknown;
+}
+
+export interface GetScrollRestorationKeyFunction {
+  (location: Location, matches: UseMatchesMatch[]): string | null;
+}
+
+export interface Path {
+  pathname: string;
+  search: string;
+  hash: string;
+}
+
+export interface RouterSubscriber {
+  (state: RouterState): void;
+}
+export interface GetScrollPositionFunction {
+  (): number;
+}
+
+declare type LinkNavigateOptions = {
+  replace?: boolean;
+  state?: any;
+  preventScrollReset?: boolean;
+};
+
+export declare type AgnosticDataRouteObject = AgnosticDataIndexRouteObject | AgnosticDataNonIndexRouteObject;
+export declare type To = string | Partial<Path>;
+export declare type HydrationState = any;
+export declare type FormMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
+export declare type FormEncType = 'application/x-www-form-urlencoded' | 'multipart/form-data';
+export declare type RouterNavigateOptions = LinkNavigateOptions | SubmissionNavigateOptions;
+export declare type AgnosticRouteObject = AgnosticIndexRouteObject | AgnosticNonIndexRouteObject;
+
+declare type SubmissionNavigateOptions = {
+  replace?: boolean;
+  state?: any;
+  formMethod?: FormMethod;
+  formEncType?: FormEncType;
+  formData: FormData;
+};
+
+export interface RouterInit {
+  basename: string;
+  routes: AgnosticRouteObject[];
+  history: History;
+  hydrationData?: HydrationState;
+}
+
+export type NavigationStates = {
+  Idle: any;
+  Loading: any;
+  Submitting: any;
+};
+
+export type Navigation = NavigationStates[keyof NavigationStates];
+
+export type RouteData = any;
+export type Fetcher = any;
+
+export declare enum HistoryAction {
+  Pop = 'POP',
+  Push = 'PUSH',
+  Replace = 'REPLACE',
+}
+
+export interface RouterState {
+  historyAction: Action | HistoryAction | any;
+  location: any;
+  matches: AgnosticDataRouteMatch[];
+  initialized: boolean;
+  navigation: Navigation;
+  restoreScrollPosition: number | false | null;
+  preventScrollReset: boolean;
+  revalidation: any;
+  loaderData: RouteData;
+  actionData: RouteData | null;
+  errors: RouteData | null;
+  fetchers: Map<string, Fetcher>;
+}
+export interface Router {
+  basename: string | undefined;
+  state: RouterState;
+  routes: AgnosticDataRouteObject[];
+  _internalFetchControllers: any;
+  _internalActiveDeferreds: any;
+  initialize(): Router;
+  subscribe(fn: RouterSubscriber): () => void;
+  enableScrollRestoration(
+    savedScrollPositions: Record<string, number>,
+    getScrollPosition: GetScrollPositionFunction,
+    getKey?: any,
+  ): () => void;
+  navigate(to: number): void;
+  navigate(to: To, opts?: RouterNavigateOptions): void;
+  fetch(key: string, routeId: string, href: string, opts?: RouterNavigateOptions): void;
+  revalidate(): void;
+  createHref(location: Location | URL): string;
+  getFetcher(key?: string): any;
+  deleteFetcher(key?: string): void;
+  dispose(): void;
+}
+
+export type CreateRouterFunction = (routes: RouteObject[], opts?: any) => Router;
---
+++ packages/react/test/reactrouterv6.4.test.tsx
@@ -0,0 +1,263 @@
+import { render } from '@testing-library/react';
+import { Request } from 'node-fetch';
+import * as React from 'react';
+import {
+  createMemoryRouter,
+  createRoutesFromChildren,
+  matchPath,
+  matchRoutes,
+  Navigate,
+  RouterProvider,
+  useLocation,
+  useNavigationType,
+} from 'react-router-6.4';
+
+import { reactRouterV6Instrumentation,wrapCreateBrowserRouter  } from '../src';
+import { CreateRouterFunction } from '../src/types';
+
+beforeAll(() => {
+  // @ts-ignore need to override global Request because it's not in the jest environment (even with an
+  // `@jest-environment jsdom` directive, for some reason)
+  global.Request = Request;
+});
+
+describe('React Router v6.4', () => {
+  function createInstrumentation(_opts?: {
+    startTransactionOnPageLoad?: boolean;
+    startTransactionOnLocationChange?: boolean;
+  }): [jest.Mock, { mockSetName: jest.Mock; mockFinish: jest.Mock }] {
+    const options = {
+      matchPath: _opts ? matchPath : undefined,
+      startTransactionOnLocationChange: true,
+      startTransactionOnPageLoad: true,
+      ..._opts,
+    };
+    const mockFinish = jest.fn();
+    const mockSetName = jest.fn();
+    const mockStartTransaction = jest.fn().mockReturnValue({ setName: mockSetName, finish: mockFinish });
+
+    reactRouterV6Instrumentation(
+      React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    )(mockStartTransaction, options.startTransactionOnPageLoad, options.startTransactionOnLocationChange);
+    return [mockStartTransaction, { mockSetName, mockFinish }];
+  }
+
+  describe('wrapCreateBrowserRouter', () => {
+    it('starts a pageload transaction', () => {
+      const [mockStartTransaction] = createInstrumentation();
+      const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createMemoryRouter as CreateRouterFunction);
+
+      const router = sentryCreateBrowserRouter(
+        [
+          {
+            path: '/',
+            element: <div>TEST</div>,
+          },
+        ],
+        {
+          initialEntries: ['/'],
+        },
+      );
+
+      render(<RouterProvider router={router} />);
+
+      expect(mockStartTransaction).toHaveBeenCalledTimes(1);
+      expect(mockStartTransaction).toHaveBeenCalledWith({
+        name: '/',
+        op: 'pageload',
+        tags: {
+          'routing.instrumentation': 'react-router-v6',
+        },
+        metadata: {
+          source: 'url',
+        },
+      });
+    });
+
+    it('starts a navigation transaction', () => {
+      const [mockStartTransaction] = createInstrumentation();
+      const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createMemoryRouter as CreateRouterFunction);
+
+      const router = sentryCreateBrowserRouter(
+        [
+          {
+            path: '/',
+            element: <Navigate to="/about" />,
+          },
+          {
+            path: 'about',
+            element: <div>About</div>,
+          },
+        ],
+        {
+          initialEntries: ['/'],
+        },
+      );
+
+      render(<RouterProvider router={router} />);
+
+      expect(mockStartTransaction).toHaveBeenCalledTimes(2);
+      expect(mockStartTransaction).toHaveBeenLastCalledWith({
+        name: '/about',
+        op: 'navigation',
+        tags: { 'routing.instrumentation': 'react-router-v6' },
+        metadata: { source: 'route' },
+      });
+    });
+
+    it('works with nested routes', () => {
+      const [mockStartTransaction] = createInstrumentation();
+      const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createMemoryRouter as CreateRouterFunction);
+
+      const router = sentryCreateBrowserRouter(
+        [
+          {
+            path: '/',
+            element: <Navigate to="/about/us" />,
+          },
+          {
+            path: 'about',
+            element: <div>About</div>,
+            children: [
+              {
+                path: 'us',
+                element: <div>Us</div>,
+              },
+            ],
+          },
+        ],
+        {
+          initialEntries: ['/'],
+        },
+      );
+
+      render(<RouterProvider router={router} />);
+
+      expect(mockStartTransaction).toHaveBeenCalledTimes(2);
+      expect(mockStartTransaction).toHaveBeenLastCalledWith({
+        name: '/about/us',
+        op: 'navigation',
+        tags: { 'routing.instrumentation': 'react-router-v6' },
+        metadata: { source: 'route' },
+      });
+    });
+
+    it('works with parameterized paths', () => {
+      const [mockStartTransaction] = createInstrumentation();
+      const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createMemoryRouter as CreateRouterFunction);
+
+      const router = sentryCreateBrowserRouter(
+        [
+          {
+            path: '/',
+            element: <Navigate to="/about/us" />,
+          },
+          {
+            path: 'about',
+            element: <div>About</div>,
+            children: [
+              {
+                path: ':page',
+                element: <div>Page</div>,
+              },
+            ],
+          },
+        ],
+        {
+          initialEntries: ['/'],
+        },
+      );
+
+      render(<RouterProvider router={router} />);
+
+      expect(mockStartTransaction).toHaveBeenCalledTimes(2);
+      expect(mockStartTransaction).toHaveBeenLastCalledWith({
+        name: '/about/:page',
+        op: 'navigation',
+        tags: { 'routing.instrumentation': 'react-router-v6' },
+        metadata: { source: 'route' },
+      });
+    });
+
+    it('works with paths with multiple parameters', () => {
+      const [mockStartTransaction] = createInstrumentation();
+      const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createMemoryRouter as CreateRouterFunction);
+
+      const router = sentryCreateBrowserRouter(
+        [
+          {
+            path: '/',
+            element: <Navigate to="/stores/foo/products/234" />,
+          },
+          {
+            path: 'stores',
+            element: <div>Stores</div>,
+            children: [
+              {
+                path: ':storeId',
+                element: <div>Store</div>,
+                children: [
+                  {
+                    path: 'products',
+                    element: <div>Products</div>,
+                    children: [
+                      {
+                        path: ':productId',
+                        element: <div>Product</div>,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        {
+          initialEntries: ['/'],
+        },
+      );
+
+      render(<RouterProvider router={router} />);
+
+      expect(mockStartTransaction).toHaveBeenCalledTimes(2);
+      expect(mockStartTransaction).toHaveBeenLastCalledWith({
+        name: '/stores/:storeId/products/:productId',
+        op: 'navigation',
+        tags: { 'routing.instrumentation': 'react-router-v6' },
+        metadata: { source: 'route' },
+      });
+    });
+
+    it('updates pageload transaction to a parameterized route', () => {
+      const [mockStartTransaction, { mockSetName }] = createInstrumentation();
+      const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createMemoryRouter as CreateRouterFunction);
+
+      const router = sentryCreateBrowserRouter(
+        [
+          {
+            path: 'about',
+            element: <div>About</div>,
+            children: [
+              {
+                path: ':page',
+                element: <div>page</div>,
+              },
+            ],
+          },
+        ],
+        {
+          initialEntries: ['/about/us'],
+        },
+      );
+
+      render(<RouterProvider router={router} />);
+
+      expect(mockStartTransaction).toHaveBeenCalledTimes(1);
+      expect(mockSetName).toHaveBeenLastCalledWith('/about/:page', 'route');
+    });
+  });
+});
