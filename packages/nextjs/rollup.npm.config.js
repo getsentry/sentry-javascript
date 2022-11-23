7e1b8bcf466e7dbd5f5f2bf8594ed123c8dd6014
Katie Byers <lobsterkatie@gmail.com>
2022-11-23 00:53:24 -0800

    add `webpack.ts` to rollup config

--- packages/nextjs/rollup.npm.config.js
+++ packages/nextjs/rollup.npm.config.js
@@ -5,7 +5,12 @@ export default [
     makeBaseNPMConfig({
       // We need to include `instrumentServer.ts` separately because it's only conditionally required, and so rollup
       // doesn't automatically include it when calculating the module dependency tree.
-      entrypoints: ['src/index.server.ts', 'src/index.client.ts', 'src/utils/instrumentServer.ts'],
+      entrypoints: [
+        'src/index.server.ts',
+        'src/index.client.ts',
+        'src/utils/instrumentServer.ts',
+        'src/config/webpack.ts',
+      ],
 
       // prevent this internal nextjs code from ending up in our built package (this doesn't happen automatially because
       // the name doesn't match an SDK dependency)
