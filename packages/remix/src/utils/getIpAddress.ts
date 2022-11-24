0f5bb31216d2c7ca7cabcb46be838a93b08c5117
Onur Temizkan <onur@narval.co.uk>
2022-11-24 10:16:22 +0000

    Update packages/remix/src/utils/getIpAddress.ts
    Co-authored-by: Katie Byers <katie.byers@sentry.io>

--- packages/remix/src/utils/getIpAddress.ts
+++ packages/remix/src/utils/getIpAddress.ts
@@ -4,23 +4,6 @@
 import { isIP } from 'net';
 
 /**
- * This is the list of headers, in order of preference, that will be used to
- * determine the client's IP address.
- */
-const headerNames = [
-  'X-Client-IP',
-  'X-Forwarded-For',
-  'Fly-Client-IP',
-  'CF-Connecting-IP',
-  'Fastly-Client-Ip',
-  'True-Client-Ip',
-  'X-Real-IP',
-  'X-Cluster-Client-IP',
-  'X-Forwarded',
-  'Forwarded-For',
-  'Forwarded',
-];
-/**
  * Get the IP address of the client sending a request.
  *
  * It receives a Request headers object and use it to get the
@@ -45,35 +28,58 @@ const headerNames = [
  * will be returned.
  */
 export function getClientIPAddress(headers: Headers): string | null {
-  const ipAddress = headerNames
-    .map((headerName: string) => {
-      const value = headers.get(headerName);
-      if (headerName === 'Forwarded') {
-        return parseForwardedHeader(value);
-      }
-      if (!value?.includes(', ')) return value;
-      return value.split(', ');
-    })
-    .reduce((acc: string[], val) => {
-      if (!val) {
-        return acc;
-      }
+  // The headers to check, in priority order
+  const headerNames = [
+    "X-Client-IP",
+    "X-Forwarded-For",
+    "Fly-Client-IP",
+    "CF-Connecting-IP",
+    "Fastly-Client-Ip",
+    "True-Client-Ip",
+    "X-Real-IP",
+    "X-Cluster-Client-IP",
+    "X-Forwarded",
+    "Forwarded-For",
+    "Forwarded",
+  ];
 
-      return acc.concat(val);
-    }, [])
-    .find(ip => {
-      if (ip === null) return false;
-      return isIP(ip);
-    });
+  // This will end up being Array<string | string[] | undefined | null> because of the various possible values a header
+  // can take
+  const headerValues = headerNames.map((headerName: string) => {
+    const value = headers.get(headerName);
+    
+    if (headerName === "Forwarded") {
+      return parseForwardedHeader(value);
+    }
+    
+    return value?.split(", ");
+  });
 
-  return ipAddress ?? null;
+  // Flatten the array and filter out any falsy entries
+  const flattenedHeaderValues = headerValues.reduce((acc: string[], val) => {
+    if (!val) {
+      return acc;
+    }
+
+    return acc.concat(val);
+  }, []);
+
+  // Find the first value which is a valid IP address, if any
+  const ipAddress = flattenedHeaderValues.find((ip) => ip !== null && isIP(ip));
+
+  return ipAddress || null;
 }
 
 function parseForwardedHeader(value: string | null): string | null {
-  if (!value) return null;
-  for (const part of value.split(';')) {
-    if (part.startsWith('for=')) return part.slice(4);
-    continue;
+  if (!value) {
+    return null;
   }
+
+  for (const part of value.split(";")) {
+    if (part.startsWith("for=")) {
+      return part.slice(4);
+    }
+  }
+
   return null;
 }
