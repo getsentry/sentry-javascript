12a90b62c17a1b72a59d9ab201817ba0470dfc2c
Lukas Stracke <lukas.stracke@sentry.io>
2022-11-29 11:30:28 +0100

    fix test tsconfig and jest config
    test
    
    more test
    
    remove duplicated `globals` entry in jest.config after rebase
    
    add `__DEBUG_BUILD__`

--- packages/replay/jest.config.ts
+++ packages/replay/jest.config.ts
@@ -1,23 +1,24 @@
 import type { Config } from '@jest/types';
 import { pathsToModuleNameMapper } from 'ts-jest';
+import { jsWithTs as jsWithTsPreset } from 'ts-jest/presets';
 
-import { compilerOptions } from './tsconfig.json';
+import { compilerOptions } from './tsconfig.test.json';
 
 export default async (): Promise<Config.InitialOptions> => {
   return {
+    ...jsWithTsPreset,
     verbose: true,
-    preset: 'ts-jest/presets/js-with-ts', // needed when import worker.js
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/tsconfig.test.json',
+      },
+      __DEBUG_BUILD__: true,
+    },
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
       prefix: '<rootDir>/',
     }),
     setupFilesAfterEnv: ['./jest.setup.ts'],
     testEnvironment: 'jsdom',
     testMatch: ['<rootDir>/test/**/*(*.)@(spec|test).ts'],
-    globals: {
-      'ts-jest': {
-        tsconfig: '<rootDir>/tsconfig.json',
-      },
-      __DEBUG_BUILD__: true,
-    },
   };
 };
