9b6a565288f91317a194cbe6c64b004890aa3ce6
Lukas Stracke <lukas.stracke@sentry.io>
2022-11-29 14:50:41 +0100

    fix liner

--- packages/replay/jest.config.ts
+++ packages/replay/jest.config.ts
@@ -1,21 +1,14 @@
 import type { Config } from '@jest/types';
 import { pathsToModuleNameMapper } from 'ts-jest';
 
+// @ts-ignore
+import test from '../../jest/jest.config';
 import { compilerOptions } from './tsconfig.test.json';
 
-import { jsWithTs as jsWithTsPreset } from 'ts-jest/presets';
-
 export default async (): Promise<Config.InitialOptions> => {
-  console.log(jsWithTsPreset);
-
   return {
-    ...jsWithTsPreset,
+    ...test,
     verbose: true,
-    globals: {
-      'ts-jest': {
-        tsconfig: '<rootDir>/tsconfig.test.json',
-      },
-    },
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
       prefix: '<rootDir>/',
     }),
