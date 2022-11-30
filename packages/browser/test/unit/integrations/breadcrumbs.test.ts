226e91e8d22822b3202c9eab4efb1e4f9fefe507
Tim Fish <tim@timfish.uk>
2022-11-30 11:43:09 +0100

    Add sentry breadcrumb test

---
+++ packages/browser/test/unit/integrations/breadcrumbs.test.ts
@@ -0,0 +1,34 @@
+import { BrowserClient, Breadcrumbs, Hub, flush } from '../../../src';
+import { getCurrentHub } from '@sentry/core';
+import { getDefaultBrowserClientOptions } from '../helper/browser-client-options';
+
+const hub = new Hub();
+
+jest.mock('@sentry/core', () => {
+  const original = jest.requireActual('@sentry/core');
+  return {
+    ...original,
+    getCurrentHub: () => hub,
+  };
+});
+
+describe('Breadcrumbs', () => {
+  it('Should add sentry breadcrumb', async () => {
+    const addBreadcrumb = jest.fn();
+    hub.addBreadcrumb = addBreadcrumb;
+
+    const client = new BrowserClient({
+      ...getDefaultBrowserClientOptions(),
+      dsn: 'https://username@domain/123',
+      integrations: [new Breadcrumbs()],
+    });
+
+    getCurrentHub().bindClient(client);
+
+    client.captureMessage('test');
+    await flush(2000);
+
+    expect(addBreadcrumb.mock.calls[0][0].category).toEqual('sentry.event');
+    expect(addBreadcrumb.mock.calls[0][0].message).toEqual('test');
+  });
+});
