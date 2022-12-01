e285e50eeba9e372dc2cc35d70a9520bd45a9d1c
Lukas Stracke <lukas.stracke@sentry.io>
2022-12-01 10:56:58 +0100

    build(replay): Generate Replay Addon CDN bundles

---
+++ packages/replay/rollup.bundle.config.js
@@ -0,0 +1,36 @@
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+
+import { makeBaseBundleConfig, makeBundleConfigVariants } from '../../rollup/index.js';
+
+import pkg from './package.json';
+
+const baseBundleConfig = makeBaseBundleConfig({
+  bundleType: 'addon',
+  entrypoints: ['src/index.ts'],
+  jsVersion: 'es6',
+  licenseTitle: '@sentry/replay',
+  outputFileBase: () => 'bundles/replay',
+  packageSpecificConfig: {
+    // We don't add @sentry/browser to the bundle, hence we mark peerDeps as external
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          __SENTRY_REPLAY_VERSION__: JSON.stringify(pkg.version),
+        },
+      }),
+      commonjs(),
+    ],
+    output: {
+      // set exports to 'named' or 'auto' so that rollup doesn't warn about
+      // the default export in `worker/worker.js`
+      exports: 'auto',
+    },
+  },
+});
+
+const builds = makeBundleConfigVariants(baseBundleConfig);
+
+export default builds;
