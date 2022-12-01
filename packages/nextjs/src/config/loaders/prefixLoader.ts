22fab0f8abb76d0c37070c1ad0def28d0175109f
Katie Byers <lobsterkatie@gmail.com>
2022-12-01 06:34:08 -0800

    parameterize prefix loader template name and replacement values

--- packages/nextjs/src/config/loaders/prefixLoader.ts
+++ packages/nextjs/src/config/loaders/prefixLoader.ts
@@ -1,26 +1,38 @@
+import { escapeStringForRegex } from '@sentry/utils';
 import * as fs from 'fs';
 import * as path from 'path';
 
 import { LoaderThis } from './types';
 
 type LoaderOptions = {
-  distDir: string;
+  templatePrefix: string;
+  replacements: Array<[string, string]>;
 };
 
 /**
  * Inject templated code into the beginning of a module.
+ *
+ * Options:
+ *   - `templatePrefix`: The XXX in `XXXPrefixLoaderTemplate.ts`, to specify which template to use
+ *   - `replacements`: An array of tuples of the form `[<placeholder>, <replacementValue>]`, used for doing global
+ *        string replacement in the template. Note: The replacement is done sequentially, in the order in which the
+ *        replacement values are given. If any placeholder is a substring of any replacement value besides its own, make
+ *        sure to order the tuples in such a way as to avoid over-replacement.
  */
 export default function prefixLoader(this: LoaderThis<LoaderOptions>, userCode: string): string {
   // We know one or the other will be defined, depending on the version of webpack being used
-  const { distDir } = 'getOptions' in this ? this.getOptions() : this.query;
+  const { templatePrefix, replacements } = 'getOptions' in this ? this.getOptions() : this.query;
 
-  const templatePath = path.resolve(__dirname, '../templates/prefixLoaderTemplate.js');
+  const templatePath = path.resolve(__dirname, `../templates/${templatePrefix}PrefixLoaderTemplate.js`);
   // make sure the template is included when runing `webpack watch`
   this.addDependency(templatePath);
 
-  // Fill in the placeholder
+  // Fill in placeholders
   let templateCode = fs.readFileSync(templatePath).toString();
-  templateCode = templateCode.replace('__DIST_DIR__', distDir.replace(/\\/g, '\\\\'));
+  replacements.forEach(([placeholder, value]) => {
+    const placeholderRegex = new RegExp(escapeStringForRegex(placeholder), 'g');
+    templateCode = templateCode.replace(placeholderRegex, value);
+  });
 
   return `${templateCode}\n${userCode}`;
 }
