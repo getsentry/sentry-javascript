6e915c31c5243513efb7f50cfbc4adc419699f08
Lukas Stracke <lukas.stracke@sentry.io>
2022-12-01 14:49:12 +0100

    build(replay): Remove replay version string replacement (#6367)
    Remove the magic string replacement of `__SENTRY_REPLAY_VERSION__` at build time in favour of `@sentry/core`'s `SENTRY_VERSION` constant.

--- packages/replay/rollup.npm.config.js
+++ packages/replay/rollup.npm.config.js
@@ -1,5 +1,3 @@
-import replace from '@rollup/plugin-replace';
-
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index';
 
 import pkg from './package.json';
@@ -9,15 +7,6 @@ export default makeNPMConfigVariants(
     hasBundles: true,
     packageSpecificConfig: {
       external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-      plugins: [
-        // TODO: Remove this - replay version will be in sync w/ SDK version
-        replace({
-          preventAssignment: true,
-          values: {
-            __SENTRY_REPLAY_VERSION__: JSON.stringify(pkg.version),
-          },
-        }),
-      ],
       output: {
         // set exports to 'named' or 'auto' so that rollup doesn't warn about
         // the default export in `worker/worker.js`
