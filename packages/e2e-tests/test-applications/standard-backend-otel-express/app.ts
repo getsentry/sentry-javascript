6430065bd6730ab9b177ef0fd054aca59d138c02
Abhijeet Prasad <aprasad@sentry.io>
2022-12-02 13:01:28 +0100

    add tests

--- packages/e2e-tests/test-applications/standard-backend-otel-express/app.ts
+++ packages/e2e-tests/test-applications/standard-backend-otel-express/app.ts
@@ -1,14 +1,28 @@
 import express from 'express';
-import * as Sentry from '@sentry/node';
+// import * as Sentry from '@sentry/node';
+// import { SentryPropagator, SentrySpanProcessor } from '@sentry/opentelemetry-node';
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
-Sentry.init({ dsn: process.env.E2E_TEST_DSN, instrumenter: 'otel' });
+// Sentry.init({ dsn: process.env.E2E_TEST_DSN, instrumenter: 'otel' });
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  instrumentations: [getNodeAutoInstrumentations()],
+
+  // Sentry config
+  // spanProcessor: new SentrySpanProcessor(),
+  // textMapPropagator: new SentryPropagator(),
+});
+
+sdk.start();
 
 const app = express();
 
 const port = 4000;
 
 // The request handler must be the first middleware on the app
-app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.requestHandler());
 
 app.get('/', (_req, res) => {
   res.send('GET request to /');
@@ -24,12 +38,12 @@ app.get('/users/:id', (req, res) => {
 
 app.get('/error', (_req, res) => {
   const error = new Error('GET request to /error');
-  Sentry.captureException(error);
+  // Sentry.captureException(error);
   res.status(500).send('GET request with error to /error');
 });
 
 // The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
+// app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
