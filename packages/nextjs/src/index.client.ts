95282f3a68b038c15e4dab91e8be7c475f0b54d1
Luca Forstner <luca.forstner@sentry.io>
2022-12-05 09:48:08 +0000

    Skip init in client side sdk

--- packages/nextjs/src/index.client.ts
+++ packages/nextjs/src/index.client.ts
@@ -28,8 +28,20 @@ export { BrowserTracing };
 // Treeshakable guard to remove all code related to tracing
 declare const __SENTRY_TRACING__: boolean;
 
+// This is a variable that Next.js will string replace during build with a string if run in an edge runtime from Next.js
+// v12.2.1-canary.3 onwards:
+// https://github.com/vercel/next.js/blob/166e5fb9b92f64c4b5d1f6560a05e2b9778c16fb/packages/next/build/webpack-config.ts#L206
+declare const EdgeRuntime: string | undefined;
+
 /** Inits the Sentry NextJS SDK on the browser with the React SDK. */
 export function init(options: NextjsOptions): void {
+  if (typeof EdgeRuntime === 'string') {
+    // If the SDK is imported when using the Vercel Edge Runtime, it will import the browser SDK, even though it is
+    // running the server part of a Next.js application. We can use the `EdgeRuntime` to check for that case and make
+    // the init call a no-op. This will prevent the SDK from crashing on the Edge Runtime.
+    return;
+  }
+
   buildMetadata(options, ['nextjs', 'react']);
   options.environment = options.environment || process.env.NODE_ENV;
 
