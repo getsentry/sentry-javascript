93a338e69d76f1f7fb3179246e27a0057aa755cb
JonasBa <jonas@badalic.com>
2022-12-07 17:18:10 -0500

    feat(profiling): add profile envelope item type

--- packages/types/src/envelope.ts
+++ packages/types/src/envelope.ts
@@ -26,7 +26,8 @@ export type EnvelopeItemType =
   | 'sessions'
   | 'transaction'
   | 'attachment'
-  | 'event';
+  | 'event'
+  | 'profile';
 
 export type BaseEnvelopeHeaders = {
   [key: string]: unknown;
