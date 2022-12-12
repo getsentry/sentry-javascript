4e8f3aab7331d36fbe4a68ead4615f9f0f78ac14
Luca Forstner <luca.forstner@sentry.io>
2022-12-12 07:35:57 +0000

    Improve comment wording

--- packages/nextjs/src/config/loaders/valueInjectionLoader.ts
+++ packages/nextjs/src/config/loaders/valueInjectionLoader.ts
@@ -8,7 +8,7 @@ type LoaderOptions = {
  * Set values on the global/window object at the start of a module.
  *
  * Options:
- *   - `values`: A record where the keys correspond to the keys of the global values to set and the values
+ *   - `values`: An object where the keys correspond to the keys of the global values to set and the values
  *        correspond to the values of the values on the global object. Values must be JSON serializable.
  */
 export default function valueInjectionLoader(this: LoaderThis<LoaderOptions>, userCode: string): string {
