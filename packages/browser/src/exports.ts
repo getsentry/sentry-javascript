fcb608f638af4752a847bd8b2f8090bcc019187d
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-12-13 14:56:13 +0100

    chore(internal): Export event builder method for use in other sdks

--- packages/browser/src/exports.ts
+++ packages/browser/src/exports.ts
@@ -57,5 +57,6 @@ export {
   opera11StackLineParser,
   winjsStackLineParser,
 } from './stack-parsers';
+export { eventFromException, eventFromMessage, parseStackFrames } from './eventbuilder';
 export { defaultIntegrations, forceLoad, init, lastEventId, onLoad, showReportDialog, flush, close, wrap } from './sdk';
 export { GlobalHandlers, TryCatch, Breadcrumbs, LinkedErrors, HttpContext, Dedupe } from './integrations';
