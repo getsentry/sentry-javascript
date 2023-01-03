0585512a2b9c1ea1eb55c8f1361d5b56c59ab87e
Tim Fish <tim@timfish.uk>
2023-01-03 18:06:22 +0000

    Also check isElectronNodeRenderer

--- packages/replay/src/util/isBrowser.ts
+++ packages/replay/src/util/isBrowser.ts
@@ -4,3 +4,9 @@ export function isBrowser(): boolean {
   // eslint-disable-next-line no-restricted-globals
   return typeof window !== 'undefined' && !isNodeEnv();
 }
+
+type ElectronProcess = { type?: string };
+
+export function isElectronNodeRenderer(): boolean {
+  return typeof process !== 'undefined' && (process as ElectronProcess).type === 'renderer';
+}
