23a7d0b2fc418a90ae2559f2680c6d417b6d17d2
Lukas Stracke <lukas.stracke@sentry.io>
2023-01-04 09:17:24 +0100

    ref(replay): Replace `lodash.debounce` with custom debounce implementation (#6593)
    Replace lodash's `debounce` function with a custom, minimal implementation that
    - Delays the function invocation by a `wait` time and gate it with a `maxWait` value
    - Provides the return value of the invocation for subsequent debounced function calls
    - Provides a `flush` and `cancel` function on the debounced function (analogously to lodash).
    - Invokes the function _after_ the wait/maxwait time triggered (i.e. on the trailing edge). Lodash allows to choose between the leading and trailing edge, which makes the implementation much more complicated than for us necessary
    - Works for functions _without_ parameters. By not supporting args, we can further cut down bundle size. (We might want to revisit this in the future but for now, this should do).
    
    With this change, we can also get rid of the package patch we introduced in #6551 and of the `commonjs()` plugin in our build process.

--- packages/replay/src/replay.ts
+++ packages/replay/src/replay.ts
@@ -2,7 +2,6 @@
 import { addGlobalEventProcessor, captureException, getCurrentHub, setContext } from '@sentry/core';
 import { Breadcrumb, ReplayEvent } from '@sentry/types';
 import { addInstrumentationHandler, logger } from '@sentry/utils';
-import debounce from 'lodash.debounce';
 import { EventType, record } from 'rrweb';
 
 import {
@@ -45,6 +44,7 @@ import { createBreadcrumb } from './util/createBreadcrumb';
 import { createPayload } from './util/createPayload';
 import { createPerformanceSpans } from './util/createPerformanceSpans';
 import { createReplayEnvelope } from './util/createReplayEnvelope';
+import { debounce } from './util/debounce';
 import { getReplayEvent } from './util/getReplayEvent';
 import { isExpired } from './util/isExpired';
 import { isSessionExpired } from './util/isSessionExpired';
@@ -859,7 +859,6 @@ export class ReplayContainer implements ReplayContainerInterface {
     // A flush is about to happen, cancel any queued flushes
     this._debouncedFlush?.cancel();
 
-    // No existing flush in progress, proceed with flushing.
     // this._flushLock acts as a lock so that future calls to `flush()`
     // will be blocked until this promise resolves
     if (!this._flushLock) {
@@ -892,8 +891,8 @@ export class ReplayContainer implements ReplayContainerInterface {
    */
   flushImmediate(): Promise<void> {
     this._debouncedFlush();
-    // `.flush` is provided by lodash.debounce
-    return this._debouncedFlush.flush();
+    // `.flush` is provided by the debounced function, analogously to lodash.debounce
+    return this._debouncedFlush.flush() as Promise<void>;
   }
 
   /**
