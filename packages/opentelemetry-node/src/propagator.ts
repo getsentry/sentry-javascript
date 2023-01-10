99401ea89725e1add6cc15a60cfd8994d5780397
Abhijeet Prasad <aprasad@sentry.io>
2023-01-10 13:50:11 +0100

    fix(otel): Prevent baggage from being overwritten

--- packages/opentelemetry-node/src/propagator.ts
+++ packages/opentelemetry-node/src/propagator.ts
@@ -1,17 +1,18 @@
 import {
+  Baggage,
   Context,
   isSpanContextValid,
+  propagation,
   TextMapGetter,
-  TextMapPropagator,
   TextMapSetter,
   trace,
   TraceFlags,
 } from '@opentelemetry/api';
-import { isTracingSuppressed } from '@opentelemetry/core';
+import { isTracingSuppressed, W3CBaggagePropagator } from '@opentelemetry/core';
 import {
   baggageHeaderToDynamicSamplingContext,
-  dynamicSamplingContextToSentryBaggageHeader,
   extractTraceparentData,
+  SENTRY_BAGGAGE_KEY_PREFIX,
 } from '@sentry/utils';
 
 import {
@@ -25,7 +26,7 @@ import { SENTRY_SPAN_PROCESSOR_MAP } from './spanprocessor';
 /**
  * Injects and extracts `sentry-trace` and `baggage` headers from carriers.
  */
-export class SentryPropagator implements TextMapPropagator {
+export class SentryPropagator extends W3CBaggagePropagator {
   /**
    * @inheritDoc
    */
@@ -41,10 +42,18 @@ export class SentryPropagator implements TextMapPropagator {
 
       if (span.transaction) {
         const dynamicSamplingContext = span.transaction.getDynamicSamplingContext();
-        const sentryBaggageHeader = dynamicSamplingContextToSentryBaggageHeader(dynamicSamplingContext);
-        if (sentryBaggageHeader) {
-          setter.set(carrier, SENTRY_BAGGAGE_HEADER, sentryBaggageHeader);
-        }
+
+        const baggage = propagation.getBaggage(context) || propagation.createBaggage({});
+        const baggageWithSentryInfo = Object.entries(dynamicSamplingContext).reduce<Baggage>(
+          (b, [dscKey, dscValue]) => {
+            if (dscValue) {
+              return b.setEntry(`${SENTRY_BAGGAGE_KEY_PREFIX}${dscKey}`, { value: dscValue });
+            }
+            return b;
+          },
+          baggage,
+        );
+        super.inject(propagation.setBaggage(context, baggageWithSentryInfo), carrier, setter);
       }
     }
   }
