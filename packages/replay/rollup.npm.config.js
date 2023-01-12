b1c3dd5ddb00aaa8a7950a57caa30d0ae2e15b8a
Lukas Stracke <lukas.stracke@sentry.io>
2023-01-12 16:22:57 +0100

    build(replay): Rename `node_modules` directory inside build output directories

--- packages/replay/rollup.npm.config.js
+++ packages/replay/rollup.npm.config.js
@@ -1,6 +1,7 @@
 import path from 'path';
 
 import replace from '@rollup/plugin-replace';
+import renameNodeModules from 'rollup-plugin-rename-node-modules';
 
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index';
 
@@ -18,6 +19,7 @@ export default makeNPMConfigVariants(
             __SENTRY_REPLAY_VERSION__: JSON.stringify(pkg.version),
           },
         }),
+        renameNodeModules('ext'),
       ],
       output: {
         // set exports to 'named' or 'auto' so that rollup doesn't warn about
