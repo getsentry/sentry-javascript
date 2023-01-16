8c777732a85e503ce1f5e8c5f7ac57fd63b6e172
Lukas Stracke <lukas.stracke@sentry.io>
2023-01-16 18:05:01 +0100

    ref(replay): Send SDK version in Replay events

--- packages/replay/rollup.npm.config.js
+++ packages/replay/rollup.npm.config.js
@@ -1,22 +1,9 @@
-import replace from '@rollup/plugin-replace';
-
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index';
-
-import pkg from './package.json';
 
 export default makeNPMConfigVariants(
   makeBaseNPMConfig({
     hasBundles: true,
     packageSpecificConfig: {
-      plugins: [
-        // TODO: Remove this - replay version will be in sync w/ SDK version
-        replace({
-          preventAssignment: true,
-          values: {
-            __SENTRY_REPLAY_VERSION__: JSON.stringify(pkg.version),
-          },
-        }),
-      ],
       output: {
         // set exports to 'named' or 'auto' so that rollup doesn't warn about
         // the default export in `worker/worker.js`
