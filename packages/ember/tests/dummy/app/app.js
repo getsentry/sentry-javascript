2ca09197e9e5f38bafc9c93c7749c5dc582cfc33
Francesco Novy <francesco.novy@sentry.io>
2023-01-18 11:31:38 +0100

    test(ember): Ensure tests are not flak

--- packages/ember/tests/dummy/app/app.js
+++ packages/ember/tests/dummy/app/app.js
@@ -4,7 +4,14 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import * as Sentry from '@sentry/ember';
 
-Sentry.init();
+Sentry.init({
+  browserTracingOptions: {
+    _experiments: {
+      // This lead to some flaky tests, as that is sometimes logged
+      enableLongTask: false,
+    },
+  },
+});
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
