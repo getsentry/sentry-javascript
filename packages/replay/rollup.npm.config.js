18146a7f8ca0dc5f460914134a595b11c4280a12
Lukas Stracke <lukas.stracke@sentry.io>
2023-01-18 13:42:17 +0100

    add comment to rollup config

--- packages/replay/rollup.npm.config.js
+++ packages/replay/rollup.npm.config.js
@@ -8,6 +8,8 @@ export default makeNPMConfigVariants(
         // set exports to 'named' or 'auto' so that rollup doesn't warn about
         // the default export in `worker/worker.js`
         exports: 'named',
+        // set preserveModules to false because for Replay we actually want
+        // to bundle everything into one file.
         preserveModules: false,
       },
     },
