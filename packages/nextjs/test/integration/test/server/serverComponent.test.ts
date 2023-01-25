e214d97dea30fffeb5f4adc9965c8489d9e7d4a0
Luca Forstner <luca.forstner@sentry.io>
2023-01-25 19:35:33 +0000

    Skip test

--- packages/nextjs/test/integration/test/server/serverComponent.test.ts
+++ packages/nextjs/test/integration/test/server/serverComponent.test.ts
@@ -2,6 +2,10 @@ import { NextTestEnv } from './utils/helpers';
 
 describe('Loading the server component', () => {
   it('should capture an error event', async () => {
+    if (process.env.USE_APPDIR !== 'true') {
+      return;
+    }
+
     const env = await NextTestEnv.init();
     const url = `${env.url}/servercomponent`;
 
