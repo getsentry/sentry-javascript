57b44dfda6fbce0c064bbad8c57e94ac86394362
Luca Forstner <luca.forstner@sentry.io>
2023-01-27 12:54:33 +0000

    🤯

--- packages/nextjs/src/edge/wrapAppDirComponentWithSentry.ts
+++ packages/nextjs/src/edge/wrapAppDirComponentWithSentry.ts
@@ -4,10 +4,12 @@ import { captureException } from '@sentry/core';
  * Wraps an `app` directory server component with Sentry error instrumentation.
  */
 export function wrapAppDirComponentWithSentry(wrappingTarget: any): any {
-  return async function WrappedPage(this: unknown, ...args: any[]) {
+  // Super interesting: even though users may define server components as async functions, Next.js will turn them into
+  // synchronous functions and it will transform any`await`s into instances of the`use` hook. 🤯
+  return function sentryWrappedServerComponent(this: unknown, ...args: any[]) {
     try {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      return await wrappingTarget.apply(this, args);
+      return wrappingTarget.apply(this, args);
     } catch (e) {
       captureException(e);
       throw e;
