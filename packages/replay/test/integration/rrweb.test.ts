62aafd9f08767e21e6f98d21b1416349030541b0
Billy Vong <billyvg@users.noreply.github.com>
2023-01-31 12:03:58 -0500

    update snapshot

--- packages/replay/test/integration/rrweb.test.ts
+++ packages/replay/test/integration/rrweb.test.ts
@@ -25,6 +25,7 @@ describe('Integration | rrweb', () => {
         "inlineStylesheet": true,
         "maskAllInputs": true,
         "maskInputFn": undefined,
+        "maskInputOptions": undefined,
         "maskInputSelector": ".sentry-mask,[data-sentry-mask]",
         "maskTextFn": undefined,
         "maskTextSelector": "body *:not(style), body *:not(script)",
