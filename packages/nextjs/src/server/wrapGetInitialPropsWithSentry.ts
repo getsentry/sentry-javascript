33f17d1cc9b64a7527b1b313ac60513f4be3c763
Luca Forstner <luca.forstner@sentry.io>
2023-01-31 16:19:47 +0000

    fix(nextjs): Use Proxies to wrap to preserve static methods

--- packages/nextjs/src/server/wrapGetInitialPropsWithSentry.ts
+++ packages/nextjs/src/server/wrapGetInitialPropsWithSentry.ts
@@ -20,46 +20,48 @@ type GetInitialProps = Required<NextPage>['getInitialProps'];
  * @returns A wrapped version of the function
  */
 export function wrapGetInitialPropsWithSentry(origGetInitialProps: GetInitialProps): GetInitialProps {
-  return async function (this: unknown, ...args: Parameters<GetInitialProps>): Promise<ReturnType<GetInitialProps>> {
-    if (isBuild()) {
-      return origGetInitialProps.apply(this, args);
-    }
-
-    const [context] = args;
-    const { req, res } = context;
-
-    const errorWrappedGetInitialProps = withErrorInstrumentation(origGetInitialProps);
-    const options = getCurrentHub().getClient()?.getOptions();
-
-    // Generally we can assume that `req` and `res` are always defined on the server:
-    // https://nextjs.org/docs/api-reference/data-fetching/get-initial-props#context-object
-    // This does not seem to be the case in dev mode. Because we have no clean way of associating the the data fetcher
-    // span with each other when there are no req or res objects, we simply do not trace them at all here.
-    if (hasTracingEnabled() && req && res && options?.instrumenter === 'sentry') {
-      const tracedGetInitialProps = withTracedServerSideDataFetcher(errorWrappedGetInitialProps, req, res, {
-        dataFetcherRouteName: context.pathname,
-        requestedRouteName: context.pathname,
-        dataFetchingMethodName: 'getInitialProps',
-      });
-
-      const initialProps: {
-        _sentryTraceData?: string;
-        _sentryBaggage?: string;
-      } = await tracedGetInitialProps.apply(this, args);
-
-      const requestTransaction = getTransactionFromRequest(req);
-      if (requestTransaction) {
-        initialProps._sentryTraceData = requestTransaction.toTraceparent();
-
-        const dynamicSamplingContext = requestTransaction.getDynamicSamplingContext();
-        initialProps._sentryBaggage = dynamicSamplingContextToSentryBaggageHeader(dynamicSamplingContext);
+  return new Proxy(origGetInitialProps, {
+    apply: async (wrappingTarget, thisArg, args: Parameters<GetInitialProps>) => {
+      if (isBuild()) {
+        return wrappingTarget.apply(thisArg, args);
       }
 
-      return initialProps;
-    } else {
-      return errorWrappedGetInitialProps.apply(this, args);
-    }
-  };
+      const [context] = args;
+      const { req, res } = context;
+
+      const errorWrappedGetInitialProps = withErrorInstrumentation(wrappingTarget);
+      const options = getCurrentHub().getClient()?.getOptions();
+
+      // Generally we can assume that `req` and `res` are always defined on the server:
+      // https://nextjs.org/docs/api-reference/data-fetching/get-initial-props#context-object
+      // This does not seem to be the case in dev mode. Because we have no clean way of associating the the data fetcher
+      // span with each other when there are no req or res objects, we simply do not trace them at all here.
+      if (hasTracingEnabled() && req && res && options?.instrumenter === 'sentry') {
+        const tracedGetInitialProps = withTracedServerSideDataFetcher(errorWrappedGetInitialProps, req, res, {
+          dataFetcherRouteName: context.pathname,
+          requestedRouteName: context.pathname,
+          dataFetchingMethodName: 'getInitialProps',
+        });
+
+        const initialProps: {
+          _sentryTraceData?: string;
+          _sentryBaggage?: string;
+        } = await tracedGetInitialProps.apply(thisArg, args);
+
+        const requestTransaction = getTransactionFromRequest(req);
+        if (requestTransaction) {
+          initialProps._sentryTraceData = requestTransaction.toTraceparent();
+
+          const dynamicSamplingContext = requestTransaction.getDynamicSamplingContext();
+          initialProps._sentryBaggage = dynamicSamplingContextToSentryBaggageHeader(dynamicSamplingContext);
+        }
+
+        return initialProps;
+      } else {
+        return errorWrappedGetInitialProps.apply(thisArg, args);
+      }
+    },
+  });
 }
 
 /**
