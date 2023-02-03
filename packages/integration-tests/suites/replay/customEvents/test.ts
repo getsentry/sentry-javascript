89dea4c5add111e5d700af675ccf5ce5c4c4001b
Lukas Stracke <lukas.stracke@sentry.io>
2023-02-03 16:04:55 +0100

    only run on chromium

--- packages/integration-tests/suites/replay/customEvents/test.ts
+++ packages/integration-tests/suites/replay/customEvents/test.ts
@@ -11,50 +11,55 @@ import {
 } from '../../../utils/replayEventTemplates';
 import { getCustomRecordingEvents, getReplayEvent, waitForReplayRequest } from '../../../utils/replayHelpers';
 
-sentryTest('replay recording should contain default performance spans', async ({ getLocalTestPath, page }) => {
-  // Replay bundles are es6 only
-  if (process.env.PW_BUNDLE && process.env.PW_BUNDLE.startsWith('bundle_es5')) {
-    sentryTest.skip();
-  }
+sentryTest(
+  'replay recording should contain default performance spans',
+  async ({ getLocalTestPath, page, browserName }) => {
+    // Replay bundles are es6 only and most performance entries are only available in chromium
+    if ((process.env.PW_BUNDLE && process.env.PW_BUNDLE.startsWith('bundle_es5')) || browserName !== 'chromium') {
+      sentryTest.skip();
+    }
 
-  const reqPromise0 = waitForReplayRequest(page, 0);
-  const reqPromise1 = waitForReplayRequest(page, 1);
+    const reqPromise0 = waitForReplayRequest(page, 0);
+    const reqPromise1 = waitForReplayRequest(page, 1);
 
-  await page.route('https://dsn.ingest.sentry.io/**/*', route => {
-    return route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({ id: 'test-id' }),
+    await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+      return route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ id: 'test-id' }),
+      });
     });
-  });
 
-  const url = await getLocalTestPath({ testDir: __dirname });
+    const url = await getLocalTestPath({ testDir: __dirname });
 
-  await page.goto(url);
-  const replayEvent0 = getReplayEvent(await reqPromise0);
-  const { performanceSpans: performanceSpans0 } = getCustomRecordingEvents(await reqPromise0);
+    await page.goto(url);
+    const replayEvent0 = getReplayEvent(await reqPromise0);
+    const { performanceSpans: performanceSpans0 } = getCustomRecordingEvents(await reqPromise0);
 
-  expect(replayEvent0).toEqual(getExpectedReplayEvent({ segment_id: 0 }));
+    expect(replayEvent0).toEqual(getExpectedReplayEvent({ segment_id: 0 }));
 
-  await page.click('button');
+    await page.click('button');
 
-  const replayEvent1 = getReplayEvent(await reqPromise1);
-  const { performanceSpans: performanceSpans1 } = getCustomRecordingEvents(await reqPromise1);
+    const replayEvent1 = getReplayEvent(await reqPromise1);
+    const { performanceSpans: performanceSpans1 } = getCustomRecordingEvents(await reqPromise1);
 
-  expect(replayEvent1).toEqual(getExpectedReplayEvent({ segment_id: 1, urls: [], replay_start_timestamp: undefined }));
+    expect(replayEvent1).toEqual(
+      getExpectedReplayEvent({ segment_id: 1, urls: [], replay_start_timestamp: undefined }),
+    );
 
-  // We can't guarantee the order of the performance spans, or in which of the two segments they are sent
-  // So we collect them all and check that they are all there
-  const collectedPerformanceSpans = [...performanceSpans0, ...performanceSpans1];
+    // We can't guarantee the order of the performance spans, or in which of the two segments they are sent
+    // So to avoid flakes, we collect them all and check that they are all there
+    const collectedPerformanceSpans = [...performanceSpans0, ...performanceSpans1];
 
-  expect(collectedPerformanceSpans).toEqual(
-    expect.arrayContaining([
-      expectedNavigationPerformanceSpan,
-      expectedLCPPerformanceSpan,
-      expectedFPPerformanceSpan,
-      expectedFCPPerformanceSpan,
-      expectedMemoryPerformanceSpan, // two memory spans - once per flush
-      expectedMemoryPerformanceSpan,
-    ]),
-  );
-});
+    expect(collectedPerformanceSpans).toEqual(
+      expect.arrayContaining([
+        expectedNavigationPerformanceSpan,
+        expectedLCPPerformanceSpan,
+        expectedFPPerformanceSpan,
+        expectedFCPPerformanceSpan,
+        expectedMemoryPerformanceSpan, // two memory spans - once per flush
+        expectedMemoryPerformanceSpan,
+      ]),
+    );
+  },
+);
