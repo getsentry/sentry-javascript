bfc2b30f0daf3bdad318027cb9a4a70d48d3799d
Tim Fish <tim@timfish.uk>
2023-02-05 19:45:29 +0100

    add an integration test

---
+++ packages/integration-tests/suites/transport/offline/queued/test.ts
@@ -0,0 +1,39 @@
+import { expect } from '@playwright/test';
+import type { Event } from '@sentry/types';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getMultipleSentryEnvelopeRequests } from '../../../../utils/helpers';
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sentryTest('should queue and retry events when they fail to send', async ({ getLocalTestPath, page }) => {
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  // This would be the obvious way to test offline support but it doesn't appear to work!
+  // await context.setOffline(true);
+
+  // Abort all envelope requests so the event gets queued
+  await page.route(/ingest\.sentry\.io/, route => route.abort());
+  await page.goto(url);
+  await delay(1_000);
+  await page.unroute(/ingest\.sentry\.io/);
+
+  // The previous event should now be queued
+
+  // This will force the page to be reloaded and a new event to be sent
+  const eventData = await getMultipleSentryEnvelopeRequests<Event>(page, 3, { url, timeout: 10_000 });
+
+  // Filter out any client reports
+  const events = eventData.filter(e => !('discarded_events' in e)) as Event[];
+
+  expect(events).toHaveLength(2);
+
+  // The next two events will be message events starting with 'foo'
+  expect(events[0].message?.startsWith('foo'));
+  expect(events[1].message?.startsWith('foo'));
+
+  // But because these are two different events, they should have different random numbers in the message
+  expect(events[0].message !== events[1].message);
+});
