8d7514927b26887bae54f681bd559f8a213cbf54
Francesco Novy <francesco.novy@sentry.io>
2023-02-09 15:53:50 +0100

    ref: Extract worker into package

---
+++ packages/replay-worker/rollup.worker.config.js
@@ -0,0 +1,35 @@
+// inspired by https://justinribeiro.com/chronicle/2020/07/17/building-module-web-workers-for-cross-browser-compatibility-with-rollup/
+
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
+import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
+
+const config = defineConfig({
+  input: ['./src/index.ts'],
+  output: {
+    dir: './build/',
+    format: 'esm',
+  },
+  plugins: [
+    typescript({ tsconfig: './tsconfig.json', inlineSourceMap: false, sourceMap: false, inlineSources: false }),
+    resolve(),
+    terser({
+      mangle: {
+        module: true,
+      },
+    }),
+    {
+      name: 'worker-to-string',
+      renderChunk(code) {
+        return `export default \`${code}\`;`;
+      },
+    },
+    copy({
+      targets: [{ src: 'types/*', dest: 'build' }],
+    }),
+  ],
+});
+
+export default config;
---
+++ packages/replay-worker/src/Compressor.ts
@@ -0,0 +1,83 @@
+import { constants, Deflate, deflate } from 'pako';
+
+/**
+ * A stateful compressor that can be used to batch compress events.
+ */
+export class Compressor {
+  /**
+   * pako deflator instance
+   */
+  public deflate: Deflate;
+
+  /**
+   * If any events have been added.
+   */
+  private _hasEvents: boolean;
+
+  public constructor() {
+    this._init();
+  }
+
+  /**
+   * Clear the compressor buffer.
+   */
+  public clear(): void {
+    this._init();
+  }
+
+  /**
+   * Add an event to the compressor buffer.
+   */
+  public addEvent(data: string): void {
+    if (!data) {
+      throw new Error('Adding invalid event');
+    }
+    // If the event is not the first event, we need to prefix it with a `,` so
+    // that we end up with a list of events
+    const prefix = this._hasEvents ? ',' : '';
+    // TODO: We may want Z_SYNC_FLUSH or Z_FULL_FLUSH (not sure the difference)
+    // Using NO_FLUSH here for now as we can create many attachments that our
+    // web UI will get API rate limited.
+    this.deflate.push(prefix + data, constants.Z_SYNC_FLUSH);
+
+    this._hasEvents = true;
+  }
+
+  /**
+   * Finish compression of the current buffer.
+   */
+  public finish(): Uint8Array {
+    // We should always have a list, it can be empty
+    this.deflate.push(']', constants.Z_FINISH);
+
+    if (this.deflate.err) {
+      throw this.deflate.err;
+    }
+
+    // Copy result before we create a new deflator and return the compressed
+    // result
+    const result = this.deflate.result;
+
+    this._init();
+
+    return result;
+  }
+
+  /**
+   * Re-initialize the compressor buffer.
+   */
+  private _init(): void {
+    this._hasEvents = false;
+    this.deflate = new Deflate();
+
+    // Fake an array by adding a `[`
+    this.deflate.push('[', constants.Z_NO_FLUSH);
+  }
+}
+
+/**
+ * Compress a string.
+ */
+export function compress(data: string): Uint8Array {
+  return deflate(data);
+}
