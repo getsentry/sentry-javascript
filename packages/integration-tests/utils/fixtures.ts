e88b81b3e1e1ec9381cf9d222e77881092d3a38d
Billy Vong <billyvg@users.noreply.github.com>
2023-02-09 14:19:04 -0500

    remove snapshot suffix, remove more html

--- packages/integration-tests/utils/fixtures.ts
+++ packages/integration-tests/utils/fixtures.ts
@@ -22,6 +22,7 @@ const getAsset = (assetDir: string, asset: string): string => {
 };
 
 export type TestFixtures = {
+  _autoSnapshotSuffix: void;
   testDir: string;
   getLocalTestPath: (options: { testDir: string }) => Promise<string>;
   runInChromium: (fn: (...args: unknown[]) => unknown, args?: unknown[]) => unknown;
@@ -35,10 +36,13 @@ export type TestFixtures = {
 };
 
 const sentryTest = base.extend<TestFixtures>({
-  _autoSnapshotSuffix: [async ({ }, use, testInfo) => {
-    testInfo.snapshotSuffix = '';
-    await use();
-  }, { auto: true }],
+  _autoSnapshotSuffix: [
+    async ({}, use, testInfo) => {
+      testInfo.snapshotSuffix = '';
+      await use();
+    },
+    { auto: true },
+  ],
 
   getLocalTestPath: ({}, use, testInfo) => {
     return use(async ({ testDir }) => {
