3b0257fb9d616a4215c785cd7175992a597d43c8
Luca Forstner <luca.forstner@sentry.io>
2023-02-14 12:35:41 +0000

    Add to test

--- packages/e2e-tests/test-applications/create-next-app/sentry.server.config.ts
+++ packages/e2e-tests/test-applications/create-next-app/sentry.server.config.ts
@@ -17,7 +17,9 @@ Sentry.init({
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  // that it will also get attached to your source maps,
+
+  integrations: [new Sentry.Integrations.LocalVariables()],
 });
 
 Sentry.addGlobalEventProcessor(event => {
