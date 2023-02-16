6ade121c866188159011636eebbd643566cb9277
Abhijeet Prasad <aprasad@sentry.io>
2023-02-16 11:38:04 +0100

    use state to describe types

--- packages/react/src/errorboundary.tsx
+++ packages/react/src/errorboundary.tsx
@@ -48,11 +48,17 @@ export type ErrorBoundaryProps = {
   beforeCapture?(scope: Scope, error: Error | null, componentStack: string | null): void;
 };
 
-type ErrorBoundaryState = {
-  componentStack: React.ErrorInfo['componentStack'] | null;
-  error: Error | null;
-  eventId: string | null;
-};
+type ErrorBoundaryState =
+  | {
+      componentStack: null;
+      error: null;
+      eventId: null;
+    }
+  | {
+      componentStack: React.ErrorInfo['componentStack'];
+      error: Error;
+      eventId: string;
+    };
 
 const INITIAL_STATE = {
   componentStack: null,
@@ -133,18 +139,16 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
 
   public render(): React.ReactNode {
     const { fallback, children } = this.props;
-    const { error, componentStack, eventId } = this.state;
+    const state = this.state;
 
-    if (error) {
+    if (state.error) {
       let element: React.ReactElement | undefined = undefined;
       if (typeof fallback === 'function') {
         element = fallback({
-          error,
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          componentStack: componentStack!,
+          error: state.error,
+          componentStack: state.componentStack,
           resetError: this.resetErrorBoundary,
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          eventId: eventId!,
+          eventId: state.eventId,
         });
       } else {
         element = fallback;
