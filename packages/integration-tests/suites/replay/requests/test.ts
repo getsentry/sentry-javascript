1a5cac7f1c309d0f15c5e1e0a369c565a50bb288
Lukas Stracke <lukas.stracke@sentry.io>
2023-02-17 15:53:00 +0100

    test(replay): Add test for fetch and XHR performance spans

---
+++ packages/integration-tests/suites/replay/requests/test.ts
@@ -0,0 +1,73 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../utils/fixtures';
+import { expectedFetchPerformanceSpan, expectedXHRPerformanceSpan } from '../../../utils/replayEventTemplates';
+import { getReplayRecordingContent, shouldSkipReplayTest, waitForReplayRequest } from '../../../utils/replayHelpers';
+
+sentryTest('replay recording should contain fetch request span', async ({ getLocalTestPath, page }) => {
+  if (shouldSkipReplayTest()) {
+    sentryTest.skip();
+  }
+
+  // we're only interested in segment 1 which should contain the fetch span
+  const reqPromise1 = waitForReplayRequest(page, 1);
+
+  await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({ id: 'test-id' }),
+    });
+  });
+
+  await page.route('https://example.com', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: 'hello world',
+    });
+  });
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  await page.goto(url);
+  await page.click('#fetch');
+
+  const { performanceSpans } = getReplayRecordingContent(await reqPromise1);
+
+  expect(performanceSpans).toContainEqual(expectedFetchPerformanceSpan);
+});
+
+sentryTest('replay recording should contain XHR request span', async ({ getLocalTestPath, page }) => {
+  if (shouldSkipReplayTest()) {
+    sentryTest.skip();
+  }
+
+  // we're only interested in segment 1 which should contain the fetch span
+  const reqPromise1 = waitForReplayRequest(page, 1);
+
+  await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({ id: 'test-id' }),
+    });
+  });
+
+  await page.route('https://example.com', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: 'hello world',
+    });
+  });
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  await page.goto(url);
+  await page.click('#xhr');
+
+  const { performanceSpans } = getReplayRecordingContent(await reqPromise1);
+
+  expect(performanceSpans).toContainEqual(expectedXHRPerformanceSpan);
+});
