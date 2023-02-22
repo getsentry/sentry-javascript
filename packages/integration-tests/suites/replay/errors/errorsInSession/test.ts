00f76d610114d2aa4d3b1edc053bd66f9791f21e
Lukas Stracke <lukas.stracke@sentry.io>
2023-02-22 12:49:41 +0100

    test(replay): Add tests for error-mode and error linking

---
+++ packages/integration-tests/suites/replay/errors/errorsInSession/test.ts
@@ -0,0 +1,77 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { envelopeRequestParser } from '../../../../utils/helpers';
+import { expectedClickBreadcrumb, getExpectedReplayEvent } from '../../../../utils/replayEventTemplates';
+import {
+  getReplayEvent,
+  getReplayRecordingContent,
+  shouldSkipReplayTest,
+  waitForReplayRequest,
+} from '../../../../utils/replayHelpers';
+
+sentryTest(
+  '[session-mode] replay event should contain an error id of an error that occurred during session recording',
+  async ({ getLocalTestPath, page }) => {
+    if (shouldSkipReplayTest()) {
+      sentryTest.skip();
+    }
+
+    let errorEventId: string = 'invalid_id';
+
+    const reqPromise0 = waitForReplayRequest(page, 0);
+    const reqPromise1 = waitForReplayRequest(page, 1);
+
+    await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+      const event = envelopeRequestParser(route.request());
+      // error event have no type field
+      if (event && !event.type && event.event_id) {
+        errorEventId = event.event_id;
+      }
+
+      return route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ id: 'test-id' }),
+      });
+    });
+
+    const url = await getLocalTestPath({ testDir: __dirname });
+
+    await page.goto(url);
+    await page.click('#go-background');
+    const req0 = await reqPromise0;
+
+    await page.click('#error');
+    await page.click('#go-background');
+    const req1 = await reqPromise1;
+
+    const event0 = getReplayEvent(req0);
+    const content0 = getReplayRecordingContent(req0);
+
+    const event1 = getReplayEvent(req1);
+    const content1 = getReplayRecordingContent(req1);
+
+    expect(event0).toEqual(getExpectedReplayEvent());
+
+    // The first event should have both, full and incremental snapshots,
+    // as we recorded and kept all events in the buffer
+    expect(content0.fullSnapshots).toHaveLength(1);
+    expect(content0.incrementalSnapshots).toHaveLength(0);
+
+    expect(event1).toEqual(
+      getExpectedReplayEvent({
+        replay_start_timestamp: undefined,
+        segment_id: 1,
+        // @ts-ignore this is fine
+        error_ids: [errorEventId],
+        urls: [],
+      }),
+    );
+
+    // Also the second snapshot should have a full snapshot, as we switched from error to session
+    // mode which triggers another checkout
+    expect(content1.fullSnapshots).toHaveLength(0);
+    expect(content1.breadcrumbs).toEqual(expect.arrayContaining([expectedClickBreadcrumb]));
+  },
+);
---
+++ packages/integration-tests/suites/replay/errors/subject.js
@@ -0,0 +1,14 @@
+document.getElementById('go-background').addEventListener('click', () => {
+  Object.defineProperty(document, 'hidden', { value: true, writable: true });
+  const ev = document.createEvent('Event');
+  ev.initEvent('visibilitychange');
+  document.dispatchEvent(ev);
+});
+
+document.getElementById('error').addEventListener('click', () => {
+  throw new Error('Ooops');
+});
+
+document.getElementById('log').addEventListener('click', () => {
+  console.log('Some message');
+});
