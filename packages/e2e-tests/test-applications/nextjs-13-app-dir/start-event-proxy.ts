d9a8153b26657df21db4469fd96a75da43f2ecc9
Luca Forstner <luca.forstner@sentry.io>
2023-02-23 22:00:50 +0000

    .

--- packages/e2e-tests/test-applications/nextjs-13-app-dir/start-event-proxy.ts
+++ packages/e2e-tests/test-applications/nextjs-13-app-dir/start-event-proxy.ts
@@ -1,6 +1,8 @@
-import { startEventProxyServer } from '../../test-utils/event-proxy-server';
+const { startEventProxyServer } = require('../../test-utils/event-proxy-server');
 
 startEventProxyServer({
   port: 27496,
   proxyServerName: 'nextjs-13-app-dir',
 });
+
+export {};
