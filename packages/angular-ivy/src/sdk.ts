cca25146efc55ae57e5bf2f0f5265a545c47b061
Lukas Stracke <lukas.stracke@sentry.io>
2023-02-27 13:21:48 +0100

    feat(angular): Add Ivy-compatible Angular SDK package (#7264)
    Add a new SDK package to our monorepo: `@sentry/angular-ivy`. 
    
    While this is technically a new SDK, its content and functionality is identical to `@sentry/angular`. Only the build configuration differs:
    
    * The Ivy SDK is built with Angular 12, allowing for a compatibility of NG12-15
    * The Ivy SDK is built with `compilationMode: partial`, enabeling a build format that is compatible with Angular's Ivy rendering engine. 
      * This means that `ngcc` no longer needs to step in at initial build time to up-compile the SDK. Which is good because `ngcc` will be removed in Angular 16 (https://github.com/angular/angular-cli/pull/24720)
    * The Ivy SDK's build output follows the Angular Package Format (APF) v12 standard ([spec](https://docs.google.com/document/d/1CZC2rcpxffTDfRDs6p1cfbmKNLA6x5O-NtkJglDaBVs/preview)) which is very similar to APF 10 which we used before (see #4641 for more details)
    
    Because functionality-wise there's no difference to `@sentry/angular`, I opted to symlink the source files instead of duplicating them. The only exception is `sdk.ts` which needed some adaption for the new package, like setting the SDK name and adjusting the min version check. For the same reason, this new package currently doesn't contain tests. We'll need to reconsider this approach (symlinking and testing) if we ever need to make package-specific adjustments.

--- packages/angular-ivy/src/sdk.ts
+++ packages/angular-ivy/src/sdk.ts
@@ -11,10 +11,10 @@ import { IS_DEBUG_BUILD } from './flags';
 export function init(options: BrowserOptions): void {
   options._metadata = options._metadata || {};
   options._metadata.sdk = {
-    name: 'sentry.javascript.angular',
+    name: 'sentry.javascript.angular-ivy',
     packages: [
       {
-        name: 'npm:@sentry/angular',
+        name: 'npm:@sentry/angular-ivy',
         version: SDK_VERSION,
       },
     ],
@@ -26,7 +26,7 @@ export function init(options: BrowserOptions): void {
 }
 
 function checkAndSetAngularVersion(): void {
-  const ANGULAR_MINIMUM_VERSION = 10;
+  const ANGULAR_MINIMUM_VERSION = 12;
 
   const angularVersion = VERSION && VERSION.major ? parseInt(VERSION.major, 10) : undefined;
 
@@ -34,9 +34,10 @@ function checkAndSetAngularVersion(): void {
     if (angularVersion < ANGULAR_MINIMUM_VERSION) {
       IS_DEBUG_BUILD &&
         logger.warn(
-          `The Sentry SDK does not officially support Angular ${angularVersion}.`,
-          `This version of the Sentry SDK supports Angular ${ANGULAR_MINIMUM_VERSION} and above.`,
-          'Please consider upgrading your Angular version or downgrading the Sentry SDK.',
+          `This Sentry SDK does not officially support Angular ${angularVersion}.`,
+          `This SDK only supports Angular ${ANGULAR_MINIMUM_VERSION} and above.`,
+          "If you're using Angular 10 or 11, please use `@sentry/angular` instead.",
+          'Otherwise, please consider upgrading your Angular version.',
         );
     }
     setContext('angular', { version: angularVersion });
