3672d3c1fd2d8b8a0f18d847ab6ca278753120f1
Luca Forstner <luca.forstner@sentry.io>
2023-02-28 13:45:06 +0000

    make list reporter

--- packages/e2e-tests/test-applications/create-next-app/playwright.config.ts
+++ packages/e2e-tests/test-applications/create-next-app/playwright.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'dot',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
