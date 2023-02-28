3e744582526b10c4025056827dc701e8385566c9
Lukas Stracke <lukas.stracke@sentry.io>
2023-02-28 10:00:27 +0100

    don't compression, change test expectation

--- packages/integration-tests/suites/replay/flushing/test.ts
+++ packages/integration-tests/suites/replay/flushing/test.ts
@@ -4,68 +4,56 @@ import { sentryTest } from '../../../utils/fixtures';
 import { getExpectedReplayEvent } from '../../../utils/replayEventTemplates';
 import { getReplayEvent, shouldSkipReplayTest, waitForReplayRequest } from '../../../utils/replayHelpers';
 
-// Sync this with init.js - not we take seconds here instead of ms
-const FLUSH_DELAY_SECONDS = 0.5;
+for (let index = 0; index < 50; index++) {
+  sentryTest(
+    `replay events are flushed after max flash delay was reached (${index})`,
+    async ({ getLocalTestPath, page }) => {
+      if (shouldSkipReplayTest()) {
+        sentryTest.skip();
+      }
 
-for (let index = 0; index < 25; index++) {
-  sentryTest(`replay recording flushes every FLUSH_DELAY_SECONDS (${index})}`, async ({ getLocalTestPath, page }) => {
-    if (shouldSkipReplayTest()) {
-      sentryTest.skip();
-    }
+      const reqPromise0 = waitForReplayRequest(page, 0);
+      const reqPromise1 = waitForReplayRequest(page, 1);
+      const reqPromise2 = waitForReplayRequest(page, 2);
 
-    const reqPromise0 = waitForReplayRequest(page, 0);
-    const reqPromise1 = waitForReplayRequest(page, 1);
-    const reqPromise2 = waitForReplayRequest(page, 2);
-
-    await page.route('https://dsn.ingest.sentry.io/**/*', route => {
-      return route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({ id: 'test-id' }),
+      await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+        return route.fulfill({
+          status: 200,
+          contentType: 'application/json',
+          body: JSON.stringify({ id: 'test-id' }),
+        });
       });
-    });
 
-    const url = await getLocalTestPath({ testDir: __dirname });
+      const url = await getLocalTestPath({ testDir: __dirname });
 
-    await page.goto(url);
-    const replayEvent0 = getReplayEvent(await reqPromise0);
-    expect(replayEvent0).toEqual(getExpectedReplayEvent());
+      await page.goto(url);
+      const replayEvent0 = getReplayEvent(await reqPromise0);
+      expect(replayEvent0).toEqual(getExpectedReplayEvent());
 
-    // trigger mouse click
-    void page.click('#go-background');
+      // trigger one mouse click
+      void page.click('#something');
 
-    const replayEvent1 = getReplayEvent(await reqPromise1);
-    expect(replayEvent1).toEqual(
-      getExpectedReplayEvent({ replay_start_timestamp: undefined, segment_id: 1, urls: [] }),
-    );
+      // this must eventually lead to a flush after the max delay was reached
+      const replayEvent1 = getReplayEvent(await reqPromise1);
+      expect(replayEvent1).toEqual(
+        getExpectedReplayEvent({ replay_start_timestamp: undefined, segment_id: 1, urls: [] }),
+      );
 
-    // trigger mouse click every 100ms, it should still flush after 0.5s even if clicks are ongoing
-    for (let i = 0; i < 70; i++) {
-      setTimeout(async () => {
-        try {
-          await page.click('#go-background');
-        } catch {
-          // ignore errors here, we don't care if the page is closed
-        }
-      }, i * 100);
-    }
+      // trigger mouse click every 100ms, it should still flush after the max delay even if clicks are ongoing
+      for (let i = 0; i < 700; i++) {
+        setTimeout(async () => {
+          try {
+            await page.click('#something');
+          } catch {
+            // ignore errors here, we don't care if the page is closed
+          }
+        }, i * 100);
+      }
 
-    const replayEvent2 = getReplayEvent(await reqPromise2);
-    expect(replayEvent2).toEqual(
-      getExpectedReplayEvent({ replay_start_timestamp: undefined, segment_id: 2, urls: [] }),
-    );
-
-    // Ensure time diff is about 500ms between each event
-    const diff1 = replayEvent1.timestamp! - replayEvent0.timestamp!;
-    const diff2 = replayEvent2.timestamp! - replayEvent1.timestamp!;
-
-    // Playwright is very inconsistent with timing, so we have to ease up the expectations a lot here.
-    // Generally, we'd expect to see a diff of FLUSH_DELAY_SECONDS, but we've observed test flakes up to 2.5s.
-    // The beste we can do here is ensure that the flushes actually happen in a somewhat reasonable time frame within
-    // one order of magnitude of FLUSH_DELAY_SECONDS.
-    expect(diff1).toBeLessThan(FLUSH_DELAY_SECONDS * 10);
-    expect(diff1).toBeGreaterThanOrEqual(FLUSH_DELAY_SECONDS - 0.4);
-    expect(diff2).toBeLessThan(FLUSH_DELAY_SECONDS * 10);
-    expect(diff2).toBeGreaterThanOrEqual(FLUSH_DELAY_SECONDS - 0.4);
-  });
+      const replayEvent2 = getReplayEvent(await reqPromise2);
+      expect(replayEvent2).toEqual(
+        getExpectedReplayEvent({ replay_start_timestamp: undefined, segment_id: 2, urls: [] }),
+      );
+    },
+  );
 }
