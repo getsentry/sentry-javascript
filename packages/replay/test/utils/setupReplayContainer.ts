f69efd9abc76bbdbc2bdab102c95126286a9b88e
Francesco Novy <francesco.novy@sentry.io>
2023-03-01 09:25:06 +0100

    ref(replay): Use `SESSION_IDLE_DURATION` instead of `VISIBILITY_CHANGE_TIMEOUT`

--- packages/replay/test/utils/setupReplayContainer.ts
+++ packages/replay/test/utils/setupReplayContainer.ts
@@ -1,4 +1,3 @@
-import { SESSION_IDLE_DURATION } from '../../src/constants';
 import { createEventBuffer } from '../../src/eventBuffer';
 import { ReplayContainer } from '../../src/replay';
 import type { RecordingOptions, ReplayPluginOptions } from '../../src/types';
@@ -28,7 +27,7 @@ export function setupReplayContainer({
 
   clearSession(replay);
   replay['_setInitialState']();
-  replay['_loadAndCheckSession'](SESSION_IDLE_DURATION);
+  replay['_loadAndCheckSession']();
   replay['_isEnabled'] = true;
   replay.eventBuffer = createEventBuffer({
     useCompression: options?.useCompression || false,
