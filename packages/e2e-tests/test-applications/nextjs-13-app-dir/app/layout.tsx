621a8e35e5d5e39e912a67d9e1b16c103ef1efae
Luca Forstner <luca.forstner@sentry.io>
2023-03-02 15:05:25 +0000

    .

--- packages/e2e-tests/test-applications/nextjs-13-app-dir/app/layout.tsx
+++ packages/e2e-tests/test-applications/nextjs-13-app-dir/app/layout.tsx
@@ -1,7 +1,12 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div style={{ backgroundColor: 'lightblue', padding: '12px' }}>
+          <h1>Root layout</h1>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
