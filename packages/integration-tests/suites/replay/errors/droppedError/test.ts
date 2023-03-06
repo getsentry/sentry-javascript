15e783e7067036297255f68cdf86640bb9296efa
Francesco Novy <francesco.novy@sentry.io>
2023-03-06 17:20:04 +0100

    test

--- packages/integration-tests/suites/replay/errors/droppedError/test.ts
+++ packages/integration-tests/suites/replay/errors/droppedError/test.ts
@@ -17,58 +17,56 @@ import { getReplayEvent, shouldSkipReplayTest, waitForReplayRequest } from '../.
  * TODO: Once we have lifecycle hooks, we should revisit this test and make sure it behaves as expected.
  *       This means that the recording should not be started or stopped if the error that triggered it is not sent.
  */
-for (let i = 0; i < 100; i++) {
-  sentryTest(
-    `[error-mode] should start recording if an error occurred although the error was dropped RUN ${i}`,
-    async ({ getLocalTestPath, page }) => {
-      if (shouldSkipReplayTest()) {
-        sentryTest.skip();
-      }
+sentryTest(
+  '[error-mode] should start recording if an error occurred although the error was dropped',
+  async ({ getLocalTestPath, page }) => {
+    if (shouldSkipReplayTest()) {
+      sentryTest.skip();
+    }
 
-      const reqPromise0 = waitForReplayRequest(page, 0);
-      const reqPromise1 = waitForReplayRequest(page, 1);
-      const reqPromise2 = waitForReplayRequest(page, 2);
+    const reqPromise0 = waitForReplayRequest(page, 0);
+    const reqPromise1 = waitForReplayRequest(page, 1);
+    const reqPromise2 = waitForReplayRequest(page, 2);
 
-      let callsToSentry = 0;
+    let callsToSentry = 0;
 
-      await page.route('https://dsn.ingest.sentry.io/**/*', route => {
-        callsToSentry++;
+    await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+      callsToSentry++;
 
-        return route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: JSON.stringify({ id: 'test-id' }),
-        });
+      return route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ id: 'test-id' }),
       });
+    });
 
-      const url = await getLocalTestPath({ testDir: __dirname });
+    const url = await getLocalTestPath({ testDir: __dirname });
 
-      await page.goto(url);
-      await page.click('#go-background');
-      expect(callsToSentry).toEqual(0);
+    await page.goto(url);
+    await page.click('#go-background');
+    expect(callsToSentry).toEqual(0);
 
-      await page.click('#error');
-      const req0 = await reqPromise0;
+    await page.click('#error');
+    const req0 = await reqPromise0;
 
-      await page.click('#go-background');
-      await reqPromise1;
+    await page.click('#go-background');
+    await reqPromise1;
 
-      await page.click('#log');
-      await page.click('#go-background');
-      await reqPromise2;
+    await page.click('#log');
+    await page.click('#go-background');
+    await reqPromise2;
 
-      // Note: The fact that reqPromise1/reqPromise2 are fulfilled prooves that the recording continues
+    // Note: The fact that reqPromise1/reqPromise2 are fulfilled prooves that the recording continues
 
-      const event0 = getReplayEvent(req0);
+    const event0 = getReplayEvent(req0);
 
-      expect(event0).toEqual(
-        getExpectedReplayEvent({
-          contexts: { replay: { error_sample_rate: 1, session_sample_rate: 0 } },
-          // This is by design. A dropped error shouldn't be in this list.
-          error_ids: [],
-          replay_type: 'error',
-        }),
-      );
-    },
-  );
-}
+    expect(event0).toEqual(
+      getExpectedReplayEvent({
+        contexts: { replay: { error_sample_rate: 1, session_sample_rate: 0 } },
+        // This is by design. A dropped error shouldn't be in this list.
+        error_ids: [],
+        replay_type: 'error',
+      }),
+    );
+  },
+);
