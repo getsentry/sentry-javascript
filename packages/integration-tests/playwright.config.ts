77a810d58410c3bdfd0c8b5757020bb13bded5d7
Francesco Novy <francesco.novy@sentry.io>
2023-03-06 15:48:37 +0100

    test: Run playwright tests in parallel (#7334)

--- packages/integration-tests/playwright.config.ts
+++ packages/integration-tests/playwright.config.ts
@@ -2,6 +2,10 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   retries: 0,
-  workers: 3,
+  // Run tests inside of a single file in parallel
+  fullyParallel: true,
+  // Use 5 workers on CI, else use defaults (based on available CPU cores)
+  // Note that 5 is a random number selected to work well with our CI setup
+  workers: process.env.CI ? 5 : undefined,
 };
 export default config;
--- packages/nextjs/playwright.config.ts
+++ packages/nextjs/playwright.config.ts
@@ -6,7 +6,11 @@ const config: PlaywrightTestConfig = {
   use: {
     baseURL: 'http://localhost:3000',
   },
-  workers: 3,
+  // Run tests inside of a single file in parallel
+  fullyParallel: true,
+  // Use 5 workers on CI, else use defaults (based on available CPU cores)
+  // Note that 5 is a random number selected to work well with our CI setup
+  workers: process.env.CI ? 5 : undefined,
   webServer: {
     cwd: path.join(__dirname, 'test', 'integration'),
     command: 'yarn start',
