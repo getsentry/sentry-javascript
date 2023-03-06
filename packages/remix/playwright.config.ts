5ef5321f395bfd3d56adcfa63ee0af6a64c5e170
Francesco Novy <francesco.novy@sentry.io>
2023-03-06 12:52:38 +0100

    add better comment

--- packages/remix/playwright.config.ts
+++ packages/remix/playwright.config.ts
@@ -8,6 +8,7 @@ const config: PlaywrightTestConfig = {
   // Run tests inside of a single file in parallel
   fullyParallel: true,
   // Use 5 workers on CI, else use defaults (based on available CPU cores)
+  // Note that 5 is a random number selected to work well with our CI setup
   workers: process.env.CI ? 5 : undefined,
   webServer: {
     command: '(cd test/integration/ && yarn build && yarn start)',
