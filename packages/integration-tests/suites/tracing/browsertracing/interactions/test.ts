fff6f8071bd28fa50eb63c3711bf12359c2a2853
Ash Anand <ash.anand@sentry.io>
2023-03-06 13:44:56 -0500

    Add new test to confirm one txn per interaction

--- packages/integration-tests/suites/tracing/browsertracing/interactions/test.ts
+++ packages/integration-tests/suites/tracing/browsertracing/interactions/test.ts
@@ -32,3 +32,30 @@ sentryTest('should capture interaction transaction.', async ({ browserName, getL
   expect(interactionSpan.op).toBe('ui.action.click');
   expect(interactionSpan.description).toBe('body > button');
 });
+
+sentryTest('should create only one transaction per interaction', async ({ browserName, getLocalTestPath, page }) => {
+  const supportedBrowsers = ['chromium', 'firefox'];
+
+  if (!supportedBrowsers.includes(browserName)) {
+    sentryTest.skip();
+  }
+
+  await page.route('**/path/to/script.js', (route: Route) => route.fulfill({ path: `${__dirname}/assets/script.js` }));
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  await getFirstSentryEnvelopeRequest<Event>(page, url);
+
+  for (let i = 0; i < 4; i++) {
+    setTimeout(async () => {
+      await page.locator('[data-test-id=interaction-button]').click();
+    }, i * 100);
+  }
+
+  const envelopes = await getMultipleSentryEnvelopeRequests<Event>(page, 4);
+  expect(envelopes).toHaveLength(4);
+
+  envelopes.forEach(event => {
+    expect(event.spans).toHaveLength(1);
+  });
+});
