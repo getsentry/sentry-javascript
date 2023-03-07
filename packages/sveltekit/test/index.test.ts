19d59006e6960c9f2f40497c6b77e71b4727f6e7
Lukas Stracke <lukas.stracke@sentry.io>
2023-03-07 13:18:05 +0100

    add placeholder test

---
+++ packages/sveltekit/test/index.test.ts
@@ -0,0 +1,12 @@
+import * as SentryClient from '../src/client';
+import * as SentryServer from '../src/server';
+
+describe('SvelteKit SDK', () => {
+  // This is a place holder test at best to satisfy the test runner
+  it('exports client and server SDKs', () => {
+    expect(SentryClient).toBeDefined();
+    expect(SentryServer).toBeDefined();
+    expect(SentryClient.init).toBeDefined();
+    expect(SentryServer.init).toBeDefined()
+  })
+})
