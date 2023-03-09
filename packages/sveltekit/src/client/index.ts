5702314b28d4bfa58a71dedf512967de46b2443d
Lukas Stracke <lukas.stracke@sentry.io>
2023-03-09 16:00:49 +0100

    s/server/client placeholder

--- packages/sveltekit/src/client/index.ts
+++ packages/sveltekit/src/client/index.ts
@@ -1,4 +1,4 @@
 export * from '@sentry/svelte';
 
 // Just here so that eslint is happy until we export more stuff here
-export const PLACEHOLDER_SERVER = 'PLACEHOLDER';
+export const PLACEHOLDER_CLIENT = 'PLACEHOLDER';
