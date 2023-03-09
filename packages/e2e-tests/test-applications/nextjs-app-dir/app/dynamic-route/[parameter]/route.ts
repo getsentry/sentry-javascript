2054789e7d7729f959ea7f4822868895054c5132
Luca Forstner <luca.forstner@sentry.io>
2023-03-09 08:12:27 +0000

    .

--- packages/e2e-tests/test-applications/nextjs-app-dir/app/dynamic-route/[parameter]/route.ts
+++ packages/e2e-tests/test-applications/nextjs-app-dir/app/dynamic-route/[parameter]/route.ts
@@ -1,33 +1,29 @@
 import { NextResponse } from 'next/server';
 
-interface Params {
-  params: Record<string, string>;
+export async function GET() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'GET' });
 }
 
-export async function GET(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'GET' });
+export async function POST() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'POST' });
 }
 
-export async function POST(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'POST' });
+export async function PUT() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'PUT' });
 }
 
-export async function PUT(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'PUT' });
+export async function PATCH() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'PATCH' });
 }
 
-export async function PATCH(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'PATCH' });
+export async function DELETE() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'DELETE' });
 }
 
-export async function DELETE(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'DELETE' });
+export async function HEAD() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'HEAD' });
 }
 
-export async function HEAD(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'HEAD' });
-}
-
-export async function OPTIONS(_request: Request, { params }: Params) {
-  return NextResponse.json({ data: 'I am a dynamic route!', params, method: 'OPTIONS' });
+export async function OPTIONS() {
+  return NextResponse.json({ data: 'I am a dynamic route!', method: 'OPTIONS' });
 }
