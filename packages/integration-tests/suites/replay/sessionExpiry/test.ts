c192c9aa942bb98426c10ad03dc7a009cac8af8d
Billy Vong <billyvg@users.noreply.github.com>
2023-03-09 02:30:25 -0500

    test(replay): Add integration tests for input masking on change (#7260)
    ---------
    
    Co-authored-by: Francesco Novy <francesco.novy@sentry.io>

--- packages/integration-tests/suites/replay/sessionExpiry/test.ts
+++ packages/integration-tests/suites/replay/sessionExpiry/test.ts
@@ -14,7 +14,7 @@ import {
 // Session should expire after 2s - keep in sync with init.js
 const SESSION_TIMEOUT = 2000;
 
-sentryTest('handles an expired session RUN', async ({ getLocalTestPath, page }) => {
+sentryTest('handles an expired session', async ({ getLocalTestPath, page }) => {
   if (shouldSkipReplayTest()) {
     sentryTest.skip();
   }
