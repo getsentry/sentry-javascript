053c9549dba73b99d8b39c17df8cd0f530b5cc04
Abhijeet Prasad <aprasad@sentry.io>
2023-03-10 14:02:12 +0100

    lint fix

--- packages/sveltekit/test/client/handleError.test.ts
+++ packages/sveltekit/test/client/handleError.test.ts
@@ -1,5 +1,4 @@
 import { Scope } from '@sentry/svelte';
-
 // For now disable the import/no-unresolved rule, because we don't have a way to
 // tell eslint that we are only importing types from the @sveltejs/kit package without
 // adding a custom resolver, which will take too much time.
