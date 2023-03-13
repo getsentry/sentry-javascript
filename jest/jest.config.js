0857181b4c908f45016d93145b865df2db459fb0
Abhijeet Prasad <aprasad@sentry.io>
2023-03-13 16:44:44 +0100

    my remaining playing around

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,9 +1,23 @@
+const jest_workaround = require.resolve('jest_workaround');
+
+const swrc = {
+  jsc: {
+    experimental: {
+      keepImportAssertions: true,
+      plugins: [[jest_workaround, {}]],
+    },
+  },
+  module: {
+    type: 'commonjs',
+  },
+};
+
 module.exports = {
   // this is the package root, even when tests are being run at the repo level
   rootDir: process.cwd(),
   collectCoverage: true,
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest', swrc],
   },
   coverageDirectory: '<rootDir>/coverage',
   moduleFileExtensions: ['js', 'ts', 'tsx'],
