d11f68697878a73665286258c0a32219b47c592a
Jonas <jonas@badalic.com>
2023-03-20 12:09:00 -0400

    ref(core): move beforeEnvelope to client (#7527)

--- packages/core/src/baseclient.ts
+++ packages/core/src/baseclient.ts
@@ -647,6 +647,8 @@ export abstract class BaseClient<O extends ClientOptions> implements Client<O> {
    */
   protected _sendEnvelope(envelope: Envelope): PromiseLike<void | TransportMakeRequestResponse> | void {
     if (this._transport && this._dsn) {
+      this.emit('beforeEnvelope', envelope);
+
       return this._transport.send(envelope).then(null, reason => {
         __DEBUG_BUILD__ && logger.error('Error while sending event:', reason);
       });
