b6f03bf1ceb281879845f61f1105ff7f98d87071
Jonas <jonas@badalic.com>
2023-03-20 06:18:26 -0400

    ref(utils): reorder regexpa and add tests (#7505)

--- packages/utils/src/stacktrace.ts
+++ packages/utils/src/stacktrace.ts
@@ -125,76 +125,83 @@ function node(getModule?: GetModuleFn): StackLineParserFn {
 
   // eslint-disable-next-line complexity
   return (line: string) => {
+    const lineMatch = line.match(FULL_MATCH);
+
+    if (lineMatch) {
+      let object: string | undefined;
+      let method: string | undefined;
+      let functionName: string | undefined;
+      let typeName: string | undefined;
+      let methodName: string | undefined;
+
+      if (lineMatch[1]) {
+        functionName = lineMatch[1];
+
+        let methodStart = functionName.lastIndexOf('.');
+        if (functionName[methodStart - 1] === '.') {
+          methodStart--;
+        }
+
+        if (methodStart > 0) {
+          object = functionName.slice(0, methodStart);
+          method = functionName.slice(methodStart + 1);
+          const objectEnd = object.indexOf('.Module');
+          if (objectEnd > 0) {
+            functionName = functionName.slice(objectEnd + 1);
+            object = object.slice(0, objectEnd);
+          }
+        }
+        typeName = undefined;
+      }
+
+      if (method) {
+        typeName = object;
+        methodName = method;
+      }
+
+      if (method === '<anonymous>') {
+        methodName = undefined;
+        functionName = undefined;
+      }
+
+      if (functionName === undefined) {
+        methodName = methodName || '<anonymous>';
+        functionName = typeName ? `${typeName}.${methodName}` : methodName;
+      }
+
+      let filename = lineMatch[2] && lineMatch[2].startsWith('file://') ? lineMatch[2].slice(7) : lineMatch[2];
+      const isNative = lineMatch[5] === 'native';
+
+      if (!filename && lineMatch[5] && !isNative) {
+        filename = lineMatch[5];
+      }
+
+      const isInternal =
+        isNative || (filename && !filename.startsWith('/') && !filename.startsWith('.') && !filename.includes(':\\'));
+
+      // in_app is all that's not an internal Node function or a module within node_modules
+      // note that isNative appears to return true even for node core libraries
+      // see https://github.com/getsentry/raven-node/issues/176
+
+      const in_app = !isInternal && filename !== undefined && !filename.includes('node_modules/');
+
+      return {
+        filename,
+        module: getModule ? getModule(filename) : undefined,
+        function: functionName,
+        lineno: parseInt(lineMatch[3], 10) || undefined,
+        colno: parseInt(lineMatch[4], 10) || undefined,
+        in_app,
+      };
+    }
+
     if (line.match(FILENAME_MATCH)) {
       return {
         filename: line,
       };
     }
 
-    const lineMatch = line.match(FULL_MATCH);
-    if (!lineMatch) {
-      return undefined;
-    }
-
-    let object: string | undefined;
-    let method: string | undefined;
-    let functionName: string | undefined;
-    let typeName: string | undefined;
-    let methodName: string | undefined;
-
-    if (lineMatch[1]) {
-      functionName = lineMatch[1];
-
-      let methodStart = functionName.lastIndexOf('.');
-      if (functionName[methodStart - 1] === '.') {
-        methodStart--;
-      }
-
-      if (methodStart > 0) {
-        object = functionName.slice(0, methodStart);
-        method = functionName.slice(methodStart + 1);
-        const objectEnd = object.indexOf('.Module');
-        if (objectEnd > 0) {
-          functionName = functionName.slice(objectEnd + 1);
-          object = object.slice(0, objectEnd);
-        }
-      }
-      typeName = undefined;
-    }
-
-    if (method) {
-      typeName = object;
-      methodName = method;
-    }
-
-    if (method === '<anonymous>') {
-      methodName = undefined;
-      functionName = undefined;
-    }
-
-    if (functionName === undefined) {
-      methodName = methodName || '<anonymous>';
-      functionName = typeName ? `${typeName}.${methodName}` : methodName;
-    }
-
-    const filename = lineMatch[2] && lineMatch[2].startsWith('file://') ? lineMatch[2].slice(7) : lineMatch[2];
-    const isNative = lineMatch[5] === 'native';
-    const isInternal =
-      isNative || (filename && !filename.startsWith('/') && !filename.startsWith('.') && filename.indexOf(':\\') !== 1);
-
-    // in_app is all that's not an internal Node function or a module within node_modules
-    // note that isNative appears to return true even for node core libraries
-    // see https://github.com/getsentry/raven-node/issues/176
-    const in_app = !isInternal && filename !== undefined && !filename.includes('node_modules/');
-
-    return {
-      filename,
-      module: getModule ? getModule(filename) : undefined,
-      function: functionName,
-      lineno: parseInt(lineMatch[3], 10) || undefined,
-      colno: parseInt(lineMatch[4], 10) || undefined,
-      in_app,
-    };
+    return undefined;
   };
 }
 
