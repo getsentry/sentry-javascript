4fbc91115e8f69b47c5e1c94ac089b71b836e451
Lukas Stracke <lukas.stracke@sentry.io>
2023-03-20 17:01:28 +0100

    add tests

--- packages/sveltekit/src/client/sdk.ts
+++ packages/sveltekit/src/client/sdk.ts
@@ -28,12 +28,12 @@ export function init(options: BrowserOptions): void {
 function addClientIntegrations(options: BrowserOptions): void {
   let integrations = options.integrations || [];
 
-  // This evaluates to true unless __SENTRY_TRACING__ is text-replaced with "false", in which case everything inside
-  // will get treeshaken away
+  // This evaluates to true unless __SENTRY_TRACING__ is text-replaced with "false",
+  // in which case everything inside will get treeshaken away
   if (typeof __SENTRY_TRACING__ === 'undefined' || __SENTRY_TRACING__) {
     if (hasTracingEnabled(options)) {
       const defaultBrowserTracingIntegration = new BrowserTracing({
-        tracePropagationTargets: [...defaultRequestInstrumentationOptions.tracePropagationTargets, /^(api\/)/],
+        tracePropagationTargets: [...defaultRequestInstrumentationOptions.tracePropagationTargets],
         // TODO: Add SvelteKit router instrumentations
         // routingInstrumentation: sveltekitRoutingInstrumentation,
       });
