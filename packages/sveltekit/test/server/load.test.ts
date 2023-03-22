b81386aa807d4370d71de994aad6bf0f6f427ef9
Abhijeet Prasad <aprasad@sentry.io>
2023-03-22 13:43:29 +0100

    making progress

--- packages/sveltekit/test/server/load.test.ts
+++ packages/sveltekit/test/server/load.test.ts
@@ -20,6 +20,19 @@ vi.mock('@sentry/node', async () => {
   };
 });
 
+const mockTrace = vi.fn();
+
+vi.mock('@sentry/core', async () => {
+  const original = (await vi.importActual('@sentry/core')) as any;
+  return {
+    ...original,
+    trace: (...args) => {
+      mockTrace(...args);
+      return original.trace(...args);
+    },
+  };
+});
+
 const mockAddExceptionMechanism = vi.fn();
 
 vi.mock('@sentry/utils', async () => {
@@ -34,14 +47,41 @@ function getById(_id?: string) {
   throw new Error('error');
 }
 
+const MOCK_LOAD_ARGS: any = {
+  params: { id: '1' },
+  route: {
+    id: '/users/[id]',
+  },
+  request: {
+    headers: {
+      get: (key: string) => {
+        if (key === 'sentry-trace') {
+          return '1234567890abcdef1234567890abcdef-1234567890abcdef-1';
+        }
+
+        if (key === 'baggage') {
+          return (
+            'sentry-environment=production,sentry-release=1.0.0,sentry-transaction=dogpark,' +
+            'sentry-user_segment=segmentA,sentry-public_key=dogsarebadatkeepingsecrets,' +
+            'sentry-trace_id=1234567890abcdef1234567890abcdef,sentry-sample_rate=1'
+          );
+        }
+
+        return null;
+      },
+    },
+  },
+};
+
 describe('wrapLoadWithSentry', () => {
   beforeEach(() => {
     mockCaptureException.mockClear();
     mockAddExceptionMechanism.mockClear();
+    mockTrace.mockClear();
     mockScope = new Scope();
   });
 
-  it('calls captureException', async () => {
+  it.only('calls captureException', async () => {
     async function load({ params }: Parameters<ServerLoad>[0]): Promise<ReturnType<ServerLoad>> {
       return {
         post: getById(params.id),
@@ -49,10 +89,35 @@ describe('wrapLoadWithSentry', () => {
     }
 
     const wrappedLoad = wrapLoadWithSentry(load);
-    const res = wrappedLoad({ params: { id: '1' } } as any);
+    const res = wrappedLoad(MOCK_LOAD_ARGS);
     await expect(res).rejects.toThrow();
 
+    // create promise that waits for timeout
+    await new Promise(resolve => setTimeout(resolve, 1000, 'timeout'));
+
     expect(mockCaptureException).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls trace function', async () => {
+    async function load({ params }: Parameters<ServerLoad>[0]): Promise<ReturnType<ServerLoad>> {
+      return {
+        post: params.id,
+      };
+    }
+
+    const wrappedLoad = wrapLoadWithSentry(load);
+    await wrappedLoad({
+      params: { id: '1' },
+      route: {
+        id: '',
+      },
+      headers: { 'sentry-trace': '1234567890abcdef1234567890abcdef-1234567890abcdef-1' },
+    } as any);
+
+    expect(mockTrace).toHaveBeenCalledTimes(1);
+    expect(mockTrace).toHaveBeenCalledWith({
+      op: 'function.sveltekit.load',
+    });
   });
 
   describe('with error() helper', () => {
