41c68b91c381c3176b821ced609a00ebee235095
Abhijeet Prasad <aprasad@sentry.io>
2023-03-22 15:06:03 +0100

    rewrite using trace

--- packages/sveltekit/src/client/load.ts
+++ packages/sveltekit/src/client/load.ts
@@ -1,4 +1,4 @@
-import type { Span } from '@sentry/core';
+import { trace } from '@sentry/core';
 import { captureException, getCurrentHub } from '@sentry/svelte';
 import { addExceptionMechanism, isThenable, objectify } from '@sentry/utils';
 import type { Load } from '@sveltejs/kit';
@@ -34,60 +34,21 @@ function sendErrorToSentry(e: unknown): unknown {
 export function wrapLoadWithSentry(origLoad: Load): Load {
   return new Proxy(origLoad, {
     apply: (wrappingTarget, thisArg, args: Parameters<Load>) => {
-      let maybePromiseResult;
-
       const [event] = args;
 
-      const scope = getCurrentHub().getScope();
-
-      let activeSpan: Span | undefined = undefined;
-      let parentSpan: Span | undefined = undefined;
-
-      if (scope) {
-        parentSpan = scope.getSpan();
-        if (parentSpan) {
-          activeSpan = parentSpan.startChild({
-            op: 'function.sveltekit.load',
-            description: event.route.id || '/',
-          });
-
-          scope.setSpan(activeSpan);
-        }
-      }
-
-      try {
-        maybePromiseResult = wrappingTarget.apply(thisArg, args);
-      } catch (e) {
-        if (activeSpan) {
-          activeSpan.setStatus('internal_error');
-          activeSpan.finish();
-        }
-        const sentryError = sendErrorToSentry(e);
-        throw sentryError;
-      }
-
-      if (isThenable(maybePromiseResult)) {
-        Promise.resolve(maybePromiseResult).then(
-          () => {
-            if (activeSpan) {
-              activeSpan.finish();
-            }
+      const routeId = event.route.id;
+      return trace(
+        {
+          op: 'function.sveltekit.load',
+          name: routeId ? routeId : event.url.pathname,
+          status: 'ok',
+          metadata: {
+            source: routeId ? 'route' : 'url',
           },
-          e => {
-            if (activeSpan) {
-              activeSpan.setStatus('internal_error');
-              activeSpan.finish();
-            }
-            sendErrorToSentry(e);
-          },
-        );
-      } else {
-        if (activeSpan) {
-          activeSpan.finish();
-        }
-      }
-
-      return maybePromiseResult;
+        },
+        () => wrappingTarget.apply(thisArg, args),
+        sendErrorToSentry,
+      );
     },
   });
 }
