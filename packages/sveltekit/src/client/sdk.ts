e97b0970f9eefe927562efed19383b08144aede8
Lukas Stracke <lukas.stracke@sentry.io>
2023-03-22 15:12:40 +0100

    feat(sveltekit): Add SvelteKit routing instrumentation (#7565)
    Add routing instrumentation to the client SvelteKit SDK. Pageload navigations are created on function call as always and updated with a proper name once the `page` store emits the route id. Navigation transactions are created by subscribing to the `navigating` store.  No need for user configuration, as the instrumentation will be added automatically on SDK intialization.

--- packages/sveltekit/src/client/sdk.ts
+++ packages/sveltekit/src/client/sdk.ts
@@ -1,17 +1,18 @@
-import { defaultRequestInstrumentationOptions } from '@sentry-internal/tracing';
 import { hasTracingEnabled } from '@sentry/core';
 import type { BrowserOptions } from '@sentry/svelte';
 import { BrowserTracing, configureScope, init as initSvelteSdk } from '@sentry/svelte';
 import { addOrUpdateIntegration } from '@sentry/utils';
 
 import { applySdkMetadata } from '../common/metadata';
+import { svelteKitRoutingInstrumentation } from './router';
 
 // Treeshakable guard to remove all code related to tracing
 declare const __SENTRY_TRACING__: boolean;
 
 /**
+ * Initialize the client side of the Sentry SvelteKit SDK.
  *
- * @param options
+ * @param options Configuration options for the SDK.
  */
 export function init(options: BrowserOptions): void {
   applySdkMetadata(options, ['sveltekit', 'svelte']);
@@ -33,14 +34,11 @@ function addClientIntegrations(options: BrowserOptions): void {
   if (typeof __SENTRY_TRACING__ === 'undefined' || __SENTRY_TRACING__) {
     if (hasTracingEnabled(options)) {
       const defaultBrowserTracingIntegration = new BrowserTracing({
-        tracePropagationTargets: [...defaultRequestInstrumentationOptions.tracePropagationTargets],
-        // TODO: Add SvelteKit router instrumentations
-        // routingInstrumentation: sveltekitRoutingInstrumentation,
+        routingInstrumentation: svelteKitRoutingInstrumentation,
       });
 
       integrations = addOrUpdateIntegration(defaultBrowserTracingIntegration, integrations, {
-        // TODO: Add SvelteKit router instrumentations
-        // options.routingInstrumentation: sveltekitRoutingInstrumentation,
+        'options.routingInstrumentation': svelteKitRoutingInstrumentation,
       });
     }
   }
