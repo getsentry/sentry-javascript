d513544303f7f17d23e22ada7bcfe716fa7e7f25
Tim Fish <tim@timfish.uk>
2023-03-23 19:54:44 +0000

    Auto discovery only returns integrations where the dependency fails to load

--- packages/tracing-internal/src/node/integrations/lazy.ts
+++ packages/tracing-internal/src/node/integrations/lazy.ts
@@ -1,46 +1,55 @@
 import type { Integration, IntegrationClass } from '@sentry/types';
 import { dynamicRequire } from '@sentry/utils';
 
-export const lazyLoadedNodePerformanceMonitoringIntegrations: (() => Integration)[] = [
+export interface LazyLoadedIntegration<T = object> extends Integration {
+  /**
+   * Loads the integration's dependency and caches it so it doesn't have to be loaded again.
+   *
+   * If this returns undefined, the dependency could not be loaded.
+   */
+  loadDependency(): T | undefined;
+}
+
+export const lazyLoadedNodePerformanceMonitoringIntegrations: (() => LazyLoadedIntegration)[] = [
   () => {
     const integration = dynamicRequire(module, './apollo') as {
-      Apollo: IntegrationClass<Integration>;
+      Apollo: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.Apollo();
   },
   () => {
     const integration = dynamicRequire(module, './apollo') as {
-      Apollo: IntegrationClass<Integration>;
+      Apollo: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.Apollo({ useNestjs: true });
   },
   () => {
     const integration = dynamicRequire(module, './graphql') as {
-      GraphQL: IntegrationClass<Integration>;
+      GraphQL: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.GraphQL();
   },
   () => {
     const integration = dynamicRequire(module, './mongo') as {
-      Mongo: IntegrationClass<Integration>;
+      Mongo: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.Mongo();
   },
   () => {
     const integration = dynamicRequire(module, './mongo') as {
-      Mongo: IntegrationClass<Integration>;
+      Mongo: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.Mongo({ mongoose: true });
   },
   () => {
     const integration = dynamicRequire(module, './mysql') as {
-      Mysql: IntegrationClass<Integration>;
+      Mysql: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.Mysql();
   },
   () => {
     const integration = dynamicRequire(module, './postgres') as {
-      Postgres: IntegrationClass<Integration>;
+      Postgres: IntegrationClass<LazyLoadedIntegration>;
     };
     return new integration.Postgres();
   },
