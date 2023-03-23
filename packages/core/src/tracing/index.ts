cc65a8121fa52fb24188dce340073b06fc789541
Tim Fish <tim@timfish.uk>
2023-03-23 14:01:48 +0000

    Remove some core exports

--- packages/core/src/tracing/index.ts
+++ packages/core/src/tracing/index.ts
@@ -2,7 +2,7 @@ export { startIdleTransaction, addTracingExtensions } from './hubextensions';
 export { IdleTransaction, TRACING_DEFAULTS } from './idletransaction';
 export { Span, spanStatusfromHttpCode } from './span';
 export { Transaction } from './transaction';
-export { extractTraceparentData, getActiveTransaction, stripUrlQueryAndFragment, TRACEPARENT_REGEXP } from './utils';
+export { extractTraceparentData, getActiveTransaction } from './utils';
 // eslint-disable-next-line deprecation/deprecation
 export { SpanStatus } from './spanstatus';
 export type { SpanStatusType } from './span';
