0a1a567bc3ec18c9d044923134aa656fe9287de4
Francesco Novy <francesco.novy@sentry.io>
2023-03-23 09:15:35 +0100

    fix(core): Ensure `ignoreErrors` only applies to error events (#7573)

--- packages/core/test/lib/integrations/inboundfilters.test.ts
+++ packages/core/test/lib/integrations/inboundfilters.test.ts
@@ -177,6 +177,11 @@ const MALFORMED_EVENT: Event = {
   },
 };
 
+const TRANSACTION_EVENT: Event = {
+  message: 'transaction message',
+  type: 'transaction',
+};
+
 describe('InboundFilters', () => {
   describe('_isSentryError', () => {
     it('should work as expected', () => {
@@ -200,6 +205,13 @@ describe('InboundFilters', () => {
         ignoreErrors: ['capture'],
       });
       expect(eventProcessor(MESSAGE_EVENT, {})).toBe(null);
+    });
+
+    it('ignores transaction event for filtering', () => {
+      const eventProcessor = createInboundFiltersEventProcessor({
+        ignoreErrors: ['transaction'],
+      });
+      expect(eventProcessor(TRANSACTION_EVENT, {})).toBe(TRANSACTION_EVENT);
     });
 
     it('string filter with exact match', () => {
