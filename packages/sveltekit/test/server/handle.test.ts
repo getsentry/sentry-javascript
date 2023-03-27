f6ce5c9bff6db025117f3ee6d974e7a5b7ab82a6
Lukas Stracke <lukas.stracke@sentry.io>
2023-03-27 16:54:02 +0200

    fix(sveltekit): Fix `<meta>` tag injection (#7612)
    Apparently, the `%sveltekit.head%` placeholder isn't present in the html that's passed to `transformPageChunk`. Hence our `<meta>` tag injection logic introduced in #7574  didn't find the anchor point where to inject the tags. This PR fixes the injection, by simply appending it to an open `<head>` tag. It's not pretty but it should do the trick 🤞 (we can revisit if this breaks folks).

--- packages/sveltekit/test/server/handle.test.ts
+++ packages/sveltekit/test/server/handle.test.ts
@@ -271,7 +271,6 @@ describe('transformPageChunk', () => {
       <meta charset="utf-8" />
       <link rel="icon" href="%sveltekit.assets%/favicon.png" />
       <meta name="viewport" content="width=device-width" />
-      %sveltekit.head%
     </head>
     <body data-sveltekit-preload-data="hover">
       <div style="display: contents">%sveltekit.body%</div>
