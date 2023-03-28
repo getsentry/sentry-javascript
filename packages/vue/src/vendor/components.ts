0743e988d6e160a530b07cb80b9ec4cb17ba9b39
Abhijeet Prasad <aprasad@sentry.io>
2023-03-28 18:52:21 +0200

    chore(vue): Add attribution license (#7633)

--- packages/vue/src/vendor/components.ts
+++ packages/vue/src/vendor/components.ts
@@ -1,6 +1,30 @@
-import type { ViewModel } from './types';
+import type { ViewModel } from '../types';
 
-// Vendored directly from https://github.com/vuejs/vue/blob/master/src/core/util/debug.js with types only changes.
+// Vendored from https://github.com/vuejs/vue/blob/612fb89547711cacb030a3893a0065b785802860/src/core/util/debug.js
+// with types only changes.
+
+// The MIT License (MIT)
+
+// Copyright (c) 2013-present, Yuxi (Evan) You
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 const classifyRE = /(?:^|[-_])(\w)/g;
 const classify = (str: string): string => str.replace(classifyRE, c => c.toUpperCase()).replace(/[-_]/g, '');
 
