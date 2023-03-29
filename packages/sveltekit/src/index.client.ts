cbc4d5d5a1192dcf707b8229a9a9ae9a7671624c
Lukas Stracke <lukas.stracke@sentry.io>
2023-03-29 11:27:59 +0200

    fix(sveltekit): Explicitly export Node SDK exports

--- packages/sveltekit/src/index.client.ts
+++ packages/sveltekit/src/index.client.ts
@@ -1,7 +1,1 @@
 export * from './client';
-
-/**
- * This const serves no purpose besides being an identifier for this file that the SDK multiplexer loader can use to
- * determine that this is in fact a file that wants to be multiplexed.
- */
-export const _SENTRY_SDK_MULTIPLEXER = true;
--- packages/sveltekit/src/server/index.ts
+++ packages/sveltekit/src/server/index.ts
@@ -1,5 +1,54 @@
+// Node SDK exports
+// Unfortunately, we cannot `exprt * from '@sentry/node'` because our CJS output mistakenly
+// puts these exports into a `default` rather than on the top-level namespace.
+// Hence, we export everything from the Node SDK explicitly:
+export {
+  addGlobalEventProcessor,
+  addBreadcrumb,
+  captureException,
+  captureEvent,
+  captureMessage,
+  configureScope,
+  createTransport,
+  extractTraceparentData,
+  getActiveTransaction,
+  getHubFromCarrier,
+  getCurrentHub,
+  Hub,
+  makeMain,
+  Scope,
+  startTransaction,
+  SDK_VERSION,
+  setContext,
+  setExtra,
+  setExtras,
+  setTag,
+  setTags,
+  setUser,
+  spanStatusfromHttpCode,
+  trace,
+  withScope,
+  autoDiscoverNodePerformanceMonitoringIntegrations,
+  makeNodeTransport,
+  defaultIntegrations,
+  defaultStackParser,
+  lastEventId,
+  flush,
+  close,
+  getSentryRelease,
+  addRequestDataToEvent,
+  DEFAULT_USER_INCLUDES,
+  extractRequestData,
+  deepReadDirSync,
+  Integrations,
+  Handlers,
+} from '@sentry/node';
+
+// We can still leave this for the carrier init and type exports
 export * from '@sentry/node';
 
+// -------------------------
+// SvelteKit SDK exports:
 export { init } from './sdk';
 export { handleErrorWithSentry } from './handleError';
 export { wrapLoadWithSentry } from './load';
