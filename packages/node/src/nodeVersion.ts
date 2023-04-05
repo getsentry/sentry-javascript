b9917d8f986ae2c6b867b35afe2b9a8e6b91ac1f
Abhijeet Prasad <aprasad@sentry.io>
2023-04-05 10:30:24 +0200

    explicitly add return type

--- packages/node/src/nodeVersion.ts
+++ packages/node/src/nodeVersion.ts
@@ -1,3 +1,3 @@
 import { parseSemver } from '@sentry/utils';
 
-export const NODE_VERSION = parseSemver(process.versions.node);
+export const NODE_VERSION: ReturnType<typeof parseSemver> = parseSemver(process.versions.node);
