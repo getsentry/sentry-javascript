f29a85fb8da64c720f88b1bd1b4d52f450e792fc
Krystof Woldrich <krystofwoldrich@gmail.com>
2023-04-05 11:29:07 +0200

    Refactor debug logs

--- packages/browser/src/userfeedback.ts
+++ packages/browser/src/userfeedback.ts
@@ -11,16 +11,13 @@ export function captureUserFeedback(feedback: UserFeedback): void {
   const transport = client && client.getTransport();
 
   if (!client) {
-    if (__DEBUG_BUILD__) {
-      logger.log('[UserFeedback] getClient did not return a Client, user feedback will not be sent.');
-    }
+    __DEBUG_BUILD__ && logger.log('[UserFeedback] getClient did not return a Client, user feedback will not be sent.');
     return;
   }
 
   if (!transport) {
-    if (__DEBUG_BUILD__) {
+    __DEBUG_BUILD__ &&
       logger.log('[UserFeedback] getTransport did not return a Transport, user feedback will not be sent.');
-    }
     return;
   }
 
