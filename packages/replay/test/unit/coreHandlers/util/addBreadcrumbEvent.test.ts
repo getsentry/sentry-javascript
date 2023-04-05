1d7f18f96998c13f1cc059ca1e3fbe0c27c7b7f6
Francesco Novy <francesco.novy@sentry.io>
2023-04-05 15:00:18 +0200

    fix(replay): Ensure circular references are handled (#7752)

---
+++ packages/replay/test/unit/coreHandlers/util/addBreadcrumbEvent.test.ts
@@ -0,0 +1,50 @@
+import { BASE_TIMESTAMP } from '../../..';
+import { addBreadcrumbEvent } from '../../../../src/coreHandlers/util/addBreadcrumbEvent';
+import type { EventBufferArray } from '../../../../src/eventBuffer/EventBufferArray';
+import { setupReplayContainer } from '../../../utils/setupReplayContainer';
+
+jest.useFakeTimers();
+
+describe('Unit | coreHandlers | util | addBreadcrumbEvent', function () {
+  beforeEach(function () {
+    jest.setSystemTime(BASE_TIMESTAMP);
+  });
+
+  it('handles circular references', async () => {
+    const breadcrumb: any = {
+      category: 'console',
+      message: 'Test message',
+      thisIsNull: null,
+      thisIsUndefined: undefined,
+      timestamp: BASE_TIMESTAMP / 1000,
+    };
+    breadcrumb['circular'] = breadcrumb;
+
+    const replay = setupReplayContainer();
+    addBreadcrumbEvent(replay, breadcrumb);
+
+    await undefined;
+    await undefined;
+    await undefined;
+    await undefined;
+    await undefined;
+
+    expect((replay.eventBuffer as EventBufferArray).events).toEqual([
+      {
+        type: 5,
+        timestamp: BASE_TIMESTAMP,
+        data: {
+          tag: 'breadcrumb',
+          payload: {
+            category: 'console',
+            message: 'Test message',
+            thisIsNull: null,
+            thisIsUndefined: '[undefined]',
+            circular: '[Circular ~]',
+            timestamp: BASE_TIMESTAMP / 1000,
+          },
+        },
+      },
+    ]);
+  });
+});
