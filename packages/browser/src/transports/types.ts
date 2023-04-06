4395d74c47b4925a3ac932e5bbc2fa5da7b04000
Lukas Stracke <lukas.stracke@sentry.io>
2023-04-06 11:18:45 +0200

    fix(browser): Adjust `BrowserTransportOptions` to support offline transport options (#7775)
    Our `transportOptions` init field currently isn't typed correctly to accept `BrowserOfflineTransportOptions`, causing type errors when trying to configure the offline transport. This patch fixes this bug by making `BrowserTransportOptions` extend `BrowserOfflineTransportOptions`.

--- packages/browser/src/transports/types.ts
+++ packages/browser/src/transports/types.ts
@@ -1,6 +1,10 @@
 import type { BaseTransportOptions } from '@sentry/types';
 
-export interface BrowserTransportOptions extends BaseTransportOptions {
+import type { BrowserOfflineTransportOptions } from './offline';
+
+type BaseTransportAndOfflineTransportOptions = BaseTransportOptions & BrowserOfflineTransportOptions;
+
+export interface BrowserTransportOptions extends BaseTransportAndOfflineTransportOptions {
   /** Fetch API init parameters. Used by the FetchTransport */
   fetchOptions?: RequestInit;
   /** Custom headers for the transport. Used by the XHRTransport and FetchTransport */
