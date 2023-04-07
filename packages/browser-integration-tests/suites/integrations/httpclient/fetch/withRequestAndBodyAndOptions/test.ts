8f4e27818e7f5027534bf1a90e98745a94bbe2ac
Francesco Novy <francesco.novy@sentry.io>
2023-04-07 11:58:32 +0200

    revert change to clone request args

--- packages/browser-integration-tests/suites/integrations/httpclient/fetch/withRequestAndBodyAndOptions/test.ts
+++ packages/browser-integration-tests/suites/integrations/httpclient/fetch/withRequestAndBodyAndOptions/test.ts
@@ -4,61 +4,64 @@ import type { Event } from '@sentry/types';
 import { sentryTest } from '../../../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest } from '../../../../../utils/helpers';
 
-sentryTest('works with a Request (with body) & options passed in', async ({ getLocalTestPath, page }) => {
-  const url = await getLocalTestPath({ testDir: __dirname });
+sentryTest(
+  'works with a Request (with body) & options passed in - handling used body',
+  async ({ getLocalTestPath, page }) => {
+    const url = await getLocalTestPath({ testDir: __dirname });
 
-  await page.route('**/foo', route => {
-    return route.fulfill({
-      status: 500,
-      body: JSON.stringify({
-        error: {
-          message: 'Internal Server Error',
+    await page.route('**/foo', route => {
+      return route.fulfill({
+        status: 500,
+        body: JSON.stringify({
+          error: {
+            message: 'Internal Server Error',
+          },
+        }),
+        headers: {
+          'Content-Type': 'text/html',
         },
-      }),
-      headers: {
-        'Content-Type': 'text/html',
-      },
+      });
     });
-  });
 
-  const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+    const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
 
-  expect(eventData.exception?.values).toHaveLength(1);
+    expect(eventData.exception?.values).toHaveLength(1);
 
-  // Not able to get the cookies from the request/response because of Playwright bug
-  // https://github.com/microsoft/playwright/issues/11035
-  expect(eventData).toMatchObject({
-    message: 'HTTP Client Error with status code: 500',
-    exception: {
-      values: [
-        {
-          type: 'Error',
-          value: 'HTTP Client Error with status code: 500',
-          mechanism: {
-            type: 'http.client',
-            handled: true,
+    // Not able to get the cookies from the request/response because of Playwright bug
+    // https://github.com/microsoft/playwright/issues/11035
+    expect(eventData).toMatchObject({
+      message: 'HTTP Client Error with status code: 500',
+      exception: {
+        values: [
+          {
+            type: 'Error',
+            value: 'HTTP Client Error with status code: 500',
+            mechanism: {
+              type: 'http.client',
+              handled: true,
+            },
+          },
+        ],
+      },
+      request: {
+        url: 'http://localhost:7654/foo',
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          cache: 'no-cache',
+          'content-type': 'application/json',
+        },
+      },
+      contexts: {
+        response: {
+          status_code: 500,
+          body_size: 45,
+          headers: {
+            'content-type': 'text/html',
+            'content-length': '45',
           },
         },
-      ],
-    },
-    request: {
-      url: 'http://localhost:7654/foo',
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        cache: 'cache',
-        'content-type': 'application/json',
       },
-    },
-    contexts: {
-      response: {
-        status_code: 500,
-        body_size: 45,
-        headers: {
-          'content-type': 'text/html',
-          'content-length': '45',
-        },
-      },
-    },
-  });
-});
+    });
+  },
+);
