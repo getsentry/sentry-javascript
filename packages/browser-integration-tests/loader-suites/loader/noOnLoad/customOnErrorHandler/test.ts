e78e65340549ad0f0fc619341ef907b137ac5087
Francesco Novy <francesco.novy@sentry.io>
2023-04-13 12:59:44 +0200

    test(loader): Update loader & test for `window.onerror`

--- packages/browser-integration-tests/loader-suites/loader/noOnLoad/customOnErrorHandler/test.ts
+++ packages/browser-integration-tests/loader-suites/loader/noOnLoad/customOnErrorHandler/test.ts
@@ -3,13 +3,13 @@ import { expect } from '@playwright/test';
 import { sentryTest } from '../../../../utils/fixtures';
 import { envelopeRequestParser, waitForErrorRequest } from '../../../../utils/helpers';
 
-sentryTest('captureException works', async ({ getLocalTestUrl, page }) => {
+sentryTest('error handler works with a recursive custom error handler', async ({ getLocalTestUrl, page }) => {
   const req = waitForErrorRequest(page);
 
   const url = await getLocalTestUrl({ testDir: __dirname });
   await page.goto(url);
 
   const eventData = envelopeRequestParser(await req);
-
-  expect(eventData.message).toBe('Test exception');
+  expect(eventData.exception?.values?.length).toBe(1);
+  expect(eventData.exception?.values?.[0]?.value).toBe('window.doSomethingWrong is not a function');
 });
