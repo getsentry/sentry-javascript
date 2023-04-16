7834539cb7df6d09356f535cc12cad5b7a8ed6ca
Luca Forstner <luca.forstner@sentry.io>
2023-04-16 15:08:19 +0000

    fix(nextjs): Mark value injection loader result as uncacheable

--- packages/nextjs/src/config/loaders/valueInjectionLoader.ts
+++ packages/nextjs/src/config/loaders/valueInjectionLoader.ts
@@ -15,6 +15,9 @@ export default function valueInjectionLoader(this: LoaderThis<LoaderOptions>, us
   // We know one or the other will be defined, depending on the version of webpack being used
   const { values } = 'getOptions' in this ? this.getOptions() : this.query;
 
+  // We do not want to cache injected values across builds
+  this.cacheable(false);
+
   // Define some global proxy that works on server and on the browser.
   let injectedCode = 'var _sentryCollisionFreeGlobalObject = typeof window === "undefined" ? global : window;\n';
 
