9940f3e03d3d6a8de9133289d9295130533e1a36
Lukas Stracke <lukas.stracke@sentry.io>
2023-04-19 11:16:19 +0200

    fix(sveltekit): Don't capture "Not Found" errors in server `handleError`

--- packages/sveltekit/src/server/handleError.ts
+++ packages/sveltekit/src/server/handleError.ts
@@ -21,6 +21,10 @@ function defaultErrorHandler({ error }: Parameters<HandleServerError>[0]): Retur
  */
 export function handleErrorWithSentry(handleError: HandleServerError = defaultErrorHandler): HandleServerError {
   return (input: { error: unknown; event: RequestEvent }): ReturnType<HandleServerError> => {
+    if (isNotFoundError(input)) {
+      return handleError(input);
+    }
+
     captureException(input.error, scope => {
       scope.addEventProcessor(event => {
         addExceptionMechanism(event, {
@@ -34,4 +38,24 @@ export function handleErrorWithSentry(handleError: HandleServerError = defaultEr
 
     return handleError(input);
   };
+}
+
+/**
+ * When a page request fails because the page is not found, SvelteKit throws a "Not found" error.
+ * In the error handler here, we can't access the response yet (which we do in the load instrumentation),
+ * so we have to check if the error is a "Not found" error by checking if the route id is missing and
+ * by checking the error message on top of the raw stack trace.
+ */
+function isNotFoundError(input: { error: unknown; event: RequestEvent }): boolean {
+  const { error, event } = input;
+  const hasNoRouteId = event.route == null || event.route.id == null;
+  const rawStack: string =
+    (error != null &&
+      typeof error === 'object' &&
+      'stack' in error &&
+      typeof error.stack === 'string' &&
+      error.stack) ||
+    '';
+
+  return hasNoRouteId && rawStack.startsWith('Error: Not found:');
 }
