57efea872805fb6fb02a1faf3582428d2f3552be
Francesco Novy <francesco.novy@sentry.io>
2023-04-20 09:51:27 +0200

    ref: Use number consts for json parsing

--- packages/replay/src/util/truncateJson/constants.ts
+++ packages/replay/src/util/truncateJson/constants.ts
@@ -1,14 +1,14 @@
-export const OBJ = 'OBJ' as const;
-export const OBJ_KEY = 'OBJ_KEY' as const;
-export const OBJ_KEY_STR = 'OBJ_KEY_STR' as const;
-export const OBJ_VAL = 'OBJ_VAL' as const;
-export const OBJ_VAL_STR = 'OBJ_VAL_STR' as const;
-export const OBJ_VAL_COMPLETED = 'OBJ_VAL_COMPLETED' as const;
+export const OBJ = 10;
+export const OBJ_KEY = 11;
+export const OBJ_KEY_STR = 12;
+export const OBJ_VAL = 13;
+export const OBJ_VAL_STR = 14;
+export const OBJ_VAL_COMPLETED = 15;
 
-export const ARR = 'ARR' as const;
-export const ARR_VAL = 'ARR_VAL' as const;
-export const ARR_VAL_STR = 'ARR_VAL_STR' as const;
-export const ARR_VAL_COMPLETED = 'ARR_VAL_COMPLETED' as const;
+export const ARR = 20;
+export const ARR_VAL = 21;
+export const ARR_VAL_STR = 22;
+export const ARR_VAL_COMPLETED = 23;
 
 export type JsonToken =
   | typeof OBJ
