30d7444866cc39483c6a91358b786015f1c12487
Francesco Novy <francesco.novy@sentry.io>
2023-04-20 16:15:20 +0200

    add tests

---
+++ packages/browser-integration-tests/suites/replay/captureConsoleLog/test.ts
@@ -0,0 +1,123 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../utils/fixtures';
+import { getCustomRecordingEvents, shouldSkipReplayTest, waitForReplayRequest } from '../../../utils/replayHelpers';
+
+sentryTest('should capture console messages in replay', async ({ getLocalTestPath, page, forceFlushReplay }) => {
+  // console integration is not used in bundles/loader
+  const bundle = process.env.PW_BUNDLE || '';
+  if (shouldSkipReplayTest() || bundle.startsWith('bundle_') || bundle.startsWith('loader_')) {
+    sentryTest.skip();
+  }
+
+  const reqPromise0 = waitForReplayRequest(page, 0);
+
+  await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({ id: 'test-id' }),
+    });
+  });
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  await page.goto(url);
+  await reqPromise0;
+
+  const reqPromise1 = waitForReplayRequest(
+    page,
+    (_event, res) => {
+      const { breadcrumbs } = getCustomRecordingEvents(res);
+
+      return breadcrumbs.filter(breadcrumb => breadcrumb.category === 'console').length > 0;
+    },
+    5_000,
+  );
+
+  await page.click('[data-log]');
+
+  await forceFlushReplay();
+
+  const { breadcrumbs } = getCustomRecordingEvents(await reqPromise1);
+
+  expect(breadcrumbs.filter(breadcrumb => breadcrumb.category === 'console')).toEqual([
+    {
+      timestamp: expect.any(Number),
+      type: 'default',
+      category: 'console',
+      data: { arguments: ['Test log', '[HTMLElement: HTMLBodyElement]'], logger: 'console' },
+      level: 'log',
+      message: 'Test log [object HTMLBodyElement]',
+    },
+  ]);
+});
+
+sentryTest('should capture very large console logs', async ({ getLocalTestPath, page, forceFlushReplay }) => {
+  // console integration is not used in bundles/loader
+  const bundle = process.env.PW_BUNDLE || '';
+  if (shouldSkipReplayTest() || bundle.startsWith('bundle_') || bundle.startsWith('loader_')) {
+    sentryTest.skip();
+  }
+
+  const reqPromise0 = waitForReplayRequest(page, 0);
+
+  await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({ id: 'test-id' }),
+    });
+  });
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  await page.goto(url);
+  await reqPromise0;
+
+  const reqPromise1 = waitForReplayRequest(
+    page,
+    (_event, res) => {
+      const { breadcrumbs } = getCustomRecordingEvents(res);
+
+      return breadcrumbs.filter(breadcrumb => breadcrumb.category === 'console').length > 0;
+    },
+    5_000,
+  );
+
+  await page.click('[data-log-large]');
+
+  await forceFlushReplay();
+
+  const { breadcrumbs } = getCustomRecordingEvents(await reqPromise1);
+
+  expect(breadcrumbs.filter(breadcrumb => breadcrumb.category === 'console')).toEqual([
+    {
+      timestamp: expect.any(Number),
+      type: 'default',
+      category: 'console',
+      data: {
+        arguments: [
+          expect.objectContaining({
+            'item-0': {
+              aa: expect.objectContaining({
+                'item-0': {
+                  aa: expect.any(Object),
+                  bb: expect.any(String),
+                  cc: expect.any(String),
+                  dd: expect.any(String),
+                },
+              }),
+              bb: expect.any(String),
+              cc: expect.any(String),
+              dd: expect.any(String),
+            },
+          }),
+        ],
+        logger: 'console',
+      },
+      level: 'log',
+      message: '[object Object]',
+    },
+  ]);
+});
