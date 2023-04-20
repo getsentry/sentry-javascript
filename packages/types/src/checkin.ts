4f8888a30ac7f0274a705f61569d11f7478fe558
Abhijeet Prasad <aprasad@sentry.io>
2023-04-20 10:21:05 +0200

    feat(node): Add monitor upsert types

--- packages/types/src/checkin.ts
+++ packages/types/src/checkin.ts
@@ -1,3 +1,17 @@
+interface CrontabSchedule {
+  type: 'crontab';
+  // The crontab schedule string, e.g. 0 * * * *.
+  value: string;
+}
+
+interface IntervalSchedule {
+  type: 'interval';
+  value: number;
+  unit: 'year' | 'month' | 'week' | 'day' | 'hour' | 'minute';
+}
+
+type MonitorSchedule = CrontabSchedule | IntervalSchedule;
+
 // https://develop.sentry.dev/sdk/check-ins/
 export interface CheckIn {
   // Check-In ID (unique and client generated).
@@ -10,4 +24,16 @@ export interface CheckIn {
   duration?: number;
   release?: string;
   environment?: string;
+  monitor_config?: {
+    schedule: MonitorSchedule;
+    // The allowed allowed margin of minutes after the expected check-in time that
+    // the monitor will not be considered missed for.
+    checkin_margin?: number;
+    // The allowed allowed duration in minutes that the monitor may be `in_progress`
+    // for before being considered failed due to timeout.
+    max_runtime?: number;
+    // A tz database string representing the timezone which the monitor's execution schedule is in.
+    // See: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    timezone?: string;
+  };
 }
