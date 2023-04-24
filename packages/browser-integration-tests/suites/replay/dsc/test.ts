9db877a1d2509d4b36e71ec687308807ad9a85e7
Francesco Novy <francesco.novy@sentry.io>
2023-04-24 08:57:54 +0200

    fix(replay): Ensure we do not set replayId on dsc if replay is disabled

--- packages/browser-integration-tests/suites/replay/dsc/test.ts
+++ packages/browser-integration-tests/suites/replay/dsc/test.ts
@@ -1,4 +1,5 @@
 import { expect } from '@playwright/test';
+import type * as Sentry from '@sentry/browser';
 import type { EventEnvelopeHeaders } from '@sentry/types';
 
 import { sentryTest } from '../../../utils/fixtures';
@@ -9,6 +10,8 @@ import {
 } from '../../../utils/helpers';
 import { getReplaySnapshot, shouldSkipReplayTest, waitForReplayRunning } from '../../../utils/replayHelpers';
 
+type TestWindow = Window & { Sentry: typeof Sentry; Replay: Sentry.Replay };
+
 sentryTest('should add replay_id to dsc of transactions', async ({ getLocalTestPath, page, browserName }) => {
   // This is flaky on webkit, so skipping there...
   if (shouldSkipReplayTest() || shouldSkipTracingTest() || browserName === 'webkit') {
@@ -17,6 +20,13 @@ sentryTest('should add replay_id to dsc of transactions', async ({ getLocalTestP
 
   const url = await getLocalTestPath({ testDir: __dirname });
   await page.goto(url);
+
+  await page.evaluate(() => {
+    (window as unknown as TestWindow).Sentry.configureScope(scope => {
+      scope.setUser({ id: 'user123', segment: 'segmentB' });
+      scope.setTransactionName('testTransactionDSC');
+    });
+  });
 
   const envHeader = await getFirstSentryEnvelopeRequest<EventEnvelopeHeaders>(page, url, envelopeHeaderRequestParser);
 
@@ -35,3 +45,41 @@ sentryTest('should add replay_id to dsc of transactions', async ({ getLocalTestP
     replay_id: replay.session?.id,
   });
 });
+
+sentryTest(
+  'should not add replay_id to dsc of transactions if replay is not enabled',
+  async ({ getLocalTestPath, page, browserName }) => {
+    // This is flaky on webkit, so skipping there...
+    if (shouldSkipReplayTest() || shouldSkipTracingTest() || browserName === 'webkit') {
+      sentryTest.skip();
+    }
+
+    const url = await getLocalTestPath({ testDir: __dirname });
+    await page.goto(url);
+
+    await page.evaluate(() => {
+      (window as unknown as TestWindow).Replay.stop();
+
+      (window as unknown as TestWindow).Sentry.configureScope(scope => {
+        scope.setUser({ id: 'user123', segment: 'segmentB' });
+        scope.setTransactionName('testTransactionDSC');
+      });
+    });
+
+    const envHeader = await getFirstSentryEnvelopeRequest<EventEnvelopeHeaders>(page, url, envelopeHeaderRequestParser);
+
+    await waitForReplayRunning(page);
+    const replay = await getReplaySnapshot(page);
+
+    expect(replay.session?.id).toBeDefined();
+
+    expect(envHeader.trace).toBeDefined();
+    expect(envHeader.trace).toEqual({
+      environment: 'production',
+      user_segment: 'segmentB',
+      sample_rate: '1',
+      trace_id: expect.any(String),
+      public_key: 'public',
+    });
+  },
+);
