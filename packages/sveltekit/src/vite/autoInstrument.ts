72003dd4eaf1bb9f4572db80d5b8ee55f6c80167
Lukas Stracke <lukas.stracke@sentry.io>
2023-04-25 15:02:36 +0200

    feat(sveltekit): Auto-wrap `load` functions with proxy module

---
+++ packages/sveltekit/src/vite/autoInstrument.ts
@@ -0,0 +1,123 @@
+/* eslint-disable @sentry-internal/sdk/no-optional-chaining */
+import * as fs from 'fs';
+import * as path from 'path';
+import type { Plugin } from 'vite';
+
+export const WRAPPED_MODULE_SUFFIX = '?sentry-auto-wrap';
+
+export type AutoInstrumentSelection = {
+  /**
+   * If this flag is `true`, the Sentry plugins will automatically instrument the `load` function of
+   * your universal `load` functions declared in your `+page.(js|ts)` and `+layout.(js|ts)` files.
+   *
+   * @default true
+   */
+  load?: boolean;
+
+  /**
+   * If this flag is `true`, the Sentry plugins will automatically instrument the `load` function of
+   * your server-only `load` functions declared in your `+page.server.(js|ts)`
+   * and `+layout.server.(js|ts)` files.
+   *
+   * @default true
+   */
+  serverLoad?: boolean;
+};
+
+type AutoInstrumentPluginOptions = AutoInstrumentSelection & {
+  debug: boolean;
+};
+
+/**
+ * Creates a Vite plugin that automatically instruments the parts of the app
+ * specified in @param options. This includes
+ *
+ * - universal `load` functions from `+page.(js|ts)` and `+layout.(js|ts)` files
+ * - server-only `load` functions from `+page.server.(js|ts)` and `+layout.server.(js|ts)` files
+ *
+ * @returns the plugin
+ */
+export async function makeAutoInstrumentationPlugin(options: AutoInstrumentPluginOptions): Promise<Plugin> {
+  const { load: shouldWrapLoad, serverLoad: shouldWrapServerLoad, debug } = options;
+
+  return {
+    name: 'sentry-auto-instrumentation',
+    // This plugin needs to run as early as possible, before the SvelteKit plugin virtualizes all paths and ids
+    enforce: 'pre',
+
+    async load(id) {
+      const applyUniversalLoadWrapper =
+        shouldWrapLoad &&
+        /^\+(page|layout)\.(js|ts|mjs|mts)$/.test(path.basename(id)) &&
+        (await canWrapLoad(id, debug));
+
+      if (applyUniversalLoadWrapper) {
+        // eslint-disable-next-line no-console
+        debug && console.log(`Wrapping ${id} with Sentry load wrapper`);
+        return getWrapperCode('wrapLoadWithSentry', `${id}${WRAPPED_MODULE_SUFFIX}`);
+      }
+
+      const applyServerLoadWrapper =
+        shouldWrapServerLoad &&
+        /^\+(page|layout)\.server\.(js|ts|mjs|mts)$/.test(path.basename(id)) &&
+        (await canWrapLoad(id, debug));
+
+      if (applyServerLoadWrapper) {
+        // eslint-disable-next-line no-console
+        debug && console.log(`Wrapping ${id} with Sentry load wrapper`);
+        return getWrapperCode('wrapServerLoadWithSentry', `${id}${WRAPPED_MODULE_SUFFIX}`);
+      }
+
+      return null;
+    },
+  };
+}
+
+/**
+ * We only want to apply our wrapper to files that
+ *
+ *  - Have no Sentry code yet in them. This is to avoid double-wrapping or interferance with custom
+ *    Sentry calls.
+ *  - Actually declare a `load` function. The second check of course is not 100% accurate, but it's good enough.
+ *    Injecting our wrapper into files that don't declare a `load` function would result in a build-time warning
+ *    because vite/rollup warns if we reference an export from the user's file in our wrapping code that
+ *    doesn't exist.
+ *
+ * Exported for testing
+ *
+ * @returns `true` if we can wrap the given file, `false` otherwise
+ */
+export async function canWrapLoad(id: string, debug: boolean): Promise<boolean> {
+  const code = (await fs.promises.readFile(id, 'utf8')).toString();
+
+  const codeWithoutComments = code.replace(/(\/\/.*| ?\/\*[^]*?\*\/)(,?)$/gm, '');
+
+  const hasSentryContent = codeWithoutComments.includes('@sentry/sveltekit');
+  if (hasSentryContent) {
+    // eslint-disable-next-line no-console
+    debug && console.log(`Skipping wrappiung ${id} because it already contains Sentry code`);
+  }
+
+  const hasLoadDeclaration = /(const|let|var|function)\s+load\s*(=|\()/gm.test(codeWithoutComments);
+  if (!hasLoadDeclaration) {
+    // eslint-disable-next-line no-console
+    debug && console.log(`Skipping wrappiung ${id} because it doesn't declare a \`load\` function`);
+  }
+
+  return !hasSentryContent && hasLoadDeclaration;
+}
+
+/**
+ * Return wrapper code fo the given module id and wrapping function
+ */
+function getWrapperCode(
+  wrapperFunction: 'wrapLoadWithSentry' | 'wrapServerLoadWithSentry',
+  idWithSuffix: string,
+): string {
+  return (
+    `import { ${wrapperFunction} } from "@sentry/sveltekit";` +
+    `import * as userModule from ${JSON.stringify(idWithSuffix)};` +
+    `export const load = userModule.load ? ${wrapperFunction}(userModule.load) : undefined;` +
+    `export * from ${JSON.stringify(idWithSuffix)};`
+  );
+}
---
+++ packages/sveltekit/test/vite/autoInstrument.test.ts
@@ -0,0 +1,204 @@
+import { vi } from 'vitest';
+
+// import { makeAutoInstrumentationPlugin } from '../../src/vite/autoInstrument';
+import * as autoInstument from '../../src/vite/autoInstrument';
+
+const DEFAULT_CONTENT = `
+  export const load = () => {};
+  export const prerender = true;
+`;
+
+let fileContent: string | undefined;
+
+vi.mock('fs', async () => {
+  const actual = await vi.importActual('fs');
+  return {
+    // @ts-ignore this exists, I promise!
+    ...actual,
+    promises: {
+      // @ts-ignore this also exists, I promise!
+      ...actual.promises,
+      readFile: vi.fn().mockImplementation(() => {
+        return fileContent || DEFAULT_CONTENT;
+      }),
+    },
+  };
+});
+
+vi.mock('rollup', () => {
+  return {
+    rollup: vi.fn().mockReturnValue({ generate: vi.fn().mockReturnValue({ output: ['transformed'] }) }),
+  };
+});
+
+describe('makeAutoInstrumentationPlugin()', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    fileContent = undefined;
+  });
+
+  it('returns the auto instrumentation plugin', async () => {
+    const plugin = await autoInstument.makeAutoInstrumentationPlugin({ debug: true, load: true, serverLoad: true });
+    expect(plugin.name).toEqual('sentry-auto-instrumentation');
+    expect(plugin.enforce).toEqual('pre');
+    expect(plugin.load).toEqual(expect.any(Function));
+  });
+
+  describe.each([
+    'path/to/+page.ts',
+    'path/to/+page.js',
+    'path/to/+page.mts',
+    'path/to/+page.mjs',
+    'path/to/+layout.ts',
+    'path/to/+layout.js',
+    'path/to/+layout.mts',
+    'path/to/+layout.mjs',
+  ])('transform %s files', (path: string) => {
+    it('wraps universal load if `load` option is `true`', async () => {
+      const plugin = await autoInstument.makeAutoInstrumentationPlugin({ debug: false, load: true, serverLoad: true });
+      // @ts-ignore this exists
+      const loadResult = await plugin.load(path);
+      expect(loadResult).toEqual(
+        'import { wrapLoadWithSentry } from "@sentry/sveltekit";' +
+          `import * as userModule from "${path}?sentry-auto-wrap";` +
+          'export const load = userModule.load ? wrapLoadWithSentry(userModule.load) : undefined;' +
+          `export * from "${path}?sentry-auto-wrap";`,
+      );
+    });
+
+    it("doesn't wrap universal load if `load` option is `false`", async () => {
+      const plugin = await autoInstument.makeAutoInstrumentationPlugin({
+        debug: false,
+        load: false,
+        serverLoad: false,
+      });
+      // @ts-ignore this exists
+      const loadResult = await plugin.load(path);
+      expect(loadResult).toEqual(null);
+    });
+  });
+
+  describe.each([
+    'path/to/+page.server.ts',
+    'path/to/+page.server.js',
+    'path/to/+page.server.mts',
+    'path/to/+page.server.mjs',
+    'path/to/+layout.server.ts',
+    'path/to/+layout.server.js',
+    'path/to/+layout.server.mts',
+    'path/to/+layout.server.mjs',
+  ])('transform %s files', (path: string) => {
+    it('wraps universal load if `load` option is `true`', async () => {
+      const plugin = await autoInstument.makeAutoInstrumentationPlugin({ debug: false, load: false, serverLoad: true });
+      // @ts-ignore this exists
+      const loadResult = await plugin.load(path);
+      expect(loadResult).toEqual(
+        'import { wrapServerLoadWithSentry } from "@sentry/sveltekit";' +
+          `import * as userModule from "${path}?sentry-auto-wrap";` +
+          'export const load = userModule.load ? wrapServerLoadWithSentry(userModule.load) : undefined;' +
+          `export * from "${path}?sentry-auto-wrap";`,
+      );
+    });
+
+    it("doesn't wrap universal load if `load` option is `false`", async () => {
+      const plugin = await autoInstument.makeAutoInstrumentationPlugin({
+        debug: false,
+        load: false,
+        serverLoad: false,
+      });
+      // @ts-ignore this exists
+      const loadResult = await plugin.load(path);
+      expect(loadResult).toEqual(null);
+    });
+  });
+});
+
+describe('canWrapLoad', () => {
+  afterEach(() => {
+    fileContent = undefined;
+  });
+
+  it.each([
+    ['export variable declaration - function pointer', 'export const load=   loadPageData'],
+    ['export variable declaration - factory function call', 'export const load    =loadPageData()'],
+    ['export variable declaration - inline function', 'export const load = () => { return { props: { msg: "hi" } } }'],
+    [
+      'export variable declaration - inline async function',
+      'export const load = async () => { return { props: { msg: "hi" } } }',
+    ],
+
+    ['export function declaration', 'export function load  (  ){ return { props: { msg: "hi" } } }'],
+    [
+      'export function declaration - with params',
+      `export async function load({fetch}){
+        const res = await fetch('https://example.com');
+        return { props: { msg: res.toString() } }
+      }`,
+    ],
+
+    [
+      'variable declaration (let)',
+      `import {something} from 'somewhere';
+      let load = async () => {};
+      export prerender = true;
+      export { load}`,
+    ],
+    [
+      'variable declaration (var)',
+      `import {something} from 'somewhere';
+      var    load=async () => {};
+      export prerender = true;
+      export { load}`,
+    ],
+
+    [
+      'function declaration',
+      `import {something} from 'somewhere';
+       async function load(){};
+       export { load }`,
+    ],
+    [
+      'function declaration, sentry commented out',
+      `import {something} from 'somewhere';
+       // import * as Sentry from '@sentry/sveltekit';
+       async function load(){};
+       export { load }`,
+    ],
+    [
+      'function declaration, sentry commented out',
+      `import {something} from 'somewhere';
+       /* import * as Sentry from '@sentry/sveltekit'; */
+       async function load(){};
+       export { load }`,
+    ],
+  ])('returns `true` if a load declaration  (%s) exists and no Sentry code was found', async (_, code) => {
+    fileContent = code;
+    expect(await autoInstument.canWrapLoad('+page.ts', false)).toEqual(true);
+  });
+
+  it.each([
+    'export const almostLoad = () => {}; export const prerender = true;',
+    'export const loadNotLoad = () => {}; export const prerender = true;',
+    'export function aload(){}; export const prerender = true;',
+    'export function loader(){}; export const prerender = true;',
+    'let loademe = false; export {loadme}',
+    'const a = {load: true}; export {a}',
+    'if (s === "load") {}',
+    'const a = load ? load : false',
+    '// const load = () => {}',
+    '/* export const load = () => {} */ export const prerender = true;',
+  ])('returns `false` if no load declaration exists', async (_, code) => {
+    fileContent = code;
+    expect(await autoInstument.canWrapLoad('+page.ts', false)).toEqual(true);
+  });
+
+  it('returns `false` if Sentry code was found', async () => {
+    fileContent = 'import * as Sentry from "@sentry/sveltekit";';
+    expect(await autoInstument.canWrapLoad('+page.ts', false)).toEqual(false);
+  });
+
+  it('returns `false` if Sentry code was found', async () => {
+    fileContent = 'import * as Sentry from "@sentry/sveltekit";';
+    expect(await autoInstument.canWrapLoad('+page.ts', false)).toEqual(false);
+  });
+});
