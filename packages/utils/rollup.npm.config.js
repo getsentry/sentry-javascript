27247de7e9969b4ae414a2fd3410a47beeaac0e4
Francesco Novy <francesco.novy@sentry.io>
2023-04-25 16:11:57 +0200

    build: Build core & utils into a single output file
    This may speed up usage, as it avoids some potentially expensive require/import calls.

--- packages/utils/rollup.npm.config.js
+++ packages/utils/rollup.npm.config.js
@@ -1,9 +1,30 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
 
-export default makeNPMConfigVariants(
+const baseVariants = makeNPMConfigVariants(
+  makeBaseNPMConfig({
+    entrypoints: ['src/index.ts'],
+    packageSpecificConfig: {
+      output: {
+        // Combine output into a single file to avoid expensive require/import calls
+        preserveModules: false,
+      },
+    },
+  }),
+);
+
+const polyfillVariants = makeNPMConfigVariants(
   makeBaseNPMConfig({
     // We build the polyfills separately because they're not included in the top-level exports of the package, in order
     // to keep them out of the public API.
-    entrypoints: ['src/index.ts', 'src/buildPolyfills/index.ts'],
+    entrypoints: ['src/buildPolyfills/index.ts'],
+    packageSpecificConfig: {
+      output: {
+        entryFileNames: () => 'buildPolyfills.js',
+        // Combine output into a single file to avoid expensive require/import calls
+        preserveModules: false,
+      },
+    },
   }),
 );
+
+export default baseVariants.concat(polyfillVariants);
--- packages/utils/scripts/fixPolyfillsBuild.ts
+++ packages/utils/scripts/fixPolyfillsBuild.ts
@@ -1,19 +1,8 @@
-import * as childProcess from 'child_process';
 import * as fs from 'fs';
 
-/**
- * Run the given shell command, piping the shell process's `stdin`, `stdout`, and `stderr` to that of the current
- * process. Returns contents of `stdout`.
- */
-function run(cmd: string, options?: childProcess.ExecSyncOptions): string | Buffer {
-  return childProcess.execSync(cmd, { stdio: 'inherit', ...options });
-}
-
-run('yarn rollup -c rollup.npm.config.js');
-
 // We want to distribute the README because it contains the MIT license blurb from Sucrase and Rollup
-fs.copyFileSync('src/buildPolyfills/README.md', 'build/cjs/buildPolyfills/README.md');
-fs.copyFileSync('src/buildPolyfills/README.md', 'build/esm/buildPolyfills/README.md');
+fs.copyFileSync('src/buildPolyfills/README.md', 'build/cjs/buildPolyfills_README.md');
+fs.copyFileSync('src/buildPolyfills/README.md', 'build/esm/buildPolyfills_README.md');
 
 // Because we import our polyfills from `@sentry/utils/cjs/buildPolyfills` and `@sentry/utils/esm/buildPolyfills` rather
 // than straight from `@sentry/utils` (so as to avoid having them in the package's public API), when tests run, they'll
