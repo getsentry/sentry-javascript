0351a0fca690ba37ea53ab3fa1a91cd33ec50c81
Francesco Novy <francesco.novy@sentry.io>
2023-04-25 16:43:09 +0200

    Revert "build: Disable `preserveModules` for tracing-internal & browser"
    This reverts commit 2cf1ea8ccdda7a4073e4749484f1bb29080a81fe.

--- packages/browser/rollup.npm.config.js
+++ packages/browser/rollup.npm.config.js
@@ -4,11 +4,5 @@ export default makeNPMConfigVariants(
   makeBaseNPMConfig({
     // packages with bundles have a different build directory structure
     hasBundles: true,
-    packageSpecificConfig: {
-      output: {
-        // Combine output into a single file to avoid expensive require/import calls
-        preserveModules: false,
-      },
-    },
   }),
 );
--- packages/tracing-internal/rollup.npm.config.js
+++ packages/tracing-internal/rollup.npm.config.js
@@ -1,12 +1,3 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
 
-export default makeNPMConfigVariants(
-  makeBaseNPMConfig({
-    packageSpecificConfig: {
-      output: {
-        // Combine output into a single file to avoid expensive require/import calls
-        preserveModules: false,
-      },
-    },
-  }),
-);
+export default makeNPMConfigVariants(makeBaseNPMConfig());
