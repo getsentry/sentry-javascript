78beffa69a74936dd98462cd6cc8d93a9abd3a41
Lukas Stracke <lukas.stracke@sentry.io>
2023-05-09 16:16:11 +0200

    ref(sveltekit): Improve determination of auto-instrumentation applicability

--- packages/sveltekit/test/vite/autoInstrument.test.ts
+++ packages/sveltekit/test/vite/autoInstrument.test.ts
@@ -121,6 +121,7 @@ describe('canWrapLoad', () => {
     ['export variable declaration - function pointer', 'export const load=   loadPageData'],
     ['export variable declaration - factory function call', 'export const load    =loadPageData()'],
     ['export variable declaration - inline function', 'export const load = () => { return { props: { msg: "hi" } } }'],
+    ['export variable declaration - inline function let', 'export let load = () => {}'],
     [
       'export variable declaration - inline async function',
       'export const load = async () => { return { props: { msg: "hi" } } }',
@@ -139,14 +140,14 @@ describe('canWrapLoad', () => {
       'variable declaration (let)',
       `import {something} from 'somewhere';
       let load = async () => {};
-      export prerender = true;
+      export const prerender = true;
       export { load}`,
     ],
     [
       'variable declaration (var)',
       `import {something} from 'somewhere';
       var    load=async () => {};
-      export prerender = true;
+      export const prerender = true;
       export { load}`,
     ],
 
@@ -176,7 +177,12 @@ describe('canWrapLoad', () => {
        async function somethingElse(){};
        export { somethingElse as  load, foo }`,
     ],
-
+    [
+      'function declaration with different string literal name',
+      `import { foo } from 'somewhere';
+       async function somethingElse(){};
+       export { somethingElse as "load", foo }`,
+    ],
     [
       'export variable declaration - inline function with assigned type',
       `import type { LayoutLoad } from './$types';
@@ -204,13 +210,11 @@ describe('canWrapLoad', () => {
     expect(await canWrapLoad('+page.ts', false)).toEqual(true);
   });
 
-  it('returns `false` if Sentry code was found', async () => {
-    fileContent = 'import * as Sentry from "@sentry/sveltekit";';
-    expect(await canWrapLoad('+page.ts', false)).toEqual(false);
-  });
-
-  it('returns `false` if Sentry code was found', async () => {
-    fileContent = 'import * as Sentry from "@sentry/sveltekit";';
+  it.each([
+    'import * as Sentry from "@sentry/sveltekit"; export const prerender = true;',
+    'export const a = 1; import { init } from "@sentry/sveltekit"; export const load = () => {}',
+  ])('returns `false` if Sentry code was found', async code => {
+    fileContent = code;
     expect(await canWrapLoad('+page.ts', false)).toEqual(false);
   });
 });
