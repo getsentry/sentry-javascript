getsentry/sentry-javascript

commit 300b2208d7a7100dd9a59cbf582f46087a061754

feat(replay): Change `stop()` to flush and remove current session (#7741)
`stop()` will now flush the eventBuffer before clearing it, as well as removing the session from Session Storage. Due to the flushing, `stop()` is now async.

Closes: https://github.com/getsentry/sentry-javascript/issues/7738

packages/replay/src/replay.ts
@@ -7,6 +7,7 @@ import { logger } from '@sentry/utils';
 import { ERROR_CHECKOUT_TIME, MAX_SESSION_LIFE, SESSION_IDLE_DURATION, WINDOW } from './constants';
 import { setupPerformanceObserver } from './coreHandlers/performanceObserver';
 import { createEventBuffer } from './eventBuffer';
+import { clearSession } from './session/clearSession';
 import { getSession } from './session/getSession';
 import { saveSession } from './session/saveSession';
 import type {
@@ -239,7 +240,7 @@ export class ReplayContainer implements ReplayContainerInterface {
    * Currently, this needs to be manually called (e.g. for tests). Sentry SDK
    * does not support a teardown
    */
-  public stop(reason?: string): void {
+  public async stop(reason?: string): Promise<void> {
     if (!this._isEnabled) {
       return;
     }
@@ -255,12 +256,24 @@ export class ReplayContainer implements ReplayContainerInterface {
         log(msg);
       }
 
+      // We can't move `_isEnabled` after awaiting a flush, otherwise we can
+      // enter into an infinite loop when `stop()` is called while flushing.
       this._isEnabled = false;
       this._removeListeners();
       this.stopRecording();
+
+      this._debouncedFlush.cancel();
+      // See comment above re: `_isEnabled`, we "force" a flush, ignoring the
+      // `_isEnabled` state of the plugin since it was disabled above.
+      await this._flush({ force: true });
+
+      // After flush, destroy event buffer
       this.eventBuffer && this.eventBuffer.destroy();
       this.eventBuffer = null;
-      this._debouncedFlush.cancel();
+
+      // Clear session from session storage, note this means if a new session
+      // is started after, it will not have `previousSessionId`
+      clearSession(this);
     } catch (err) {
       this._handleException(err);
     }
@@ -500,7 +513,7 @@ export class ReplayContainer implements ReplayContainerInterface {
     this.session = session;
 
     if (!this.session.sampled) {
-      this.stop('session unsampled');
+      void this.stop('session unsampled');
       return false;
     }
 
@@ -793,7 +806,7 @@ export class ReplayContainer implements ReplayContainerInterface {
       // This means we retried 3 times and all of them failed,
       // or we ran into a problem we don't want to retry, like rate limiting.
       // In this case, we want to completely stop the replay - otherwise, we may get inconsistent segments
-      this.stop('sendReplay');
+      void this.stop('sendReplay');
 
       const client = getCurrentHub().getClient();
 
@@ -807,8 +820,17 @@ export class ReplayContainer implements ReplayContainerInterface {
    * Flush recording data to Sentry. Creates a lock so that only a single flush
    * can be active at a time. Do not call this directly.
    */
-  private _flush: () => Promise<void> = async () => {
-    if (!this._isEnabled) {
+  private _flush = async ({
+    force = false,
+  }: {
+    /**
+     * If true, flush while ignoring the `_isEnabled` state of
+     * Replay integration. (By default, flush is noop if integration
+     * is stopped).
+     */
+    force?: boolean;
+  } = {}): Promise<void> => {
+    if (!this._isEnabled && !force) {
       // This can happen if e.g. the replay was stopped because of exceeding the retry limit
       return;
     }

packages/replay/test/integration/errorSampleRate.test.ts
@@ -9,13 +9,13 @@ import {
   WINDOW,
 } from '../../src/constants';
 import type { ReplayContainer } from '../../src/replay';
+import { clearSession } from '../../src/session/clearSession';
 import { addEvent } from '../../src/util/addEvent';
 import { PerformanceEntryResource } from '../fixtures/performanceEntry/resource';
 import type { RecordMock } from '../index';
 import { BASE_TIMESTAMP } from '../index';
 import { resetSdkMock } from '../mocks/resetSdkMock';
 import type { DomHandler } from '../types';
-import { clearSession } from '../utils/clearSession';
 import { useFakeTimers } from '../utils/use-fake-timers';
 
 useFakeTimers();

packages/browser-integration-tests/suites/replay/dsc/test.ts
@@ -54,11 +54,19 @@ sentryTest(
       sentryTest.skip();
     }
 
+    await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+      return route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ id: 'test-id' }),
+      });
+    });
+
     const url = await getLocalTestPath({ testDir: __dirname });
     await page.goto(url);
 
-    await page.evaluate(() => {
-      (window as unknown as TestWindow).Replay.stop();
+    await page.evaluate(async () => {
+      await (window as unknown as TestWindow).Replay.stop();
 
       (window as unknown as TestWindow).Sentry.configureScope(scope => {
         scope.setUser({ id: 'user123', segment: 'segmentB' });

packages/replay/src/integration.ts
@@ -219,12 +219,12 @@ Sentry.init({ replaysOnErrorSampleRate: ${errorSampleRate} })`,
    * Currently, this needs to be manually called (e.g. for tests). Sentry SDK
    * does not support a teardown
    */
-  public stop(): void {
+  public stop(): Promise<void> {
     if (!this._replay) {
-      return;
+      return Promise.resolve();
     }
 
-    this._replay.stop();
+    return this._replay.stop();
   }
 
   /**
@@ -234,9 +234,9 @@ Sentry.init({ replaysOnErrorSampleRate: ${errorSampleRate} })`,
    * Unless `continueRecording` is false, the replay will continue to record and
    * behave as a "session"-based replay.
    */
-  public flush(options?: SendBufferedReplayOptions): Promise<void> | void {
+  public flush(options?: SendBufferedReplayOptions): Promise<void> {
     if (!this._replay || !this._replay.isEnabled()) {
-      return;
+      return Promise.resolve();
     }
 
     return this._replay.sendBufferedReplayOrFlush(options);

packages/replay/src/session/clearSession.ts
@@ -1,7 +1,10 @@
 import { REPLAY_SESSION_KEY, WINDOW } from '../../src/constants';
 import type { ReplayContainer } from '../../src/types';
 
-export function clearSession(replay: ReplayContainer) {
+/**
+ * Removes the session from Session Storage and unsets session in replay instance
+ */
+export function clearSession(replay: ReplayContainer): void {
   deleteSession();
   replay.session = undefined;
 }

packages/replay/src/types.ts
@@ -472,7 +472,7 @@ export interface ReplayContainer {
   isPaused(): boolean;
   getContext(): InternalEventContext;
   start(): void;
-  stop(reason?: string): void;
+  stop(reason?: string): Promise<void>;
   pause(): void;
   resume(): void;
   startRecording(): void;

packages/replay/src/util/addEvent.ts
@@ -46,7 +46,7 @@ export async function addEvent(
     return await replay.eventBuffer.addEvent(event, isCheckout);
   } catch (error) {
     __DEBUG_BUILD__ && logger.error(error);
-    replay.stop('addEvent');
+    await replay.stop('addEvent');
 
     const client = getCurrentHub().getClient();
 

packages/replay/test/integration/events.test.ts
@@ -2,12 +2,12 @@ import { getCurrentHub } from '@sentry/core';
 
 import { WINDOW } from '../../src/constants';
 import type { ReplayContainer } from '../../src/replay';
+import { clearSession } from '../../src/session/clearSession';
 import { addEvent } from '../../src/util/addEvent';
 import { PerformanceEntryResource } from '../fixtures/performanceEntry/resource';
 import type { RecordMock } from '../index';
 import { BASE_TIMESTAMP } from '../index';
 import { resetSdkMock } from '../mocks/resetSdkMock';
-import { clearSession } from '../utils/clearSession';
 import { useFakeTimers } from '../utils/use-fake-timers';
 
 useFakeTimers();

commit ef44abc13c63850c14221d22f725ee87bd84ca4a

fix(replay): Fix buffered replays creating replay w/o error occuring
Introduced in #7741. This happens when session replay rate is > 0 and the session is unsampled, it calls `stop()` which ends up flushing without checking the recording mode (session vs buffer).

Closes #8054

packages/replay/jest.setup.ts
@@ -128,6 +128,21 @@ function checkCallForSentReplay(
   };
 }
 
+function getReplayCalls(calls: any[][][]) {
+  return calls.map(call => {
+    const arg = call[0];
+      if (arg.length !== 2) {
+        return [];
+      }
+
+      if (!arg[1][0].find(({type}: {type: string}) => ['replay_event', 'replay_recording'].includes(type))) {
+        return [];
+      }
+
+      return [ arg ];
+  }).filter(Boolean);
+}
+
 /**
  * Checks all calls to `fetch` and ensures a replay was uploaded by
  * checking the `fetch()` request's body.
@@ -143,7 +158,10 @@ const toHaveSentReplay = function (
 
   const expectedKeysLength = expected ? ('sample' in expected ? Object.keys(expected.sample) : Object.keys(expected)).length : 0;
 
-  for (const currentCall of calls) {
+  // Only want calls that send replay events (ignore error events)
+  const replayCalls = getReplayCalls(calls)
+
+  for (const currentCall of replayCalls) {
     result = checkCallForSentReplay.call(this, currentCall[0], expected);
     if (result.pass) {
       break;
@@ -193,7 +211,9 @@ const toHaveLastSentReplay = function (
   expected?: SentReplayExpected | { sample: SentReplayExpected; inverse: boolean },
 ) {
   const { calls } = (getCurrentHub().getClient()?.getTransport()?.send as MockTransport).mock;
-  const lastCall = calls[calls.length - 1]?.[0];
+  const replayCalls = getReplayCalls(calls)
+
+  const lastCall = replayCalls[calls.length - 1]?.[0];
 
   const { results, call, pass } = checkCallForSentReplay.call(this, lastCall, expected);
 

packages/replay/src/replay.ts
@@ -327,7 +327,9 @@ export class ReplayContainer implements ReplayContainerInterface {
       this._debouncedFlush.cancel();
       // See comment above re: `_isEnabled`, we "force" a flush, ignoring the
       // `_isEnabled` state of the plugin since it was disabled above.
-      await this._flush({ force: true });
+      if (this.recordingMode === 'session') {
+        await this._flush({ force: true });
+      }
 
       // After flush, destroy event buffer
       this.eventBuffer && this.eventBuffer.destroy();

packages/replay/test/integration/errorSampleRate-delayFlush.test.ts
@@ -229,6 +229,21 @@ describe('Integration | errorSampleRate with delayed flush', () => {
     });
   });
 
+  // This tests a regression where we were calling flush indiscriminantly in `stop()`
+  it('does not upload a replay event if error is not sampled', async () => {
+    // We are trying to replicate the case where error rate is 0 and session
+    // rate is > 0, we can't set them both to 0 otherwise
+    // `_loadAndCheckSession` is not called when initializing the plugin.
+    replay.stop();
+    replay['_options']['errorSampleRate'] = 0;
+    replay['_loadAndCheckSession']();
+
+    jest.runAllTimers();
+    await new Promise(process.nextTick);
+    expect(mockRecord.takeFullSnapshot).not.toHaveBeenCalled();
+    expect(replay).not.toHaveLastSentReplay();
+  });
+
   it('does not send a replay when triggering a full dom snapshot when document becomes visible after [SESSION_IDLE_EXPIRE_DURATION]ms', async () => {
     Object.defineProperty(document, 'visibilityState', {
       configurable: true,
@@ -664,7 +679,7 @@ describe('Integration | errorSampleRate with delayed flush', () => {
     jest.advanceTimersByTime(DEFAULT_FLUSH_MIN_DELAY);
     await new Promise(process.nextTick);
 
-    expect(replay).toHaveLastSentReplay();
+    expect(replay).not.toHaveLastSentReplay();
 
     // Wait a bit, shortly before session expires
     jest.advanceTimersByTime(MAX_SESSION_LIFE - 1000);

packages/replay/test/integration/errorSampleRate.test.ts
@@ -234,6 +234,22 @@ describe('Integration | errorSampleRate', () => {
     });
   });
 
+  // This tests a regression where we were calling flush indiscriminantly in `stop()`
+  it('does not upload a replay event if error is not sampled', async () => {
+    // We are trying to replicate the case where error rate is 0 and session
+    // rate is > 0, we can't set them both to 0 otherwise
+    // `_loadAndCheckSession` is not called when initializing the plugin.
+    replay.stop();
+    replay['_options']['errorSampleRate'] = 0;
+    replay['_loadAndCheckSession']();
+
+    jest.runAllTimers();
+    await new Promise(process.nextTick);
+    expect(mockRecord.takeFullSnapshot).not.toHaveBeenCalled();
+    expect(replay).not.toHaveLastSentReplay();
+  });
+
+
   it('does not send a replay when triggering a full dom snapshot when document becomes visible after [SESSION_IDLE_EXPIRE_DURATION]ms', async () => {
     Object.defineProperty(document, 'visibilityState', {
       configurable: true,