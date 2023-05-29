7321508be82e7930f8ec73529b0a0ab082b5b518
Billy Vong <billyvg@users.noreply.github.com>
2023-05-29 09:50:44 -0400

    update test

--- packages/browser-integration-tests/suites/replay/largeMutations/mutationLimit/init.js
+++ packages/browser-integration-tests/suites/replay/largeMutations/mutationLimit/init.js
@@ -4,9 +4,7 @@ window.Sentry = Sentry;
 window.Replay = new Sentry.Replay({
   flushMinDelay: 500,
   flushMaxDelay: 500,
-  _experiments: {
-    mutationLimit: 250,
-  },
+  mutationLimit: 250,
 });
 
 Sentry.init({
