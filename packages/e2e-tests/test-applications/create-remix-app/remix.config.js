4319a75a769a8979f96906f280efebf025e0c348
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-06-05 09:29:32 +0200

    fix lint errors

--- packages/e2e-tests/test-applications/create-remix-app/remix.config.js
+++ packages/e2e-tests/test-applications/create-remix-app/remix.config.js
@@ -1,11 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: "cjs",
+  serverModuleFormat: 'cjs',
   future: {
     v2_errorBoundary: true,
     v2_meta: true,
