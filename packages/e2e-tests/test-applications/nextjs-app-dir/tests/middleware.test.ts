3f7dc52b86d0c98c45ab171a19342c1bebbabefa
Luca Forstner <luca.forstner@sentry.io>
2023-06-19 10:37:07 +0000

    .

--- packages/e2e-tests/test-applications/nextjs-app-dir/tests/middleware.test.ts
+++ packages/e2e-tests/test-applications/nextjs-app-dir/tests/middleware.test.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 import { waitForTransaction, waitForError } from '../../../test-utils/event-proxy-server';
 
 test('Should create a transaction for middleware', async ({ request }) => {
+  test.skip(process.env.TEST_ENV === 'development', "Doesn't work in dev mode.");
+
   const middlewareTransactionPromise = waitForTransaction('nextjs-13-app-dir', async transactionEvent => {
     return transactionEvent?.transaction === 'middleware' && transactionEvent?.contexts?.trace?.status === 'ok';
   });
@@ -17,13 +19,17 @@ test('Should create a transaction for middleware', async ({ request }) => {
 });
 
 test('Should create a transaction with error status for faulty middleware', async ({ request }) => {
+  test.skip(process.env.TEST_ENV === 'development', "Doesn't work in dev mode.");
+
   const middlewareTransactionPromise = waitForTransaction('nextjs-13-app-dir', async transactionEvent => {
     return (
       transactionEvent?.transaction === 'middleware' && transactionEvent?.contexts?.trace?.status === 'internal_error'
     );
   });
 
-  request.get('/api/endpoint-behind-middleware', { headers: { 'x-should-throw': '1' } });
+  request.get('/api/endpoint-behind-middleware', { headers: { 'x-should-throw': '1' } }).catch(() => {
+    // Noop
+  });
 
   const middlewareTransaction = await middlewareTransactionPromise;
 
@@ -33,11 +39,15 @@ test('Should create a transaction with error status for faulty middleware', asyn
 });
 
 test('Records exceptions happening in middleware', async ({ request }) => {
+  test.skip(process.env.TEST_ENV === 'development', "Doesn't work in dev mode.");
+
   const errorEventPromise = waitForError('nextjs-13-app-dir', errorEvent => {
     return errorEvent?.exception?.values?.[0]?.value === 'Middleware Error';
   });
 
-  request.get('/api/endpoint-behind-middleware', { headers: { 'x-should-throw': '1' } });
+  request.get('/api/endpoint-behind-middleware', { headers: { 'x-should-throw': '1' } }).catch(() => {
+    // Noop
+  });
 
   expect(await errorEventPromise).toBeDefined();
 });
