e5e6a6bd8ab2bbec59879971595a7248fa132826
Lukas Stracke <lukas.stracke@sentry.io>
2023-06-20 16:27:53 +0200

    fix(angular): Filter out `TryCatch` integration by default (#8367)
    The `TryCatch` default integration interferes with the
    `SentryErrorHander` error handler of the Angular(-Ivy) SDKs by catching
    certain errors too early, before the Angular SDK-specific error handler
    can catch them. This caused missing data on the event in some or
    duplicated errors in other cases.
    
    This fix filters out the `TryCatch` by default, as long as users didn't
    set `defaultIntegrations` in their SDK init. Therefore, it makes the
    previously provided
    [workaround](https://github.com/getsentry/sentry-javascript/issues/5417#issuecomment-1453407097)
    obsolete.

--- packages/angular/test/sdk.test.ts
+++ packages/angular/test/sdk.test.ts
@@ -1,6 +1,6 @@
 import * as SentryBrowser from '@sentry/browser';
 
-import { init } from '../src/sdk';
+import { defaultIntegrations, init } from '../src/index';
 
 describe('init', () => {
   it('sets the Angular version (if available) in the global scope', () => {
@@ -12,5 +12,34 @@ describe('init', () => {
     // (and hence the dependency version of Angular core we installed (see package.json))
     expect(setContextSpy).toHaveBeenCalledTimes(1);
     expect(setContextSpy).toHaveBeenCalledWith('angular', { version: 10 });
+  });
+
+  describe('filtering out the `TryCatch` integration', () => {
+    const browserInitSpy = jest.spyOn(SentryBrowser, 'init');
+
+    beforeEach(() => {
+      browserInitSpy.mockClear();
+    });
+
+    it('filters if `defaultIntegrations` is not set', () => {
+      init({});
+
+      expect(browserInitSpy).toHaveBeenCalledTimes(1);
+
+      const options = browserInitSpy.mock.calls[0][0] || {};
+      expect(options.defaultIntegrations).not.toContainEqual(expect.objectContaining({ name: 'TryCatch' }));
+    });
+
+    it.each([false as const, defaultIntegrations])(
+      "doesn't filter if `defaultIntegrations` is set to %s",
+      defaultIntegrations => {
+        init({ defaultIntegrations });
+
+        expect(browserInitSpy).toHaveBeenCalledTimes(1);
+
+        const options = browserInitSpy.mock.calls[0][0] || {};
+        expect(options.defaultIntegrations).toEqual(defaultIntegrations);
+      },
+    );
   });
 });
