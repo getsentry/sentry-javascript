31879228cbc5635faf9438358de5a0e17b5dd6c6
Luca Forstner <luca.forstner@sentry.io>
2023-06-22 14:42:21 +0200

    fix unit tests

--- packages/tracing/test/integrations/node/prisma.test.ts
+++ packages/tracing/test/integrations/node/prisma.test.ts
@@ -1,10 +1,7 @@
 /* eslint-disable deprecation/deprecation */
-/* eslint-disable @typescript-eslint/unbound-method */
-import { Hub, Scope } from '@sentry/core';
 import { logger } from '@sentry/utils';
 
 import { Integrations } from '../../../src';
-import { getTestClient } from '../../testutils';
 
 const mockTrace = jest.fn();
 
@@ -41,10 +38,7 @@ describe('setupOnce', function () {
   const Client: PrismaClient = new PrismaClient();
 
   beforeAll(() => {
-    new Integrations.Prisma({ client: Client }).setupOnce(
-      () => undefined,
-      () => new Hub(undefined, new Scope()),
-    );
+    new Integrations.Prisma({ client: Client });
   });
 
   beforeEach(() => {
@@ -65,14 +59,7 @@ describe('setupOnce', function () {
   it("doesn't attach when using otel instrumenter", () => {
     const loggerLogSpy = jest.spyOn(logger, 'log');
 
-    const client = getTestClient({ instrumenter: 'otel' });
-    const hub = new Hub(client);
-
-    const integration = new Integrations.Prisma({ client: Client });
-    integration.setupOnce(
-      () => {},
-      () => hub,
-    );
+    new Integrations.Prisma({ client: Client });
 
     expect(loggerLogSpy).toBeCalledWith('Prisma Integration is skipped because of instrumenter configuration.');
   });
