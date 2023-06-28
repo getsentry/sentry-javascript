bcb2d2d37d337b74b55cacd1ed3173e2ec7e8f6c
Abhijeet Prasad <aprasad@sentry.io>
2023-06-28 13:38:38 -0400

    feat(types): Add PropagationContext

--- packages/types/src/tracing.ts
+++ packages/types/src/tracing.ts
@@ -1,1 +1,11 @@
+import type { DynamicSamplingContext } from './envelope';
+
 export type TracePropagationTargets = (string | RegExp)[];
+
+export interface PropagationContext {
+  traceId: string;
+  spanId: string;
+  parentSpanId: string;
+  sampled: boolean;
+  dsc: DynamicSamplingContext;
+}
