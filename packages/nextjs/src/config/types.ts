4ae67c72e45ca070a8639d8108be5e45a3f46123
Luca Forstner <luca.forstner@sentry.io>
2023-07-07 08:36:45 +0000

    Update type to be more lenient

--- packages/nextjs/src/config/types.ts
+++ packages/nextjs/src/config/types.ts
@@ -50,7 +50,7 @@ export type NextConfigObject = {
   // File extensions that count as pages in the `pages/` directory
   pageExtensions?: string[];
   // Whether Next.js should do a static export
-  output?: 'export';
+  output?: string;
   // Paths to reroute when requested
   rewrites?: () => Promise<
     | NextRewrite[]
