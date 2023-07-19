ed889bc3188dec5dab9efb2631cb4c8727cafe96
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-19 10:27:31 +0200

    chore(eslint): Add lint rules for disabled or focused tests (#8489)
    We sometimes forget to remove focused or disabled annotations after debugging tests. This of course isn't ideal and as brought up in #8485 we should add checks against this. Therefore, this patch:
    
    * Adds the [`eslint-plugin-jest` package ](https://github.com/jest-community/eslint-plugin-jest/tree/main)which contains a bunch of jest-syntax-specific rules
    * Enables the `no-focused-tests` rule to throw a lint error if a test is focused with `it.only`, `fit` or similar functions. 
    * Enables the `no-disabled-tests` rule to throw a lint error if a test (suite) from a suite is disabled/skipped with `it.skip`, `xit` or similar functions. While we sometimes skip tests on purpose, I'd argue that we generally want to avoid this, as it can also happen accidentally. For the few exceptions of this rule, we can always ignore it.

--- packages/eslint-config-sdk/src/index.js
+++ packages/eslint-config-sdk/src/index.js
@@ -167,7 +167,7 @@ module.exports = {
       },
     },
     {
-      // Configuration for test files
+      // Configuration for files in test directories
       env: {
         jest: true,
       },
@@ -184,6 +184,24 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         '@sentry-internal/sdk/no-optional-chaining': 'off',
         '@sentry-internal/sdk/no-nullish-coalescing': 'off',
+      },
+    },
+    {
+      // Configuration only for test files (this won't apply to utils or other files in test directories)
+      plugins: ['jest'],
+      env: {
+        jest: true,
+      },
+      files: ['test.ts', '*.test.ts', '*.test.tsx', '*.test.js', '*.test.jsx'],
+      rules: {
+        // Prevent permanent usage of `it.only`, `fit`, `test.only` etc
+        // We want to avoid debugging leftovers making their way into the codebase
+        'jest/no-focused-tests': 'error',
+
+        // Prevent permanent usage of `it.skip`, `xit`, `test.skip` etc
+        // We want to avoid debugging leftovers making their way into the codebase
+        // If there's a good reason to skip a test (e.g. bad flakiness), just add an ignore comment
+        'jest/no-disabled-tests': 'error',
       },
     },
     {
