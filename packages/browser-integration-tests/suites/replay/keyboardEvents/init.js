768b02548be7cccbe529aa5e4678cebdc7000f9c
Francesco Novy <francesco.novy@sentry.io>
2023-07-20 14:52:46 +0200

    test(replay): Reduce flush delay for shorter tests (#8595)

--- packages/browser-integration-tests/suites/replay/keyboardEvents/init.js
+++ packages/browser-integration-tests/suites/replay/keyboardEvents/init.js
@@ -2,8 +2,8 @@ import * as Sentry from '@sentry/browser';
 
 window.Sentry = Sentry;
 window.Replay = new Sentry.Replay({
-  flushMinDelay: 1000,
-  flushMaxDelay: 1000,
+  flushMinDelay: 200,
+  flushMaxDelay: 200,
 });
 
 Sentry.init({
