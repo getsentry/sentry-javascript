83a8ad8a9743714fb71200741e0c96510f986f88
Onur Temizkan <onur@narval.co.uk>
2023-07-20 22:37:53 +0100

    fix(tests): Update `loader` integration tests to avoid flakes.

--- packages/browser-integration-tests/loader-suites/loader/onLoad/captureExceptionInOnLoad/test.ts
+++ packages/browser-integration-tests/loader-suites/loader/onLoad/captureExceptionInOnLoad/test.ts
@@ -1,15 +1,13 @@
 import { expect } from '@playwright/test';
 
 import { sentryTest } from '../../../../utils/fixtures';
-import { envelopeRequestParser, waitForErrorRequest } from '../../../../utils/helpers';
+import { envelopeRequestParser, waitForErrorRequestOnUrl } from '../../../../utils/helpers';
 
 sentryTest('captureException works inside of onLoad', async ({ getLocalTestUrl, page }) => {
-  const req = waitForErrorRequest(page);
-
   const url = await getLocalTestUrl({ testDir: __dirname });
-  await page.goto(url);
+  const req = await waitForErrorRequestOnUrl(page, url);
 
-  const eventData = envelopeRequestParser(await req);
+  const eventData = envelopeRequestParser(req);
 
   expect(eventData.message).toBe('Test exception');
 });
