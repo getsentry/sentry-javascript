74dd18e5bef7f468fc0a9f9b2e3add7e986b98f9
Billy Vong <billyvg@users.noreply.github.com>
2023-07-31 16:25:45 -0400

    test(replay): Add additional tests for aria-label (#8675)
    Adds additional tests for `aria-label`. Will be needed for rrweb2
    upgrade

--- packages/browser-integration-tests/suites/replay/privacyInputMaskAll/test.ts
+++ packages/browser-integration-tests/suites/replay/privacyInputMaskAll/test.ts
@@ -5,6 +5,7 @@ import type { inputData } from '@sentry-internal/rrweb/typings/types';
 import { sentryTest } from '../../../utils/fixtures';
 import type { IncrementalRecordingSnapshot } from '../../../utils/replayHelpers';
 import {
+  getFullRecordingSnapshots,
   getIncrementalRecordingSnapshots,
   shouldSkipReplayTest,
   waitForReplayRequest,
@@ -56,7 +57,10 @@ sentryTest(
     const url = await getLocalTestPath({ testDir: __dirname });
 
     await page.goto(url);
-    await reqPromise0;
+    const fullSnapshot = getFullRecordingSnapshots(await reqPromise0);
+    const stringifiedSnapshot = JSON.stringify(fullSnapshot);
+    expect(stringifiedSnapshot.includes('Submit form')).toBe(false);
+    expect(stringifiedSnapshot.includes('Unmasked button')).toBe(true);
 
     const text = 'test';
 
