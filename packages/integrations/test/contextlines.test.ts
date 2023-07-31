cbfaa35e7c96616356f136d62753b9a47d54b69a
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-31 13:25:37 +0200

    move to `@sentry/integrations`

--- packages/integrations/test/contextlines.test.ts
+++ packages/integrations/test/contextlines.test.ts
@@ -1,6 +1,6 @@
 import type { StackFrame } from '@sentry/types';
 
-import { applySourceContextToFrame } from '../../../src/integrations/contextlines';
+import { applySourceContextToFrame } from '../src/contextlines';
 
 const lines = ['line1', 'line2', 'line3', 'line4', 'line5', 'line6', 'line7', 'line8', 'line9'];
 describe('ContextLines', () => {
