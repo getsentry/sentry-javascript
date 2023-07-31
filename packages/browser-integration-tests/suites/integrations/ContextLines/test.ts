3fdf04bfa86747fb66602d4178afa0c0fcb96776
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-31 08:59:06 +0200

    improve integration test

--- packages/browser-integration-tests/suites/integrations/ContextLines/test.ts
+++ packages/browser-integration-tests/suites/integrations/ContextLines/test.ts
@@ -5,15 +5,15 @@ import { sentryTest } from '../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest } from '../../../utils/helpers';
 
 sentryTest(
-  'should add source context lines around stack frames from errors in Html',
+  'should add source context lines around stack frames from errors in Html inline JS',
   async ({ getLocalTestPath, page }) => {
     const url = await getLocalTestPath({ testDir: __dirname });
 
     const eventPromise = getFirstSentryEnvelopeRequest<Event>(page, url);
 
-    await page.click('#inline-error-btn');
+    const clickPromise = page.click('#inline-error-btn');
 
-    const eventData = await eventPromise;
+    const [eventData] = await Promise.all([eventPromise, clickPromise]);
 
     expect(eventData.exception?.values).toHaveLength(1);
 
@@ -23,14 +23,12 @@ sentryTest(
       stacktrace: {
         frames: [
           {
-            colno: 97,
-            lineno: 7,
             pre_context: ['    <meta charset="utf-8">', '  </head>', '  <body>'],
             context_line:
               '      <button id="inline-error-btn" onclick="throw new Error(\'Error with context lines\')">Click me</button>',
             post_context: [
               '      <button id="script-error-btn">Click me too</button>',
-              '  <script defer="" src="init.bundle.js"></script><script defer="" src="subject.bundle.js"></script>',
+              expect.stringContaining('subject.bundle.js'), // this line varies in the test based on tarball/cdn bundle (+variants)
               '  <footer>',
             ],
           },
@@ -40,7 +38,7 @@ sentryTest(
   },
 );
 
-sentryTest('should not add source context lines to errors from scripts', async ({ getLocalTestPath, page }) => {
+sentryTest('should not add source context lines to errors from script files', async ({ getLocalTestPath, page }) => {
   const url = await getLocalTestPath({ testDir: __dirname });
 
   const eventPromise = getFirstSentryEnvelopeRequest<Event>(page, url);
