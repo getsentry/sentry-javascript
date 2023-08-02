1aa1a6d36ae5e6af38bed330fce7dbdd1df7a503
Abhijeet Prasad <aprasad@sentry.io>
2023-08-01 22:47:24 -0400

    feat(core): Add startActiveSpan and startSpan top level methods

--- packages/core/src/tracing/trace.ts
+++ packages/core/src/tracing/trace.ts
@@ -7,12 +7,15 @@ import type { Span } from './span';
 
 /**
  * Wraps a function with a transaction/span and finishes the span after the function is done.
+ * The created span is the active span and will be used as parent by other spans created inside the function
+ * and can be accessed via `Sentry.getSpan()`, as long as the function is executed while the scope is active.
  *
  * Note that if you have not enabled tracing extensions via `addTracingExtensions`
  * or you didn't set `tracesSampleRate`, this function will not generate spans
  * and the `span` returned from the callback will be undefined.
  *
  * This function is meant to be used internally and may break at any time. Use at your own risk.
+ * Please use {@link startActiveSpan} instead.
  *
  * @internal
  * @private
@@ -31,7 +34,6 @@ export function trace<T>(
 
   const hub = getCurrentHub();
   const scope = hub.getScope();
-
   const parentSpan = scope.getSpan();
 
   function getActiveSpan(): Span | undefined {
@@ -75,4 +77,50 @@ export function trace<T>(
   }
 
   return maybePromiseResult;
+}
+
+/**
+ * Wraps a function with a transaction/span and finishes the span after the function is done.
+ * The created span is the active span and will be used as parent by other spans created inside the function
+ * and can be accessed via `Sentry.getSpan()`, as long as the function is executed while the scope is active.
+ *
+ * If you want to create a span that is not set as active, use {@link startSpan}.
+ *
+ * Note that if you have not enabled tracing extensions via `addTracingExtensions`
+ * or you didn't set `tracesSampleRate`, this function will not generate spans
+ * and the `span` returned from the callback will be undefined.
+ */
+export const startActiveSpan = trace;
+
+/**
+ * Creates a span. This span is not set as active, so will not get automatic instrumentation spans
+ * as children or be able to be accessed via `Sentry.getSpan()`.
+ *
+ * If you want to create a span that is set as active, use {@link startActiveSpan}.
+ *
+ * Note that if you have not enabled tracing extensions via `addTracingExtensions`
+ * or you didn't set `tracesSampleRate` or `tracesSampler`, this function will not generate spans
+ * and the `span` returned from the callback will be undefined.
+ */
+export function startSpan(context: TransactionContext): Span | undefined {
+  if (!hasTracingEnabled()) {
+    return undefined;
+  }
+
+  const ctx = { ...context };
+  // If a name is set and a description is not, set the description to the name.
+  if (ctx.name !== undefined && ctx.description === undefined) {
+    ctx.description = ctx.name;
+  }
+
+  const hub = getCurrentHub();
+  const parentSpan = getSpan();
+  return parentSpan ? parentSpan.startChild(ctx) : hub.startTransaction(ctx);
+}
+
+/**
+ * Returns the currently active span.
+ */
+export function getSpan(): Span | undefined {
+  return getCurrentHub().getScope().getSpan();
 }
