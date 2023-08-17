0b9a8b23e4a9ac27f06abf1785b9e65d84996a97
Francesco Novy <francesco.novy@sentry.io>
2023-08-17 12:13:17 +0200

    WIP

--- packages/replay/src/replay.ts
+++ packages/replay/src/replay.ts
@@ -243,7 +243,15 @@ export class ReplayContainer implements ReplayContainerInterface {
       return;
     }
 
-    this.recordingMode = this.session.sampled === 'buffer' ? 'buffer' : 'session';
+    let recordingMode: ReplayRecordingMode = 'session';
+
+    // If segmentId > 0, it means we've previously already captured this session
+    // In this case, we still want to continue in `session` recording mode
+    if (this.session.sampled === 'buffer' && this.session.segmentId === 0) {
+      recordingMode = 'buffer';
+    }
+
+    this.recordingMode = recordingMode;
 
     logInfoNextTick(
       `[Replay] Starting replay in ${this.recordingMode} mode`,
