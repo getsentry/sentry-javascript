bf14c8aa9a64ec510c7ba84b6780b192e7ffa6a4
Luca Forstner <luca.forstner@sentry.io>
2023-08-29 11:38:11 +0000

    properly flush

--- packages/nextjs/src/common/wrapRouteHandlerWithSentry.ts
+++ packages/nextjs/src/common/wrapRouteHandlerWithSentry.ts
@@ -28,37 +28,40 @@ export function wrapRouteHandlerWithSentry<F extends (...args: any[]) => any>(
         );
         currentScope.setPropagationContext(propagationContext);
 
-        const res = await trace(
-          {
-            op: 'http.server',
-            name: `${method} ${parameterizedRoute}`,
-            status: 'ok',
-            ...traceparentData,
-            metadata: {
-              source: 'route',
-              dynamicSamplingContext: traceparentData && !dynamicSamplingContext ? {} : dynamicSamplingContext,
+        let res;
+        try {
+          res = await trace(
+            {
+              op: 'http.server',
+              name: `${method} ${parameterizedRoute}`,
+              status: 'ok',
+              ...traceparentData,
+              metadata: {
+                source: 'route',
+                dynamicSamplingContext: traceparentData && !dynamicSamplingContext ? {} : dynamicSamplingContext,
+              },
             },
-          },
-          async span => {
-            const response: Response = await originalFunction.apply(thisArg, args);
+            async span => {
+              const response: Response = await originalFunction.apply(thisArg, args);
 
-            try {
-              span?.setHttpStatus(response.status);
-            } catch {
-              // best effort
-            }
+              try {
+                span?.setHttpStatus(response.status);
+              } catch {
+                // best effort
+              }
 
-            return response;
-          },
-          error => {
-            captureException(error);
-          },
-        );
-
-        if (!platformSupportsStreaming() || process.env.NEXT_RUNTIME === 'edge') {
-          // 1. Edge tranpsort requires manual flushing
-          // 2. Lambdas require manual flushing to prevent execution freeze before the event is sent
-          await flush(1000);
+              return response;
+            },
+            error => {
+              captureException(error);
+            },
+          );
+        } finally {
+          if (!platformSupportsStreaming() || process.env.NEXT_RUNTIME === 'edge') {
+            // 1. Edge tranpsort requires manual flushing
+            // 2. Lambdas require manual flushing to prevent execution freeze before the event is sent
+            await flush(1000);
+          }
         }
 
         return res;
