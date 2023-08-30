7278e3b1ff9957c150e5949c2153ededaf3e2bfe
Francesco Novy <francesco.novy@sentry.io>
2023-08-30 08:52:52 +0200

    fix debug test

--- packages/browser-integration-tests/suites/public-api/debug/test.ts
+++ packages/browser-integration-tests/suites/public-api/debug/test.ts
@@ -5,6 +5,9 @@ import { expect } from '@playwright/test';
 import { sentryTest } from '../../../utils/fixtures';
 
 sentryTest('logs debug messages correctly', async ({ getLocalTestUrl, page }) => {
+  const bundleKey = process.env.PW_BUNDLE || '';
+  const hasDebug = !bundleKey.includes('_min');
+
   const url = await getLocalTestUrl({ testDir: __dirname });
 
   const consoleMessages: string[] = [];
@@ -17,18 +20,22 @@ sentryTest('logs debug messages correctly', async ({ getLocalTestUrl, page }) =>
 
   await page.evaluate(() => console.log('test log'));
 
-  expect(consoleMessages).toEqual([
-    'Sentry Logger [log]: Integration installed: InboundFilters',
-    'Sentry Logger [log]: Integration installed: FunctionToString',
-    'Sentry Logger [log]: Integration installed: TryCatch',
-    'Sentry Logger [log]: Integration installed: Breadcrumbs',
-    'Sentry Logger [log]: Global Handler attached: onerror',
-    'Sentry Logger [log]: Global Handler attached: onunhandledrejection',
-    'Sentry Logger [log]: Integration installed: GlobalHandlers',
-    'Sentry Logger [log]: Integration installed: LinkedErrors',
-    'Sentry Logger [log]: Integration installed: Dedupe',
-    'Sentry Logger [log]: Integration installed: HttpContext',
-    'Sentry Logger [warn]: Discarded session because of missing or non-string release',
-    'test log',
-  ]);
+  expect(consoleMessages).toEqual(
+    hasDebug
+      ? [
+          'Sentry Logger [log]: Integration installed: InboundFilters',
+          'Sentry Logger [log]: Integration installed: FunctionToString',
+          'Sentry Logger [log]: Integration installed: TryCatch',
+          'Sentry Logger [log]: Integration installed: Breadcrumbs',
+          'Sentry Logger [log]: Global Handler attached: onerror',
+          'Sentry Logger [log]: Global Handler attached: onunhandledrejection',
+          'Sentry Logger [log]: Integration installed: GlobalHandlers',
+          'Sentry Logger [log]: Integration installed: LinkedErrors',
+          'Sentry Logger [log]: Integration installed: Dedupe',
+          'Sentry Logger [log]: Integration installed: HttpContext',
+          'Sentry Logger [warn]: Discarded session because of missing or non-string release',
+          'test log',
+        ]
+      : ['[Sentry] Cannot initialize SDK with `debug` option using a non-debug bundle.', 'test log'],
+  );
 });
