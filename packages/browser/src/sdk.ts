eb2998196e63c6b9ba9ebc01d9542a1a06b143f0
Lukas Stracke <lukas.stracke@sentry.io>
2023-09-04 13:55:32 +0200

    ref(browser): Deprecate top-level `wrap` function (#8927)
    Deprecate the `wrap` function exported from `@sentry/browser`. 
    
    This function is documented nowhere, not part of the unified API and
    afaict not used by any SDKs depending on `@sentry/browser`.

--- packages/browser/src/sdk.ts
+++ packages/browser/src/sdk.ts
@@ -192,10 +192,15 @@ export function onLoad(callback: () => void): void {
 /**
  * Wrap code within a try/catch block so the SDK is able to capture errors.
  *
+ * @deprecated This function will be removed in v8.
+ * It is not part of Sentry's official API and it's easily replaceable by using a try/catch block
+ * and calling Sentry.captureException.
+ *
  * @param fn A function to wrap.
  *
  * @returns The result of wrapped function call.
  */
+// TODO(v8): Remove this function
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function wrap(fn: (...args: any) => any): any {
   return internalWrap(fn)();
