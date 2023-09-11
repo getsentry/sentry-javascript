d5667e3b90a3a6d736f4c5b80e32515a1b444033
Francesco Novy <francesco.novy@sentry.io>
2023-09-11 13:45:38 +0200

    build(lint): Remove unneeded `@ts-expect` comments

--- packages/replay-worker/src/_worker.ts
+++ packages/replay-worker/src/_worker.ts
@@ -3,7 +3,6 @@ import { handleMessage } from './handleMessage';
 addEventListener('message', handleMessage);
 
 // Immediately send a message when worker loads, so we know the worker is ready
-// @ts-expect-error this syntax is actually fine
 postMessage({
   id: undefined,
   method: 'init',
