9fd188cab7f9d00c0223121185383bed6a10a509
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-09-11 16:22:49 +0200

    feat(types): Export `BeforeFinishCallback`

--- packages/core/src/tracing/index.ts
+++ packages/core/src/tracing/index.ts
@@ -1,5 +1,6 @@
 export { startIdleTransaction, addTracingExtensions } from './hubextensions';
 export { IdleTransaction, TRACING_DEFAULTS } from './idletransaction';
+export type { BeforeFinishCallback } from './idletransaction';
 export { Span, spanStatusfromHttpCode } from './span';
 export { Transaction } from './transaction';
 export { extractTraceparentData, getActiveTransaction } from './utils';
