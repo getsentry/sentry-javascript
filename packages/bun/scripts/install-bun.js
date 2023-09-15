0b1c553fd2d8492fc60379b8bc95950d18d79b88
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-09-15 10:11:56 +0200

    fix: Adding a stupid test

---
+++ packages/bun/scripts/install-bun.js
@@ -0,0 +1,44 @@
+const { exec } = require('child_process');
+const https = require('https');
+
+// Define the URL of the Bash script for bun installation
+const installScriptUrl = 'https://bun.sh/install';
+
+// Check if bun is installed
+exec('bun -version', error => {
+  if (error) {
+    console.error('bun is not installed. Installing...');
+    // Download and execute the installation script
+    https
+      .get(installScriptUrl, res => {
+        if (res.statusCode !== 200) {
+          console.error(`Failed to download the installation script (HTTP ${res.statusCode})`);
+          process.exit(1);
+        }
+
+        res.setEncoding('utf8');
+        let scriptData = '';
+
+        res.on('data', chunk => {
+          scriptData += chunk;
+        });
+
+        res.on('end', () => {
+          // Execute the downloaded script
+          exec(scriptData, installError => {
+            if (installError) {
+              console.error('Failed to install bun:', installError);
+              process.exit(1);
+            }
+            console.log('bun has been successfully installed.');
+          });
+        });
+      })
+      .on('error', e => {
+        console.error('Failed to download the installation script:', e);
+        process.exit(1);
+      });
+  } else {
+    // Bun is installed
+  }
+});
---
+++ packages/bun/test/sdk.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'bun:test';
+
+import { init } from '../src/index';
+
+test("calling init shouldn't fail", () => {
+  init({
+    dsn: 'https://00000000000000000000000000000000@o000000.ingest.sentry.io/0000000',
+  });
+  expect(true).toBe(true);
+});
