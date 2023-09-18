1cfd5765c8654875218a4bc79fbc6d4041ad0f64
Onur Temizkan <onur@narval.co.uk>
2023-09-18 18:48:36 +0100

    Switch to `isRouteErrorResponse` on Remix v2.

--- packages/e2e-tests/test-applications/create-remix-app/app/entry.server.tsx
+++ packages/e2e-tests/test-applications/create-remix-app/app/entry.server.tsx
@@ -22,11 +22,7 @@ Sentry.init({
 });
 
 export function handleError(error: unknown, { request }: DataFunctionArgs): void {
-  if (error instanceof Error) {
-    Sentry.captureRemixServerException(error, 'remix.server', request);
-  } else {
-    Sentry.captureException(error);
-  }
+  Sentry.captureRemixServerException(error, 'remix.server', request);
 }
 
 export default function handleRequest(
