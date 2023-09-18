545996493f302c19ae02a2f570948c1a4adb4ce7
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-09-18 17:53:02 +0200

    Apply suggestions from code review
    Co-authored-by: Abhijeet Prasad <aprasad@sentry.io>

--- packages/bun/src/client.ts
+++ packages/bun/src/client.ts
@@ -5,14 +5,14 @@ import * as os from 'os';
 import type { BunClientOptions } from './types';
 
 /**
- * The Sentry Node SDK Client.
+ * The Sentry Bun SDK Client.
  *
  * @see BunClientOptions for documentation on configuration options.
  * @see SentryClient for usage documentation.
  */
 export class BunClient extends ServerRuntimeClient<BunClientOptions> {
   /**
-   * Creates a new Node SDK instance.
+   * Creates a new Bun SDK instance.
    * @param options Configuration options for this SDK.
    */
   public constructor(options: BunClientOptions) {
