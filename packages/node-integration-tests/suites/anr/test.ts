c9025da1fedba88ff8c42d2afad7eee56d0c6ce4
Tim Fish <tim@timfish.uk>
2023-09-21 02:13:19 +0100

    Dont test stack frame detail on node < 12

--- packages/node-integration-tests/suites/anr/test.ts
+++ packages/node-integration-tests/suites/anr/test.ts
@@ -1,9 +1,15 @@
 import type { Event } from '@sentry/node';
+import { parseSemver } from '@sentry/utils';
 import * as childProcess from 'child_process';
 import * as path from 'path';
 
+const NODE_VERSION = parseSemver(process.versions.node).major || 0;
+
 test('should report ANR when event loop blocked', done => {
-  expect.assertions(5);
+  // The stack trace is different when node < 12
+  const testFramesDetails = NODE_VERSION >= 12;
+
+  expect.assertions(testFramesDetails ? 6 : 4);
 
   const testScriptPath = path.resolve(__dirname, 'scenario.js');
 
@@ -13,8 +19,12 @@ test('should report ANR when event loop blocked', done => {
     expect(event.exception?.values?.[0].mechanism).toEqual({ type: 'ANR' });
     expect(event.exception?.values?.[0].type).toEqual('ApplicationNotResponding');
     expect(event.exception?.values?.[0].value).toEqual('Application Not Responding for at least 200 ms');
-    expect(event.exception?.values?.[0].stacktrace?.frames?.[2].function).toEqual('?');
-    expect(event.exception?.values?.[0].stacktrace?.frames?.[3].function).toEqual('longWork');
+    expect(event.exception?.values?.[0].stacktrace?.frames?.length).toBeGreaterThan(4);
+
+    if (testFramesDetails) {
+      expect(event.exception?.values?.[0].stacktrace?.frames?.[2].function).toEqual('?');
+      expect(event.exception?.values?.[0].stacktrace?.frames?.[3].function).toEqual('longWork');
+    }
 
     done();
   });
