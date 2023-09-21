549beda8c41eea9a14f19ea0354820aab725be29
Tim Fish <tim@timfish.uk>
2023-09-21 19:17:01 +0100

    Also test ESM

--- packages/node-integration-tests/suites/anr/test.ts
+++ packages/node-integration-tests/suites/anr/test.ts
@@ -5,27 +5,54 @@ import * as path from 'path';
 
 const NODE_VERSION = parseSemver(process.versions.node).major || 0;
 
-test('should report ANR when event loop blocked', done => {
-  // The stack trace is different when node < 12
-  const testFramesDetails = NODE_VERSION >= 12;
+describe('should report ANR when event loop blocked', () => {
+  test('CJS', done => {
+    // The stack trace is different when node < 12
+    const testFramesDetails = NODE_VERSION >= 12;
 
-  expect.assertions(testFramesDetails ? 6 : 4);
+    expect.assertions(testFramesDetails ? 6 : 4);
 
-  const testScriptPath = path.resolve(__dirname, 'scenario.js');
+    const testScriptPath = path.resolve(__dirname, 'scenario.js');
 
-  childProcess.exec(`node ${testScriptPath}`, { encoding: 'utf8' }, (_, stdout) => {
-    const event = JSON.parse(stdout) as Event;
+    childProcess.exec(`node ${testScriptPath}`, { encoding: 'utf8' }, (_, stdout) => {
+      const event = JSON.parse(stdout) as Event;
 
-    expect(event.exception?.values?.[0].mechanism).toEqual({ type: 'ANR' });
-    expect(event.exception?.values?.[0].type).toEqual('ApplicationNotResponding');
-    expect(event.exception?.values?.[0].value).toEqual('Application Not Responding for at least 200 ms');
-    expect(event.exception?.values?.[0].stacktrace?.frames?.length).toBeGreaterThan(4);
+      expect(event.exception?.values?.[0].mechanism).toEqual({ type: 'ANR' });
+      expect(event.exception?.values?.[0].type).toEqual('ApplicationNotResponding');
+      expect(event.exception?.values?.[0].value).toEqual('Application Not Responding for at least 200 ms');
+      expect(event.exception?.values?.[0].stacktrace?.frames?.length).toBeGreaterThan(4);
 
-    if (testFramesDetails) {
-      expect(event.exception?.values?.[0].stacktrace?.frames?.[2].function).toEqual('?');
-      expect(event.exception?.values?.[0].stacktrace?.frames?.[3].function).toEqual('longWork');
+      if (testFramesDetails) {
+        expect(event.exception?.values?.[0].stacktrace?.frames?.[2].function).toEqual('?');
+        expect(event.exception?.values?.[0].stacktrace?.frames?.[3].function).toEqual('longWork');
+      }
+
+      done();
+    });
+  });
+
+  test('ESM', done => {
+    // The stack trace is different when node < 12
+    if (NODE_VERSION < 12) {
+      done();
+      return;
     }
 
-    done();
+    expect.assertions(6);
+
+    const testScriptPath = path.resolve(__dirname, 'scenario.mjs');
+
+    childProcess.exec(`node ${testScriptPath}`, { encoding: 'utf8' }, (_, stdout) => {
+      const event = JSON.parse(stdout) as Event;
+
+      expect(event.exception?.values?.[0].mechanism).toEqual({ type: 'ANR' });
+      expect(event.exception?.values?.[0].type).toEqual('ApplicationNotResponding');
+      expect(event.exception?.values?.[0].value).toEqual('Application Not Responding for at least 200 ms');
+      expect(event.exception?.values?.[0].stacktrace?.frames?.length).toBeGreaterThan(4);
+      expect(event.exception?.values?.[0].stacktrace?.frames?.[2].function).toEqual('?');
+      expect(event.exception?.values?.[0].stacktrace?.frames?.[3].function).toEqual('longWork');
+
+      done();
+    });
   });
 });
