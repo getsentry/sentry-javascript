262e249839126af9fa43ff7c32fb17be7fe21691
Billy Vong <billyvg@users.noreply.github.com>
2023-10-03 10:28:47 -0400

    cleanup

--- packages/feedback/test/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './mocks/mockRrweb'; // XXX: Needs to happen before `mockSdk` or importing Replay!
-export * from './mocks/mockSdk';
-
-export const BASE_TIMESTAMP = new Date('2020-02-02 00:00:00').getTime(); // 1580619600000
