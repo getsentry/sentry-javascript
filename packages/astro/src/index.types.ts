cdf46ec0de4b2a7ac884f8183961b32ead762b50
Lukas Stracke <lukas.stracke@sentry.io>
2023-10-06 12:09:22 +0200

    feat(astro): Add server and client SDK init functions

--- packages/astro/src/index.types.ts
+++ packages/astro/src/index.types.ts
@@ -1,1 +1,25 @@
-export type Placeholder = true;
+/* eslint-disable import/export */
+
+// We export everything from both the client part of the SDK and from the server part.
+// Some of the exports collide, which is not allowed, unless we redifine the colliding
+// exports in this file - which we do below.
+export * from './index.client';
+export * from './index.server';
+
+import type { Integration, Options, StackParser } from '@sentry/types';
+
+import type * as clientSdk from './index.client';
+import type * as serverSdk from './index.server';
+
+/** Initializes Sentry Astro SDK */
+export declare function init(options: Options | clientSdk.BrowserOptions | serverSdk.NodeOptions): void;
+
+// We export a merged Integrations object so that users can (at least typing-wise) use all integrations everywhere.
+export declare const Integrations: typeof clientSdk.Integrations & typeof serverSdk.Integrations;
+
+export declare const defaultIntegrations: Integration[];
+export declare const defaultStackParser: StackParser;
+
+export declare function close(timeout?: number | undefined): PromiseLike<boolean>;
+export declare function flush(timeout?: number | undefined): PromiseLike<boolean>;
+export declare function lastEventId(): string | undefined;
