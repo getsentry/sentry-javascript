96c905cfc222c5a26e424e4a291636c3ac1e1146
Tim Fish <tim@timfish.uk>
2023-10-10 12:30:30 +0200

    Ensure deno is installed

---
+++ packages/deno/scripts/install-deno.mjs
@@ -0,0 +1,31 @@
+import { execSync } from 'child_process';
+
+async function download(url) {
+  try {
+    return await fetch(url).then(res => res.text());
+  } catch (e) {
+    console.error('Failed to download', url, e);
+    process.exit(1);
+  }
+}
+
+try {
+  // Ensure we're using the latest Deno version
+  execSync('deno upgrade', { stdio: 'inherit' });
+} catch (_) {
+  console.error('Deno is not installed. Installing...');
+  if (process.platform === 'win32') {
+    // TODO
+    console.error('Please install Deno manually: https://docs.deno.com/runtime/manual/getting_started/installation');
+    process.exit(1);
+  } else {
+    const script = await download('https://deno.land/x/install/install.sh');
+
+    try {
+      execSync(script, { stdio: 'inherit' });
+    } catch (e) {
+      console.error('Failed to install Deno', e);
+      process.exit(1);
+    }
+  }
+}
