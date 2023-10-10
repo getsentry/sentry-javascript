e5b90b89e8010047c81750822e7fac496b58988f
Billy Vong <billyvg@users.noreply.github.com>
2023-10-10 11:40:14 -0400

    lint

--- packages/feedback/src/sendFeedback.ts
+++ packages/feedback/src/sendFeedback.ts
@@ -1,5 +1,6 @@
 import type { BrowserClient, Replay } from '@sentry/browser';
 import { getCurrentHub } from '@sentry/core';
+import { GLOBAL_OBJ } from '@sentry/utils';
 
 import { sendFeedbackRequest } from './util/sendFeedbackRequest';
 
@@ -17,20 +18,25 @@ interface SendFeedbackOptions {
 /**
  * Public API to send a Feedback item to Sentry
  */
-export function sendFeedback({name, email, message, url = document.location.href}: SendFeedbackParams, {includeReplay = true}: SendFeedbackOptions = {}) {
-    const replay = includeReplay ? getCurrentHub()?.getClient<BrowserClient>()?.getIntegrationById('Replay') as Replay | undefined : undefined;
+export function sendFeedback(
+  { name, email, message, url = document.location.href }: SendFeedbackParams,
+  { includeReplay = true }: SendFeedbackOptions = {},
+) {
+  const replay = includeReplay
+    ? (getCurrentHub()?.getClient<BrowserClient>()?.getIntegrationById('Replay') as Replay | undefined)
+    : undefined;
 
-    // Prepare session replay
-    replay?.flush();
-    const replayId = replay?.getReplayId();
+  // Prepare session replay
+  replay?.flush();
+  const replayId = replay?.getReplayId();
 
-    return sendFeedbackRequest({
-      feedback: {
-        name,
-        email,
-        message,
-        url,
-        replay_id: replayId,
-      }
-    });
+  return sendFeedbackRequest({
+    feedback: {
+      name,
+      email,
+      message,
+      url,
+      replay_id: replayId,
+    },
+  });
 }
--- packages/feedback/src/widget/util/createElement.ts
+++ packages/feedback/src/widget/util/createElement.ts
@@ -1,54 +1,49 @@
-    /**
-     *
-     */
-    export function createElement<K extends keyof HTMLElementTagNameMap>(tagName: K, attributes: {[key: string]: string|boolean|EventListenerOrEventListenerObject}| null, ...children: any): HTMLElementTagNameMap[K] {
-        const element = document.createElement(tagName);
+/**
+ * Helper function to create an element. Could be used as a JSX factory
+ * (i.e. React-like syntax).
+ */
+export function createElement<K extends keyof HTMLElementTagNameMap>(
+  tagName: K,
+  attributes: { [key: string]: string | boolean | EventListenerOrEventListenerObject } | null,
+  ...children: any
+): HTMLElementTagNameMap[K] {
+  const element = document.createElement(tagName);
 
-        if (attributes) {
-          Object.entries(attributes).forEach(([attribute, attributeValue]) => {
-            if (attribute === 'className' && typeof attributeValue === 'string') { // JSX does not allow class as a valid name
-              element.setAttribute('class', attributeValue);
-            } else if (typeof attributeValue === 'boolean' && attributeValue) {
-              element.setAttribute(attribute, '');
-            } else if (typeof attributeValue === 'string'){
-              element.setAttribute(attribute, attributeValue);
-            } else if (attribute.startsWith('on') && typeof attributeValue === 'function') {
-              element.addEventListener(attribute.substring(2).toLowerCase(), attributeValue);
-            }
-          })
-        }
-        Object.values(children).forEach(child => appendChild(element, child))
+  if (attributes) {
+    Object.entries(attributes).forEach(([attribute, attributeValue]) => {
+      if (attribute === 'className' && typeof attributeValue === 'string') {
+        // JSX does not allow class as a valid name
+        element.setAttribute('class', attributeValue);
+      } else if (typeof attributeValue === 'boolean' && attributeValue) {
+        element.setAttribute(attribute, '');
+      } else if (typeof attributeValue === 'string') {
+        element.setAttribute(attribute, attributeValue);
+      } else if (attribute.startsWith('on') && typeof attributeValue === 'function') {
+        element.addEventListener(attribute.substring(2).toLowerCase(), attributeValue);
+      }
+    });
+  }
+  for (const child of children) {
+    appendChild(element, child);
+  }
 
-        return element;
+  return element;
+}
+
+function appendChild(parent: Node, child: any): void {
+  if (typeof child === 'undefined' || child === null) {
+    return;
+  }
+
+  if (Array.isArray(child)) {
+    for (const value of child) {
+      appendChild(parent, value);
     }
-
-    function appendChild(parent: Node, child: any) {
-        if (typeof child === 'undefined' || child === null) {
-            return;
-        }
-
-        if (Array.isArray(child)) {
-            for (const value of child) {
-                appendChild(parent, value);
-            }
-        } else if (typeof child === 'string') {
-            parent.appendChild(document.createTextNode(child));
-        } else if (child instanceof Node) {
-            parent.appendChild(child);
-        } else {
-            parent.appendChild(document.createTextNode(String(child)));
-        }
-    }
-
-// export function createElement(tagName: keyof HTMLElementTagNameMap, attributes: Record<string, string>, ...children: HTMLElement[]) {
-//   const el = document.createElement(tagName)
-//   Object.entries(attributes).forEach(([key, val]) => el[key] = val);
-//   children.forEach(child => el.appendChild(child));
-//   return el;
-// }
-
-// const form = createElement('form');
-
-  // c('form', {},
-  //   c('input', {}),
-  // )
+  } else if (typeof child === 'string') {
+    parent.appendChild(document.createTextNode(child));
+  } else if (child instanceof Node) {
+    parent.appendChild(child);
+  } else {
+    parent.appendChild(document.createTextNode(String(child)));
+  }
+}
