0a4aa0e3f10b5093a9924b5498b86b63ceb0e771
Tim Fish <tim@timfish.uk>
2023-10-11 18:06:37 +0100

    Fix build

--- packages/deno/src/integrations/index.ts
+++ packages/deno/src/integrations/index.ts
@@ -1,4 +1,4 @@
 export { DenoContext } from './context';
 export { GlobalHandlers } from './globalhandlers';
-export { NormalizePaths } from './normalize';
+export { NormalizePaths } from './normalizepaths';
 export { ContextLines } from './contextlines';
