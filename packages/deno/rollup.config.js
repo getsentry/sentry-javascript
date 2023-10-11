bbc7ba1d69e3c98677517727e9e2b3fad1b8d1b8
Tim Fish <tim@timfish.uk>
2023-10-11 17:53:59 +0100

    Build with sucrase and tsc

--- packages/deno/rollup.config.js
+++ packages/deno/rollup.config.js
@@ -1,51 +1,24 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import sucrase from '@rollup/plugin-sucrase';
 
-export default [
-  {
-    input: ['src/index.ts'],
-    output: {
-      dir: 'build',
-      sourcemap: true,
-      preserveModules: false,
-      strict: false,
-      freeze: false,
-      interop: 'auto',
-      format: 'esm',
-      banner: '/// <reference types="./index.d.ts" />',
-    },
-    plugins: [
-      nodeResolve({
-        extensions: ['.mjs', '.js', '.json', '.node', '.ts', '.tsx'],
-      }),
-      commonjs(),
-      replace({
-        preventAssignment: true,
-        values: {
-          __DEBUG_BUILD__: true,
-          __SENTRY_DEBUG__: true,
-          __SENTRY_BROWSER_BUNDLE__: false,
-        },
-      }),
-      typescript({ tsconfig: './tsconfig.build.json' }),
-    ],
+export default {
+  input: ['src/index.ts'],
+  output: {
+    dir: 'build',
+    sourcemap: true,
+    preserveModules: false,
+    strict: false,
+    freeze: false,
+    interop: 'auto',
+    format: 'esm',
+    banner: '/// <reference types="./index.d.ts" />',
   },
-  {
-    input: './build/index.d.ts',
-    output: [{ file: 'build/index.d.ts', format: 'es' }],
-    plugins: [
-      dts({ respectExternal: true }),
-      // The bundled types contain a declaration for the __DEBUG_BUILD__ global
-      // This can result in errors about duplicate global declarations so we strip it out!
-      {
-        name: 'strip-global',
-        renderChunk(code) {
-          return { code: code.replace(/declare global \{\s*const __DEBUG_BUILD__: boolean;\s*\}/g, '') };
-        },
-      },
-    ],
-  },
-];
+  plugins: [
+    nodeResolve({
+      extensions: ['.mjs', '.js', '.json', '.node', '.ts', '.tsx'],
+    }),
+    commonjs(),
+    sucrase({ transforms: ['typescript'] }),
+  ],
+};
