de6e723ee0fadd058f9f6e4771a5f3fb25030e6b
Oleh Aloshkin <olegaleshkin@gmail.com>
2023-10-16 19:37:15 +0200

    Fixed typo

--- packages/browser-integration-tests/suites/public-api/captureMessage/parameterized_message/test.ts
+++ packages/browser-integration-tests/suites/public-api/captureMessage/parameterized_message/test.ts
@@ -4,7 +4,7 @@ import type { Event } from '@sentry/types';
 import { sentryTest } from '../../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest } from '../../../../utils/helpers';
 
-sentryTest('should capture a paramaterized representation of the message', async ({ getLocalTestPath, page }) => {
+sentryTest('should capture a parameterized representation of the message', async ({ getLocalTestPath, page }) => {
   const url = await getLocalTestPath({ testDir: __dirname });
 
   const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
