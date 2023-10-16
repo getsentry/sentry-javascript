2ca60503b52536101bb6ee22c587d5b581327c82
Onur Temizkan <onur@narval.co.uk>
2023-10-16 17:49:50 +0100

    Read DSNs from env.ts on client-side.

--- packages/e2e-tests/test-applications/remix-cloudflare-pages/app/root.tsx
+++ packages/e2e-tests/test-applications/remix-cloudflare-pages/app/root.tsx
@@ -1,32 +1,16 @@
-import { json, LinksFunction } from '@remix-run/cloudflare';
+import { LinksFunction } from '@remix-run/cloudflare';
 import { cssBundleHref } from '@remix-run/css-bundle';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
 import { withSentry } from '@sentry/remix';
-import { env } from '../env';
 
 export const links: LinksFunction = () => [...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : [])];
 
-export const loader = () => {
-  return json({
-    ENV: {
-      SENTRY_DSN: env.SENTRY_DSN,
-    },
-  });
-};
-
 function App() {
-  const { ENV } = useLoaderData();
-
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(ENV)}`,
-          }}
-        />
         <Meta />
         <Links />
       </head>
--- packages/e2e-tests/test-applications/remix-cloudflare-workers/app/entry.server.tsx
+++ packages/e2e-tests/test-applications/remix-cloudflare-workers/app/entry.server.tsx
@@ -15,7 +15,6 @@ Sentry.workerInit({
   dsn: env.SENTRY_DSN,
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  debug: true,
 });
 
 export function handleError(error: unknown, { request }: DataFunctionArgs): void {
