be83cfc9c5c3d7f3ae233d6f0da09bb741fa66cd
Billy Vong <billyvg@users.noreply.github.com>
2023-10-17 15:08:01 -0400

    meta: lint fixes

--- packages/feedback/src/constants.ts
+++ packages/feedback/src/constants.ts
@@ -23,7 +23,6 @@ export const DEFAULT_THEME = {
   },
 };
 
-
 export const ACTOR_LABEL = 'Report a Bug';
 export const CANCEL_BUTTON_LABEL = 'Cancel';
 export const SUBMIT_BUTTON_LABEL = 'Send Bug Report';
@@ -33,5 +32,5 @@ export const EMAIL_LABEL = 'Email';
 export const MESSAGE_PLACEHOLDER = "What's the bug? What did you expect?";
 export const MESSAGE_LABEL = 'Description';
 export const NAME_PLACEHOLDER = 'Your Name';
-export const NAME_LABEL= 'Name';
+export const NAME_LABEL = 'Name';
 export const SUCCESS_MESSAGE_TEXT = 'Thank you for your report!';
