cd8b208b6d4c27f5e68e889c9a34b4ea29d22501
Billy Vong <billyvg@users.noreply.github.com>
2023-10-16 22:55:35 -0400

    move feedback class to integration.ts, make isAnonymous work, hide false in createElement

--- packages/feedback/src/types/index.ts
+++ packages/feedback/src/types/index.ts
@@ -52,21 +52,22 @@ export interface FeedbackConfigurationWithDefaults {
   isAnonymous: boolean;
 
   /**
-   * Should the email field be required
+   * Should the email field be required?
    */
   isEmailRequired: boolean;
 
   /**
-   * Should the name field be required
+   * Should the name field be required?
    */
   isNameRequired: boolean;
 
   /**
-   * Should the email input field be visible?
+   * Should the email input field be visible? Note: email will still be collected if set via `Sentry.setUser()`
    */
   showEmail: boolean;
+
   /**
-   * Should the name input field be visible?
+   * Should the name input field be visible? Note: name will still be collected if set via `Sentry.setUser()`
    */
   showName: boolean;
 
