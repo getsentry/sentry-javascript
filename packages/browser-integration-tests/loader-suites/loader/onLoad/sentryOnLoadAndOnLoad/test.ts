855cb08d708a08dcbb9a111afa6d17fa03e1f2a6
Francesco Novy <francesco.novy@sentry.io>
2023-10-23 11:30:01 +0200

    test(loader): Add tests for loader `window.sentryOnLoad`

---
+++ packages/browser-integration-tests/loader-suites/loader/onLoad/sentryOnLoadAndOnLoad/test.ts
@@ -0,0 +1,15 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { envelopeRequestParser, waitForErrorRequestOnUrl } from '../../../../utils/helpers';
+
+sentryTest('sentryOnLoad callback is used', async ({ getLocalTestUrl, page }) => {
+  const url = await getLocalTestUrl({ testDir: __dirname });
+  const req = await waitForErrorRequestOnUrl(page, url);
+
+  const eventData = envelopeRequestParser(req);
+
+  expect(eventData.message).toBe('Test exception: 0.123');
+
+  expect(await page.evaluate('Sentry.getCurrentHub().getClient().getOptions().tracesSampleRate')).toEqual(0.123);
+});
