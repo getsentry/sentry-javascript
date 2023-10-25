d01a5548bd3edb608cacd23c23b25739e272f7f0
Luca Forstner <luca.forstner@sentry.io>
2023-10-25 15:59:17 +0000

    Rookie race condition mistake :')

--- packages/e2e-tests/test-applications/nextjs-app-dir/tests/pages-ssr-errors.test.ts
+++ packages/e2e-tests/test-applications/nextjs-app-dir/tests/pages-ssr-errors.test.ts
@@ -2,22 +2,22 @@ import { test, expect } from '@playwright/test';
 import { waitForError } from '../event-proxy-server';
 
 test('Will capture error for SSR rendering error (Class Component)', async ({ page }) => {
-  await page.goto('/pages-router/ssr-error-class');
-
   const errorEventPromise = waitForError('nextjs-13-app-dir', errorEvent => {
     return errorEvent?.exception?.values?.[0]?.value === 'Pages SSR Error Class';
   });
+
+  await page.goto('/pages-router/ssr-error-class');
 
   const errorEvent = await errorEventPromise;
   expect(errorEvent).toBeDefined();
 });
 
 test('Will capture error for SSR rendering error (Functional Component)', async ({ page }) => {
-  await page.goto('/pages-router/ssr-error-fc');
-
   const errorEventPromise = waitForError('nextjs-13-app-dir', errorEvent => {
     return errorEvent?.exception?.values?.[0]?.value === 'Pages SSR Error FC';
   });
+
+  await page.goto('/pages-router/ssr-error-fc');
 
   const errorEvent = await errorEventPromise;
   expect(errorEvent).toBeDefined();
