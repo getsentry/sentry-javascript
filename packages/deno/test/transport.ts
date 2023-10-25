f0b02819826e0b50ac57dcd41e3e662fea5d42de
Tim Fish <tim@timfish.uk>
2023-10-25 14:15:59 +0200

    fix(deno): Build Sentry dependencies for tests (#9312)
    Co-authored-by: Luca Forstner <luca.forstner@sentry.io>

--- packages/deno/test/transport.ts
+++ packages/deno/test/transport.ts
@@ -1,30 +1,25 @@
-import { createTransport } from 'npm:@sentry/core';
-import type {
-  BaseTransportOptions,
-  Envelope,
-  Transport,
-  TransportMakeRequestResponse,
-  TransportRequest,
-} from 'npm:@sentry/types';
-import { parseEnvelope } from 'npm:@sentry/utils';
+import type { sentryTypes } from '../build-test/index.js';
+import { sentryCore, sentryUtils } from '../build-test/index.js';
 
-export interface TestTransportOptions extends BaseTransportOptions {
-  callback: (envelope: Envelope) => void;
+export interface TestTransportOptions extends sentryTypes.BaseTransportOptions {
+  callback: (envelope: sentryTypes.Envelope) => void;
 }
 
 /**
  * Creates a Transport that uses the Fetch API to send events to Sentry.
  */
-export function makeTestTransport(callback: (envelope: Envelope) => void) {
-  return (options: BaseTransportOptions): Transport => {
-    async function doCallback(request: TransportRequest): Promise<TransportMakeRequestResponse> {
-      await callback(parseEnvelope(request.body, new TextEncoder(), new TextDecoder()));
+export function makeTestTransport(callback: (envelope: sentryTypes.Envelope) => void) {
+  return (options: sentryTypes.BaseTransportOptions): sentryTypes.Transport => {
+    async function doCallback(
+      request: sentryTypes.TransportRequest,
+    ): Promise<sentryTypes.TransportMakeRequestResponse> {
+      await callback(sentryUtils.parseEnvelope(request.body, new TextEncoder(), new TextDecoder()));
 
       return Promise.resolve({
         statusCode: 200,
       });
     }
 
-    return createTransport(options, doCallback);
+    return sentryCore.createTransport(options, doCallback);
   };
 }
