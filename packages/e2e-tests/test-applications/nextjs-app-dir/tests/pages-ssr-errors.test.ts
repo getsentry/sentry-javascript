78e61ff9a96086dc9dd8203abf213dbfbc652cc8
Luca Forstner <luca.forstner@sentry.io>
2023-10-27 13:26:09 +0200

    feat(nextjs): Trace errors in page component SSR (#9388)

--- packages/e2e-tests/test-applications/nextjs-app-dir/tests/pages-ssr-errors.test.ts
+++ packages/e2e-tests/test-applications/nextjs-app-dir/tests/pages-ssr-errors.test.ts
@@ -1,24 +1,38 @@
 import { test, expect } from '@playwright/test';
-import { waitForError } from '../event-proxy-server';
+import { waitForError, waitForTransaction } from '../event-proxy-server';
 
-test('Will capture error for SSR rendering error (Class Component)', async ({ page }) => {
+test('Will capture error for SSR rendering error with a connected trace (Class Component)', async ({ page }) => {
   const errorEventPromise = waitForError('nextjs-13-app-dir', errorEvent => {
     return errorEvent?.exception?.values?.[0]?.value === 'Pages SSR Error Class';
   });
 
+  const serverComponentTransaction = waitForTransaction('nextjs-13-app-dir', async transactionEvent => {
+    return (
+      transactionEvent?.transaction === '/pages-router/ssr-error-class' &&
+      (await errorEventPromise).contexts?.trace?.trace_id === transactionEvent.contexts?.trace?.trace_id
+    );
+  });
+
   await page.goto('/pages-router/ssr-error-class');
 
-  const errorEvent = await errorEventPromise;
-  expect(errorEvent).toBeDefined();
+  expect(await errorEventPromise).toBeDefined();
+  expect(await serverComponentTransaction).toBeDefined();
 });
 
-test('Will capture error for SSR rendering error (Functional Component)', async ({ page }) => {
+test('Will capture error for SSR rendering error with a connected trace (Functional Component)', async ({ page }) => {
   const errorEventPromise = waitForError('nextjs-13-app-dir', errorEvent => {
     return errorEvent?.exception?.values?.[0]?.value === 'Pages SSR Error FC';
   });
 
+  const serverComponentTransaction = waitForTransaction('nextjs-13-app-dir', async transactionEvent => {
+    return (
+      transactionEvent?.transaction === '/pages-router/ssr-error-fc' &&
+      (await errorEventPromise).contexts?.trace?.trace_id === transactionEvent.contexts?.trace?.trace_id
+    );
+  });
+
   await page.goto('/pages-router/ssr-error-fc');
 
-  const errorEvent = await errorEventPromise;
-  expect(errorEvent).toBeDefined();
+  expect(await errorEventPromise).toBeDefined();
+  expect(await serverComponentTransaction).toBeDefined();
 });
