3545fd50c4da248718026cf2a0560c323069cecd
Luca Forstner <luca.forstner@sentry.io>
2023-10-27 16:25:05 +0200

    fix(nextjs): Silence warning about usage of `process` in runtimes where it is not available (#9394)

--- packages/utils/src/isBrowser.ts
+++ packages/utils/src/isBrowser.ts
@@ -1,4 +1,5 @@
 import { isNodeEnv } from './node';
+import { GLOBAL_OBJ } from './worldwide';
 
 /**
  * Returns true if we are in the browser.
@@ -12,5 +13,8 @@ type ElectronProcess = { type?: string };
 
 // Electron renderers with nodeIntegration enabled are detected as Node.js so we specifically test for them
 function isElectronNodeRenderer(): boolean {
-  return typeof process !== 'undefined' && (process as ElectronProcess).type === 'renderer';
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    (GLOBAL_OBJ as any).process !== undefined && ((GLOBAL_OBJ as any).process as ElectronProcess).type === 'renderer'
+  );
 }
