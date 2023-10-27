697d334095f0b5766f58406ccb8f19e77b65051b
Luca Forstner <luca.forstner@sentry.io>
2023-10-27 07:04:20 +0000

    test(e2e): Assert that there are no funky build time warnings when building Next.js apps

--- packages/e2e-tests/test-applications/nextjs-app-dir/assert-build.ts
+++ packages/e2e-tests/test-applications/nextjs-app-dir/assert-build.ts
@@ -3,12 +3,13 @@ import * as assert from 'assert/strict';
 
 const buildOutput = fs.readFileSync('.tmp_build_output', 'utf-8');
 
-// Assert that all static components stay static and all dynamic components stay dynamic
+// Assert that there was no funky build time warning
+assert.doesNotMatch(buildOutput, /Import trace for requested module/); // This is Next.js/Webpack speech for "something is off"
 
+// Assert that all static components stay static and all dynamic components stay dynamic
 assert.match(buildOutput, /○ \/client-component/);
 assert.match(buildOutput, /● \/client-component\/parameter\/\[\.\.\.parameters\]/);
 assert.match(buildOutput, /● \/client-component\/parameter\/\[parameter\]/);
-
 assert.match(buildOutput, /λ \/server-component/);
 assert.match(buildOutput, /λ \/server-component\/parameter\/\[\.\.\.parameters\]/);
 assert.match(buildOutput, /λ \/server-component\/parameter\/\[parameter\]/);
