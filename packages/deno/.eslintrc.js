4371b2c87f16f4f044847b7ebac49fff8a3ade21
Luca Forstner <luca.forstner@sentry.io>
2023-10-30 17:30:38 +0100

    build: Clean ember and deno packages properly (#9411)

--- packages/deno/.eslintrc.js
+++ packages/deno/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
-  ignorePatterns: ['lib.deno.d.ts', 'scripts/*.mjs'],
+  ignorePatterns: ['lib.deno.d.ts', 'scripts/*.mjs', 'build-types/**', 'build-test/**', 'build/**'],
   rules: {
     '@sentry-internal/sdk/no-optional-chaining': 'off',
     '@sentry-internal/sdk/no-nullish-coalescing': 'off',
