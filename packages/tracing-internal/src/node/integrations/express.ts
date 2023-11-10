bc6faa5554b163cbf7e4e8eb9e86e98db2b8e3d2
Lubomir Igonda <lubomir.igonda@goodrequest.com>
2023-11-10 14:28:08 +0000

    fix(tracing-internal): Fix case when originalURL contain query params

--- packages/tracing-internal/src/node/integrations/express.ts
+++ packages/tracing-internal/src/node/integrations/express.ts
@@ -543,7 +543,9 @@ export function preventDuplicateSegments(
   reconstructedRoute?: string,
   layerPath?: string,
 ): string | undefined {
-  const originalUrlSplit = originalUrl?.split('/').filter(v => !!v);
+  // filter query params
+  const normalizeURL = originalUrl?.split('?')?.[0];
+  const originalUrlSplit = normalizeURL?.split('/').filter(v => !!v);
   let tempCounter = 0;
   const currentOffset = reconstructedRoute?.split('/').filter(v => !!v).length || 0;
   const result = layerPath
