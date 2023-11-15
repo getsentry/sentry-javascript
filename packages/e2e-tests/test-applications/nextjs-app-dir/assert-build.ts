f4967b62b80058b1d17e5103bc6d13698314d8c5
Luca Forstner <luca.forstner@sentry.io>
2023-11-15 11:58:53 +0000

    .

--- packages/e2e-tests/test-applications/nextjs-app-dir/assert-build.ts
+++ packages/e2e-tests/test-applications/nextjs-app-dir/assert-build.ts
@@ -1,11 +1,16 @@
 import * as fs from 'fs';
 import * as assert from 'assert/strict';
 
+const packageJson = require('./package.json');
+const nextjsVersion = packageJson.dependencies.next;
+
 const buildStdout = fs.readFileSync('.tmp_build_stdout', 'utf-8');
 const buildStderr = fs.readFileSync('.tmp_build_stderr', 'utf-8');
 
-// Assert that there was no funky build time warning
-assert.doesNotMatch(buildStderr, /Import trace for requested module/); // This is Next.js/Webpack speech for "something is off"
+// Assert that there was no funky build time warning when we are on a stable (pinned) version
+if (nextjsVersion !== 'latest' && nextjsVersion !== 'canary') {
+  assert.doesNotMatch(buildStderr, /Import trace for requested module/); // This is Next.js/Webpack speech for "something is off"
+}
 
 // Assert that all static components stay static and all dynamic components stay dynamic
 assert.match(buildStdout, /○ \/client-component/);
