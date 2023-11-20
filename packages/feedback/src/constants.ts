6d648748754022296e6f4b33cfb157d1a93ec0b6
Billy Vong <billyvg@users.noreply.github.com>
2023-11-20 16:26:21 -0500

    Update packages/feedback/src/constants.ts

--- packages/feedback/src/constants.ts
+++ packages/feedback/src/constants.ts
@@ -2,7 +2,7 @@ import { GLOBAL_OBJ } from "@sentry/utils";
 
 // exporting a separate copy of `WINDOW` rather than exporting the one from `@sentry/browser`
 // prevents the browser package from being bundled in the CDN bundle, and avoids a
-// circular dependency between the browser and replay packages
+// circular dependency between the browser and feedback packages
 export const WINDOW = GLOBAL_OBJ as typeof GLOBAL_OBJ & Window;
 
 const LIGHT_BACKGROUND = '#ffffff';
