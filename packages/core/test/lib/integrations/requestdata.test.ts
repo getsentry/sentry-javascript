8946ea732d4dc8318513428aefb61a895acc0641
Luca Forstner <luca.forstner@sentry.io>
2023-11-22 12:48:12 +0000

    Fix request data tests

--- packages/core/test/lib/integrations/requestdata.test.ts
+++ packages/core/test/lib/integrations/requestdata.test.ts
@@ -1,12 +1,10 @@
 import type { RequestDataIntegrationOptions } from '@sentry/core';
 import { getCurrentHub, Hub, makeMain, RequestData } from '@sentry/core';
-import type { Event, EventProcessor, PolymorphicRequest } from '@sentry/types';
+import type { Event, EventProcessor } from '@sentry/types';
 import * as sentryUtils from '@sentry/utils';
-import * as http from 'http';
+import type { IncomingMessage } from 'http';
 
-import { NodeClient } from '../../src/client';
-import { requestHandler } from '../../src/handlers';
-import { getDefaultNodeClientOptions } from '../helper/node-client-options';
+import { getDefaultTestClientOptions, TestClient } from '../../mocks/client';
 
 const addRequestDataToEventSpy = jest.spyOn(sentryUtils, 'addRequestDataToEvent');
 const requestDataEventProcessor = jest.fn();
@@ -26,8 +24,8 @@ function initWithRequestDataIntegrationOptions(integrationOptions: RequestDataIn
     ...integrationOptions,
   });
 
-  const client = new NodeClient(
-    getDefaultNodeClientOptions({
+  const client = new TestClient(
+    getDefaultTestClientOptions({
       dsn: 'https://dogsarebadatkeepingsecrets@squirrelchasers.ingest.sentry.io/12312012',
       integrations: [requestDataIntegration],
     }),
@@ -41,7 +39,7 @@ function initWithRequestDataIntegrationOptions(integrationOptions: RequestDataIn
 }
 
 describe('`RequestData` integration', () => {
-  let req: http.IncomingMessage, event: Event;
+  let req: IncomingMessage, event: Event;
 
   beforeEach(() => {
     req = {
@@ -50,7 +48,7 @@ describe('`RequestData` integration', () => {
       protocol,
       hostname,
       originalUrl: `${path}?${queryString}`,
-    } as unknown as http.IncomingMessage;
+    } as unknown as IncomingMessage;
     event = { sdkProcessingMetadata: { request: req } };
   });
 
@@ -99,55 +97,6 @@ describe('`RequestData` integration', () => {
 
       expect(passedOptions?.include?.user).toEqual(expect.arrayContaining(['id']));
       expect(passedOptions?.include?.user).not.toEqual(expect.arrayContaining(['email']));
-    });
-  });
-
-  describe('usage with express request handler and GCP wrapper', () => {
-    it('uses options from Express request handler', async () => {
-      const sentryRequestMiddleware = requestHandler({ include: { transaction: 'methodPath' } });
-      const res = new http.ServerResponse(req);
-      const next = jest.fn();
-
-      initWithRequestDataIntegrationOptions({ transactionNamingScheme: 'path' });
-
-      sentryRequestMiddleware(req, res, next);
-
-      await getCurrentHub().getScope()!.applyToEvent(event, {});
-      requestDataEventProcessor(event);
-
-      const passedOptions = addRequestDataToEventSpy.mock.calls[0][2];
-
-      // `transaction` matches the request middleware's option, not the integration's option
-      expect(passedOptions?.include).toEqual(expect.objectContaining({ transaction: 'methodPath' }));
-    });
-
-    it('uses options from GCP wrapper', async () => {
-      type GCPHandler = (req: PolymorphicRequest, res: http.ServerResponse) => void;
-      const mockGCPWrapper = (origHandler: GCPHandler, options: Record<string, unknown>): GCPHandler => {
-        const wrappedHandler: GCPHandler = (req, res) => {
-          getCurrentHub().getScope().setSDKProcessingMetadata({
-            request: req,
-            requestDataOptionsFromGCPWrapper: options,
-          });
-          origHandler(req, res);
-        };
-        return wrappedHandler;
-      };
-
-      const wrappedGCPFunction = mockGCPWrapper(jest.fn(), { include: { transaction: 'methodPath' } });
-      const res = new http.ServerResponse(req);
-
-      initWithRequestDataIntegrationOptions({ transactionNamingScheme: 'path' });
-
-      wrappedGCPFunction(req, res);
-
-      await getCurrentHub().getScope()!.applyToEvent(event, {});
-      requestDataEventProcessor(event);
-
-      const passedOptions = addRequestDataToEventSpy.mock.calls[0][2];
-
-      // `transaction` matches the GCP wrapper's option, not the integration's option
-      expect(passedOptions?.include).toEqual(expect.objectContaining({ transaction: 'methodPath' }));
     });
   });
 });
