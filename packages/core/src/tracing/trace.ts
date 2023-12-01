7f908d90150ba2d160291641efb80770aafbce9d
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-01 17:13:21 +0100

    remove outdated comment

--- packages/core/src/tracing/trace.ts
+++ packages/core/src/tracing/trace.ts
@@ -211,7 +211,6 @@ export function continueTrace({
   sentryTrace: Parameters<typeof tracingContextFromHeaders>[0];
   baggage: Parameters<typeof tracingContextFromHeaders>[1];
 }): Partial<TransactionContext>;
-
 export function continueTrace<V>(
   {
     sentryTrace,
@@ -227,7 +226,6 @@ export function continueTrace<V>(
  * These values can be obtained from incoming request headers,
  * or in the browser from `<meta name="sentry-trace">` and `<meta name="baggage">` HTML tags.
  *
- * It also takes an optional `request` option, which if provided will also be added to the scope & transaction metadata.
  * The callback receives a transactionContext that may be used for `startTransaction` or `startSpan`.
  */
 export function continueTrace<V>(
