b5c5eb2796e1a9c13f42467660af4d5e603596f7
Luca Forstner <luca.forstner@sentry.io>
2023-12-12 13:58:17 +0000

    why

--- packages/e2e-tests/test-applications/nextjs-14/tests/generation-functions.test.ts
+++ packages/e2e-tests/test-applications/nextjs-14/tests/generation-functions.test.ts
@@ -1,65 +1,79 @@
 import { expect, test } from '@playwright/test';
 import { waitForError, waitForTransaction } from '../event-proxy-server';
 
-test('Should send a transaction event for a generateMetadata() function invokation', async ({ page }, testInfo) => {
+test('Should send a transaction event for a generateMetadata() function invokation', async ({ page }) => {
+  const testTitle = 'foobarasdf';
+
   const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
-    return transactionEvent?.transaction === 'Page.generateMetadata (/generation-functions)';
+    return (
+      transactionEvent?.transaction === 'Page.generateMetadata (/generation-functions)' &&
+      transactionEvent.contexts?.trace?.data?.['searchParams']?.['metadataTitle'] === testTitle
+    );
   });
 
-  await page.goto(`/generation-functions`);
+  await page.goto(`/generation-functions?metadataTitle=${testTitle}`);
+
+  expect(await transactionPromise).toBeDefined();
+
+  const pageTitle = await page.title();
+  expect(pageTitle).toBe(testTitle);
+});
+
+test('Should send a transaction and an error event for a faulty generateMetadata() function invokation', async ({
+  page,
+}) => {
+  const testTitle = 'foobarbaz';
+
+  const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
+    return (
+      transactionEvent?.transaction === 'Page.generateMetadata (/generation-functions)' &&
+      transactionEvent.contexts?.trace?.data?.['searchParams']?.['metadataTitle'] === testTitle
+    );
+  });
+
+  const errorEventPromise = waitForError('nextjs-14', errorEvent => {
+    return errorEvent?.exception?.values?.[0]?.value === 'generateMetadata Error';
+  });
+
+  await page.goto(`/generation-functions?metadataTitle=${testTitle}&shouldThrowInGenerateMetadata=1`);
+
+  expect(await transactionPromise).toBeDefined();
+  expect(await errorEventPromise).toBeDefined();
+});
+
+test('Should send a transaction event for a generateViewport() function invokation', async ({ page }) => {
+  const testTitle = 'floob';
+
+  const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
+    return (
+      transactionEvent?.transaction === 'Page.generateViewport (/generation-functions)' &&
+      transactionEvent.contexts?.trace?.data?.['searchParams']?.['viewportThemeColor'] === testTitle
+    );
+  });
+
+  await page.goto(`/generation-functions?viewportThemeColor=${testTitle}`);
 
   expect(await transactionPromise).toBeDefined();
 });
 
-// test('Should send a transaction and an error event for a faulty generateMetadata() function invokation', async ({
-//   page,
-// }, testInfo) => {
-//   const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
-//     return (
-//       transactionEvent?.transaction === 'Page.generateMetadata (/generation-functions)' &&
-//       transactionEvent.contexts?.trace?.data?.['searchParams']?.['metadataTitle'] === testInfo.testId
-//     );
-//   });
+test('Should send a transaction and an error event for a faulty generateViewport() function invokation', async ({
+  page,
+}) => {
+  const testTitle = 'blargh';
 
-//   const errorEventPromise = waitForError('nextjs-14', errorEvent => {
-//     return errorEvent?.exception?.values?.[0]?.value === 'generateMetadata Error';
-//   });
+  const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
+    return (
+      transactionEvent?.transaction === 'Page.generateViewport (/generation-functions)' &&
+      transactionEvent.contexts?.trace?.data?.['searchParams']?.['viewportThemeColor'] === testTitle
+    );
+  });
 
-//   await page.goto(`/generation-functions?metadataTitle=${testInfo.testId}&shouldThrowInGenerateMetadata=1`);
+  const errorEventPromise = waitForError('nextjs-14', errorEvent => {
+    return errorEvent?.exception?.values?.[0]?.value === 'generateViewport Error';
+  });
 
-//   expect(await transactionPromise).toBeDefined();
-//   expect(await errorEventPromise).toBeDefined();
-// });
+  await page.goto(`/generation-functions?viewportThemeColor=${testTitle}&shouldThrowInGenerateViewport=1`);
 
-// test('Should send a transaction event for a generateViewport() function invokation', async ({ page }, testInfo) => {
-//   const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
-//     return (
-//       transactionEvent?.transaction === 'Page.generateViewport (/generation-functions)' &&
-//       transactionEvent.contexts?.trace?.data?.['searchParams']?.['viewportThemeColor'] === testInfo.testId
-//     );
-//   });
-
-//   await page.goto(`/generation-functions?viewportThemeColor=${testInfo.testId}`);
-
-//   expect(await transactionPromise).toBeDefined();
-// });
-
-// test('Should send a transaction and an error event for a faulty generateViewport() function invokation', async ({
-//   page,
-// }, testInfo) => {
-//   const transactionPromise = waitForTransaction('nextjs-14', async transactionEvent => {
-//     return (
-//       transactionEvent?.transaction === 'Page.generateViewport (/generation-functions)' &&
-//       transactionEvent.contexts?.trace?.data?.['searchParams']?.['viewportThemeColor'] === testInfo.testId
-//     );
-//   });
-
-//   const errorEventPromise = waitForError('nextjs-14', errorEvent => {
-//     return errorEvent?.exception?.values?.[0]?.value === 'generateViewport Error';
-//   });
-
-//   await page.goto(`/generation-functions?viewportThemeColor=${testInfo.testId}&shouldThrowInGenerateViewport=1`);
-
-//   expect(await transactionPromise).toBeDefined();
-//   expect(await errorEventPromise).toBeDefined();
-// });
+  expect(await transactionPromise).toBeDefined();
+  expect(await errorEventPromise).toBeDefined();
+});
