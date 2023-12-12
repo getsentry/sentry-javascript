7ba1bf82752d0af2c41e3845460b907940874b13
Winston Hoy <winston@commonenergy.us>
2023-12-12 13:24:19 -0500

    feat(nextjs): Export common shims for server and edge nextjs modules
    Export common shims (BrowserTracing, Feedback, Replay) to improve DX by:
    - logging error when server/edge SDK inits invoke client only modules
    - avoiding compilation warnings for uninvoked client only configuration
    
    Fixes GH-9796

--- packages/nextjs/src/server/index.ts
+++ packages/nextjs/src/server/index.ts
@@ -6,6 +6,8 @@ import { Integrations, configureScope, getCurrentHub, init as nodeInit } from '@
 import type { EventProcessor } from '@sentry/types';
 import type { IntegrationWithExclusionOption } from '@sentry/utils';
 import { addOrUpdateIntegration, escapeStringForRegex, logger } from '@sentry/utils';
+// This is exported to warn when server/edge SDK inits invoke client only modules and to avoid compilation warnings for uninvoked client only configuration
+export { BrowserTracing, Feedback, Replay } from '@sentry-internal/integration-shims';
 
 import { DEBUG_BUILD } from '../common/debug-build';
 import { devErrorSymbolicationEventProcessor } from '../common/devErrorSymbolicationEventProcessor';
