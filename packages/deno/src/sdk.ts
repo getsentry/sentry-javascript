ba118652bb3d9df40a4601cc39e905a6954040a7
Tim Fish <tim@timfish.uk>
2023-12-13 10:10:41 +0100

    Fix test snapshots

--- packages/deno/src/sdk.ts
+++ packages/deno/src/sdk.ts
@@ -5,7 +5,7 @@ import type { StackParser } from '@sentry/types';
 import { createStackParser, nodeStackLineParser, stackParserFromStackParserOptions } from '@sentry/utils';
 
 import { DenoClient } from './client';
-import { ContextLines, DenoContext, DenoCron, GlobalHandlers, NormalizePaths } from './integrations';
+import { ContextLines, DenoContext, GlobalHandlers, NormalizePaths } from './integrations';
 import { makeFetchTransport } from './transports';
 import type { DenoOptions } from './types';
 
