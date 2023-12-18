8d9a653a64a63b9c8f391886d7cd14ceafc9a9ac
Francesco Novy <francesco.novy@sentry.io>
2023-12-18 14:38:50 +0100

    feat(core): Deprecate `pushScope` & `popScope`

--- packages/opentelemetry/test/custom/hub.test.ts
+++ packages/opentelemetry/test/custom/hub.test.ts
@@ -26,6 +26,7 @@ describe('OpenTelemetryHub', () => {
   it('pushScope() creates correct scope', () => {
     const hub = new OpenTelemetryHub();
 
+    // eslint-disable-next-line deprecation/deprecation
     const scope = hub.pushScope();
     expect(scope).toBeInstanceOf(OpenTelemetryScope);
 
