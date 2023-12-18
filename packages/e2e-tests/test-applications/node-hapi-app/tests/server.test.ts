f92241405000e2deffb2d240d71412adb40913a6
Francesco Novy <francesco.novy@sentry.io>
2023-12-18 12:14:52 +0100

    build: Fix linting (#9888)
    Damn, merged a PR without rebasing it first, it did not have the new
    biome rules yet 😅 so develop is failing linting right now...

--- packages/e2e-tests/test-applications/node-hapi-app/tests/server.test.ts
+++ packages/e2e-tests/test-applications/node-hapi-app/tests/server.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { waitForError, waitForTransaction } from '../event-proxy-server';
 
