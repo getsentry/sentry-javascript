37f4466e584930e4be2f4ae4be6e1a7548465bdf
Tim Fish <tim@timfish.uk>
2023-12-19 18:48:37 +0100

    Fix broken merge

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -79,7 +79,7 @@ export { defaultIntegrations, init, defaultStackParser, getSentryRelease } from 
 export { addRequestDataToEvent, DEFAULT_USER_INCLUDES, extractRequestData } from '@sentry/utils';
 export { deepReadDirSync } from './utils';
 export { getModuleFromFilename } from './module';
-export { enableAnrDetection } from './anr';
+export { enableAnrDetection, isAnrChildProcess } from './anr';
 export { cron } from './cron';
 
 import { Integrations as CoreIntegrations } from '@sentry/core';
