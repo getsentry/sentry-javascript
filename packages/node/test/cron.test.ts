42abf2925f2cd4cad0ce2c344bbd2d1c42f62c43
Tim Fish <tim@timfish.uk>
2023-12-19 17:09:48 +0100

    add another test

--- packages/node/test/cron.test.ts
+++ packages/node/test/cron.test.ts
@@ -14,28 +14,46 @@ describe('cron', () => {
     jest.restoreAllMocks();
   });
 
-  test('node-cron', done => {
-    const nodeCron: NodeCron = {
-      schedule: (expression: string, callback: () => void, options?: NodeCronOptions): unknown => {
-        expect(expression).toBe('* * * Jan,Sep Sun');
-        expect(callback).toBeInstanceOf(Function);
-        expect(options?.name).toBe('my-cron-job');
-        return callback();
-      },
-    };
+  describe('node-cron', () => {
+    test('calls withMonitor', done => {
+      const nodeCron: NodeCron = {
+        schedule: (expression: string, callback: () => void, options?: NodeCronOptions): unknown => {
+          expect(expression).toBe('* * * Jan,Sep Sun');
+          expect(callback).toBeInstanceOf(Function);
+          expect(options?.name).toBe('my-cron-job');
+          return callback();
+        },
+      };
 
-    const cronWithCheckIn = instrumentNodeCron(nodeCron);
+      const cronWithCheckIn = instrumentNodeCron(nodeCron);
 
-    cronWithCheckIn.schedule(
-      '* * * Jan,Sep Sun',
-      () => {
-        expect(withMonitorSpy).toHaveBeenCalledTimes(1);
-        expect(withMonitorSpy).toHaveBeenLastCalledWith('my-cron-job', expect.anything(), {
-          schedule: { type: 'crontab', value: '* * * 1,9 0' },
+      cronWithCheckIn.schedule(
+        '* * * Jan,Sep Sun',
+        () => {
+          expect(withMonitorSpy).toHaveBeenCalledTimes(1);
+          expect(withMonitorSpy).toHaveBeenLastCalledWith('my-cron-job', expect.anything(), {
+            schedule: { type: 'crontab', value: '* * * 1,9 0' },
+          });
+          done();
+        },
+        { name: 'my-cron-job' },
+      );
+    });
+
+    test('throws without supplied name', () => {
+      const nodeCron: NodeCron = {
+        schedule: (): unknown => {
+          return undefined;
+        },
+      };
+
+      const cronWithCheckIn = instrumentNodeCron(nodeCron);
+
+      expect(() => {
+        cronWithCheckIn.schedule('* * * * *', () => {
+          //
         });
-        done();
-      },
-      { name: 'my-cron-job' },
-    );
+      }).toThrowError('Missing "name" for scheduled job. A name is required for Sentry check-in monitoring.');
+    });
   });
 });
