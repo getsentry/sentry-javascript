cbec9221693b9110558f132dabdaa15d561f4571
Tim Fish <tim@timfish.uk>
2023-12-20 15:08:55 +0000

    Write worker build output to `build/*`

--- packages/node/rollup.anr-worker.config.js
+++ packages/node/rollup.anr-worker.config.js
@@ -1,23 +1,35 @@
 import { makeBaseBundleConfig } from '../../rollup/index.js';
 
-export default makeBaseBundleConfig({
-  bundleType: 'node-worker',
-  entrypoints: ['src/integrations/anr/worker.ts'],
-  jsVersion: 'es6',
-  licenseTitle: '@sentry/node',
-  outputFileBase: () => 'worker-script.ts',
-  packageSpecificConfig: {
-    output: {
-      dir: './src/integrations/anr',
-      sourcemap: false,
-    },
-    plugins: [
-      {
-        name: 'output-base64-worker-script',
-        renderChunk(code) {
-          return `export const base64WorkerScript = '${Buffer.from(code).toString('base64')}';`;
-        },
+function createAnrWorkerConfig(destDir, esm) {
+  return makeBaseBundleConfig({
+    bundleType: 'node-worker',
+    entrypoints: ['src/integrations/anr/worker.ts'],
+    jsVersion: 'es6',
+    licenseTitle: '@sentry/node',
+    outputFileBase: () => 'worker-script.js',
+    packageSpecificConfig: {
+      output: {
+        dir: destDir,
+        sourcemap: false,
       },
-    ],
-  },
-});
+      plugins: [
+        {
+          name: 'output-base64-worker-script',
+          renderChunk(code) {
+            const base64Code = Buffer.from(code).toString('base64');
+            if (esm) {
+              return `export const base64WorkerScript = '${base64Code}';`;
+            } else {
+              return `exports.base64WorkerScript = '${base64Code}';`;
+            }
+          },
+        },
+      ],
+    },
+  });
+}
+
+export const anrWorkerConfigs = [
+  createAnrWorkerConfig('build/esm/integrations/anr', true),
+  createAnrWorkerConfig('build/cjs/integrations/anr', false),
+];
--- packages/node/rollup.npm.config.js
+++ packages/node/rollup.npm.config.js
@@ -1,3 +1,8 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
+import { anrWorkerConfigs } from './rollup.anr-worker.config.js';
 
-export default makeNPMConfigVariants(makeBaseNPMConfig());
+export default [
+  ...makeNPMConfigVariants(makeBaseNPMConfig()),
+  // The ANR worker builds must come after the main build because they overwrite the worker-script.js file
+  ...anrWorkerConfigs,
+];
