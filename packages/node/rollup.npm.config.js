90efe247b4d9caff470ee3ae68aebf8310e54fa3
Tim Fish <tim@timfish.uk>
2023-12-20 17:53:04 +0000

    A couple of merge mistakes

--- packages/node/rollup.npm.config.js
+++
@@ -1,8 +0,0 @@
-import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
-import { anrWorkerConfigs } from './rollup.anr-worker.config.mjs';
-
-export default [
-  ...makeNPMConfigVariants(makeBaseNPMConfig()),
-  // The ANR worker builds must come after the main build because they overwrite the worker-script.js file
-  ...anrWorkerConfigs,
-];
