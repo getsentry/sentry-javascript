51830a5c2f87df43d39cf947a4f63c6980f67aeb
Abhijeet Prasad <aprasad@sentry.io>
2023-12-20 13:31:17 -0500

    fix ts3.8 test

--- packages/e2e-tests/test-applications/generic-ts3.8/index.ts
+++ packages/e2e-tests/test-applications/generic-ts3.8/index.ts
@@ -1,2 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// we need to import the SDK to ensure tsc check the types
+// biome-ignore lint/nursery/noUnusedImports: we need to import the SDK to ensure tsc check the types
+import * as _SentryBrowser from '@sentry/browser';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryCore from '@sentry/core';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryHub from '@sentry/hub';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryIntegrations from '@sentry/integrations';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryNode from '@sentry/node';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryOpentelemetry from '@sentry/opentelemetry-node';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryReplay from '@sentry/replay';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryTracing from '@sentry/tracing';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryTypes from '@sentry/types';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryUtils from '@sentry/utils';
+// biome-ignore lint/nursery/noUnusedImports:
+import * as _SentryWasm from '@sentry/wasm';
