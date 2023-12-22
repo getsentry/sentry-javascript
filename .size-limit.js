d18a8df104e5a912705dde5519c63a7f58f58c80
Francesco Novy <francesco.novy@sentry.io>
2023-12-22 09:06:07 +0100

    build: Fix size limit

--- .size-limit.js
+++ .size-limit.js
@@ -60,7 +60,7 @@ module.exports = [
     name: '@sentry/browser (incl. Tracing, Replay, Feedback) - ES6 CDN Bundle (gzipped)',
     path: 'packages/browser/build/bundles/bundle.tracing.replay.feedback.min.js',
     gzip: true,
-    limit: '75 KB',
+    limit: '90 KB',
   },
   {
     name: '@sentry/browser (incl. Tracing, Replay) - ES6 CDN Bundle (gzipped)',
