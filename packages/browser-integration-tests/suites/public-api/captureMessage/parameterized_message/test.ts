8c8bf1017a21fa24e482cbc18a339ac088b29f3c
Oleh Aloshkin <olegaleshkin@gmail.com>
2023-12-31 12:46:26 +0100

    Skipping test for some bundles

--- packages/browser-integration-tests/suites/public-api/captureMessage/parameterized_message/test.ts
+++ packages/browser-integration-tests/suites/public-api/captureMessage/parameterized_message/test.ts
@@ -2,9 +2,13 @@ import { expect } from '@playwright/test';
 import type { Event } from '@sentry/types';
 
 import { sentryTest } from '../../../../utils/fixtures';
-import { getFirstSentryEnvelopeRequest } from '../../../../utils/helpers';
+import { getFirstSentryEnvelopeRequest, shouldSkipTracingTest } from '../../../../utils/helpers';
 
 sentryTest('should capture a parameterized representation of the message', async ({ getLocalTestPath, page }) => {
+  if (shouldSkipTracingTest()) {
+    sentryTest.skip();
+  }
+
   const url = await getLocalTestPath({ testDir: __dirname });
 
   const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
