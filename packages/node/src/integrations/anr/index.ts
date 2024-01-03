011ba71efb03d780c8a718f8995e1c5439ca1837
Tim Fish <tim@timfish.uk>
2024-01-03 16:33:07 +0100

    fix(node): ANR fixes and additions  (#9998)
    This PR makes a few fixes and additions to the ANR feature.
    - Stops passing the `sdk` property with the event since
    `enhanceEventWithSdkInfo` results in duplicate integrations/packages
    being sent!
    - Allows the passings of `staticTags` to attach to ANR events 
    - Required by Electron SDK to tag the source
    `process/origin/environment`
      - Could also be useful for other users
    - Optionally enable normalising of stack frame paths to the app root
      - Required for Electron
      - Soon required for Node with #9072
    
    The path normalisation code (and tests) are from the Electron SDK and is
    well tested on all platforms. However, it will only be called when
    `appRootPath` is supplied. If/when we add path normalisation to Node, it
    will have a default which can be overridden.
    
    The Electron SDK will then wrap the Node Anr integration something like
    this:
    ```ts
    class Anr extends NodeAnr {
      public constructor(options: Partial<Options> = {}) {
        super({
          ...options,
          staticTags: {
            'event.environment': 'javascript',
            'event.origin': 'electron',
            'event.process': 'browser',
            ...options.tags,        
          },
          appRootPath: app.getAppPath(),
        });
      }
    }
    ```

--- packages/node/src/integrations/anr/index.ts
+++ packages/node/src/integrations/anr/index.ts
@@ -101,9 +101,11 @@ async function _startWorker(client: NodeClient, _options: Partial<Options>): Pro
     release: initOptions.release,
     dist: initOptions.dist,
     sdkMetadata,
+    appRootPath: _options.appRootPath,
     pollInterval: _options.pollInterval || DEFAULT_INTERVAL,
     anrThreshold: _options.anrThreshold || DEFAULT_HANG_THRESHOLD,
     captureStackTrace: !!_options.captureStackTrace,
+    staticTags: _options.staticTags || {},
     contexts,
   };
 
