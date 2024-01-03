8fdb9035081d742e04e74899f9a992f2fcde9b0c
Tim Fish <tim@timfish.uk>
2024-01-03 16:52:11 +0100

    disable lint

--- packages/node/src/cron/common.ts
+++ packages/node/src/cron/common.ts
@@ -44,6 +44,7 @@ const replacements: [string, string][] = [
  */
 export function replaceCronNames(cronExpression: string): string {
   return replacements.reduce(
+    // eslint-disable-next-line @sentry-internal/sdk/no-regexp-constructor
     (acc, [name, replacement]) => acc.replace(new RegExp(name, 'gi'), replacement),
     cronExpression,
   );
