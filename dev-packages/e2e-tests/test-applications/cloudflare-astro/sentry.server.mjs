613582b144a8b6043af15dd569c65db8723be2f3
Yagiz Nizipli <yagiz@nizipli.com>
2024-01-09 16:57:34 -0500

    fixup

--- dev-packages/e2e-tests/test-applications/cloudflare-astro/sentry.server.mjs
+++ dev-packages/e2e-tests/test-applications/cloudflare-astro/sentry.server.mjs
@@ -1,7 +1,5 @@
 import * as Sentry from '@sentry/astro';
-import { ProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
   dsn: process.env.E2E_TEST_DSN,
-  integrations: [new ProfilingIntegration()],
 });
