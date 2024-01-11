6d1a44741d1562caf3803f60b91ed82001f9f8e9
Tim Fish <tim@timfish.uk>
2024-01-11 21:17:34 +0100

    ignore sessions

--- dev-packages/node-integration-tests/suites/proxy/test.ts
+++ dev-packages/node-integration-tests/suites/proxy/test.ts
@@ -3,6 +3,7 @@ import { createRunner } from '../../utils/runner';
 test('proxies sentry requests', done => {
   createRunner(__dirname, 'basic.js')
     .withMockSentryServer()
+    .ignore('session')
     .expect({
       event: {
         message: 'Hello, via proxy!',
