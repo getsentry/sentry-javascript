fed7da29c1f932dc39c31b9e204bbd0758548ed3
Billy Vong <billyvg@users.noreply.github.com>
2024-01-11 12:24:32 -0500

    lint

--- packages/replay-canvas/src/canvas.ts
+++ packages/replay-canvas/src/canvas.ts
@@ -1,3 +1,4 @@
+import { CanvasManager } from '@sentry-internal/rrweb';
 import { convertIntegrationFnToClass } from '@sentry/core';
 import type { CanvasManagerInterface, CanvasManagerOptions } from '@sentry/replay';
 import type { Integration, IntegrationClass, IntegrationFn } from '@sentry/types';
