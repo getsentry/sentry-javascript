f79d000daff556bb0ab7fa123743dcf856efa43e
JonasBa <jonas@badalic.com>
2024-01-16 10:08:24 -0500

    rollback to v3 upload artifacts

--- packages/profiling-node/jest.config.js
+++ packages/profiling-node/jest.config.js
@@ -1,5 +1,7 @@
 const baseConfig = require('../../jest/jest.config.js');
 
+console.log('baseConfig', baseConfig);
 module.exports = {
-  ...baseConfig
+  ...baseConfig,
+  testEnvironment: 'node',
 };
