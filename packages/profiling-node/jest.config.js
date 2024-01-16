3500f7bd92cf31bd5a6a765d95fc6a896c8de159
JonasBa <jonas@badalic.com>
2024-01-16 10:01:13 -0500

    test build

--- packages/profiling-node/jest.config.js
+++ packages/profiling-node/jest.config.js
@@ -1,20 +1,5 @@
+const baseConfig = require('../../jest/jest.config.js');
+
 module.exports = {
-  verbose: true,
-  forceExit: true,
-  collectCoverage: true,
-  testMatch: ['**/*.test.ts'],
-  testPathIgnorePatterns: ['benchmarks/'],
-  resetMocks: true,
-  restoreMocks: true,
-  silent: false,
-  transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.test.json',
-      },
-    ],
-  },
+  ...baseConfig
 };
