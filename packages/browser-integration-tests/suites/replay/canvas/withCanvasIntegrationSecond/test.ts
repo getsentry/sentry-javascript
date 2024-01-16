8db7bde0eebc573e1d801d7f2d0beaf675623f21
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-01-15 19:03:27 -0500

    fix again

--- packages/browser-integration-tests/suites/replay/canvas/withCanvasIntegrationSecond/test.ts
+++ packages/browser-integration-tests/suites/replay/canvas/withCanvasIntegrationSecond/test.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
 
 import { sentryTest } from '../../../../utils/fixtures';
-import { getReplaySnapshot, shouldSkipReplayTest } from '../../../../utils/replayHelpers';
+import { getReplaySnapshot, shouldSkipReplayTest, waitForReplayRequest } from '../../../../utils/replayHelpers';
 
 sentryTest('sets up canvas when adding ReplayCanvas integration after Replay', async ({ getLocalTestUrl, page }) => {
-  if (shouldSkipReplayTest()) {
+  if (shouldSkipReplayTest() || (process.env.PW_BUNDLE || '').startsWith('bundle')) {
     sentryTest.skip();
   }
+
+  const reqPromise0 = waitForReplayRequest(page, 0);
 
   await page.route('https://dsn.ingest.sentry.io/**/*', route => {
     return route.fulfill({
@@ -19,10 +21,11 @@ sentryTest('sets up canvas when adding ReplayCanvas integration after Replay', a
   const url = await getLocalTestUrl({ testDir: __dirname });
 
   await page.goto(url);
+  await reqPromise0;
 
   const replay = await getReplaySnapshot(page);
-  const canvasOptions = replay._options._experiments?.canvas;
-  expect(canvasOptions.fps).toBe(4);
-  expect(canvasOptions.quality).toBe(0.6);
+  const canvasOptions = replay._canvas;
+  expect(canvasOptions?.sampling.canvas).toBe(2);
+  expect(canvasOptions?.dataURLOptions.quality).toBe(0.4);
   expect(replay._hasCanvas).toBe(true);
 });
