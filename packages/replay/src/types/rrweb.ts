bdb58a480d1736a630ebc55380a30d6e80bcabfb
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-01-16 14:25:04 -0500

    fix merge

--- packages/replay/src/types/rrweb.ts
+++ packages/replay/src/types/rrweb.ts
@@ -56,7 +56,7 @@ export interface CanvasManagerInterface {
 
 export interface CanvasManagerOptions {
   recordCanvas: boolean;
-  isManualSnapshot?: boolean;
+  enableManualSnapshot?: boolean;
   blockClass: string | RegExp;
   blockSelector: string | null;
   unblockSelector: string | null;
