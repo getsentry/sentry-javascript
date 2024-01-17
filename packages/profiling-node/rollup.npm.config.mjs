f0d821d9468e2cf9df412879b1bc90624e570e86
JonasBa <jonas@badalic.com>
2024-01-17 07:15:12 -0500

    test

--- packages/profiling-node/rollup.npm.config.mjs
+++ packages/profiling-node/rollup.npm.config.mjs
@@ -1,3 +1,6 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollup-utils';
 
 const configs = makeNPMConfigVariants(makeBaseNPMConfig());
@@ -7,4 +10,12 @@ if (!cjsConfig) {
   throw new Error('CJS config is required for profiling-node.');
 }
 
-export default {...cjsConfig, target: 'node'};
+const config = {
+  ...cjsConfig,
+  input: 'src/index.ts',
+  output: { ...cjsConfig.output, dir: 'lib', format: 'cjs', preserveModules: false },
+  plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+};
+
+
+export default config;
