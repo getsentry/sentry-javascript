c40425f91f53b098360a28a4143c24895eaba19e
JonasBa <jonas@badalic.com>
2024-01-17 09:02:38 -0500

    test

--- packages/profiling-node/rollup.npm.config.mjs
+++ packages/profiling-node/rollup.npm.config.mjs
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollup-utils';
+import { makeBaseNPMConfig, makeNPMConfigVariants, plugins } from '@sentry-internal/rollup-utils';
 
 const configs = makeNPMConfigVariants(makeBaseNPMConfig());
 const cjsConfig = configs.find(config => config.output.format === 'cjs');
@@ -14,7 +14,7 @@ const config = {
   ...cjsConfig,
   input: 'src/index.ts',
   output: { ...cjsConfig.output, file: 'lib/index.js', format: 'cjs', dir: undefined, preserveModules: false },
-  plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+  plugins: [plugins.makeLicensePlugin('Sentry Node Profiling'), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
 };
 
 export default config;
