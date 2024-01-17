da2bfd2531fec7054af495743946d9ef299dad5b
JonasBa <jonas@badalic.com>
2024-01-16 19:43:16 -0500

    fix import

--- packages/profiling-node/rollup.npm.config.mjs
+++ packages/profiling-node/rollup.npm.config.mjs
@@ -3,11 +3,10 @@ import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollu
 const configs = makeNPMConfigVariants(makeBaseNPMConfig());
 const cjsConfig = configs.find(config => config.format === 'cjs');
 
-if(!cjsConfig) {
+if (!cjsConfig) {
   throw new Error('CJS config is required for profiling-node.');
 }
 
 export default {
   ...cjsConfig,
-}
-
+};
