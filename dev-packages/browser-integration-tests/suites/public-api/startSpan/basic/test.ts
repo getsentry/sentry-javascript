1f8f34cdcd230aa49827ad234c80fa3663e1c63a
Lukas Stracke <lukas.stracke@sentry.io>
2024-01-18 14:52:26 +0100

    more Event -> SerializedEvent

--- dev-packages/browser-integration-tests/suites/public-api/startSpan/basic/test.ts
+++ dev-packages/browser-integration-tests/suites/public-api/startSpan/basic/test.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import type { Event } from '@sentry/types';
+import type { SerializedEvent } from '@sentry/types';
 
 import { sentryTest } from '../../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest, shouldSkipTracingTest } from '../../../../utils/helpers';
@@ -10,7 +10,7 @@ sentryTest('should send a transaction in an envelope', async ({ getLocalTestPath
   }
 
   const url = await getLocalTestPath({ testDir: __dirname });
-  const transaction = await getFirstSentryEnvelopeRequest<Event>(page, url);
+  const transaction = await getFirstSentryEnvelopeRequest<SerializedEvent>(page, url);
 
   expect(transaction.transaction).toBe('parent_span');
   expect(transaction.spans).toBeDefined();
@@ -22,15 +22,13 @@ sentryTest('should report finished spans as children of the root transaction', a
   }
 
   const url = await getLocalTestPath({ testDir: __dirname });
-  const transaction = await getFirstSentryEnvelopeRequest<Event>(page, url);
+  const transaction = await getFirstSentryEnvelopeRequest<SerializedEvent>(page, url);
 
   const rootSpanId = transaction?.contexts?.trace?.span_id;
 
   expect(transaction.spans).toHaveLength(1);
 
   const span_1 = transaction.spans?.[0];
-  // eslint-disable-next-line deprecation/deprecation
   expect(span_1?.description).toBe('child_span');
-  // @ts-expect-error this property is not defined on Event. But in fact, `parent_span_id` is correct here
   expect(span_1?.parent_span_id).toEqual(rootSpanId);
 });
--- dev-packages/browser-integration-tests/suites/public-api/startTransaction/basic_usage/test.ts
+++ dev-packages/browser-integration-tests/suites/public-api/startTransaction/basic_usage/test.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import type { Event } from '@sentry/types';
+import type { SerializedEvent } from '@sentry/types';
 
 import { sentryTest } from '../../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest, shouldSkipTracingTest } from '../../../../utils/helpers';
@@ -10,7 +10,7 @@ sentryTest('should report a transaction in an envelope', async ({ getLocalTestPa
   }
 
   const url = await getLocalTestPath({ testDir: __dirname });
-  const transaction = await getFirstSentryEnvelopeRequest<Event>(page, url);
+  const transaction = await getFirstSentryEnvelopeRequest<SerializedEvent>(page, url);
 
   expect(transaction.transaction).toBe('test_transaction_1');
   expect(transaction.spans).toBeDefined();
@@ -22,7 +22,7 @@ sentryTest('should report finished spans as children of the root transaction', a
   }
 
   const url = await getLocalTestPath({ testDir: __dirname });
-  const transaction = await getFirstSentryEnvelopeRequest<Event>(page, url);
+  const transaction = await getFirstSentryEnvelopeRequest<SerializedEvent>(page, url);
 
   const rootSpanId = transaction?.contexts?.trace?.span_id;
 
@@ -30,22 +30,15 @@ sentryTest('should report finished spans as children of the root transaction', a
 
   const span_1 = transaction.spans?.[0];
 
-  // eslint-disable-next-line deprecation/deprecation
   expect(span_1?.op).toBe('span_1');
-  // @ts-expect-error this property is not defined on the incorrectly used Event
   expect(span_1?.parent_span_id).toEqual(rootSpanId);
-  // eslint-disable-next-line deprecation/deprecation
   expect(span_1?.data).toMatchObject({ foo: 'bar', baz: [1, 2, 3] });
 
   const span_3 = transaction.spans?.[1];
-  // eslint-disable-next-line deprecation/deprecation
   expect(span_3?.op).toBe('span_3');
-  // @ts-expect-error this property is not defined on the incorrectly used Event
   expect(span_3?.parent_span_id).toEqual(rootSpanId);
 
   const span_5 = transaction.spans?.[2];
-  // eslint-disable-next-line deprecation/deprecation
   expect(span_5?.op).toBe('span_5');
-  // @ts-expect-error this property is not defined on the incorrectly used Event
   expect(span_5?.parent_span_id).toEqual(span_3?.span_id);
 });
