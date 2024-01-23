6a207d6d1e91beb3df0a7b86d33245ff4d7c07dd
JonasBa <jonas@badalic.com>
2024-01-23 09:44:58 -0500

    remove sentry hub

--- packages/profiling-node/scripts/binaries.js
+++ packages/profiling-node/scripts/binaries.js
@@ -10,15 +10,11 @@ function getModuleName() {
   const arch = process.env['BUILD_ARCH'] || os.arch();
 
   if (platform === 'darwin' && arch === 'arm64') {
-    const identifier = [platform, 'arm64', abi.getAbi(process.versions.node, 'node')]
-      .filter(Boolean)
-      .join('-');
+    const identifier = [platform, 'arm64', abi.getAbi(process.versions.node, 'node')].filter(Boolean).join('-');
     return `sentry_cpu_profiler-${identifier}.node`;
   }
 
-  const identifier = [platform, arch, stdlib, abi.getAbi(process.versions.node, 'node')]
-    .filter(Boolean)
-    .join('-');
+  const identifier = [platform, arch, stdlib, abi.getAbi(process.versions.node, 'node')].filter(Boolean).join('-');
 
   return `sentry_cpu_profiler-${identifier}.node`;
 }
