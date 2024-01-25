f025eead144c810fa1f7f2a549e1cac849e5819e
Lukas Stracke <lukas.stracke@sentry.io>
2024-01-25 11:34:37 +0100

    same thing for OnUncaughtException integration
    this didn't cause an error

--- packages/nextjs/src/server/onUncaughtExceptionIntegration.ts
+++ packages/nextjs/src/server/onUncaughtExceptionIntegration.ts
@@ -1,11 +1,10 @@
 import { Integrations } from '@sentry/node';
-const { OnUncaughtException: OriginalOnUncaughtException } = Integrations;
 
 /**
  * A custom OnUncaughtException integration that does not exit by default.
  */
-export class OnUncaughtException extends OriginalOnUncaughtException {
-  public constructor(options?: ConstructorParameters<typeof OriginalOnUncaughtException>[0]) {
+export class OnUncaughtException extends Integrations.OnUncaughtException {
+  public constructor(options?: ConstructorParameters<typeof Integrations.OnUncaughtException>[0]) {
     super({
       exitEvenIfOtherHandlersAreRegistered: false,
       ...options,
