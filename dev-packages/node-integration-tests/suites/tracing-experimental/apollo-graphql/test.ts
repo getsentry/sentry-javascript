dad667437f2151552e3216decfe9626bc6241fd3
Onur Temizkan <onur@narval.co.uk>
2024-01-25 19:35:33 +0000

    test(node): Add mutation case to GraphQL OTEL integration tests.

--- dev-packages/node-integration-tests/suites/tracing-experimental/apollo-graphql/test.ts
+++ dev-packages/node-integration-tests/suites/tracing-experimental/apollo-graphql/test.ts
@@ -2,37 +2,61 @@ import { conditionalTest } from '../../../utils';
 import { createRunner } from '../../../utils/runner';
 
 conditionalTest({ min: 14 })('GraphQL/Apollo Tests', () => {
-  const EXPECTED_TRANSACTION = {
-    transaction: 'Test Transaction',
-    spans: expect.arrayContaining([
-      expect.objectContaining({
-        data: {
-          'graphql.operation.type': 'query',
-          'graphql.source': '{hello}',
-          'otel.kind': 'INTERNAL',
-          'sentry.origin': 'auto.graphql.otel.graphql',
-        },
-        description: 'query',
-        status: 'ok',
-        origin: 'auto.graphql.otel.graphql',
-      }),
-      expect.objectContaining({
-        data: {
-          'graphql.field.name': 'hello',
-          'graphql.field.path': 'hello',
-          'graphql.field.type': 'String',
-          'graphql.source': 'hello',
-          'otel.kind': 'INTERNAL',
-          'sentry.origin': 'manual',
-        },
-        description: 'graphql.resolve',
-        status: 'ok',
-        origin: 'manual',
-      }),
-    ]),
-  };
-
   test('CJS - should instrument GraphQL queries used from Apollo Server.', done => {
-    createRunner(__dirname, 'scenario.js').expect({ transaction: EXPECTED_TRANSACTION }).start(done);
+    const EXPECTED_TRANSACTION = {
+      transaction: 'Test Transaction',
+      spans: expect.arrayContaining([
+        expect.objectContaining({
+          data: {
+            'graphql.operation.type': 'query',
+            'graphql.source': '{hello}',
+            'otel.kind': 'INTERNAL',
+            'sentry.origin': 'auto.graphql.otel.graphql',
+          },
+          description: 'query',
+          status: 'ok',
+          origin: 'auto.graphql.otel.graphql',
+        }),
+        expect.objectContaining({
+          data: {
+            'graphql.field.name': 'hello',
+            'graphql.field.path': 'hello',
+            'graphql.field.type': 'String',
+            'graphql.source': 'hello',
+            'otel.kind': 'INTERNAL',
+            'sentry.origin': 'manual',
+          },
+          description: 'graphql.resolve',
+          status: 'ok',
+          origin: 'manual',
+        }),
+      ]),
+    };
+
+    createRunner(__dirname, 'scenario-query.js').expect({ transaction: EXPECTED_TRANSACTION }).start(done);
+  });
+
+  test('CJS - should instrument GraphQL mutations used from Apollo Server.', done => {
+    const EXPECTED_TRANSACTION = {
+      transaction: 'Test Transaction',
+      spans: expect.arrayContaining([
+        expect.objectContaining({
+          data: {
+            'graphql.operation.name': 'Mutation',
+            'graphql.operation.type': 'mutation',
+            'graphql.source': `mutation Mutation($email: String) {
+  login(email: $email)
+}`,
+            'otel.kind': 'INTERNAL',
+            'sentry.origin': 'auto.graphql.otel.graphql',
+          },
+          description: 'mutation Mutation',
+          status: 'ok',
+          origin: 'auto.graphql.otel.graphql',
+        }),
+      ]),
+    };
+
+    createRunner(__dirname, 'scenario-mutation.js').expect({ transaction: EXPECTED_TRANSACTION }).start(done);
   });
 });
