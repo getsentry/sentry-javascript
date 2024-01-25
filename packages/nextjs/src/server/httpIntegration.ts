0807af72299430e8b6c1ec75c9bee90fa2f9aea5
Lukas Stracke <lukas.stracke@sentry.io>
2024-01-25 11:14:37 +0100

    fix(nextjs): Fix Http integration type declaration

--- packages/nextjs/src/server/httpIntegration.ts
+++ packages/nextjs/src/server/httpIntegration.ts
@@ -1,11 +1,10 @@
 import { Integrations } from '@sentry/node';
-const { Http: OriginalHttp } = Integrations;
 
 /**
  * A custom HTTP integration where we always enable tracing.
  */
-export class Http extends OriginalHttp {
-  public constructor(options?: ConstructorParameters<typeof OriginalHttp>[0]) {
+export class Http extends Integrations.Http {
+  public constructor(options?: ConstructorParameters<typeof Integrations.Http>[0]) {
     super({
       ...options,
       tracing: true,
