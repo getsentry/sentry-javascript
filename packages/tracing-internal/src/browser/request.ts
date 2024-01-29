getsentry/sentry-javascript

commit 52495c00ba700dd1df58025ef49195048279b692

ref(tracing): Remove startChild calls from fetch/xhr instrumentation (#10236)

packages/tracing-internal/src/browser/request.ts
@@ -1,6 +1,5 @@
 /* eslint-disable max-lines */
 import {
-  getActiveSpan,
   getClient,
   getCurrentScope,
   getDynamicSamplingContextFromClient,
@@ -9,6 +8,7 @@ import {
   hasTracingEnabled,
   spanToJSON,
   spanToTraceHeader,
+  startInactiveSpan,
 } from '@sentry/core';
 import type { HandlerDataXhr, SentryWrappedXMLHttpRequest, Span } from '@sentry/types';
 import {
@@ -275,22 +275,19 @@ export function xhrCallback(
   }
 
   const scope = getCurrentScope();
-  const parentSpan = getActiveSpan();
 
-  const span =
-    shouldCreateSpanResult && parentSpan
-      ? // eslint-disable-next-line deprecation/deprecation
-        parentSpan.startChild({
-          data: {
-            type: 'xhr',
-            'http.method': sentryXhrData.method,
-            url: sentryXhrData.url,
-          },
-          description: `${sentryXhrData.method} ${sentryXhrData.url}`,
-          op: 'http.client',
-          origin: 'auto.http.browser',
-        })
-      : undefined;
+  const span = shouldCreateSpanResult
+    ? startInactiveSpan({
+        attributes: {
+          type: 'xhr',
+          'http.method': sentryXhrData.method,
+          url: sentryXhrData.url,
+        },
+        name: `${sentryXhrData.method} ${sentryXhrData.url}`,
+        op: 'http.client',
+        origin: 'auto.http.browser',
+      })
+    : undefined;
 
   if (span) {
     xhr.__sentry_xhr_span_id__ = span.spanContext().spanId;

packages/tracing-internal/src/common/fetch.ts
@@ -1,5 +1,4 @@
 import {
-  getActiveSpan,
   getClient,
   getCurrentScope,
   getDynamicSamplingContextFromClient,
@@ -7,6 +6,7 @@ import {
   getRootSpan,
   hasTracingEnabled,
   spanToTraceHeader,
+  startInactiveSpan,
 } from '@sentry/core';
 import type { Client, HandlerDataFetch, Scope, Span, SpanOrigin } from '@sentry/types';
 import {
@@ -76,24 +76,21 @@ export function instrumentFetchRequest(
 
   const scope = getCurrentScope();
   const client = getClient();
-  const parentSpan = getActiveSpan();
 
   const { method, url } = handlerData.fetchData;
 
-  const span =
-    shouldCreateSpanResult && parentSpan
-      ? // eslint-disable-next-line deprecation/deprecation
-        parentSpan.startChild({
-          data: {
-            url,
-            type: 'fetch',
-            'http.method': method,
-          },
-          description: `${method} ${url}`,
-          op: 'http.client',
-          origin: spanOrigin,
-        })
-      : undefined;
+  const span = shouldCreateSpanResult
+    ? startInactiveSpan({
+        attributes: {
+          url,
+          type: 'fetch',
+          'http.method': method,
+        },
+        name: `${method} ${url}`,
+        op: 'http.client',
+        origin: spanOrigin,
+      })
+    : undefined;
 
   if (span) {
     handlerData.fetchData.__span = span.spanContext().spanId;

packages/tracing-internal/test/browser/request.test.ts
@@ -1,31 +1,13 @@
 /* eslint-disable deprecation/deprecation */
-import * as sentryCore from '@sentry/core';
-import { Hub, makeMain, spanToJSON } from '@sentry/core';
-import type { HandlerDataFetch, HandlerDataXhr, SentryWrappedXMLHttpRequest } from '@sentry/types';
 import * as utils from '@sentry/utils';
-import { SENTRY_XHR_DATA_KEY } from '@sentry/utils';
 
-import type { Transaction } from '../../../tracing/src';
-import { Span, addExtensionMethods, spanStatusfromHttpCode } from '../../../tracing/src';
-import { getDefaultBrowserClientOptions } from '../../../tracing/test/testutils';
-import {
-  extractNetworkProtocol,
-  instrumentOutgoingRequests,
-  shouldAttachHeaders,
-  xhrCallback,
-} from '../../src/browser/request';
-import { instrumentFetchRequest } from '../../src/common/fetch';
-import { TestClient } from '../utils/TestClient';
+import { extractNetworkProtocol, instrumentOutgoingRequests, shouldAttachHeaders } from '../../src/browser/request';
 
 beforeAll(() => {
-  addExtensionMethods();
   // @ts-expect-error need to override global Request because it's not in the jest environment (even with an
   // `@jest-environment jsdom` directive, for some reason)
   global.Request = {};
 });
-
-const hasTracingEnabled = jest.spyOn(sentryCore, 'hasTracingEnabled');
-const setRequestHeader = jest.fn();
 
 describe('instrumentOutgoingRequests', () => {
   beforeEach(() => {
@@ -56,363 +38,6 @@ describe('instrumentOutgoingRequests', () => {
     instrumentOutgoingRequests({ traceXHR: false });
 
     expect(addXhrSpy).not.toHaveBeenCalled();
-  });
-});
-
-describe('callbacks', () => {
-  let hub: Hub;
-  let transaction: Transaction;
-  const alwaysCreateSpan = () => true;
-  const alwaysAttachHeaders = () => true;
-  const startTimestamp = 1356996072000;
-  const endTimestamp = 1356996072000;
-
-  beforeAll(() => {
-    const options = getDefaultBrowserClientOptions({ tracesSampleRate: 1 });
-    hub = new Hub(new TestClient(options));
-    makeMain(hub);
-  });
-
-  beforeEach(() => {
-    transaction = hub.startTransaction({ name: 'organizations/users/:userid', op: 'pageload' }) as Transaction;
-    hub.getScope().setSpan(transaction);
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe('fetchCallback()', () => {
-    let fetchHandlerData: HandlerDataFetch;
-
-    const fetchSpan = {
-      data: {
-        'http.method': 'GET',
-        url: 'http://dogs.are.great/',
-        type: 'fetch',
-      },
-      description: 'GET http://dogs.are.great/',
-      op: 'http.client',
-      parentSpanId: expect.any(String),
-      spanId: expect.any(String),
-      startTimestamp: expect.any(Number),
-      traceId: expect.any(String),
-    };
-
-    beforeEach(() => {
-      fetchHandlerData = {
-        args: ['http://dogs.are.great/', {}],
-        fetchData: { url: 'http://dogs.are.great/', method: 'GET' },
-        startTimestamp,
-      };
-    });
-
-    it.each([
-      // each case is [shouldCreateSpanReturnValue, shouldAttachHeadersReturnValue, expectedSpan, expectedHeaderKeys]
-      [true, true, expect.objectContaining(fetchSpan), ['sentry-trace', 'baggage']],
-      [true, false, expect.objectContaining(fetchSpan), []],
-      [false, true, undefined, ['sentry-trace', 'baggage']],
-      [false, false, undefined, []],
-    ])(
-      'span creation/header attachment interaction - shouldCreateSpan: %s, shouldAttachHeaders: %s',
-      (shouldCreateSpanReturnValue, shouldAttachHeadersReturnValue, expectedSpan, expectedHeaderKeys) => {
-        instrumentFetchRequest(
-          fetchHandlerData,
-          () => shouldCreateSpanReturnValue,
-          () => shouldAttachHeadersReturnValue,
-          {},
-        );
-
-        // spans[0] is the transaction itself
-        const newSpan = transaction.spanRecorder?.spans[1] as Span;
-        expect(newSpan).toEqual(expectedSpan);
-
-        const headers = (fetchHandlerData.args[1].headers as Record<string, string>) || {};
-        expect(Object.keys(headers)).toEqual(expectedHeaderKeys);
-      },
-    );
-
-    it('adds neither fetch request spans nor fetch request headers if tracing is disabled', () => {
-      hasTracingEnabled.mockReturnValueOnce(false);
-      const spans = {};
-
-      instrumentFetchRequest(fetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      expect(spans).toEqual({});
-
-      const headers = (fetchHandlerData.args[1].headers as Record<string, string>) || {};
-      expect(Object.keys(headers)).toEqual([]);
-    });
-
-    it('creates and finishes fetch span on active transaction', () => {
-      const spans = {};
-
-      // triggered by request being sent
-      instrumentFetchRequest(fetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const newSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      expect(newSpan).toBeDefined();
-      expect(newSpan).toBeInstanceOf(Span);
-      expect(newSpan.data).toEqual({
-        'http.method': 'GET',
-        type: 'fetch',
-        url: 'http://dogs.are.great/',
-      });
-      expect(newSpan.description).toBe('GET http://dogs.are.great/');
-      expect(newSpan.op).toBe('http.client');
-      const spanId = fetchHandlerData.fetchData?.__span;
-      expect(spanId).toBeDefined();
-
-      const postRequestFetchHandlerData = {
-        ...fetchHandlerData,
-        endTimestamp,
-      };
-
-      // triggered by response coming back
-      instrumentFetchRequest(postRequestFetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      expect(spanToJSON(newSpan).timestamp).toBeDefined();
-    });
-
-    it('sets response status on finish', () => {
-      const spans: Record<string, Span> = {};
-
-      // triggered by request being sent
-      instrumentFetchRequest(fetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const newSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      expect(newSpan).toBeDefined();
-
-      const postRequestFetchHandlerData = {
-        ...fetchHandlerData,
-        endTimestamp,
-        response: { status: 404 } as Response,
-      };
-
-      // triggered by response coming back
-      instrumentFetchRequest(postRequestFetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      expect(newSpan.status).toBe(spanStatusfromHttpCode(404));
-    });
-
-    it('ignores response with no associated span', () => {
-      // the request might be missed somehow. E.g. if it was sent before tracing gets enabled.
-
-      const postRequestFetchHandlerData = {
-        ...fetchHandlerData,
-        endTimestamp,
-        response: { status: 404 } as Response,
-      };
-
-      // in that case, the response coming back will be ignored
-      instrumentFetchRequest(postRequestFetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, {});
-
-      const newSpan = transaction.spanRecorder?.spans[1];
-
-      expect(newSpan).toBeUndefined();
-    });
-
-    it('uses active span to generate sentry-trace header', () => {
-      const spans: Record<string, Span> = {};
-      // triggered by request being sent
-      instrumentFetchRequest(fetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const activeSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      const postRequestFetchHandlerData = {
-        ...fetchHandlerData,
-        endTimestamp,
-        response: { status: 200 } as Response,
-      };
-
-      // triggered by response coming back
-      instrumentFetchRequest(postRequestFetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const headers = (fetchHandlerData.args[1].headers as Record<string, string>) || {};
-      expect(headers['sentry-trace']).toEqual(`${activeSpan.traceId}-${activeSpan.spanId}-1`);
-    });
-
-    it('adds content-length to span data on finish', () => {
-      const spans: Record<string, Span> = {};
-
-      // triggered by request being sent
-      instrumentFetchRequest(fetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const newSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      expect(newSpan).toBeDefined();
-
-      const postRequestFetchHandlerData = {
-        ...fetchHandlerData,
-        endTimestamp,
-        response: { status: 404, headers: { get: () => 123 } },
-      } as unknown as HandlerDataFetch;
-
-      // triggered by response coming back
-      instrumentFetchRequest(postRequestFetchHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const finishedSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      expect(finishedSpan).toBeDefined();
-      expect(finishedSpan).toBeInstanceOf(Span);
-      expect(spanToJSON(finishedSpan).data).toEqual({
-        'http.response_content_length': 123,
-        'http.method': 'GET',
-        'http.response.status_code': 404,
-        type: 'fetch',
-        url: 'http://dogs.are.great/',
-        'sentry.op': 'http.client',
-        'sentry.origin': 'auto.http.browser',
-      });
-      expect(finishedSpan.op).toBe('http.client');
-    });
-  });
-
-  describe('xhrCallback()', () => {
-    let xhrHandlerData: HandlerDataXhr;
-
-    const xhrSpan = {
-      data: {
-        'http.method': 'GET',
-        url: 'http://dogs.are.great/',
-        type: 'xhr',
-      },
-      description: 'GET http://dogs.are.great/',
-      op: 'http.client',
-      parentSpanId: expect.any(String),
-      spanId: expect.any(String),
-      startTimestamp: expect.any(Number),
-      traceId: expect.any(String),
-    };
-
-    beforeEach(() => {
-      xhrHandlerData = {
-        args: ['GET', 'http://dogs.are.great/'],
-        xhr: {
-          [SENTRY_XHR_DATA_KEY]: {
-            method: 'GET',
-            url: 'http://dogs.are.great/',
-            status_code: 200,
-            request_headers: {},
-          },
-          __sentry_xhr_span_id__: '1231201211212012',
-          setRequestHeader,
-        } as SentryWrappedXMLHttpRequest,
-        startTimestamp,
-      };
-    });
-
-    it.each([
-      // each case is [shouldCreateSpanReturnValue, shouldAttachHeadersReturnValue, expectedSpan, expectedHeaderKeys]
-      [true, true, expect.objectContaining(xhrSpan), ['sentry-trace', 'baggage']],
-      [true, false, expect.objectContaining(xhrSpan), []],
-      [false, true, undefined, ['sentry-trace', 'baggage']],
-      [false, false, undefined, []],
-    ])(
-      'span creation/header attachment interaction - shouldCreateSpan: %s, shouldAttachHeaders: %s',
-      (shouldCreateSpanReturnValue, shouldAttachHeadersReturnValue, expectedSpan, expectedHeaderKeys) => {
-        xhrCallback(
-          xhrHandlerData,
-          () => shouldCreateSpanReturnValue,
-          () => shouldAttachHeadersReturnValue,
-          {},
-        );
-
-        // spans[0] is the transaction itself
-        const newSpan = transaction.spanRecorder?.spans[1] as Span;
-        expect(newSpan).toEqual(expectedSpan);
-
-        const headerKeys = setRequestHeader.mock.calls.map(header => header[0]);
-        expect(headerKeys).toEqual(expectedHeaderKeys);
-      },
-    );
-
-    it('adds neither xhr request spans nor xhr request headers if tracing is disabled', () => {
-      hasTracingEnabled.mockReturnValueOnce(false);
-      const spans = {};
-
-      xhrCallback(xhrHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      expect(spans).toEqual({});
-      expect(setRequestHeader).not.toHaveBeenCalled();
-    });
-
-    it('creates and finishes XHR span on active transaction', () => {
-      const spans = {};
-
-      // triggered by request being sent
-      xhrCallback(xhrHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const newSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      expect(newSpan).toBeInstanceOf(Span);
-      expect(newSpan.data).toEqual({
-        'http.method': 'GET',
-        type: 'xhr',
-        url: 'http://dogs.are.great/',
-      });
-      expect(newSpan.description).toBe('GET http://dogs.are.great/');
-      expect(newSpan.op).toBe('http.client');
-      const spanId = xhrHandlerData.xhr?.__sentry_xhr_span_id__;
-      expect(spanId).toBeDefined();
-      expect(spanId).toEqual(newSpan?.spanId);
-
-      const postRequestXHRHandlerData = {
-        ...xhrHandlerData,
-        endTimestamp,
-      };
-
-      // triggered by response coming back
-      xhrCallback(postRequestXHRHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      expect(spanToJSON(newSpan).timestamp).toBeDefined();
-    });
-
-    it('sets response status on finish', () => {
-      const spans = {};
-
-      // triggered by request being sent
-      xhrCallback(xhrHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      const newSpan = transaction.spanRecorder?.spans[1] as Span;
-
-      expect(newSpan).toBeDefined();
-
-      const postRequestXHRHandlerData = {
-        ...xhrHandlerData,
-        endTimestamp,
-      };
-      postRequestXHRHandlerData.xhr![SENTRY_XHR_DATA_KEY]!.status_code = 404;
-
-      // triggered by response coming back
-      xhrCallback(postRequestXHRHandlerData, alwaysCreateSpan, alwaysAttachHeaders, spans);
-
-      expect(newSpan.status).toBe(spanStatusfromHttpCode(404));
-    });
-
-    it('ignores response with no associated span', () => {
-      // the request might be missed somehow. E.g. if it was sent before tracing gets enabled.
-
-      const postRequestXHRHandlerData: HandlerDataXhr = {
-        ...{
-          xhr: {
-            [SENTRY_XHR_DATA_KEY]: xhrHandlerData.xhr?.[SENTRY_XHR_DATA_KEY],
-          },
-        },
-        args: ['GET', 'http://dogs.are.great/'],
-        startTimestamp,
-        endTimestamp,
-      };
-
-      // in that case, the response coming back will be ignored
-      xhrCallback(postRequestXHRHandlerData, alwaysCreateSpan, alwaysAttachHeaders, {});
-
-      const newSpan = transaction.spanRecorder?.spans[1];
-
-      expect(newSpan).toBeUndefined();
-    });
   });
 });
 

commit 9690d7d512d194f885d193a3ded73f0e309b241b

fix(tracing): Only create request span if there is active span (#10375)
This was a regression introduced with
https://github.com/getsentry/sentry-javascript/pull/10236, we shouldn't
arbitrarily call `startInactiveSpan` given we create transactions under
the hood currently.

packages/tracing-internal/src/browser/request.ts
@@ -1,6 +1,7 @@
 /* eslint-disable max-lines */
 import {
   SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN,
+  getActiveSpan,
   getClient,
   getCurrentScope,
   getDynamicSamplingContextFromClient,
@@ -279,18 +280,21 @@ export function xhrCallback(
   const scope = getCurrentScope();
   const isolationScope = getIsolationScope();
 
-  const span = shouldCreateSpanResult
-    ? startInactiveSpan({
-        attributes: {
-          type: 'xhr',
-          'http.method': sentryXhrData.method,
-          url: sentryXhrData.url,
-          [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.http.browser',
-        },
-        name: `${sentryXhrData.method} ${sentryXhrData.url}`,
-        op: 'http.client',
-      })
-    : undefined;
+  // only create a child span if there is an active span. This is because
+  // `startInactiveSpan` can still create a transaction under the hood
+  const span =
+    shouldCreateSpanResult && getActiveSpan()
+      ? startInactiveSpan({
+          attributes: {
+            type: 'xhr',
+            'http.method': sentryXhrData.method,
+            url: sentryXhrData.url,
+            [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.http.browser',
+          },
+          name: `${sentryXhrData.method} ${sentryXhrData.url}`,
+          op: 'http.client',
+        })
+      : undefined;
 
   if (span) {
     xhr.__sentry_xhr_span_id__ = span.spanContext().spanId;

packages/tracing-internal/src/common/fetch.ts
@@ -1,5 +1,6 @@
 import {
   SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN,
+  getActiveSpan,
   getClient,
   getCurrentScope,
   getDynamicSamplingContextFromClient,
@@ -81,18 +82,21 @@ export function instrumentFetchRequest(
 
   const { method, url } = handlerData.fetchData;
 
-  const span = shouldCreateSpanResult
-    ? startInactiveSpan({
-        attributes: {
-          url,
-          type: 'fetch',
-          'http.method': method,
-          [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: spanOrigin,
-        },
-        name: `${method} ${url}`,
-        op: 'http.client',
-      })
-    : undefined;
+  // only create a child span if there is an active span. This is because
+  // `startInactiveSpan` can still create a transaction under the hood
+  const span =
+    shouldCreateSpanResult && getActiveSpan()
+      ? startInactiveSpan({
+          attributes: {
+            url,
+            type: 'fetch',
+            'http.method': method,
+            [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: spanOrigin,
+          },
+          name: `${method} ${url}`,
+          op: 'http.client',
+        })
+      : undefined;
 
   if (span) {
     handlerData.fetchData.__span = span.spanContext().spanId;