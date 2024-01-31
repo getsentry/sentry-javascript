f5ffacf3839a98d0d7014079c7cf1ff52e713036
Lukas Stracke <lukas.stracke@sentry.io>
2024-01-31 16:21:32 +0100

    test(e2e): Add tests to SvelteKit 2.x E2E test app (#9944)
    Add proper e2e tests to our Sveltekit 2.x test application. 
    
    1. Distributed pageload trace - important for browserTracing rework
    2. Distributed navigation trace - important for browserTracing rework
    3. Client side component error
    4. Client side universal load error
    5. Server side universal load error
    6. Server side server load error
    7. Server route/API error

--- dev-packages/e2e-tests/test-applications/sveltekit-2/playwright.config.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-2/playwright.config.ts
@@ -1,13 +1,19 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
+// Fix urls not resolving to localhost on Node v17+
+// See: https://github.com/axios/axios/issues/3821#issuecomment-1413727575
+import { setDefaultResultOrder } from 'dns';
+setDefaultResultOrder('ipv4first');
+
 const testEnv = process.env.TEST_ENV;
 
 if (!testEnv) {
   throw new Error('No test env defined');
 }
 
-const port = 3030;
+const svelteKitPort = 3030;
+const eventProxyPort = 3031;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,7 +30,8 @@ const config: PlaywrightTestConfig = {
     timeout: 10000,
   },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
+  workers: 1,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* `next dev` is incredibly buggy with the app dir */
@@ -36,7 +43,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://localhost:${port}`,
+    baseURL: `http://localhost:${svelteKitPort}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -55,15 +62,17 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'pnpm ts-node --esm start-event-proxy.ts',
-      port: 3031,
+      command: 'pnpm ts-node-script start-event-proxy.ts',
+      port: eventProxyPort,
+      reuseExistingServer: false,
     },
     {
       command:
         testEnv === 'development'
-          ? `pnpm wait-port ${port} && pnpm dev --port ${port}`
-          : `pnpm wait-port ${port} && pnpm preview --port ${port}`,
-      port,
+          ? `pnpm wait-port ${eventProxyPort} && pnpm dev --port ${svelteKitPort}`
+          : `pnpm wait-port ${eventProxyPort} && PORT=${svelteKitPort} node build`,
+      port: svelteKitPort,
+      reuseExistingServer: false,
     },
   ],
 };
