2000d7e14611769078d6c3e67a30905e090ec24e
Francesco Novy <francesco.novy@sentry.io>
2024-02-01 09:20:47 +0100

    feat(node-experimental): Update tracing integrations to functional style (#10443)
    This refactors the node-experimental performance integrations to
    functional style.
    Importantly, it also slightly updates the options for httpIntegration &
    nativeNodeFetchIntegration, with what they can/should be in v8:
    
    * There is no way to disable span creation for them (this is pretty
    hacky to do, and prob. not needed - not allowing this simplifies this a
    lot)
    * You can define filters for incoming/outgoing requests to filter them
    fully - that will filter them both for breadcrumbs and spans.
    * spans are not created anyhow when tracing is disabled.

--- packages/node-experimental/src/integrations/getAutoPerformanceIntegrations.ts
+++ packages/node-experimental/src/integrations/getAutoPerformanceIntegrations.ts
@@ -1,73 +1,32 @@
 import type { Integration } from '@sentry/types';
 
-import type { NodePerformanceIntegration } from './NodePerformanceIntegration';
-import { Express } from './express';
-import { Fastify } from './fastify';
-import { GraphQL } from './graphql';
-import { Hapi } from './hapi';
-import { Mongo } from './mongo';
-import { Mongoose } from './mongoose';
-import { Mysql } from './mysql';
-import { Mysql2 } from './mysql2';
-import { Nest } from './nest';
-import { Postgres } from './postgres';
-import { Prisma } from './prisma';
-
-const INTEGRATIONS: (() => NodePerformanceIntegration<unknown>)[] = [
-  () => {
-    return new Express();
-  },
-  () => {
-    return new Fastify();
-  },
-  () => {
-    return new GraphQL();
-  },
-  () => {
-    return new Mongo();
-  },
-  () => {
-    return new Mongoose();
-  },
-  () => {
-    return new Mysql();
-  },
-  () => {
-    return new Mysql2();
-  },
-  () => {
-    return new Postgres();
-  },
-  () => {
-    return new Prisma();
-  },
-  () => {
-    return new Nest();
-  },
-  () => {
-    return new Hapi();
-  },
-];
+import { expressIntegration } from './express';
+import { fastifyIntegration } from './fastify';
+import { graphqlIntegration } from './graphql';
+import { hapiIntegration } from './hapi';
+import { mongoIntegration } from './mongo';
+import { mongooseIntegration } from './mongoose';
+import { mysqlIntegration } from './mysql';
+import { mysql2Integration } from './mysql2';
+import { nestIntegration } from './nest';
+import { postgresIntegration } from './postgres';
+import { prismaIntegration } from './prisma';
 
 /**
- * Get auto-dsicovered performance integrations.
- * Note that due to the way OpenTelemetry instrumentation works, this will generally still return Integrations
- * for stuff that may not be installed. This is because Otel only instruments when the module is imported/required,
- * so if the package is not required at all it will not be patched, and thus not instrumented.
- * But the _Sentry_ Integration will still be added.
- * This _may_ be a bit confusing because it shows all integrations as being installed in the debug logs, but this is
- * technically not wrong because we install it (it just doesn't do anything).
+ * With OTEL, all performance integrations will be added, as OTEL only initializes them when the patched package is actually required.
  */
 export function getAutoPerformanceIntegrations(): Integration[] {
-  const loadedIntegrations = INTEGRATIONS.map(tryLoad => {
-    try {
-      const integration = tryLoad();
-      const isLoaded = integration.loadInstrumentations();
-      return isLoaded ? integration : false;
-    } catch (_) {
-      return false;
-    }
-  }).filter(integration => !!integration) as Integration[];
-
-  return loadedIntegrations;
+  return [
+    expressIntegration(),
+    fastifyIntegration(),
+    graphqlIntegration(),
+    mongoIntegration(),
+    mongooseIntegration(),
+    mysqlIntegration(),
+    mysql2Integration(),
+    postgresIntegration(),
+    prismaIntegration(),
+    nestIntegration(),
+    hapiIntegration(),
+  ];
 }
--- packages/node-experimental/src/integrations/http.ts
+++ packages/node-experimental/src/integrations/http.ts
@@ -3,9 +3,9 @@ import type { Span } from '@opentelemetry/api';
 import { SpanKind } from '@opentelemetry/api';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { addBreadcrumb, hasTracingEnabled, isSentryRequestUrl } from '@sentry/core';
+import { addBreadcrumb, defineIntegration, hasTracingEnabled, isSentryRequestUrl } from '@sentry/core';
 import { _INTERNAL, getClient, getSpanKind, setSpanMetadata } from '@sentry/opentelemetry';
-import type { EventProcessor, Hub, Integration } from '@sentry/types';
+import type { EventProcessor, Hub, Integration, IntegrationFn } from '@sentry/types';
 import { stringMatchesSomePattern } from '@sentry/utils';
 
 import { getIsolationScope, setIsolationScope } from '../sdk/api';
@@ -14,6 +14,97 @@ import { addOriginToSpan } from '../utils/addOriginToSpan';
 import { getRequestUrl } from '../utils/getRequestUrl';
 
 interface HttpOptions {
+  /**
+   * Whether breadcrumbs should be recorded for requests.
+   * Defaults to true
+   */
+  breadcrumbs?: boolean;
+
+  /**
+   * Do not capture spans or breadcrumbs for outgoing HTTP requests to URLs where the given callback returns `true`.
+   * This controls both span & breadcrumb creation - spans will be non recording if tracing is disabled.
+   */
+  ignoreOutgoingRequests?: (url: string) => boolean;
+
+  /**
+   * Do not capture spans or breadcrumbs for incoming HTTP requests to URLs where the given callback returns `true`.
+   * This controls both span & breadcrumb creation - spans will be non recording if tracing is disabled.
+   */
+  ignoreIncomingRequests?: (url: string) => boolean;
+}
+
+const _httpIntegration = ((options: HttpOptions = {}) => {
+  const _breadcrumbs = typeof options.breadcrumbs === 'undefined' ? true : options.breadcrumbs;
+  const _ignoreOutgoingRequests = options.ignoreOutgoingRequests;
+  const _ignoreIncomingRequests = options.ignoreIncomingRequests;
+
+  return {
+    name: 'Http',
+    setupOnce() {
+      const instrumentations = [
+        new HttpInstrumentation({
+          ignoreOutgoingRequestHook: request => {
+            const url = getRequestUrl(request);
+
+            if (!url) {
+              return false;
+            }
+
+            if (isSentryRequestUrl(url, getClient())) {
+              return true;
+            }
+
+            if (_ignoreOutgoingRequests && _ignoreOutgoingRequests(url)) {
+              return true;
+            }
+
+            return false;
+          },
+
+          ignoreIncomingRequestHook: request => {
+            const url = getRequestUrl(request);
+
+            const method = request.method?.toUpperCase();
+            // We do not capture OPTIONS/HEAD requests as transactions
+            if (method === 'OPTIONS' || method === 'HEAD') {
+              return true;
+            }
+
+            if (_ignoreIncomingRequests && _ignoreIncomingRequests(url)) {
+              return true;
+            }
+
+            return false;
+          },
+
+          requireParentforOutgoingSpans: true,
+          requireParentforIncomingSpans: false,
+          requestHook: (span, req) => {
+            _updateSpan(span, req);
+
+            // Update the isolation scope, isolate this request
+            if (getSpanKind(span) === SpanKind.SERVER) {
+              setIsolationScope(getIsolationScope().clone());
+            }
+          },
+          responseHook: (span, res) => {
+            if (_breadcrumbs) {
+              _addRequestBreadcrumb(span, res);
+            }
+          },
+        }),
+      ];
+
+      registerInstrumentations({
+        instrumentations,
+      });
+    },
+  };
+}) satisfies IntegrationFn;
+
+export const httpIntegration = defineIntegration(_httpIntegration);
+
+interface OldHttpOptions {
   /**
    * Whether breadcrumbs should be recorded for requests
    * Defaults to true
@@ -39,6 +130,8 @@ interface HttpOptions {
  * * Create spans for outgoing requests
  *
  * Note that this integration is also needed for the Express integration to work!
+ *
+ * @deprecated Use `httpIntegration()` instead.
  */
 export class Http implements Integration {
   /**
@@ -65,7 +158,8 @@ export class Http implements Integration {
   /**
    * @inheritDoc
    */
-  public constructor(options: HttpOptions = {}) {
+  public constructor(options: OldHttpOptions = {}) {
+    // eslint-disable-next-line deprecation/deprecation
     this.name = Http.id;
     this._breadcrumbs = typeof options.breadcrumbs === 'undefined' ? true : options.breadcrumbs;
     this._spans = typeof options.spans === 'undefined' ? undefined : options.spans;
@@ -127,7 +221,7 @@ export class Http implements Integration {
           requireParentforOutgoingSpans: true,
           requireParentforIncomingSpans: false,
           requestHook: (span, req) => {
-            this._updateSpan(span, req);
+            _updateSpan(span, req);
 
             // Update the isolation scope, isolate this request
             if (getSpanKind(span) === SpanKind.SERVER) {
@@ -135,7 +229,9 @@ export class Http implements Integration {
             }
           },
           responseHook: (span, res) => {
-            this._addRequestBreadcrumb(span, res);
+            if (this._breadcrumbs) {
+              _addRequestBreadcrumb(span, res);
+            }
           },
         }),
       ],
@@ -148,39 +244,39 @@ export class Http implements Integration {
   public unregister(): void {
     this._unload?.();
   }
+}
 
-  /** Update the span with data we need. */
-  private _updateSpan(span: Span, request: ClientRequest | IncomingMessage): void {
-    addOriginToSpan(span, 'auto.http.otel.http');
+/** Update the span with data we need. */
+function _updateSpan(span: Span, request: ClientRequest | IncomingMessage): void {
+  addOriginToSpan(span, 'auto.http.otel.http');
 
-    if (getSpanKind(span) === SpanKind.SERVER) {
-      setSpanMetadata(span, { request });
-    }
+  if (getSpanKind(span) === SpanKind.SERVER) {
+    setSpanMetadata(span, { request });
+  }
+}
+
+/** Add a breadcrumb for outgoing requests. */
+function _addRequestBreadcrumb(span: Span, response: IncomingMessage | ServerResponse): void {
+  if (getSpanKind(span) !== SpanKind.CLIENT) {
+    return;
   }
 
-  /** Add a breadcrumb for outgoing requests. */
-  private _addRequestBreadcrumb(span: Span, response: IncomingMessage | ServerResponse): void {
-    if (!this._breadcrumbs || getSpanKind(span) !== SpanKind.CLIENT) {
-      return;
-    }
-
-    const data = _INTERNAL.getRequestSpanData(span);
-    addBreadcrumb(
-      {
-        category: 'http',
-        data: {
-          status_code: response.statusCode,
-          ...data,
-        },
-        type: 'http',
+  const data = _INTERNAL.getRequestSpanData(span);
+  addBreadcrumb(
+    {
+      category: 'http',
+      data: {
+        status_code: response.statusCode,
+        ...data,
       },
-      {
-        event: 'response',
-        // TODO FN: Do we need access to `request` here?
-        // If we do, we'll have to use the `applyCustomAttributesOnSpan` hook instead,
-        // but this has worse context semantics than request/responseHook.
-        response,
-      },
-    );
-  }
+      type: 'http',
+    },
+    {
+      event: 'response',
+      // TODO FN: Do we need access to `request` here?
+      // If we do, we'll have to use the `applyCustomAttributesOnSpan` hook instead,
+      // but this has worse context semantics than request/responseHook.
+      response,
+    },
+  );
 }
