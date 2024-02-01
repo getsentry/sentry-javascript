9c86f1c0c0bfc53731159a0f49144f90aad8599b
Lukas Stracke <lukas.stracke@sentry.io>
2024-02-01 10:25:30 +0100

    rm vite config e2e

--- dev-packages/e2e-tests/test-applications/sveltekit/vite.config.js
+++ dev-packages/e2e-tests/test-applications/sveltekit/vite.config.js
@@ -1,4 +1,3 @@
-import { version } from '$app/environment';
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
@@ -7,9 +6,6 @@ export default defineConfig({
   plugins: [
     sentrySvelteKit({
       autoUploadSourceMaps: false,
-      sourceMapsUploadOptions: {
-        release: version,
-      },
     }),
     sveltekit(),
   ],
