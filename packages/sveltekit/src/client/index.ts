0e52c0798f4bffa3f2bf3fa86c1c916090740fd1
Lukas Stracke <lukas.stracke@sentry.io>
2024-02-02 13:46:49 +0100

    feat(sveltekit): Add custom `browserTracingIntegration()` (#10450)
    Deprecates SvelteKit's `BrowserTracing` integration in favour of
    the new `browserTracingIntegration` functional integration. The new
    integration now also directly initializes the routing instrumentation.

--- packages/sveltekit/src/client/index.ts
+++ packages/sveltekit/src/client/index.ts
@@ -3,3 +3,4 @@ export * from '@sentry/svelte';
 export { init } from './sdk';
 export { handleErrorWithSentry } from './handleError';
 export { wrapLoadWithSentry } from './load';
+export { browserTracingIntegration } from './browserTracingIntegration';
