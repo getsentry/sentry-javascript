548db068905c97a6999870c9ad12439781fc5bd9
Francesco Novy <francesco.novy@sentry.io>
2024-02-02 14:58:07 +0100

    feat: Export `setHttpStatus` from all packages
    This replaces an old API but was not exported anywhere.

--- packages/astro/src/index.server.ts
+++ packages/astro/src/index.server.ts
@@ -49,6 +49,7 @@ export {
   // eslint-disable-next-line deprecation/deprecation
   spanStatusfromHttpCode,
   getSpanStatusFromHttpCode,
+  setHttpStatus,
   // eslint-disable-next-line deprecation/deprecation
   trace,
   withScope,
