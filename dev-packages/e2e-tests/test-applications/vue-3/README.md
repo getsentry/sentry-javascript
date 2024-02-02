3a60d55ff04e83caa55bc68546387cb40775efb5
Lukas Stracke <lukas.stracke@sentry.io>
2024-02-02 15:26:01 +0100

    test(e2e): Add Vue 3 E2E tests

---
+++ dev-packages/e2e-tests/test-applications/vue-3/README.md
@@ -0,0 +1,3 @@
+# Vue 3 E2E Test App
+
+E2E test app for Vue 3 and `@sentry/vue`.
---
+++ dev-packages/e2e-tests/test-applications/vue-3/tests/errors.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test';
+import { waitForError } from '../event-proxy-server';
+
+test('sends an error', async ({ page }) => {
+  const errorPromise = waitForError('vue-3', async errorEvent => {
+    return !errorEvent?.transaction;
+  });
+
+  await page.goto(`/`);
+
+  await page.locator('#errorBtn').click();
+
+  const error = await errorPromise;
+
+  expect(error).toMatchObject({
+    exception: {
+      values: [
+        {
+          type: 'Error',
+          value: 'This is a Vue test error',
+          mechanism: {
+            type: 'generic',
+            handled: false,
+          },
+        },
+      ],
+    },
+  });
+});
