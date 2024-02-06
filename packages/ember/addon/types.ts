dea36e70d7d51de400259739f40dd8ca4652a3be
Francesco Novy <francesco.novy@sentry.io>
2024-02-06 10:59:54 +0100

    feat(browser): Deprecate `BrowserTracing` integration
    There is a proper replacement for all of them now.
    
    fix linting issues

--- packages/ember/addon/types.ts
+++ packages/ember/addon/types.ts
@@ -1,7 +1,9 @@
-import type { BrowserOptions, BrowserTracing } from '@sentry/browser';
+import type { BrowserOptions, BrowserTracing, browserTracingIntegration } from '@sentry/browser';
 import type { Transaction, TransactionContext } from '@sentry/types';
 
-type BrowserTracingOptions = ConstructorParameters<typeof BrowserTracing>[0];
+type BrowserTracingOptions = Parameters<typeof browserTracingIntegration>[0] &
+  // eslint-disable-next-line deprecation/deprecation
+  ConstructorParameters<typeof BrowserTracing>[0];
 
 export type EmberSentryConfig = {
   sentry: BrowserOptions & { browserTracingOptions?: BrowserTracingOptions };
