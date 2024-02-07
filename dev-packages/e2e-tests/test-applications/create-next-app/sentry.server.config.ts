ca6010bbd65f65b287802e611f702d000e5f4f56
Abhijeet Prasad <aprasad@sentry.io>
2024-02-07 14:23:32 -0500

    feat(v8/node): Remove LocalVariables class integration (#10558)
    Works toward https://github.com/getsentry/sentry-javascript/issues/8844
    in a more incremental way.

--- dev-packages/e2e-tests/test-applications/create-next-app/sentry.server.config.ts
+++ dev-packages/e2e-tests/test-applications/create-next-app/sentry.server.config.ts
@@ -15,12 +15,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_E2E_TEST_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-
-  integrations: [new Sentry.Integrations.LocalVariables()],
+  integrations: [Sentry.localVariablesIntegration()],
 });
 
 Sentry.addEventProcessor(event => {
