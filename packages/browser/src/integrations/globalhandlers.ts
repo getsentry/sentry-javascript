bd22d1aba5f2c8f7c2733e8840c5b70f8ac73ab1
Abhijeet Prasad <aprasad@sentry.io>
2024-02-07 13:35:35 -0500

    fix typing issues

--- packages/browser/src/integrations/globalhandlers.ts
+++ packages/browser/src/integrations/globalhandlers.ts
@@ -107,24 +107,23 @@ function _getUnhandledRejectionError(error: unknown): unknown {
     return error;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const e = error as any;
-
   // dig the object of the rejection out of known event types
   try {
+    type ErrorWithReason = { reason: unknown };
     // PromiseRejectionEvents store the object of the rejection under 'reason'
     // see https://developer.mozilla.org/en-US/docs/Web/API/PromiseRejectionEvent
-    if ('reason' in e) {
-      return e.reason;
+    if ('reason' in (error as ErrorWithReason)) {
+      return (error as ErrorWithReason).reason;
     }
 
+    type CustomEventWithDetail = { detail: { reason: unknown } };
     // something, somewhere, (likely a browser extension) effectively casts PromiseRejectionEvents
     // to CustomEvents, moving the `promise` and `reason` attributes of the PRE into
     // the CustomEvent's `detail` attribute, since they're not part of CustomEvent's spec
     // see https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent and
     // https://github.com/getsentry/sentry-javascript/issues/2380
-    else if ('detail' in e && 'reason' in e.detail) {
-      return e.detail.reason;
+    if ('detail' in (error as CustomEventWithDetail) && 'reason' in (error as CustomEventWithDetail).detail) {
+      return (error as CustomEventWithDetail).detail.reason;
     }
   } catch {} // eslint-disable-line no-empty
 
