11d854eeb12b5ac4209de4c0500c77e11b1b677e
Francesco Novy <francesco.novy@sentry.io>
2024-02-12 14:36:18 +0100

    feat(opentelemetry): Do not capture span events as breadcrumbs (#10612)
    This allows us to simplify things, for now - we may add this back later
    in a more generic way, when/if we add the events to our shared span
    interface.

--- packages/opentelemetry/src/custom/scope.ts
+++ packages/opentelemetry/src/custom/scope.ts
@@ -1,15 +1,8 @@
-import type { Span } from '@opentelemetry/api';
-import type { TimedEvent } from '@opentelemetry/sdk-trace-base';
 import { Scope } from '@sentry/core';
-import type { Breadcrumb, ScopeData, SeverityLevel, Span as SentrySpan } from '@sentry/types';
-import { dropUndefinedKeys, logger } from '@sentry/utils';
+import type { Span as SentrySpan } from '@sentry/types';
+import { logger } from '@sentry/utils';
 
 import { DEBUG_BUILD } from '../debug-build';
-import { InternalSentrySemanticAttributes } from '../semanticAttributes';
-import { convertOtelTimeToSeconds } from '../utils/convertOtelTimeToSeconds';
-import { getActiveSpan } from '../utils/getActiveSpan';
-import { getSpanParent } from '../utils/spanData';
-import { spanHasEvents } from '../utils/spanTypes';
 
 /** A fork of the classic scope with some otel specific stuff. */
 export class OpenTelemetryScope extends Scope {
@@ -64,74 +57,4 @@ export class OpenTelemetryScope extends Scope {
 
     return this;
   }
-
-  /** @inheritDoc */
-  public getScopeData(): ScopeData {
-    const data = super.getScopeData();
-
-    data.breadcrumbs = this._getBreadcrumbs();
-
-    return data;
-  }
-
-  /**
-   * @inheritDoc
-   */
-  protected _getBreadcrumbs(): Breadcrumb[] {
-    const span = getActiveSpan();
-    const spanBreadcrumbs = span ? getBreadcrumbsForSpan(span) : [];
-
-    return spanBreadcrumbs.length > 0 ? this._breadcrumbs.concat(spanBreadcrumbs) : this._breadcrumbs;
-  }
-}
-
-/**
- * Get all breadcrumbs for the given span as well as it's parents.
- */
-function getBreadcrumbsForSpan(span: Span): Breadcrumb[] {
-  const events = span ? getOtelEvents(span) : [];
-
-  return events.map(otelEventToBreadcrumb);
-}
-
-function otelEventToBreadcrumb(event: TimedEvent): Breadcrumb {
-  const attributes = event.attributes || {};
-
-  const type = attributes[InternalSentrySemanticAttributes.BREADCRUMB_TYPE] as string | undefined;
-  const level = attributes[InternalSentrySemanticAttributes.BREADCRUMB_LEVEL] as SeverityLevel | undefined;
-  const eventId = attributes[InternalSentrySemanticAttributes.BREADCRUMB_EVENT_ID] as string | undefined;
-  const category = attributes[InternalSentrySemanticAttributes.BREADCRUMB_CATEGORY] as string | undefined;
-  const dataStr = attributes[InternalSentrySemanticAttributes.BREADCRUMB_DATA] as string | undefined;
-
-  const breadcrumb: Breadcrumb = dropUndefinedKeys({
-    timestamp: convertOtelTimeToSeconds(event.time),
-    message: event.name,
-    type,
-    level,
-    event_id: eventId,
-    category,
-  });
-
-  if (typeof dataStr === 'string') {
-    try {
-      const data = JSON.parse(dataStr);
-      breadcrumb.data = data;
-    } catch (e) {} // eslint-disable-line no-empty
-  }
-
-  return breadcrumb;
-}
-
-function getOtelEvents(span: Span, events: TimedEvent[] = []): TimedEvent[] {
-  if (spanHasEvents(span)) {
-    events.push(...span.events);
-  }
-
-  // Go up parent chain and collect events
-  const parent = getSpanParent(span);
-  if (parent) {
-    return getOtelEvents(parent, events);
-  }
-
-  return events;
 }
