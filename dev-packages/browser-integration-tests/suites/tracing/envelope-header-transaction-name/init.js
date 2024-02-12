ab9ee76e1c9416cd16d1308fa632ed77f4fc7a01
Luca Forstner <luca.forstner@sentry.io>
2024-02-12 15:02:57 +0000

    Revert weird test failure?

--- dev-packages/browser-integration-tests/suites/tracing/envelope-header-transaction-name/init.js
+++ dev-packages/browser-integration-tests/suites/tracing/envelope-header-transaction-name/init.js
@@ -1,11 +1,11 @@
 import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
 
 window.Sentry = Sentry;
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  integrations: [Sentry.browserTracingIntegration()],
-  tracePropagationTargets: [/.*/],
+  integrations: [new Integrations.BrowserTracing({ tracingOrigins: [/.*/] })],
   environment: 'production',
   tracesSampleRate: 1,
   debug: true,
