b4b349cf1e19c14f9f09b54ae90280f9600571ca
Edward Gou <edward.gou@sentry.io>
2024-02-17 12:17:38 -0500

    Datacategory map

--- packages/utils/src/envelope.ts
+++ packages/utils/src/envelope.ts
@@ -209,6 +209,7 @@ const ITEM_TYPE_TO_DATA_CATEGORY_MAP: Record<EnvelopeItemType, DataCategory> = {
   replay_recording: 'replay',
   check_in: 'monitor',
   feedback: 'feedback',
+  span: 'span',
   // TODO: This is a temporary workaround until we have a proper data category for metrics
   statsd: 'unknown',
 };
