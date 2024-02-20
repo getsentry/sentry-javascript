de681dcf7d6dac69da9374bbdbe2e2f7e00f0fdc
Abhijeet Prasad <aprasad@sentry.io>
2024-02-20 11:10:32 -0500

    feat(v8): Update eventFromUnknownInput to only use client (#10692)
    ref https://github.com/getsentry/sentry-javascript/issues/10100

--- packages/node/test/eventbuilders.test.ts
+++ packages/node/test/eventbuilders.test.ts
@@ -1,4 +1,3 @@
-import type { Hub } from '@sentry/types';
 import { eventFromUnknownInput } from '@sentry/utils';
 
 import { defaultStackParser } from '../src';
@@ -27,52 +26,6 @@ describe('eventFromUnknownInput', () => {
       },
     } as any;
     const event = eventFromUnknownInput(client, defaultStackParser, deepObject);
-
-    const serializedObject = event.extra?.__serialized__;
-    expect(serializedObject).toBeDefined();
-    expect(serializedObject).toEqual({
-      a: {
-        b: {
-          c: {
-            d: {
-              e: {
-                f: '[Object]',
-              },
-            },
-          },
-        },
-      },
-    });
-  });
-
-  test('uses normalizeDepth from init options (passing getCurrentHub)', () => {
-    const deepObject = {
-      a: {
-        b: {
-          c: {
-            d: {
-              e: {
-                f: {
-                  g: 'foo',
-                },
-              },
-            },
-          },
-        },
-      },
-    };
-
-    const getCurrentHub = jest.fn(() => {
-      return {
-        getClient: () => ({
-          getOptions(): any {
-            return { normalizeDepth: 6 };
-          },
-        }),
-      } as unknown as Hub;
-    });
-
-    const event = eventFromUnknownInput(getCurrentHub, defaultStackParser, deepObject);
 
     const serializedObject = event.extra?.__serialized__;
     expect(serializedObject).toBeDefined();
