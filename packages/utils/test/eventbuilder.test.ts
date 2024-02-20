ddaf99ce0c7515a2073aca62f71fe88a2678dc1f
Abhijeet Prasad <aprasad@sentry.io>
2024-02-20 10:03:41 -0500

    set getOptions properly

--- packages/utils/test/eventbuilder.test.ts
+++ packages/utils/test/eventbuilder.test.ts
@@ -5,28 +5,31 @@ import { createStackParser, eventFromUnknownInput, nodeStackLineParser } from '.
 const stackParser = createStackParser(nodeStackLineParser());
 
 describe('eventFromUnknownInput', () => {
+  const fakeClient = {
+    getOptions: () => ({}),
+  } as Client;
   test('object with useless props', () => {
-    const event = eventFromUnknownInput({} as Client, stackParser, { foo: { bar: 'baz' }, prop: 1 });
+    const event = eventFromUnknownInput(fakeClient, stackParser, { foo: { bar: 'baz' }, prop: 1 });
     expect(event.exception?.values?.[0].value).toBe('Object captured as exception with keys: foo, prop');
   });
 
   test('object with name prop', () => {
-    const event = eventFromUnknownInput({} as Client, stackParser, { foo: { bar: 'baz' }, name: 'BadType' });
+    const event = eventFromUnknownInput(fakeClient, stackParser, { foo: { bar: 'baz' }, name: 'BadType' });
     expect(event.exception?.values?.[0].value).toBe("'BadType' captured as exception");
   });
 
   test('object with name and message props', () => {
-    const event = eventFromUnknownInput({} as Client, stackParser, { message: 'went wrong', name: 'BadType' });
+    const event = eventFromUnknownInput(fakeClient, stackParser, { message: 'went wrong', name: 'BadType' });
     expect(event.exception?.values?.[0].value).toBe("'BadType' captured as exception with message 'went wrong'");
   });
 
   test('object with message prop', () => {
-    const event = eventFromUnknownInput({} as Client, stackParser, { foo: { bar: 'baz' }, message: 'Some message' });
+    const event = eventFromUnknownInput(fakeClient, stackParser, { foo: { bar: 'baz' }, message: 'Some message' });
     expect(event.exception?.values?.[0].value).toBe('Some message');
   });
 
   test('passing client directly', () => {
-    const event = eventFromUnknownInput({} as Client, stackParser, { foo: { bar: 'baz' }, prop: 1 });
+    const event = eventFromUnknownInput(fakeClient, stackParser, { foo: { bar: 'baz' }, prop: 1 });
     expect(event.exception?.values?.[0].value).toBe('Object captured as exception with keys: foo, prop');
   });
 });
