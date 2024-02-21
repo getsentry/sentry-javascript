b6ad00c52e96655ebd6af2a073cd6ed194d5d7b6
Luca Forstner <luca.forstner@sentry.io>
2024-02-21 16:03:22 +0000

    remove BrowserTracing browser integration tests

--- dev-packages/browser-integration-tests/suites/tracing/browsertracing/backgroundtab-pageload/subject.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('go-background').addEventListener('click', () => {
-  setTimeout(() => {
-    Object.defineProperty(document, 'hidden', { value: true, writable: true });
-    const ev = document.createEvent('Event');
-    ev.initEvent('visibilitychange');
-    document.dispatchEvent(ev);
-  }, 250);
-});
