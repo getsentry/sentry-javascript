d36a78eec22294121031ebfc0203301c43f7c2f5
Abhijeet Prasad <aprasad@sentry.io>
2024-02-22 15:32:32 -0500

    add to add transaction

--- dev-packages/browser-integration-tests/suites/public-api/startTransaction/init.js
+++ dev-packages/browser-integration-tests/suites/public-api/startTransaction/init.js
@@ -2,6 +2,8 @@ import * as Sentry from '@sentry/browser';
 
 window.Sentry = Sentry;
 
+Sentry.addTracingExtensions();
+
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   tracesSampleRate: 1.0,
