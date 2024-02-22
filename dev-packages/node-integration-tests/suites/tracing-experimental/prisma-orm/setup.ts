4c63decf8f293f12ad0dbf631dcec30efd475394
Onur Temizkan <onur@narval.co.uk>
2024-02-22 10:50:00 +0000

    Add comment about minimum node version.

--- dev-packages/node-integration-tests/suites/tracing-experimental/prisma-orm/setup.ts
+++ dev-packages/node-integration-tests/suites/tracing-experimental/prisma-orm/setup.ts
@@ -3,6 +3,8 @@ import { parseSemver } from '@sentry/utils';
 
 const NODE_VERSION = parseSemver(process.versions.node);
 
+// Prisma v5 requires Node.js v16+
+// https://www.prisma.io/docs/orm/more/upgrade-guides/upgrading-versions/upgrading-to-prisma-5#nodejs-minimum-version-change
 if (NODE_VERSION.major && NODE_VERSION.major < 16) {
   // eslint-disable-next-line no-console
   console.warn(`Skipping Prisma tests on Node: ${NODE_VERSION.major}`);
