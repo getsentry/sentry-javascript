7b9dafa5de692f224d72ce39d5b1a109ccd68529
Onur Temizkan <onur@narval.co.uk>
2024-02-23 13:27:23 +0000

    Remove `BrowserTracing`

--- dev-packages/e2e-tests/test-applications/create-remix-app-express-vite-dev/app/entry.client.tsx
+++ dev-packages/e2e-tests/test-applications/create-remix-app-express-vite-dev/app/entry.client.tsx
@@ -7,8 +7,10 @@ Sentry.init({
   environment: 'qa', // dynamic sampling bias to keep transactions
   dsn: window.ENV.SENTRY_DSN,
   integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.remixRouterInstrumentation(useEffect, useLocation, useMatches),
+    Sentry.browserTracingIntegration({
+      useEffect,
+      useLocation,
+      useMatches,
     }),
     new Sentry.Replay(),
   ],
