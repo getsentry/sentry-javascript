3cb5108fe3d228e2d4253fe4f33402db090c2c77
Billy Vong <billyvg@users.noreply.github.com>
2024-02-29 15:21:09 -0330

    feat(feedback): Flush replays when feedback form opens (#10567)
    Flush replay when the feedback form is first opened instead of at submit
    time
    
    We are making this change because we have noticed a lot of feedback
    replays only consist of the user submitting the feedback and not what
    they did prior to submitting feedback. This may result in false
    positives if users open but do not submit feedback, but this should make
    replays from feedback more useful.

--- packages/feedback/src/widget/createWidget.ts
+++ packages/feedback/src/widget/createWidget.ts
@@ -1,4 +1,4 @@
-import { getCurrentScope } from '@sentry/core';
+import { getClient, getCurrentScope } from '@sentry/core';
 import { logger } from '@sentry/utils';
 
 import type { FeedbackFormData, FeedbackInternalOptions, FeedbackWidget } from '../types';
@@ -8,6 +8,8 @@ import { Actor } from './Actor';
 import type { DialogComponent } from './Dialog';
 import { Dialog } from './Dialog';
 import { SuccessMessage } from './SuccessMessage';
+
+import { DEBUG_BUILD } from '../debug-build';
 
 interface CreateWidgetParams {
   /**
@@ -125,6 +127,21 @@ export function createWidget({
   }
 
   /**
+   * Internal handler when dialog is opened
+   */
+  function handleOpenDialog(): void {
+    // Flush replay if integration exists
+    const client = getClient();
+    const replay = client && client.getIntegrationByName<{ name: string; flush: () => Promise<void> }>('Replay');
+    if (!replay) {
+      return;
+    }
+    replay.flush().catch(err => {
+      DEBUG_BUILD && logger.error(err);
+    });
+  }
+
+  /**
    * Displays the default actor
    */
   function showActor(): void {
@@ -156,6 +173,7 @@ export function createWidget({
         if (options.onFormOpen) {
           options.onFormOpen();
         }
+        handleOpenDialog();
         return;
       }
 
@@ -208,6 +226,7 @@ export function createWidget({
       if (options.onFormOpen) {
         options.onFormOpen();
       }
+      handleOpenDialog();
     } catch (err) {
       // TODO: Error handling?
       logger.error(err);
