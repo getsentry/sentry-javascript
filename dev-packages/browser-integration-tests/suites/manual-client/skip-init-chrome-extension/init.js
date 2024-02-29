784b485020b91aac4f2ac0160ae6df27d91304d9
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-02-29 14:08:02 +0100

    feat(browser): Prevent initialization in browser extensions (#10844)
    Prevents initialization inside chrome.* and browser.* extension
    environments.
    Also refactored init() in browser because of eslint warning about too
    much complexity.
    Fixes https://github.com/getsentry/sentry-javascript/issues/10632

---
+++ dev-packages/browser-integration-tests/suites/manual-client/skip-init-chrome-extension/init.js
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/browser';
+
+window.Sentry = Sentry;
+
+// We mock this here to simulate a Chrome browser extension
+window.chrome = { runtime: { id: 'mock-extension-id' } };
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+});
