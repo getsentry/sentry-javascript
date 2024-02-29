8c5ebc06d454204aa4185a6574ca43df9576f7d6
s1gr1d <sigrid.huemer@posteo.at>
2024-02-29 11:29:50 +0100

    fix tests

--- packages/astro/src/client/sdk.ts
+++ packages/astro/src/client/sdk.ts
@@ -30,13 +30,15 @@ export function init(options: BrowserOptions): void {
 }
 
 function getDefaultIntegrations(options: BrowserOptions): Integration[] | undefined {
+  const defaultIntegrations = [...getBrowserDefaultIntegrations(options)];
+
   // This evaluates to true unless __SENTRY_TRACING__ is text-replaced with "false",
   // in which case everything inside will get treeshaken away
   if (typeof __SENTRY_TRACING__ === 'undefined' || __SENTRY_TRACING__) {
     if (hasTracingEnabled(options)) {
-      return [...getBrowserDefaultIntegrations(options), browserTracingIntegration()];
+      return [...defaultIntegrations, browserTracingIntegration()];
     }
   }
 
-  return undefined;
+  return defaultIntegrations;
 }
