30e9c405e20f9841c277cce16c4f3ebfee8dac79
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-02-29 00:56:58 -0500

    wip: cropping box follows mouse, need to remove screenshot editor & help  once submitting works

--- packages/feedback-screenshot/src/screenshotButton.tsx
+++ packages/feedback-screenshot/src/screenshotButton.tsx
@@ -2,7 +2,7 @@ import { h, render } from 'preact';
 import { useState, useCallback, useEffect } from 'preact/hooks';
 import { useTakeScreenshot } from './useTakeScreenshot';
 import type { VNode } from 'preact';
-import { ScreenshotWidget } from './screenshotEditor';
+import { ScreenshotWidget } from './screenshotWidget';
 
 type Props = {
   croppingRef: HTMLDivElement;
---
+++ packages/feedback-screenshot/src/screenshotWidget.tsx
@@ -0,0 +1,207 @@
+import type { VNode } from 'preact';
+import { useCallback, useEffect, useRef, useState } from 'preact/hooks';
+import { GLOBAL_OBJ } from '@sentry/utils';
+import { h } from 'preact';
+import { once } from 'events';
+
+// exporting a separate copy of `WINDOW` rather than exporting the one from `@sentry/browser`
+// prevents the browser package from being bundled in the CDN bundle, and avoids a
+// circular dependency between the browser and feedback packages
+export const WINDOW = GLOBAL_OBJ as typeof GLOBAL_OBJ & Window;
+
+type Props = {
+  screenshotImage: HTMLCanvasElement | null;
+  setScreenshotImage: (screenshot: HTMLCanvasElement | null) => void;
+};
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Rect {
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+}
+
+const constructRect = (start: Point, end: Point) => {
+  return {
+    x: Math.min(start.x, end.x),
+    y: Math.min(start.y, end.y),
+    width: Math.abs(start.x - end.x),
+    height: Math.abs(start.y - end.y),
+  };
+};
+
+const getCanvasRenderSize = (width: number, height: number) => {
+  const maxWidth = WINDOW.innerWidth;
+  const maxHeight = WINDOW.innerHeight;
+
+  if (width > maxWidth) {
+    height = (maxWidth / width) * height;
+    width = maxWidth;
+  }
+
+  if (height > maxHeight) {
+    width = (maxHeight / height) * width;
+    height = maxHeight;
+  }
+
+  return { width, height };
+};
+
+export function ScreenshotWidget({ screenshotImage, setScreenshotImage }: Props): VNode | null {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const currentRatio = useRef<number>(1);
+  const [rectStart, setRectStart] = useState({ x: 0, y: 0 });
+  const [rectEnd, setRectEnd] = useState({ x: 0, y: 0 });
+  const [confirmCrop, setConfirmCrop] = useState(false);
+  const [crop, setCrop] = useState<Rect>({ x: 0, y: 0, width: 0, height: 0 });
+  const imageRef = useRef<HTMLCanvasElement>(null);
+  const renderSize = getCanvasRenderSize(screenshotImage?.width ?? 0, screenshotImage?.height ?? 0);
+
+  useEffect(() => {
+    const imageCanvas = imageRef.current;
+    const ctx = imageCanvas?.getContext('2d');
+    const img = new Image();
+    if (screenshotImage) {
+      img.src = screenshotImage.toDataURL();
+    }
+    img.onload = () => {
+      if (imageCanvas && ctx) {
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+        setCanvasSize(imageCanvas);
+        ctx.drawImage(img, 0, 0);
+      }
+    };
+  }, [screenshotImage]);
+
+  function setCanvasSize(canvas: HTMLCanvasElement) {
+    if (screenshotImage) {
+      canvas.style.width = `${renderSize.width}px`;
+      canvas.style.height = `${renderSize.height}px`;
+      canvas.style.top = `${(WINDOW.innerHeight - renderSize.height) / 2}px`;
+      canvas.style.left = `${(WINDOW.innerWidth - renderSize.width) / 2}px`;
+
+      setRectEnd({ x: renderSize.width, y: renderSize.height });
+    }
+  }
+
+  function refreshCanvas() {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext('2d');
+    if (canvas && ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      const rect = constructRect(rectStart, rectEnd);
+      setCrop(rect);
+
+      // draw gray overlay around the selection
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+      ctx.fillRect(0, 0, canvas.width, rect.y);
+      ctx.fillRect(0, rect.y, rect.x, rect.height);
+      ctx.fillRect(rect.x + rect.width, rect.y, canvas.width, rect.height);
+      ctx.fillRect(0, rect.y + rect.height, canvas.width, canvas.height);
+
+      // draw selection border
+      ctx.strokeStyle = '#ff0000';
+      ctx.lineWidth = 6;
+      ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    }
+  }
+
+  const handleMouseMove = useCallback((e: { offsetX: number; offsetY: number }) => {
+    console.log('move');
+    setRectStart({
+      x: Math.floor(e.offsetX / currentRatio.current),
+      y: Math.floor(e.offsetY / currentRatio.current),
+    });
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    console.log('up');
+    canvasRef.current?.removeEventListener('mousemove', handleMouseMove);
+    canvasRef.current?.removeEventListener('click', handleMouseUp);
+    setConfirmCrop(true);
+  }, []);
+
+  function onGrabButton() {
+    console.log('down');
+    setConfirmCrop(false);
+    canvasRef.current?.addEventListener('click', handleMouseUp);
+    canvasRef.current?.addEventListener('mousemove', handleMouseMove);
+  }
+
+  // canvasRef.current?.addEventListener('mousedown', onGrabButton);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.width = renderSize.width;
+      canvas.height = renderSize.height;
+      setCanvasSize(canvas);
+    }
+  }, [screenshotImage]);
+
+  useEffect(() => {
+    refreshCanvas();
+  }, [rectStart, rectEnd]);
+
+  function submit(rect?: Rect) {
+    const canvas = imageRef.current;
+    if (!rect) {
+      setScreenshotImage(canvas);
+      return;
+    }
+    // eslint-disable-next-line no-restricted-globals
+    const cutoutCanvas = document.createElement('canvas');
+    cutoutCanvas.width = rect.width;
+    cutoutCanvas.height = rect.height;
+    const cutoutCtx = cutoutCanvas.getContext('2d');
+    if (cutoutCtx && canvas) {
+      cutoutCtx.drawImage(canvas, rect.x, rect.y, rect.width, rect.height, 0, 0, rect.width, rect.height);
+    }
+
+    setScreenshotImage(cutoutCanvas);
+    screenshotImage = cutoutCanvas;
+  }
+
+  return (
+    <div style="padding-right: 16px; position:relative;">
+      <canvas ref={imageRef}></canvas>
+      <canvas style={{ position: 'absolute' }} ref={canvasRef}></canvas>
+      <button
+        style={{ width: '32px', position: 'absolute', left: rectStart.x, top: rectStart.y }}
+        onMouseDown={e => {
+          e.preventDefault();
+          onGrabButton();
+        }}
+        onClick={e => {
+          e.preventDefault();
+          handleMouseUp();
+        }}
+      ></button>
+      <button style={{ position: 'absolute', left: rectStart.x, top: rectEnd.y }}></button>
+      <button style={{ position: 'absolute', left: rectEnd.x, top: rectStart.y }}></button>
+      <button style={{ position: 'absolute', left: rectEnd.x, top: rectEnd.y }}></button>
+      <button
+        style={{
+          width: '100px',
+          height: '100px',
+          background: 'purple',
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          display: confirmCrop ? 'inline' : 'none',
+        }}
+        onClick={e => {
+          e.preventDefault();
+          submit(crop);
+        }}
+      ></button>
+    </div>
+  );
+}
