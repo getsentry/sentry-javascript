c502b64e0d432825a01482ba6a647887b6fb1de1
Francesco Novy <francesco.novy@sentry.io>
2024-03-05 10:24:18 +0000

    ref: Refactor some deprecated `startSpan` options (#10825)
    Mostly, changes `origin` to be set as an attribute, plus some further
    small adjustments.
    
    In the case of bun, I also changed it to keep the request on the
    isolation scope (vs. the span).

--- packages/tracing-internal/src/browser/browserTracingIntegration.ts
+++ packages/tracing-internal/src/browser/browserTracingIntegration.ts
@@ -1,4 +1,5 @@
 import type { IdleTransaction } from '@sentry/core';
+import { SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN } from '@sentry/core';
 import { getActiveSpan } from '@sentry/core';
 import { getCurrentHub } from '@sentry/core';
 import {
@@ -232,7 +233,7 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
       };
     }
 
-    const finalContext = beforeStartSpan ? beforeStartSpan(expandedContext) : expandedContext;
+    const finalContext: TransactionContext = beforeStartSpan ? beforeStartSpan(expandedContext) : expandedContext;
 
     // If `beforeStartSpan` set a custom name, record that fact
     finalContext.attributes =
@@ -323,10 +324,10 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
         const context: StartSpanOptions = {
           name: WINDOW.location.pathname,
           // pageload should always start at timeOrigin (and needs to be in s, not ms)
-          startTimestamp: browserPerformanceTimeOrigin ? browserPerformanceTimeOrigin / 1000 : undefined,
-          origin: 'auto.pageload.browser',
+          startTime: browserPerformanceTimeOrigin ? browserPerformanceTimeOrigin / 1000 : undefined,
           attributes: {
             [SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'url',
+            [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.pageload.browser',
           },
         };
         startBrowserTracingPageLoadSpan(client, context);
@@ -352,9 +353,9 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
             startingUrl = undefined;
             const context: StartSpanOptions = {
               name: WINDOW.location.pathname,
-              origin: 'auto.navigation.browser',
               attributes: {
                 [SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'url',
+                [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.navigation.browser',
               },
             };
 
