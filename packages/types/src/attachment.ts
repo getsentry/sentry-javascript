9b4c8559bba8a7c61a075fe8c146c7d9a79ea83d
Tim Fish <tim@timfish.uk>
2024-03-06 18:05:02 +0000

    fix(types): Add `AttachmentType` and use for envelope `attachment_type` property (#10946)
    `Attachment.attachmentType` was changed to use a string union. The
    `attachment_type` header property should match so I added an extra type
    for this.

--- packages/types/src/attachment.ts
+++ packages/types/src/attachment.ts
@@ -1,6 +1,23 @@
+export type AttachmentType =
+  | 'event.attachment'
+  | 'event.minidump'
+  | 'event.applecrashreport'
+  | 'unreal.context'
+  | 'unreal.logs';
+
+/**
+ * An attachment to an event. This is used to upload arbitrary data to Sentry.
+ *
+ * Please take care to not add sensitive information in attachments.
+ *
+ * https://develop.sentry.dev/sdk/envelopes/#attachment
+ */
 export interface Attachment {
   data: string | Uint8Array;
   filename: string;
   contentType?: string;
-  attachmentType?: string;
+  /**
+   * The type of the attachment. Defaults to `event.attachment` if not specified.
+   */
+  attachmentType?: AttachmentType;
 }
