546f9b635aee7c4dc8e12648d6c70ee8abf949f2
Francesco Novy <francesco.novy@sentry.io>
2024-03-07 17:14:50 +0000

    use `node:https` instead of `https` import

--- packages/node-experimental/src/transports/http.ts
+++ packages/node-experimental/src/transports/http.ts
@@ -1,5 +1,5 @@
-import * as http from 'http';
-import * as https from 'https';
+import * as http from 'node:http';
+import * as https from 'node:https';
 import { Readable } from 'stream';
 import { createGzip } from 'zlib';
 import { createTransport } from '@sentry/core';
