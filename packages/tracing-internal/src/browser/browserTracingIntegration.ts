6ef628d45ae971d738f62936d8c7a59cbe0ab8c9
Lukas Stracke <lukas.stracke@sentry.io>
2024-03-08 16:54:54 +0100

    ref(browser): Update `scope.transactionName` on pageload and navigation span creation

--- packages/tracing-internal/src/browser/browserTracingIntegration.ts
+++ packages/tracing-internal/src/browser/browserTracingIntegration.ts
@@ -364,6 +364,8 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
 export function startBrowserTracingPageLoadSpan(client: Client, spanOptions: StartSpanOptions): Span | undefined {
   client.emit('startPageLoadSpan', spanOptions);
 
+  getCurrentScope().setTransactionName(spanOptions.name);
+
   const span = getActiveSpan();
   const op = span && spanToJSON(span).op;
   return op === 'pageload' ? span : undefined;
@@ -375,6 +377,8 @@ export function startBrowserTracingPageLoadSpan(client: Client, spanOptions: Sta
  */
 export function startBrowserTracingNavigationSpan(client: Client, spanOptions: StartSpanOptions): Span | undefined {
   client.emit('startNavigationSpan', spanOptions);
+
+  getCurrentScope().setTransactionName(spanOptions.name);
 
   const span = getActiveSpan();
   const op = span && spanToJSON(span).op;
