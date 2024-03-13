924d7403c002743e8c85ec1df7c575cf3a317033
Abhijeet Prasad <aprasad@sentry.io>
2024-03-12 22:50:47 -0400

    use json plugin

--- packages/profiling-node/rollup.npm.config.mjs
+++ packages/profiling-node/rollup.npm.config.mjs
@@ -2,11 +2,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { makeBaseNPMConfig } from '@sentry-internal/rollup-utils';
+import { makeJsonPlugin } from '@sentry-internal/rollup-utils/plugins/index.mjs';
 
 export default makeBaseNPMConfig({
   packageSpecificConfig: {
     input: 'src/index.ts',
     output: { file: 'lib/index.js', format: 'cjs', dir: undefined, preserveModules: false },
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+    plugins: [resolve(), makeJsonPlugin(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
   },
 });
