52beb2ff1a54aa2296a8ad24f3f8f3843ed3784a
Francesco Novy <francesco.novy@sentry.io>
2024-03-13 15:27:49 +0000

    test(remix): Update Remix E2E tests
    This does two things:
    
    1. Remove an unused remix integration test
    2. Add a test that ensures we send correct server & browser transactions that are correctly linked

--- dev-packages/e2e-tests/test-applications/create-remix-app-express-vite-dev/app/entry.server.tsx
+++ dev-packages/e2e-tests/test-applications/create-remix-app-express-vite-dev/app/entry.server.tsx
@@ -17,6 +17,7 @@ Sentry.init({
   dsn: process.env.E2E_TEST_DSN,
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  tunnel: 'http://localhost:3031/', // proxy server
 });
 
 export const handleError = Sentry.wrapRemixHandleError;
