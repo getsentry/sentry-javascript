49460a4a325e284dcd7ea49c827f771d4d664f9e
s1gr1d <sigrid.huemer@posteo.at>
2024-03-14 10:22:27 +0100

    add missing await

--- dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/async-spans/test.ts
+++ dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/async-spans/test.ts
@@ -11,10 +11,11 @@ sentryTest(
   'async spans with different durations lead to unexpected behavior in browser (no "asynchronous context tracking")',
   async ({ getLocalTestPath, page }) => {
     const url = await getLocalTestPath({ testDir: __dirname });
-    page.goto(url);
+    await page.goto(url);
 
     await page.waitForFunction(
       () =>
+        typeof window !== 'undefined' &&
         (window as unknown as WindowWithSpan).firstWaitingSpan &&
         (window as unknown as WindowWithSpan).secondWaitingSpan &&
         (window as unknown as WindowWithSpan).thirdWaitingSpan,
