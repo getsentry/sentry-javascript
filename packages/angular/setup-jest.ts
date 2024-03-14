e88816ae7024993cf5398920a9add5aba5c3cdf4
Lukas Stracke <lukas.stracke@sentry.io>
2024-03-14 12:18:34 +0100

    upgrade angular 11

--- packages/angular/setup-jest.ts
+++ packages/angular/setup-jest.ts
@@ -1,4 +1,5 @@
 import 'zone.js';
+import 'zone.js/testing';
 
 import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
