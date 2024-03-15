b8c75f4ba8a6948252902b0bd8839ba0568dcf84
Tim Fish <tim@timfish.uk>
2024-03-15 14:54:18 +0000

    feat(node): Remove deprecated/duplicate/unused definitions (#11120)
    - Removes `makeMain` impl and exports from node (and downstream SDKs)
    - Removes custom `getCurrentHub/getClient` impl in favour of core
    - Removes `callExtensionMethod` as its not called from anywhere
    - Fixes all the tests that relied on `getClient` never returning
    undefined
    - Removes `_callExtensionMethod` from core as it's not used

--- packages/core/src/hub.ts
+++ packages/core/src/hub.ts
@@ -530,20 +530,6 @@ export class Hub implements HubInterface {
       client.captureSession(session);
     }
   }
-
-  /**
-   * Calls global extension method and binding current instance to the function call
-   */
-  // @ts-expect-error Function lacks ending return statement and return type does not include 'undefined'. ts(2366)
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private _callExtensionMethod<T>(method: string, ...args: any[]): T {
-    const carrier = getMainCarrier();
-    const sentry = getSentryCarrier(carrier);
-    if (sentry.extensions && typeof sentry.extensions[method] === 'function') {
-      return sentry.extensions[method].apply(this, args);
-    }
-    DEBUG_BUILD && logger.warn(`Extension method ${method} couldn't be found, doing nothing.`);
-  }
 }
 
 /**
