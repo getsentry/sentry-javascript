558ec88168b88143ecaa4ba3ab1b9908b2dce59e
Francesco Novy <francesco.novy@sentry.io>
2024-03-15 11:03:37 +0000

    use side effects from original package.json

--- dev-packages/rollup-utils/plugins/make-esm-plugin.mjs
+++ dev-packages/rollup-utils/plugins/make-esm-plugin.mjs
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 /**
  * Outputs a package.json file with {type: module} in the root of the output directory so that Node
  * treats .js files as ESM.
@@ -5,11 +7,24 @@
 export function makePackageNodeEsm() {
   return {
     name: 'make-package-node-esm',
-    generateBundle() {
+    async generateBundle() {
+      // We need to keep the `sideEffects` value from the original package.json,
+      // as e.g. webpack seems to depend on this
+      // without this, tree shaking does not work as expected
+      const packageJSONPath = (await this.resolve('package.json')).id;
+
+      const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'));
+      const sideEffects = packageJSON.sideEffects;
+
+      const newPackageJSON = {
+        type: 'module',
+        sideEffects,
+      };
+
       this.emitFile({
         type: 'asset',
         fileName: 'package.json',
-        source: '{ "type": "module", "sideEffects": false }',
+        source: JSON.stringify(newPackageJSON),
       });
     },
   };
