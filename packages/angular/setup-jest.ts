b5ffacb11d455fa7c3b48cd487cb8a296530f94c
s1gr1d <sigrid.huemer@posteo.at>
2024-03-15 14:43:56 +0100

    upgrade angular 13

--- packages/angular/setup-jest.ts
+++ packages/angular/setup-jest.ts
@@ -1,8 +1,8 @@
 import 'zone.js';
 import 'zone.js/testing';
 
-import { TestBed } from '@angular/core/testing';
+import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-TestBed.resetTestEnvironment();
-TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().resetTestEnvironment();
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
