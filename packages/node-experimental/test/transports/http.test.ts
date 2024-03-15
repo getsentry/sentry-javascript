1acf54f819bb56ed91f66928d6d7d8b7905dec79
Luca Forstner <luca.forstner@sentry.io>
2024-03-15 11:50:36 +0100

    feat(nextjs): Bump minimum required Next.js version to `13.2.0` (#11097)

--- packages/node-experimental/test/transports/http.test.ts
+++ packages/node-experimental/test/transports/http.test.ts
@@ -53,14 +53,14 @@ function setupTestServer(
     res.connection?.end();
   });
 
-  testServer.listen(18100);
+  testServer.listen(18101);
 
   return new Promise(resolve => {
     testServer?.on('listening', resolve);
   });
 }
 
-const TEST_SERVER_URL = 'http://localhost:18100';
+const TEST_SERVER_URL = 'http://localhost:18101';
 
 const EVENT_ENVELOPE = createEnvelope<EventEnvelope>({ event_id: 'aa3ff046696b4bc6b609ce6d28fde9e2', sent_at: '123' }, [
   [{ type: 'event' }, { event_id: 'aa3ff046696b4bc6b609ce6d28fde9e2' }] as EventItem,
