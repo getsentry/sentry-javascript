c6a4764e8a65dddb51ea4c9a96632892569aae49
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-03-16 18:34:38 -0400

    fix smallest crop & lint

--- packages/feedback/src/screenshot/components/ScreenshotEditor.tsx
+++ packages/feedback/src/screenshot/components/ScreenshotEditor.tsx
@@ -129,35 +129,39 @@ export function makeScreenshotEditorComponent({ h, imageBuffer, dialog }: Factor
 
     const makeHandleMouseMove = useCallback((corner: string) => {
       return function (e: MouseEvent) {
-        const mouseX = e.clientX - croppingRef.current!.getBoundingClientRect().x;
-        const mouseY = e.clientY - croppingRef.current!.getBoundingClientRect().y;
+        if (!croppingRef.current) {
+          return;
+        }
+        const cropCanvas = croppingRef.current;
+        const mouseX = e.clientX - cropCanvas.getBoundingClientRect().x;
+        const mouseY = e.clientY - cropCanvas.getBoundingClientRect().y;
         switch (corner) {
           case 'topleft':
             setCroppingRect(prev => ({
               ...prev,
-              startx: Math.min(Math.max(0, mouseX), prev.endx - 30),
-              starty: Math.min(Math.max(0, mouseY), prev.endy - 30),
+              startx: Math.min(Math.max(0, mouseX), prev.endx - 33),
+              starty: Math.min(Math.max(0, mouseY), prev.endy - 33),
             }));
             break;
           case 'topright':
             setCroppingRect(prev => ({
               ...prev,
-              endx: Math.max(Math.min(mouseX, croppingRef.current!.width), prev.startx + 30),
-              starty: Math.min(Math.max(0, mouseY), prev.endy - 30),
+              endx: Math.max(Math.min(mouseX, cropCanvas.width), prev.startx + 33),
+              starty: Math.min(Math.max(0, mouseY), prev.endy - 33),
             }));
             break;
           case 'bottomleft':
             setCroppingRect(prev => ({
               ...prev,
-              startx: Math.min(Math.max(0, mouseX), prev.endx - 30),
-              endy: Math.max(Math.min(mouseY, croppingRef.current!.height), prev.starty + 30),
+              startx: Math.min(Math.max(0, mouseX), prev.endx - 33),
+              endy: Math.max(Math.min(mouseY, cropCanvas.height), prev.starty + 33),
             }));
             break;
           case 'bottomright':
             setCroppingRect(prev => ({
               ...prev,
-              endx: Math.max(Math.min(mouseX, croppingRef.current!.width), prev.startx + 30),
-              endy: Math.max(Math.min(mouseY, croppingRef.current!.height), prev.starty + 30),
+              endx: Math.max(Math.min(mouseX, cropCanvas.width), prev.startx + 33),
+              endy: Math.max(Math.min(mouseY, cropCanvas.height), prev.starty + 33),
             }));
             break;
         }
