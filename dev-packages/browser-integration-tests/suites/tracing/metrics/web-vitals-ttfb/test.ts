e3551163831866f50a80831949cdc981ddae7785
Abhijeet Prasad <aprasad@sentry.io>
2024-03-18 18:32:14 -0400

    better playwright test

--- dev-packages/browser-integration-tests/suites/tracing/metrics/web-vitals-ttfb/test.ts
+++ dev-packages/browser-integration-tests/suites/tracing/metrics/web-vitals-ttfb/test.ts
@@ -9,12 +9,19 @@ sentryTest('should capture TTFB vital.', async ({ getLocalTestPath, page }) => {
     sentryTest.skip();
   }
 
+  page.route('**', async (route) => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    route.continue()
+  });
+
   const url = await getLocalTestPath({ testDir: __dirname });
   const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
 
   expect(eventData.measurements).toBeDefined();
   expect(eventData.measurements?.ttfb?.value).toBeDefined();
+  // ttfb should be greater than 0 because of the `page.route` delay. We don't want to test
+  // the exact value here because it's not deterministic.
   expect(eventData.measurements?.ttfb?.value).toBeGreaterThan(0);
+
   expect(eventData.measurements?.['ttfb.requestTime']?.value).toBeDefined();
-  expect(eventData.measurements?.['ttfb.requestTime']?.value).toBeGreaterThan(0);
 });
