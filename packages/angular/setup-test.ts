d25e035a64fb7c1f4190d744b9b0e5044034bcec
s1gr1d <sigrid.huemer@posteo.at>
2024-03-18 13:29:46 +0100

    upgrade angular 13 - use vitest

--- packages/angular/setup-test.ts
+++ packages/angular/setup-test.ts
@@ -1,5 +1,4 @@
-import 'zone.js';
-import 'zone.js/testing';
+import './patch-vitest';
 
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
