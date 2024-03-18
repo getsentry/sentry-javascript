f415ea084ab04dee64f1b485e0ce55f4b2db1d32
Edward Gou <edward.gou@sentry.io>
2024-03-18 18:21:14 -0400

    biome

--- dev-packages/browser-integration-tests/suites/tracing/browsertracing/interactions/assets/script.js
+++ dev-packages/browser-integration-tests/suites/tracing/browsertracing/interactions/assets/script.js
@@ -1,19 +1,17 @@
-const createDelayFunction =
-  (delay = 70) =>
-  e => {
-    const startTime = Date.now();
+const createDelayFunction = (delay = 70) => e => {
+  const startTime = Date.now();
 
-    function getElasped() {
-      const time = Date.now();
-      return time - startTime;
-    }
+  function getElasped() {
+    const time = Date.now();
+    return time - startTime;
+  }
 
-    while (getElasped() < delay) {
-      //
-    }
+  while (getElasped() < delay) {
+    //
+  }
 
-    e.target.classList.add('clicked');
-  };
+  e.target.classList.add('clicked');
+};
 
 document.querySelector('[data-test-id=interaction-button]').addEventListener('click', createDelayFunction());
 document.querySelector('[data-test-id=annotated-button]').addEventListener('click', createDelayFunction());
