b22ad78d2d77fca45694a69a5cc971e7ae00de7f
Abhijeet Prasad <aprasad@sentry.io>
2024-03-19 10:21:14 -0400

    use typeof fix

--- packages/nextjs/src/server/httpIntegration.ts
+++ packages/nextjs/src/server/httpIntegration.ts
@@ -1,12 +1,5 @@
-import { defineIntegration } from '@sentry/core';
 import { httpIntegration as originalHttpIntegration } from '@sentry/node-experimental';
-import type { IntegrationFn } from '@sentry/types';
 
-export const customHttpIntegration = ((options?: Parameters<typeof originalHttpIntegration>[0]) => {
-  return originalHttpIntegration({
-    ...options,
-    tracing: true,
-  });
-}) satisfies IntegrationFn;
-
-export const httpIntegration = defineIntegration(customHttpIntegration);
+export const httpIntegration: typeof originalHttpIntegration = options => {
+  return originalHttpIntegration({ ...options, tracing: true });
+};
--- packages/nextjs/src/server/onUncaughtExceptionIntegration.ts
+++ packages/nextjs/src/server/onUncaughtExceptionIntegration.ts
@@ -1,12 +1,5 @@
-import { defineIntegration } from '@sentry/core';
-import { onUncaughtExceptionIntegration as originalOnUncaughtExceptionIntegration } from '@sentry/node-experimental';
-import type { IntegrationFn } from '@sentry/types';
+import { onUncaughtExceptionIntegration as originalOnUncaughtExceptionIntegration } from '@sentry/node';
 
-export const customOnUncaughtException = ((options?: Parameters<typeof originalOnUncaughtExceptionIntegration>[0]) => {
-  return originalOnUncaughtExceptionIntegration({
-    exitEvenIfOtherHandlersAreRegistered: false,
-    ...options,
-  });
-}) satisfies IntegrationFn;
-
-export const onUncaughtExceptionIntegration = defineIntegration(customOnUncaughtException);
+export const onUncaughtExceptionIntegration: typeof originalOnUncaughtExceptionIntegration = options => {
+  return originalOnUncaughtExceptionIntegration({ ...options, exitEvenIfOtherHandlersAreRegistered: false });
+};
