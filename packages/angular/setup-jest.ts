9a3aa0fdd5009a9c77facf57a2754ea02da3861f
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-03-19 15:23:05 +0100

    feat(v8/angular): Merge angular and angular-ivy packages and start Angular support at v14 (#11091)
    Start using vitest for better ESM support (since Angular 13)
    closes https://github.com/getsentry/sentry-javascript/issues/9830
    
    ---------
    
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>

--- packages/angular/setup-jest.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js';
-
-import { TestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-TestBed.resetTestEnvironment();
-TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
