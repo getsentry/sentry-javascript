3d6ff72eb6de5e987de294d1b458aa09c4eaf447
Tim Fish <tim@timfish.uk>
2024-03-20 12:25:53 +0000

    clean up types

--- packages/browser/src/transports/types.ts
+++ packages/browser/src/transports/types.ts
@@ -1,10 +1,6 @@
 import type { BaseTransportOptions } from '@sentry/types';
 
-import type { BrowserOfflineTransportOptions } from './offline';
-
-type BaseTransportAndOfflineTransportOptions = BaseTransportOptions & BrowserOfflineTransportOptions;
-
-export interface BrowserTransportOptions extends BaseTransportAndOfflineTransportOptions {
+export interface BrowserTransportOptions extends BaseTransportOptions {
   /** Fetch API init parameters. Used by the FetchTransport */
   fetchOptions?: RequestInit;
   /** Custom headers for the transport. Used by the XHRTransport and FetchTransport */
