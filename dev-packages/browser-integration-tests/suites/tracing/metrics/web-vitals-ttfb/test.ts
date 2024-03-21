64fb9ce2a3df553479efb50f43dd6b38ba5759b6
Abhijeet Prasad <aprasad@sentry.io>
2024-03-21 12:49:18 -0400

    nvm on test changes

--- dev-packages/browser-integration-tests/suites/tracing/metrics/web-vitals-ttfb/test.ts
+++ dev-packages/browser-integration-tests/suites/tracing/metrics/web-vitals-ttfb/test.ts
@@ -10,24 +10,10 @@ sentryTest('should capture TTFB vital.', async ({ getLocalTestPath, page }) => {
     sentryTest.skip();
   }
 
-  page.route('**', async route => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    route.continue();
-  });
-  page.route('**/path/to/image.png', async (route: Route) => {
-    // Simulate a delay in the response to make sure we capture the TTFB vital.
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    return route.fulfill({ path: `${__dirname}/assets/sentry-logo-600x179.png` });
-  });
-
   const url = await getLocalTestPath({ testDir: __dirname });
   const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
 
   expect(eventData.measurements).toBeDefined();
   expect(eventData.measurements?.ttfb?.value).toBeDefined();
-  // ttfb should be greater than 0 because of the `page.route` delay. We don't want to test
-  // the exact value here because it's not deterministic.
-  expect(eventData.measurements?.ttfb?.value).toBeGreaterThan(0);
-
   expect(eventData.measurements?.['ttfb.requestTime']?.value).toBeDefined();
 });
