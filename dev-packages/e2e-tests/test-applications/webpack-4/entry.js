de0ebb690dccbe10fed2343d8b40cdb0ce33f149
Tim Fish <tim@timfish.uk>
2024-03-25 11:34:49 +0000

    Test browser tracing too

--- dev-packages/e2e-tests/test-applications/webpack-4/entry.js
+++ dev-packages/e2e-tests/test-applications/webpack-4/entry.js
@@ -1,10 +1,11 @@
-import { captureException, init } from '@sentry/browser';
+import { browserTracingIntegration, captureException, init } from '@sentry/browser';
 
 init({
   dsn: process.env.E2E_TEST_DSN,
+  integrations: [browserTracingIntegration()],
 });
 
 setTimeout(() => {
   const eventId = captureException(new Error('I am an error!'));
   window.capturedExceptionId = eventId;
-}, 200);
+}, 2000);
