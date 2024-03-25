45bd0a82a1b4aab417b0d5fafe8ae7b45139e11a
Francesco Novy <francesco.novy@sentry.io>
2024-03-25 11:13:49 +0100

    feat(node): Do not include `prismaIntegration` by default
    This seems to have issues in ESM builds (not just in astro, but also saw it in remix), so for now removing this from defaults - users can still add it manually if needed. We may find a better solution for this before v8 is stable.

--- packages/astro/src/server/sdk.ts
+++ packages/astro/src/server/sdk.ts
@@ -1,6 +1,5 @@
 import { applySdkMetadata } from '@sentry/core';
 import type { NodeOptions } from '@sentry/node';
-import { getDefaultIntegrations } from '@sentry/node';
 import { init as initNodeSdk, setTag } from '@sentry/node';
 
 /**
@@ -10,9 +9,6 @@ import { init as initNodeSdk, setTag } from '@sentry/node';
 export function init(options: NodeOptions): void {
   const opts = {
     ...options,
-    // TODO v8: For now, we disable the Prisma integration, because that has weird esm-cjs interop issues
-    // We should figure these out and fix these before v8 goes stable.
-    defaultIntegrations: getDefaultIntegrations(options).filter(integration => integration.name !== 'Prisma'),
   };
   applySdkMetadata(opts, 'astro', ['astro', 'node']);
 
