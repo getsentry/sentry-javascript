b262a70fc657a12e61fa5a8765c02f96b6b994b3
s1gr1d <sigrid.huemer@posteo.at>
2024-03-27 16:26:39 +0100

    review comments

--- packages/nextjs/src/common/wrapRouteHandlerWithSentry.ts
+++ packages/nextjs/src/common/wrapRouteHandlerWithSentry.ts
@@ -3,10 +3,10 @@ import {
   addTracingExtensions,
   captureException,
   getActiveSpan,
-  getIsolationScope,
   getRootSpan,
   handleCallbackErrors,
   setHttpStatus,
+  withIsolationScope,
 } from '@sentry/core';
 import { winterCGHeadersToDict } from '@sentry/utils';
 import { isNotFoundNavigationError, isRedirectNavigationError } from './nextNavigationErrorUtils';
@@ -29,50 +29,52 @@ export function wrapRouteHandlerWithSentry<F extends (...args: any[]) => any>(
 
   return new Proxy(routeHandler, {
     apply: async (originalFunction, thisArg, args) => {
-      getIsolationScope().setSDKProcessingMetadata({
-        request: {
-          headers: headers ? winterCGHeadersToDict(headers) : undefined,
-        },
-      });
-
-      try {
-        const activeSpan = getActiveSpan();
-        const rootSpan = activeSpan && getRootSpan(activeSpan);
-
-        const response: Response = await handleCallbackErrors(
-          () => originalFunction.apply(thisArg, args),
-          error => {
-            // Next.js throws errors when calling `redirect()`. We don't wanna report these.
-            if (isRedirectNavigationError(error)) {
-              // Don't do anything
-            } else if (isNotFoundNavigationError(error) && rootSpan) {
-              rootSpan.setStatus({ code: SPAN_STATUS_ERROR, message: 'not_found' });
-            } else {
-              captureException(error, {
-                mechanism: {
-                  handled: false,
-                },
-              });
-            }
+      return withIsolationScope(async isolationScope => {
+        isolationScope.setSDKProcessingMetadata({
+          request: {
+            headers: headers ? winterCGHeadersToDict(headers) : undefined,
           },
-        );
+        });
 
         try {
-          if (rootSpan && response.status) {
-            setHttpStatus(rootSpan, response.status);
-          }
-        } catch {
-          // best effort - response may be undefined?
-        }
+          const activeSpan = getActiveSpan();
+          const rootSpan = activeSpan && getRootSpan(activeSpan);
 
-        return response;
-      } finally {
-        if (!platformSupportsStreaming() || process.env.NEXT_RUNTIME === 'edge') {
-          // 1. Edge transport requires manual flushing
-          // 2. Lambdas require manual flushing to prevent execution freeze before the event is sent
-          await flushQueue();
+          const response: Response = await handleCallbackErrors(
+            () => originalFunction.apply(thisArg, args),
+            error => {
+              // Next.js throws errors when calling `redirect()`. We don't wanna report these.
+              if (isRedirectNavigationError(error)) {
+                // Don't do anything
+              } else if (isNotFoundNavigationError(error) && rootSpan) {
+                rootSpan.setStatus({ code: SPAN_STATUS_ERROR, message: 'not_found' });
+              } else {
+                captureException(error, {
+                  mechanism: {
+                    handled: false,
+                  },
+                });
+              }
+            },
+          );
+
+          try {
+            if (rootSpan && response.status) {
+              setHttpStatus(rootSpan, response.status);
+            }
+          } catch {
+            // best effort - response may be undefined?
+          }
+
+          return response;
+        } finally {
+          if (!platformSupportsStreaming() || process.env.NEXT_RUNTIME === 'edge') {
+            // 1. Edge transport requires manual flushing
+            // 2. Lambdas require manual flushing to prevent execution freeze before the event is sent
+            await flushQueue();
+          }
         }
-      }
+      });
     },
   });
 }
