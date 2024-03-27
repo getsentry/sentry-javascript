bf49286742047562c491016ca6d9fef6149d2e76
Francesco Novy <francesco.novy@sentry.io>
2024-03-27 12:15:08 +0100

    remove leftovers...

--- dev-packages/browser-integration-tests/suites/public-api/startSpan/circular_data/subject.js
+++ dev-packages/browser-integration-tests/suites/public-api/startSpan/circular_data/subject.js
@@ -3,5 +3,5 @@ const egg = { contains: chicken };
 chicken.lays = egg;
 
 Sentry.startSpan({ name: 'circular_object_test_transaction', data: { chicken } }, () => {
-  Sentry.startSpan({ op: 'circular_object_test_span', data: { chicken } }, () => undefined);
+  Sentry.startSpan({ op: 'circular_object_test_span', attributes: { chicken } }, () => undefined);
 });
