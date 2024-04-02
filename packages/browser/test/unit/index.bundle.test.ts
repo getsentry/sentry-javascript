26da63a9ffb720f2867113ab907e5693c6a05d43
Abhijeet Prasad <aprasad@sentry.io>
2024-04-02 11:02:06 -0400

    feat(v8): Remove all class based integrations (#11345)
    - Removes all class based integrations from `tracing-internal` and
    re-organizes exports
    - Removes top level `Integrations` export from browser package
    
    resolves https://github.com/getsentry/sentry-javascript/issues/8844

--- packages/browser/test/unit/index.bundle.test.ts
+++ packages/browser/test/unit/index.bundle.test.ts
@@ -4,10 +4,6 @@ import * as TracingBundle from '../../src/index.bundle';
 
 describe('index.bundle', () => {
   it('has correct exports', () => {
-    Object.keys(TracingBundle.Integrations).forEach(key => {
-      expect((TracingBundle.Integrations[key] as any).name).toStrictEqual(expect.any(String));
-    });
-
     expect(TracingBundle.replayIntegration).toBe(replayIntegrationShim);
     expect(TracingBundle.feedbackIntegration).toBe(feedbackIntegrationShim);
   });
--- packages/tracing-internal/src/node/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '../exports';
-
-export * from './integrations';
