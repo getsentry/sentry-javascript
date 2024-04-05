20f06095978880eda5f5ef1fa1c0f4eb28f4994a
Tim Fish <tim@timfish.uk>
2024-04-05 22:05:02 +0200

    another try

--- dev-packages/browser-integration-tests/suites/replay/captureReplayOffline/test.ts
+++ dev-packages/browser-integration-tests/suites/replay/captureReplayOffline/test.ts
@@ -8,19 +8,8 @@ sentryTest('should capture replays offline', async ({ getLocalTestPath, page }) 
     sentryTest.skip();
   }
 
-  const url = await getLocalTestPath({ testDir: __dirname });
-
-  // This would be the obvious way to test offline support but it doesn't appear to work!
-  // await context.setOffline(true);
-
-  // Abort the first envelope request so the event gets queued
-  await page.route(/ingest\.sentry\.io/, route => route.abort(), { times: 1 });
-
-  await new Promise(resolve => setTimeout(resolve, 2_000));
-
-  await page.goto(url);
-
-  await new Promise(resolve => setTimeout(resolve, 2_000));
+  const reqPromise0 = waitForReplayRequest(page, 0);
+  const reqPromise1 = waitForReplayRequest(page, 1);
 
   await page.route('https://dsn.ingest.sentry.io/**/*', route => {
     return route.fulfill({
@@ -30,13 +19,21 @@ sentryTest('should capture replays offline', async ({ getLocalTestPath, page }) 
     });
   });
 
-  await new Promise(resolve => setTimeout(resolve, 2_000));
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  // This would be the obvious way to test offline support but it doesn't appear to work!
+  // await context.setOffline(true);
+
+  // Abort the first envelope request so the event gets queued
+  await page.route(/ingest\.sentry\.io/, route => route.abort(), { times: 1 });
+
+  await page.goto(url);
 
   // Now send a second event which should be queued after the the first one and force flushing the queue
   await page.locator('button').click();
 
-  const replayEvent0 = getReplayEvent(await waitForReplayRequest(page, 0));
-  const replayEvent1 = getReplayEvent(await waitForReplayRequest(page, 1));
+  const replayEvent0 = getReplayEvent(await reqPromise0);
+  const replayEvent1 = getReplayEvent(await reqPromise1);
 
   // Check that we received the envelopes in the correct order
   expect(replayEvent0.timestamp).toBeGreaterThan(0);
