697ba8a3bdcb6cee9c70385bf297402f590ec1b0
Tim Fish <tim@timfish.uk>
2024-04-05 14:14:53 +0200

    Add browser integration test

---
+++ dev-packages/browser-integration-tests/suites/replay/captureReplayOffline/test.ts
@@ -0,0 +1,50 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../utils/fixtures';
+import { getReplayEvent, shouldSkipReplayTest, waitForReplayRequest } from '../../../utils/replayHelpers';
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sentryTest('should capture replays offline', async ({ getLocalTestPath, page }) => {
+  if (shouldSkipReplayTest()) {
+    sentryTest.skip();
+  }
+
+  await page.route('https://dsn.ingest.sentry.io/**/*', route => {
+    return route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({ id: 'test-id' }),
+    });
+  });
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  // This would be the obvious way to test offline support but it doesn't appear to work!
+  // await context.setOffline(true);
+
+  let abortedCount = 0;
+
+  // Abort the first envelope request so the event gets queued
+  await page.route(/ingest\.sentry\.io/, route => {
+    abortedCount += 1;
+    return route.abort();
+  });
+  await page.goto(url);
+  await delay(1_000);
+  await page.unroute(/ingest\.sentry\.io/);
+
+  expect(abortedCount).toBe(1);
+
+  await delay(100);
+  // Now send a second event which should be queued after the the first one and force flushing the queue
+  await page.locator('button').click();
+
+  const replayEvent0 = getReplayEvent(await waitForReplayRequest(page, 0));
+  const replayEvent1 = getReplayEvent(await waitForReplayRequest(page, 1));
+
+  // Check that we received the envelopes in the correct order
+  expect(replayEvent0.timestamp).toBeLessThan(replayEvent1.timestamp || 0);
+});
