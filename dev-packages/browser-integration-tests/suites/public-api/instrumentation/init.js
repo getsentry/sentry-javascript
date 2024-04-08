687350af8ed5f21d2d92a92d8d3ee67de397ce65
Abhijeet Prasad <aprasad@sentry.io>
2024-04-08 10:28:46 -0400

    test: Port onerror tests to playwright

--- dev-packages/browser-integration-tests/suites/public-api/instrumentation/init.js
+++ dev-packages/browser-integration-tests/suites/public-api/instrumentation/init.js
@@ -3,5 +3,5 @@ import * as Sentry from '@sentry/browser';
 window.Sentry = Sentry;
 
 Sentry.init({
-  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  dsn: 'https://public@dsn.ingest.sentry.io/1337'
 });
---
+++ dev-packages/browser-integration-tests/suites/public-api/instrumentation/onError/thrown-strings/test.ts
@@ -0,0 +1,17 @@
+import { expect } from '@playwright/test';
+import type { Event } from '@sentry/types';
+
+import { sentryTest } from '../../../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest } from '../../../../../utils/helpers';
+
+sentryTest('should catch thrown strings', async ({ getLocalTestPath, page }) => {
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+
+  expect(eventData.exception?.values).toHaveLength(1);
+  expect(eventData.exception?.values?.[0].type).toMatch(/SyntaxError/);
+
+  // two frames - one from the eval and one from try catch
+  expect(eventData.exception?.values?.[0].stacktrace?.frames?.length).toBe(2);
+});
