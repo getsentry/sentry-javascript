9c8f0d343a597fcb4602afd2ce87bb6d1a632b55
Francesco Novy <francesco.novy@sentry.io>
2024-04-08 12:10:53 +0100

    feat(browser): Add `lazyLoadIntegration` utility (#11339)
    This can be used to lazy load a pluggable integration.
    
    Usage:
    
    ```js
    async function getOrLazyLoadFeedback() {
      const existing = Sentry.getFeedback(); // check if it has already been installed
      if (existing) {
        return existing;
      }
      try {
        const feedbackIntegration = await Sentry.lazyLoadIntegration('feedbackIntegration');
        client.addIntegration(feedbackIntegration());
      } catch(error) {
        // this can error, we need to handle this!
      }
    }
    ```
    
    Closes https://github.com/getsentry/sentry-javascript/issues/10905

---
+++ dev-packages/browser-integration-tests/suites/integrations/lazyLoad/validIntegration/test.ts
@@ -0,0 +1,24 @@
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+
+sentryTest('it allows to lazy load an integration', async ({ getLocalTestUrl, page }) => {
+  const url = await getLocalTestUrl({ testDir: __dirname });
+
+  await page.goto(url);
+
+  const hasIntegration = await page.evaluate('!!window.Sentry.getClient()?.getIntegrationByName("HttpClient")');
+  expect(hasIntegration).toBe(false);
+
+  const scriptTagsBefore = await page.evaluate<number>('document.querySelectorAll("script").length');
+
+  await page.evaluate('window._testLazyLoadIntegration()');
+  await page.waitForFunction('window._integrationLoaded');
+
+  const scriptTagsAfter = await page.evaluate<number>('document.querySelectorAll("script").length');
+
+  const hasIntegration2 = await page.evaluate('!!window.Sentry.getClient()?.getIntegrationByName("HttpClient")');
+  expect(hasIntegration2).toBe(true);
+
+  expect(scriptTagsAfter).toBe(scriptTagsBefore + 1);
+});
