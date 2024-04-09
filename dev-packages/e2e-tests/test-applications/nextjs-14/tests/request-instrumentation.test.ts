4676ca33373c0982b9a9d4aeb0ca2e586a73c61e
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-04-09 15:27:23 +0200

    fix(nextjs): Remove Http integration from Next.js (#11304)
    Next.js provides their own OTel http integration, which conflicts with
    ours
    ref https://github.com/getsentry/sentry-javascript/pull/11016
    added commit from this PR:
    https://github.com/getsentry/sentry-javascript/pull/11319
    
    ---------
    
    Co-authored-by: Luca Forstner <luca.forstner@sentry.io>
    Co-authored-by: Francesco Novy <francesco.novy@sentry.io>

--- dev-packages/e2e-tests/test-applications/nextjs-14/tests/request-instrumentation.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-14/tests/request-instrumentation.test.ts
@@ -19,16 +19,15 @@ test('Should send a transaction with a fetch span', async ({ page }) => {
     }),
   );
 
-  // TODO: Uncomment the below when fixed. For whatever reason that we now have accepted, spans created with Node.js' http.get() will not attach themselves to transactions.
-  // More info: https://github.com/getsentry/sentry-javascript/pull/11016/files#diff-10fa195789425786c6e5e769380be18790768f0b76319ee41bbb488d9fe50405R4
-  // expect((await transactionPromise).spans).toContainEqual(
-  //   expect.objectContaining({
-  //     data: expect.objectContaining({
-  //       'http.method': 'GET',
-  //       'sentry.op': 'http.client',
-  //       'sentry.origin': 'auto.http.otel.http',
-  //     }),
-  //     description: 'GET http://example.com/',
-  //   }),
-  // );
+  expect((await transactionPromise).spans).toContainEqual(
+    expect.objectContaining({
+      data: expect.objectContaining({
+        'http.method': 'GET',
+        'sentry.op': 'http.client',
+        // todo: without the HTTP integration in the Next.js SDK, this is set to 'manual' -> we could rename this to be more specific
+        'sentry.origin': 'manual',
+      }),
+      description: 'GET http://example.com/',
+    }),
+  );
 });
