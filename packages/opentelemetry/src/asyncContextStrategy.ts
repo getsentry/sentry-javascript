5c9a354e22b849d528ade6f537a88595feb29401
Lukas Stracke <lukas.stracke@sentry.io>
2024-04-12 15:59:13 +0200

    biome...

--- packages/opentelemetry/src/asyncContextStrategy.ts
+++ packages/opentelemetry/src/asyncContextStrategy.ts
@@ -1,12 +1,7 @@
 import * as api from '@opentelemetry/api';
-import {
-  getCurrentHubShim,
-  getDefaultCurrentScope,
-  getDefaultIsolationScope,
-  setAsyncContextStrategy,
-} from '@sentry/core';
+import { getDefaultCurrentScope, getDefaultIsolationScope, setAsyncContextStrategy } from '@sentry/core';
 import type { withActiveSpan as defaultWithActiveSpan } from '@sentry/core';
-import type { Hub, Scope } from '@sentry/types';
+import type { Scope } from '@sentry/types';
 
 import {
   SENTRY_FORK_ISOLATION_SCOPE_CONTEXT_KEY,
