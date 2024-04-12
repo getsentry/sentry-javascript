e658aff0ff1f567b803d2972964bb7533a369faa
Lukas Stracke <lukas.stracke@sentry.io>
2024-04-12 17:43:05 +0200

    test

--- packages/browser/src/sdk.ts
+++ packages/browser/src/sdk.ts
@@ -1,4 +1,4 @@
-import { getCurrentScope } from '@sentry/core';
+import { getCurrentScope, withScope } from '@sentry/core';
 import { functionToStringIntegration, inboundFiltersIntegration } from '@sentry/core';
 import {
   captureSession,
@@ -156,6 +156,11 @@ export function init(browserOptions: BrowserOptions = {}): void {
   };
 
   initAndBind(BrowserClient, clientOptions);
+
+  // tmp test
+  getCurrentScope();
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  withScope(() => {});
 
   if (options.autoSessionTracking) {
     startSessionTracking();
