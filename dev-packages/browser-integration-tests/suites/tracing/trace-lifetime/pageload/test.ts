4583556bf5c4845920a221ae40bd5436692f1106
Lukas Stracke <lukas.stracke@sentry.io>
2024-04-15 12:26:42 +0200

    fix(browser): Ensure pageload trace remains active after pageload span finished

---
+++ dev-packages/browser-integration-tests/suites/tracing/trace-lifetime/pageload/test.ts
@@ -0,0 +1,50 @@
+import { expect } from '@playwright/test';
+import type { Event } from '@sentry/types';
+import { sentryTest } from '../../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest, shouldSkipTracingTest } from '../../../../utils/helpers';
+
+sentryTest(
+  'should create a new trace for a navigation after the initial pageload',
+  async ({ getLocalTestPath, page }) => {
+    if (shouldSkipTracingTest()) {
+      sentryTest.skip();
+    }
+
+    const url = await getLocalTestPath({ testDir: __dirname });
+
+    const pageloadEvent = await getFirstSentryEnvelopeRequest<Event>(page, url);
+    const navigationEvent1 = await getFirstSentryEnvelopeRequest<Event>(page, `${url}#foo`);
+
+    expect(pageloadEvent.contexts?.trace?.op).toBe('pageload');
+    expect(navigationEvent1.contexts?.trace?.op).toBe('navigation');
+
+    const pageloadTraceId = pageloadEvent.contexts?.trace?.trace_id;
+    const navigation1TraceId = navigationEvent1.contexts?.trace?.trace_id;
+
+    expect(pageloadTraceId).toMatch(/^[0-9a-f]{32}$/);
+    expect(navigation1TraceId).toMatch(/^[0-9a-f]{32}$/);
+    expect(pageloadTraceId).not.toEqual(navigation1TraceId);
+  },
+);
+
+sentryTest('error after pageload should have pageload traceId', async ({ getLocalTestPath, page }) => {
+  if (shouldSkipTracingTest()) {
+    sentryTest.skip();
+  }
+
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const pageloadEvent = await getFirstSentryEnvelopeRequest<Event>(page, url);
+  expect(pageloadEvent.contexts?.trace?.op).toBe('pageload');
+
+  const pageloadTraceId = pageloadEvent.contexts?.trace?.trace_id;
+  expect(pageloadTraceId).toMatch(/^[0-9a-f]{32}$/);
+
+  const [, errorEvent] = await Promise.all([
+    page.locator('#errorBtn').click(),
+    getFirstSentryEnvelopeRequest<Event>(page),
+  ]);
+
+  const errorTraceId = errorEvent.contexts?.trace?.trace_id;
+  expect(errorTraceId).toBe(pageloadTraceId);
+});
