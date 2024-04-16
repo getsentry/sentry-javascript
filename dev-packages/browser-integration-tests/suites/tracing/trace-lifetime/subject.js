8cb70e3b4862e5d34fc41e5d6981ec0a0660eab0
Lukas Stracke <lukas.stracke@sentry.io>
2024-04-16 09:19:11 +0200

    test(browser-integration-tests): Test trace lifetime for outgoing `fetch` requests (#11614)
    Add test for `sentry-trace` and `baggage` header values so that the
    headers contain the correct traceId in the folllowing four scenarios:
    
    * request during pageload span
    * request after pageload span
    * request during navigation span
    * request after navigation span

--- dev-packages/browser-integration-tests/suites/tracing/trace-lifetime/subject.js
+++ dev-packages/browser-integration-tests/suites/tracing/trace-lifetime/subject.js
@@ -2,3 +2,8 @@ const errorBtn = document.getElementById('errorBtn');
 errorBtn.addEventListener('click', () => {
   throw new Error('Sentry Test Error');
 });
+
+const fetchBtn = document.getElementById('fetchBtn');
+fetchBtn.addEventListener('click', async () => {
+  await fetch('http://example.com');
+});
