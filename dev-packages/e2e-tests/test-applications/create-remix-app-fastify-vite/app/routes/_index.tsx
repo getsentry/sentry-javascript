ee6cb16a4ad224d084311a16a072f7314ebe0c73
Onur Temizkan <onur@narval.co.uk>
2024-04-19 10:56:53 +0100

    Setup E2E test application.

--- dev-packages/e2e-tests/test-applications/create-remix-app-fastify-vite/app/routes/_index.tsx
+++ dev-packages/e2e-tests/test-applications/create-remix-app-fastify-vite/app/routes/_index.tsx
@@ -1,45 +1,27 @@
-import type { MetaFunction } from '@remix-run/node';
+import { Link, useSearchParams } from '@remix-run/react';
 import * as Sentry from '@sentry/remix';
 
-export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
-};
-
 export default function Index() {
+  const [searchParams] = useSearchParams();
+
+  if (searchParams.get('tag')) {
+    Sentry.setTag('sentry_test', searchParams.get('tag'));
+  }
+
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <div>
-            <span>Remix + Sentry on the client</span>
-            <input
-              type="button"
-              value="Capture Exception"
-              id="exception-button"
-              onClick={() => {
-                const eventId = Sentry.captureException(new Error('I am an error!'));
-                window.capturedExceptionId = eventId;
-              }}
-            />
-          </div>
-        </li>
-      </ul>
+    <div>
+      <input
+        type="button"
+        value="Capture Exception"
+        id="exception-button"
+        onClick={() => {
+          const eventId = Sentry.captureException(new Error('I am an error!'));
+          window.capturedExceptionId = eventId;
+        }}
+      />
+      <Link to="/user/5" id="navigation">
+        navigate
+      </Link>
     </div>
   );
 }
