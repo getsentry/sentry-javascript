780a9366bc3c0faa35aa615d2382912d7d7c9f62
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-04-19 15:35:24 -0700

    fix import path for tests

--- packages/browser/test/unit/index.bundle.feedback.test.ts
+++ packages/browser/test/unit/index.bundle.feedback.test.ts
@@ -1,10 +1,11 @@
-import { replayIntegrationShim } from '@sentry-internal/integration-shims';
-import { feedbackIntegration } from '@sentry/browser';
+import { browserTracingIntegrationShim, replayIntegrationShim } from '@sentry-internal/integration-shims';
+import { feedbackIntegration } from '../../src';
 
 import * as FeedbackBundle from '../../src/index.bundle.feedback';
 
 describe('index.bundle.feedback', () => {
   it('has correct exports', () => {
+    expect(FeedbackBundle.browserTracingIntegration).toBe(browserTracingIntegrationShim);
     expect(FeedbackBundle.replayIntegration).toBe(replayIntegrationShim);
     expect(FeedbackBundle.feedbackIntegration).toBe(feedbackIntegration);
   });
