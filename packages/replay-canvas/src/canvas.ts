1d8df820407bbc333149a58447eb7cde14674067
Billy Vong <billyvg@users.noreply.github.com>
2024-04-22 14:03:14 -0230

    feat(replay): Add "maxCanvasSize" option for replay canvases
    We do not want to allow users to capture super large canvases, enforce a
    max size limit
    
    Requires https://github.com/getsentry/rrweb/pull/174
    Closes https://github.com/getsentry/sentry-javascript/issues/10498
    Backport of https://github.com/getsentry/sentry-javascript/pull/11617

--- packages/replay-canvas/src/canvas.ts
+++ packages/replay-canvas/src/canvas.ts
@@ -5,12 +5,14 @@ import type { Integration, IntegrationClass, IntegrationFn } from '@sentry/types
 
 interface ReplayCanvasOptions {
   enableManualSnapshot?: boolean;
+  maxCanvasSize?: [width: number, height: number];
   quality: 'low' | 'medium' | 'high';
 }
 
 type GetCanvasManager = (options: InternalCanvasManagerOptions) => InternalCanvasManagerInterface;
 export interface ReplayCanvasIntegrationOptions {
   enableManualSnapshot?: boolean;
+  maxCanvasSize?: number;
   recordCanvas: true;
   getCanvasManager: GetCanvasManager;
   sampling: {
@@ -53,12 +55,18 @@ const CANVAS_QUALITY = {
 };
 
 const INTEGRATION_NAME = 'ReplayCanvas';
+const DEFAULT_MAX_CANVAS_SIZE = 1280;
 
 /** Exported only for type safe tests. */
 export const _replayCanvasIntegration = ((options: Partial<ReplayCanvasOptions> = {}) => {
+  const [maxCanvasWidth, maxCanvasHeight] = options.maxCanvasSize || [];
   const _canvasOptions = {
     quality: options.quality || 'medium',
     enableManualSnapshot: options.enableManualSnapshot,
+    maxCanvasSize: [
+      maxCanvasWidth ? Math.min(maxCanvasWidth, DEFAULT_MAX_CANVAS_SIZE) : DEFAULT_MAX_CANVAS_SIZE,
+      maxCanvasHeight ? Math.min(maxCanvasHeight, DEFAULT_MAX_CANVAS_SIZE) : DEFAULT_MAX_CANVAS_SIZE,
+    ] as [number, number],
   };
 
   let canvasManagerResolve: (value: CanvasManager) => void;
@@ -69,15 +77,16 @@ export const _replayCanvasIntegration = ((options: Partial<ReplayCanvasOptions> 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     setupOnce() {},
     getOptions(): ReplayCanvasIntegrationOptions {
-      const { quality, enableManualSnapshot } = _canvasOptions;
+      const { quality, enableManualSnapshot, maxCanvasSize } = _canvasOptions;
 
       return {
         enableManualSnapshot,
         recordCanvas: true,
-        getCanvasManager: (options: InternalCanvasManagerOptions) => {
+        getCanvasManager: (getCanvasManagerOptions: InternalCanvasManagerOptions) => {
           const manager = new CanvasManager({
-            ...options,
+            ...getCanvasManagerOptions,
             enableManualSnapshot,
+            maxCanvasSize,
             errorHandler: (err: unknown) => {
               try {
                 if (typeof err === 'object') {
