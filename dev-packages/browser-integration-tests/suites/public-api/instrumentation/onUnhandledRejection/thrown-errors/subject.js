c5d9c3e60a9d49bcf0562b3a76c9d81cc86d5c88
Abhijeet Prasad <aprasad@sentry.io>
2024-04-23 13:34:54 -0400

    test: Port unhandledrejection tests to playwright

---
+++ dev-packages/browser-integration-tests/suites/public-api/instrumentation/onUnhandledRejection/thrown-errors/subject.js
@@ -0,0 +1,9 @@
+function run() {
+  const reason = new Error('promiseError');
+  const promise = Promise.reject(reason);
+  const event = new PromiseRejectionEvent('unhandledrejection', { promise, reason });
+  // simulate window.onunhandledrejection without generating a Script error
+  window.onunhandledrejection(event);
+}
+
+run();
