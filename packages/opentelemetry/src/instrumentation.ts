0a440ddcfd30d5570125e28364afe63f171ec501
Abhijeet Prasad <aprasad@sentry.io>
2024-04-23 12:36:31 -0400

    feat(opentelemetry): use rest args for addOpenTelemetryInstrumentation (#11721)
    update the `addOpenTelemetryInstrumentation` API to use rest args.

--- packages/opentelemetry/src/instrumentation.ts
+++ packages/opentelemetry/src/instrumentation.ts
@@ -5,10 +5,8 @@ import { registerInstrumentations } from '@opentelemetry/instrumentation';
  * This method takes an OpenTelemetry instrumentation or
  * array of instrumentations and registers them with OpenTelemetry.
  */
-export function addOpenTelemetryInstrumentation(
-  instrumentation: InstrumentationOption | InstrumentationOption[],
-): void {
+export function addOpenTelemetryInstrumentation(...instrumentations: InstrumentationOption[]): void {
   registerInstrumentations({
-    instrumentations: Array.isArray(instrumentation) ? instrumentation : [instrumentation],
+    instrumentations,
   });
 }
