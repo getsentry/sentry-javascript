3eea5370b418ca341f78e05318b8ea563eba3f7e
Abhijeet Prasad <aprasad@sentry.io>
2024-04-24 08:25:11 -0400

    test(browser): Port unhandledrejection tests to playwright (#11758)
    ref https://github.com/getsentry/sentry-javascript/issues/11084
    
    This test ports
    `packages/browser/test/integration/suites/onunhandledrejection.js`
    playwright. Because of the same limitations as outlined with the on
    error tests https://github.com/getsentry/sentry-javascript/pull/11666, I
    had to use calls to `window.onunhandledrejection` to simulate these
    tests instead of just using `Promise.reject` to test the handler.
    
    https://github.com/getsentry/sentry-javascript/issues/11678 tracks being
    able to fix this so we can avoid directly calling
    `window.onunhandledrejection` to test.
    
    As `onunhandledrejection.js` was the last suite to use the old
    integration tests, I fully removed that code and the corresponding GH
    action workflow. I also removed the monorepo deps on `karma`, `chai` and
    `sinon`. Extremely satisfying.

---
+++ dev-packages/browser-integration-tests/suites/public-api/instrumentation/onUnhandledRejection/thrown-objects/test.ts
@@ -0,0 +1,21 @@
+import { expect } from '@playwright/test';
+import type { Event } from '@sentry/types';
+
+import { sentryTest } from '../../../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest } from '../../../../../utils/helpers';
+
+sentryTest('should capture unhandledrejection with an object', async ({ getLocalTestPath, page }) => {
+  const url = await getLocalTestPath({ testDir: __dirname });
+
+  const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
+
+  expect(eventData.exception?.values).toHaveLength(1);
+  expect(eventData.exception?.values?.[0]).toMatchObject({
+    type: 'UnhandledRejection',
+    value: 'Object captured as promise rejection with keys: a, b, c',
+    mechanism: {
+      type: 'onunhandledrejection',
+      handled: false,
+    },
+  });
+});
