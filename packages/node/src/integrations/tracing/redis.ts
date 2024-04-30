b9c67cffe1c989600003117a57822a9dbf635b6f
s1gr1d <sigrid.huemer@posteo.at>
2024-04-30 17:23:09 +0200

    add instrumentation for ioredis

--- packages/node/src/integrations/tracing/redis.ts
+++ packages/node/src/integrations/tracing/redis.ts
@@ -1,34 +1,20 @@
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
-import { RedisInstrumentation as LegacyRedisInstrumentation } from '@opentelemetry/instrumentation-redis';
-import { RedisInstrumentation } from '@opentelemetry/instrumentation-redis-4';
+// import { RedisInstrumentation as LegacyRedisInstrumentation } from '@opentelemetry/instrumentation-redis';
+// import { RedisInstrumentation } from '@opentelemetry/instrumentation-redis-4';
 import { defineIntegration } from '@sentry/core';
 import { addOpenTelemetryInstrumentation } from '@sentry/opentelemetry';
 import type { IntegrationFn } from '@sentry/types';
 
-interface RedisOptions {
-  client?: 'ioredis' | 'redis-v2' | 'redis-v3' | 'redis-v4';
-}
-
-const _redisIntegration = ((options: RedisOptions = { client: 'redis-v4' }) => {
-
+const _redisIntegration = (() => {
   return {
     name: 'Redis',
     setupOnce() {
-      switch (options.client) {
-        case 'ioredis':
-          addOpenTelemetryInstrumentation(new IORedisInstrumentation({}));
-          break;
-        case 'redis-v2':
-        case 'redis-v3':
-          addOpenTelemetryInstrumentation(new LegacyRedisInstrumentation({}));
-          break;
-        case 'redis-v4':
-          addOpenTelemetryInstrumentation(new RedisInstrumentation({}));
-          break;
-        default:
-          // No need to do anything, as the default is already 'redis-v4'
-          break;
-      }
+      addOpenTelemetryInstrumentation([
+        new IORedisInstrumentation({}),
+        // todo: implement them gradually
+        // new LegacyRedisInstrumentation({}),
+        // new RedisInstrumentation({}),
+      ]);
     },
   };
 }) satisfies IntegrationFn;
