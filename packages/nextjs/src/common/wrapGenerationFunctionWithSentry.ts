525d705d1c12643c5765a8b18f1580c0af57d839
s1gr1d <sigrid.huemer@posteo.at>
2024-04-30 15:45:20 +0200

    revert conditional for generationFunction

--- packages/nextjs/src/common/wrapGenerationFunctionWithSentry.ts
+++ packages/nextjs/src/common/wrapGenerationFunctionWithSentry.ts
@@ -59,14 +59,7 @@ export function wrapGenerationFunctionWithSentry<F extends (...args: any[]) => a
         const propagationContext = commonObjectToPropagationContext(headers, incomingPropagationContext);
 
         return withIsolationScope(isolationScope, () => {
-          if (
-            componentType &&
-            (!isolationScope.getScopeData().transactionName ||
-              !isolationScope.getScopeData().transactionName?.includes('generate'))
-          ) {
-            // only set name if not already set (or overwrite if not "generate..."), otherwise it gets overwritten by subsequent calls
-            isolationScope.setTransactionName(`${componentType}.${generationFunctionIdentifier} (${componentRoute})`);
-          }
+          isolationScope.setTransactionName(`${componentType}.${generationFunctionIdentifier} (${componentRoute})`);
           isolationScope.setSDKProcessingMetadata({
             request: {
               headers: headers ? winterCGHeadersToDict(headers) : undefined,
