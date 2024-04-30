e2d3fea01bf19351bb0e593f142408670af21e79
Francesco Novy <francesco.novy@sentry.io>
2024-04-30 10:19:23 +0200

    WIP

---
+++ dev-packages/e2e-tests/test-applications/ember-embroider/ember-cli-build.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    tests: false,
+  });
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    staticEmberSource: true,
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
+};
