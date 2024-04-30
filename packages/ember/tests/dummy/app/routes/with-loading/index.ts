b46a73557e5c5495e895150403252577d5d4774f
Francesco Novy <francesco.novy@sentry.io>
2024-04-30 10:08:47 +0200

    fix(ember): Ensure unnecessary spans are avoided (#11846)
    Extracted out from
    https://github.com/getsentry/sentry-javascript/pull/11827/.
    
    I added tests for loading & error states as well, to ensure this does
    not regress.

---
+++ packages/ember/tests/dummy/app/routes/with-loading/index.ts
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { instrumentRoutePerformance } from '@sentry/ember';
+
+import timeout from '../../helpers/utils';
+
+class WithLoadingIndexRoute extends Route {
+  public model(): Promise<void> {
+    return timeout(1000);
+  }
+}
+
+export default instrumentRoutePerformance(WithLoadingIndexRoute);
