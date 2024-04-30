007c0ef329183747ebfa94fbad24507dbc023b70
Francesco Novy <francesco.novy@sentry.io>
2024-04-30 13:26:17 +0200

    better config?

--- dev-packages/e2e-tests/test-applications/ember-embroider/ember-cli-build.js
+++ dev-packages/e2e-tests/test-applications/ember-embroider/ember-cli-build.js
@@ -5,11 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     tests: false,
+    hinting: false,
   });
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
     staticHelpers: true,
     staticModifiers: true,
