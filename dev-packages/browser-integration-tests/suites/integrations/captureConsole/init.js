548c2a87939759f1c6f233d34f310cb542458e7b
Francesco Novy <francesco.novy@sentry.io>
2024-04-30 10:00:53 +0200

    fix cdn test

--- dev-packages/browser-integration-tests/suites/integrations/captureConsole/init.js
+++ dev-packages/browser-integration-tests/suites/integrations/captureConsole/init.js
@@ -1,9 +1,10 @@
 import * as Sentry from '@sentry/browser';
+import { captureConsoleIntegration } from '@sentry/browser';
 
 window.Sentry = Sentry;
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  integrations: [Sentry.captureConsoleIntegration()],
+  integrations: [captureConsoleIntegration()],
   autoSessionTracking: false,
 });
