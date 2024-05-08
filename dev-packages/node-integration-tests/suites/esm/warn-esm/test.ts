c3c2bd319da0214aaeb269cdfbdf49ae56a187a2
Luca Forstner <luca.forstner@sentry.io>
2024-05-08 15:04:26 +0200

    feat(node): Register ESM patching hooks in `init` for supported Node.js versions (#11933)

--- dev-packages/node-integration-tests/suites/esm/warn-esm/test.ts
+++ dev-packages/node-integration-tests/suites/esm/warn-esm/test.ts
@@ -5,14 +5,32 @@ afterAll(() => {
 });
 
 const esmWarning =
-  '[Sentry] You are using the Sentry SDK with an ESM build. This version of the SDK is not compatible with ESM. Please either build your application with CommonJS, or use v7 of the SDK.';
+  '[Sentry] You are using Node.js in ESM mode ("import syntax"). The Sentry Node.js SDK is not compatible with ESM in Node.js versions before 18.19.0 or before 20.6.0. Please either build your application with CommonJS ("require() syntax"), or use version 7.x of the Sentry Node.js SDK.';
 
-test('warns if using ESM', async () => {
+test("warns if using ESM on Node.js versions that don't support `register()`", async () => {
+  const nodeMajorVersion = Number(process.versions.node.split('.')[0]);
+  if (nodeMajorVersion >= 18) {
+    return;
+  }
+
   const runner = createRunner(__dirname, 'server.mjs').ignore('session', 'sessions', 'event').start();
 
   await runner.makeRequest('get', '/test/success');
 
   expect(runner.getLogs()).toContain(esmWarning);
+});
+
+test('does not warn if using ESM on Node.js versions that support `register()`', async () => {
+  const nodeMajorVersion = Number(process.versions.node.split('.')[0]);
+  if (nodeMajorVersion < 18) {
+    return;
+  }
+
+  const runner = createRunner(__dirname, 'server.mjs').ignore('session', 'sessions', 'event').start();
+
+  await runner.makeRequest('get', '/test/success');
+
+  expect(runner.getLogs()).not.toContain(esmWarning);
 });
 
 test('does not warn if using CJS', async () => {
