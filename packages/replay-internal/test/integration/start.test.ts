1f91a37fd7c69339ff96a9dfbb9fde37892f23af
Billy Vong <billyvg@users.noreply.github.com>
2024-05-13 10:03:13 -0400

    lint

--- packages/replay-internal/test/integration/start.test.ts
+++ packages/replay-internal/test/integration/start.test.ts
@@ -3,32 +3,21 @@ import { vi } from 'vitest';
 import { getClient } from '@sentry/core';
 import type { Transport } from '@sentry/types';
 
-import {
-  DEFAULT_FLUSH_MIN_DELAY,
-  MAX_REPLAY_DURATION,
-  SESSION_IDLE_EXPIRE_DURATION,
-  WINDOW,
-} from '../../src/constants';
+import { DEFAULT_FLUSH_MIN_DELAY, SESSION_IDLE_EXPIRE_DURATION } from '../../src/constants';
+import type { Replay } from '../../src/integration';
 import type { ReplayContainer } from '../../src/replay';
 import { BASE_TIMESTAMP } from '../index';
-import type { RecordMock } from '../mocks/mockRrweb';
 import { resetSdkMock } from '../mocks/resetSdkMock';
-import type { DomHandler } from '../types';
 import { useFakeTimers } from '../utils/use-fake-timers';
-import type { Replay } from '../../src/integration';
 
 useFakeTimers();
-
-const prevLocation = WINDOW.location;
 
 describe('Integration | start', () => {
   let replay: ReplayContainer;
   let integration: Replay;
-  let domHandler: DomHandler;
-  let mockRecord: RecordMock;
 
   beforeEach(async () => {
-    ({ mockRecord, domHandler, replay, integration } = await resetSdkMock({
+    ({ replay, integration } = await resetSdkMock({
       replayOptions: {
         stickySession: false,
       },
@@ -47,11 +36,6 @@ describe('Integration | start', () => {
 
     await vi.runAllTimersAsync();
     vi.setSystemTime(new Date(BASE_TIMESTAMP));
-
-    Object.defineProperty(WINDOW, 'location', {
-      value: prevLocation,
-      writable: true,
-    });
   });
 
   it('sends replay when calling `start()` after [SESSION_IDLE_EXPIRE_DURATION]ms', async () => {
