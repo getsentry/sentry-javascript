e1c055a8b6abf35448941371938d712448eb97ff
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-05-12 22:56:20 -0400

    check for empty user

--- packages/feedback/src/modal/integration.tsx
+++ packages/feedback/src/modal/integration.tsx
@@ -5,11 +5,25 @@ import type {
   FeedbackFormData,
   FeedbackModalIntegration,
   IntegrationFn,
+  User,
 } from '@sentry/types';
 import { h, render } from 'preact';
 import { DOCUMENT } from '../constants';
 import { Dialog } from './components/Dialog';
 import { createDialogStyles } from './components/Dialog.css';
+
+function getUser(): User | undefined {
+  const currentUser = getCurrentScope().getUser();
+  const isolationUser = getIsolationScope().getUser();
+  const globalUser = getGlobalScope().getUser();
+  if (currentUser && Object.keys(currentUser).length) {
+    return currentUser;
+  }
+  if (isolationUser && Object.keys(isolationUser).length) {
+    return isolationUser;
+  }
+  return globalUser;
+}
 
 export const feedbackModalIntegration = ((): FeedbackModalIntegration => {
   return {
@@ -19,7 +33,7 @@ export const feedbackModalIntegration = ((): FeedbackModalIntegration => {
     createDialog: ({ options, screenshotIntegration, sendFeedback, shadow }: CreateDialogProps) => {
       const shadowRoot = shadow as unknown as ShadowRoot;
       const userKey = options.useSentryUser;
-      const user = getCurrentScope().getUser() || getIsolationScope().getUser() || getGlobalScope().getUser();
+      const user = getUser();
 
       const el = DOCUMENT.createElement('div');
       const style = createDialogStyles();
