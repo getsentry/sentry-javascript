3bf35884c371a49d9295986d8027237f54b7d4d9
Francesco Novy <francesco.novy@sentry.io>
2024-05-14 09:16:17 +0200

    test(node): Add test for express without tracing

---
+++ dev-packages/node-integration-tests/suites/express/without-tracing/test.ts
@@ -0,0 +1,50 @@
+import { cleanupChildProcesses, createRunner } from '../../../utils/runner';
+
+afterAll(() => {
+  cleanupChildProcesses();
+});
+
+test('correctly applies isolation scope even without tracing', async () => {
+  const runner = createRunner(__dirname, 'server.ts')
+    .ignore('session', 'sessions')
+    .expect({
+      event: {
+        tags: {
+          global: 'tag',
+          'isolation-scope': 'tag',
+          'isolation-scope-1': '1',
+          // We can't properly test non-existance of fields here, so we cast this to a string to test it here
+          'isolation-scope-transactionName': 'undefined',
+        },
+        // Request is correctly set
+        request: {
+          url: expect.stringContaining('/test/isolationScope/1'),
+          headers: {
+            'user-agent': expect.stringContaining(''),
+          },
+        },
+      },
+    })
+    .expect({
+      event: {
+        tags: {
+          global: 'tag',
+          'isolation-scope': 'tag',
+          'isolation-scope-2': '2',
+          // We can't properly test non-existance of fields here, so we cast this to a string to test it here
+          'isolation-scope-transactionName': 'undefined',
+        },
+        // Request is correctly set
+        request: {
+          url: expect.stringContaining('/test/isolationScope/2'),
+          headers: {
+            'user-agent': expect.stringContaining(''),
+          },
+        },
+      },
+    })
+    .start();
+
+  await runner.makeRequest('get', '/test/isolationScope/1');
+  await runner.makeRequest('get', '/test/isolationScope/2');
+});
