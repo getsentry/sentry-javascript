f7bd662a97e284598e581bed909dd4a19f403e5c
Abhijeet Prasad <aprasad@sentry.io>
2024-05-22 03:29:10 -0400

    feat(google-cloud): Expose ESM build (#12149)
    This exposes the ESM build for `@sentry/google-cloud-serverless`, with
    the caveat that the performance related instrumentation does not work
    atm. These need to be ported to use OpenTelemetry instrumentation.

--- packages/google-cloud-serverless/src/sdk.ts
+++ packages/google-cloud-serverless/src/sdk.ts
@@ -5,13 +5,22 @@ import type { Integration, Options, SdkMetadata } from '@sentry/types';
 import { googleCloudGrpcIntegration } from './integrations/google-cloud-grpc';
 import { googleCloudHttpIntegration } from './integrations/google-cloud-http';
 
+function isCjs(): boolean {
+  return typeof require !== 'undefined';
+}
+
+function getCjsOnlyIntegrations(): Integration[] {
+  return isCjs()
+    ? [
+        googleCloudHttpIntegration({ optional: true }), // We mark this integration optional since '@google-cloud/common' module could be missing.
+        googleCloudGrpcIntegration({ optional: true }), // We mark this integration optional since 'google-gax' module could be missing.
+      ]
+    : [];
+}
+
 /** Get the default integrations for the GCP SDK. */
 export function getDefaultIntegrations(_options: Options): Integration[] {
-  return [
-    ...getDefaultIntegrationsWithoutPerformance(),
-    googleCloudHttpIntegration({ optional: true }), // We mark this integration optional since '@google-cloud/common' module could be missing.
-    googleCloudGrpcIntegration({ optional: true }), // We mark this integration optional since 'google-gax' module could be missing.
-  ];
+  return [...getDefaultIntegrationsWithoutPerformance(), ...getCjsOnlyIntegrations()];
 }
 
 /**
