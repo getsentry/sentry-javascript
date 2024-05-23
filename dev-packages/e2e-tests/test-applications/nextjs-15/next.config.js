e0a1df2c31e76ec7054205d1f47af1110c94afc2
Luca Forstner <luca.forstner@sentry.io>
2024-05-23 15:46:08 +0000

    Add failing test

--- dev-packages/e2e-tests/test-applications/nextjs-15/next.config.js
+++ dev-packages/e2e-tests/test-applications/nextjs-15/next.config.js
@@ -1,7 +1,11 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
+};
 
 module.exports = withSentryConfig(nextConfig, {
   silent: true,
