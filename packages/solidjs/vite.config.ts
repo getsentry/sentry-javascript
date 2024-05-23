28e4ffc7112c5dc97d929ddd54c9fcd435e177f2
Andrei Borza <andrei.borza@sentry.io>
2024-05-23 17:16:04 +0200

    feat(solidjs): Add solidjs SDK basic package

---
+++ packages/solidjs/vite.config.ts
@@ -0,0 +1,14 @@
+import solidPlugin from 'vite-plugin-solid';
+import type { UserConfig } from 'vitest';
+import baseConfig from '../../vite/vite.config';
+
+export default {
+  ...baseConfig,
+  plugins: [solidPlugin()],
+  test: {
+    // test exists, no idea why TS doesn't recognize it
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...(baseConfig as UserConfig & { test: any }).test,
+    environment: 'jsdom',
+  },
+};
