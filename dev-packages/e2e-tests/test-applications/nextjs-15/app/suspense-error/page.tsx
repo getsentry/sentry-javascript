77d4600c876fe6d062fba798b8a769538adfa057
Luca Forstner <luca.forstner@sentry.io>
2024-05-28 13:22:16 +0000

    fail tests

--- dev-packages/e2e-tests/test-applications/nextjs-15/app/suspense-error/page.tsx
+++ dev-packages/e2e-tests/test-applications/nextjs-15/app/suspense-error/page.tsx
@@ -1,8 +1,15 @@
-import { ClientComponentTakingAPromise } from './client-page';
-
+import * as Sentry from '@sentry/nextjs';
+import { use } from 'react';
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const promise = fetch('http://example.com/').then(() => 'foobar');
-  return <ClientComponentTakingAPromise promise={promise} />;
+  try {
+    use(fetch('http://example.com/'));
+  } catch (e) {
+    Sentry.captureException(e); // This error should not be reported
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for any async event processors to run
+    await Sentry.flush();
+  }
+
+  return <p>test</p>;
 }
