d63d7c673f4a837628ead6bbe2f70e5b1613ab65
Francesco Novy <francesco.novy@sentry.io>
2024-05-28 16:32:59 +0200

    feat(metrics): Add `timings` method to metrics (#12226)
    This introduces a new method, `metrics.timing()`, which can be used in
    two ways:
    
    1. With a numeric value, to simplify creating a distribution metric.
    This will default to `second` as unit:
    
    ```js
    Sentry.metrics.timing('myMetric', 100);
    ```
    
    2. With a callback, which will wrap the duration of the callback. This
    can accept a sync or async callback. It will create an inactive span
    around the callback and at the end emit a metric with the duration of
    the span in seconds:
    
    ```js
    const returnValue = Sentry.metrics.timing('myMetric', measureThisFunction);
    ```
    
    Closes https://github.com/getsentry/sentry-javascript/issues/12215

--- dev-packages/browser-integration-tests/suites/metrics/metricsEvent/init.js
+++ dev-packages/browser-integration-tests/suites/metrics/metricsEvent/init.js
@@ -15,3 +15,20 @@ Sentry.metrics.gauge('gauge', 5);
 Sentry.metrics.gauge('gauge', '15');
 Sentry.metrics.set('set', 'nope');
 Sentry.metrics.set('set', 'another');
+
+Sentry.metrics.timing('timing', 99, 'hour');
+Sentry.metrics.timing('timingSync', () => {
+  sleepSync(200);
+});
+Sentry.metrics.timing('timingAsync', async () => {
+  await new Promise(resolve => setTimeout(resolve, 200));
+});
+
+function sleepSync(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
