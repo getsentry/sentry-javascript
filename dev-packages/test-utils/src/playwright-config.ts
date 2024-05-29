1af377cd20e5fc0b354239c9763fe6647de24781
Francesco Novy <francesco.novy@sentry.io>
2024-05-29 16:07:51 +0200

    fix config

--- dev-packages/test-utils/src/playwright-config.ts
+++ dev-packages/test-utils/src/playwright-config.ts
@@ -55,10 +55,11 @@ export function getPlaywrightConfig(
         name: 'chromium',
         use: {
           // This comes from `devices["Desktop Chrome"]
+          // We inline this instead of importing this,
+          // because playwright otherwise complains that it was imported twice :(
           userAgent:
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.26 Safari/537.36',
           viewport: { width: 1280, height: 720 },
-          screen: { width: 1920, height: 1080 },
           deviceScaleFactor: 1,
           isMobile: false,
           hasTouch: false,
