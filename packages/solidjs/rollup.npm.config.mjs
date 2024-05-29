752b61812061de77c491a70ea342b7b3c1b1c9a6
Andrei Borza <andrei.borza@sentry.io>
2024-05-29 10:14:47 +0200

    feat(solidjs): Remove unnecessary external deps in rollup config

--- packages/solidjs/rollup.npm.config.mjs
+++ packages/solidjs/rollup.npm.config.mjs
@@ -1,9 +1,3 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollup-utils';
 
-export default makeNPMConfigVariants(
-  makeBaseNPMConfig({
-    packageSpecificConfig: {
-      external: ['solid-js', 'solid-js/web', '@solidjs/router'],
-    },
-  }),
-);
+export default makeNPMConfigVariants(makeBaseNPMConfig());
