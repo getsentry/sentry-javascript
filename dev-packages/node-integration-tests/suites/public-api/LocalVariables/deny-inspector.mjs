dc6bbec9705bdfae76cec405822ee3d37e3d05b3
Tim Fish <tim@timfish.uk>
2024-05-29 11:53:32 +0200

    test: Fix flakey `inspector` test (#12274)
    Uses an alternative test method.
    
    Should reduce failures like
    [this](https://github.com/getsentry/sentry-javascript/actions/runs/9281632639/job/25538103763).

--- dev-packages/node-integration-tests/suites/public-api/LocalVariables/deny-inspector.mjs
+++ dev-packages/node-integration-tests/suites/public-api/LocalVariables/deny-inspector.mjs
@@ -1,13 +1,21 @@
-import * as Sentry from '@sentry/node';
-import Hook from 'import-in-the-middle';
+import { register } from 'node:module';
 
-Hook((_, name) => {
-  if (name === 'inspector') {
-    throw new Error('No inspector!');
+const hookScript = Buffer.from(`
+
+  `);
+
+register(
+  new URL(`data:application/javascript,
+export async function resolve(specifier, context, nextResolve) {
+  if (specifier === 'node:inspector' || specifier === 'inspector') {
+    throw new Error('Should not use node:inspector module');
   }
-  if (name === 'node:inspector') {
-    throw new Error('No inspector!');
-  }
-});
+
+  return nextResolve(specifier);
+}`),
+  import.meta.url,
+);
+
+const Sentry = await import('@sentry/node');
 
 Sentry.init({});
