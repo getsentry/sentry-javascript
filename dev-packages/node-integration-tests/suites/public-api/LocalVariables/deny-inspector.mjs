c48458e8aaacf924da4005b255a789e5312af0b5
Tim Fish <tim@timfish.uk>
2024-05-29 11:35:13 +0200

    lint

--- dev-packages/node-integration-tests/suites/public-api/LocalVariables/deny-inspector.mjs
+++ dev-packages/node-integration-tests/suites/public-api/LocalVariables/deny-inspector.mjs
@@ -4,14 +4,17 @@ const hookScript = Buffer.from(`
 
   `);
 
-register(new URL(`data:application/javascript,
+register(
+  new URL(`data:application/javascript,
 export async function resolve(specifier, context, nextResolve) {
   if (specifier === 'node:inspector' || specifier === 'inspector') {
     throw new Error('Should not use node:inspector module');
   }
 
   return nextResolve(specifier);
-}`), import.meta.url);
+}`),
+  import.meta.url,
+);
 
 const Sentry = await import('@sentry/node');
 
