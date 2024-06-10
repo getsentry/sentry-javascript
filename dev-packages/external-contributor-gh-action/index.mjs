a4c2b678c61e0099c67a99e5d3cd5ce2e528b182
Francesco Novy <francesco.novy@sentry.io>
2024-06-10 10:54:10 +0200

    WIP

---
+++ dev-packages/external-contributor-gh-action/index.mjs
@@ -0,0 +1,64 @@
+import { promises as fs } from 'node:fs';
+import path from 'node:path';
+import * as core from '@actions/core';
+
+const UNRELEASED_HEADING = `## Unreleased
+
+- "You miss 100 percent of the chances you don't take. — Wayne Gretzky" — Michael Scott
+`;
+
+const contributorMessageRegex = /Work in this release was contributed by (.+)\. Thank you for your contribution!/;
+
+async function run() {
+  const { getInput } = core;
+
+  const name = getInput('name');
+
+  if (!name) {
+    return;
+  }
+
+  const cwd = process.cwd();
+  const changelogFilePath = path.resolve(cwd, 'CHANGELOG.md');
+
+  const changelogStr = await fs.readFile(changelogFilePath, 'utf8');
+
+  // Find the unreleased section
+  const start = changelogStr.indexOf(UNRELEASED_HEADING) + UNRELEASED_HEADING.length;
+  const end = changelogStr.slice(start).indexOf('## ');
+
+  const inBetween = changelogStr.slice(start, start + end);
+
+  const existing = contributorMessageRegex.exec(inBetween);
+
+  // If the contributor message already exists, add the new contributor to the list
+  if (existing) {
+    const users = existing[1].split(/(?:,? and )|(?:, )/);
+    if (!users.includes(name)) {
+      users.push(name);
+    }
+
+    const formatter = new Intl.ListFormat('en', {
+      style: 'long',
+      type: 'conjunction',
+    });
+
+    const newContributors = formatter.format(users);
+    const newChangelog = changelogStr.replace(
+      contributorMessageRegex,
+      `Work in this release was contributed by ${newContributors}. Thank you for your contribution!`,
+    );
+
+    fs.writeFile(changelogFilePath, newChangelog);
+    return;
+  }
+
+  // If the contributor message does not exist, add it
+  const newChangelog = changelogStr.replace(
+    UNRELEASED_HEADING,
+    `${UNRELEASED_HEADING}\nWork in this release was contributed by ${name}. Thank you for your contribution!\n`,
+  );
+  fs.writeFile(changelogFilePath, newChangelog);
+}
+
+run();
