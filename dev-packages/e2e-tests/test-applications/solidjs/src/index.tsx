942406bfe7a02ef6939fc08531f326e6fbe7f114
Andrei Borza <andrei.borza@sentry.io>
2024-06-10 10:21:16 +0200

    feat(solidjs): Remove dsn from test app

--- dev-packages/e2e-tests/test-applications/solidjs/src/index.tsx
+++ dev-packages/e2e-tests/test-applications/solidjs/src/index.tsx
@@ -7,9 +7,7 @@ import PageRoot from './pageroot';
 import { routes } from './routes';
 
 Sentry.init({
-  dsn:
-    import.meta.env.PUBLIC_E2E_TEST_DSN ||
-    'https://3b6c388182fb435097f41d181be2b2ba@o4504321058471936.ingest.sentry.io/4504321066008576',
+  dsn: import.meta.env.PUBLIC_E2E_TEST_DSN,
   debug: true,
   environment: 'qa', // dynamic sampling bias to keep transactions
   integrations: [Sentry.solidRouterBrowserTracingIntegration({ useBeforeLeave, useLocation })],
