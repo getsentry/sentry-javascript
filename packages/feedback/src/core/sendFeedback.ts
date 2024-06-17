5ad693e3f9fd59ca63a162c3dc9a1d094155e6fd
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-06-17 16:22:45 -0400

    lint

--- packages/feedback/src/core/sendFeedback.ts
+++ packages/feedback/src/core/sendFeedback.ts
@@ -48,16 +48,15 @@ export const sendFeedback: SendFeedback = (
       if (
         response &&
         typeof response.statusCode === 'number' &&
-        (response.statusCode >= 200 && response.statusCode < 300)
+        response.statusCode >= 200 &&
+        response.statusCode < 300
       ) {
-        resolve(eventId)
-      }
-      else if (response.statusCode === 0) {
+        resolve(eventId);
+      } else if (response.statusCode === 0) {
         return reject(
           'Unable to send Feedback. This is because of network issues, or because you are using an ad-blocker.',
         );
-      }
-      else {
+      } else {
         return reject('Unable to send Feedback. Invalid response from server.');
       }
     });
