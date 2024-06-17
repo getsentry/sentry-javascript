a98ad0f6a97d2a326ff83b943c4449379a176730
Onur Temizkan <onur@narval.co.uk>
2024-06-17 18:31:28 +0100

    fix(remix): Export no-op as `captureRemixServerException` from client SDK (#12497)
    Resolves #9594

--- packages/remix/src/index.client.tsx
+++ packages/remix/src/index.client.tsx
@@ -1,10 +1,30 @@
 import { applySdkMetadata, setTag } from '@sentry/core';
 import { init as reactInit } from '@sentry/react';
+import { logger } from '@sentry/utils';
+import { DEBUG_BUILD } from './utils/debug-build';
 import type { RemixOptions } from './utils/remixOptions';
 export { captureRemixErrorBoundaryError } from './client/errors';
 export { withSentry } from './client/performance';
 
 export { browserTracingIntegration } from './client/browserTracingIntegration';
+
+// This is a no-op function that does nothing. It's here to make sure that the
+// function signature is the same as in the server SDK.
+// See issue: https://github.com/getsentry/sentry-javascript/issues/9594
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export async function captureRemixServerException(
+  err: unknown,
+  name: string,
+  request: Request,
+  isRemixV2: boolean,
+): Promise<void> {
+  DEBUG_BUILD &&
+    logger.warn(
+      '`captureRemixServerException` is a server-only function and should not be called in the browser. ' +
+        'This function is a no-op in the browser environment.',
+    );
+}
+/* eslint-enable @typescript-eslint/no-unused-vars */
 
 export * from '@sentry/react';
 
