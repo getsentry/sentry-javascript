55c1365f151f449dc8011f1f611b248a78e3ab1b
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-06-17 12:18:05 -0700

    Add a quick integration test that passes tags along

--- dev-packages/browser-integration-tests/suites/feedback/captureFeedback/init.js
+++ dev-packages/browser-integration-tests/suites/feedback/captureFeedback/init.js
@@ -4,5 +4,9 @@ window.Sentry = Sentry;
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  integrations: [Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.feedbackIntegration({
+      tags: { from: 'integration init' },
+    }),
+  ],
 });
