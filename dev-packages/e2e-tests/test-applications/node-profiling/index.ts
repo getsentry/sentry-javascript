780af9076f39eb1df2ae05e6b953ab7d1c8b5a4a
Abhijeet Prasad <aprasad@sentry.io>
2024-06-19 10:50:13 -0400

    test: Update profiling e2e test to use typescript

--- dev-packages/e2e-tests/test-applications/node-profiling/index.ts
+++ dev-packages/e2e-tests/test-applications/node-profiling/index.ts
@@ -1,7 +1,7 @@
-const Sentry = require('@sentry/node');
-const { nodeProfilingIntegration } = require('@sentry/profiling-node');
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 Sentry.init({
   dsn: 'https://7fa19397baaf433f919fbe02228d5470@o1137848.ingest.sentry.io/6625302',
