f2e8773834c9b0b6c299960cc70ae165881cad8c
Andrei Borza <andrei.borza@sentry.io>
2024-06-24 14:26:45 +0200

    feat(solid): Remove need to pass router hooks to solid integration

--- dev-packages/e2e-tests/test-applications/solid/src/index.tsx
+++ dev-packages/e2e-tests/test-applications/solid/src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import * as Sentry from '@sentry/solid';
-import { Router, useBeforeLeave, useLocation } from '@solidjs/router';
+import { solidRouterBrowserTracingIntegration, withSentryRouterRouting } from '@sentry/solid/solidrouter';
+import { Router } from '@solidjs/router';
 import { render } from 'solid-js/web';
 import './index.css';
 import PageRoot from './pageroot';
@@ -10,12 +11,12 @@ Sentry.init({
   dsn: import.meta.env.PUBLIC_E2E_TEST_DSN,
   debug: true,
   environment: 'qa', // dynamic sampling bias to keep transactions
-  integrations: [Sentry.solidRouterBrowserTracingIntegration({ useBeforeLeave, useLocation })],
+  integrations: [solidRouterBrowserTracingIntegration()],
   release: 'e2e-test',
   tunnel: 'http://localhost:3031/', // proxy server
   tracesSampleRate: 1.0,
 });
 
-const SentryRouter = Sentry.withSentryRouterRouting(Router);
+const SentryRouter = withSentryRouterRouting(Router);
 
 render(() => <SentryRouter root={PageRoot}>{routes}</SentryRouter>, document.getElementById('root'));
