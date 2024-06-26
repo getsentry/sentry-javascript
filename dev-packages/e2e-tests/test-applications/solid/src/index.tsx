1092d778d57066b67ac583b5b38155b8c39d9ebf
Andrei <168741329+andreiborza@users.noreply.github.com>
2024-06-26 15:47:59 +0200

    chore(solid): Add e2e test app for solid sdk without solidrouter (#12661)
    Moved the existing app to `solid-solidrouter` and created a new for
    `solid` without using any of the solid router integration.

--- dev-packages/e2e-tests/test-applications/solid/src/index.tsx
+++ dev-packages/e2e-tests/test-applications/solid/src/index.tsx
@@ -1,22 +1,19 @@
 /* @refresh reload */
 import * as Sentry from '@sentry/solid';
-import { solidRouterBrowserTracingIntegration, withSentryRouterRouting } from '@sentry/solid/solidrouter';
-import { Router } from '@solidjs/router';
 import { render } from 'solid-js/web';
+import App from './app';
 import './index.css';
-import PageRoot from './pageroot';
-import { routes } from './routes';
 
 Sentry.init({
-  dsn: import.meta.env.PUBLIC_E2E_TEST_DSN,
+  dsn:
+    import.meta.env.PUBLIC_E2E_TEST_DSN ||
+    'https://3b6c388182fb435097f41d181be2b2ba@o4504321058471936.ingest.sentry.io/4504321066008576',
   debug: true,
   environment: 'qa', // dynamic sampling bias to keep transactions
-  integrations: [solidRouterBrowserTracingIntegration()],
+  integrations: [Sentry.browserTracingIntegration()],
   release: 'e2e-test',
   tunnel: 'http://localhost:3031/', // proxy server
   tracesSampleRate: 1.0,
 });
 
-const SentryRouter = withSentryRouterRouting(Router);
-
-render(() => <SentryRouter root={PageRoot}>{routes}</SentryRouter>, document.getElementById('root'));
+render(() => <App />, document.getElementById('root'));
