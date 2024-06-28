24dfc666fae9c2f0ce827acf5ac5c44917d15e99
Abhijeet Prasad <aprasad@sentry.io>
2024-06-28 12:41:49 -0400

    chore: Remove replace-in-file and inquirer (#12687)
    ref: https://github.com/getsentry/sentry-javascript/pull/12644
    
    We can remove `replace-in-file` with some built-in functions and clean
    up our lockfile even more.
    
    Extracted from the previous PR because 1 big change was breaking CI and
    I couldn't figure out the root cause.

--- scripts/versionbump.js
+++ scripts/versionbump.js
@@ -1,21 +1,31 @@
-const replace = require('replace-in-file');
+const { readFile, writeFile } = require('node:fs').promises;
 const pjson = require(`${process.cwd()}/package.json`);
 
-const files = process.argv.slice(2);
-if (files.length === 0) {
-  console.error('Please provide files to bump');
-  process.exit(1);
+const REPLACE_REGEX = /\d+\.\d+.\d+(?:-\w+(?:\.\w+)?)?/g;
+
+async function run() {
+  const files = process.argv.slice(2);
+  if (files.length === 0) {
+    // eslint-disable-next-line no-console
+    console.error('[versionbump] Please provide files to bump');
+    process.exit(1);
+  }
+
+  try {
+    await Promise.all(
+      files.map(async file => {
+        const data = String(await readFile(file, 'utf8'));
+        await writeFile(file, data.replace(REPLACE_REGEX, pjson.version));
+      }),
+    );
+
+    // eslint-disable-next-line no-console
+    console.log(`[versionbump] Bumped version for ${files.join(', ')}`);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('[versionbump] Error occurred:', error);
+    process.exit(1);
+  }
 }
 
-replace({
-  files: files,
-  from: /\d+\.\d+.\d+(?:-\w+(?:\.\w+)?)?/g,
-  to: pjson.version,
-})
-  .then(changedFiles => {
-    console.log('Modified files:', changedFiles.join(', '));
-  })
-  .catch(error => {
-    console.error('Error occurred:', error);
-    process.exit(1);
-  });
+run();
