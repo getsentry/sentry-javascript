5c25ba1e92a09c4100b4da483573155224464e5a
Onur Temizkan <onur@narval.co.uk>
2024-06-28 17:14:27 +0100

    feat(otel): Add support for non-tuple OTEL time.

--- packages/opentelemetry/src/utils/convertOtelTimeToSeconds.ts
+++ packages/opentelemetry/src/utils/convertOtelTimeToSeconds.ts
@@ -1,4 +1,10 @@
 /** Convert an OTEL time to seconds */
-export function convertOtelTimeToSeconds([seconds, nano]: [number, number]): number {
+export function convertOtelTimeToSeconds(otelTime: [number, number] | number): number {
+  if (!Array.isArray(otelTime)) {
+    return otelTime;
+  }
+
+  const [seconds, nano] = otelTime;
+
   return seconds + nano / 1_000_000_000;
 }
