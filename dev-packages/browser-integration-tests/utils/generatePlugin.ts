de07108d8eab2977130dbaf4c91de1ffa7bba6fd
Francesco Novy <francesco.novy@sentry.io>
2024-07-01 13:27:11 +0200

    test: Fix tsconfig for browser-integration-tests (#12708)
    Not sure how this worked before, but there was a bunch of stuff that
    conflicted with `noUncheckedIndexedAccess` :thinking:
    
    Noticed this locally when running browser integration tests now.

--- dev-packages/browser-integration-tests/utils/generatePlugin.ts
+++ dev-packages/browser-integration-tests/utils/generatePlugin.ts
@@ -128,8 +128,9 @@ function generateSentryAlias(): Record<string, string> {
 
       const modulePath = path.resolve(PACKAGES_DIR, packageName);
 
-      if (useCompiledModule && bundleKey && BUNDLE_PATHS[packageName]?.[bundleKey]) {
-        const bundlePath = path.resolve(modulePath, BUNDLE_PATHS[packageName][bundleKey]);
+      const bundleKeyPath = bundleKey && BUNDLE_PATHS[packageName]?.[bundleKey];
+      if (useCompiledModule && bundleKeyPath) {
+        const bundlePath = path.resolve(modulePath, bundleKeyPath);
 
         return [packageJSON['name'], bundlePath];
       }
@@ -175,8 +176,8 @@ class SentryScenarioGenerationPlugin {
           (statement: { specifiers: [{ imported: { name: string } }] }, source: string) => {
             const imported = statement.specifiers?.[0]?.imported?.name;
 
-            if (imported && IMPORTED_INTEGRATION_CDN_BUNDLE_PATHS[imported]) {
-              const bundleName = IMPORTED_INTEGRATION_CDN_BUNDLE_PATHS[imported];
+            const bundleName = imported && IMPORTED_INTEGRATION_CDN_BUNDLE_PATHS[imported];
+            if (bundleName) {
               this.requiredIntegrations.push(bundleName);
             } else if (source === '@sentry/wasm') {
               this.requiresWASMIntegration = true;
@@ -190,7 +191,7 @@ class SentryScenarioGenerationPlugin {
       HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync(this._name, (data, cb) => {
         if (useBundleOrLoader) {
           const bundleName = 'browser';
-          const bundlePath = BUNDLE_PATHS[bundleName][bundleKey];
+          const bundlePath = BUNDLE_PATHS[bundleName]?.[bundleKey];
 
           if (!bundlePath) {
             throw new Error(`Could not find bundle or loader for key ${bundleKey}`);
@@ -215,10 +216,10 @@ class SentryScenarioGenerationPlugin {
                   '__LOADER_OPTIONS__',
                   JSON.stringify({
                     dsn: 'https://public@dsn.ingest.sentry.io/1337',
-                    ...loaderConfig.options,
+                    ...loaderConfig?.options,
                   }),
                 )
-                .replace('__LOADER_LAZY__', loaderConfig.lazy ? 'true' : 'false');
+                .replace('__LOADER_LAZY__', loaderConfig?.lazy ? 'true' : 'false');
             });
           }
 
@@ -240,36 +241,41 @@ class SentryScenarioGenerationPlugin {
                 path.resolve(
                   PACKAGES_DIR,
                   'feedback',
-                  BUNDLE_PATHS['feedback'][integrationBundleKey].replace('[INTEGRATION_NAME]', integration),
+                  BUNDLE_PATHS['feedback']?.[integrationBundleKey]?.replace('[INTEGRATION_NAME]', integration) || '',
                 ),
                 fileName,
               );
             });
           }
 
-          this.requiredIntegrations.forEach(integration => {
-            const fileName = `${integration}.bundle.js`;
-            addStaticAssetSymlink(
-              this.localOutPath,
-              path.resolve(
-                PACKAGES_DIR,
-                'browser',
-                BUNDLE_PATHS['integrations'][integrationBundleKey].replace('[INTEGRATION_NAME]', integration),
-              ),
-              fileName,
-            );
+          const baseIntegrationFileName = BUNDLE_PATHS['integrations']?.[integrationBundleKey];
 
-            const integrationObject = createHtmlTagObject('script', {
-              src: fileName,
+          if (baseIntegrationFileName) {
+            this.requiredIntegrations.forEach(integration => {
+              const fileName = `${integration}.bundle.js`;
+              addStaticAssetSymlink(
+                this.localOutPath,
+                path.resolve(
+                  PACKAGES_DIR,
+                  'browser',
+                  baseIntegrationFileName.replace('[INTEGRATION_NAME]', integration),
+                ),
+                fileName,
+              );
+
+              const integrationObject = createHtmlTagObject('script', {
+                src: fileName,
+              });
+
+              data.assetTags.scripts.unshift(integrationObject);
             });
+          }
 
-            data.assetTags.scripts.unshift(integrationObject);
-          });
-
-          if (this.requiresWASMIntegration && BUNDLE_PATHS['wasm'][integrationBundleKey]) {
+          const baseWasmFileName = BUNDLE_PATHS['wasm']?.[integrationBundleKey];
+          if (this.requiresWASMIntegration && baseWasmFileName) {
             addStaticAssetSymlink(
               this.localOutPath,
-              path.resolve(PACKAGES_DIR, 'wasm', BUNDLE_PATHS['wasm'][integrationBundleKey]),
+              path.resolve(PACKAGES_DIR, 'wasm', baseWasmFileName),
               'wasm.bundle.js',
             );
 
