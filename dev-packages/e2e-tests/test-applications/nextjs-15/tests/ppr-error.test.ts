d35762ae0bb28fee7be228fc58a7a819804c6703
Luca Forstner <luca.forstner@sentry.io>
2024-07-02 15:34:43 +0000

    Fix next-15 e2e tests

--- dev-packages/e2e-tests/test-applications/nextjs-15/tests/ppr-error.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-15/tests/ppr-error.test.ts
@@ -3,17 +3,20 @@ import { waitForError, waitForTransaction } from '@sentry-internal/test-utils';
 
 test('should not capture React-internal errors for PPR rendering', async ({ page }) => {
   const pageServerComponentTransactionPromise = waitForTransaction('nextjs-15', async transactionEvent => {
-    return transactionEvent?.transaction === 'Page Server Component (/ppr-error/[param])';
+    return transactionEvent?.transaction === 'GET /ppr-error/[param]';
   });
 
   let errorEventReceived = false;
-  waitForError('nextjs-15', async transactionEvent => {
-    return transactionEvent?.transaction === 'Page Server Component (/ppr-error/[param])';
+  waitForError('nextjs-15', async errorEvent => {
+    return errorEvent?.transaction === 'Page Server Component (/ppr-error/[param])';
   }).then(() => {
     errorEventReceived = true;
   });
 
   await page.goto(`/ppr-error/foobar?id=1`);
+
+  // Just to be a little bit more sure
+  await page.waitForTimeout(5000);
 
   const pageServerComponentTransaction = await pageServerComponentTransactionPromise;
   expect(pageServerComponentTransaction).toBeDefined();
