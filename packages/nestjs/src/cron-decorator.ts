8e79b96baa180bf9509a2682bb2ac89ae3277b3f
nicohrubec <nicolas.hrubec@outlook.com>
2024-07-08 13:58:35 +0200

    Update decorator

--- packages/nestjs/src/cron-decorator.ts
+++ packages/nestjs/src/cron-decorator.ts
@@ -4,23 +4,20 @@ import type { MonitorConfig } from '@sentry/types';
 /**
  * A decorator wrapping the native nest Cron decorator, sending check-ins to Sentry.
  */
-export const SentryCron = (
-  monitorSlug: string,
-  monitorConfig: MonitorConfig | undefined,
-): MethodDecorator => {
-  return (target : unknown, propertyKey, descriptor: PropertyDescriptor) => {
+export const SentryCron = (monitorSlug: string, monitorConfig: MonitorConfig | undefined): MethodDecorator => {
+  return (target: unknown, propertyKey, descriptor: PropertyDescriptor) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const originalMethod = descriptor.value as (...args: any[]) => Promise<any>;
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     descriptor.value = async function (...args: any[]) {
-      Sentry.withMonitor(
+      return Sentry.withMonitor(
         monitorSlug,
-        () => {
+        async () => {
           return originalMethod.apply(this, args);
         },
         monitorConfig,
-      )
+      );
     };
     return descriptor;
   };
