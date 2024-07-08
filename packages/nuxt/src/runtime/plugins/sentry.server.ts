dcadf221b6fe6090edcbfb443a3d425b5360488e
s1gr1d <sigrid.huemer@posteo.at>
2024-07-08 14:53:00 +0200

    feat(nuxt): Add server error hook

---
+++ packages/nuxt/src/runtime/plugins/sentry.server.ts
@@ -0,0 +1,23 @@
+import { captureException } from '@sentry/node';
+import { H3Error } from 'h3';
+import { defineNitroPlugin } from 'nitropack/runtime';
+
+export default defineNitroPlugin(nitroApp => {
+  nitroApp.hooks.hook('error', (error, context) => {
+    // Do not handle 404 and 422
+    if (error instanceof H3Error) {
+      if (error.statusCode === 404 || error.statusCode === 422) {
+        return;
+      }
+    }
+
+    if (context) {
+      captureException(error, {
+        captureContext: { extra: { nuxt: context } },
+        mechanism: { handled: false },
+      });
+    } else {
+      captureException(error);
+    }
+  });
+});
