795d1eacd9bdec38907fdde8a4bd2c03b5854356
s1gr1d <sigrid.huemer@posteo.at>
2024-07-09 10:35:11 +0200

    extract structured context

--- packages/nuxt/src/runtime/plugins/sentry.server.ts
+++ packages/nuxt/src/runtime/plugins/sentry.server.ts
@@ -1,9 +1,10 @@
 import { captureException } from '@sentry/node';
 import { H3Error } from 'h3';
 import { defineNitroPlugin } from 'nitropack/runtime';
+import { extractErrorContext } from '../utils';
 
 export default defineNitroPlugin(nitroApp => {
-  nitroApp.hooks.hook('error', (error, context) => {
+  nitroApp.hooks.hook('error', (error, errorContext) => {
     // Do not handle 404 and 422
     if (error instanceof H3Error) {
       if (error.statusCode === 404 || error.statusCode === 422) {
@@ -11,9 +12,11 @@ export default defineNitroPlugin(nitroApp => {
       }
     }
 
-    if (context) {
+    if (errorContext) {
+      const structuredContext = extractErrorContext(errorContext);
+
       captureException(error, {
-        captureContext: { contexts: { nuxt: context } },
+        captureContext: { contexts: { nuxt: structuredContext } },
         mechanism: { handled: false },
       });
     } else {
