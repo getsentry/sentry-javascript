5963dbeecd76cb3a1b9c70b8a2af9da4580bdba2
nicohrubec <nicolas.hrubec@outlook.com>
2024-07-09 15:39:04 +0200

    Add nest error handler setup to nest SDK

--- packages/nestjs/src/index.ts
+++ packages/nestjs/src/index.ts
@@ -1,6 +1,7 @@
 export * from '@sentry/node';
 
 export { init } from './sdk';
+export { nestIntegration, setupNestErrorHandler } from './setup';
 
 export { SentryTraced } from './span-decorator';
 export { SentryCron } from './cron-decorator';
