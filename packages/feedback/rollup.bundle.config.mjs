82da8c899f473083d37fc6d8553afa19e3083e58
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-07-10 09:39:04 -0700

    rm bonus changes

--- packages/feedback/rollup.bundle.config.mjs
+++ packages/feedback/rollup.bundle.config.mjs
@@ -19,21 +19,6 @@ export default [
   ...makeBundleConfigVariants(
     makeBaseBundleConfig({
       bundleType: 'addon',
-      entrypoints: ['src/core/integration.ts'],
-      jsVersion: 'es6',
-      licenseTitle: '@sentry-internal/feedback-core',
-      outputFileBase: () => 'bundles/feedback-core',
-      sucrase: {
-        // The feedback widget is using preact so we need different pragmas and jsx runtimes
-        jsxPragma: 'h',
-        jsxFragmentPragma: 'Fragment',
-        jsxRuntime: 'classic',
-      },
-    }),
-  ),
-  ...makeBundleConfigVariants(
-    makeBaseBundleConfig({
-      bundleType: 'addon',
       entrypoints: ['src/screenshot/integration.ts'],
       jsVersion: 'es6',
       licenseTitle: '@sentry-internal/feedback',
