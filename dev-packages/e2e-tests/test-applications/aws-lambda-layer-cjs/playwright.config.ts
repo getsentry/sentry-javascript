8be63cf7baabd464b141ff5d0886a89b55b05fd0
Lukas Stracke <lukas.stracke@sentry.io>
2024-07-10 10:42:46 +0200

    remove more bloat

--- dev-packages/e2e-tests/test-applications/aws-lambda-layer-cjs/playwright.config.ts
+++ dev-packages/e2e-tests/test-applications/aws-lambda-layer-cjs/playwright.config.ts
@@ -1,8 +1,3 @@
 import { getPlaywrightConfig } from '@sentry-internal/test-utils';
 
-// Fix urls not resolving to localhost on Node v17+
-// See: https://github.com/axios/axios/issues/3821#issuecomment-1413727575
-import { setDefaultResultOrder } from 'dns';
-setDefaultResultOrder('ipv4first');
-
 export default getPlaywrightConfig();
