8e2154b8e1014db3e4c683c11e154ec53aec1f70
Luca Forstner <luca.forstner@sentry.io>
2024-07-11 15:32:38 +0000

    feat(nextjs): Add `experimental_captureRequestError`

---
+++ packages/nextjs/src/common/captureRequestError.ts
@@ -0,0 +1,46 @@
+import { captureException, withScope } from '@sentry/core';
+
+type RequestInfo = {
+  url: string;
+  method: string;
+  headers: Record<string, string | string[] | undefined>;
+};
+
+type ErrorContext = {
+  routerKind: string; // 'Pages Router' | 'App Router'
+  routePath: string;
+  routeType: string; // 'render' | 'route' | 'middleware'
+};
+
+/**
+ * Reports error for the Next.js `onRequestError` instrumentation hook.
+ */
+export function experimental_captureRequestError(
+  error: unknown,
+  request: RequestInfo,
+  errorContext: ErrorContext,
+): void {
+  withScope(scope => {
+    scope.setSDKProcessingMetadata({
+      request: {
+        headers: request.headers,
+        method: request.method,
+      },
+    });
+
+    scope.setContext('nextjs', {
+      request_path: request.url,
+      router_kind: errorContext.routerKind,
+      router_path: errorContext.routePath,
+      route_type: errorContext.routeType,
+    });
+
+    scope.setTransactionName(errorContext.routePath);
+
+    captureException(error, {
+      mechanism: {
+        handled: false,
+      },
+    });
+  });
+}
