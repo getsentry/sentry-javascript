dbf33fcbbbdd68c00cad7c57e231e105a2a63f0a
nicohrubec <nicolas.hrubec@outlook.com>
2024-07-11 15:29:58 +0200

    Bsaeline

--- dev-packages/e2e-tests/test-applications/nestjs/src/main.ts
+++ dev-packages/e2e-tests/test-applications/nestjs/src/main.ts
@@ -2,7 +2,7 @@
 import './instrument';
 
 // Import other modules
-import { BaseExceptionFilter, HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import * as Sentry from '@sentry/nestjs';
 import { AppModule1, AppModule2 } from './app.module';
 
@@ -10,10 +10,9 @@ const app1Port = 3030;
 const app2Port = 3040;
 
 async function bootstrap() {
-  const app1 = await NestFactory.create(AppModule1);
+  let app1 = await NestFactory.create(AppModule1);
 
-  const { httpAdapter } = app1.get(HttpAdapterHost);
-  Sentry.setupNestErrorHandler(app1, new BaseExceptionFilter(httpAdapter));
+  Sentry.setupNestErrorHandler(app1);
 
   await app1.listen(app1Port);
 
--- packages/nestjs/src/setup.ts
+++ packages/nestjs/src/setup.ts
@@ -1,3 +1,5 @@
+import type { ArgumentsHost } from '@nestjs/common';
+import { DiscoveryService } from '@nestjs/core';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import {
   SEMANTIC_ATTRIBUTE_SENTRY_OP,
@@ -40,9 +42,23 @@ interface MinimalNestJsApp {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     intercept: (context: MinimalNestJsExecutionContext, next: { handle: () => any }) => any;
   }) => void;
+
+  get: <T>(type: new (...args: any[]) => T) => T;
+}
+
+interface ExceptionFilterInstance {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  catch: (exception: unknown, host: ArgumentsHost) => any;
+}
+
+interface Provider {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  metatype: any;
+  instance: ExceptionFilterInstance;
 }
 
 const INTEGRATION_NAME = 'Nest';
+const CATCH_WATERMARK = '__catch__';
 
 export const instrumentNest = generateInstrumentOnce(INTEGRATION_NAME, () => new NestInstrumentation());
 
@@ -65,7 +81,7 @@ export const nestIntegration = defineIntegration(_nestIntegration);
 /**
  * Setup an error handler for Nest.
  */
-export function setupNestErrorHandler(app: MinimalNestJsApp, baseFilter: NestJsErrorFilter): void {
+export function setupNestErrorHandler(app: MinimalNestJsApp): void {
   // Sadly, NestInstrumentation has no requestHook, so we need to add the attributes here
   // We register this hook in this method, because if we register it in the integration `setup`,
   // it would always run even for users that are not even using Nest.js
@@ -95,28 +111,7 @@ export function setupNestErrorHandler(app: MinimalNestJsApp, baseFilter: NestJsE
     },
   });
 
-  const wrappedFilter = new Proxy(baseFilter, {
-    get(target, prop, receiver) {
-      if (prop === 'catch') {
-        const originalCatch = Reflect.get(target, prop, receiver);
-
-        return (exception: unknown, host: unknown) => {
-          const status_code = (exception as { status?: number }).status;
-
-          // don't report expected errors
-          if (status_code !== undefined && status_code >= 400 && status_code < 500) {
-            return originalCatch.apply(target, [exception, host]);
-          }
-
-          captureException(exception);
-          return originalCatch.apply(target, [exception, host]);
-        };
-      }
-      return Reflect.get(target, prop, receiver);
-    },
-  });
-
-  app.useGlobalFilters(wrappedFilter);
+  checkinExceptionFilters(app);
 }
 
 function addNestSpanAttributes(span: Span): void {
@@ -133,5 +128,32 @@ function addNestSpanAttributes(span: Span): void {
   span.setAttributes({
     [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.http.otel.nestjs',
     [SEMANTIC_ATTRIBUTE_SENTRY_OP]: `${type}.nestjs`,
+  });
+}
+
+function checkinExceptionFilters(app: MinimalNestJsApp): void {
+  const discoveryService = app.get(DiscoveryService);
+  const providers = discoveryService.getProviders() as Provider[];
+  const exceptionFilters = providers.filter(
+    ({ metatype }) => metatype && Reflect.getMetadata(CATCH_WATERMARK, metatype),
+  );
+
+  exceptionFilters.map(mod => {
+    const instance = mod.instance;
+    const originalCatch = instance.catch;
+
+    instance.catch = function (exception: unknown, host) {
+      const status_code = (exception as { status?: number }).status;
+
+      // don't report expected errors
+      if (status_code !== undefined && status_code >= 400 && status_code < 500) {
+        return originalCatch.apply(target, [exception, host]);
+      }
+
+      captureException(exception);
+      return originalCatch.apply(this, [exception, host]);
+    };
+
+    return mod;
   });
 }
