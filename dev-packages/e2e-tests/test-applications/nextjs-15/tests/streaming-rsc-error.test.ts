4eace9b8354b02272050db8934742c9c5f794a5a
Luca Forstner <luca.forstner@sentry.io>
2024-07-15 13:00:12 +0000

    fix

--- dev-packages/e2e-tests/test-applications/nextjs-15/tests/streaming-rsc-error.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-15/tests/streaming-rsc-error.test.ts
@@ -9,7 +9,7 @@ test('Should capture errors for crashing streaming promises in server components
   const [, minor, patch, canary] = packageJson.dependencies.next.split('.');
 
   test.skip(
-    minor === '0' && patch === '0' && patch.includes('canary') && Number(canary) < 63,
+    minor === '0' && patch === '0' && ((patch.includes('canary') && Number(canary) < 63) || patch.includes('rc')),
     'Next.js version does not expose these errors',
   );
 
