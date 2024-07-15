27fad2266de76c58022a869ba2f835cd750c0f04
nicohrubec <nicolas.hrubec@outlook.com>
2024-07-15 16:59:53 +0200

    Move interceptor to separate class

--- packages/nestjs/src/setup.ts
+++ packages/nestjs/src/setup.ts
@@ -1,14 +1,10 @@
-import { Logger } from '@nestjs/common';
-import type { DynamicModule, OnModuleInit } from '@nestjs/common';
+import type {CallHandler, DynamicModule, ExecutionContext, NestInterceptor, OnModuleInit} from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { Global, Module } from '@nestjs/common';
-import type { ModuleRef } from '@nestjs/core';
-import {BaseExceptionFilter} from '@nestjs/core';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import {
   SEMANTIC_ATTRIBUTE_SENTRY_OP,
   SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN,
-  captureException,
   defineIntegration,
   getClient,
   getDefaultIsolationScope,
@@ -18,37 +14,8 @@ import {
 import { generateInstrumentOnce } from '@sentry/node';
 import type { IntegrationFn, Span } from '@sentry/types';
 import { logger } from '@sentry/utils';
-
-interface MinimalNestJsExecutionContext {
-  getType: () => string;
-
-  switchToHttp: () => {
-    // minimal request object
-    // according to official types, all properties are required but
-    // let's play it safe and assume they're optional
-    getRequest: () => {
-      route?: {
-        path?: string;
-      };
-      method?: string;
-    };
-  };
-}
-
-interface NestJsErrorFilter {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  catch(exception: any, host: any): void;
-}
-
-interface MinimalNestJsApp {
-  useGlobalFilters: (arg0: NestJsErrorFilter) => void;
-  useGlobalInterceptors: (interceptor: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    intercept: (context: MinimalNestJsExecutionContext, next: { handle: () => any }) => any;
-  }) => void;
-
-  get: <T>(type: new (...args: any[]) => T) => T;
-}
+import type { Observable } from 'rxjs';
+import {APP_INTERCEPTOR} from '@nestjs/core';
 
 const INTEGRATION_NAME = 'Nest';
 
@@ -71,75 +38,45 @@ const _nestIntegration = (() => {
 export const nestIntegration = defineIntegration(_nestIntegration);
 
 /**
+ * 
+ */
+export class SentryTracingInterceptor implements NestInterceptor {
+  /**
+   *
+   */
+  public intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    if (getIsolationScope() === getDefaultIsolationScope()) {
+      logger.warn('Isolation scope is still the default isolation scope, skipping setting transactionName.');
+      return next.handle();
+    }
+
+    if (context.getType() === 'http') {
+      const req = context.switchToHttp().getRequest();
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (req.route) {
+        // eslint-disable-next-line @sentry-internal/sdk/no-optional-chaining,@typescript-eslint/no-unsafe-member-access
+        getIsolationScope().setTransactionName(`${req.method?.toUpperCase() || 'GET'} ${req.route.path}`);
+      }
+    }
+
+    return next.handle();
+  }
+}
+
+/**
  * Set up a nest service that provides error handling and performance tracing.
  */
 export class SentryIntegrationService implements OnModuleInit {
-  // eslint-disable-next-line @sentry-internal/sdk/no-class-field-initializers
-  private readonly _logger = new Logger(SentryIntegrationService.name);
-
-  public constructor(
-    private readonly _moduleRef: ModuleRef
-  ) {}
-
   /**
    * Called when the SentryModuleIntegration gets initialized.
    */
   public onModuleInit(): void {
-    const app: MinimalNestJsApp = this._moduleRef.get('NestApplication');
-    const baseFilter: NestJsErrorFilter = new BaseExceptionFilter();
-
-    if (!app) {
-      this._logger.warn('Failed to retrieve the application instance.');
-      return;
-    }
-
     const client = getClient();
     if (client) {
       client.on('spanStart', span => {
         addNestSpanAttributes(span);
       });
     }
-
-    app.useGlobalInterceptors({
-      intercept(context, next) {
-        if (getIsolationScope() === getDefaultIsolationScope()) {
-          logger.warn('Isolation scope is still the default isolation scope, skipping setting transactionName.');
-          return next.handle();
-        }
-
-        if (context.getType() === 'http') {
-          const req = context.switchToHttp().getRequest();
-          if (req.route) {
-            // eslint-disable-next-line @sentry-internal/sdk/no-optional-chaining
-            getIsolationScope().setTransactionName(`${req.method?.toUpperCase() || 'GET'} ${req.route.path}`);
-          }
-        }
-
-        return next.handle();
-      },
-    });
-
-    const wrappedFilter = new Proxy(baseFilter, {
-      get(target, prop, receiver) {
-        if (prop === 'catch') {
-          const originalCatch = Reflect.get(target, prop, receiver);
-
-          return (exception: unknown, host: unknown) => {
-            const status_code = (exception as { status?: number }).status;
-
-            if (status_code !== undefined && status_code >= 400 && status_code < 500) {
-              return originalCatch.apply(target, [exception, host]);
-            }
-
-            captureException(exception);
-            return originalCatch.apply(target, [exception, host]);
-          };
-        }
-        return Reflect.get(target, prop, receiver);
-      },
-    });
-
-    app.useGlobalFilters(wrappedFilter);
   }
 }
 
@@ -153,7 +90,13 @@ export class SentryIntegrationModule {
   public static forRoot(): DynamicModule {
     return {
       module: SentryIntegrationModule,
-      providers: [SentryIntegrationService],
+      providers: [
+        SentryIntegrationService,
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: SentryTracingInterceptor
+        }
+      ],
       exports: [SentryIntegrationService],
     };
   }
@@ -176,6 +119,7 @@ function addNestSpanAttributes(span: Span): void {
   });
 }
 
+Injectable()(SentryTracingInterceptor);
 Injectable()(SentryIntegrationService);
 Global()(SentryIntegrationModule);
 Module({
