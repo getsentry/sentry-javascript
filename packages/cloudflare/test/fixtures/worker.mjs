824155587afea50fdcbec045bb7b9f520655299b
Abhijeet Prasad <aprasad@sentry.io>
2024-07-16 17:14:19 -0400

    feat(cloudflare): Add cloudflare sdk scaffolding

--- packages/cloudflare/test/fixtures/worker.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * @type {import('@cloudflare/workers-types').ExportedHandler}
- */
-export default {
-  async fetch(_request, _env, _ctx) {
-    return new Response('Hello Sentry!');
-  },
-};
