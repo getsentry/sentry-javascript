b32759e3d340ec2204e35661efc0f7f51ba1f039
nicohrubec <nicolas.hrubec@outlook.com>
2024-07-17 15:32:46 +0200

    Lint

--- dev-packages/e2e-tests/test-applications/nestjs-with-submodules/src/app.module.ts
+++ dev-packages/e2e-tests/test-applications/nestjs-with-submodules/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ExampleModule } from "./example-module/example.module";
+import { ExampleModule } from './example-module/example.module';
 
 @Module({
   imports: [ExampleModule],
