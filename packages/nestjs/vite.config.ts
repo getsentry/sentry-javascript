cf6eb010f712cea0c016fb6701ab8a75b253988f
Abhijeet Prasad <aprasad@sentry.io>
2024-07-18 14:18:39 -0400

    test: Remove explict casting of baseconfig in vite configs (#12979)
    Something I noticed while doing my jest -> vitest conversions, we don't
    need this anymore.

--- packages/nestjs/vite.config.ts
+++ packages/nestjs/vite.config.ts
@@ -1,13 +1,10 @@
-import type { UserConfig } from 'vitest';
 import { defineConfig } from 'vitest/config';
 import baseConfig from '../../vite/vite.config';
 
 export default defineConfig({
   ...baseConfig,
   test: {
-    // test exists, no idea why TS doesn't recognize it
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ...(baseConfig as UserConfig & { test: any }).test,
+    ...baseConfig.test,
     environment: 'node',
   },
 });
