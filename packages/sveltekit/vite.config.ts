ea815155c87b879c955d5ff4585340dd627f61e1
Abhijeet Prasad <aprasad@sentry.io>
2024-07-18 10:22:13 -0400

    test: Remove explict casting of baseconfig in vite configs

--- packages/sveltekit/vite.config.ts
+++ packages/sveltekit/vite.config.ts
@@ -1,16 +1,12 @@
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-import type { UserConfig } from 'vitest';
-
 import baseConfig from '../../vite/vite.config';
 
 export default {
   ...baseConfig,
   test: {
-    // test exists, no idea why TS doesn't recognize it
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ...(baseConfig as UserConfig & { test: any }).test,
+    ...baseConfig.test,
     setupFiles: ['./test/vitest.setup.ts'],
     alias: [
       {
