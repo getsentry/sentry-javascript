c48d87cafcd95f2404161a68f5071585085e6bf7
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-07-22 16:55:18 +0200

    test(nuxt): Add Nuxt 3 E2E test for client-side (#13002)
    Adding E2E test app with `@nuxt/test-utils` based on the [Nuxt
    docs](https://nuxt.com/docs/getting-started/testing#testing-in-a-browser).

---
+++ dev-packages/e2e-tests/test-applications/nuxt-3/components/ErrorButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+const triggerError = () => {
+  throw new Error('Error thrown from Nuxt-3 E2E test app');
+};
+</script>
+
+<template>
+  <button id="errorBtn" @click="triggerError">Trigger Error</button>
+</template>
