263a31f1561e5339d6fb63b2e7bfa6cdfe4e5f96
s1gr1d <sigrid.huemer@posteo.at>
2024-07-22 13:45:55 +0200

    test(nuxt): Add Nuxt 3 E2E test for client

---
+++ dev-packages/e2e-tests/test-applications/nuxt-3/tests/errors.client.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@nuxt/test-utils/playwright';
+import { waitForError } from '@sentry-internal/test-utils';
+
+test.describe('client-side errors', async () => {
+  test('captures error thrown on click', async ({ page }) => {
+    const errorPromise = waitForError('nuxt-3', async errorEvent => {
+      return errorEvent?.exception?.values?.[0]?.value === 'Error thrown from Nuxt-3 E2E test app';
+    });
+
+    await page.goto(`/client-error`);
+    await page.locator('#errorBtn').click();
+
+    const error = await errorPromise;
+
+    expect(error).toMatchObject({
+      exception: {
+        values: [
+          {
+            type: 'Error',
+            value: 'Error thrown from Nuxt-3 E2E test app',
+            mechanism: {
+              handled: false,
+            },
+          },
+        ],
+      },
+    });
+  });
+});
