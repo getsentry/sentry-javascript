5621092d01f2fbbfda741eac4826ad11d304a8c0
Francesco Novy <francesco.novy@sentry.io>
2024-07-22 16:17:33 +0200

    fix(node): Do not emit fetch spans when tracing is disabled
    Ensure that TWP works with fetch, but we do not emit a span.
    Otherwise, if a user does not use our tracing functionality but adds their own fetch instrumentation, they get spans twice.

--- dev-packages/node-integration-tests/jest.setup.js
+++ dev-packages/node-integration-tests/jest.setup.js
@@ -1,2 +1,8 @@
+const { cleanupChildProcesses } = require('./utils/runner');
+
 // Increases test timeout from 5s to 45s
 jest.setTimeout(45000);
+
+afterEach(() => {
+  cleanupChildProcesses();
+});
