adb332c75ad7761cab4ee6105c9f4af32168e22c
s1gr1d <sigrid.huemer@posteo.at>
2024-07-23 10:56:50 +0200

    always add default integrations and browsertracing

--- packages/nuxt/src/client/sdk.ts
+++ packages/nuxt/src/client/sdk.ts
@@ -1,11 +1,10 @@
 import {
-  type BrowserOptions,
   browserTracingIntegration,
   getDefaultIntegrations as getBrowserDefaultIntegrations,
   init as initBrowser,
 } from '@sentry/browser';
-import { applySdkMetadata, hasTracingEnabled } from '@sentry/core';
-import type { Client, Integration } from '@sentry/types';
+import { applySdkMetadata } from '@sentry/core';
+import type { Client } from '@sentry/types';
 import type { SentryNuxtOptions } from '../common/types';
 
 /**
@@ -15,26 +14,11 @@ import type { SentryNuxtOptions } from '../common/types';
  */
 export function init(options: SentryNuxtOptions): Client | undefined {
   const sentryOptions = {
-    defaultIntegrations: getDefaultIntegrations(options),
+    defaultIntegrations: [...getBrowserDefaultIntegrations(options), browserTracingIntegration()],
     ...options,
   };
 
   applySdkMetadata(sentryOptions, 'nuxt', ['nuxt', 'vue']);
 
   return initBrowser(sentryOptions);
-}
-
-// Tree-shakable guard to remove all code related to tracing
-declare const __SENTRY_TRACING__: boolean;
-
-function getDefaultIntegrations(options: BrowserOptions): Integration[] | undefined {
-  // This evaluates to true unless __SENTRY_TRACING__ is text-replaced with "false",
-  // in which case everything inside will get tree-shaken away
-  if (typeof __SENTRY_TRACING__ === 'undefined' || __SENTRY_TRACING__) {
-    if (hasTracingEnabled(options)) {
-      return [...getBrowserDefaultIntegrations(options), browserTracingIntegration()];
-    }
-  }
-
-  return undefined;
 }
