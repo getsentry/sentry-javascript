a508a2b5f9375edad8801e65dfbfd1dd9233de44
Charly Gomez <charly.gomez@sentry.io>
2024-07-25 11:17:14 +0200

    chore: remove unused event proxy

--- dev-packages/e2e-tests/test-applications/node-otel-custom-sampler/src/instrument.ts
+++ dev-packages/e2e-tests/test-applications/node-otel-custom-sampler/src/instrument.ts
@@ -12,6 +12,7 @@ Sentry.init({
   debug: !!process.env.DEBUG,
   tunnel: `http://localhost:3031/`, // proxy server
   skipOpenTelemetrySetup: true,
+  // By defining _any_ sample rate, tracing intergations will be added by default
   tracesSampleRate: 1,
 });
 
