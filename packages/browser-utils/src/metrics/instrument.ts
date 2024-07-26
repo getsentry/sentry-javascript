29672a07e504f295f5bf2d5d71939c00cf819d81
Lukas Stracke <lukas.stracke@sentry.io>
2024-07-26 09:38:49 +0200

    use own perofrmance observer to decouple from replay

--- packages/browser-utils/src/metrics/instrument.ts
+++ packages/browser-utils/src/metrics/instrument.ts
@@ -226,7 +226,9 @@ function instrumentCls(): StopListening {
       });
       _previousCls = metric;
     },
-    { reportAllChanges: false },
+    // We want the callback to be called whenever the CLS value updates.
+    // By default, the callback is only called when the tab goes to the background.
+    { reportAllChanges: true },
   );
 }
 
