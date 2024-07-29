a903ba0f4f76d7c1fda866788f635992232a446d
Francesco Novy <francesco.novy@sentry.io>
2024-07-29 14:14:13 +0200

    fix unnecessary

--- packages/browser/src/integrations-bundle/index.feedback.ts
+++ packages/browser/src/integrations-bundle/index.feedback.ts
@@ -2,6 +2,6 @@ import { feedbackAsyncIntegration } from '../feedbackAsync';
 
 export { getFeedback } from '@sentry-internal/feedback';
 
-export { feedbackAsyncIntegration as feedbackAsyncIntegration, feedbackAsyncIntegration as feedbackIntegration };
+export { feedbackAsyncIntegration, feedbackAsyncIntegration as feedbackIntegration };
 
 export { captureFeedback } from '@sentry/core';
