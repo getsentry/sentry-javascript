33146ff42f921831116c028ba90fd8ad88f6112f
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-07-29 01:25:01 -0400

    fix browser test

--- packages/replay-internal/test/unit/util/createPerformanceEntry.test.ts
+++ packages/replay-internal/test/unit/util/createPerformanceEntry.test.ts
@@ -89,7 +89,7 @@ describe('Unit | util | createPerformanceEntries', () => {
   });
 
   describe('getCumulativeLayoutShift', () => {
-    it('works with an CLS metric', async () => {
+    it('works with a CLS metric', async () => {
       const metric = {
         value: 5108.299,
         rating: 'good' as const,
@@ -103,7 +103,7 @@ describe('Unit | util | createPerformanceEntries', () => {
         name: 'cumulative-layout-shift',
         start: 1672531205.108299,
         end: 1672531205.108299,
-        data: { value: 5108.299, size: 5108.299, rating: 'good', nodeId: undefined },
+        data: { value: 5108.299, size: 5108.299, rating: 'good', nodeId: [] },
       });
     });
   });
