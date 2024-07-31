edda5a37f7e8d50c4157aaf23c2c2a7d00fdd543
Tim Fish <tim@timfish.uk>
2024-07-31 21:47:28 +0200

    lint

--- dev-packages/node-integration-tests/suites/esm/import-in-the-middle/app.mjs
+++ dev-packages/node-integration-tests/suites/esm/import-in-the-middle/app.mjs
@@ -3,10 +3,10 @@ import * as Sentry from '@sentry/node';
 import * as iitm from 'import-in-the-middle';
 
 new iitm.Hook((_, name) => {
-  if(name !== 'http') {
+  if (name !== 'http') {
     throw new Error(`'http' should be the only hooked modules but we just hooked '${name}'`);
   }
-})
+});
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
@@ -19,4 +19,3 @@ Sentry.init({
 await import('./sub-module.mjs');
 await import('http');
 await import('os');
-
