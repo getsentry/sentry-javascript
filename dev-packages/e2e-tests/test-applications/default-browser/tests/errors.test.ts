859ec814f2f8f0ef703d49d246c62a69fbc75e15
Kaung Zin Hein <kaungzinhein113@gmail.com>
2024-07-30 20:56:10 -0400

    test(browser): Add error event test

--- dev-packages/e2e-tests/test-applications/default-browser/tests/errors.test.ts
+++ dev-packages/e2e-tests/test-applications/default-browser/tests/errors.test.ts
@@ -1,5 +1,30 @@
 import { expect, test } from '@playwright/test';
+import { waitForError } from '@sentry-internal/test-utils';
 
-test('testing', () => {
-  expect(true).toBe(true);
+test('Should send correct error event', async ({ page }) => {
+  const errorEventPromise = waitForError('default-browser', event => {
+    return !event.type && event.exception?.values?.[0]?.value === 'I am an error!';
+  });
+
+  await page.goto('/');
+
+  const exceptionButton = page.locator('id=exception-button');
+  await exceptionButton.click();
+
+  const errorEvent = await errorEventPromise;
+
+  expect(errorEvent.exception?.values).toHaveLength(1);
+  expect(errorEvent.exception?.values?.[0]?.value).toBe('I am an error!');
+
+  expect(errorEvent.transaction).toBe('/');
+
+  expect(errorEvent.request).toEqual({
+    url: 'http://localhost:3030/',
+    headers: expect.any(Object),
+  });
+
+  expect(errorEvent.contexts?.trace).toEqual({
+    trace_id: expect.any(String),
+    span_id: expect.any(String),
+  });
 });
