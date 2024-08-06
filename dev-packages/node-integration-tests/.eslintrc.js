0f13518f8d9d1995df9508b86b9f5af88f7d6127
Lukas Stracke <lukas.stracke@sentry.io>
2024-08-06 14:26:21 +0200

    chore(lint): Allow `ts-ignore` in Node integration tests

--- dev-packages/node-integration-tests/.eslintrc.js
+++ dev-packages/node-integration-tests/.eslintrc.js
@@ -20,6 +20,14 @@ module.exports = {
       },
       rules: {
         '@typescript-eslint/typedef': 'off',
+        // Explicitly allow ts-ignore with description for Node integration tests
+        // Reason: We run these tests on TS3.8 which doesn't support `@ts-expect-error`
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            'ts-ignore': 'allow-with-description',
+          },
+        ],
       },
     },
   ],
