3715f11d9523adfc9e7626428cda103cdf90ce9d
Andrei Borza <andrei.borza@sentry.io>
2024-08-07 16:42:59 +0200

    fix(aws-serverless): Extract sentry trace data from handler `context` over `event`

--- packages/aws-serverless/src/integration/awslambda.ts
+++ packages/aws-serverless/src/integration/awslambda.ts
@@ -1,20 +1,42 @@
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
 import { SEMANTIC_ATTRIBUTE_SENTRY_OP, SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN, defineIntegration } from '@sentry/core';
-import { addOpenTelemetryInstrumentation } from '@sentry/node';
+import { generateInstrumentOnce } from '@sentry/node';
 import type { IntegrationFn } from '@sentry/types';
+import { eventContextExtractor } from '../utils';
 
-const _awsLambdaIntegration = (() => {
+interface AwsLambdaOptions {
+  /**
+   * Disables the AWS context propagation and instead uses
+   * Sentry's context. This should usually be `true` when
+   * using Sentry to instrument Lambdas.
+   */
+  disableAwsContextPropagation?: boolean;
+}
+
+export const instrumentAwsLambda = generateInstrumentOnce<AwsLambdaOptions>(
+  'AwsLambda',
+  (_options: AwsLambdaOptions = {}) => {
+    const options = {
+      disableAwsContextPropagation: true,
+      ..._options,
+    };
+
+    return new AwsLambdaInstrumentation({
+      ...options,
+      eventContextExtractor,
+      requestHook(span) {
+        span.setAttribute(SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN, 'auto.otel.aws-lambda');
+        span.setAttribute(SEMANTIC_ATTRIBUTE_SENTRY_OP, 'function.aws.lambda');
+      },
+    });
+  },
+);
+
+const _awsLambdaIntegration = ((options: AwsLambdaOptions = {}) => {
   return {
     name: 'AwsLambda',
     setupOnce() {
-      addOpenTelemetryInstrumentation(
-        new AwsLambdaInstrumentation({
-          requestHook(span) {
-            span.setAttribute(SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN, 'auto.otel.aws-lambda');
-            span.setAttribute(SEMANTIC_ATTRIBUTE_SENTRY_OP, 'function.aws.lambda');
-          },
-        }),
-      );
+      instrumentAwsLambda(options);
     },
   };
 }) satisfies IntegrationFn;
