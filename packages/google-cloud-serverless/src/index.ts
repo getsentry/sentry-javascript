7190436066b1f7844a6f8b682fa0fb1ea43f08c3
Luca Forstner <luca.forstner@sentry.io>
2024-08-09 09:41:58 +0000

    fix exports

--- packages/google-cloud-serverless/src/index.ts
+++ packages/google-cloud-serverless/src/index.ts
@@ -57,6 +57,7 @@ export {
   nodeContextIntegration,
   localVariablesIntegration,
   requestDataIntegration,
+  fsIntegration,
   functionToStringIntegration,
   inboundFiltersIntegration,
   linkedErrorsIntegration,
