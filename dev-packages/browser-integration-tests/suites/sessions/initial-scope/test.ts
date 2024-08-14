23980a0470ddf1a5fae0843d98cc86e2e2e6b628
Francesco Novy <francesco.novy@sentry.io>
2024-08-14 13:00:32 +0200

    test: Ensure browser-integration-tests cannot conflict due to build (#13338)
    This streamlines some stuff in our browser integration tests, to fix
    some flakiness (hopefully).
    
    The biggest change is that instead of always building into `dist` for
    each test file, each test will now build into a random subfolder, e.g.
    `dist/abc`. This way, multiple tests in a single file will never
    conflict with each other.
    
    Additionally it also streamlines some of the tests I encountered while
    looking at stuff, hopefully reducing flakes further.
    
    Closes https://github.com/getsentry/sentry-javascript/issues/13321

--- dev-packages/browser-integration-tests/suites/sessions/initial-scope/test.ts
+++ dev-packages/browser-integration-tests/suites/sessions/initial-scope/test.ts
@@ -16,14 +16,10 @@ sentryTest('should start a new session on pageload.', async ({ getLocalTestPath,
   expect(session.did).toBe('1337');
 });
 
-sentryTest('should start a new session with navigation.', async ({ getLocalTestPath, page, browserName }) => {
-  // Navigations get CORS error on Firefox and WebKit as we're using `file://` protocol.
-  if (browserName !== 'chromium') {
-    sentryTest.skip();
-  }
+sentryTest('should start a new session with navigation.', async ({ getLocalTestUrl, page }) => {
+  const url = await getLocalTestUrl({ testDir: __dirname });
 
-  const url = await getLocalTestPath({ testDir: __dirname });
-  await page.route('**/foo', (route: Route) => route.fulfill({ path: `${__dirname}/dist/index.html` }));
+  await page.route('**/foo', (route: Route) => route.continue({ url }));
 
   await page.route('https://dsn.ingest.sentry.io/**/*', route => {
     return route.fulfill({
