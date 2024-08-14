d558c1b555a68d74073c80fd4b598fcb9ccd2195
Lukas Stracke <lukas.stracke@sentry.io>
2024-08-14 17:48:08 +0200

    formatting, linting

--- packages/opentelemetry/src/propagator.ts
+++ packages/opentelemetry/src/propagator.ts
@@ -5,7 +5,6 @@ import { propagation, trace } from '@opentelemetry/api';
 import { W3CBaggagePropagator, isTracingSuppressed } from '@opentelemetry/core';
 import { ATTR_URL_FULL, SEMATTRS_HTTP_URL } from '@opentelemetry/semantic-conventions';
 import type { continueTrace } from '@sentry/core';
-import { hasTracingEnabled } from '@sentry/core';
 import { getRootSpan } from '@sentry/core';
 import { spanToJSON } from '@sentry/core';
 import {
@@ -93,8 +92,6 @@ export class SentryPropagator extends W3CBaggagePropagator {
     const activeSpan = trace.getSpan(context);
     const url = activeSpan && getCurrentURL(activeSpan);
 
-    console.log('1', { activeSpan, url });
-
     const tracePropagationTargets = getClient()?.getOptions()?.tracePropagationTargets;
     if (
       typeof url === 'string' &&
@@ -106,7 +103,6 @@ export class SentryPropagator extends W3CBaggagePropagator {
           '[Tracing] Not injecting trace data for url because it does not match tracePropagationTargets:',
           url,
         );
-      console.log('2', { tracePropagationTargets });
       return;
     }
 
@@ -115,7 +111,6 @@ export class SentryPropagator extends W3CBaggagePropagator {
 
     const { dynamicSamplingContext, traceId, spanId, sampled } = getInjectionData(context);
 
-    console.log('3', { existingBaggageHeader, baggage, dynamicSamplingContext, traceId, spanId, sampled });
     if (existingBaggageHeader) {
       const baggageEntries = parseBaggageHeader(existingBaggageHeader);
 
@@ -124,7 +119,6 @@ export class SentryPropagator extends W3CBaggagePropagator {
           baggage = baggage.setEntry(key, { value });
         });
       }
-      console.log('4', { baggageEntries, baggage });
     }
 
     if (dynamicSamplingContext) {
@@ -134,17 +128,14 @@ export class SentryPropagator extends W3CBaggagePropagator {
         }
         return b;
       }, baggage);
-      console.log('5', { dynamicSamplingContext, baggage });
     }
 
     // We also want to avoid setting the default OTEL trace ID, if we get that for whatever reason
     if (traceId && traceId !== INVALID_TRACEID) {
       setter.set(carrier, SENTRY_TRACE_HEADER, generateSentryTraceHeader(traceId, spanId, sampled));
-      console.log('6', { carrier, traceId, spanId, sampled });
     }
 
     super.inject(propagation.setBaggage(context, baggage), carrier, setter);
-    console.log('7', { carrier });
   }
 
   /**
