b7935c693f752c18aa0bd4926371049fbf44c6bc
Lukas Stracke <lukas.stracke@sentry.io>
2024-08-23 12:16:35 +0200

    disable dashboard for now

--- dev-packages/stryker-config/src/stryker.config.mjs
+++ dev-packages/stryker-config/src/stryker.config.mjs
@@ -5,7 +5,7 @@ import child_process from 'child_process';
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
   packageManager: 'yarn',
-  reporters: ['html', 'clear-text', 'progress', 'json', 'dashboard'],
+  reporters: ['html', 'clear-text', 'progress', 'json'],
   testRunner: 'vitest',
   coverageAnalysis: 'perTest',
   ignoreStatic: true,
