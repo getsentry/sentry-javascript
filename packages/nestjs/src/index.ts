2fcd377f94302579a0581faed85ca00fefb8c73e
nicohrubec <nicolas.hrubec@outlook.com>
2024-08-28 16:07:06 +0200

    Move decorators to folder

--- packages/nestjs/src/index.ts
+++ packages/nestjs/src/index.ts
@@ -2,6 +2,6 @@ export * from '@sentry/node';
 
 export { init } from './sdk';
 
-export { SentryTraced } from './span-decorator';
-export { SentryCron } from './cron-decorator';
-export { WithSentry } from './error-decorator';
+export { SentryTraced } from './decorators/span-decorator';
+export { SentryCron } from './decorators/cron-decorator';
+export { WithSentry } from './decorators/error-decorator';
