9a7affb119e570b64b3408f37b17da52bd48f8f3
Andrei Borza <andrei.borza@sentry.io>
2024-09-04 14:47:51 +0900

    Import but don't use sentry-vite-plugin

--- packages/solidstart/src/vite/index.ts
+++ packages/solidstart/src/vite/index.ts
@@ -1,4 +1,4 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const sentrySolidStartVite = sentryVitePlugin
+export const sentrySolidStartVite = () => [];
