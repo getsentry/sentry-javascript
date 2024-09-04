22ec9711362041e0663f2bf8bdbda630281eef9e
Andrei Borza <andrei.borza@sentry.io>
2024-09-04 16:02:28 +0900

    Disable telemetry

--- packages/solidstart/src/vite/index.ts
+++ packages/solidstart/src/vite/index.ts
@@ -2,11 +2,5 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const sentrySolidStartVite = () => [
-  ...sentryVitePlugin(),
-  {
-    name: 'DebugPlugin',
-    buildEnd(error: unknown) {
-      console.log({ error });
-    },
-  },
+  ...sentryVitePlugin({ telemetry: false }),
 ];
