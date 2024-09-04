54304dcb1470267b3cbf347cb52d0f2c93845838
Andrei Borza <andrei.borza@sentry.io>
2024-09-04 15:21:21 +0900

    Add debug plugin

--- packages/solidstart/src/vite/index.ts
+++ packages/solidstart/src/vite/index.ts
@@ -1,4 +1,12 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin'
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const sentrySolidStartVite = () => sentryVitePlugin()
+export const sentrySolidStartVite = () => [
+  ...sentryVitePlugin(),
+  {
+    name: 'DebugPlugin',
+    buildEnd(error: unknown) {
+      console.log({ error });
+    },
+  },
+];
