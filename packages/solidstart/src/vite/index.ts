3754de50798a3d028df3381ea2bcea5087e524ff
Andrei Borza <andrei.borza@sentry.io>
2024-09-04 14:22:00 +0900

    Add sentry-vite-plugin

--- packages/solidstart/src/vite/index.ts
+++ packages/solidstart/src/vite/index.ts
@@ -1,2 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const sentrySolidStartVite = () => [];
+export const sentrySolidStartVite = () => [...sentryVitePlugin()]
