ec2f04ee11430d546b3861f4d05298c72eec5e74
Andrei Borza <andrei.borza@sentry.io>
2024-09-04 16:18:51 +0900

    Try awaiting...

--- packages/solidstart/src/vite/index.ts
+++ packages/solidstart/src/vite/index.ts
@@ -1,6 +1,4 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const sentrySolidStartVite = () => [
-  ...sentryVitePlugin({ telemetry: false }),
-];
+export const sentrySolidStartVite = async () => [...(await sentryVitePlugin({ telemetry: false }))];
