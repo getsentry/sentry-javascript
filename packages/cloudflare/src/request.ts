033a154e9281b481e4a00157353ba59540e3839d
Tim Fish <tim@timfish.uk>
2024-09-05 11:54:13 +0200

    lint

--- packages/cloudflare/src/request.ts
+++ packages/cloudflare/src/request.ts
@@ -1,10 +1,10 @@
 import type { ExecutionContext, IncomingRequestCfProperties } from '@cloudflare/workers-types';
 
 import {
+  SEMANTIC_ATTRIBUTE_HTTP_REQUEST_METHOD,
   SEMANTIC_ATTRIBUTE_SENTRY_OP,
   SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN,
   SEMANTIC_ATTRIBUTE_SENTRY_SOURCE,
-  SEMANTIC_ATTRIBUTE_HTTP_REQUEST_METHOD,
   SEMANTIC_ATTRIBUTE_URL_FULL,
   captureException,
   continueTrace,
