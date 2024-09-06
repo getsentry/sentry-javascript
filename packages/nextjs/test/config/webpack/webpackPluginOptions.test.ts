548a40242c51f4b62f568aa7dd39f769a9796604
Andrei Borza <andrei.borza@sentry.io>
2024-09-06 14:30:58 +0900

    fix(nextjs): Use posix paths for sourcemap uploads
    Currently, we use `path.join` to provide sourcemap directories.
    On Windows, the resulting strings would use `\` which is an escape
    character for glob patterns and thus no sourcemaps would be found.
    
    This fix ensures we use posix paths which glob then handles correctly
    on Windows too.
    
    Closes: https://github.com/getsentry/sentry-javascript/issues/13270
    Partly fixes: https://github.com/getsentry/sentry-javascript/issues/13288

--- packages/nextjs/test/config/webpack/webpackPluginOptions.test.ts
+++ packages/nextjs/test/config/webpack/webpackPluginOptions.test.ts
@@ -2,6 +2,7 @@ import type { BuildContext, NextConfigObject } from '../../../src/config/types';
 import { getWebpackPluginOptions } from '../../../src/config/webpackPluginOptions';
 
 function generateBuildContext(overrides: {
+  dir?: string;
   isServer: boolean;
   nextjsConfig?: NextConfigObject;
 }): BuildContext {
@@ -9,7 +10,7 @@ function generateBuildContext(overrides: {
     dev: false, // The plugin is not included in dev mode
     isServer: overrides.isServer,
     buildId: 'test-build-id',
-    dir: '/my/project/dir',
+    dir: overrides.dir ?? '/my/project/dir',
     config: overrides.nextjsConfig ?? {},
     totalPages: 2,
     defaultLoaders: true,
@@ -171,4 +172,19 @@ describe('getWebpackPluginOptions()', () => {
       assets: [],
     });
   });
+
+  it('passes posix paths to the plugin', () => {
+    const buildContext = generateBuildContext({ dir: 'C:\\my\\windows\\project\\dir', isServer: false });
+    const generatedPluginOptions = getWebpackPluginOptions(buildContext, { widenClientFileUpload: true });
+    expect(generatedPluginOptions.sourcemaps).toMatchObject({
+      assets: ['C:/my/windows/project/dir/.next/static/chunks/**'],
+      ignore: [
+        'C:/my/windows/project/dir/.next/static/chunks/framework-*',
+        'C:/my/windows/project/dir/.next/static/chunks/framework.*',
+        'C:/my/windows/project/dir/.next/static/chunks/main-*',
+        'C:/my/windows/project/dir/.next/static/chunks/polyfills-*',
+        'C:/my/windows/project/dir/.next/static/chunks/webpack-*',
+      ],
+    });
+  })
 });
