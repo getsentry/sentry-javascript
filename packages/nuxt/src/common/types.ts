8bd51f42b234b2f54016cf83a60c1ea694fb071f
s1gr1d <sigrid.huemer@posteo.at>
2024-09-10 17:17:27 +0200

    change option naming

--- packages/nuxt/src/common/types.ts
+++ packages/nuxt/src/common/types.ts
@@ -101,12 +101,14 @@ export type SentryNuxtModuleOptions = {
   debug?: boolean;
 
   /**
-   * Enabling simplified deployment can be used for environments where modifying the node option `--import` is not possible.
-   * If this flag is `true`, the Sentry SDK will import the server config at the top of the server entry file to load the SDK on the server.
+   * Enabling basic server tracing can be used for environments where modifying the node option `--import` is not possible.
+   * However, enabling this option only supports limited tracing instrumentation. Only http traces will be collected (but no database-specific traces etc.).
    *
-   * Note that enabling this option only supports limited tracing instrumentation. Only http traces will be collected.
+   * If this option is `true`, the Sentry SDK will import the Sentry server config at the top of the server entry file to load the SDK on the server.
+   *
+   * **DO NOT** enable this option if you've already added the node option `--import` in your node start script. This would initialize Sentry twice on the server-side and leads to unexpected issues.
    *
    * @default false
    */
-  simplifiedDeployment?: boolean;
+  experimental_basicServerTracing?: boolean;
 };
