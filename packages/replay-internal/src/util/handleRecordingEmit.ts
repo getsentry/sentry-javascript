e944daa3d71272f87fc324ae3f0b540ed0645b8e
Billy Vong <billyvg@users.noreply.github.com>
2024-09-10 18:06:44 -0230

    feat(replay): Add experimental option to allow for a checkout every 6 minutes (#13069)
    Including more checkouts will improve replayer scrubbing since it will
    reduce the number of mutations that need to be processed (especially for
    longer replays). The downside is that it will increase the size of
    replays since we will have up to 9 more snapshots per replay (max replay
    duration is 60 minutes / 6 minute checkouts).

--- packages/replay-internal/src/util/handleRecordingEmit.ts
+++ packages/replay-internal/src/util/handleRecordingEmit.ts
@@ -58,9 +58,14 @@ export function getHandleRecordingEmit(replay: ReplayContainer): RecordingEmitCa
         return false;
       }
 
+      const session = replay.session;
+
       // Additionally, create a meta event that will capture certain SDK settings.
       // In order to handle buffer mode, this needs to either be done when we
-      // receive checkout events or at flush time.
+      // receive checkout events or at flush time. We have an experimental mode
+      // to perform multiple checkouts a session (the idea is to improve
+      // seeking during playback), so also only include if segmentId is 0
+      // (handled in `addSettingsEvent`).
       //
       // `isCheckout` is always true, but want to be explicit that it should
       // only be added for checkouts
@@ -72,22 +77,22 @@ export function getHandleRecordingEmit(replay: ReplayContainer): RecordingEmitCa
       // of the previous session. Do not immediately flush in this case
       // to avoid capturing only the checkout and instead the replay will
       // be captured if they perform any follow-up actions.
-      if (replay.session && replay.session.previousSessionId) {
+      if (session && session.previousSessionId) {
         return true;
       }
 
       // When in buffer mode, make sure we adjust the session started date to the current earliest event of the buffer
       // this should usually be the timestamp of the checkout event, but to be safe...
-      if (replay.recordingMode === 'buffer' && replay.session && replay.eventBuffer) {
+      if (replay.recordingMode === 'buffer' && session && replay.eventBuffer) {
         const earliestEvent = replay.eventBuffer.getEarliestTimestamp();
         if (earliestEvent) {
           DEBUG_BUILD &&
             logger.info(`Updating session start time to earliest event in buffer to ${new Date(earliestEvent)}`);
 
-          replay.session.started = earliestEvent;
+          session.started = earliestEvent;
 
           if (replay.getOptions().stickySession) {
-            saveSession(replay.session);
+            saveSession(session);
           }
         }
       }
