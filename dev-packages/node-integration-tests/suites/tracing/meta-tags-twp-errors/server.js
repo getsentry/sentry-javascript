c4be89b3466bb1674a6781ff013da123059e07fb
Lukas Stracke <lukas.stracke@sentry.io>
2024-09-18 12:49:30 +0200

    does this trigger a different error now?

--- dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/server.js
+++ dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/server.js
@@ -21,22 +21,22 @@ app.get('/test', (_req, res) => {
   console.log('X1');
   Sentry.captureException(new Error('This is a test error'));
   console.log('X2');
-  Sentry.getClient().on('beforeEnvelope', envelope => {
-    console.log('X3');
-    const event = envelope[1][0][1];
-    if (event.exception.values[0].value === 'This is a test error') {
-      console.log('X4');
-      const { trace_id, span_id } = event.contexts.trace;
-      res.send({
-        traceData: Sentry.getTraceData(),
-        traceMetaTags: Sentry.getTraceMetaTags(),
-        errorTraceContext: {
-          trace_id,
-          span_id,
-        },
-      });
-    }
-  });
+  // Sentry.getClient().on('beforeEnvelope', envelope => {
+  //   console.log('X3');
+  //   const event = envelope[1][0][1];
+  //   if (event.exception.values[0].value === 'This is a test error') {
+  //     console.log('X4');
+  //     const { trace_id, span_id } = event.contexts.trace;
+  //     res.send({
+  //       traceData: Sentry.getTraceData(),
+  //       traceMetaTags: Sentry.getTraceMetaTags(),
+  //       errorTraceContext: {
+  //         trace_id,
+  //         span_id,
+  //       },
+  //     });
+  //   }
+  // });
 });
 
 Sentry.setupExpressErrorHandler(app);
