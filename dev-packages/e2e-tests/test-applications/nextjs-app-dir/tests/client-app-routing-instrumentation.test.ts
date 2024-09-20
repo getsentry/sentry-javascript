97974baebd2a5ed8636c1bed5d35b3782b754209
Luca Forstner <luca.forstner@sentry.io>
2024-09-20 09:52:46 +0200

    ref(nextjs): Improve app router routing instrumentation accuracy (#13695)
    Improves the Next.js routing instrumentation by patching the Next.js
    router and instrumenting window popstates.
    
    A few details on this PR that might explain weird-looking logic:
    - The patching of the router is in a setInterval because Next.js may
    take a while to write the router to the window object and we don't have
    a cue when that has happened.
    - We are using a combination of patching `router.back`/`router.forward`
    and the `popstate` event to emit a properly named transaction, because
    `router.back` and `router.forward` aren't passed any useful strings we
    could use as txn names.
    - Because there is a slight delay between the
    `router.back`/`router.forward` calls and the `popstate` event, we
    temporarily give the navigation span an invalid name that we use as an
    indicator to drop if one may leak through.

--- dev-packages/e2e-tests/test-applications/nextjs-app-dir/tests/client-app-routing-instrumentation.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-app-dir/tests/client-app-routing-instrumentation.test.ts
@@ -53,3 +53,148 @@ test('Creates a navigation transaction for app router routes', async ({ page }) 
   expect(await clientNavigationTransactionPromise).toBeDefined();
   expect(await serverComponentTransactionPromise).toBeDefined();
 });
+
+test('Creates a navigation transaction for `router.push()`', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/router-push` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'router.push'
+    );
+  });
+
+  await page.goto('/navigation');
+  await page.waitForTimeout(3000);
+  await page.getByText('router.push()').click();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
+
+test('Creates a navigation transaction for `router.replace()`', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/router-replace` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'router.replace'
+    );
+  });
+
+  await page.goto('/navigation');
+  await page.waitForTimeout(3000);
+  await page.getByText('router.replace()').click();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
+
+test('Creates a navigation transaction for `router.back()`', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/1337/router-back` &&
+      transactionEvent.contexts?.trace?.op === 'navigation'
+    );
+  });
+
+  await page.goto('/navigation/1337/router-back');
+  await page.waitForTimeout(3000);
+  await page.getByText('Go back home').click();
+  await page.waitForTimeout(3000);
+  await page.getByText('router.back()').click();
+
+  expect(await navigationTransactionPromise).toMatchObject({
+    contexts: {
+      trace: {
+        data: {
+          'navigation.type': 'router.back',
+        },
+      },
+    },
+  });
+});
+
+test('Creates a navigation transaction for `router.forward()`', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/router-push` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'router.forward'
+    );
+  });
+
+  await page.goto('/navigation');
+  await page.waitForTimeout(3000);
+  await page.getByText('router.push()').click();
+  await page.waitForTimeout(3000);
+  await page.goBack();
+  await page.waitForTimeout(3000);
+  await page.getByText('router.forward()').click();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
+
+test('Creates a navigation transaction for `<Link />`', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/link` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'router.push'
+    );
+  });
+
+  await page.goto('/navigation');
+  await page.getByText('Normal Link').click();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
+
+test('Creates a navigation transaction for `<Link replace />`', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/link-replace` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'router.replace'
+    );
+  });
+
+  await page.goto('/navigation');
+  await page.waitForTimeout(3000);
+  await page.getByText('Link Replace').click();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
+
+test('Creates a navigation transaction for browser-back', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/browser-back` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'browser.popstate'
+    );
+  });
+
+  await page.goto('/navigation/42/browser-back');
+  await page.waitForTimeout(3000);
+  await page.getByText('Go back home').click();
+  await page.waitForTimeout(3000);
+  await page.goBack();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
+
+test('Creates a navigation transaction for browser-forward', async ({ page }) => {
+  const navigationTransactionPromise = waitForTransaction('nextjs-app-dir', transactionEvent => {
+    return (
+      transactionEvent?.transaction === `/navigation/42/router-push` &&
+      transactionEvent.contexts?.trace?.op === 'navigation' &&
+      transactionEvent.contexts.trace.data?.['navigation.type'] === 'browser.popstate'
+    );
+  });
+
+  await page.goto('/navigation');
+  await page.getByText('router.push()').click();
+  await page.waitForTimeout(3000);
+  await page.goBack();
+  await page.waitForTimeout(3000);
+  await page.goForward();
+
+  expect(await navigationTransactionPromise).toBeDefined();
+});
