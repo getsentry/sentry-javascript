c0a5a3e16b1225f16d46431d876bae9116363104
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-09-23 13:15:02 +0200

    fix(nuxt): Use Nuxt error hooks instead of errorHandler to prevent 500 (#13748)
    Adds a new option `attachErrorHandler` to the `vueIntegration`. This
    option is used in the Nuxt SDK to prevent wrapping the existing Nuxt
    error handler. Instead, the errors are captured in the Nuxt hooks.
    
    fixes: https://github.com/getsentry/sentry-javascript/issues/12515
    
    ---------
    
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>

--- dev-packages/e2e-tests/test-applications/nuxt-3/tests/errors.client.test.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-3/tests/errors.client.test.ts
@@ -55,4 +55,51 @@ test.describe('client-side errors', async () => {
       },
     });
   });
+
+  test('page is still interactive after client error', async ({ page }) => {
+    const error1Promise = waitForError('nuxt-3', async errorEvent => {
+      return errorEvent?.exception?.values?.[0]?.value === 'Error thrown from Nuxt-3 E2E test app';
+    });
+
+    await page.goto(`/client-error`);
+    await page.locator('#errorBtn').click();
+
+    const error1 = await error1Promise;
+
+    const error2Promise = waitForError('nuxt-3', async errorEvent => {
+      return errorEvent?.exception?.values?.[0]?.value === 'Another Error thrown from Nuxt-3 E2E test app';
+    });
+
+    await page.locator('#errorBtn2').click();
+
+    const error2 = await error2Promise;
+
+    expect(error1).toMatchObject({
+      exception: {
+        values: [
+          {
+            type: 'Error',
+            value: 'Error thrown from Nuxt-3 E2E test app',
+            mechanism: {
+              handled: false,
+            },
+          },
+        ],
+      },
+    });
+
+    expect(error2).toMatchObject({
+      exception: {
+        values: [
+          {
+            type: 'Error',
+            value: 'Another Error thrown from Nuxt-3 E2E test app',
+            mechanism: {
+              handled: false,
+            },
+          },
+        ],
+      },
+    });
+  });
 });
