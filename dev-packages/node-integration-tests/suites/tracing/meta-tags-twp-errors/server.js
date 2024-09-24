f7520f6688703109face2f98a48c919f38c7ff05
Lukas Stracke <lukas.stracke@sentry.io>
2024-09-24 13:31:29 +0200

    cleanup tests

--- dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/server.js
+++ dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/server.js
@@ -4,18 +4,6 @@ const Sentry = require('@sentry/node');
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   transport: loggingTransport,
-  beforeSend(event) {
-    if (!event.contexts.traceData) {
-      event.contexts = {
-        ...event.contexts,
-        traceData: {
-          ...Sentry.getTraceData(),
-          metaTags: Sentry.getTraceMetaTags(),
-        },
-      };
-    }
-    return event;
-  },
 });
 
 // express must be required after Sentry is initialized
@@ -24,11 +12,6 @@ const express = require('express');
 const app = express();
 
 app.get('/test', (_req, res) => {
-  Sentry.captureException(new Error('test error'));
-  res.status(200).send();
-});
-
-app.get('/test-scope', (_req, res) => {
   Sentry.withScope(scope => {
     scope.setContext('traceData', {
       ...Sentry.getTraceData(),
--- dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/meta-tags-twp-errors/test.ts
@@ -30,31 +30,6 @@ describe('errors in TwP mode have same trace in trace context and getTraceData()
       .makeRequest('get', '/test');
   });
 
-  test('in incoming request with Custom Scope', done => {
-    createRunner(__dirname, 'server.js')
-      .expect({
-        event: event => {
-          const { contexts } = event;
-          const { trace_id, span_id } = contexts?.trace || {};
-          expect(trace_id).toMatch(/^[a-f0-9]{32}$/);
-          expect(span_id).toMatch(/^[a-f0-9]{16}$/);
-
-          const traceData = contexts?.traceData || {};
-
-          expect(traceData['sentry-trace']).toEqual(`${trace_id}-${span_id}`);
-
-          expect(traceData.baggage).toContain(`sentry-trace_id=${trace_id}`);
-          expect(traceData.baggage).not.toContain('sentry-sampled=');
-
-          expect(traceData.metaTags).toContain(`<meta name="sentry-trace" content="${trace_id}-${span_id}"/>`);
-          expect(traceData.metaTags).toContain(`sentry-trace_id=${trace_id}`);
-          expect(traceData.metaTags).not.toContain('sentry-sampled=');
-        },
-      })
-      .start(done)
-      .makeRequest('get', '/test-scope');
-  });
-
   test('outside of a request handler', done => {
     createRunner(__dirname, 'no-server.js')
       .expect({
