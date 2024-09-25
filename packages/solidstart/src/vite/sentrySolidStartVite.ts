d5b247aa4dbb7015f9c480c7a5a34f7be8c32bf6
Andrei Borza <andrei.borza@sentry.io>
2024-09-25 13:45:25 +0900

    Change ordering of plugins

--- packages/solidstart/src/vite/sentrySolidStartVite.ts
+++ packages/solidstart/src/vite/sentrySolidStartVite.ts
@@ -9,13 +9,19 @@ import type { SentrySolidStartPluginOptions } from './types';
 export const sentrySolidStartVite = (options: SentrySolidStartPluginOptions = {}): Plugin[] => {
   const sentryPlugins: Plugin[] = [];
 
-  sentryPlugins.push(makeBuildInstrumentationFilePlugin(options));
-
   if (process.env.NODE_ENV !== 'development') {
     if (options.sourceMapsUploadOptions?.enabled ?? true) {
       sentryPlugins.push(...makeSourceMapsVitePlugin(options));
     }
   }
+
+  // TODO: Ensure this file is source mapped too.
+  // Placing this after the sentry vite plugin means this
+  // file won't get a sourcemap and won't have a debug id injected.
+  // Because the file is just copied over to the output server
+  // directory the release injection file from sentry vite plugin
+  // wouldn't resolve correctly otherwise.
+  sentryPlugins.push(makeBuildInstrumentationFilePlugin(options));
 
   return sentryPlugins;
 };
