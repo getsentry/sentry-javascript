f86431e032b40aa8e9aaa4f333d82609928612c5
Luca Forstner <luca.forstner@sentry.io>
2024-09-30 09:03:25 +0000

    next-14

--- dev-packages/e2e-tests/test-applications/nextjs-14/app/generation-functions/page.tsx
+++ dev-packages/e2e-tests/test-applications/nextjs-14/app/generation-functions/page.tsx
@@ -10,30 +10,40 @@ export default function Page() {
 export async function generateMetadata({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams:
+    | { shouldThrowInGenerateMetadata?: string; metadataTitle?: string }
+    | Promise<{ shouldThrowInGenerateMetadata?: string; metadataTitle?: string }>;
 }) {
+  // We need to dynamically check for this because Next.js made the API async for Next.js 15 and we use this test in canary tests
+  const normalizedSearchParams = await searchParams;
+
   Sentry.setTag('my-isolated-tag', true);
   Sentry.setTag('my-global-scope-isolated-tag', getDefaultIsolationScope().getScopeData().tags['my-isolated-tag']); // We set this tag to be able to assert that the previously set tag has not leaked into the global isolation scope
 
-  if (searchParams['shouldThrowInGenerateMetadata']) {
+  if (normalizedSearchParams['shouldThrowInGenerateMetadata']) {
     throw new Error('generateMetadata Error');
   }
 
   return {
-    title: searchParams['metadataTitle'] ?? 'not set',
+    title: normalizedSearchParams['metadataTitle'] ?? 'not set',
   };
 }
 
-export function generateViewport({
+export async function generateViewport({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | undefined };
+  searchParams:
+    | { viewportThemeColor?: string; shouldThrowInGenerateViewport?: string }
+    | Promise<{ viewportThemeColor?: string; shouldThrowInGenerateViewport?: string }>;
 }) {
-  if (searchParams['shouldThrowInGenerateViewport']) {
+  // We need to dynamically check for this because Next.js made the API async for Next.js 15 and we use this test in canary tests
+  const normalizedSearchParams = await searchParams;
+
+  if (normalizedSearchParams['shouldThrowInGenerateViewport']) {
     throw new Error('generateViewport Error');
   }
 
   return {
-    themeColor: searchParams['viewportThemeColor'] ?? 'black',
+    themeColor: normalizedSearchParams['viewportThemeColor'] ?? 'black',
   };
 }
