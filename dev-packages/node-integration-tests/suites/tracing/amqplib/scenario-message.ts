35c3f534734d72018adf43207c312ff0f031d0fc
Onur Temizkan <onur@narval.co.uk>
2024-10-03 12:52:49 +0300

    fix(test): Fix flaky amqplib test

--- dev-packages/node-integration-tests/suites/tracing/amqplib/scenario-message.ts
+++ dev-packages/node-integration-tests/suites/tracing/amqplib/scenario-message.ts
@@ -4,16 +4,23 @@ import { connectToRabbitMQ, consumeMessageFromQueue, createQueue, sendMessageToQ
 
 const queueName = 'queue1';
 
+// Stop the process from exiting before the transaction is sent
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+setInterval(() => {}, 1000);
+
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   const { connection, channel } = await connectToRabbitMQ();
   await createQueue(queueName, channel);
 
+  const consumeMessagePromise = consumeMessageFromQueue(queueName, channel);
+
   await Sentry.startSpan({ name: 'root span' }, async () => {
     sendMessageToQueue(queueName, channel, JSON.stringify({ foo: 'bar01' }));
   });
 
-  await consumeMessageFromQueue(queueName, channel);
+  await consumeMessagePromise;
+
   await channel.close();
   await connection.close();
 })();
