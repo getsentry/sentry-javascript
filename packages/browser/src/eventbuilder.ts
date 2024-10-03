ea6dbda46e0b878329ff9bbd43c3e3e2a5a0db12
s1gr1d <sigrid.huemer@posteo.at>
2024-10-03 10:35:00 +0200

    test(wasm): Add unit tests

--- packages/browser/src/eventbuilder.ts
+++ packages/browser/src/eventbuilder.ts
@@ -167,26 +167,34 @@ function isWebAssemblyException(exception: unknown): exception is WebAssembly.Ex
 }
 
 /**
+ * Only exported for testing
+ *
  * There are cases where error is an WebAssembly.Exception object
  * https://github.com/getsentry/sentry-javascript/issues/13787
  * In this specific case we try to extract name/type from .message in WebAssembly.Exception
+ *
+ * @hidden
  */
-function extractName(ex: Error & { message: { error?: Error } }): string | undefined {
+export function extractName(ex: Error & { message: { error?: Error } }): string | undefined {
   const name = ex && ex.name;
   if (!name && isWebAssemblyException(ex)) {
     // Emscripten sets array[type, message] to the "message" property on the WebAssembly.Exception object
     const hasTypeInMessage = ex.message && Array.isArray(ex.message) && ex.message.length == 2;
     return hasTypeInMessage ? ex.message[0] : 'WebAssembly.Exception';
   }
-  return name; // May return undefined value
+  return name;
 }
 
 /**
+ * Only exported for testing
+ *
  * There are cases where stacktrace.message is an Event object
  * https://github.com/getsentry/sentry-javascript/issues/1949
  * In this specific case we try to extract stacktrace.message.error.message
+ *
+ * @hidden
  */
-function extractMessage(ex: Error & { message: { error?: Error } }): string {
+export function extractMessage(ex: Error & { message: { error?: Error } }): string {
   const message = ex && ex.message;
   if (!message) {
     return 'No error message';
