7b2af0f5299eeed133a5d5ff9beb1acc1cb2c192
Luca Forstner <luca.forstner@sentry.io>
2024-10-08 08:46:03 +0000

    Drop any spans emitted by Next.js for pages router API routes

---
+++ packages/nextjs/src/common/span-attributes-with-logic-attached.ts
@@ -0,0 +1,4 @@
+/**
+ * If this attribute is attached to a transaction, the Next.js SDK will drop that transaction.
+ */
+export const TRANSACTION_ATTR_SHOULD_DROP_TRANSACTION = 'sentry.drop_transaction';
