18c058a79f582870af446ba8d528e4ecfc09ffb6
Lukas Stracke <lukas.stracke@sentry.io>
2024-10-11 11:48:24 +0200

    test(browser): Add test for current DSC transaction name updating behaviour

---
+++ dev-packages/browser-integration-tests/suites/tracing/dsc-txn-name-update/subject.js
@@ -0,0 +1,36 @@
+const btnStartSpan = document.getElementById('btnStartSpan');
+const btnUpdateName = document.getElementById('btnUpdateName');
+const btnMakeRequest = document.getElementById('btnMakeRequest');
+const btnCaptureError = document.getElementById('btnCaptureError');
+const btnEndSpan = document.getElementById('btnEndSpan');
+
+btnStartSpan.addEventListener('click', () => {
+  Sentry.startSpanManual(
+    { name: 'test-root-span', attributes: { [Sentry.SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'url' } },
+    async span => {
+      console.log('Root span started');
+      window.__traceId = span.spanContext().traceId;
+      await new Promise(resolve => {
+        btnEndSpan.addEventListener('click', resolve);
+      });
+      span.end();
+      console.log('Root span ended');
+    },
+  );
+});
+
+let updateCnt = 0;
+btnUpdateName.addEventListener('click', () => {
+  const span = Sentry.getActiveSpan();
+  const rootSpan = Sentry.getRootSpan(span);
+  rootSpan.updateName(`updated-root-span-${++updateCnt}`);
+  rootSpan.setAttribute(Sentry.SEMANTIC_ATTRIBUTE_SENTRY_SOURCE, 'route');
+});
+
+btnMakeRequest.addEventListener('click', () => {
+  fetch('https://example.com/api');
+});
+
+btnCaptureError.addEventListener('click', () => {
+  Sentry.captureException(new Error('test-error'));
+});
