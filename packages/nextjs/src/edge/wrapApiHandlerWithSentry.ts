f433c84324fba5ba24cae011b205ef5c24fb8ab7
Luca Forstner <luca.forstner@sentry.io>
2024-10-14 10:06:11 +0000

    Refactor edge api handler wrapper to not use edge wrapper utils

--- packages/nextjs/src/edge/wrapApiHandlerWithSentry.ts
+++ packages/nextjs/src/edge/wrapApiHandlerWithSentry.ts
@@ -1,4 +1,15 @@
-import { withEdgeWrapping } from '../common/utils/edgeWrapperUtils';
+import {
+  SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN,
+  SEMANTIC_ATTRIBUTE_SENTRY_SOURCE,
+  captureException,
+  getActiveSpan,
+  getCurrentScope,
+  getIsolationScope,
+  handleCallbackErrors,
+  startSpan,
+} from '@sentry/core';
+import { vercelWaitUntil, winterCGRequestToRequestData } from '@sentry/utils';
+import { flushSafelyWithTimeout } from '../common/utils/responseEnd';
 import type { EdgeRouteHandler } from './types';
 
 /**
@@ -9,18 +20,59 @@ export function wrapApiHandlerWithSentry<H extends EdgeRouteHandler>(
   parameterizedRoute: string,
 ): (...params: Parameters<H>) => Promise<ReturnType<H>> {
   return new Proxy(handler, {
-    apply: (wrappingTarget, thisArg, args: Parameters<H>) => {
-      const req = args[0];
+    apply: async (wrappingTarget, thisArg, args: Parameters<H>) => {
+      const req: unknown = args[0];
 
-      const wrappedHandler = withEdgeWrapping(wrappingTarget, {
-        spanDescription: !(req instanceof Request)
-          ? `handler (${parameterizedRoute})`
-          : `${req.method} ${parameterizedRoute}`,
-        spanOp: 'http.server',
-        mechanismFunctionName: 'wrapApiHandlerWithSentry',
-      });
+      if (req instanceof Request) {
+        getIsolationScope().setSDKProcessingMetadata({
+          request: winterCGRequestToRequestData(req),
+        });
+        getCurrentScope().setTransactionName(`${req.method} ${parameterizedRoute}`);
+      } else {
+        getCurrentScope().setTransactionName(`handler (${parameterizedRoute})`);
+      }
 
-      return wrappedHandler.apply(thisArg, args);
+      let spanName: string;
+      let op: string | undefined = 'http.server';
+
+      // If there is an active span, it likely means that the automatic Next.js OTEL instrumentation worked and we can
+      // rely on that for parameterization.
+      if (getActiveSpan()) {
+        spanName = `handler (${parameterizedRoute})`;
+        op = undefined;
+      } else if (req instanceof Request) {
+        spanName = `${req.method} ${parameterizedRoute}`;
+      } else {
+        spanName = `handler ${parameterizedRoute}`;
+      }
+
+      const handlerResult = await startSpan(
+        {
+          name: spanName,
+          op: op,
+          attributes: {
+            [SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'route',
+            [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.function.nextjs.wrapApiHandlerWithSentry',
+          },
+        },
+        () => {
+          return handleCallbackErrors(
+            () => wrappingTarget.apply(thisArg, args),
+            error => {
+              captureException(error, {
+                mechanism: {
+                  type: 'instrument',
+                  handled: false,
+                },
+              });
+            },
+          );
+        },
+      );
+
+      vercelWaitUntil(flushSafelyWithTimeout());
+
+      return handlerResult;
     },
   });
 }
