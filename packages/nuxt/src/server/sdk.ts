f6872addba0c62d798446bed208881440532c5dd
s1gr1d <sigrid.huemer@posteo.at>
2024-10-15 15:15:37 +0200

    feat(nuxt): Add Http `responseHook` with `waitUntil`

--- packages/nuxt/src/server/sdk.ts
+++ packages/nuxt/src/server/sdk.ts
@@ -1,7 +1,7 @@
-import { applySdkMetadata, getGlobalScope } from '@sentry/core';
-import { init as initNode } from '@sentry/node';
+import { applySdkMetadata, flush, getGlobalScope } from '@sentry/core';
+import { httpIntegration, init as initNode } from '@sentry/node';
 import type { Client, EventProcessor } from '@sentry/types';
-import { logger } from '@sentry/utils';
+import { logger, vercelWaitUntil } from '@sentry/utils';
 import { DEBUG_BUILD } from '../common/debug-build';
 import type { SentryNuxtServerOptions } from '../common/types';
 
@@ -14,6 +14,17 @@ export function init(options: SentryNuxtServerOptions): Client | undefined {
   const sentryOptions = {
     ...options,
     registerEsmLoaderHooks: mergeRegisterEsmLoaderHooks(options),
+    integrations: [
+      httpIntegration({
+        instrumentation: {
+          responseHook: () => {
+            // Makes it possible to end the tracing span before closing the Vercel lambda (https://vercel.com/docs/functions/functions-api-reference#waituntil)
+            vercelWaitUntil(flushSafelyWithTimeout());
+          },
+        },
+      }),
+      ...(Array.isArray(options.integrations) ? options.integrations : []),
+    ],
   };
 
   applySdkMetadata(sentryOptions, 'nuxt', ['nuxt', 'node']);
@@ -63,4 +74,17 @@ export function mergeRegisterEsmLoaderHooks(
     };
   }
   return options.registerEsmLoaderHooks ?? { exclude: [/vue/] };
+}
+
+/**
+ * Flushes pending Sentry events with a 2-second timeout and in a way that cannot create unhandled promise rejections.
+ */
+export async function flushSafelyWithTimeout(): Promise<void> {
+  try {
+    DEBUG_BUILD && logger.log('Flushing events...');
+    await flush(2000);
+    DEBUG_BUILD && logger.log('Done flushing events');
+  } catch (e) {
+    DEBUG_BUILD && logger.log('Error while flushing events:\n', e);
+  }
 }
