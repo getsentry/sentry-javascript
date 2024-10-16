156ba88f34e44d909f80eb645dc0a01f430ee221
Luca Forstner <luca.forstner@sentry.io>
2024-10-16 10:37:44 +0000

    Fix test?

--- dev-packages/e2e-tests/test-applications/nextjs-app-dir/tests/async-context-edge.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-app-dir/tests/async-context-edge.test.ts
@@ -3,7 +3,10 @@ import { waitForTransaction } from '@sentry-internal/test-utils';
 
 test('Should allow for async context isolation in the edge SDK', async ({ request }) => {
   const edgerouteTransactionPromise = waitForTransaction('nextjs-app-dir', async transactionEvent => {
-    return transactionEvent?.transaction === 'GET /api/async-context-edge-endpoint';
+    return (
+      transactionEvent?.transaction === 'GET /api/async-context-edge-endpoint' &&
+      transactionEvent.contexts?.runtime?.name === 'vercel-edge'
+    );
   });
 
   await request.get('/api/async-context-edge-endpoint');
@@ -13,6 +16,6 @@ test('Should allow for async context isolation in the edge SDK', async ({ reques
   const outerSpan = asyncContextEdgerouteTransaction.spans?.find(span => span.description === 'outer-span');
   const innerSpan = asyncContextEdgerouteTransaction.spans?.find(span => span.description === 'inner-span');
 
-  expect(outerSpan?.parent_span_id).toStrictEqual(asyncContextEdgerouteTransaction.contexts?.trace?.span_id);
+  expect(outerSpan?.trace_id).toStrictEqual(asyncContextEdgerouteTransaction.contexts?.trace?.trace_id);
   expect(innerSpan?.parent_span_id).toStrictEqual(asyncContextEdgerouteTransaction.contexts?.trace?.span_id);
 });
