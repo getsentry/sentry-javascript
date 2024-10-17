5964c0f77fa7722d62bb5479426153571e1cb226
Billy Vong <billyvg@users.noreply.github.com>
2024-10-17 17:05:19 -0400

    Update packages/replay-internal/src/util/handleRecordingEmit.ts
    Co-authored-by: Catherine Lee <55311782+c298lee@users.noreply.github.com>

--- packages/replay-internal/src/util/handleRecordingEmit.ts
+++ packages/replay-internal/src/util/handleRecordingEmit.ts
@@ -74,12 +74,11 @@ export function getHandleRecordingEmit(replay: ReplayContainer): RecordingEmitCa
 
       // When in buffer mode, make sure we adjust the session started date to the current earliest event of the buffer
       // this should usually be the timestamp of the checkout event, but to be safe...
-      if (replay.recordingMode === 'buffer' && session) {
+      if (replay.recordingMode === 'buffer' && session && replay.eventBuffer) {
         DEBUG_BUILD &&
-          replay.eventBuffer &&
           logger.info('Attempting to fetch earliest event from event buffer type: ', replay.eventBuffer.type);
 
-        const earliestEvent = replay.eventBuffer && replay.eventBuffer.getEarliestTimestamp();
+        const earliestEvent = replay.eventBuffer.getEarliestTimestamp();
 
         if (earliestEvent) {
           DEBUG_BUILD &&
