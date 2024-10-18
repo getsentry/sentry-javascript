69974703bcabfc39946a619923548b30fe5c20a3
s1gr1d <sigrid.huemer@posteo.at>
2024-10-18 14:04:56 +0200

    review comment

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -78,7 +78,7 @@ export default defineNuxtModule<ModuleOptions>({
           consoleSandbox(() => {
             // eslint-disable-next-line no-console
             console.log(
-              '[Sentry] Your application is running in development mode. Note: @sentry/nuxt is in beta and may not work as expected on the server-side (Nitro).',
+              '[Sentry] Your application is running in development mode. Note: @sentry/nuxt is in beta and may not work as expected on the server-side (Nitro). Errors are reported, but tracing does not work.',
             );
           });
         }
