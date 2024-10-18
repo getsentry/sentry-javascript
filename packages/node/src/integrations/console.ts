be9195c19f8efa068900bc5a568c2aa9ffbe11e7
Luca Forstner <luca.forstner@sentry.io>
2024-10-18 07:55:26 +0000

    Just truncate bro

--- packages/node/src/integrations/console.ts
+++ packages/node/src/integrations/console.ts
@@ -1,12 +1,9 @@
 import * as util from 'node:util';
 import { addBreadcrumb, defineIntegration, getClient } from '@sentry/core';
 import type { IntegrationFn } from '@sentry/types';
-import { addConsoleInstrumentationHandler, severityLevelFromString } from '@sentry/utils';
+import { addConsoleInstrumentationHandler, severityLevelFromString, truncate } from '@sentry/utils';
 
 const INTEGRATION_NAME = 'Console';
-
-const TRUNCATION_MSG = ' (breadcrumb truncated)';
-const MAX_BREADCRUMB_MSG_LENGTH = 2048; // 2 KB
 
 const _consoleIntegration = (() => {
   return {
@@ -17,17 +14,11 @@ const _consoleIntegration = (() => {
           return;
         }
 
-        // Truncate the breadcrumb length to max 2KB including the truncated snippet
-        let formattedMessage: string = util.format.apply(undefined, args);
-        if (formattedMessage.length > MAX_BREADCRUMB_MSG_LENGTH) {
-          formattedMessage = formattedMessage.slice(MAX_BREADCRUMB_MSG_LENGTH - TRUNCATION_MSG.length) + TRUNCATION_MSG;
-        }
-
         addBreadcrumb(
           {
             category: 'console',
             level: severityLevelFromString(level),
-            message: formattedMessage,
+            message: truncate(util.format.apply(undefined, args), 2048), // 2KB
           },
           {
             input: [...args],
