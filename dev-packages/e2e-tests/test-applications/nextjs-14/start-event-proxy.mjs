1797b5d254a8497b06f4f3b602e1302ae86a02ca
Luca Forstner <luca.forstner@sentry.io>
2024-10-21 11:06:59 +0000

    test(e2e): Create event dumps for all Next.js test apps

--- dev-packages/e2e-tests/test-applications/nextjs-14/start-event-proxy.mjs
+++ dev-packages/e2e-tests/test-applications/nextjs-14/start-event-proxy.mjs
@@ -1,6 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { startEventProxyServer } from '@sentry-internal/test-utils';
+
+const packageJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json')));
 
 startEventProxyServer({
   port: 3031,
   proxyServerName: 'nextjs-14',
+  envelopeDumpPath: path.join(
+    process.cwd(),
+    `event-dumps/next-14-v${packageJson.dependencies.next}-${process.env.TEST_ENV}.dump`,
+  ),
 });
