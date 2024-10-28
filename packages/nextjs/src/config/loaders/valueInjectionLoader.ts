b6724527e0452d4a630b0dcc180ca79796938fe7
Luca Forstner <luca.forstner@sentry.io>
2024-10-28 10:57:16 +0000

    fix(nextjs): Respect directives in value injection loader

--- packages/nextjs/src/config/loaders/valueInjectionLoader.ts
+++ packages/nextjs/src/config/loaders/valueInjectionLoader.ts
@@ -4,6 +4,15 @@ type LoaderOptions = {
   values: Record<string, unknown>;
 };
 
+// We need to be careful not to inject anything before any `"use strict";`s or "use client"s or really any other directive.
+// As an additional complication directives may come after any number of comments.
+// This regex is shamelessly stolen from: https://github.com/getsentry/sentry-javascript-bundler-plugins/blob/7f984482c73e4284e8b12a08dfedf23b5a82f0af/packages/bundler-plugin-core/src/index.ts#L535-L539
+const SKIP_COMMENT_AND_DIRECTIVE_REGEX =
+  // Note: CodeQL complains that this regex potentially has n^2 runtime. This likely won't affect realistic files.
+  // Rollup doesn't like if we put this regex as a literal (?). No idea why.
+  // eslint-disable-next-line @sentry-internal/sdk/no-regexp-constructor
+  new RegExp('^(?:\\s*|/\\*(?:.|\\r|\\n)*?\\*/|//.*[\\n\\r])*(?:"[^"]*";|\'[^\']*\';)?');
+
 /**
  * Set values on the global/window object at the start of a module.
  *
@@ -22,5 +31,7 @@ export default function valueInjectionLoader(this: LoaderThis<LoaderOptions>, us
     .map(([key, value]) => `globalThis["${key}"] = ${JSON.stringify(value)};`)
     .join('\n');
 
-  return `${injectedCode}\n${userCode}`;
+  return userCode.replace(SKIP_COMMENT_AND_DIRECTIVE_REGEX, match => {
+    return match + injectedCode;
+  });
 }
