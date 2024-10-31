0b75fcb38d96a20ba03ad4791a95b9b6f83a23fb
Francesco Novy <francesco.novy@sentry.io>
2024-10-31 15:54:37 +0100

    fix some test comments

--- dev-packages/node-integration-tests/suites/tracing/nestjs-no-express/scenario.ts
+++ dev-packages/node-integration-tests/suites/tracing/nestjs-no-express/scenario.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck These are only tests
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import { loggingTransport, sendPortToRunner } from '@sentry-internal/node-integration-tests';
@@ -35,7 +33,7 @@ class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('test-exception/:id')
-  async testException(@Param('id') id: string): void {
+  async testException(@Param('id') id: string): Promise<void> {
     Sentry.captureException(new Error(`error with id ${id}`));
   }
 }
