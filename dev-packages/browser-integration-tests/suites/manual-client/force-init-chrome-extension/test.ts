4726237bb67242685a02acd540df8e56cffcb757
Lukas Stracke <lukas.stracke@sentry.io>
2024-10-31 09:52:54 +0100

    test name

--- dev-packages/browser-integration-tests/suites/manual-client/force-init-chrome-extension/test.ts
+++ dev-packages/browser-integration-tests/suites/manual-client/force-init-chrome-extension/test.ts
@@ -1,13 +1,16 @@
 import { expect } from '@playwright/test';
 import { sentryTest } from '../../../utils/fixtures';
 
-sentryTest('should not initialize when inside a Chrome browser extension', async ({ getLocalTestUrl, page }) => {
-  const url = await getLocalTestUrl({ testDir: __dirname });
-  await page.goto(url);
+sentryTest(
+  'initializes inside a Chrome browser extension if `skipBrowserExtensionCheck` is set',
+  async ({ getLocalTestUrl, page }) => {
+    const url = await getLocalTestUrl({ testDir: __dirname });
+    await page.goto(url);
 
-  const isInitialized = await page.evaluate(() => {
-    return !!(window as any).Sentry.isInitialized();
-  });
+    const isInitialized = await page.evaluate(() => {
+      return !!(window as any).Sentry.isInitialized();
+    });
 
-  expect(isInitialized).toBe(true);
-});
+    expect(isInitialized).toBe(true);
+  },
+);
