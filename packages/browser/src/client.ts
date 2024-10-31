ab9177455fa28bbc8c3bd36998a62ac8c62d0885
Lukas Stracke <lukas.stracke@sentry.io>
2024-10-31 10:40:54 +0100

    adjust jsdoc

--- packages/browser/src/client.ts
+++ packages/browser/src/client.ts
@@ -35,11 +35,12 @@ export type BrowserOptions = Options<BrowserTransportOptions> &
      * because browser extensions require a different Sentry initialization process:
      * https://docs.sentry.io/platforms/javascript/best-practices/shared-environments/
      *
+     * Setting up the SDK in a browser extension with global error monitoring is not recommended
+     * and will likely flood you with errors from other web sites or extensions. This can heavily
+     * impact your quota and cause interference with your and other Sentry SDKs in shared environments.
+     *
      * If this check wrongfully flags your setup as a browser extension, you can set this
      * option to `true` to skip the check.
-     *
-     * Setting up the SDK in a browser extension with global error monitoring is not recommended
-     * and will likely flood you with errors from other web sites or extensions.
      *
      * @default false
      */
