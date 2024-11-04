6729214ab7963071c54b28fbaee737a2f306e33d
Charly Gomez <charly.gomez@sentry.io>
2024-11-04 12:41:45 +0100

    fix(core): Silently fail maybeInstrument (#14140)
    Co-authored-by: Francesco Novy <francesco.novy@sentry.io>

--- .size-limit.js
+++ .size-limit.js
@@ -224,7 +224,7 @@ module.exports = [
     import: createImport('init'),
     ignore: ['next/router', 'next/constants'],
     gzip: true,
-    limit: '39.1 KB',
+    limit: '40 KB',
   },
   // SvelteKit SDK (ESM)
   {
