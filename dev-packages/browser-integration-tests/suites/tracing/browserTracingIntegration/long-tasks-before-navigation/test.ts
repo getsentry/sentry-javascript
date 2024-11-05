6bf0b8fff18d83480271163bbbb616ece60fe245
Lukas Stracke <lukas.stracke@sentry.io>
2024-11-05 13:28:32 +0100

    add regression test

---
+++ dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/long-tasks-before-navigation/test.ts
@@ -0,0 +1,27 @@
+import type { Event } from '@sentry/types';
+import { expect } from '@playwright/test';
+
+import { sentryTest } from '../../../../utils/fixtures';
+import { getFirstSentryEnvelopeRequest, shouldSkipTracingTest } from '../../../../utils/helpers';
+
+sentryTest(
+  "doesn't capture long task spans starting before a navigation in the navigation transaction",
+  async ({ browserName, getLocalTestPath, page }) => {
+    // Long tasks only work on chrome
+    if (shouldSkipTracingTest() || browserName !== 'chromium') {
+      sentryTest.skip();
+    }
+    const url = await getLocalTestPath({ testDir: __dirname });
+
+    await page.goto(url);
+
+    await page.locator('#myButton').click();
+
+    const navigationTransactionEvent = await getFirstSentryEnvelopeRequest<Event>(page, url);
+
+    expect(navigationTransactionEvent.contexts?.trace?.op).toBe('navigation');
+
+    const longTaskSpans = navigationTransactionEvent?.spans?.filter(span => span.op === 'ui.long-task');
+    expect(longTaskSpans).toHaveLength(0);
+  },
+);
