f9a659b8aa2f3d8ebf91ff2b28cc177d856d4bde
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-11-05 17:09:48 -0800

    Delete LD pkg

--- packages/launchdarkly/src/index.ts
+++
@@ -1,6 +0,0 @@
-// This file is used as entry point to generate the npm package and CDN bundles.
-
-export { launchDarklyIntegration } from './core/integration';
-
-// export type {
-// } from './types';
