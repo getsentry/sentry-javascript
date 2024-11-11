68999c9299ac7654c637634a20357cee719f6aab
Luca Forstner <luca.forstner@sentry.io>
2024-11-11 09:00:46 +0000

    Fix scoping and add tests

--- dev-packages/e2e-tests/test-applications/nextjs-t3/tests/trpc-error.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-t3/tests/trpc-error.test.ts
@@ -12,9 +12,10 @@ test('should capture error with trpc context', async ({ page }) => {
   const trpcError = await errorEventPromise;
 
   expect(trpcError).toBeDefined();
-  expect(trpcError.contexts.trpc).toBeDefined();
-  expect(trpcError.contexts.trpc.procedure_type).toEqual('mutation');
-  expect(trpcError.contexts.trpc.input).toEqual({ name: 'I love dogs' });
+  expect(trpcError.contexts?.trpc).toBeDefined();
+  expect(trpcError.contexts?.trpc?.procedure_type).toEqual('mutation');
+  expect(trpcError.contexts?.trpc?.procedure_path).toBe('post.throwError');
+  expect(trpcError.contexts?.trpc?.input).toEqual({ name: 'I love dogs' });
 });
 
 test('should create transaction with trpc input for error', async ({ page }) => {
@@ -26,9 +27,5 @@ test('should create transaction with trpc input for error', async ({ page }) => 
   await page.click('#error-button');
 
   const trpcTransaction = await trpcTransactionPromise;
-
   expect(trpcTransaction).toBeDefined();
-  expect(trpcTransaction.contexts.trpc).toBeDefined();
-  expect(trpcTransaction.contexts.trpc.procedure_type).toEqual('mutation');
-  expect(trpcTransaction.contexts.trpc.input).toEqual({ name: 'I love dogs' });
 });
