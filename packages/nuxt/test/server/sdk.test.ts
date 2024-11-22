3d614bd6d8964bfec0d3b8698f727f61a9f03113
s1gr1d <sigrid.huemer@posteo.at>
2024-11-22 21:50:46 +0100

    add tests

--- packages/nuxt/test/server/sdk.test.ts
+++ packages/nuxt/test/server/sdk.test.ts
@@ -1,12 +1,23 @@
 import * as SentryNode from '@sentry/node';
 import type { NodeClient } from '@sentry/node';
+import { Scope } from '@sentry/node';
+import { getGlobalScope } from '@sentry/node';
 import { SDK_VERSION } from '@sentry/node';
+import type { EventProcessor } from '@sentry/types';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import type { SentryNuxtServerOptions } from '../../src/common/types';
 import { init } from '../../src/server';
-import { mergeRegisterEsmLoaderHooks } from '../../src/server/sdk';
+import { clientSourceMapErrorFilter, mergeRegisterEsmLoaderHooks } from '../../src/server/sdk';
 
 const nodeInit = vi.spyOn(SentryNode, 'init');
+
+let passedEventProcessors: EventProcessor[] = [];
+const addEventProcessor = vi
+  .spyOn(getGlobalScope(), 'addEventProcessor')
+  .mockImplementation((eventProcessor: EventProcessor) => {
+    passedEventProcessors = [...passedEventProcessors, eventProcessor];
+    return new Scope();
+  });
 
 describe('Nuxt Server SDK', () => {
   describe('init', () => {
@@ -82,6 +93,80 @@ describe('Nuxt Server SDK', () => {
         }),
         expect.any(Object),
       );
+    });
+
+    it('registers an event processor', async () => {
+      init({
+        dsn: 'https://public@dsn.ingest.sentry.io/1337',
+      });
+
+      expect(addEventProcessor).toHaveBeenCalledTimes(2);
+      expect(passedEventProcessors[0]?.id).toEqual('NuxtLowQualityTransactionsFilter');
+      expect(passedEventProcessors[1]?.id).toEqual('NuxtClientSourceMapErrorFilter');
+    });
+  });
+
+  describe('clientSourceMapErrorFilter', () => {
+    const options = { debug: false };
+    const filter = clientSourceMapErrorFilter(options);
+
+    describe('filters out errors', () => {
+      it.each([
+        [
+          'source map errors with leading /',
+          {
+            exception: { values: [{ value: "ENOENT: no such file or directory, open '/path/to/_nuxt/file.js.map'" }] },
+          },
+        ],
+        [
+          'source map errors without leading /',
+          { exception: { values: [{ value: "ENOENT: no such file or directory, open 'path/to/_nuxt/file.js.map'" }] } },
+        ],
+        [
+          'source map errors with long path',
+          {
+            exception: {
+              values: [
+                {
+                  value:
+                    "ENOENT: no such file or directory, open 'path/to/public/_nuxt/public/long/long/path/file.js.map'",
+                },
+              ],
+            },
+          },
+        ],
+      ])('filters out %s', (_, event) => {
+        // @ts-expect-error Event type is not correct in tests
+        expect(filter(event)).toBeNull();
+      });
+    });
+
+    describe('does not filter out errors', () => {
+      it.each([
+        ['other errors', { exception: { values: [{ value: 'Some other error' }] } }],
+        ['events with no exceptions', {}],
+        [
+          'events without _nuxt in path',
+          {
+            exception: { values: [{ value: "ENOENT: no such file or directory, open '/path/to/other/file.js.map'" }] },
+          },
+        ],
+        [
+          'source map errors with different casing',
+          {
+            exception: { values: [{ value: "ENOENT: No Such file or directory, open '/path/to/_nuxt/file.js.map'" }] },
+          },
+        ],
+        [
+          'non-source-map file',
+          { exception: { values: [{ value: "ENOENT: no such file or directory, open '/path/to/_nuxt/file.js'" }] } },
+        ],
+        ['events with no exception values', { exception: { values: [] } }],
+        ['events with null exception value', { exception: { values: [null] } }],
+      ])('does not filter out %s', (_, event) => {
+        // @ts-expect-error Event type is not correct in tests
+        expect(filter(event)).toEqual(event);
+      });
     });
   });
 
