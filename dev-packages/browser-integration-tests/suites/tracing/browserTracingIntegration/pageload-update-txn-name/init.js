95a6d65b9a43b3b99e0a06efb8b457cf0c6f43b3
Lukas Stracke <lukas.stracke@sentry.io>
2024-11-27 07:38:02 -0500

    oops

--- dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/pageload-update-txn-name/init.js
+++ dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/pageload-update-txn-name/init.js
@@ -3,20 +3,8 @@ import * as Sentry from '@sentry/browser';
 window.Sentry = Sentry;
 window._testBaseTimestamp = performance.timeOrigin / 1000;
 
-const sentryFeatureFlagsIntegration = Sentry.featureFlagsIntegration({
-  featureFlags: {
-    'session-aggregates': true,
-  },
-});
-
 Sentry.init({
-  integrations: [sentryFeatureFlagsIntegration],
-});
-
-Sentry.getFlagsIntegration().addFlags({
-  'session-aggregates': true,
-});
-
-Sentry.addFlags({
-  'session-aggregates': true,
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1,
 });
