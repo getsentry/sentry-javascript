e4175e5baf4ba8aa6c36f1a3257059c3e83f39b5
Luca Forstner <luca.forstner@sentry.io>
2024-11-27 12:37:49 +0000

    Recommendation to true.

--- packages/remix/src/utils/remixOptions.ts
+++ packages/remix/src/utils/remixOptions.ts
@@ -18,6 +18,7 @@ export type RemixOptions = (Options | BrowserOptions | NodeOptions) & {
          * Enables OpenTelemetry Remix instrumentation
          *
          * @deprecated Setting this option to `false` is deprecated as the next major version will default to behaving as if this option were `true` and the option itself will be removed.
+         * It is recommended to set this option to `true`.
          */
         autoInstrumentRemix?: false;
       }
