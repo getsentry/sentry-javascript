d4505a4efae31ca24eb20d95d7d95a5ce17c4f86
JonasBa <jonas@badalic.com>
2024-11-29 22:33:54 -0500

    test runtime

--- dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
+++ dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
@@ -10,21 +10,21 @@ function assertUnpatechedRequire(cycle) {
 }
 
 assertUnpatechedRequire('at startup');
-import * as Sentry from '@sentry/node';
-assertUnpatechedRequire('after importing sentry/node');
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
-assertUnpatechedRequire('after importing sentry/profiling-node');
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+// import * as Sentry from '@sentry/node';
+// assertUnpatechedRequire('after importing sentry/node');
+// import { nodeProfilingIntegration } from '@sentry/profiling-node';
+// assertUnpatechedRequire('after importing sentry/profiling-node');
+// const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-Sentry.init({
-  dsn: 'https://7fa19397baaf433f919fbe02228d5470@o1137848.ingest.sentry.io/6625302',
-  integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: 'https://7fa19397baaf433f919fbe02228d5470@o1137848.ingest.sentry.io/6625302',
+//   integrations: [nodeProfilingIntegration()],
+//   tracesSampleRate: 1.0,
+//   profilesSampleRate: 1.0,
+// });
 
-Sentry.startSpan({ name: 'Precompile test' }, async () => {
-  await wait(500);
-});
+// Sentry.startSpan({ name: 'Precompile test' }, async () => {
+//   await wait(500);
+// });
 
-assertUnpatechedRequire('after a span was created');
+// assertUnpatechedRequire('after a span was created');
