4626a4e03b8b9c71b6cf20e1f7bc11b2cc793b64
JonasBa <jonas@badalic.com>
2024-11-30 14:06:00 -0500

    test profiling

--- dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
+++ dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
@@ -9,16 +9,14 @@ function assertUnpatechedRequire(cycle) {
   }
 }
 
-assertUnpatechedRequire('at startup');
 import * as Sentry from '@sentry/node';
-// assertUnpatechedRequire('after importing sentry/node');
-// import { nodeProfilingIntegration } from '@sentry/profiling-node';
-// assertUnpatechedRequire('after importing sentry/profiling-node');
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+assertUnpatechedRequire('after importing sentry/profiling-node');
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 Sentry.init({
   dsn: 'https://7fa19397baaf433f919fbe02228d5470@o1137848.ingest.sentry.io/6625302',
-  // integrations: [nodeProfilingIntegration()],
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
 });
