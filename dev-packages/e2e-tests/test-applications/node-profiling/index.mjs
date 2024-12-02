de1a11f8c764c33f40982842a370385edd6da2ad
JonasBa <jonas@badalic.com>
2024-12-02 16:20:17 -0500

    define main and module

--- dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
+++ dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
@@ -1,21 +1,24 @@
 // This tests asserts that @sentr/profiling-node is not patching globalThis values, which
 // breaks our runtime detection and can break instrumentation
 // https://github.com/getsentry/sentry-javascript/issues/14525#issuecomment-2511208064
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+console.log('🧵 Starting ESM test');
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 function assertUnpatechedRequire() {
-  if (typeof globalThis.require !== 'undefined') {
+  if (typeof require !== 'undefined') {
     // Test that globalThis.require is not defined by any side effects of the profiling
     // https://github.com/getsentry/sentry-javascript/issues/13662
     throw new Error(
       `globalThis.require should not be defined, check that profiling integration is not defining it, received: ` +
-        typeof globalThis.require,
+        typeof require,
     );
   }
 }
 
-import * as Sentry from '@sentry/node';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 Sentry.init({
   dsn: 'https://7fa19397baaf433f919fbe02228d5470@o1137848.ingest.sentry.io/6625302',
