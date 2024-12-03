629bedd0b686b00c8cbdba0866d5ecd50af9bd71
JonasBa <jonas@badalic.com>
2024-12-03 13:23:51 -0500

    format

--- dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
+++ dev-packages/e2e-tests/test-applications/node-profiling/index.mjs
@@ -19,7 +19,6 @@ function assertUnpatechedRequire() {
   }
 }
 
-
 Sentry.init({
   dsn: 'https://7fa19397baaf433f919fbe02228d5470@o1137848.ingest.sentry.io/6625302',
   integrations: [nodeProfilingIntegration()],
