02cd633299d766c969ed7688631ca27519e77bb5
Luca Forstner <luca.forstner@sentry.io>
2024-12-03 10:40:59 +0000

    docs

--- packages/browser/src/integrations/session.ts
+++ packages/browser/src/integrations/session.ts
@@ -5,6 +5,7 @@ import { WINDOW } from '../helpers';
 
 /**
  * When added, automatically creates sessions which allow you to track adoption and crashes (crash free rate) in your Releases in Sentry.
+ * More information: https://docs.sentry.io/product/releases/health/
  *
  * Note: In order for session tracking to work, you need to set up Releases: https://docs.sentry.io/product/releases/
  */
