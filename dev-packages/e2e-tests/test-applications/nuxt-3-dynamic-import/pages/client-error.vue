30c4bc5b75ad70dbc3478dc88d692fe2e7ac4ce2
s1gr1d <sigrid.huemer@posteo.at>
2024-12-05 11:09:13 +0100

    add E2E tests

---
+++ dev-packages/e2e-tests/test-applications/nuxt-3-dynamic-import/pages/client-error.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ErrorButton from '../components/ErrorButton.vue';
+</script>
+
+<template>
+  <ErrorButton id="errorBtn"  error-text="Error thrown from Nuxt-3 E2E test app"/>
+  <ErrorButton id="errorBtn2" error-text="Another Error thrown from Nuxt-3 E2E test app"/>
+</template>
+
+
+
---
+++ dev-packages/e2e-tests/test-applications/nuxt-3-top-level-import/playwright.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import type { ConfigOptions } from '@nuxt/test-utils/playwright';
+import { getPlaywrightConfig } from '@sentry-internal/test-utils';
+
+const nuxtConfigOptions: ConfigOptions = {
+  nuxt: {
+    rootDir: fileURLToPath(new URL('.', import.meta.url)),
+  },
+};
+
+/*  Make sure to import from '@nuxt/test-utils/playwright' in the tests
+ *  Like this: import { expect, test } from '@nuxt/test-utils/playwright' */
+
+const config = getPlaywrightConfig({
+  startCommand: `pnpm start`,
+  use: { ...nuxtConfigOptions },
+});
+
+export default config;
