e1dae9daf2a0b56e347de260345b37a7e9a6c176
s1gr1d <sigrid.huemer@posteo.at>
2024-12-06 10:48:26 +0100

    fix(nuxt): Flush serverless to send events

--- packages/nuxt/src/runtime/plugins/sentry.server.ts
+++ packages/nuxt/src/runtime/plugins/sentry.server.ts
@@ -1,3 +1,14 @@
+import {
+  GLOBAL_OBJ,
+  consoleSandbox,
+  flush,
+  getClient,
+  getDefaultIsolationScope,
+  getIsolationScope,
+  logger,
+  vercelWaitUntil,
+  withIsolationScope,
+} from '@sentry/core';
 import * as Sentry from '@sentry/node';
 import { H3Error } from 'h3';
 import { defineNitroPlugin } from 'nitropack/runtime';
@@ -5,7 +16,34 @@ import type { NuxtRenderHTMLContext } from 'nuxt/app';
 import { addSentryTracingMetaTags, extractErrorContext } from '../utils';
 
 export default defineNitroPlugin(nitroApp => {
-  nitroApp.hooks.hook('error', (error, errorContext) => {
+  nitroApp.h3App.handler = new Proxy(nitroApp.h3App.handler, {
+    async apply(handlerTarget, handlerThisArg, handlerArgs: Parameters<typeof nitroApp.h3App.handler>) {
+      // In environments where we cannot make use of OTel httpInstrumentation, e.g. when using top level import
+      // of the server instrumentation file instead of `--import` or dynamic import like on vercel
+      // we still need to ensure requests are properly isolated
+      const isolationScope = getIsolationScope();
+      const newIsolationScope = isolationScope === getDefaultIsolationScope() ? isolationScope.clone() : isolationScope;
+
+      consoleSandbox(() => {
+        // eslint-disable-next-line no-console
+        console.log(
+          `[Sentry] Patched event handler. Using ${
+            isolationScope === newIsolationScope ? 'existing' : 'new'
+          } isolationscope.`,
+        );
+      });
+
+      return withIsolationScope(newIsolationScope, async () => {
+        try {
+          return await handlerTarget.apply(handlerThisArg, handlerArgs);
+        } finally {
+          await flushIfServerless();
+        }
+      });
+    },
+  });
+
+  nitroApp.hooks.hook('error', async (error, errorContext) => {
     // Do not handle 404 and 422
     if (error instanceof H3Error) {
       // Do not report if status code is 3xx or 4xx
@@ -29,6 +67,8 @@ export default defineNitroPlugin(nitroApp => {
       captureContext: { contexts: { nuxt: structuredContext } },
       mechanism: { handled: false },
     });
+
+    await flushIfServerless();
   });
 
   // @ts-expect-error - 'render:html' is a valid hook name in the Nuxt context
@@ -36,3 +76,27 @@ export default defineNitroPlugin(nitroApp => {
     addSentryTracingMetaTags(html.head);
   });
 });
+
+async function flushIfServerless(): Promise<void> {
+  const isServerless = !!process.env.LAMBDA_TASK_ROOT || !!process.env.VERCEL || !!process.env.NETLIFY;
+
+  // @ts-expect-error This is not typed
+  if (GLOBAL_OBJ[Symbol.for('@vercel/request-context')]) {
+    vercelWaitUntil(flushWithTimeout());
+  } else if (isServerless) {
+    await flushWithTimeout();
+  }
+}
+
+async function flushWithTimeout(): Promise<void> {
+  const sentryClient = getClient();
+  const isDebug = sentryClient ? sentryClient.getOptions().debug : false;
+
+  try {
+    isDebug && logger.log('Flushing events...');
+    await flush(2000);
+    isDebug && logger.log('Done flushing events');
+  } catch (e) {
+    isDebug && logger.log('Error while flushing events:\n', e);
+  }
+}
