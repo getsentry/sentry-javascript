351574426d8c8c00963a9c9612dcf11daf566651
Luca Forstner <luca.forstner@sentry.io>
2024-12-11 13:44:48 +0000

    fix test

--- dev-packages/node-integration-tests/suites/esm/import-in-the-middle/test.ts
+++ dev-packages/node-integration-tests/suites/esm/import-in-the-middle/test.ts
@@ -1,12 +1,15 @@
+import { spawnSync } from 'child_process';
+import { join } from 'path';
 import { conditionalTest } from '../../../utils';
-import { cleanupChildProcesses, createRunner } from '../../../utils/runner';
+import { cleanupChildProcesses } from '../../../utils/runner';
 
 afterAll(() => {
   cleanupChildProcesses();
 });
 
 conditionalTest({ min: 18 })('import-in-the-middle', () => {
-  test('should only instrument modules that we have instrumentation for', done => {
-    createRunner(__dirname, 'app.mjs').ensureNoErrorOutput().start(done);
+  test('should only instrument modules that we have instrumentation for', () => {
+    const result = spawnSync('node', [join(__dirname, 'app.mjs')], { encoding: 'utf-8' });
+    expect(result.stderr).not.toMatch('should be the only hooked modules but we just hooked');
   });
 });
