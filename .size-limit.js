2cff1f7bff8a7588e695efced80afbb0a8d1f5e4
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-12-12 17:26:29 +0100

    meta(svelte): Increase size limit (#14685)
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>

--- .size-limit.js
+++ .size-limit.js
@@ -147,7 +147,7 @@ module.exports = [
     path: 'packages/svelte/build/esm/index.js',
     import: createImport('init'),
     gzip: true,
-    limit: '24 KB',
+    limit: '25 KB',
   },
   // Browser CDN bundles
   {
