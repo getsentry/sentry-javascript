dc510d20a391838b1b85908a5af1526534f154bd
Sam Greening <2552620+SG60@users.noreply.github.com>
2024-12-17 11:21:50 +0000

    test(sveltekit-cloudflare-pages): add an e2e test for @sentry/sveltekit on cloudflare pages
    Seems to have an issue with wrangler hanging during the playwright test
    though.

---
+++ dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/src/routes/server-load-fetch/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<main>
+  <h1>Server Load Fetch</h1>
+  <p>{JSON.stringify(data, null, 2)}</p>
+</main>
---
+++ dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/src/routes/server-route-error/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<h1>Server Route error</h1>
+
+<p>
+  Message: {data.msg}
+</p>
