1099eac05e0d99bce618d8ae2cd60417b2806413
Francesco Gringl-Novy <francesco.novy@sentry.io>
2025-01-07 09:35:41 +0100

    fix(feedback/v8): Avoid lazy loading code for `syncFeedbackIntegration`
    Closes https://github.com/getsentry/sentry-javascript/issues/14891
    
    This PR updates the sync feedback integration to avoid it pulling in the
    `lazyLoadIntegration` code. This is not really needed and leads to some
    problems (and also bundle size increase).
    
    For this I updated the type signature of `buildFeedbackIntegration` to
    ensure that _either_ `lazyLoadIntegration` _or_ the getter functions are
    provided, so we can type-safely use this.
    
    We probably also want to backport this to v8 then.

--- packages/browser/src/feedbackSync.ts
+++ packages/browser/src/feedbackSync.ts
@@ -3,11 +3,9 @@ import {
   feedbackModalIntegration,
   feedbackScreenshotIntegration,
 } from '@sentry-internal/feedback';
-import { lazyLoadIntegration } from './utils/lazyLoadIntegration';
 
 /** Add a widget to capture user feedback to your application. */
 export const feedbackSyncIntegration = buildFeedbackIntegration({
-  lazyLoadIntegration,
   getModalIntegration: () => feedbackModalIntegration,
   getScreenshotIntegration: () => feedbackScreenshotIntegration,
 });
