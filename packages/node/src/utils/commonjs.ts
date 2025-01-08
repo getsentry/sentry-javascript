331f955da15787607b81bba7e72200016c9af32d
Luca Forstner <luca.forstner@sentry.io>
2025-01-08 13:30:55 +0000

    don't change how we detect commonjs

--- packages/node/src/utils/commonjs.ts
+++ packages/node/src/utils/commonjs.ts
@@ -1,7 +1,4 @@
-import { GLOBAL_OBJ } from '@sentry/core';
-
 /** Detect CommonJS. */
 export function isCjs(): boolean {
-  // @ts-expect-error Require is not defined on global obj
-  return !!GLOBAL_OBJ.require;
+  return typeof require !== 'undefined';
 }
