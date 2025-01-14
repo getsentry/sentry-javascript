46e834bb157a65488ee39ccb9626cc9e922ec165
Charly Gomez <charly.gomez@sentry.io>
2025-01-14 19:07:21 +0100

    update integration tests

--- packages/remix/test/integration/instrument.server.cjs
+++ packages/remix/test/integration/instrument.server.cjs
@@ -4,5 +4,5 @@ Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   tracesSampleRate: 1,
   tracePropagationTargets: ['example.org'],
-  autoInstrumentRemix: process.env.USE_OTEL === '1',
+  autoInstrumentRemix: true,
 });
