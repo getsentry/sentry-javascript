244f579a042f27ab38d7f8cc7a3974118536027c
Charly Gomez <charly.gomez@sentry.io>
2025-01-16 18:59:08 +0100

    rename app dir

--- packages/remix/test/integration/remix.config.js
+++ packages/remix/test/integration/remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  appDirectory: 'app_v2',
+  appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   serverBuildPath: 'build/index.js',
   publicPath: '/build/',
