aa115441edc0fd3d29dd5715bb6ede4116c03cb7
Charly Gomez <charly.gomez@sentry.io>
2025-01-16 17:51:31 +0100

    biome

--- packages/remix/test/integration/app_v2/entry.client.tsx
+++ packages/remix/test/integration/app_v2/entry.client.tsx
@@ -1,6 +1,6 @@
 import { RemixBrowser, useLocation, useMatches } from '@remix-run/react';
 import * as Sentry from '@sentry/remix';
-import { startTransition, StrictMode, useEffect } from 'react';
+import { StrictMode, startTransition, useEffect } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
 Sentry.init({
