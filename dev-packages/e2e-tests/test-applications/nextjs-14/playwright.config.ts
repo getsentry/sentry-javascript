99629aa88cb5603fd948dba1df1b119fb7d5b111
Luca Forstner <luca.forstner@sentry.io>
2025-01-17 12:52:34 +0000

    ci(e2e-test): Unparallelize nextjs-14 e2e tests

--- dev-packages/e2e-tests/test-applications/nextjs-14/playwright.config.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-14/playwright.config.ts
@@ -5,15 +5,9 @@ if (!testEnv) {
   throw new Error('No test env defined');
 }
 
-const config = getPlaywrightConfig(
-  {
-    startCommand: testEnv === 'development' ? 'pnpm next dev -p 3030' : 'pnpm next start -p 3030',
-    port: 3030,
-  },
-  {
-    // This comes with the risk of tests leaking into each other but the tests run quite slow so we should parallelize
-    workers: '100%',
-  },
-);
+const config = getPlaywrightConfig({
+  startCommand: testEnv === 'development' ? 'pnpm next dev -p 3030' : 'pnpm next start -p 3030',
+  port: 3030,
+});
 
 export default config;
