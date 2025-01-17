292f73b4d5d94331531745a157eb31087838a59d
Charly Gomez <charly.gomez@sentry.io>
2025-01-17 10:47:03 +0100

    move integration tests to esm

--- packages/remix/test/integration/instrument.server.mjs
+++ packages/remix/test/integration/instrument.server.mjs
@@ -1,4 +1,4 @@
-const Sentry = require('@sentry/remix');
+import * as Sentry from '@sentry/remix';
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
--- packages/remix/test/integration/remix.config.js
+++ packages/remix/test/integration/remix.config.js
@@ -1,8 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   serverBuildPath: 'build/index.js',
   publicPath: '/build/',
-  serverModuleFormat: 'cjs',
 };
