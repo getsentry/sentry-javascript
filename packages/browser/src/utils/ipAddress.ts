3115cc42eaa01aac1fd153880858cb5fb31a7c7a
Krystof Woldrich <krystof.woldrich@sentry.io>
2025-01-21 15:52:48 +0100

    change jsdoc

--- packages/browser/src/utils/ipAddress.ts
+++ packages/browser/src/utils/ipAddress.ts
@@ -1,10 +1,11 @@
 import type { Session, SessionAggregates, User } from '@sentry/core';
 
+// By default, we want to infer the IP address, unless this is explicitly set to `null`
+// We do this after all other processing is done
+// If `ip_address` is explicitly set to `null` or a value, we leave it as is
+
 /**
  * @internal
- * By default, we want to infer the IP address, unless this is explicitly set to `null`
- * We do this after all other processing is done
- * If `ip_address` is explicitly set to `null` or a value, we leave it as is
  */
 export function addAutoIpAddressToUser(objWithMaybeUser: { user?: User | null }): void {
   if (objWithMaybeUser.user?.ip_address === undefined) {
@@ -17,8 +18,6 @@ export function addAutoIpAddressToUser(objWithMaybeUser: { user?: User | null })
 
 /**
  * @internal
- * Adds the `ip_address` attribute to the session if it is not explicitly set to `null` or a value.
- * @param session The session to add the `ip_address` attribute to.
  */
 export function addAutoIpAddressToSession(session: Session | SessionAggregates): void {
   if ('aggregates' in session) {
