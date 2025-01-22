b05ae85cf781f6af181e0d259a14ffc3256c3575
Charly Gomez <charly.gomez@sentry.io>
2025-01-22 09:19:43 +0100

    ...

--- dev-packages/e2e-tests/test-applications/nestjs-11/tests/errors.test.ts
+++ dev-packages/e2e-tests/test-applications/nestjs-11/tests/errors.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { waitForError, waitForTransaction } from '@sentry-internal/test-utils';
 
-test.only('Sends exception to Sentry', async ({ baseURL }) => {
+test('Sends exception to Sentry', async ({ baseURL }) => {
   const errorEventPromise = waitForError('nestjs-11', event => {
     return !event.type && event.exception?.values?.[0]?.value === 'This is an exception with id 123';
   });
