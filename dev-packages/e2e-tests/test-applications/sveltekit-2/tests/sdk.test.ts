52cd25f732304a3d1ebfc7871b67097710ba6227
Lukas Stracke <lukas.stracke@sentry.io>
2025-01-23 09:23:49 +0100

    fix test

--- dev-packages/e2e-tests/test-applications/sveltekit-2/tests/sdk.test.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-2/tests/sdk.test.ts
@@ -4,6 +4,7 @@ import { waitForInitialPageload } from './utils';
 test.describe('SDK-internal behavior', () => {
   test("Doesn't inject fetch proxy script for SvelteKit>=2.16.0", async ({ page }) => {
     await waitForInitialPageload(page, { route: '/client-error' });
+    // @ts-expect-error - this is the variable the fetch proxy script sets if it would be injected
     const proxyHandle = await page.waitForFunction(() => globalThis._sentryFetchProxy);
     expect(proxyHandle).toBeUndefined();
   });
