4c20f14f0ebf74498cca5d0313003868516aee39
Kaung Zin Hein <83657429+Zen-cronic@users.noreply.github.com>
2025-01-22 22:42:00 -0500

    fix(core): Revert resolved rebase conflict
    Signed-off-by: Kaung Zin Hein <83657429+Zen-cronic@users.noreply.github.com>

--- packages/core/test/utils-hoist/instrument/fetch.test.ts
+++ packages/core/test/utils-hoist/instrument/fetch.test.ts
@@ -1,31 +1,25 @@
 import { parseFetchArgs } from '../../../src/utils-hoist/instrument/fetch';
 
 describe('instrument > parseFetchArgs', () => {
-  const data = { name: 'Test' };
-
   it.each([
-    ['string URL only', ['http://example.com'], { method: 'GET', url: 'http://example.com', body: null }],
-    ['URL object only', [new URL('http://example.com')], { method: 'GET', url: 'http://example.com/', body: null }],
-    ['Request URL only', [{ url: 'http://example.com' }], { method: 'GET', url: 'http://example.com', body: null }],
+    ['string URL only', ['http://example.com'], { method: 'GET', url: 'http://example.com' }],
+    ['URL object only', [new URL('http://example.com')], { method: 'GET', url: 'http://example.com/' }],
+    ['Request URL only', [{ url: 'http://example.com' }], { method: 'GET', url: 'http://example.com' }],
     [
       'Request URL & method only',
       [{ url: 'http://example.com', method: 'post' }],
-      { method: 'POST', url: 'http://example.com', body: null },
+      { method: 'POST', url: 'http://example.com' },
     ],
-    [
-      'string URL & options',
-      ['http://example.com', { method: 'post', body: JSON.stringify(data) }],
-      { method: 'POST', url: 'http://example.com', body: '{"name":"Test"}' },
-    ],
+    ['string URL & options', ['http://example.com', { method: 'post' }], { method: 'POST', url: 'http://example.com' }],
     [
       'URL object & options',
-      [new URL('http://example.com'), { method: 'post', body: JSON.stringify(data) }],
-      { method: 'POST', url: 'http://example.com/', body: '{"name":"Test"}' },
+      [new URL('http://example.com'), { method: 'post' }],
+      { method: 'POST', url: 'http://example.com/' },
     ],
     [
       'Request URL & options',
-      [{ url: 'http://example.com' }, { method: 'post', body: JSON.stringify(data) }],
-      { method: 'POST', url: 'http://example.com', body: '{"name":"Test"}' },
+      [{ url: 'http://example.com' }, { method: 'post' }],
+      { method: 'POST', url: 'http://example.com' },
     ],
   ])('%s', (_name, args, expected) => {
     const actual = parseFetchArgs(args as unknown[]);
