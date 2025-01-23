a76f243e1c529b1ae8bb658f382ab2bce5409770
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2025-01-23 17:32:32 +0100

    feat(v8/solidstart): Add `withSentry` wrapper for SolidStart config (#15135)
    Backport of https://github.com/getsentry/sentry-javascript/pull/14862
    and https://github.com/getsentry/sentry-javascript/pull/14863
    
    ---------
    
    Co-authored-by: Andrei Borza <andrei.borza@sentry.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,50 @@
 
 - "You miss 100 percent of the chances you don't take. — Wayne Gretzky" — Michael Scott
 
+- **feat(solidstart): Default to `--import` setup and add `autoInjectServerSentry` ([#14862](https://github.com/getsentry/sentry-javascript/pull/14862))**
+
+To enable the SolidStart SDK, wrap your SolidStart Config with `withSentry`. The `sentrySolidStartVite` plugin is now automatically
+added by `withSentry` and you can pass the Sentry build-time options like this:
+
+```js
+import { defineConfig } from '@solidjs/start/config';
+import { withSentry } from '@sentry/solidstart';
+
+export default defineConfig(
+  withSentry(
+    {
+      /* Your SolidStart config options... */
+    },
+    {
+      // Options for setting up source maps
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  ),
+);
+```
+
+With the `withSentry` wrapper, the Sentry server config should not be added to the `public` directory anymore.
+Add the Sentry server config in `src/instrument.server.ts`. Then, the server config will be placed inside the server build output as `instrument.server.mjs`.
+
+Now, there are two options to set up the SDK:
+
+1. **(recommended)** Provide an `--import` CLI flag to the start command like this (path depends on your server setup):
+   `node --import ./.output/server/instrument.server.mjs .output/server/index.mjs`
+2. Add `autoInjectServerSentry: 'top-level-import'` and the Sentry config will be imported at the top of the server entry (comes with tracing limitations)
+   ```js
+   withSentry(
+     {
+       /* Your SolidStart config options... */
+     },
+     {
+       // Optional: Install Sentry with a top-level import
+       autoInjectServerSentry: 'top-level-import',
+     },
+   );
+   ```
+
 ## 8.51.0
 
 ### Important Changes
--- packages/solidstart/src/vite/sentrySolidStartVite.ts
+++ packages/solidstart/src/vite/sentrySolidStartVite.ts
@@ -1,12 +1,35 @@
-import type { Plugin } from 'vite';
+import type { Plugin, UserConfig } from 'vite';
+import { makeBuildInstrumentationFilePlugin } from './buildInstrumentationFile';
 import { makeSourceMapsVitePlugin } from './sourceMaps';
 import type { SentrySolidStartPluginOptions } from './types';
 
+// todo(v9): Don't export to users anymore and remove deprecation (and eslint warning silencing) when it's not exported anymore
 /**
  * Various Sentry vite plugins to be used for SolidStart.
+ *
+ * @deprecated This plugin will be removed in v9. Instead, use `withSentry` to wrap your SolidStart config. Example:
+ * ```
+ * export default defineConfig(
+ *   withSentry(
+ *     {
+ *       // SolidStart config...
+ *     },
+ *     {
+ *       // Sentry config
+ *       org: process.env.SENTRY_ORG,
+ *       project: process.env.SENTRY_PROJECT,
+ *       authToken: process.env.SENTRY_AUTH_TOKEN,
+ *     },
+ *   ),
+ * );
+ * ```
  */
 export const sentrySolidStartVite = (options: SentrySolidStartPluginOptions = {}): Plugin[] => {
   const sentryPlugins: Plugin[] = [];
+
+  if (options.autoInjectServerSentry !== 'experimental_dynamic-import') {
+    sentryPlugins.push(makeBuildInstrumentationFilePlugin(options));
+  }
 
   if (process.env.NODE_ENV !== 'development') {
     if (options.sourceMapsUploadOptions?.enabled ?? true) {
@@ -15,4 +38,18 @@ export const sentrySolidStartVite = (options: SentrySolidStartPluginOptions = {}
   }
 
   return sentryPlugins;
+};
+
+/**
+ * Helper to add the Sentry SolidStart vite plugin to a vite config.
+ */
+export const addSentryPluginToVite = (config: UserConfig = {}, options: SentrySolidStartPluginOptions): UserConfig => {
+  const plugins = Array.isArray(config.plugins) ? [...config.plugins] : [];
+  // eslint-disable-next-line deprecation/deprecation
+  plugins.unshift(sentrySolidStartVite(options));
+
+  return {
+    ...config,
+    plugins,
+  };
 };
