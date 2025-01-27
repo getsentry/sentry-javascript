a182658cc234c0c14667c4d083c2f476854d9dd0
Luca Forstner <luca.forstner@sentry.io>
2025-01-27 13:39:05 +0000

    Add v5 tests

---
+++ dev-packages/node-integration-tests/suites/tracing/prisma-orm-v5/scenario.js
@@ -0,0 +1,57 @@
+const Sentry = require('@sentry/node');
+const { PrismaInstrumentation } = require('@prisma/instrumentation');
+const { loggingTransport } = require('@sentry-internal/node-integration-tests');
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  release: '1.0',
+  tracesSampleRate: 1.0,
+  transport: loggingTransport,
+  integrations: [
+    Sentry.prismaIntegration({
+      prismaInstrumentation: new PrismaInstrumentation(),
+    }),
+  ],
+});
+
+const { randomBytes } = require('crypto');
+const { PrismaClient } = require('@prisma/client');
+
+// Stop the process from exiting before the transaction is sent
+setInterval(() => {}, 1000);
+
+async function run() {
+  const client = new PrismaClient();
+
+  await Sentry.startSpan(
+    {
+      name: 'Test Transaction',
+      op: 'transaction',
+    },
+    async span => {
+      await client.user.create({
+        data: {
+          name: 'Tilda',
+          email: `tilda_${randomBytes(4).toString('hex')}@sentry.io`,
+        },
+      });
+
+      await client.user.findMany();
+
+      await client.user.deleteMany({
+        where: {
+          email: {
+            contains: 'sentry.io',
+          },
+        },
+      });
+
+      setTimeout(async () => {
+        span.end();
+        await client.$disconnect();
+      }, 500);
+    },
+  );
+}
+
+run();
