6a4d772d41310456bb0df460c19d66c774913140
Lukas Stracke <lukas.stracke@sentry.io>
2025-01-27 16:46:13 +0100

    test(v8/e2e): Avoid making request to example.com in nextjs e2e test
    example.com still seems flaky, let's avoid making requests to it

--- dev-packages/e2e-tests/test-applications/nextjs-14/app/request-instrumentation/page.tsx
+++ dev-packages/e2e-tests/test-applications/nextjs-14/app/request-instrumentation/page.tsx
@@ -3,9 +3,9 @@ import http from 'http';
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  await fetch('http://example.com/', { cache: 'no-cache' }).then(res => res.text());
+  await fetch('http://github.com/', { cache: 'no-cache' }).then(res => res.text());
   await new Promise<void>(resolve => {
-    http.get('http://example.com/', res => {
+    http.get('http://github.com/', res => {
       res.on('data', () => {
         // Noop consuming some data so that request can close :)
       });
