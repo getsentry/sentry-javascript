0b53e27664988a88fd3a5f1161601158249e3244
Lukas Stracke <lukas.stracke@sentry.io>
2025-01-28 16:04:48 +0100

    add e2e tests to check for fetch proxy on versions 2.5.0 and ^2.16.0

--- dev-packages/e2e-tests/test-applications/sveltekit-2-twp/tests/sdk.test.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-2-twp/tests/sdk.test.ts
@@ -1,16 +1,16 @@
 import { expect, test } from '@playwright/test';
-import { waitForInitialPageload } from './utils';
 
 test.describe('SDK-internal behavior', () => {
-  test("Doesn't inject fetch proxy script for SvelteKit>=2.16.0", async ({ page }) => {
-    await waitForInitialPageload(page, { route: '/' });
+  test('Injects fetch proxy script for SvelteKit<2.16.0', async ({ page }) => {
+    await page.goto('/');
+
     const sentryCarrier = await page.evaluate('typeof window.__SENTRY__');
     const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
 
     // sanity check
     expect(sentryCarrier).toBe('object');
 
-    // fetch proxy script didn't run
-    expect(proxyHandle).toBe('undefined');
+    // fetch proxy script ran
+    expect(proxyHandle).toBe('function');
   });
 });
