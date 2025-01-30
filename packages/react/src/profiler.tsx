caebf119c8367daf32f77cb680f9a42e6d40abaa
Charly Gomez <charly.gomez@sentry.io>
2025-01-30 15:48:12 -0800

    fix default import

--- packages/react/src/profiler.tsx
+++ packages/react/src/profiler.tsx
@@ -1,8 +1,12 @@
 import { startInactiveSpan } from '@sentry/browser';
 import type { Span } from '@sentry/core';
 import { SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN, spanToJSON, timestampInSeconds, withActiveSpan } from '@sentry/core';
-import hoistNonReactStatics from 'hoist-non-react-statics';
+import * as hoistNonReactStatics from 'hoist-non-react-statics';
 import * as React from 'react';
+
+// Ensure we use the default export from hoist-non-react-statics if available,
+// falling back to the module itself. This handles both ESM and CJS usage.
+const hoistStatics: typeof hoistNonReactStatics.default = hoistNonReactStatics.default || hoistNonReactStatics;
 
 import { REACT_MOUNT_OP, REACT_RENDER_OP, REACT_UPDATE_OP } from './constants';
 
@@ -170,7 +174,7 @@ function withProfiler<P extends Record<string, any>>(
 
   // Copy over static methods from Wrapped component to Profiler HOC
   // See: https://reactjs.org/docs/higher-order-components.html#static-methods-must-be-copied-over
-  hoistNonReactStatics(Wrapped, WrappedComponent);
+  hoistStatics(Wrapped, WrappedComponent);
   return Wrapped;
 }
 
@@ -236,4 +240,4 @@ function useProfiler(
   }, []);
 }
 
-export { withProfiler, Profiler, useProfiler };
+export { Profiler, useProfiler, withProfiler };
