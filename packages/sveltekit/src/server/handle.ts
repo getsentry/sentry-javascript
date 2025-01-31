99b0d150ad8433fbee84edfc1ceca58824db6049
Lukas Stracke <lukas.stracke@sentry.io>
2025-01-31 16:47:57 +0100

    init SDK configured for CF in dev mode

--- packages/sveltekit/src/server/handle.ts
+++ packages/sveltekit/src/server/handle.ts
@@ -150,9 +150,22 @@ export function isFetchProxyRequired(version: string): boolean {
 }
 
 /**
- * actual implementation in ../worker/handle.ts
- * @return no-op handler when initCLoudflareSentryHandle is called via node/server entry point
+ * Actual implementation in ../worker/handle.ts
+ *
+ * This handler initializes the Sentry Node(!) SDK with the passed options. This is necessary to get
+ * the SDK configured for cloudflare working in dev mode.
+ *
+ * @return version of initCLoudflareSentryHandle that is called via node/server entry point
  */
-export function initCloudflareSentryHandle(_options: unknown): Handle {
-  return ({ event, resolve }) => resolve(event);
+export function initCloudflareSentryHandle(options: CloudflareOptions): Handle {
+  let sentryInitialized = false;
+
+  return ({ event, resolve }) => {
+    if (!sentryInitialized) {
+      sentryInitialized = true;
+      init(options);
+    }
+
+    return resolve(event);
+  };
 }
