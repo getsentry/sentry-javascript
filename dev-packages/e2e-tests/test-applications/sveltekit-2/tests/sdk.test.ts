86af2530c7157ab6625e8939cd9bedf28a6e1ec8
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-05 16:01:47 +0100

    add second test to be sure

--- dev-packages/e2e-tests/test-applications/sveltekit-2/tests/sdk.test.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-2/tests/sdk.test.ts
@@ -2,15 +2,17 @@ import { expect, test } from '@playwright/test';
 import { waitForInitialPageload } from './utils';
 
 test.describe('SDK-internal behavior', () => {
-  test("Doesn't inject fetch proxy script for SvelteKit>=2.16.0", async ({ page }) => {
-    await waitForInitialPageload(page, { route: '/' });
-    const sentryCarrier = await page.evaluate('typeof window.__SENTRY__');
-    const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
+  for (let i = 0; i < 100; i++) {
+    test("Doesn't inject fetch proxy script for SvelteKit>=2.16.0", async ({ page }) => {
+      await waitForInitialPageload(page, { route: '/' });
 
-    // sanity check
-    expect(sentryCarrier).toBe('object');
+      // @ts-expect-error this is defined
+      await page.waitForFunction(() => typeof window.__SENTRY__ === 'object');
 
-    // fetch proxy script didn't run
-    expect(proxyHandle).toBe('undefined');
-  });
+      const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
+
+      // fetch proxy script didn't run
+      expect(proxyHandle).toBe('undefined');
+    });
+  }
 });
