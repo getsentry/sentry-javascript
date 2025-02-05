525f71f414dc91ea22c215a06201f905f3d4cb26
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-05 15:58:21 +0100

    test(sveltekit): Unflake SvelteKit fetch proxy E2E test

--- dev-packages/e2e-tests/test-applications/sveltekit-2.5.0-twp/tests/sdk.test.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-2.5.0-twp/tests/sdk.test.ts
@@ -1,16 +1,17 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('SDK-internal behavior', () => {
-  test('Injects fetch proxy script for SvelteKit<2.16.0', async ({ page }) => {
-    await page.goto('/');
+  // TODO remove before merging
+  for (let i = 0; i < 100; i++) {
+    test(`Injects fetch proxy script for SvelteKit<2.16.0 ${i}`, async ({ page }) => {
+      await page.goto('/');
 
-    const sentryCarrier = await page.evaluate('typeof window.__SENTRY__');
-    const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
+      // @ts-expect-error this is defined
+      await page.waitForFunction(() => typeof window.__SENTRY__ === 'object');
+      const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
 
-    // sanity check
-    expect(sentryCarrier).toBe('object');
-
-    // fetch proxy script ran
-    expect(proxyHandle).toBe('function');
-  });
+      // fetch proxy script ran
+      expect(proxyHandle).toBe('function');
+    });
+  }
 });
