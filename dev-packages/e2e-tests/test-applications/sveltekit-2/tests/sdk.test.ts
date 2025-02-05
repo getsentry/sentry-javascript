c50a57b6a811b2c35d914637b8c69a2c16826b28
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-05 16:42:09 +0100

    revert for loop

--- dev-packages/e2e-tests/test-applications/sveltekit-2/tests/sdk.test.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-2/tests/sdk.test.ts
@@ -2,17 +2,15 @@ import { expect, test } from '@playwright/test';
 import { waitForInitialPageload } from './utils';
 
 test.describe('SDK-internal behavior', () => {
-  for (let i = 0; i < 100; i++) {
-    test(`Doesn't inject fetch proxy script for SvelteKit>=2.16.0 ${i}`, async ({ page }) => {
-      await waitForInitialPageload(page, { route: '/' });
+  test("Doesn't inject fetch proxy script for SvelteKit>=2.16.0", async ({ page }) => {
+    await waitForInitialPageload(page, { route: '/' });
 
-      // @ts-expect-error this is defined
-      await page.waitForFunction(() => typeof window.__SENTRY__ === 'object');
+    // @ts-expect-error this is defined
+    await page.waitForFunction(() => typeof window.__SENTRY__ === 'object');
 
-      const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
+    const proxyHandle = await page.evaluate('typeof window._sentryFetchProxy');
 
-      // fetch proxy script didn't run
-      expect(proxyHandle).toBe('undefined');
-    });
-  }
+    // fetch proxy script didn't run
+    expect(proxyHandle).toBe('undefined');
+  });
 });
