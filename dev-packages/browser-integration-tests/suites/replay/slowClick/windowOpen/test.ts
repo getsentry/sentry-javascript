53194d7f50aecd2bffad0d1ec23a0855f148db4a
Francesco Gringl-Novy <francesco.novy@sentry.io>
2025-02-07 12:59:57 +0100

    test(browser): Avoid using `example.com` for browser-integration-tests (#15332)
    Mostly we already add a route for this anyhow, but it can be confusing.
    So let's just use a proper non-existing URL there to ensure we notice
    issues earlier.

--- dev-packages/browser-integration-tests/suites/replay/slowClick/windowOpen/test.ts
+++ dev-packages/browser-integration-tests/suites/replay/slowClick/windowOpen/test.ts
@@ -8,14 +8,6 @@ sentryTest('window.open() is considered for slow click', async ({ getLocalTestUr
     sentryTest.skip();
   }
 
-  await page.route('http://example.com/', route => {
-    return route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({}),
-    });
-  });
-
   const url = await getLocalTestUrl({ testDir: __dirname });
 
   await Promise.all([waitForReplayRequest(page, 0), page.goto(url)]);
@@ -63,5 +55,5 @@ sentryTest('window.open() is considered for slow click', async ({ getLocalTestUr
   const pages = context.pages();
 
   expect(pages.length).toBe(2);
-  expect(pages[1].url()).toBe('https://example.com/');
+  expect(pages[1].url()).toBe('https://github.com/');
 });
