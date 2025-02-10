a47484109b81195005d3fbb4b7092c4b2dc64847
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2025-02-10 11:03:38 +0100

    fix(astro): Add vue to `registerEsmLoaderHooks` (#15352)
    Makes sure that `vue` is not wrapped and users don't get the error
    `$haml has already been declared`
    
    closes https://github.com/getsentry/sentry-javascript/issues/13982

--- packages/astro/test/server/sdk.test.ts
+++ packages/astro/test/server/sdk.test.ts
@@ -1,8 +1,8 @@
 import * as SentryNode from '@sentry/node';
-import { SDK_VERSION } from '@sentry/node';
-import { vi } from 'vitest';
+import { type NodeOptions, SDK_VERSION } from '@sentry/node';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 
-import { init } from '../../src/server/sdk';
+import { init, mergeRegisterEsmLoaderHooks } from '../../src/server/sdk';
 
 const nodeInit = vi.spyOn(SentryNode, 'init');
 
@@ -41,6 +41,44 @@ describe('Sentry server SDK', () => {
 
     it('returns client from init', () => {
       expect(init({})).not.toBeUndefined();
+    });
+  });
+
+  describe('mergeRegisterEsmLoaderHooks', () => {
+    it('merges exclude array when registerEsmLoaderHooks is an object with an exclude array', () => {
+      const options: NodeOptions = {
+        registerEsmLoaderHooks: { exclude: [/test/] },
+      };
+      const result = mergeRegisterEsmLoaderHooks(options);
+      expect(result).toEqual({ exclude: [/test/, /vue/] });
+    });
+
+    it('sets exclude array when registerEsmLoaderHooks is an object without an exclude array', () => {
+      const options: NodeOptions = {
+        registerEsmLoaderHooks: {},
+      };
+      const result = mergeRegisterEsmLoaderHooks(options);
+      expect(result).toEqual({ exclude: [/vue/] });
+    });
+
+    it('returns boolean when registerEsmLoaderHooks is a boolean', () => {
+      const options1: NodeOptions = {
+        registerEsmLoaderHooks: true,
+      };
+      const result1 = mergeRegisterEsmLoaderHooks(options1);
+      expect(result1).toBe(true);
+
+      const options2: NodeOptions = {
+        registerEsmLoaderHooks: false,
+      };
+      const result2 = mergeRegisterEsmLoaderHooks(options2);
+      expect(result2).toBe(false);
+    });
+
+    it('sets exclude array when registerEsmLoaderHooks is undefined', () => {
+      const options: NodeOptions = {};
+      const result = mergeRegisterEsmLoaderHooks(options);
+      expect(result).toEqual({ exclude: [/vue/] });
     });
   });
 });
