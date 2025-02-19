79af7302a818bec77721f919f17f3989475cd924
s1gr1d <sigrid.huemer@posteo.at>
2025-02-19 11:00:35 +0100

    feat(nuxt): Add `enableNitroErrorHandler` to server options

--- packages/nuxt/src/common/types.ts
+++ packages/nuxt/src/common/types.ts
@@ -6,13 +6,14 @@ import type { init as initVue } from '@sentry/vue';
 // Omitting Vue 'app' as the Nuxt SDK will add the app instance in the client plugin (users do not have to provide this)
 // Adding `& object` helps TS with inferring that this is not `undefined` but an object type
 export type SentryNuxtClientOptions = Omit<Parameters<typeof initVue>[0] & object, 'app'>;
-export type SentryNuxtServerOptions = Parameters<typeof initNode>[0] & object & {
+export type SentryNuxtServerOptions = Parameters<typeof initNode>[0] & {
   /**
    * Enables the Sentry error handler for the Nitro error hook.
    *
    * When enabled, exceptions are automatically sent to Sentry with additional data such as the transaction name and Nitro error context.
+   * It's recommended to keep this enabled unless you need to implement a custom error handler.
    *
-   * To add a custom error handler, disable this option and implement your handler based on the default one:
+   * If you need a custom implementation, disable this option and refer to the default handler as a reference:
    * https://github.com/getsentry/sentry-javascript/blob/da8ba8d77a28b43da5014acc8dd98906d2180cc1/packages/nuxt/src/runtime/plugins/sentry.server.ts#L20-L46
    *
    * @default true
