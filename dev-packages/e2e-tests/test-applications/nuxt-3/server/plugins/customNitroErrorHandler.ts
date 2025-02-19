d42d04f6054fbf32d6b51ceff2cbb78b52f2ed3a
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2025-02-19 13:28:34 +0100

    feat(nuxt): Add `enableNitroErrorHandler` to server options (#15444)
    closes https://github.com/getsentry/sentry-javascript/issues/15409

--- dev-packages/e2e-tests/test-applications/nuxt-3/server/plugins/customNitroErrorHandler.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-3/server/plugins/customNitroErrorHandler.ts
@@ -1,33 +1,12 @@
-import {
-  GLOBAL_OBJ,
-  flush,
-  getDefaultIsolationScope,
-  getIsolationScope,
-  logger,
-  vercelWaitUntil,
-  withIsolationScope,
-} from '@sentry/core';
+import { Context, GLOBAL_OBJ, dropUndefinedKeys, flush, logger, vercelWaitUntil } from '@sentry/core';
 import * as SentryNode from '@sentry/node';
-import { type EventHandler, H3Error } from 'h3';
-import { defineNitroPlugin } from 'nitropack/runtime';
-import type { NuxtRenderHTMLContext } from 'nuxt/app';
-import { addSentryTracingMetaTags, extractErrorContext } from '../utils';
+import { H3Error } from 'h3';
+import type { CapturedErrorContext } from 'nitropack';
+import { defineNitroPlugin } from '#imports';
 
+// Copy from SDK-internal error handler (nuxt/src/runtime/plugins/sentry.server.ts)
 export default defineNitroPlugin(nitroApp => {
-  nitroApp.h3App.handler = patchEventHandler(nitroApp.h3App.handler);
-
   nitroApp.hooks.hook('error', async (error, errorContext) => {
-    const sentryClient = SentryNode.getClient();
-    const sentryClientOptions = sentryClient?.getOptions();
-
-    if (
-      sentryClientOptions &&
-      'enableNitroErrorHandler' in sentryClientOptions &&
-      sentryClientOptions.enableNitroErrorHandler === false
-    ) {
-      return;
-    }
-
     // Do not handle 404 and 422
     if (error instanceof H3Error) {
       // Do not report if status code is 3xx or 4xx
@@ -54,12 +33,28 @@ export default defineNitroPlugin(nitroApp => {
 
     await flushIfServerless();
   });
-
-  // @ts-expect-error - 'render:html' is a valid hook name in the Nuxt context
-  nitroApp.hooks.hook('render:html', (html: NuxtRenderHTMLContext) => {
-    addSentryTracingMetaTags(html.head);
-  });
 });
+
+function extractErrorContext(errorContext: CapturedErrorContext): Context {
+  const structuredContext: Context = {
+    method: undefined,
+    path: undefined,
+    tags: undefined,
+  };
+
+  if (errorContext) {
+    if (errorContext.event) {
+      structuredContext.method = errorContext.event._method || undefined;
+      structuredContext.path = errorContext.event._path || undefined;
+    }
+
+    if (Array.isArray(errorContext.tags)) {
+      structuredContext.tags = errorContext.tags || undefined;
+    }
+  }
+
+  return dropUndefinedKeys(structuredContext);
+}
 
 async function flushIfServerless(): Promise<void> {
   const isServerless =
@@ -87,27 +82,4 @@ async function flushWithTimeout(): Promise<void> {
   } catch (e) {
     isDebug && logger.log('Error while flushing events:\n', e);
   }
-}
-
-function patchEventHandler(handler: EventHandler): EventHandler {
-  return new Proxy(handler, {
-    async apply(handlerTarget, handlerThisArg, handlerArgs: Parameters<EventHandler>) {
-      const isolationScope = getIsolationScope();
-      const newIsolationScope = isolationScope === getDefaultIsolationScope() ? isolationScope.clone() : isolationScope;
-
-      logger.log(
-        `Patched h3 event handler. ${
-          isolationScope === newIsolationScope ? 'Using existing' : 'Created new'
-        } isolation scope.`,
-      );
-
-      return withIsolationScope(newIsolationScope, async () => {
-        try {
-          return await handlerTarget.apply(handlerThisArg, handlerArgs);
-        } finally {
-          await flushIfServerless();
-        }
-      });
-    },
-  });
 }
