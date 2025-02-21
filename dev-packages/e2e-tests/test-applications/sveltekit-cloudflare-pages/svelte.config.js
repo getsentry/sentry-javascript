67b94ef43ceceee6d767de4783066a5234ee774e
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-21 09:46:01 +0100

    handle http prerender error

--- dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/svelte.config.js
+++ dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/svelte.config.js
@@ -12,6 +12,11 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
+    prerender: {
+      handleHttpError: err => {
+        console.log(JSON.stringify(err, null, 2));
+      },
+    },
   },
 };
 
