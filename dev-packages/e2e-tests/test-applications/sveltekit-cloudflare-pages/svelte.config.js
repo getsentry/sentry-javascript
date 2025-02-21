b9371895e1f508192c0c49f494af355cd50a5299
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-21 10:27:48 +0100

    ignore prerender error?

--- dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/svelte.config.js
+++ dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/svelte.config.js
@@ -13,9 +13,7 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     prerender: {
-      handleHttpError: err => {
-        console.log(JSON.stringify(err, null, 2));
-      },
+      handleHttpError: 'ignore',
     },
   },
 };
