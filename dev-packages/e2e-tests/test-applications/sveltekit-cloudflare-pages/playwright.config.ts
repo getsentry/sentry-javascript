2fd7bcc2e4c85412b6e0f06e93a3ca0bb75c61bf
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-21 09:29:46 +0100

    replace more npm with pnpm

--- dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/playwright.config.ts
+++ dev-packages/e2e-tests/test-applications/sveltekit-cloudflare-pages/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'pnpm run build && pnpm run preview',
     port: 4173,
   },
 
