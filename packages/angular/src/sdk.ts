377efc9db30af76a7fb1a3bd4895e659c0b16255
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-26 17:46:49 +0100

    lint

--- packages/angular/src/sdk.ts
+++ packages/angular/src/sdk.ts
@@ -34,6 +34,7 @@ export function getDefaultIntegrations(_options: BrowserOptions = {}): Integrati
   //  - https://github.com/getsentry/sentry-javascript/issues/2744
   return [
     // TODO(v10): Replace with `eventFiltersIntegration` once we remove the deprecated `inboundFiltersIntegration`
+    // eslint-disable-next-line deprecation/deprecation
     inboundFiltersIntegration(),
     functionToStringIntegration(),
     breadcrumbsIntegration(),
