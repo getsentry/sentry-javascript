0151813993d86a0b22d1ecdac983169e4f4e9431
Abhijeet Prasad <aprasad@sentry.io>
2025-02-27 15:17:59 -0500

    feat(core): Add types for logs protocol and envelope

--- packages/core/src/types-hoist/index.ts
+++ packages/core/src/types-hoist/index.ts
@@ -110,6 +110,7 @@ export type {
   TraceFlag,
 } from './span';
 export type { SpanStatus } from './spanStatus';
+export type { Log, LogAttribute, LogSeverityLevel, LogAttributeValueType } from './log';
 export type { TimedEvent } from './timedEvent';
 export type { StackFrame } from './stackframe';
 export type { Stacktrace, StackParser, StackLineParser, StackLineParserFn } from './stacktrace';
