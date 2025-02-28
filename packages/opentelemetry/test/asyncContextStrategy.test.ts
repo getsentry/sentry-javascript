983e1c0d58e30c0f625f62c8cd43e71ea6852657
Tim Fish <tim@timfish.uk>
2025-02-28 17:35:09 +0100

    test(otel): Migrate to Vitest

--- packages/opentelemetry/test/asyncContextStrategy.test.ts
+++ packages/opentelemetry/test/asyncContextStrategy.test.ts
@@ -7,6 +7,7 @@ import {
   withIsolationScope,
   withScope,
 } from '@sentry/core';
+import { describe, beforeEach, afterEach, afterAll, test, expect, it } from 'vitest';
 
 import type { Scope } from '@sentry/core';
 import { setOpenTelemetryContextAsyncContextStrategy } from '../src/asyncContextStrategy';
@@ -301,130 +302,142 @@ describe('asyncContextStrategy', () => {
   });
 
   describe('withScope()', () => {
-    it('will make the passed scope the active scope within the callback', done => {
-      withScope(scope => {
-        expect(getCurrentScope()).toBe(scope);
-        done();
-      });
-    });
-
-    it('will pass a scope that is different from the current active isolation scope', done => {
-      withScope(scope => {
-        expect(getIsolationScope()).not.toBe(scope);
-        done();
-      });
-    });
-
-    it('will always make the inner most passed scope the current scope when nesting calls', done => {
-      withIsolationScope(_scope1 => {
-        withIsolationScope(scope2 => {
-          expect(getIsolationScope()).toBe(scope2);
+    it('will make the passed scope the active scope within the callback', () =>
+      new Promise<void>(done => {
+        withScope(scope => {
+          expect(getCurrentScope()).toBe(scope);
           done();
         });
-      });
-    });
+      }));
 
-    it('forks the scope when not passing any scope', done => {
-      const initialScope = getCurrentScope();
-      initialScope.setTag('aa', 'aa');
+    it('will pass a scope that is different from the current active isolation scope', () =>
+      new Promise<void>(done => {
+        withScope(scope => {
+          expect(getIsolationScope()).not.toBe(scope);
+          done();
+        });
+      }));
 
-      withScope(scope => {
-        expect(getCurrentScope()).toBe(scope);
-        scope.setTag('bb', 'bb');
-        expect(scope).not.toBe(initialScope);
-        expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
-        done();
-      });
-    });
+    it('will always make the inner most passed scope the current scope when nesting calls', () =>
+      new Promise<void>(done => {
+        withIsolationScope(_scope1 => {
+          withIsolationScope(scope2 => {
+            expect(getIsolationScope()).toBe(scope2);
+            done();
+          });
+        });
+      }));
 
-    it('forks the scope when passing undefined', done => {
-      const initialScope = getCurrentScope();
-      initialScope.setTag('aa', 'aa');
+    it('forks the scope when not passing any scope', () =>
+      new Promise<void>(done => {
+        const initialScope = getCurrentScope();
+        initialScope.setTag('aa', 'aa');
 
-      withScope(undefined, scope => {
-        expect(getCurrentScope()).toBe(scope);
-        scope.setTag('bb', 'bb');
-        expect(scope).not.toBe(initialScope);
-        expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
-        done();
-      });
-    });
+        withScope(scope => {
+          expect(getCurrentScope()).toBe(scope);
+          scope.setTag('bb', 'bb');
+          expect(scope).not.toBe(initialScope);
+          expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
+          done();
+        });
+      }));
 
-    it('sets the passed in scope as active scope', done => {
-      const initialScope = getCurrentScope();
-      initialScope.setTag('aa', 'aa');
+    it('forks the scope when passing undefined', () =>
+      new Promise<void>(done => {
+        const initialScope = getCurrentScope();
+        initialScope.setTag('aa', 'aa');
 
-      const customScope = new ScopeClass();
+        withScope(undefined, scope => {
+          expect(getCurrentScope()).toBe(scope);
+          scope.setTag('bb', 'bb');
+          expect(scope).not.toBe(initialScope);
+          expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
+          done();
+        });
+      }));
 
-      withScope(customScope, scope => {
-        expect(getCurrentScope()).toBe(customScope);
-        expect(scope).toBe(customScope);
-        done();
-      });
-    });
+    it('sets the passed in scope as active scope', () =>
+      new Promise<void>(done => {
+        const initialScope = getCurrentScope();
+        initialScope.setTag('aa', 'aa');
+
+        const customScope = new ScopeClass();
+
+        withScope(customScope, scope => {
+          expect(getCurrentScope()).toBe(customScope);
+          expect(scope).toBe(customScope);
+          done();
+        });
+      }));
   });
 
   describe('withIsolationScope()', () => {
-    it('will make the passed isolation scope the active isolation scope within the callback', done => {
-      withIsolationScope(scope => {
-        expect(getIsolationScope()).toBe(scope);
-        done();
-      });
-    });
-
-    it('will pass an isolation scope that is different from the current active scope', done => {
-      withIsolationScope(scope => {
-        expect(getCurrentScope()).not.toBe(scope);
-        done();
-      });
-    });
-
-    it('will always make the inner most passed scope the current scope when nesting calls', done => {
-      withIsolationScope(_scope1 => {
-        withIsolationScope(scope2 => {
-          expect(getIsolationScope()).toBe(scope2);
+    it('will make the passed isolation scope the active isolation scope within the callback', () =>
+      new Promise<void>(done => {
+        withIsolationScope(scope => {
+          expect(getIsolationScope()).toBe(scope);
           done();
         });
-      });
-    });
+      }));
 
-    it('forks the isolation scope when not passing any isolation scope', done => {
-      const initialScope = getIsolationScope();
-      initialScope.setTag('aa', 'aa');
+    it('will pass an isolation scope that is different from the current active scope', () =>
+      new Promise<void>(done => {
+        withIsolationScope(scope => {
+          expect(getCurrentScope()).not.toBe(scope);
+          done();
+        });
+      }));
 
-      withIsolationScope(scope => {
-        expect(getIsolationScope()).toBe(scope);
-        scope.setTag('bb', 'bb');
-        expect(scope).not.toBe(initialScope);
-        expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
-        done();
-      });
-    });
+    it('will always make the inner most passed scope the current scope when nesting calls', () =>
+      new Promise<void>(done => {
+        withIsolationScope(_scope1 => {
+          withIsolationScope(scope2 => {
+            expect(getIsolationScope()).toBe(scope2);
+            done();
+          });
+        });
+      }));
 
-    it('forks the isolation scope when passing undefined', done => {
-      const initialScope = getIsolationScope();
-      initialScope.setTag('aa', 'aa');
+    it('forks the isolation scope when not passing any isolation scope', () =>
+      new Promise<void>(done => {
+        const initialScope = getIsolationScope();
+        initialScope.setTag('aa', 'aa');
 
-      withIsolationScope(undefined, scope => {
-        expect(getIsolationScope()).toBe(scope);
-        scope.setTag('bb', 'bb');
-        expect(scope).not.toBe(initialScope);
-        expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
-        done();
-      });
-    });
+        withIsolationScope(scope => {
+          expect(getIsolationScope()).toBe(scope);
+          scope.setTag('bb', 'bb');
+          expect(scope).not.toBe(initialScope);
+          expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
+          done();
+        });
+      }));
 
-    it('sets the passed in isolation scope as active isolation scope', done => {
-      const initialScope = getIsolationScope();
-      initialScope.setTag('aa', 'aa');
+    it('forks the isolation scope when passing undefined', () =>
+      new Promise<void>(done => {
+        const initialScope = getIsolationScope();
+        initialScope.setTag('aa', 'aa');
 
-      const customScope = new ScopeClass();
+        withIsolationScope(undefined, scope => {
+          expect(getIsolationScope()).toBe(scope);
+          scope.setTag('bb', 'bb');
+          expect(scope).not.toBe(initialScope);
+          expect(scope.getScopeData().tags).toEqual({ aa: 'aa', bb: 'bb' });
+          done();
+        });
+      }));
 
-      withIsolationScope(customScope, scope => {
-        expect(getIsolationScope()).toBe(customScope);
-        expect(scope).toBe(customScope);
-        done();
-      });
-    });
+    it('sets the passed in isolation scope as active isolation scope', () =>
+      new Promise<void>(done => {
+        const initialScope = getIsolationScope();
+        initialScope.setTag('aa', 'aa');
+
+        const customScope = new ScopeClass();
+
+        withIsolationScope(customScope, scope => {
+          expect(getIsolationScope()).toBe(customScope);
+          expect(scope).toBe(customScope);
+          done();
+        });
+      }));
   });
 });
