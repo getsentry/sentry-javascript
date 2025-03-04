d71e6adbd85c0426fee1392966b48cd607dc87dd
Lukas Stracke <lukas.stracke@sentry.io>
2025-03-04 17:55:33 +0100

    fix(sveltekit): Correctly parse angle bracket type assertions for auto instrumentation

---
+++ dev-packages/e2e-tests/test-applications/sveltekit-2/src/routes/type-assertion/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let data
+</script>
+
+<h1>Type Assertion</h1>
+
+<p>
+  TIL:
+<code>const myCanvas = <HTMLCanvasElement>document.getElementById("main_canvas");</code>
+  is equal to
+<code>const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;</code>
+</p>
+
+<p>
+  Message: {data.msg}
+</p>
