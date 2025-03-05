911e29865ebe582ed3fde7b082ba9e7ca7180640
s1gr1d <sigrid.huemer@posteo.at>
2025-03-05 16:29:21 +0100

    feat(nuxt): Add Cloudflare Nitro plugin

--- packages/nuxt/src/runtime/plugins/sentry.server.ts
+++ packages/nuxt/src/runtime/plugins/sentry.server.ts
@@ -1,95 +1,22 @@
-import {
-  flush,
-  getDefaultIsolationScope,
-  getIsolationScope,
-  GLOBAL_OBJ,
-  logger,
-  vercelWaitUntil,
-  withIsolationScope,
-} from '@sentry/core';
-import * as SentryNode from '@sentry/node';
+import { getDefaultIsolationScope, getIsolationScope, logger, withIsolationScope } from '@sentry/core';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { type EventHandler, H3Error } from 'h3';
+import { type EventHandler } from 'h3';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineNitroPlugin } from 'nitropack/runtime';
 import type { NuxtRenderHTMLContext } from 'nuxt/app';
-import { addSentryTracingMetaTags, extractErrorContext } from '../utils';
+import { addSentryTracingMetaTags, flushIfServerless } from '../utils';
+import { sentryCaptureErrorHook } from '../hooks/captureErrorHook';
 
 export default defineNitroPlugin(nitroApp => {
   nitroApp.h3App.handler = patchEventHandler(nitroApp.h3App.handler);
 
-  nitroApp.hooks.hook('error', async (error, errorContext) => {
-    const sentryClient = SentryNode.getClient();
-    const sentryClientOptions = sentryClient?.getOptions();
-
-    if (
-      sentryClientOptions &&
-      'enableNitroErrorHandler' in sentryClientOptions &&
-      sentryClientOptions.enableNitroErrorHandler === false
-    ) {
-      return;
-    }
-
-    // Do not handle 404 and 422
-    if (error instanceof H3Error) {
-      // Do not report if status code is 3xx or 4xx
-      if (error.statusCode >= 300 && error.statusCode < 500) {
-        return;
-      }
-    }
-
-    const { method, path } = {
-      method: errorContext.event?._method ? errorContext.event._method : '',
-      path: errorContext.event?._path ? errorContext.event._path : null,
-    };
-
-    if (path) {
-      SentryNode.getCurrentScope().setTransactionName(`${method} ${path}`);
-    }
-
-    const structuredContext = extractErrorContext(errorContext);
-
-    SentryNode.captureException(error, {
-      captureContext: { contexts: { nuxt: structuredContext } },
-      mechanism: { handled: false },
-    });
-
-    await flushIfServerless();
-  });
+  nitroApp.hooks.hook('error', sentryCaptureErrorHook);
 
   // @ts-expect-error - 'render:html' is a valid hook name in the Nuxt context
   nitroApp.hooks.hook('render:html', (html: NuxtRenderHTMLContext) => {
     addSentryTracingMetaTags(html.head);
   });
 });
-
-async function flushIfServerless(): Promise<void> {
-  const isServerless =
-    !!process.env.FUNCTIONS_WORKER_RUNTIME || // Azure Functions
-    !!process.env.LAMBDA_TASK_ROOT || // AWS Lambda
-    !!process.env.VERCEL ||
-    !!process.env.NETLIFY;
-
-  // @ts-expect-error This is not typed
-  if (GLOBAL_OBJ[Symbol.for('@vercel/request-context')]) {
-    vercelWaitUntil(flushWithTimeout());
-  } else if (isServerless) {
-    await flushWithTimeout();
-  }
-}
-
-async function flushWithTimeout(): Promise<void> {
-  const sentryClient = SentryNode.getClient();
-  const isDebug = sentryClient ? sentryClient.getOptions().debug : false;
-
-  try {
-    isDebug && logger.log('Flushing events...');
-    await flush(2000);
-    isDebug && logger.log('Done flushing events');
-  } catch (e) {
-    isDebug && logger.log('Error while flushing events:\n', e);
-  }
-}
 
 function patchEventHandler(handler: EventHandler): EventHandler {
   return new Proxy(handler, {
