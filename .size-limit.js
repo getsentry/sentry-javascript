7d22aa284bf93554c3396d0b98f87761547bb26d
Abhijeet Prasad <aprasad@sentry.io>
2025-03-06 12:23:01 -0500

    chore: bump size-limit

--- .size-limit.js
+++ .size-limit.js
@@ -107,7 +107,7 @@ module.exports = [
     path: 'packages/browser/build/npm/esm/index.js',
     import: createImport('init', 'feedbackAsyncIntegration'),
     gzip: true,
-    limit: '34 KB',
+    limit: '34.5 KB',
   },
   // React SDK (ESM)
   {
