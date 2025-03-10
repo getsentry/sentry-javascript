62f3b09068b218652a5a7dd12aac2a2182754d64
Lukas Stracke <lukas.stracke@sentry.io>
2025-03-10 12:29:40 +0100

    fix e2e test

--- dev-packages/e2e-tests/test-applications/sveltekit-2/src/routes/type-assertion/+page.svelte
+++ dev-packages/e2e-tests/test-applications/sveltekit-2/src/routes/type-assertion/+page.svelte
@@ -5,10 +5,8 @@
 <h1>Type Assertion</h1>
 
 <p>
-  TIL:
-<code>const myCanvas = <HTMLCanvasElement>document.getElementById("main_canvas");</code>
-  is equal to
-<code>const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;</code>
+  This route only exists to ensure we don't emit a build error because of the angle bracket type assertion in +page.ts
+  see https://github.com/getsentry/sentry-javascript/issues/9318
 </p>
 
 <p>
