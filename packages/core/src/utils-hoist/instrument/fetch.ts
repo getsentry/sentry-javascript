46e4deddac1cf2f12fef75df170107ff9961494b
Francesco Novy <francesco.novy@sentry.io>
2025-03-19 14:48:13 +0100

    take top level domain only

--- packages/core/src/utils-hoist/instrument/fetch.ts
+++ packages/core/src/utils-hoist/instrument/fetch.ts
@@ -120,7 +120,10 @@ function instrumentFetch(onFetchResolved?: (response: Response) => void, skipNat
           ) {
             try {
               const url = new URL(handlerData.fetchData.url);
-              error.message = `${error.message} (${url.host})`;
+              // We only want to take the top-level domain, e.g. xxx.sentry.io should become sentry.io
+              // We do this to avoid noise when there may be dynamic subdomains
+              const host = url.host.split('.').slice(-2).join('.');
+              error.message = `${error.message} (${host})`;
             } catch {
               // ignore it if errors happen here
             }
