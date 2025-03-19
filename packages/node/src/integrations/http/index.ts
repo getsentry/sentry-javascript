ce13e76c673af44a43b02fbb8f4b8da57e475246
Francesco Novy <francesco.novy@sentry.io>
2025-03-19 13:05:24 +0100

    fix(node): Ensure httpIntegration propagates traces
    This is another stab at https://github.com/getsentry/sentry-javascript/pull/15233, which was reverted here: https://github.com/getsentry/sentry-javascript/pull/15354, with a different implementation.  It is required to make https://github.com/getsentry/sentry-javascript/pull/15730 work.

--- packages/node/src/integrations/http/index.ts
+++ packages/node/src/integrations/http/index.ts
@@ -160,6 +160,9 @@ export const httpIntegration = defineIntegration((options: HttpOptions = {}) => 
         // If spans are not instrumented, it means the HttpInstrumentation has not been added
         // In that case, we want to handle incoming trace extraction ourselves
         extractIncomingTraceFromHeader: !instrumentSpans,
+        // If spans are not instrumented, it means the HttpInstrumentation has not been added
+        // In that case, we want to handle trace propagation ourselves
+        propagateTraceInOutgoingRequests: !instrumentSpans,
       });
     },
   };
