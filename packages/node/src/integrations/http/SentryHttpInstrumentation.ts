2f16b2d4fdfc6d0976e1215610793cd7e21187a6
Luca Forstner <luca.forstner@sentry.io>
2025-03-19 09:39:28 +0100

    fix(node): Always flush on Vercel before Lambda freeze (#15602)

--- packages/node/src/integrations/http/SentryHttpInstrumentation.ts
+++ packages/node/src/integrations/http/SentryHttpInstrumentation.ts
@@ -24,6 +24,7 @@ import {
 import { DEBUG_BUILD } from '../../debug-build';
 import { getRequestUrl } from '../../utils/getRequestUrl';
 import { getRequestInfo } from './vendor/getRequestInfo';
+import { stealthWrap } from './utils';
 
 type Http = typeof http;
 type Https = typeof https;
@@ -266,46 +267,6 @@ export class SentryHttpInstrumentation extends InstrumentationBase<SentryHttpIns
       };
     };
   }
-}
-
-/**
- * This is a minimal version of `wrap` from shimmer:
- * https://github.com/othiym23/shimmer/blob/master/index.js
- *
- * In contrast to the original implementation, this version does not allow to unwrap,
- * and does not make it clear that the method is wrapped.
- * This is necessary because we want to wrap the http module with our own code,
- * while still allowing to use the HttpInstrumentation from OTEL.
- *
- * Without this, if we'd just use `wrap` from shimmer, the OTEL instrumentation would remove our wrapping,
- * because it only allows any module to be wrapped a single time.
- */
-function stealthWrap<Nodule extends object, FieldName extends keyof Nodule>(
-  nodule: Nodule,
-  name: FieldName,
-  wrapper: (original: Nodule[FieldName]) => Nodule[FieldName],
-): Nodule[FieldName] {
-  const original = nodule[name];
-  const wrapped = wrapper(original);
-
-  defineProperty(nodule, name, wrapped);
-  return wrapped;
-}
-
-// Sets a property on an object, preserving its enumerability.
-function defineProperty<Nodule extends object, FieldName extends keyof Nodule>(
-  obj: Nodule,
-  name: FieldName,
-  value: Nodule[FieldName],
-): void {
-  const enumerable = !!obj[name] && Object.prototype.propertyIsEnumerable.call(obj, name);
-
-  Object.defineProperty(obj, name, {
-    configurable: true,
-    enumerable: enumerable,
-    writable: true,
-    value: value,
-  });
 }
 
 /** Add a breadcrumb for outgoing requests. */
