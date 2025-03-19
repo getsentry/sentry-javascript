70d4a20bf23a330a66095ffa53ea3f2830779524
Luca Forstner <luca.forstner@sentry.io>
2025-03-19 15:40:01 +0100

    test

--- dev-packages/e2e-tests/test-applications/nextjs-turbo/tests/app-router/pageload-transaction.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-turbo/tests/app-router/pageload-transaction.test.ts
@@ -1,9 +1,20 @@
 import { expect, test } from '@playwright/test';
 import { waitForTransaction } from '@sentry-internal/test-utils';
+import { parseSemver } from '@sentry/core';
+
+const packageJson = require('../../package.json');
+const nextjsVersion = packageJson.dependencies.next;
+const { major, minor } = parseSemver(nextjsVersion);
 
 test('Should record pageload transactions (this test verifies that the client SDK is initialized)', async ({
   page,
 }) => {
+  // TODO: Remove this skippage when Next.js 15.3.0 is released and bump version in package json to 15.3.0
+  test.skip(
+    major === 15 && minor !== undefined && minor < 3,
+    'Next.js version does not support clientside instrumentation',
+  );
+
   const pageloadTransactionPromise = waitForTransaction('nextjs-turbo', async transactionEvent => {
     return transactionEvent?.transaction === '/pageload-transaction';
   });
