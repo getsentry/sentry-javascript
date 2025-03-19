9024c1a6e9662742d29d20a906615510e71df0f4
Francesco Novy <francesco.novy@sentry.io>
2025-03-19 14:30:25 +0100

    fix the test

--- dev-packages/node-integration-tests/suites/tracing/requests/http-no-tracing-no-spans/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/requests/http-no-tracing-no-spans/test.ts
@@ -1,10 +1,11 @@
+import { expect, test } from 'vitest';
 import { createRunner } from '../../../../utils/runner';
 import { createTestServer } from '../../../../utils/server';
 
-test('outgoing http requests are correctly instrumented with tracing & spans disabled', done => {
+test('outgoing http requests are correctly instrumented with tracing & spans disabled', async () => {
   expect.assertions(11);
 
-  createTestServer(done)
+  const [SERVER_URL, closeTestServer] = await createTestServer()
     .get('/api/v0', headers => {
       expect(headers['sentry-trace']).toEqual(expect.stringMatching(/^([a-f0-9]{32})-([a-f0-9]{16})$/));
       expect(headers['sentry-trace']).not.toEqual('00000000000000000000000000000000-0000000000000000');
@@ -23,73 +24,75 @@ test('outgoing http requests are correctly instrumented with tracing & spans dis
       expect(headers['baggage']).toBeUndefined();
       expect(headers['sentry-trace']).toBeUndefined();
     })
-    .start()
-    .then(([SERVER_URL, closeTestServer]) => {
-      createRunner(__dirname, 'scenario.ts')
-        .withEnv({ SERVER_URL })
-        .ensureNoErrorOutput()
-        .expect({
-          event: {
-            exception: {
-              values: [
-                {
-                  type: 'Error',
-                  value: 'foo',
-                },
-              ],
+    .start();
+
+  await createRunner(__dirname, 'scenario.ts')
+    .withEnv({ SERVER_URL })
+    .ensureNoErrorOutput()
+    .expect({
+      event: {
+        exception: {
+          values: [
+            {
+              type: 'Error',
+              value: 'foo',
             },
-            breadcrumbs: [
-              {
-                message: 'manual breadcrumb',
-                timestamp: expect.any(Number),
-              },
-              {
-                category: 'http',
-                data: {
-                  'http.method': 'GET',
-                  url: `${SERVER_URL}/api/v0`,
-                  status_code: 200,
-                  ADDED_PATH: '/api/v0',
-                },
-                timestamp: expect.any(Number),
-                type: 'http',
-              },
-              {
-                category: 'http',
-                data: {
-                  'http.method': 'GET',
-                  url: `${SERVER_URL}/api/v1`,
-                  status_code: 200,
-                  ADDED_PATH: '/api/v1',
-                },
-                timestamp: expect.any(Number),
-                type: 'http',
-              },
-              {
-                category: 'http',
-                data: {
-                  'http.method': 'GET',
-                  url: `${SERVER_URL}/api/v2`,
-                  status_code: 200,
-                  ADDED_PATH: '/api/v2',
-                },
-                timestamp: expect.any(Number),
-                type: 'http',
-              },
-              {
-                category: 'http',
-                data: {
-                  'http.method': 'GET',
-                  url: `${SERVER_URL}/api/v3`,
-                  status_code: 200,
-                  ADDED_PATH: '/api/v3',
-                },
-                timestamp: expect.any(Number),
-                type: 'http',
-              },
-            ],
+          ],
+        },
+        breadcrumbs: [
+          {
+            message: 'manual breadcrumb',
+            timestamp: expect.any(Number),
           },
-        })
-        .start(closeTestServer);
-    });
+          {
+            category: 'http',
+            data: {
+              'http.method': 'GET',
+              url: `${SERVER_URL}/api/v0`,
+              status_code: 200,
+              ADDED_PATH: '/api/v0',
+            },
+            timestamp: expect.any(Number),
+            type: 'http',
+          },
+          {
+            category: 'http',
+            data: {
+              'http.method': 'GET',
+              url: `${SERVER_URL}/api/v1`,
+              status_code: 200,
+              ADDED_PATH: '/api/v1',
+            },
+            timestamp: expect.any(Number),
+            type: 'http',
+          },
+          {
+            category: 'http',
+            data: {
+              'http.method': 'GET',
+              url: `${SERVER_URL}/api/v2`,
+              status_code: 200,
+              ADDED_PATH: '/api/v2',
+            },
+            timestamp: expect.any(Number),
+            type: 'http',
+          },
+          {
+            category: 'http',
+            data: {
+              'http.method': 'GET',
+              url: `${SERVER_URL}/api/v3`,
+              status_code: 200,
+              ADDED_PATH: '/api/v3',
+            },
+            timestamp: expect.any(Number),
+            type: 'http',
+          },
+        ],
+      },
+    })
+    .start()
+    .completed();
+
+  closeTestServer();
 });
