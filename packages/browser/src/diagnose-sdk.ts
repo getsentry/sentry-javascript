77b07ca81c524c6be8e39cc0255b483daa18670a
Luca Forstner <luca.forstner@sentry.io>
2025-03-25 11:54:58 +0100

    comment

--- packages/browser/src/diagnose-sdk.ts
+++ packages/browser/src/diagnose-sdk.ts
@@ -5,7 +5,7 @@ import { getCurrentScope } from '@sentry/core';
  *
  * Possible return values wrapped in a Promise:
  * - `"no-client-active"` - There was no active client when the function was called. This possibly means that the SDK was not initialized yet.
- * - `"sentry-unreachable"` - There was no active client when the function was called. This possibly means that the SDK was not initialized yet.
+ * - `"sentry-unreachable"` - The Sentry SaaS servers were not reachable. This likely means that there is an ad blocker active on the page or that there are other connection issues.
  *
  * If the function doesn't detect an issue it resolves to `undefined`.
  */
