f6b4763fd2852317a335c28b3a21df9326792512
Luca Forstner <luca.forstner@sentry.io>
2025-03-27 16:55:39 +0100

    .

--- dev-packages/browser-integration-tests/suites/public-api/captureException/linkedErrors/subject.js
+++ dev-packages/browser-integration-tests/suites/public-api/captureException/linkedErrors/subject.js
@@ -8,6 +8,6 @@ wat.cause = new Error(`This is a very long message that should not be truncated 
 this is a very long message that should not be truncated and hopefully won't be,
 this is a very long message that should not be truncated and hopefully won't be,
 this is a very long message that should not be truncated and hopefully won't be,
-this is a very long message that should not be truncated and hopefully won't be,`);
+this is a very long message that should not be truncated and hopefully won't be`);
 
 Sentry.captureException(wat);
--- dev-packages/browser-integration-tests/suites/public-api/captureException/linkedErrors/test.ts
+++ dev-packages/browser-integration-tests/suites/public-api/captureException/linkedErrors/test.ts
@@ -31,7 +31,7 @@ this is a very long message that should not be truncated and hopefully won't be`
 this is a very long message that should not be truncated and won't be,
 this is a very long message that should not be truncated and won't be,
 this is a very long message that should not be truncated and won't be,
-this is a very long message that should not be truncated and won't be,`,
+this is a very long message that should not be truncated and won't be`,
     mechanism: {
       type: 'generic',
       handled: true,
