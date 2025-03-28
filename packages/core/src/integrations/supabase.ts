6bb5f64eb06f6e36d697dffde9a5dd489c6dfd98
Onur Temizkan <onur@narval.co.uk>
2025-03-28 18:41:54 +0000

    feat(core): Add Supabase Queues support

--- packages/core/src/integrations/supabase.ts
+++ packages/core/src/integrations/supabase.ts
@@ -13,6 +13,14 @@ import { debug } from '../utils/debug-logger';
 import { isPlainObject } from '../utils/is';
 import { addExceptionMechanism } from '../utils/misc';
 
+export interface SupabaseClientConstructor {
+  prototype: {
+    from: (table: string) => PostgRESTQueryBuilder;
+    schema: (schema: string) => { rpc: (...args: unknown[]) => Promise<unknown> };
+  };
+  rpc: (fn: string, params: Record<string, unknown>) => Promise<unknown>;
+}
+
 const AUTH_OPERATIONS_TO_INSTRUMENT = [
   'reauthenticate',
   'signInAnonymously',
@@ -114,12 +122,6 @@ export interface SupabaseBreadcrumb {
   };
 }
 
-export interface SupabaseClientConstructor {
-  prototype: {
-    from: (table: string) => PostgRESTQueryBuilder;
-  };
-}
-
 export interface PostgRESTProtoThenable {
   then: <T>(
     onfulfilled?: ((value: T) => T | PromiseLike<T>) | null,
@@ -213,6 +215,76 @@ export function translateFiltersIntoMethods(key: string, query: string): string 
   }
 
   return `${method}(${key}, ${value.join('.')})`;
+}
+
+function instrumentRpcReturnedFromSchemaCall(SupabaseClient: unknown): void {
+  (SupabaseClient as unknown as SupabaseClientConstructor).prototype.schema = new Proxy(
+    (SupabaseClient as unknown as SupabaseClientConstructor).prototype.schema,
+    {
+      apply(target, thisArg, argumentsList) {
+        const rv = Reflect.apply(target, thisArg, argumentsList);
+
+        return instrumentRpc(rv);
+      },
+    },
+  );
+}
+
+function instrumentRpc(SupabaseClient: unknown): unknown {
+  (SupabaseClient as unknown as SupabaseClientConstructor).rpc = new Proxy(
+    (SupabaseClient as unknown as SupabaseClientConstructor).rpc,
+    {
+      apply(target, thisArg, argumentsList) {
+        const isProducerSpan = argumentsList[0] === 'enqueue';
+        const isConsumerSpan = argumentsList[0] === 'dequeue';
+
+        const maybeQueueParams = argumentsList[1];
+
+        // If the second argument is not an object, it's not a queue operation
+        if (!isPlainObject(maybeQueueParams)) {
+          return Reflect.apply(target, thisArg, argumentsList);
+        }
+
+        const msg = maybeQueueParams?.msg as { title: string };
+
+        const messageId = msg?.title;
+        const queueName = maybeQueueParams?.queue_name as string;
+
+        const op = isProducerSpan ? 'queue.publish' : isConsumerSpan ? 'queue.process' : '';
+
+        // If the operation is not a queue operation, return the original function
+        if (!op) {
+          return Reflect.apply(target, thisArg, argumentsList);
+        }
+
+        return startSpan(
+          {
+            name: 'supabase.db.rpc',
+            attributes: {
+              [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.db.supabase',
+              [SEMANTIC_ATTRIBUTE_SENTRY_OP]: op,
+            },
+          },
+          async span => {
+            return (Reflect.apply(target, thisArg, argumentsList) as Promise<unknown>).then((res: unknown) => {
+              if (messageId) {
+                span.setAttribute('messaging.message.id', messageId);
+              }
+
+              if (queueName) {
+                span.setAttribute('messaging.destination.name', queueName);
+              }
+
+              span.end();
+              return res;
+            });
+          },
+        );
+      },
+    },
+  );
+
+  return SupabaseClient;
 }
 
 function instrumentAuthOperation(operation: AuthOperationFn, isAdmin = false): AuthOperationFn {
@@ -516,6 +588,8 @@ export const instrumentSupabaseClient = (supabaseClient: unknown): void => {
     supabaseClient.constructor === Function ? supabaseClient : supabaseClient.constructor;
 
   instrumentSupabaseClientConstructor(SupabaseClientConstructor);
+  instrumentRpcReturnedFromSchemaCall(SupabaseClientConstructor);
+  instrumentRpc(supabaseClient as SupabaseClientInstance);
   instrumentSupabaseAuthClient(supabaseClient as SupabaseClientInstance);
 };
 
