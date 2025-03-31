4a254a379adb371fd6afc22fa1343184eb3f469e
Birk Skyum <birk.skyum@pm.me>
2025-03-31 17:21:03 +0200

    tanstackstart-solid

---
+++ packages/tanstackstart-solid/src/config/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Wraps a TanStack Start config.
+ */
+export function wrapVinxiConfigWithSentry<C>(
+  config: C,
+  // TODO: Expand this type in the future. Right now it is just so that TS doesn't complain for our users when they copy paste from the docs.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  sentryBuildOptions: {
+    org?: string;
+    project?: string;
+    silent?: boolean;
+    authToken?: boolean;
+  } = {},
+): C {
+  return config;
+}
