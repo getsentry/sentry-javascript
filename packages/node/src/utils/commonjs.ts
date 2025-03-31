01c83742c1bd75e662b288b3a42dd1614f6c0967
s1gr1d <sigrid.huemer@posteo.at>
2025-03-31 10:00:32 +0200

    add additional checks

--- packages/node/src/utils/commonjs.ts
+++ packages/node/src/utils/commonjs.ts
@@ -1,4 +1,8 @@
 /** Detect CommonJS. */
 export function isCjs(): boolean {
-  return typeof module !== 'undefined' && typeof module.exports !== 'undefined';
+  try {
+    return typeof module !== 'undefined' && module && typeof module.exports !== 'undefined';
+  } catch {
+    return false;
+  }
 }
