94588fdcb676227c283e7711f4a5df583562acf5
Luca Forstner <luca.forstner@sentry.io>
2025-04-04 14:56:37 +0200

    test

---
+++ dev-packages/e2e-tests/test-applications/nextjs-15/tests/prefetch-spans.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test';
+import { waitForTransaction } from '@sentry-internal/test-utils';
+
+test('Prefetch client spans should have the right op', async ({ page }) => {
+  const pageloadTransactionPromise = waitForTransaction('nextjs-15', async transactionEvent => {
+    return transactionEvent?.transaction === '/prefetching';
+  });
+
+  await page.goto(`/prefetching`);
+
+  // Make it more likely that nextjs prefetches
+  await page.hover('#prefetch-link');
+
+  expect((await pageloadTransactionPromise).spans).toContainEqual(
+    expect.objectContaining({
+      op: 'http.client.prefetch',
+    }),
+  );
+});
