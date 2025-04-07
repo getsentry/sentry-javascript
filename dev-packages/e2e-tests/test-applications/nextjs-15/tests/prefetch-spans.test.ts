1530703234ed536f6a586562096d0bf59a8b4dec
Luca Forstner <luca.forstner@sentry.io>
2025-04-07 10:47:24 +0200

    .

--- dev-packages/e2e-tests/test-applications/nextjs-15/tests/prefetch-spans.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-15/tests/prefetch-spans.test.ts
@@ -1,7 +1,9 @@
 import { expect, test } from '@playwright/test';
 import { waitForTransaction } from '@sentry-internal/test-utils';
 
-test('Prefetch client spans should have the right op', async ({ page }) => {
+test('Prefetch client spans should have a http.request.prefetch attribute', async ({ page }) => {
+  test.skip(process.env.TEST_ENV === 'development', "Prefetch requests don't have the prefetch header in dev mode");
+
   const pageloadTransactionPromise = waitForTransaction('nextjs-15', async transactionEvent => {
     return transactionEvent?.transaction === '/prefetching';
   });
