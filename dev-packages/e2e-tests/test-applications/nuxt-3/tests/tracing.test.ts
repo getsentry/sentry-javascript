bce0c0177bd629aea5dd76e8a07fabb955f927a1
s1gr1d <sigrid.huemer@posteo.at>
2025-04-14 16:51:25 +0200

    try fix tests 1

--- dev-packages/e2e-tests/test-applications/nuxt-3/tests/tracing.test.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-3/tests/tracing.test.ts
@@ -13,8 +13,10 @@ test.describe('distributed tracing', () => {
       return txnEvent.transaction.includes('GET /test-param/');
     });
 
-    const [_, clientTxnEvent, serverTxnEvent] = await Promise.all([
-      page.goto(`/test-param/${PARAM}`),
+    const res = await page.goto(`/test-param/${PARAM}`);
+    const data = await res.json();
+
+    const [clientTxnEvent, serverTxnEvent] = await Promise.all([
       clientTxnEventPromise,
       serverTxnEventPromise,
       expect(page.getByText(`Param: ${PARAM}`)).toBeVisible(),
@@ -33,7 +35,7 @@ test.describe('distributed tracing', () => {
     });
 
     expect(serverTxnEvent).toMatchObject({
-      transaction: 'GET /test-param/s0me-param', // todo: parametrize (nitro)
+      transaction: `GET /test-param/${PARAM}`, // todo: parametrize (nitro)
       transaction_info: { source: 'url' },
       type: 'transaction',
       contexts: {
@@ -44,19 +46,21 @@ test.describe('distributed tracing', () => {
       },
     });
 
+    const baggage = (data.headers.baggage || null).split(',');
+
     // connected trace
     expect(clientTxnEvent.contexts?.trace?.trace_id).not.toBeUndefined();
     expect(clientTxnEvent.contexts?.trace?.parent_span_id).not.toBeUndefined();
-    expect(clientTxnEvent.contexts?.trace?.baggage).not.toBeUndefined();
+    expect(baggage).not.toBeNull();
 
     expect(clientTxnEvent.contexts?.trace?.trace_id).toBe(serverTxnEvent.contexts?.trace?.trace_id);
     expect(clientTxnEvent.contexts?.trace?.parent_span_id).toBe(serverTxnEvent.contexts?.trace?.span_id);
-    expect((clientTxnEvent.contexts?.trace?.baggage as string | undefined)?.split(',').sort()).toEqual(
+    expect(baggage).toEqual(
       expect.arrayContaining([
         'sentry-sample-rate=1.0',
         'sentry-sampled=true',
         `sentry-trace_id=${serverTxnEvent.contexts?.trace?.trace_id}`,
-        'sentry-transaction=GET%20%2Ftest-param%2Fs0me-param',
+        `sentry-transaction=GET%20%2Ftest-param%2F${PARAM}`,
       ]),
     );
   });
