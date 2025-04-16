f08ea7f3f94be40c79762a55eaa30a0cef628068
s1gr1d <sigrid.huemer@posteo.at>
2025-04-16 12:32:21 +0200

    fix sample rate string

--- dev-packages/e2e-tests/test-applications/nuxt-3/tests/tracing.test.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-3/tests/tracing.test.ts
@@ -25,7 +25,7 @@ test.describe('distributed tracing', () => {
     expect(baggageMetaTagContent).toContain(`sentry-trace_id=${serverTxnEvent.contexts?.trace?.trace_id}`);
     expect(baggageMetaTagContent).toContain(`sentry-transaction=GET%20%2Ftest-param%2F${PARAM}`); // URL-encoded for 'GET /test-param/s0me-param'
     expect(baggageMetaTagContent).toContain('sentry-sampled=true');
-    expect(baggageMetaTagContent).toContain('sentry-sample-rate=1.0');
+    expect(baggageMetaTagContent).toContain('sentry-sample_rate=1.0');
 
     const sentryTraceMetaTagContent = await page.locator('meta[name="sentry-trace"]').getAttribute('content');
     const [metaTraceId, metaParentSpanId, metaSampled] = sentryTraceMetaTagContent?.split('-') || [];
