3939687cc7deee5cbd9518a39dc98d3db8ab8081
Francesco Novy <francesco.novy@sentry.io>
2025-04-25 08:52:30 +0200

    fix tanstack export files

--- packages/tanstackstart/src/index.server.ts
+++ packages/tanstackstart/src/index.server.ts
@@ -1,3 +1,7 @@
+/* eslint-disable import/export */
+
+// TODO: For now these are empty re-exports, but we may add actual implementations here
+// so we keep this to be future proof
 export * from './config';
 export * from './server';
 export * from './common';
