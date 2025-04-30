68d07337b2bfc6d57f9b03fa191fcf6861a11196
Francesco Novy <francesco.novy@sentry.io>
2025-04-30 10:13:36 +0200

    genericPool

--- dev-packages/node-integration-tests/suites/tracing/genericPool/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/genericPool/test.ts
@@ -1,35 +1,37 @@
-import { afterAll, describe, expect, test } from 'vitest';
-import { cleanupChildProcesses, createRunner } from '../../../utils/runner';
+import { afterAll, describe, expect } from 'vitest';
+import { cleanupChildProcesses, createEsmAndCjsTests } from '../../../utils/runner';
 
 describe('genericPool auto instrumentation', () => {
   afterAll(() => {
     cleanupChildProcesses();
   });
 
-  test('should auto-instrument `genericPool` package when calling pool.require()', async () => {
-    const EXPECTED_TRANSACTION = {
-      transaction: 'Test Transaction',
-      spans: expect.arrayContaining([
-        expect.objectContaining({
-          description: expect.stringMatching(/^generic-pool\.ac?quire/),
-          origin: 'auto.db.otel.generic_pool',
-          data: {
-            'sentry.origin': 'auto.db.otel.generic_pool',
-          },
-          status: 'ok',
-        }),
+  createEsmAndCjsTests(__dirname, 'scenario.mjs', 'instrument.mjs', (createRunner, test) => {
+    test('should auto-instrument `genericPool` package when calling pool.require()', async () => {
+      const EXPECTED_TRANSACTION = {
+        transaction: 'Test Transaction',
+        spans: expect.arrayContaining([
+          expect.objectContaining({
+            description: expect.stringMatching(/^generic-pool\.ac?quire/),
+            origin: 'auto.db.otel.generic_pool',
+            data: {
+              'sentry.origin': 'auto.db.otel.generic_pool',
+            },
+            status: 'ok',
+          }),
 
-        expect.objectContaining({
-          description: expect.stringMatching(/^generic-pool\.ac?quire/),
-          origin: 'auto.db.otel.generic_pool',
-          data: {
-            'sentry.origin': 'auto.db.otel.generic_pool',
-          },
-          status: 'ok',
-        }),
-      ]),
-    };
+          expect.objectContaining({
+            description: expect.stringMatching(/^generic-pool\.ac?quire/),
+            origin: 'auto.db.otel.generic_pool',
+            data: {
+              'sentry.origin': 'auto.db.otel.generic_pool',
+            },
+            status: 'ok',
+          }),
+        ]),
+      };
 
-    await createRunner(__dirname, 'scenario.js').expect({ transaction: EXPECTED_TRANSACTION }).start().completed();
+      await createRunner().expect({ transaction: EXPECTED_TRANSACTION }).start().completed();
+    });
   });
 });
