ec310ed13d750c9ae2cb14182ed1bd30274031be
Charly Gomez <charly.gomez@sentry.io>
2025-05-03 22:17:23 +0200

    add integration

--- packages/react-router/src/client/sdk.ts
+++ packages/react-router/src/client/sdk.ts
@@ -2,18 +2,29 @@ import type { BrowserOptions } from '@sentry/browser';
 import { init as browserInit } from '@sentry/browser';
 import type { Client } from '@sentry/core';
 import { applySdkMetadata, setTag } from '@sentry/core';
+import { BROWSER_TRACING_INTEGRATION_ID } from '../../../browser/build/npm/types/tracing/browserTracingIntegration';
+import { reactRouterTracingIntegration } from './tracingIntegration';
 
 /**
  * Initializes the client side of the React Router SDK.
  */
 export function init(options: BrowserOptions): Client | undefined {
-  const opts = {
-    ...options,
-  };
+  // If BrowserTracing integration was passed to options, replace it with React Router tracing integration
+  if (options.integrations && Array.isArray(options.integrations)) {
+    const hasBrowserTracing = options.integrations.some(
+      integration => integration.name === BROWSER_TRACING_INTEGRATION_ID,
+    );
+    if (hasBrowserTracing) {
+      options.integrations = options.integrations.filter(
+        integration => integration.name !== BROWSER_TRACING_INTEGRATION_ID,
+      );
+      options.integrations.push(reactRouterTracingIntegration());
+    }
+  }
 
-  applySdkMetadata(opts, 'react-router', ['react-router', 'browser']);
+  applySdkMetadata(options, 'react-router', ['react-router', 'browser']);
 
-  const client = browserInit(opts);
+  const client = browserInit(options);
 
   setTag('runtime', 'browser');
 
