245af5e57036b9349f7cf8b101b30d5f1d38e5c8
Francesco Novy <francesco.novy@sentry.io>
2025-05-06 14:13:31 +0200

    lol size limit??!

--- .size-limit.js
+++ .size-limit.js
@@ -79,7 +79,7 @@ module.exports = [
     path: 'packages/browser/build/npm/esm/index.js',
     import: createImport('init', 'browserTracingIntegration', 'replayIntegration', 'replayCanvasIntegration'),
     gzip: true,
-    limit: '81 KB',
+    limit: '82 KB',
   },
   {
     name: '@sentry/browser (incl. Tracing, Replay, Feedback)',
