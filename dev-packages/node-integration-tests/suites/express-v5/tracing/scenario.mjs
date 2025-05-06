38316fd3bff2a3079db61042021ecb055e40caf8
Francesco Gringl-Novy <francesco.novy@sentry.io>
2025-05-06 15:34:08 +0200

    test(node): Update express integration tests (#16204)
    This updates naming etc. for express (v5) tests, and makes sure that we
    test this in esm & cjs in a basic way.
    It also adds test for handling of the root route (`/`) as well as 404,
    where current failures are shown (see
    https://github.com/getsentry/sentry-javascript/issues/16203).

--- dev-packages/node-integration-tests/suites/express-v5/tracing/scenario.mjs
+++ dev-packages/node-integration-tests/suites/express-v5/tracing/scenario.mjs
@@ -1,20 +1,8 @@
-const { loggingTransport } = require('@sentry-internal/node-integration-tests');
-const Sentry = require('@sentry/node');
-
-Sentry.init({
-  dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  release: '1.0',
-  // disable attaching headers to /test/* endpoints
-  tracePropagationTargets: [/^(?!.*test).*$/],
-  tracesSampleRate: 1.0,
-  transport: loggingTransport,
-});
-
-// express must be required after Sentry is initialized
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { startExpressServerAndSendPortToRunner } = require('@sentry-internal/node-integration-tests');
+import * as Sentry from '@sentry/node';
+import { startExpressServerAndSendPortToRunner } from '@sentry-internal/node-integration-tests';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
 
 const app = express();
 
@@ -22,6 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.raw());
+
+app.get('/', (_req, res) => {
+  res.send({ response: 'response 0' });
+});
 
 app.get('/test/express', (_req, res) => {
   res.send({ response: 'response 1' });
