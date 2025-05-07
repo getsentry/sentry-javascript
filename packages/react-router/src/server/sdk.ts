56c4346668c474338321eb9d7c47a724f1d85f7d
RulaKhaled <rola.abuhasna@sentry.io>
2025-05-07 13:32:49 +0200

    feat(react-router): Create a transaction filter for react router

--- packages/react-router/src/server/sdk.ts
+++ packages/react-router/src/server/sdk.ts
@@ -1,4 +1,4 @@
-import { applySdkMetadata, logger, setTag } from '@sentry/core';
+import { type EventProcessor, applySdkMetadata, getGlobalScope, logger, setTag } from '@sentry/core';
 import type { NodeClient, NodeOptions } from '@sentry/node';
 import { init as initNodeSdk } from '@sentry/node';
 import { DEBUG_BUILD } from '../common/debug-build';
@@ -20,5 +20,33 @@ export function init(options: NodeOptions): NodeClient | undefined {
   setTag('runtime', 'node');
 
   DEBUG_BUILD && logger.log('SDK successfully initialized');
+
+  getGlobalScope().addEventProcessor(lowQualityTransactionsFilter(options));
+
   return client;
+}
+
+/**
+ * Filters out noisy transactions such as requests to node_modules
+ *
+ * @param options The NodeOptions passed to the SDK
+ * @returns An EventProcessor that filters low-quality transactions
+ */
+export function lowQualityTransactionsFilter(options: NodeOptions): EventProcessor {
+  return Object.assign(
+    (event => {
+      if (event.type !== 'transaction' || !event.transaction) {
+        return event;
+      }
+
+      if (event.transaction.match(/\/node_modules\//)) {
+        options.debug &&
+          logger.log('[ReactRouter] Filtered node_modules transaction:', event.transaction);
+        return null;
+      }
+
+      return event;
+    }) satisfies EventProcessor,
+    { id: 'ReactRouterLowQualityTransactionsFilter' },
+  );
 }
