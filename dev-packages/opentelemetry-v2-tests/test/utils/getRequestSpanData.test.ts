e472f97fbae58ec956279533bd4f0ee2c3c3b809
Andrei Borza <andrei.borza@sentry.io>
2025-05-09 18:07:55 +0200

    Add `opentelemetry-v2-tests` dev-packages and ensure they pass with Otel v1 deps

---
+++ dev-packages/opentelemetry-v2-tests/test/utils/getRequestSpanData.test.ts
@@ -0,0 +1,80 @@
+/* eslint-disable deprecation/deprecation */
+import type { Span } from '@opentelemetry/api';
+import { trace } from '@opentelemetry/api';
+import type { BasicTracerProvider } from '@opentelemetry/sdk-trace-base';
+import { SEMATTRS_HTTP_METHOD, SEMATTRS_HTTP_URL } from '@opentelemetry/semantic-conventions';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { getRequestSpanData } from '../../../../packages/opentelemetry/src/utils/getRequestSpanData';
+import { setupOtel } from '../helpers/initOtel';
+import { cleanupOtel } from '../helpers/mockSdkInit';
+import { getDefaultTestClientOptions, TestClient } from '../helpers/TestClient';
+
+describe('getRequestSpanData', () => {
+  let provider: BasicTracerProvider | undefined;
+
+  beforeEach(() => {
+    const client = new TestClient(getDefaultTestClientOptions({ tracesSampleRate: 1 }));
+    provider = setupOtel(client);
+  });
+
+  afterEach(() => {
+    cleanupOtel(provider);
+  });
+
+  function createSpan(name: string): Span {
+    return trace.getTracer('test').startSpan(name);
+  }
+
+  it('works with basic span', () => {
+    const span = createSpan('test-span');
+    const data = getRequestSpanData(span);
+
+    expect(data).toEqual({});
+  });
+
+  it('works with http span', () => {
+    const span = createSpan('test-span');
+    span.setAttributes({
+      [SEMATTRS_HTTP_URL]: 'http://example.com?foo=bar#baz',
+      [SEMATTRS_HTTP_METHOD]: 'GET',
+    });
+
+    const data = getRequestSpanData(span);
+
+    expect(data).toEqual({
+      url: 'http://example.com',
+      'http.method': 'GET',
+      'http.query': '?foo=bar',
+      'http.fragment': '#baz',
+    });
+  });
+
+  it('works without method', () => {
+    const span = createSpan('test-span');
+    span.setAttributes({
+      [SEMATTRS_HTTP_URL]: 'http://example.com',
+    });
+
+    const data = getRequestSpanData(span);
+
+    expect(data).toEqual({
+      url: 'http://example.com',
+      'http.method': 'GET',
+    });
+  });
+
+  it('works with incorrect URL', () => {
+    const span = createSpan('test-span');
+    span.setAttributes({
+      [SEMATTRS_HTTP_URL]: 'malformed-url-here',
+      [SEMATTRS_HTTP_METHOD]: 'GET',
+    });
+
+    const data = getRequestSpanData(span);
+
+    expect(data).toEqual({
+      url: 'malformed-url-here',
+      'http.method': 'GET',
+    });
+  });
+});
---
+++ dev-packages/opentelemetry-v2-tests/test/utils/setupCheck.test.ts
@@ -0,0 +1,46 @@
+import { BasicTracerProvider } from '@opentelemetry/sdk-trace-base';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { SentrySampler } from '../../../../packages/opentelemetry/src/sampler';
+import { SentrySpanProcessor } from '../../../../packages/opentelemetry/src/spanProcessor';
+import { openTelemetrySetupCheck } from '../../../../packages/opentelemetry/src/utils/setupCheck';
+import { setupOtel } from '../helpers/initOtel';
+import { cleanupOtel } from '../helpers/mockSdkInit';
+import { getDefaultTestClientOptions, TestClient } from '../helpers/TestClient';
+
+describe('openTelemetrySetupCheck', () => {
+  let provider: BasicTracerProvider | undefined;
+
+  beforeEach(() => {
+    cleanupOtel(provider);
+  });
+
+  afterEach(() => {
+    cleanupOtel(provider);
+  });
+
+  it('returns empty array by default', () => {
+    const setup = openTelemetrySetupCheck();
+    expect(setup).toEqual([]);
+  });
+
+  it('returns all setup parts', () => {
+    const client = new TestClient(getDefaultTestClientOptions());
+    provider = setupOtel(client);
+
+    const setup = openTelemetrySetupCheck();
+    expect(setup).toEqual(['SentrySampler', 'SentrySpanProcessor', 'SentryPropagator', 'SentryContextManager']);
+  });
+
+  it('returns partial setup parts', () => {
+    const client = new TestClient(getDefaultTestClientOptions());
+    provider = new BasicTracerProvider({
+      sampler: new SentrySampler(client),
+    });
+    // We want to test this deprecated case also works
+    // eslint-disable-next-line deprecation/deprecation
+    provider.addSpanProcessor(new SentrySpanProcessor());
+
+    const setup = openTelemetrySetupCheck();
+    expect(setup).toEqual(['SentrySampler', 'SentrySpanProcessor']);
+  });
+});
