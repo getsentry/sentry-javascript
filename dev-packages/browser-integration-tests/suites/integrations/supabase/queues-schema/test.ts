1680b7d7cbfd9500a01c00166fc1e53938be53a7
Onur Temizkan <onur@narval.co.uk>
2025-05-13 12:45:54 +0100

    Lint

--- dev-packages/browser-integration-tests/suites/integrations/supabase/queues-schema/test.ts
+++ dev-packages/browser-integration-tests/suites/integrations/supabase/queues-schema/test.ts
@@ -1,6 +1,5 @@
 import { type Page, expect } from '@playwright/test';
 import type { Event } from '@sentry/core';
-
 import { sentryTest } from '../../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest, shouldSkipTracingTest } from '../../../../utils/helpers';
 
