6139d0801cc00c5b19502e4b5d5100adbf40f8bc
Francesco Novy <francesco.novy@sentry.io>
2025-05-14 13:11:41 +0200

    fix replay test

--- dev-packages/browser-integration-tests/suites/replay/multiple-pages/test.ts
+++ dev-packages/browser-integration-tests/suites/replay/multiple-pages/test.ts
@@ -210,7 +210,7 @@ sentryTest(
     expect(replayEvent6).toEqual(
       getExpectedReplayEvent({
         segment_id: 6,
-        urls: ['/spa'],
+        urls: [`${TEST_HOST}/spa`],
         request: {
           url: `${TEST_HOST}/spa`,
           headers: {
