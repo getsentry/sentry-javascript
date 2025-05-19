de40109ec2026082b85322ec771d4dab421c96c0
Francesco Novy <francesco.novy@sentry.io>
2025-05-19 10:58:49 +0200

    feat(browser): Detect redirects when emitting navigation spans

--- dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/navigation-redirect/late/init.js
+++ dev-packages/browser-integration-tests/suites/tracing/browserTracingIntegration/navigation-redirect/late/init.js
@@ -4,12 +4,11 @@ window.Sentry = Sentry;
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  integrations: [Sentry.browserTracingIntegration({ idleTimeout: 2000 })],
+  integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1,
 });
 
-// Navigate to a new page to abort the pageload
-// We have to wait >300ms to avoid the redirect handling
+// trigger redirect later
 setTimeout(() => {
   window.history.pushState({}, '', '/sub-page');
-}, 500);
+}, 400);
