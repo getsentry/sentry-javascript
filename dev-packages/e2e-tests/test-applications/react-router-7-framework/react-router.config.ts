a904a02c199a2363aba413a90a5d0b456554cda2
RulaKhaled <rola.abuhasna@sentry.io>
2025-05-20 16:31:19 +0200

    test(react-router): Add and fix tests for pre rendered routes

--- dev-packages/e2e-tests/test-applications/react-router-7-framework/react-router.config.ts
+++ dev-packages/e2e-tests/test-applications/react-router-7-framework/react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from '@react-router/dev/config';
 export default {
   ssr: true,
   // todo: check why this messes up client tracing in tests
-  // prerender: ['/performance/static'],
+  prerender: ['/performance/static'],
 } satisfies Config;
