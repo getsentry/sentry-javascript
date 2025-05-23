fff626d360cca90c04613d21ad98ca0486188751
Lukas Stracke <lukas.stracke@sentry.io>
2025-05-23 14:20:31 +0200

    maybe fix instr

--- packages/react-router/src/server/instrumentation/reactRouter.ts
+++ packages/react-router/src/server/instrumentation/reactRouter.ts
@@ -40,7 +40,7 @@ export class ReactRouterInstrumentation extends InstrumentationBase<Instrumentat
         if (isWrapped(moduleExports['createRequestHandler'])) {
           this._unwrap(moduleExports, 'createRequestHandler');
         }
-        this._wrap(moduleExports, 'createRequestHandler', this._patchCreateRequestHandler());
+        this._wrap(moduleExports, 'createRequestHandler', _patchCreateRequestHandler);
         return moduleExports;
       },
       (moduleExports: ReactRouterModuleExports) => {
@@ -50,58 +50,60 @@ export class ReactRouterInstrumentation extends InstrumentationBase<Instrumentat
 
     return reactRouterServerModule;
   }
+}
 
-  /**
-   * Returns a patched version of the createRequestHandler function that adds Sentry performance monitoring.
-   * This wraps the request handler to create spans for data loader and action requests.
-   */
-  private _patchCreateRequestHandler(): (original: typeof reactRouter.createRequestHandler) => any {
-    return function sentryWrappedCreateRequestHandler(this: unknown, ...args: unknown[]) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore not sure why original isn't found here?
-      const originalRequestHandler = (original as typeof reactRouter.createRequestHandler).apply(this, args);
-      return async function sentryWrappedRequestHandler(request: Request, initialContext?: unknown) {
-        let url: URL;
-        try {
-          url = new URL(request.url);
-        } catch (error) {
-          return originalRequestHandler(request, initialContext);
-        }
+/**
+ * Returns a patched version of the createRequestHandler function that adds Sentry performance monitoring.
+ * This wraps the request handler to create spans for data loader and action requests.
+ */
+function _patchCreateRequestHandler(
+  original: typeof reactRouter.createRequestHandler,
+): typeof reactRouter.createRequestHandler {
+  return function sentryWrappedCreateRequestHandler(this: unknown, ...args: unknown[]) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore not sure why original isn't found here?
+    const originalRequestHandler = (original as typeof reactRouter.createRequestHandler).apply(this, args);
+    return async function sentryWrappedRequestHandler(request: Request, initialContext?: unknown) {
+      let url: URL;
+      try {
+        url = new URL(request.url);
+      } catch (error) {
+        return originalRequestHandler(request, initialContext);
+      }
 
-        // We currently just want to trace loaders and actions
-        if (!isDataRequest(url.pathname)) {
-          return originalRequestHandler(request, initialContext);
-        }
+      // We currently just want to trace loaders and actions
+      if (!isDataRequest(url.pathname)) {
+        return originalRequestHandler(request, initialContext);
+      }
 
-        const activeSpan = getActiveSpan();
-        const rootSpan = activeSpan && getRootSpan(activeSpan);
+      const activeSpan = getActiveSpan();
+      const rootSpan = activeSpan && getRootSpan(activeSpan);
 
-        if (!rootSpan) {
-          DEBUG_BUILD && logger.debug('No active root span found, skipping tracing for data request');
-          return originalRequestHandler(request, initialContext);
-        }
+      if (!rootSpan) {
+        DEBUG_BUILD && logger.debug('No active root span found, skipping tracing for data request');
+        return originalRequestHandler(request, initialContext);
+      }
 
-        // Set the source and overwrite attributes on the root span to ensure the transaction name
-        // is derived from the raw URL pathname rather than any parameterized route that may be set later
-        // TODO: try to set derived parameterized route from build here (args[0])
-        rootSpan.setAttributes({
-          [SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'url',
-          [SEMANTIC_ATTRIBUTE_SENTRY_OVERWRITE]: `${request.method} ${url.pathname}`,
-        });
+      // Set the source and overwrite attributes on the root span to ensure the transaction name
+      // is derived from the raw URL pathname rather than any parameterized route that may be set later
+      // TODO: try to set derived parameterized route from build here (args[0])
+      rootSpan.setAttributes({
+        [SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'url',
+        [SEMANTIC_ATTRIBUTE_SENTRY_OVERWRITE]: `${request.method} ${url.pathname}`,
+      });
 
-        return startSpan(
-          {
-            name: getSpanName(url.pathname, request.method),
-            attributes: {
-              [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.http.react-router',
-              [SEMANTIC_ATTRIBUTE_SENTRY_OP]: getOpName(url.pathname, request.method),
-            },
+      return startSpan(
+        {
+          name: getSpanName(url.pathname, request.method),
+          attributes: {
+            [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.http.react-router',
+            [SEMANTIC_ATTRIBUTE_SENTRY_OP]: getOpName(url.pathname, request.method),
           },
-          () => {
-            return originalRequestHandler(request, initialContext);
-          },
-        );
-      };
+        },
+        () => {
+          return originalRequestHandler(request, initialContext);
+        },
+      );
     };
-  }
+  };
 }
