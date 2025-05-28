7301bf6d5929a87d161af89d9a1e03e637502d65
Lukas Stracke <lukas.stracke@sentry.io>
2025-05-28 14:45:09 +0200

    test(browser): Add test demonstrating EventTarget double instrumentation

---
+++ dev-packages/browser-integration-tests/suites/integrations/browserApiErrors/init.js
@@ -0,0 +1,17 @@
+import * as Sentry from '@sentry/browser';
+
+window.Sentry = Sentry;
+
+const btn = document.getElementById('btn');
+
+const myClickListener = () => {
+  console.log('clicked');
+};
+
+btn.addEventListener('click', myClickListener);
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+});
+
+btn.addEventListener('click', myClickListener);
