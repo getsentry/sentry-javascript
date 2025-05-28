efa962a079c2bc3e587144e81fb4df8f98a48e05
RulaKhaled <rola.abuhasna@sentry.io>
2025-05-28 12:58:23 +0200

    remove links

--- dev-packages/e2e-tests/test-applications/react-router-7-framework-spa/app/root.tsx
+++ dev-packages/e2e-tests/test-applications/react-router-7-framework-spa/app/root.tsx
@@ -11,19 +11,6 @@ import type { Route } from "./+types/root";
 import "./app.css";
 import * as Sentry from '@sentry/react-router';
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
