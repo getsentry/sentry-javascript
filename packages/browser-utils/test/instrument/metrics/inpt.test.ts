1cc4a8ae745d589ea09814f47a2f281bc99f991f
Lukas Stracke <lukas.stracke@sentry.io>
2025-06-04 19:04:34 +0200

    tests

--- packages/browser-utils/test/instrument/metrics/inpt.test.ts
+++ packages/browser-utils/test/instrument/metrics/inpt.test.ts
@@ -1,4 +1,4 @@
-import { afterEach } from 'node:test';
+import { afterEach, beforeEach } from 'node:test';
 import { describe, expect, it, vi } from 'vitest';
 import { _onInp, _trackINP } from '../../../src/metrics/inp';
 import * as instrument from '../../../src/metrics/instrument';
@@ -25,6 +25,10 @@ describe('_trackINP', () => {
 describe('_onInp', () => {
   const startStandaloneWebVitalSpan = vi.spyOn(utils, 'startStandaloneWebVitalSpan');
 
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   it('early-returns if the INP metric entry has no value', () => {
     const metric = {
       value: undefined,
@@ -38,10 +42,39 @@ describe('_onInp', () => {
   it('early-returns if the INP metric value is greater than 60 seconds', () => {
     const metric = {
       value: 60_001,
-      entries: [],
+      entries: [{ name: 'click', duration: 60_001, interactionId: 1 }],
     };
     // @ts-expect-error - incomplete metric object
     _onInp({ metric });
     expect(startStandaloneWebVitalSpan).not.toHaveBeenCalled();
+  });
+
+  it('early-returns if the inp metric has an unknown interaction type', () => {
+    const metric = {
+      value: 10,
+      entries: [{ name: 'unknown', duration: 10, interactionId: 1 }],
+    };
+    // @ts-expect-error - incomplete metric object
+    _onInp({ metric });
+    expect(startStandaloneWebVitalSpan).not.toHaveBeenCalled();
+  });
+
+  it('starts a span for a valid INP metric entry', () => {
+    const metric = {
+      value: 10,
+      entries: [{ name: 'click', duration: 10, interactionId: 1 }],
+    };
+    // @ts-expect-error - incomplete metric object
+    _onInp({ metric });
+    expect(startStandaloneWebVitalSpan).toHaveBeenCalledWith({
+      attributes: {
+        'sentry.exclusive_time': 10,
+        'sentry.op': 'ui.interaction.click',
+        'sentry.origin': 'auto.http.browser.inp',
+      },
+      name: '<unknown>',
+      startTime: NaN,
+      transaction: undefined,
+    });
   });
 });
