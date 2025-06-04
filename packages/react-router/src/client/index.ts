ac22be2f5cd5a30983bbd6df703a8811b1c81b32
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2025-06-04 10:58:52 +0200

    feat(react-router): Re-export functions from `@sentry/react` (#16465)
    Re-exports functionality from the `@sentry/react`. However, not
    everything is exported as the react router exports would be confusing.
    
    closes https://github.com/getsentry/sentry-javascript/issues/16459

--- packages/react-router/src/client/index.ts
+++ packages/react-router/src/client/index.ts
@@ -2,3 +2,14 @@ export * from '@sentry/browser';
 
 export { init } from './sdk';
 export { reactRouterTracingIntegration } from './tracingIntegration';
+
+export {
+  captureReactException,
+  reactErrorHandler,
+  Profiler,
+  withProfiler,
+  useProfiler,
+  ErrorBoundary,
+  withErrorBoundary,
+} from '@sentry/react';
+export type { ErrorBoundaryProps, FallbackRender } from '@sentry/react';
