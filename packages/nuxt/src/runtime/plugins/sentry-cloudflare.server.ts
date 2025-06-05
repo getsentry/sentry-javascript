f11be16ea59b7e95930efb5fb549f8beaeab6926
s1gr1d <sigrid.huemer@posteo.at>
2025-06-05 17:27:57 +0200

    better cloudflare plugin

--- packages/nuxt/src/runtime/plugins/sentry-cloudflare.server.ts
+++ packages/nuxt/src/runtime/plugins/sentry-cloudflare.server.ts
@@ -1,6 +1,7 @@
 import type { ExecutionContext } from '@cloudflare/workers-types';
 import type { CloudflareOptions } from '@sentry/cloudflare';
 import { setAsyncLocalStorageAsyncContextStrategy, wrapRequestHandler } from '@sentry/cloudflare';
+import { getDefaultIsolationScope, getIsolationScope, logger } from '@sentry/core';
 import type { NitroApp, NitroAppPlugin } from 'nitropack';
 import type { NuxtRenderHTMLContext } from 'nuxt/app';
 import { sentryCaptureErrorHook } from '../hooks/captureErrorHook';
@@ -34,13 +35,46 @@ function isEventType(event: unknown): event is CfEventType {
   );
 }
 
-export const cloudflareNitroPlugin =
-  (sentryOptions: CloudflareOptions): NitroAppPlugin =>
+/**
+ * Sentry Cloudflare Nitro plugin for when using the "cloudflare-pages" preset in Nuxt.
+ * This plugin automatically sets up Sentry error monitoring and performance tracking for Cloudflare Pages projects.
+ *
+ * Instead of adding a `sentry.server.config.ts` file, export this plugin in the `server/plugins` directory
+ * with the necessary Sentry options to enable Sentry for your Cloudflare Pages project.
+ *
+ *
+ * @example Basic usage
+ * ```ts
+ * // nitro/plugins/sentry.ts
+ * import { defineNitroPlugin } from '#imports'
+ * import { sentryCloudflareNitroPlugin } from '@sentry/nuxt/module/plugins'
+ *
+ * export default defineNitroPlugin(sentryCloudflareNitroPlugin({
+ *   dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+ *   tracesSampleRate: 1.0,
+ * }));
+ * ```
+ *
+ * @example Dynamic configuration with nitroApp
+ * ```ts
+ * // nitro/plugins/sentry.ts
+ * import { defineNitroPlugin } from '#imports'
+ * import { sentryCloudflareNitroPlugin } from '@sentry/nuxt/module/plugins'
+ *
+ * export default defineNitroPlugin(sentryCloudflareNitroPlugin(nitroApp => ({
+ *   dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+ *   debug: nitroApp.h3App.options.debug
+ * })));
+ * ```
+ */
+export const sentryCloudflareNitroPlugin =
+  (optionsOrFn: CloudflareOptions | ((nitroApp: NitroApp) => CloudflareOptions)): NitroAppPlugin =>
   (nitroApp: NitroApp): void => {
     nitroApp.localFetch = new Proxy(nitroApp.localFetch, {
       async apply(handlerTarget, handlerThisArg, handlerArgs: [string, unknown]) {
-        // fixme: is this the correct spot?
         setAsyncLocalStorageAsyncContextStrategy();
+
+        const sentryOptions = typeof optionsOrFn === 'function' ? optionsOrFn(nitroApp) : optionsOrFn;
 
         const pathname = handlerArgs[0];
         const event = handlerArgs[1];
@@ -52,7 +86,16 @@ export const cloudflareNitroPlugin =
             context: event.context.cloudflare.context,
           };
 
-          // todo: wrap in isolation scope (like regular handler)
+          const isolationScope = getIsolationScope();
+          const newIsolationScope =
+            isolationScope === getDefaultIsolationScope() ? isolationScope.clone() : isolationScope;
+
+          logger.log(
+            `Patched Cloudflare handler (\`nitroApp.localFetch\`). ${
+              isolationScope === newIsolationScope ? 'Using existing' : 'Created new'
+            } isolation scope.`,
+          );
+
           return wrapRequestHandler(requestHandlerOptions, () => handlerTarget.apply(handlerThisArg, handlerArgs));
         }
 
@@ -63,6 +106,7 @@ export const cloudflareNitroPlugin =
     // @ts-expect-error - 'render:html' is a valid hook name in the Nuxt context
     nitroApp.hooks.hook('render:html', (html: NuxtRenderHTMLContext) => {
       // fixme: it's attaching the html meta tag but it's not connecting the trace
+      // fixme: its' actually connecting the trace but the meta tags are cached
       addSentryTracingMetaTags(html.head);
     });
 
