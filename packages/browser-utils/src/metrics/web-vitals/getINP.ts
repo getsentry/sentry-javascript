2dcadad33b768e173e46243e97ef2a0ede5a962c
Lukas Stracke <lukas.stracke@sentry.io>
2025-06-06 12:50:37 +0200

    lint

--- packages/browser-utils/src/metrics/web-vitals/getINP.ts
+++ packages/browser-utils/src/metrics/web-vitals/getINP.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import { WINDOW } from '../../types';
 import { bindReporter } from './lib/bindReporter';
 import { initMetric } from './lib/initMetric';
 import { initUnique } from './lib/initUnique';
