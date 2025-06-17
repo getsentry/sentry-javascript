39e12587acfcb037ec44386f75d695d0c4ab2d8d
Francesco Novy <francesco.novy@sentry.io>
2025-06-17 12:27:25 +0200

    handle keypress too

--- packages/browser/src/tracing/browserTracingIntegration.ts
+++ packages/browser/src/tracing/browserTracingIntegration.ts
@@ -338,7 +338,7 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
   };
 
   let _collectWebVitals: undefined | (() => void);
-  let lastClickTimestamp: number | undefined;
+  let lastInteractionTimestamp: number | undefined;
 
   /** Create routing idle transaction. */
   function _createRouteSpan(client: Client, startSpanOptions: StartSpanOptions, makeActive = true): void {
@@ -446,7 +446,11 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
       }
 
       if (detectRedirects && optionalWindowDocument) {
-        addEventListener('click', () => (lastClickTimestamp = timestampInSeconds()), { capture: true, passive: true });
+        const clickHandler = (): void => {
+          lastInteractionTimestamp = timestampInSeconds();
+        };
+        addEventListener('click', () => clickHandler, { capture: true, passive: true });
+        addEventListener('keypress', () => clickHandler, { capture: true, passive: true });
       }
 
       function maybeEndActiveSpan(): void {
@@ -562,7 +566,7 @@ export const browserTracingIntegration = ((_options: Partial<BrowserTracingOptio
             startingUrl = undefined;
             const parsed = parseStringToURLObject(to);
             const activeSpan = getActiveIdleSpan(client);
-            const navigationIsRedirect = activeSpan && detectRedirects && isRedirect(activeSpan, lastClickTimestamp);
+            const navigationIsRedirect = activeSpan && detectRedirects && isRedirect(activeSpan, lastInteractionTimestamp);
             startBrowserTracingNavigationSpan(
               client,
               {
@@ -736,7 +740,7 @@ function setActiveIdleSpan(client: Client, span: Span | undefined): void {
 // The max. time in seconds between two pageload/navigation spans that makes us consider the second one a redirect
 const REDIRECT_THRESHOLD = 0.3;
 
-function isRedirect(activeSpan: Span, lastClickTimestamp: number | undefined): boolean {
+function isRedirect(activeSpan: Span, lastInteractionTimestamp: number | undefined): boolean {
   const spanData = spanToJSON(activeSpan);
 
   const now = dateTimestampInSeconds();
@@ -750,7 +754,7 @@ function isRedirect(activeSpan: Span, lastClickTimestamp: number | undefined): b
 
   // A click happened in the last 300ms?
   // --> never consider this a redirect
-  if (lastClickTimestamp && now - lastClickTimestamp <= REDIRECT_THRESHOLD) {
+  if (lastInteractionTimestamp && now - lastInteractionTimestamp <= REDIRECT_THRESHOLD) {
     return false;
   }
 
