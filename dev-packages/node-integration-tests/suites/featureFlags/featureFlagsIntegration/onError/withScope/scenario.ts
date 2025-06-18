646417ac2970d86609977b8775d36f5a9e836fdf
Francesco Novy <francesco.novy@sentry.io>
2025-06-18 11:22:33 +0200

    test(node): Unflake feature flag test

--- dev-packages/node-integration-tests/suites/featureFlags/featureFlagsIntegration/onError/withScope/scenario.ts
+++ dev-packages/node-integration-tests/suites/featureFlags/featureFlagsIntegration/onError/withScope/scenario.ts
@@ -1,22 +1,26 @@
-import type { Scope } from '@sentry/node';
 import * as Sentry from '@sentry/node';
 import { loggingTransport } from '@sentry-internal/node-integration-tests';
+
+const flagsIntegration = Sentry.featureFlagsIntegration();
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   sampleRate: 1.0,
   transport: loggingTransport,
-  integrations: [Sentry.featureFlagsIntegration()],
+  integrations: [flagsIntegration],
 });
 
-const flagsIntegration = Sentry.getClient()?.getIntegrationByName<Sentry.FeatureFlagsIntegration>('FeatureFlags');
-flagsIntegration?.addFeatureFlag('shared', true);
+flagsIntegration.addFeatureFlag('shared', true);
 
-Sentry.withScope((_scope: Scope) => {
-  flagsIntegration?.addFeatureFlag('forked', true);
-  flagsIntegration?.addFeatureFlag('shared', false);
+Sentry.withScope(() => {
+  flagsIntegration.addFeatureFlag('forked', true);
+  flagsIntegration.addFeatureFlag('shared', false);
   Sentry.captureException(new Error('Error in forked scope'));
 });
 
-flagsIntegration?.addFeatureFlag('main', true);
-throw new Error('Error in main scope');
+flagsIntegration.addFeatureFlag('main', true);
+
+// To ensure order of sent events
+setTimeout(() => {
+  throw new Error('Error in main scope');
+}, 1);
