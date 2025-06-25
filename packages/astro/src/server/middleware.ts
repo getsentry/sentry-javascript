82c0250b2051edc8bd5b935f9eb8697e1ef6cca0
Francesco Novy <francesco.novy@sentry.io>
2025-06-25 13:25:42 +0200

    better handling of errors

--- packages/astro/src/server/middleware.ts
+++ packages/astro/src/server/middleware.ts
@@ -184,22 +184,33 @@ async function instrumentRequest(
 
               const newResponseStream = new ReadableStream({
                 start: async controller => {
+                  // Assign to a new variable to avoid TS losing the narrower type checked above.
+                  const body = originalBody;
+
+                  async function* bodyReporter(): AsyncGenerator<string | Buffer> {
+                    try {
+                      for await (const chunk of body) {
+                        yield chunk;
+                      }
+                    } catch (e) {
+                      // Report stream errors coming from user code or Astro rendering.
+                      sendErrorToSentry(e);
+                      throw e;
+                    }
+                  }
+
+                  const reportedBody = bodyReporter();
+
                   try {
-                    for await (const chunk of originalBody) {
+                    for await (const chunk of reportedBody) {
                       const html = typeof chunk === 'string' ? chunk : decoder.decode(chunk, { stream: true });
                       const modifiedHtml = addMetaTagToHead(html);
                       controller.enqueue(new TextEncoder().encode(modifiedHtml));
                     }
                   } catch (e) {
-                    sendErrorToSentry(e);
                     controller.error(e);
                   } finally {
-                    // try catch this, as it could have been manually closed by a user-land middleware
-                    try {
-                      controller.close();
-                    } catch {
-                      // we assume this means it was already closed
-                    }
+                    controller.close();
                   }
                 },
               });
