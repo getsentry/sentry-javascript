74c80235bacc25b8b10c70f6898feac8b50a9623
Onur Temizkan <onur@narval.co.uk>
2025-06-25 17:53:13 +0100

    Remove Fastify version check

--- packages/node/src/integrations/tracing/fastify/index.ts
+++ packages/node/src/integrations/tracing/fastify/index.ts
@@ -57,6 +57,35 @@ const INTEGRATION_NAME_V3 = 'Fastify-V3';
 
 export const instrumentFastifyV3 = generateInstrumentOnce(INTEGRATION_NAME_V3, () => new FastifyInstrumentationV3());
 
+function handleFastifyError(
+  this: any,
+  error: Error,
+  request: FastifyRequest & { opentelemetry?: () => { span?: Span } },
+  reply: FastifyReply,
+  shouldHandleError: (error: Error, request: FastifyRequest, reply: FastifyReply) => boolean,
+  handlerOrigin: 'diagnostics-channel' | 'onError-hook',
+): void {
+  // Diagnostics channel runs before the onError hook, so we can use it to check if the handler was already registered
+  if (handlerOrigin === 'diagnostics-channel') {
+    this.diagnosticsChannelExists = true;
+  }
+
+  if (this.diagnosticsChannelExists && handlerOrigin === 'onError-hook') {
+    DEBUG_BUILD &&
+      logger.warn(
+        'Fastify error handler was already registered via diagnostics channel.',
+        'You can safely remove `setupFastifyErrorHandler` call.',
+      );
+
+    // If the diagnostics channel already exists, we don't need to handle the error again
+    return;
+  }
+
+  if (shouldHandleError(error, request, reply)) {
+    captureException(error);
+  }
+}
+
 export const instrumentFastify = generateInstrumentOnce(INTEGRATION_NAME, () => {
   const fastifyOtelInstrumentationInstance = new FastifyOtelInstrumentation();
   const plugin = fastifyOtelInstrumentationInstance.plugin();
@@ -89,9 +118,7 @@ export const instrumentFastify = generateInstrumentOnce(INTEGRATION_NAME, () => 
       reply: FastifyReply;
     };
 
-    if (shouldHandleError(error, request, reply)) {
-      captureException(error);
-    }
+    handleFastifyError(error, request, reply, shouldHandleError, 'diagnostics-channel');
   });
 
   // Returning this as unknown not to deal with the internal types of the FastifyOtelInstrumentation
@@ -161,19 +188,9 @@ export function setupFastifyErrorHandler(fastify: FastifyInstance, options?: Par
   const shouldHandleError = options?.shouldHandleError || defaultShouldHandleError;
   const plugin = Object.assign(
     function (fastify: FastifyInstance, _options: unknown, done: () => void): void {
-      if (fastify.version?.startsWith('5.')) {
-        // Fastify 5.x uses diagnostics channel for error handling
-        DEBUG_BUILD &&
-          logger.warn(
-            'Fastify 5.x detected, using diagnostics channel for error handling.\nYou can safely remove `setupFastifyErrorHandler` call.',
-          );
-      } else {
-        fastify.addHook('onError', async (request, reply, error) => {
-          if (shouldHandleError(error, request, reply)) {
-            captureException(error);
-          }
-        });
-      }
+      fastify.addHook('onError', async (request, reply, error) => {
+        handleFastifyError(error, request, reply, shouldHandleError, 'onError-hook');
+      });
       done();
     },
     {
