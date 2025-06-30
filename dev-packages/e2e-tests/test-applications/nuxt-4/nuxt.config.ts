588d63f4057d0da33517e800d41b9cdcef7298a8
s1gr1d <sigrid.huemer@posteo.at>
2025-06-30 09:56:23 +0200

    test(nuxt): Use official Nuxt 4 version for E2E test

--- dev-packages/e2e-tests/test-applications/nuxt-4/nuxt.config.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-4/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  future: { compatibilityVersion: 4 },
   compatibilityDate: '2025-06-06',
   imports: { autoImport: false },
 
