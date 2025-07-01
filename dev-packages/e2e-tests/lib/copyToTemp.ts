87630f5a6eb35f9c9b5b844420f88dc4dfb10774
Francesco Novy <francesco.novy@sentry.io>
2025-07-01 17:19:25 +0200

    actually make abs

--- dev-packages/e2e-tests/lib/copyToTemp.ts
+++ dev-packages/e2e-tests/lib/copyToTemp.ts
@@ -18,7 +18,7 @@ function fixPackageJson(cwd: string): void {
 
   // 1. Fix file dependencies
   if (packageJson.devDependencies?.['@sentry-internal/test-utils']) {
-    const newPath = resolve(join(__dirname, '../../test-utils'));
+    const newPath = resolve('/', join(__dirname, '../../test-utils'));
     packageJson.devDependencies['@sentry-internal/test-utils'] = `link:${newPath}`;
     // eslint-disable-next-line no-console
     console.log(`Fixed devDependencies['@sentry-internal/test-utils'] to ${newPath}`);
@@ -26,7 +26,7 @@ function fixPackageJson(cwd: string): void {
 
   // 2. Fix volta extends
   if (packageJson.volta?.extends === '../../package.json') {
-    const newPath = resolve(join(__dirname, '../package.json'));
+    const newPath = resolve('/', join(__dirname, '../package.json'));
     packageJson.volta.extends = newPath;
     // eslint-disable-next-line no-console
     console.log(`Fixed volta.extends to ${newPath}`);
