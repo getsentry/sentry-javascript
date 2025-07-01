7f830807eec07811aeda6b4e92898780857400a1
Charly Gomez <charly.gomez@sentry.io>
2025-07-01 15:59:14 +0200

    ts-ignore the args

--- dev-packages/e2e-tests/test-applications/nextjs-app-dir/app/server-component/featureFlag/page.tsx
+++ dev-packages/e2e-tests/test-applications/nextjs-app-dir/app/server-component/featureFlag/page.tsx
@@ -6,8 +6,11 @@ export default async function FeatureFlagServerComponent() {
   Sentry.buildLaunchDarklyFlagUsedHandler();
   Sentry.launchDarklyIntegration();
   Sentry.openFeatureIntegration();
+  // @ts-ignore - we just want to test that the statsigIntegration is imported
   Sentry.statsigIntegration();
+  // @ts-ignore - we just want to test that the unleashIntegration is imported
   Sentry.unleashIntegration();
+  // @ts-ignore - we just want to test that the OpenFeatureIntegrationHook is imported
   Sentry.OpenFeatureIntegrationHook();
 
   return <div>FeatureFlagServerComponent</div>;
