e740361e1d53d7fa2437f521b68bb3c8fb25dad1
s1gr1d <sigrid.huemer@posteo.at>
2025-07-07 13:24:05 +0200

    add test for distributed client-side API request

---
+++ dev-packages/e2e-tests/test-applications/nuxt-4/app/pages/test-param/fetch-api/[param].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useFetch, useRoute } from '#imports';
+
+const route = useRoute();
+const param = route.params.param as string;
+
+const { data } = await useFetch(`/api/test-param/${param}`, {
+  server: false, // Don't fetch during SSR
+});
+</script>
+
+<template>
+  <div>
+    <p v-if="data">Server Data: {{ data }}</p>
+  </div>
+</template>
