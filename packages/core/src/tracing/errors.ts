9184779192efd884509b30c394aebb0c74175fbc
Tim Fish <tim@timfish.uk>
2025-07-10 13:32:04 +0200

    Lint

--- packages/core/src/tracing/errors.ts
+++ packages/core/src/tracing/errors.ts
@@ -41,5 +41,3 @@ export function registerSpanErrorInstrumentation(): void {
   addGlobalErrorInstrumentationHandler(errorCallback);
   addGlobalUnhandledRejectionInstrumentationHandler(errorCallback);
 }
-
-
