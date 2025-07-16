75439efcc1c1ed2596b6b4b954738e68f9f71294
Charly Gomez <charly.gomez@sentry.io>
2025-07-16 17:36:43 +0200

    test(nextjs): Run e2e tests for nextjs-15 in turbopack (#17018)

--- dev-packages/e2e-tests/test-applications/nextjs-15/playwright.config.mjs
+++ dev-packages/e2e-tests/test-applications/nextjs-15/playwright.config.mjs
@@ -5,8 +5,24 @@ if (!testEnv) {
   throw new Error('No test env defined');
 }
 
+const getStartCommand = () => {
+  if (testEnv === 'dev-turbopack') {
+    return 'pnpm next dev -p 3030 --turbopack';
+  }
+
+  if (testEnv === 'development') {
+    return 'pnpm next dev -p 3030';
+  }
+
+  if (testEnv === 'production') {
+    return 'pnpm next start -p 3030';
+  }
+
+  throw new Error(`Unknown test env: ${testEnv}`);
+};
+
 const config = getPlaywrightConfig({
-  startCommand: testEnv === 'development' ? 'pnpm next dev -p 3030' : 'pnpm next start -p 3030',
+  startCommand: getStartCommand(),
   port: 3030,
 });
 
--- dev-packages/e2e-tests/test-applications/nextjs-15/tests/prefetch-spans.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-15/tests/prefetch-spans.test.ts
@@ -2,7 +2,10 @@ import { expect, test } from '@playwright/test';
 import { waitForTransaction } from '@sentry-internal/test-utils';
 
 test('Prefetch client spans should have a http.request.prefetch attribute', async ({ page }) => {
-  test.skip(process.env.TEST_ENV === 'development', "Prefetch requests don't have the prefetch header in dev mode");
+  test.skip(
+    process.env.TEST_ENV === 'development' || process.env.TEST_ENV === 'dev-turbopack',
+    "Prefetch requests don't have the prefetch header in dev mode",
+  );
 
   const pageloadTransactionPromise = waitForTransaction('nextjs-15', async transactionEvent => {
     return transactionEvent?.transaction === '/prefetching';
