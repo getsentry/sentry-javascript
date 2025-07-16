6f773f669e41981d3872f8fa53af66d77b632c62
Lukas Stracke <lukas.stracke@sentry.io>
2025-07-16 12:43:15 +0200

    fix lint error

--- dev-packages/browser-integration-tests/suites/integrations/webWorker/init.js
+++ dev-packages/browser-integration-tests/suites/integrations/webWorker/init.js
@@ -3,18 +3,9 @@ import * as Sentry from '@sentry/browser';
 // Initialize Sentry with webWorker integration
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  debug: true,
-  beforeSend(event) {
-    console.log('xx beforeSend', JSON.stringify(event.exception.values[0].stacktrace.frames, null, 2));
-    return event;
-  },
 });
 
 const worker = new Worker('/worker.js');
-
-worker.addEventListener('message', event => {
-  console.log('xx message', event);
-});
 
 Sentry.addIntegration(Sentry.webWorkerIntegration({ worker }));
 
