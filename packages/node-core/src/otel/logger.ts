99360ba555314779f332f279ad3fe3476178f8c5
Abhijeet Prasad <aprasad@sentry.io>
2025-07-16 03:42:21 -0400

    ref(node-core|node): Use `debug` instead of `logger` (#17025)
    resolves https://github.com/getsentry/sentry-javascript/issues/16946
    resolves https://github.com/getsentry/sentry-javascript/issues/16947

--- packages/node-core/src/otel/logger.ts
+++ packages/node-core/src/otel/logger.ts
@@ -1,18 +1,20 @@
 import { diag, DiagLogLevel } from '@opentelemetry/api';
-import { logger } from '@sentry/core';
+import { debug } from '@sentry/core';
 
 /**
- * Setup the OTEL logger to use our own logger.
+ * Setup the OTEL logger to use our own debug logger.
  */
 export function setupOpenTelemetryLogger(): void {
-  const otelLogger = new Proxy(logger as typeof logger & { verbose: (typeof logger)['debug'] }, {
-    get(target, prop, receiver) {
-      const actualProp = prop === 'verbose' ? 'debug' : prop;
-      return Reflect.get(target, actualProp, receiver);
-    },
-  });
-
   // Disable diag, to ensure this works even if called multiple times
   diag.disable();
-  diag.setLogger(otelLogger, DiagLogLevel.DEBUG);
+  diag.setLogger(
+    {
+      error: debug.error,
+      warn: debug.warn,
+      info: debug.log,
+      debug: debug.log,
+      verbose: debug.log,
+    },
+    DiagLogLevel.DEBUG,
+  );
 }
