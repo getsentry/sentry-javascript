9b8ab32e59b4d6a100d67c1a358903879c93b5ec
Charly Gomez <charly.gomez@sentry.io>
2025-07-28 14:43:54 +0200

    not needed

--- dev-packages/e2e-tests/test-applications/nextjs-pages-dir/next.config.js
+++ dev-packages/e2e-tests/test-applications/nextjs-pages-dir/next.config.js
@@ -3,7 +3,6 @@ const { withSentryConfig } = require('@sentry/nextjs');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverActions: true,
   },
 };
