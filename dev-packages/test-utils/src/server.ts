79240d66ecb78d554a124c20bff213c0626b812b
Tim Fish <tim@timfish.uk>
2025-07-29 15:11:45 +0200

    test(v9/cloudflare): Add integration test infrastructure

---
+++ dev-packages/test-utils/src/server.ts
@@ -0,0 +1,39 @@
+import type { Envelope } from '@sentry/core';
+import { parseEnvelope } from '@sentry/core';
+import express from 'express';
+import type { AddressInfo } from 'net';
+
+/**
+ * Creates a basic Sentry server that accepts POST to the envelope endpoint
+ *
+ * This does no checks on the envelope, it just calls the callback if it managed to parse an envelope from the raw POST
+ * body data.
+ */
+export function createBasicSentryServer(onEnvelope: (env: Envelope) => void): Promise<[number, () => void]> {
+  const app = express();
+
+  app.use(express.raw({ type: () => true, inflate: true, limit: '100mb' }));
+  app.post('/api/:id/envelope/', (req, res) => {
+    try {
+      const env = parseEnvelope(req.body as Buffer);
+      onEnvelope(env);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+    }
+
+    res.status(200).send();
+  });
+
+  return new Promise(resolve => {
+    const server = app.listen(0, () => {
+      const address = server.address() as AddressInfo;
+      resolve([
+        address.port,
+        () => {
+          server.close();
+        },
+      ]);
+    });
+  });
+}
