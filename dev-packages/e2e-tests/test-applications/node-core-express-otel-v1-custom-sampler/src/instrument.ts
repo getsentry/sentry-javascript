426b2418c2555ab563f086728dcd1221f96aabfe
Andrei Borza <andrei.borza@sentry.io>
2025-07-30 10:06:38 +0200

    Remove some more DSNs

--- dev-packages/e2e-tests/test-applications/node-core-express-otel-v1-custom-sampler/src/instrument.ts
+++ dev-packages/e2e-tests/test-applications/node-core-express-otel-v1-custom-sampler/src/instrument.ts
@@ -6,9 +6,7 @@ import { CustomSampler } from './custom-sampler';
 
 Sentry.init({
   environment: 'qa', // dynamic sampling bias to keep transactions
-  dsn:
-    process.env.E2E_TEST_DSN ||
-    'https://3b6c388182fb435097f41d181be2b2ba@o4504321058471936.ingest.sentry.io/4504321066008576',
+  dsn: process.env.E2E_TEST_DSN,
   includeLocalVariables: true,
   debug: !!process.env.DEBUG,
   tunnel: `http://localhost:3031/`, // proxy server
