e695b87fe955d4f98ce191b3e67985b8af953ba1
Lukas Stracke <lukas.stracke@sentry.io>
2025-07-31 12:05:50 +0200

    fix test

--- packages/aws-serverless/test/sdk.test.ts
+++ packages/aws-serverless/test/sdk.test.ts
@@ -501,7 +501,7 @@ describe('AWSLambda', () => {
       // @ts-expect-error just mocking around...
       expect(evtProcessor(event).exception.values[0]?.mechanism).toEqual({
         handled: false,
-        type: 'generic',
+        type: 'aws-serverless.handler',
       });
     }
   });
