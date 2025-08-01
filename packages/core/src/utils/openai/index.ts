0cb799f874aec85c88b7326bd00d42289f78feaf
Lukas Stracke <lukas.stracke@sentry.io>
2025-08-01 16:54:12 +0200

    update type to trace origin, set origin

--- packages/core/src/utils/openai/index.ts
+++ packages/core/src/utils/openai/index.ts
@@ -50,7 +50,11 @@ function extractRequestAttributes(args: unknown[], methodPath: string): Record<s
   const attributes: Record<string, unknown> = {
     [GEN_AI_SYSTEM_ATTRIBUTE]: 'openai',
     [GEN_AI_OPERATION_NAME_ATTRIBUTE]: getOperationName(methodPath),
+<<<<<<< HEAD
     [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.function.openai',
+=======
+    [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'auto.ai.openai',
+>>>>>>> c3e94e7d9 (update type to trace origin, set origin)
   };
 
   // Chat completion API accepts web_search_options and tools as parameters
@@ -258,7 +262,7 @@ function instrumentMethod<T extends unknown[], R>(
             captureException(error, {
               mechanism: {
                 handled: false,
-                type: 'openai.stream',
+                type: 'auto.ai.openai.stream',
                 data: {
                   function: methodPath,
                 },
@@ -290,7 +294,7 @@ function instrumentMethod<T extends unknown[], R>(
             captureException(error, {
               mechanism: {
                 handled: false,
-                type: 'openai',
+                type: 'auto.ai.openai',
                 data: {
                   function: methodPath,
                 },
