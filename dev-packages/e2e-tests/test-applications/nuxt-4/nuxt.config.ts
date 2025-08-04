fc9bc4e0162a97b310d8adfbb2017693acd83c0a
s1gr1d <sigrid.huemer@posteo.at>
2025-08-04 13:17:41 +0200

    feat(nuxt): Do not inject trace meta-tags on cached HTML pages

--- dev-packages/e2e-tests/test-applications/nuxt-4/nuxt.config.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-4/nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-06-06',
   imports: { autoImport: false },
 
+  routeRules: {
+    '/rendering-modes/client-side-only-page': { ssr: false },
+    '/rendering-modes/pre-rendered-page': { prerender: true },
+    '/rendering-modes/swr-cached-page': { swr: true },
+    '/rendering-modes/swr-1h-cached-page': { swr: 3600 },
+    '/rendering-modes/isr-cached-page': { isr: true },
+    '/rendering-modes/isr-1h-cached-page': { isr: 3600 },
+  },
+
   modules: ['@pinia/nuxt', '@sentry/nuxt/module'],
 
   runtimeConfig: {
