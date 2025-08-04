a1efb5e8b4cd971ecf502d68735514d368635d1e
Francesco Gringl-Novy <francesco.novy@sentry.io>
2025-08-04 14:56:12 +0200

    feat(react-router): Streamline build logs (#17303)
    Streamlines react router build logs. Those are mostly already gated
    behind `debug`, so smaller changes here:
    
    1. Streamline with style from other parts of the SDK
    2. Log compact message when source maps are disabled without `debug:
    true`
    3. Add some tests for this

--- packages/react-router/src/vite/makeEnableSourceMapsPlugin.ts
+++ packages/react-router/src/vite/makeEnableSourceMapsPlugin.ts
@@ -1,4 +1,3 @@
-import { consoleSandbox } from '@sentry/core';
 import type { Plugin, UserConfig } from 'vite';
 import type { SentryReactRouterBuildOptions } from './types';
 
@@ -47,36 +46,35 @@ export function getUpdatedSourceMapSettings(
   let updatedSourceMapSetting = viteSourceMap;
 
   const settingKey = 'vite.build.sourcemap';
+  const debug = sentryPluginOptions?.debug;
 
   if (viteSourceMap === false) {
     updatedSourceMapSetting = viteSourceMap;
 
-    consoleSandbox(() => {
-      //  eslint-disable-next-line no-console
+    if (debug) {
+      // Longer debug message with more details
+      // eslint-disable-next-line no-console
       console.warn(
         `[Sentry] Source map generation is currently disabled in your Vite configuration (\`${settingKey}: false \`). This setting is either a default setting or was explicitly set in your configuration. Sentry won't override this setting. Without source maps, code snippets on the Sentry Issues page will remain minified. To show unminified code, enable source maps in \`${settingKey}\` (e.g. by setting them to \`hidden\`).`,
       );
-    });
+    } else {
+      // eslint-disable-next-line no-console
+      console.warn('[Sentry] Source map generation is disabled in your Vite configuration.');
+    }
   } else if (viteSourceMap && ['hidden', 'inline', true].includes(viteSourceMap)) {
     updatedSourceMapSetting = viteSourceMap;
 
-    if (sentryPluginOptions?.debug) {
-      consoleSandbox(() => {
-        // eslint-disable-next-line no-console
-        console.log(
-          `[Sentry] We discovered \`${settingKey}\` is set to \`${viteSourceMap.toString()}\`. Sentry will keep this source map setting. This will un-minify the code snippet on the Sentry Issue page.`,
-        );
-      });
-    }
+    debug &&
+      // eslint-disable-next-line no-console
+      console.log(
+        `[Sentry] We discovered \`${settingKey}\` is set to \`${viteSourceMap.toString()}\`. Sentry will keep this source map setting. This will un-minify the code snippet on the Sentry Issue page.`,
+      );
   } else {
     updatedSourceMapSetting = 'hidden';
-
-    consoleSandbox(() => {
-      //  eslint-disable-next-line no-console
+    debug && //  eslint-disable-next-line no-console
       console.log(
         `[Sentry] Enabled source map generation in the build options with \`${settingKey}: 'hidden'\`. The source maps  will be deleted after they were uploaded to Sentry.`,
       );
-    });
   }
 
   return updatedSourceMapSetting;
