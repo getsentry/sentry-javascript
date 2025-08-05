9f29e0a0f16fd4358d4434fee9c2372c0d32a4fe
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2025-08-05 11:46:52 +0200

    feat(v9/nuxt): Do not inject trace meta-tags on cached HTML pages (#17305) (#17319)
    > Backport of https://github.com/getsentry/sentry-javascript/pull/17305
    
    Don't inject tracing meta tags on cached HTML pages (SWR and
    pre-rendered pages). This means won't have connected traces for those
    pages anymore. But before, multiple pageloads were listed in one trace
    as they were all connected to one backend request.
    
    Closes https://github.com/getsentry/sentry-javascript/issues/16045
    
    I spent most of the time creating the tests for this PR and verifying it
    works - the logic is so small 😅

--- packages/nuxt/src/runtime/plugins/sentry.server.ts
+++ packages/nuxt/src/runtime/plugins/sentry.server.ts
@@ -5,8 +5,7 @@ import {
   getIsolationScope,
   withIsolationScope,
 } from '@sentry/core';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { type EventHandler } from 'h3';
+import type { EventHandler, H3Event } from 'h3';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineNitroPlugin } from 'nitropack/runtime';
 import type { NuxtRenderHTMLContext } from 'nuxt/app';
@@ -22,8 +21,21 @@ export default defineNitroPlugin(nitroApp => {
   nitroApp.hooks.hook('error', sentryCaptureErrorHook);
 
   // @ts-expect-error - 'render:html' is a valid hook name in the Nuxt context
-  nitroApp.hooks.hook('render:html', (html: NuxtRenderHTMLContext) => {
-    addSentryTracingMetaTags(html.head);
+  nitroApp.hooks.hook('render:html', (html: NuxtRenderHTMLContext, { event }: { event: H3Event }) => {
+    const headers = event.node.res?.getHeaders() || {};
+
+    const isPreRenderedPage = Object.keys(headers).includes('x-nitro-prerender');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const isSWRCachedPage = event?.context?.cache?.options.swr as boolean | undefined;
+
+    if (!isPreRenderedPage && !isSWRCachedPage) {
+      addSentryTracingMetaTags(html.head);
+    } else {
+      const reason = isPreRenderedPage ? 'the page was pre-rendered' : 'SWR caching is enabled for the route';
+      debug.log(
+        `Not adding Sentry tracing meta tags to HTML for ${event.path} because ${reason}. This will disable distributed tracing and prevent connecting multiple client page loads to the same server request.`,
+      );
+    }
   });
 });
 
