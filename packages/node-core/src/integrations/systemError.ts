1c8810d48f06882cc88f5b445033d33e0d4a6dbb
Tim Fish <tim@timfish.uk>
2025-08-05 21:45:47 +0200

    feat(node) Capture `SystemError` context and remove paths from message

---
+++ packages/node-core/src/integrations/systemError.ts
@@ -0,0 +1,69 @@
+import { defineIntegration } from '@sentry/core';
+import { getSystemErrorMap } from 'util';
+
+const INTEGRATION_NAME = 'NodeSystemError';
+
+type SystemErrorContext = {
+  dest?: string; // If present, the file path destination when reporting a file system error
+  errno: number; // The system-provided error number
+  path?: string; // If present, the file path when reporting a file system error
+};
+
+type SystemError = Error & SystemErrorContext;
+
+function isSystemError(error: unknown): error is SystemError {
+  if (!(error instanceof Error)) {
+    return false;
+  }
+
+  if (!('errno' in error) || typeof error.errno !== 'number') {
+    return false;
+  }
+
+  // Appears this is the recommended way to check for Node.js SystemError
+  // https://github.com/nodejs/node/issues/46869
+  return getSystemErrorMap().has(error.errno);
+}
+
+/**
+ * Captures context for Node.js SystemError errors.
+ */
+export const systemErrorIntegration = defineIntegration(() => {
+  return {
+    name: INTEGRATION_NAME,
+    processEvent: (event, hint, client) => {
+      if (!isSystemError(hint.originalException)) {
+        return event;
+      }
+
+      const error = hint.originalException;
+
+      const errorContext: SystemErrorContext = {
+        ...error,
+      };
+
+      if (!client.getOptions().sendDefaultPii) {
+        delete errorContext.path;
+        delete errorContext.dest;
+      }
+
+      event.contexts = {
+        ...event.contexts,
+        node_system_error: errorContext,
+      };
+
+      for (const exception of event.exception?.values || []) {
+        if (exception.value) {
+          if (error.path && exception.value.includes(error.path)) {
+            exception.value = exception.value.replace(`'${error.path}'`, '');
+          }
+          if (error.dest && exception.value.includes(error.dest)) {
+            exception.value = exception.value.replace(`'${error.dest}'`, '');
+          }
+        }
+      }
+
+      return event;
+    },
+  };
+});
