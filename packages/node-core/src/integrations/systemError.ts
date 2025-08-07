c11eb51b820684301485dfebb39b3f3dd040b507
Tim Fish <tim@timfish.uk>
2025-08-07 11:14:33 +0100

    lint

--- packages/node-core/src/integrations/systemError.ts
+++ packages/node-core/src/integrations/systemError.ts
@@ -30,7 +30,7 @@ type Options = {
    * If true, includes the `path` and `dest` properties in the error context.
    */
   includePaths?: boolean;
-}
+};
 
 /**
  * Captures context for Node.js SystemError errors.
