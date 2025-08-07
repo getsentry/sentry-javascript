8815203535c56c6f5643c2f05a61e5dbc9212008
Onur Temizkan <onur@narval.co.uk>
2025-08-07 20:31:27 +0100

    Remove replay from e2e test app

--- dev-packages/e2e-tests/test-applications/react-router-7-lazy-routes/src/index.tsx
+++ dev-packages/e2e-tests/test-applications/react-router-7-lazy-routes/src/index.tsx
@@ -13,7 +13,6 @@ import {
 } from 'react-router-dom';
 import Index from './pages/Index';
 
-const replay = Sentry.replayIntegration();
 
 Sentry.init({
   environment: 'qa', // dynamic sampling bias to keep transactions
@@ -28,16 +27,11 @@ Sentry.init({
       trackFetchStreamPerformance: true,
       enableAsyncRouteHandlers: true,
     }),
-    replay,
   ],
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
   release: 'e2e-test',
-
-  // Always capture replays, so we can test this properly
-  replaysSessionSampleRate: 1.0,
-  replaysOnErrorSampleRate: 0.0,
 
   tunnel: 'http://localhost:3031',
 });
