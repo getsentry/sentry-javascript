b8b33f4fc050ca28a849fc974ece96e439e14e9d
Francesco Novy <francesco.novy@sentry.io>
2025-08-08 10:24:30 +0200

    WIP WIP

--- packages/node-core/src/integrations/http/index.ts
+++ packages/node-core/src/integrations/http/index.ts
@@ -117,6 +117,7 @@ export const httpIntegration = defineIntegration((options: HttpOptions = {}) => 
         ...options,
         extractIncomingTraceFromHeader: true,
         propagateTraceInOutgoingRequests: true,
+        createSpansForOutgoingRequests: true,
       });
     },
     processEvent(event) {
