9e07d7ea73d15774027bc446199362cf546bd1f0
Martin Sonnberger <martin.sonnberger@sentry.io>
2025-08-14 17:03:41 +0200

    test(aws): Run E2E tests in all supported Node versions

---
+++ dev-packages/e2e-tests/test-applications/aws-serverless/pull-sam-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to pull the correct SAM docker image based on the NODE_VERSION environment variable.
+
+set -e
+
+if [[ -z "$NODE_VERSION" ]]; then
+    echo "Error: NODE_VERSION not set"
+    exit 1
+fi
+
+echo "Pulling SAM Node $NODE_VERSION docker image..."
+docker pull --platform linux/amd64 "public.ecr.aws/sam/build-nodejs${NODE_VERSION}.x:latest"
+
+echo "Successfully pulled SAM Node $NODE_VERSION docker image"
