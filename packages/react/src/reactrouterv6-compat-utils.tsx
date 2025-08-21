00fa011c58b7d9a7981ab02d5805cc8a64fcd516
Onur Temizkan <onur@narval.co.uk>
2025-08-21 15:51:57 +0100

    Update `activeSpan` instead of `rootSpan`

--- packages/react/src/reactrouterv6-compat-utils.tsx
+++ packages/react/src/reactrouterv6-compat-utils.tsx
@@ -518,27 +518,28 @@ export function handleNavigation(opts: {
     }
 
     const activeSpan = getActiveSpan();
-    const rootSpan = activeSpan ? getRootSpan(activeSpan) : undefined;
-    const isAlreadyInNavigationSpan = rootSpan && spanToJSON(rootSpan).op === 'navigation';
+    const isAlreadyInNavigationSpan = activeSpan && spanToJSON(activeSpan).op === 'navigation';
 
     // Cross usage can result in multiple navigation spans being created without this check
     if (isAlreadyInNavigationSpan) {
       // Check if we've already set the name for this span using a custom property
       const hasBeenNamed = (
-        rootSpan as {
+        activeSpan as {
           __sentry_navigation_name_set__?: boolean;
         }
       )?.__sentry_navigation_name_set__;
       if (!hasBeenNamed) {
         // This is the first time we're setting the name for this span
-        const spanJson = spanToJSON(rootSpan);
+        const spanJson = spanToJSON(activeSpan);
         if (!spanJson.timestamp) {
-          rootSpan?.updateName(name);
-          rootSpan?.setAttribute(SEMANTIC_ATTRIBUTE_SENTRY_SOURCE, source);
+          activeSpan?.updateName(name);
         }
+
         // Mark this span as having its name set to prevent future updates
-        addNonEnumerableProperty(rootSpan, '__sentry_navigation_name_set__', true);
+        addNonEnumerableProperty(activeSpan, '__sentry_navigation_name_set__', true);
       }
+
+      activeSpan?.setAttribute(SEMANTIC_ATTRIBUTE_SENTRY_SOURCE, source);
       // If we already have a name for this span, don't update it
     } else {
       const span = startBrowserTracingNavigationSpan(client, {
