351427155b4715a86149703186967dfe291cd10e
Martin Sonnberger <martin.sonnberger@sentry.io>
2025-09-05 16:51:52 +0200

    chore(test): Remove `geist` font (#17541)
    This should fix the `enforce-license-compliance` CI check

--- dev-packages/e2e-tests/test-applications/nextjs-t3/src/app/layout.tsx
+++ dev-packages/e2e-tests/test-applications/nextjs-t3/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import '~/styles/globals.css';
 
-import { GeistSans } from 'geist/font/sans';
 import { type Metadata } from 'next';
 
 import { TRPCReactProvider } from '~/trpc/react';
@@ -13,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en">
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
--- dev-packages/e2e-tests/test-applications/nextjs-t3/tailwind.config.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-t3/tailwind.config.ts
@@ -1,14 +1,6 @@
 import { type Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.tsx'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
-      },
-    },
-  },
   plugins: [],
 } satisfies Config;
