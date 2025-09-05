af69b45241ff6283ef6d00c28226fa7229ea129e
Lukas Stracke <lukas.stracke@sentry.io>
2025-09-05 16:55:16 +0200

    ref(nestjs): Add `mechanism` to captured errors (#17312)
    The NestJS SDK didn't add a mechanism to a bunch of caught errors. This
    resulted in the errors being incorrectly marked as `handled: true` and
    the `mechansim.type` defaulting to `'generic'`.
    
    This patch:
    - adds the mechanism to all `captureException` calls within the SDK,
    marking caught exceptions as `handled: false`
    - reviewers: Please let me know if any of these should in fact be
    `handled: true`!
    - adds a specific `mechanism.type`, following the naming scheme of
    [trace
    origin](https://develop.sentry.dev/sdk/telemetry/traces/trace-origin/)
    - adjusts and adds test assertions so that we actually test on the
    expected mechanism

--- dev-packages/e2e-tests/test-applications/nestjs-fastify/tests/errors.test.ts
+++ dev-packages/e2e-tests/test-applications/nestjs-fastify/tests/errors.test.ts
@@ -14,6 +14,11 @@ test('Sends exception to Sentry', async ({ baseURL }) => {
   expect(errorEvent.exception?.values).toHaveLength(1);
   expect(errorEvent.exception?.values?.[0]?.value).toBe('This is an exception with id 123');
 
+  expect(errorEvent.exception?.values?.[0]?.mechanism).toEqual({
+    handled: false,
+    type: 'auto.http.nestjs.global_filter',
+  });
+
   expect(errorEvent.request).toEqual({
     method: 'GET',
     cookies: {},
