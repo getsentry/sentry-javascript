500989828f9b19d3f0bba9d381ce82379b4494a8
cod1k <cod1k@centro.team>
2025-09-05 15:18:32 +0300

    Introduce lock instrumentation for `context.waitUntil`
    Added `kFlushLock` to implement and manage a reusable `FlushLock` for the execution context. Enhanced promise handling with `Promise.withResolvers` and introduced type declarations for better developer experience.

--- packages/cloudflare/src/flush.ts
+++ packages/cloudflare/src/flush.ts
@@ -5,6 +5,23 @@ type FlushLock = {
   readonly finalize: () => Promise<void>;
 };
 
+const kFlushLock = Symbol.for('kFlushLock');
+
+function getInstrumentedLock<T extends object>(o: T): FlushLock | undefined {
+  const descriptor = Object.getOwnPropertyDescriptor(o, kFlushLock);
+  if (descriptor?.value) return descriptor.value as FlushLock;
+  return undefined;
+}
+
+function storeInstrumentedLock<T extends object>(o: T, lock: FlushLock): void {
+  Object.defineProperty(o, kFlushLock, {
+    value: lock,
+    enumerable: false,
+    configurable: true,
+    writable: false,
+  });
+}
+
 /**
  * Enhances the given execution context by wrapping its `waitUntil` method with a proxy
  * to monitor pending tasks, and provides a flusher function to ensure all tasks
@@ -14,25 +31,31 @@ type FlushLock = {
  * @return {FlushLock} Returns a flusher function if a valid context is provided, otherwise undefined.
  */
 export function makeFlushLock(context: ExecutionContext): FlushLock {
-  let resolveAllDone: () => void = () => undefined;
-  const allDone = new Promise<void>(res => {
-    resolveAllDone = res;
-  });
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  let lock = getInstrumentedLock(context.waitUntil);
+  if (lock) {
+    return lock;
+  }
   let pending = 0;
   const originalWaitUntil = context.waitUntil.bind(context) as typeof context.waitUntil;
-  context.waitUntil = promise => {
+  const { promise, resolve } = Promise.withResolvers();
+  const hijackedWaitUntil: typeof originalWaitUntil = promise => {
     pending++;
     return originalWaitUntil(
       promise.finally(() => {
-        if (--pending === 0) resolveAllDone();
+        if (--pending === 0) resolve();
       }),
     );
   };
-  return Object.freeze({
-    ready: allDone,
+  lock = Object.freeze({
+    ready: promise,
     finalize: () => {
-      if (pending === 0) resolveAllDone();
-      return allDone;
+      if (pending === 0) resolve();
+      return promise;
     },
-  });
+  }) as FlushLock;
+  storeInstrumentedLock(hijackedWaitUntil, lock);
+  context.waitUntil = hijackedWaitUntil;
+
+  return lock;
 }
