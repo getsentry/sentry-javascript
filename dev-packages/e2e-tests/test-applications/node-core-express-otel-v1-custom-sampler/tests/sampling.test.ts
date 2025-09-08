18f236eb5f701e05ec96b3a98ca0e70a1281611c
Francesco Novy <francesco.novy@sentry.io>
2025-09-08 09:59:26 +0200

    Revert "fix test origins"
    This reverts commit 1705573e216dd896980550e2876c3db8a57d14e2.

--- dev-packages/e2e-tests/test-applications/node-core-express-otel-v1-custom-sampler/tests/sampling.test.ts
+++ dev-packages/e2e-tests/test-applications/node-core-express-otel-v1-custom-sampler/tests/sampling.test.ts
@@ -16,7 +16,7 @@ test('Sends a sampled API route transaction', async ({ baseURL }) => {
     data: {
       'sentry.source': 'url',
       'sentry.op': 'http.server',
-      'sentry.origin': 'auto.http.otel.http',
+      'sentry.origin': 'manual',
       url: 'http://localhost:3030/task',
       'otel.kind': 'SERVER',
       'http.response.status_code': 200,
@@ -36,7 +36,7 @@ test('Sends a sampled API route transaction', async ({ baseURL }) => {
       'http.status_code': 200,
       'http.status_text': 'OK',
     },
-    origin: 'auto.http.otel.http',
+    origin: 'manual',
     op: 'http.server',
     status: 'ok',
   });
