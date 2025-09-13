367ef5c7239186686b85381750b1530ceb927069
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2025-09-13 10:42:51 +0500

    test(local-variables): Add integration tests for local variables in out-of-app frames

---
+++ dev-packages/node-integration-tests/suites/public-api/LocalVariables/local-variables-out-of-app.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+
+const Sentry = require('@sentry/node');
+// const { loggingTransport } = require('@sentry-internal/node-integration-tests'); is throwing error that package not found, so using relative path
+const { loggingTransport } = require('../../../src/index.ts');
+
+const { out_of_app_function } = require('./node_modules/test-module/out-of-app-function.js');
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  transport: loggingTransport,
+  includeLocalVariables: true,
+  integrations: [
+    Sentry.localVariablesIntegration({
+      includeOutOfAppFrames: true,
+    }),
+  ],
+  // either set each frame's in_app flag manually or import the `out_of_app_function` from a node_module directory
+  // beforeSend: (event) => {
+  //   event.exception?.values?.[0]?.stacktrace?.frames?.forEach(frame => {
+  //     if (frame.function === 'out_of_app_function') {
+  //       frame.in_app = false;
+  //     }
+  //   });
+  //   return event;
+  // },
+});
+
+function in_app_function() {
+  const inAppVar = 'in app value';
+  out_of_app_function();
+}
+
+setTimeout(async () => {
+  try {
+    in_app_function();
+  } catch (e) {
+    Sentry.captureException(e);
+    await Sentry.flush();
+    return null;
+  }
+}, 1000);
