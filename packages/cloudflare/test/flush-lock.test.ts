89279215ffb0dd6da37ea438aa2eb8ebaef856fa
0xbad0c0d3 <0xbad0c0d3@gmail.com>
2025-09-16 11:47:12 +0300

    feat(cloudflare): Introduce lock instrumentation for `context.waitUntil` to prevent multiple instrumentation (#17539)
    fixes #17514 
    P.S.: It would be better to avoid "side effects" of instrumentation
    process. But it will require more changes and deeply tested.
    
    ---------
    
    Co-authored-by: cod1k <cod1k@centro.team>

---
+++ packages/cloudflare/test/flush-lock.test.ts
@@ -0,0 +1,28 @@
+import { type ExecutionContext } from '@cloudflare/workers-types';
+import { describe, expect, it, vi } from 'vitest';
+import { makeFlushLock } from '../src/utils/flushLock';
+import { createPromiseResolver } from '../src/utils/makePromiseResolver';
+
+describe('Flush buffer test', () => {
+  const mockExecutionContext: ExecutionContext = {
+    waitUntil: vi.fn(),
+    passThroughOnException: vi.fn(),
+    props: null,
+  };
+  it('should flush buffer immediately if no waitUntil were called', async () => {
+    const { finalize } = makeFlushLock(mockExecutionContext);
+    await expect(finalize()).resolves.toBeUndefined();
+  });
+  it('waitUntil should not be wrapped mose than once', () => {
+    expect(makeFlushLock(mockExecutionContext), 'Execution context wrapped twice').toBe(
+      makeFlushLock(mockExecutionContext),
+    );
+  });
+  it('should flush buffer only after all waitUntil were finished', async () => {
+    const { promise, resolve } = createPromiseResolver();
+    const lock = makeFlushLock(mockExecutionContext);
+    mockExecutionContext.waitUntil(promise);
+    process.nextTick(resolve);
+    await expect(lock.finalize()).resolves.toBeUndefined();
+  });
+});
