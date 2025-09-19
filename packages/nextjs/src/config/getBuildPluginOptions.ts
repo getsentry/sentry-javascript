6fc29fb8b33a1ed1197a301ae3cfdfd583095e8c
Charly Gomez <charly.gomez@sentry.io>
2025-09-19 10:53:40 +0200

    Update packages/nextjs/src/config/getBuildPluginOptions.ts
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>

--- packages/nextjs/src/config/getBuildPluginOptions.ts
+++ packages/nextjs/src/config/getBuildPluginOptions.ts
@@ -48,7 +48,7 @@ type BuildTool = keyof typeof LOGGER_PREFIXES;
  * Normalizes Windows paths to POSIX format for glob patterns
  */
 function normalizePathForGlob(distPath: string): string {
-  return distPath.replace(/\\/g, '/');
+  return distPath.replaceAll(path.sep, path.posix.sep)
 }
 
 /**
