f5ca8fc1d0553723b9fc8ff8c05da37570673c1e
s1gr1d <sigrid.huemer@posteo.at>
2025-09-19 16:29:17 +0200

    test(nuxt): Update Nuxt version and add Nitro $fetch test

--- dev-packages/e2e-tests/test-applications/nuxt-4/app/pages/fetch-server-routes.vue
+++ dev-packages/e2e-tests/test-applications/nuxt-4/app/pages/fetch-server-routes.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <button @click="fetchError">Fetch Server API Error</button>
+    <button @click="fetchNitroFetch">Fetch Nitro $fetch</button>
   </div>
 </template>
 
@@ -9,5 +10,9 @@ import { useFetch } from '#imports';
 
 const fetchError = async () => {
   await useFetch('/api/server-error');
+};
+
+const fetchNitroFetch = async () => {
+  await useFetch('/api/nitro-fetch');
 };
 </script>
