bd61a0977686cebc800adf434e28a5b2516282cf
Lukas Stracke <lukas.stracke@sentry.io>
2025-09-19 14:52:52 +0200

    extract to its own file

--- packages/browser/src/tracing/browserTracingIntegration.ts
+++ packages/browser/src/tracing/browserTracingIntegration.ts
@@ -763,18 +763,6 @@ export function getMetaContent(metaName: string): string | undefined {
   return metaTag?.getAttribute('content') || undefined;
 }
 
-/**
- * Manually report the end of the page load, resulting in the SDK ending the pageload span.
- * This only works if {@link BrowserTracingOptions.enableReportPageLoaded} is set to `true`.
- * Otherwise, the pageload span will end itself based on the {@link BrowserTracingOptions.finalTimeout},
- * {@link BrowserTracingOptions.idleTimeout} and {@link BrowserTracingOptions.childSpanTimeout}.
- *
- * @param client - The client to use. If not provided, the global client will be used.
- */
-export function reportPageLoaded(client: Client | undefined = getClient()): void {
-  client?.emit('endPageloadSpan');
-}
-
 /** Start listener for interaction transactions */
 function registerInteractionListener(
   client: Client,
