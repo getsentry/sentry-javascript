970c2f0c2b968426861753cf93fced327be632b3
Lukas Stracke <lukas.stracke@sentry.io>
2025-09-19 17:21:51 +0200

    remove unused import

--- packages/browser/src/tracing/setSpanActive.ts
+++ packages/browser/src/tracing/setSpanActive.ts
@@ -1,5 +1,5 @@
 import type { Span } from '@sentry/core';
-import { _INTERNAL_setSpanForScope, addNonEnumerableProperty, getActiveSpan, getCurrentScope } from '@sentry/core';
+import { _INTERNAL_setSpanForScope, getActiveSpan, getCurrentScope } from '@sentry/core';
 
 /**
  * Sets an inactive span active on the current scope.
