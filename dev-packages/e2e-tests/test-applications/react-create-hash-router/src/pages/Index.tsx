aaffb1e90df57d3dc1d86a128cef3779bfc096f7
Charly Gomez <charly.gomez@sentry.io>
2025-09-25 17:37:00 +0200

    more tests

--- dev-packages/e2e-tests/test-applications/react-create-hash-router/src/pages/Index.tsx
+++ dev-packages/e2e-tests/test-applications/react-create-hash-router/src/pages/Index.tsx
@@ -15,6 +15,21 @@ const Index = () => {
       <Link to="/user/5" id="navigation">
         navigate
       </Link>
+      <Link to="/v2/post/1" id="navigation-post-1">
+        Post 1
+      </Link>
+      <Link to="/v2/post/1/featured" id="navigation-post-1-featured">
+        Post 1 featured
+      </Link>
+      <Link to="/v2/post/1/related" id="navigation-post-1-related">
+        Post 1 related
+      </Link>
+      <Link to="/group/1" id="navigation-group-1">
+        Group 1
+      </Link>
+      <Link to="/group/1/5" id="navigation-group-1-user-5">
+        Group 1 user 5
+      </Link>
     </>
   );
 };
