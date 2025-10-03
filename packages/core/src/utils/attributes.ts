9ef8ccf46e971f23c49e0929fdf3d2c99637c724
Lukas Stracke <lukas.stracke@sentry.io>
2025-10-03 18:05:40 +0200

    apply scope contexts, extras, request data attributes

--- packages/core/src/utils/attributes.ts
+++ packages/core/src/utils/attributes.ts
@@ -1,4 +1,8 @@
+import { normalize } from '..';
 import type { SerializedAttribute } from '../types-hoist/attributes';
+import { Primitive } from '../types-hoist/misc';
+import type { SpanAttributes, SpanAttributeValue } from '../types-hoist/span';
+import { isPrimitive } from './is';
 
 /**
  * Converts an attribute value to a serialized attribute value object, containing
@@ -46,4 +50,51 @@ export function attributeValueToSerializedAttribute(value: unknown): SerializedA
       };
     }
   }
+}
+
+/**
+ * Given an object that might contain keys with primitive, array, or object values,
+ * return a SpanAttributes object that flattens the object into a single level.
+ * - Nested keys are separated by '.'.
+ * - arrays are stringified (TODO: might change, depending on how we support array attributes)
+ * - objects are flattened
+ * - primitives are added directly
+ * - nullish values are ignored
+ * - maxDepth is the maximum depth to flatten the object to
+ *
+ * @param obj - The object to flatten into span attributes
+ * @returns The span attribute object
+ */
+export function attributesFromObject(obj: Record<string, unknown>, maxDepth = 3): SpanAttributes {
+  const result: Record<string, number | string | boolean | undefined> = {};
+
+  function primitiveOrToString(current: unknown): number | boolean | string {
+    if (typeof current === 'number' || typeof current === 'boolean' || typeof current === 'string') {
+      return current;
+    }
+    return String(current);
+  }
+
+  function flatten(current: unknown, prefix: string, depth: number): void {
+    if (current == null) {
+      return;
+    } else if (depth >= maxDepth) {
+      result[prefix] = primitiveOrToString(current);
+      return;
+    } else if (Array.isArray(current)) {
+      result[prefix] = JSON.stringify(current);
+    } else if (typeof current === 'number' || typeof current === 'string' || typeof current === 'boolean') {
+      result[prefix] = current;
+    } else if (typeof current === 'object' && current !== null && !Array.isArray(current) && depth < maxDepth) {
+      for (const [key, value] of Object.entries(current as Record<string, unknown>)) {
+        flatten(value, prefix ? `${prefix}.${key}` : key, depth + 1);
+      }
+    }
+  }
+
+  const normalizedObj = normalize(obj, maxDepth);
+
+  flatten(normalizedObj, '', 0);
+
+  return result;
 }
