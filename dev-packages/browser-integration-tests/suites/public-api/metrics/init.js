90ca1471a0c78c62bdbf1bf727b144f6ba32fd3e
Charly Gomez <charly.gomez@sentry.io>
2025-10-08 11:28:49 +0200

    update test

--- dev-packages/browser-integration-tests/suites/public-api/metrics/init.js
+++ dev-packages/browser-integration-tests/suites/public-api/metrics/init.js
@@ -4,7 +4,9 @@ window.Sentry = Sentry;
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  _enableTraceMetrics: true,
+  _experiments: {
+    enableTraceMetrics: true,
+  },
   release: '1.0.0',
   environment: 'test',
   autoSessionTracking: false, // Was causing session envelopes to be sent
