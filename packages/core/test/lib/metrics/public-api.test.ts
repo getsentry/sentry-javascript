350567f2b8eb27cc4d46c6d437621fac4e6ca83f
Charly Gomez <charly.gomez@sentry.io>
2025-10-08 12:04:02 +0200

    .

--- packages/core/test/lib/metrics/public-api.test.ts
+++ packages/core/test/lib/metrics/public-api.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { Scope } from '../../../src';
 import { _INTERNAL_getMetricBuffer } from '../../../src/metrics/internal';
 import { count, distribution, gauge } from '../../../src/metrics/public-api';
@@ -335,4 +335,3 @@ describe('Metrics Public API', () => {
     });
   });
 });
-
