d2894ad086561110265408d5ed4374f73cb0be41
Charly Gomez <charly.gomez@sentry.io>
2025-10-14 10:21:09 +0200

    canary tests

--- dev-packages/e2e-tests/test-applications/nextjs-16/next.config.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-16/next.config.ts
@@ -1,10 +1,12 @@
 import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
+const isCanaryBuild = process.env.CANARY_BUILD === 'true';
+
 const nextConfig: NextConfig = {
-  // experimental: {
-  //   cacheComponents: true,
-  // },
+  experimental: {
+    ...(isCanaryBuild ? { cacheComponents: true } : {}),
+  },
 };
 
 export default withSentryConfig(nextConfig, {
--- dev-packages/e2e-tests/test-applications/nextjs-16/tests/cached-components-error.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-16/tests/cached-components-error.test.ts
@@ -1,21 +1,21 @@
 import { expect, test } from '@playwright/test';
 import { waitForError, waitForTransaction } from '@sentry-internal/test-utils';
 
-test('should not capture React-internal errors for PPR rendering', async ({ page }) => {
-  // todo
-  test.skip(true, 'needs to run on latest canary version');
+test('should not capture React-internal errors for cached components', async ({ page }) => {
+  test.skip(process.env.CANARY_BUILD === 'true', 'needs to run on latest canary version');
+
   const pageServerComponentTransactionPromise = waitForTransaction('nextjs-16', async transactionEvent => {
     return transactionEvent?.transaction === 'GET /ppr-error/[param]';
   });
 
   let errorEventReceived = false;
   waitForError('nextjs-16', async errorEvent => {
-    return errorEvent?.transaction === 'Page Server Component (/ppr-error/[param])';
+    return errorEvent?.transaction === 'Page Server Component (/cached-components-error/[param])';
   }).then(() => {
     errorEventReceived = true;
   });
 
-  await page.goto(`/ppr-error/foobar?id=1`);
+  await page.goto(`/cached-components-error/foobar?id=1`);
 
   const pageServerComponentTransaction = await pageServerComponentTransactionPromise;
   expect(pageServerComponentTransaction).toBeDefined();
