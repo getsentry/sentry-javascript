c70b305585d62c48b515062ec9a47cdbe5c0fe83
Charly Gomez <charly.gomez@sentry.io>
2025-10-14 10:39:14 +0200

    fix skip condition

--- dev-packages/e2e-tests/test-applications/nextjs-16/tests/cached-components-error.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-16/tests/cached-components-error.test.ts
@@ -2,10 +2,10 @@ import { expect, test } from '@playwright/test';
 import { waitForError, waitForTransaction } from '@sentry-internal/test-utils';
 
 test('should not capture React-internal errors for cached components', async ({ page }) => {
-  test.skip(process.env.CANARY_BUILD === 'true', 'needs to run on latest canary version');
+  test.skip(process.env.CANARY_BUILD !== 'true', 'needs to run on latest canary version');
 
   const pageServerComponentTransactionPromise = waitForTransaction('nextjs-16', async transactionEvent => {
-    return transactionEvent?.transaction === 'GET /ppr-error/[param]';
+    return transactionEvent?.transaction === 'GET /cached-components-error/[param]';
   });
 
   let errorEventReceived = false;
