c9995b992c666dfada86a5ea19cbf2af9df24944
Tim Fish <tim@timfish.uk>
2025-10-15 17:19:58 +0200

    Fix test

--- dev-packages/browser-integration-tests/suites/tracing/trace-lifetime/startNewTraceSampling/init.js
+++ dev-packages/browser-integration-tests/suites/tracing/trace-lifetime/startNewTraceSampling/init.js
@@ -2,21 +2,8 @@ import * as Sentry from '@sentry/browser';
 
 window.Sentry = Sentry;
 
-const originalRandom = Math.random;
-
 // Force this so that the initial sampleRand is between 0.35 and 0.45
 Math.random = () =>  0.35 + Math.random() * 0.1;
-
-// polyfill for crypto.randomUUID if not available (e.g. in non-secure contexts)
-window.crypto = window.crypto || {};
-window.crypto.randomUUID =
-  window.crypto.randomUUID ||
-  (() => {
-    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, c =>
-      // eslint-disable-next-line no-bitwise
-      (c ^ (((originalRandom() * 16) & 15) >> (c / 4))).toString(16),
-    );
-  });
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
