c6f0462f71e0234efc1e337b1358d2cdfd015c3b
Abdelrahman Awad <logaretm1@gmail.com>
2025-10-15 15:20:03 +0300

    tests: added multiple database config test

--- dev-packages/e2e-tests/test-applications/nuxt-4/nuxt.config.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-4/nuxt.config.ts
@@ -24,6 +24,20 @@ export default defineNuxtConfig({
     experimental: {
       database: true,
     },
+    database: {
+      default: {
+        connector: 'sqlite',
+        options: { name: 'db' },
+      },
+      users: {
+        connector: 'sqlite',
+        options: { name: 'users_db' },
+      },
+      analytics: {
+        connector: 'sqlite',
+        options: { name: 'analytics_db' },
+      },
+    },
     storage: {
       'test-storage': {
         driver: 'memory',
