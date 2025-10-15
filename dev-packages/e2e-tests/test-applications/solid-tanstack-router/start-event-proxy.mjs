137ce944f9cdfe67217cde53262d1f5fe96be9ec
Andrei Borza <andrei.borza@sentry.io>
2025-10-15 11:16:28 +0200

    Add E2E test app

--- dev-packages/e2e-tests/test-applications/solid-tanstack-router/start-event-proxy.mjs
+++ dev-packages/e2e-tests/test-applications/solid-tanstack-router/start-event-proxy.mjs
@@ -2,5 +2,5 @@ import { startEventProxyServer } from '@sentry-internal/test-utils';
 
 startEventProxyServer({
   port: 3031,
-  proxyServerName: 'solid',
+  proxyServerName: 'solid-tanstack-router',
 });
