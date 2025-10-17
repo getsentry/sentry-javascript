a31a8e308b75b792955d917f80409726ec9793ac
Abdelrahman Awad <logaretm1@gmail.com>
2025-10-17 17:36:00 +0300

    fix: ignore registering plugins in dev mode

--- packages/nitro/src/utils/plugins.ts
+++ packages/nitro/src/utils/plugins.ts
@@ -4,6 +4,13 @@ import type { Nitro } from 'nitropack/types';
  * Adds a Nitro plugin
  */
 export function addPlugin(nitro: Nitro, plugin: string): void {
+  // in dev mode, crash occurs when trying to import #nitrpack or anything
+  // https://github.com/nuxt/icon/issues/204
+  // so just skip it in dev for now
+  if (nitro.options.dev) {
+    return;
+  }
+
   nitro.options.plugins = nitro.options.plugins || [];
   nitro.options.plugins.push(plugin);
 }
