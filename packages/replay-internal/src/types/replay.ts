c72a99cd27e9f79a6ffbfdbb069f9ce1bea2b419
Billy Vong <billyvg@users.noreply.github.com>
2025-10-28 18:05:19 -0400

    feat(replay): Add `traces_by_timestamp` to replay event
    In order to support moving our custom rrweb events to EAP, we need to send timestamps w/ trace ids so that we can identify which trace the event belongs to.
    
    In order to avoid breaking changes, we should not change the current type of trace_ids field in the replay event, instead we add a new field traces_by_timestamp

--- packages/replay-internal/src/types/replay.ts
+++ packages/replay-internal/src/types/replay.ts
@@ -335,7 +335,7 @@ export interface PopEventContext extends CommonEventContext {
   /**
    * List of Sentry trace ids that have occurred during a replay segment
    */
-  traceIds: Array<string>;
+  traceIds: Array<[number, string]>;
 }
 
 /**
@@ -348,9 +348,9 @@ export interface InternalEventContext extends CommonEventContext {
   errorIds: Set<string>;
 
   /**
-   * Set of Sentry trace ids that have occurred during a replay segment
+   * Set of [timestamp, trace_id] tuples for Sentry traces that have occurred during a replay segment
    */
-  traceIds: Set<string>;
+  traceIds: Set<[number, string]>;
 }
 
 export type Sampled = false | 'session' | 'buffer';
