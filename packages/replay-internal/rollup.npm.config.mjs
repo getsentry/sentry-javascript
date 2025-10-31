92b16c1a4d9a1f31e1a51a4363b37184e8df6518
Tim Fish <tim@timfish.uk>
2025-10-31 11:38:19 +0100

    chore(build): Use Rolldown

--- packages/replay-internal/rollup.npm.config.mjs
+++ packages/replay-internal/rollup.npm.config.mjs
@@ -8,12 +8,6 @@ export default makeNPMConfigVariants(
       output: {
         // set exports to 'named' or 'auto' so that rollup doesn't warn
         exports: 'named',
-        // set preserveModules to false because for Replay we actually want
-        // to bundle everything into one file.
-        preserveModules:
-          process.env.SENTRY_BUILD_PRESERVE_MODULES === undefined
-            ? false
-            : Boolean(process.env.SENTRY_BUILD_PRESERVE_MODULES),
       },
     },
   }),
