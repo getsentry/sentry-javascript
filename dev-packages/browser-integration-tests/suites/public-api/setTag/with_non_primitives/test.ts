94d00df61dbc32269e8f4604ca7957f54b49ef49
Lukas Stracke <lukas.stracke@sentry.io>
2025-11-11 14:53:11 +0100

    adjust integration test

--- dev-packages/browser-integration-tests/suites/public-api/setTag/with_non_primitives/test.ts
+++ dev-packages/browser-integration-tests/suites/public-api/setTag/with_non_primitives/test.ts
@@ -3,19 +3,11 @@ import type { Event } from '@sentry/core';
 import { sentryTest } from '../../../../utils/fixtures';
 import { getFirstSentryEnvelopeRequest } from '../../../../utils/helpers';
 
-sentryTest('[bug] accepts non-primitive tags', async ({ getLocalTestUrl, page }) => {
-  // this is a bug that went unnoticed due to type definitions and a bad assertion
-  // TODO: We should not accept non-primitive tags. Fix this as a follow-up.
+sentryTest("doesn't accept non-primitive tags", async ({ getLocalTestUrl, page }) => {
   const url = await getLocalTestUrl({ testDir: __dirname });
 
   const eventData = await getFirstSentryEnvelopeRequest<Event>(page, url);
 
   expect(eventData.message).toBe('non_primitives');
-
-  // TODO: This should be an empty object but instead, it is:
-  expect(eventData.tags).toEqual({
-    tag_1: {},
-    tag_2: [],
-    tag_3: ['a', {}],
-  });
+  expect(eventData.tags).toEqual({});
 });
