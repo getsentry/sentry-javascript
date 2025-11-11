a7330cad7faa3774f22ca5f6ef5989be2d53e84b
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-11-11 04:52:06 +0200

    chore: remove ts and terser from replay

--- packages/replay-worker/rollup.examples.config.mjs
+++ packages/replay-worker/rollup.examples.config.mjs
@@ -1,39 +1,23 @@
-import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
-import { defineConfig } from 'rollup';
-import { makeLicensePlugin } from '../../dev-packages/rollup-utils/plugins/index.mjs';
-
-const licensePlugin = makeLicensePlugin('Sentry Replay Worker');
+import { defineConfig } from 'rolldown';
 
 const config = defineConfig([
   {
     input: ['./src/_worker.ts'],
+    tsconfig: './tsconfig.build.json',
     output: {
       file: './examples/worker.js',
       format: 'esm',
     },
     treeshake: 'smallest',
-    plugins: [
-      typescript({ tsconfig: './tsconfig.json', inlineSourceMap: false, sourceMap: false, inlineSources: false }),
-      licensePlugin,
-    ],
   },
   {
     input: ['./src/_worker.ts'],
+    tsconfig: './tsconfig.build.json',
     output: {
       file: './examples/worker.min.js',
       format: 'esm',
     },
     treeshake: 'smallest',
-    plugins: [
-      typescript({ tsconfig: './tsconfig.json', inlineSourceMap: false, sourceMap: false, inlineSources: false }),
-      terser({
-        mangle: {
-          module: true,
-        },
-      }),
-      licensePlugin,
-    ],
   },
 ]);
 
