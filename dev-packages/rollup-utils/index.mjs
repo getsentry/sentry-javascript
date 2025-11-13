641f37e357c69f9efabb8c920aa59b42a60d43a6
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-11-13 13:34:12 +0200

    chore: properly replace the smallest preset value

--- dev-packages/rollup-utils/index.mjs
+++ dev-packages/rollup-utils/index.mjs
@@ -1,4 +1,4 @@
 export * from './plugins/index.mjs';
 export * from './bundleHelpers.mjs';
 export * from './npmHelpers.mjs';
-export { insertAt } from './utils.mjs';
+export { insertAt, treeShakePreset } from './utils.mjs';
