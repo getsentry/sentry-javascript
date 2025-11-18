c92d866d65baa13e321024dba298605f31688d7c
Lukas Stracke <lukas.stracke@sentry.io>
2025-11-18 14:19:33 +0100

    add node integration test

--- dev-packages/browser-integration-tests/suites/public-api/logger/scopeAttributes/subject.js
+++ dev-packages/browser-integration-tests/suites/public-api/logger/scopeAttributes/subject.js
@@ -1,26 +1,26 @@
 // only log attribute
 Sentry.logger.info('log_before_any_scope', { log_attr: 'log_attr_1' });
 
-Sentry.getGlobalScope().setAttribute('global_scope_attr', true);
+Sentry.getGlobalScope().setAttributes({ global_scope_attr: true });
 
 // global scope, log attribute
 Sentry.logger.info('log_after_global_scope', { log_attr: 'log_attr_2' });
 
 Sentry.withIsolationScope(isolationScope => {
-  isolationScope.setAttribute('isolation_scope_1_attr', { value: 100, unit: 'ms' });
+  isolationScope.setAttribute('isolation_scope_1_attr', { value: 100, unit: 'millisecond' });
 
   // global scope, isolation scope, log attribute
   Sentry.logger.info('log_with_isolation_scope', { log_attr: 'log_attr_3' });
 
   Sentry.withScope(scope => {
-    scope.setAttribute('scope_attr', { value: 200, unit: 'ms' });
+    scope.setAttributes({ scope_attr: { value: 200, unit: 'millisecond' } });
 
     // global scope, isolation scope, current scope attribute, log attribute
     Sentry.logger.info('log_with_scope', { log_attr: 'log_attr_4' });
   });
 
   Sentry.withScope(scope2 => {
-    scope2.setAttribute('scope_2_attr', { value: 300, unit: 'ms' });
+    scope2.setAttribute('scope_2_attr', { value: 300, unit: 'millisecond' });
 
     // global scope, isolation scope, current scope attribute, log attribute
     Sentry.logger.info('log_with_scope_2', { log_attr: 'log_attr_5' });
