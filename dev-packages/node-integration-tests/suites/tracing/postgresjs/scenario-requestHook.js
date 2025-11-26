0bfe50128b1da7298394f0f903e3494a8da76b76
Onur Temizkan <onur@narval.co.uk>
2025-11-26 14:20:47 +0000

    feat(node): Add ESM support for postgres.js instrumentation

--- dev-packages/node-integration-tests/suites/tracing/postgresjs/scenario-requestHook.js
+++ dev-packages/node-integration-tests/suites/tracing/postgresjs/scenario-requestHook.js
@@ -32,45 +32,11 @@ async function run() {
         `;
 
         await sql`
-          UPDATE "User" SET "name" = 'Foo' WHERE "email" = 'bar@baz.com';
-        `;
-
-        await sql`
           SELECT * FROM "User" WHERE "email" = 'bar@baz.com';
-        `;
-
-        // Test parameterized queries
-        await sql`
-          SELECT * FROM "User" WHERE "email" = ${'bar@baz.com'} AND "name" = ${'Foo'};
-        `;
-
-        // Test DELETE operation
-        await sql`
-          DELETE FROM "User" WHERE "email" = 'bar@baz.com';
-        `;
-
-        // Test INSERT with RETURNING
-        await sql`
-          INSERT INTO "User" ("email", "name") VALUES ('test@example.com', 'Test User') RETURNING *;
-        `;
-
-        // Test cursor-based queries
-        await sql`SELECT * from generate_series(1,1000) as x `.cursor(10, async rows => {
-          await Promise.all(rows);
-        });
-
-        // Test multiple rows at once
-        await sql`
-          SELECT * FROM "User" LIMIT 10;
         `;
 
         await sql`
           DROP TABLE "User";
-        `;
-
-        // This will be captured as an error as the table no longer exists
-        await sql`
-          SELECT * FROM "User" WHERE "email" = 'foo@baz.com';
         `;
       } finally {
         await sql.end();
@@ -79,4 +45,5 @@ async function run() {
   );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 run();
