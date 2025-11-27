55f8d3bd400f7a2b573050e695bfd840e7e9b15b
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-11-27 13:02:17 +0200

    feat: add warnings when deprecated values are used

--- packages/nextjs/src/config/withSentryConfig.ts
+++ packages/nextjs/src/config/withSentryConfig.ts
@@ -108,63 +108,87 @@ function migrateDeprecatedWebpackOptions(userSentryOptions: SentryBuildOptions):
 
   const webpack = userSentryOptions.webpack;
 
-  const withDeprecatedFallback = <T>(newValue: T | undefined, deprecatedValue: T | undefined): T | undefined => {
+  const withDeprecatedFallback = <T>(
+    newValue: T | undefined,
+    deprecatedValue: T | undefined,
+    message: string,
+  ): T | undefined => {
+    if (deprecatedValue !== undefined && deprecatedValue !== newValue) {
+      // eslint-disable-next-line no-console
+      console.warn(message);
+    }
+
     return newValue ?? deprecatedValue;
   };
+
+  const deprecatedMessage = (deprecatedPath: string, newPath: string): string =>
+    `[@sentry/nextjs] DEPRECATION WARNING: ${deprecatedPath} is deprecated and will be removed in a future version. Use ${newPath} instead.`;
 
   /* eslint-disable deprecation/deprecation */
   // Migrate each deprecated option to the new path, but only if the new path isn't already set
   webpack.autoInstrumentServerFunctions = withDeprecatedFallback(
     webpack.autoInstrumentServerFunctions,
     userSentryOptions.autoInstrumentServerFunctions,
+    deprecatedMessage('autoInstrumentServerFunctions', 'webpack.autoInstrumentServerFunctions'),
   );
 
   webpack.autoInstrumentMiddleware = withDeprecatedFallback(
     webpack.autoInstrumentMiddleware,
     userSentryOptions.autoInstrumentMiddleware,
+    deprecatedMessage('autoInstrumentMiddleware', 'webpack.autoInstrumentMiddleware'),
   );
 
   webpack.autoInstrumentAppDirectory = withDeprecatedFallback(
     webpack.autoInstrumentAppDirectory,
     userSentryOptions.autoInstrumentAppDirectory,
+    deprecatedMessage('autoInstrumentAppDirectory', 'webpack.autoInstrumentAppDirectory'),
   );
 
   webpack.excludeServerRoutes = withDeprecatedFallback(
     webpack.excludeServerRoutes,
     userSentryOptions.excludeServerRoutes,
+    deprecatedMessage('excludeServerRoutes', 'webpack.excludeServerRoutes'),
   );
 
   webpack.widenClientFileUpload = withDeprecatedFallback(
     webpack.widenClientFileUpload,
     userSentryOptions.widenClientFileUpload,
+    deprecatedMessage('widenClientFileUpload', 'webpack.widenClientFileUpload'),
   );
 
   webpack.unstable_sentryWebpackPluginOptions = withDeprecatedFallback(
     webpack.unstable_sentryWebpackPluginOptions,
     userSentryOptions.unstable_sentryWebpackPluginOptions,
+    deprecatedMessage('unstable_sentryWebpackPluginOptions', 'webpack.unstable_sentryWebpackPluginOptions'),
   );
 
   webpack.disableSentryConfig = withDeprecatedFallback(
     webpack.disableSentryConfig,
     userSentryOptions.disableSentryWebpackConfig,
+    deprecatedMessage('disableSentryWebpackConfig', 'webpack.disableSentryConfig'),
   );
 
   // Handle treeshake.debugLogs specially since it's nested
   if (userSentryOptions.disableLogger !== undefined) {
     webpack.treeshake = webpack.treeshake || {};
-    webpack.treeshake.debugLogs = withDeprecatedFallback(webpack.treeshake.debugLogs, userSentryOptions.disableLogger);
+    webpack.treeshake.debugLogs = withDeprecatedFallback(
+      webpack.treeshake.debugLogs,
+      userSentryOptions.disableLogger,
+      deprecatedMessage('disableLogger', 'webpack.treeshake.debugLogs'),
+    );
   }
 
   webpack.automaticVercelMonitors = withDeprecatedFallback(
     webpack.automaticVercelMonitors,
     userSentryOptions.automaticVercelMonitors,
+    deprecatedMessage('automaticVercelMonitors', 'webpack.automaticVercelMonitors'),
   );
 
   webpack.reactComponentAnnotation = withDeprecatedFallback(
     webpack.reactComponentAnnotation,
     userSentryOptions.reactComponentAnnotation,
+    deprecatedMessage('reactComponentAnnotation', 'webpack.reactComponentAnnotation'),
   );
-  /* eslint-enable deprecation/deprecation */
 }
 
 // Modify the materialized object form of the user's next config by deleting the `sentry` property and wrapping the
