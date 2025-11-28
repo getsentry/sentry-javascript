0aa0c4e8de17273b8bdd39294fbf1e677cd6d466
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-11-28 11:28:23 +0200

    fix: simplify import placeholder replacers

--- packages/nextjs/src/config/templates/routeHandlerWrapperTemplate.ts
+++ packages/nextjs/src/config/templates/routeHandlerWrapperTemplate.ts
@@ -60,8 +60,7 @@ function wrapHandler<T>(handler: T, method: 'GET' | 'POST' | 'PUT' | 'PATCH' | '
 // @ts-expect-error See above
 export * from '__SENTRY_WRAPPING_TARGET_FILE__';
 
-// @ts-expect-error This is the file we're wrapping
-export { default } from '__SENTRY_WRAPPING_TARGET_FILE__';
+export default routeModule.default;
 
 type RouteHandler = (...args: unknown[]) => unknown;
 
