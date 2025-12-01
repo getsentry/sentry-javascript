a32d8e5cca942fda969e35ffc3c4270d16819b38
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-01 17:07:49 +0100

    more integration tests

---
+++ dev-packages/browser-integration-tests/suites/span-first/backgroundtab-pageload/test.ts
@@ -0,0 +1,22 @@
+import { expect } from '@playwright/test';
+import { sentryTest } from '../../../utils/fixtures';
+import { shouldSkipTracingTest } from '../../../utils/helpers';
+import { getSpanOp, waitForV2Spans } from '../../../utils/spanFirstUtils';
+
+sentryTest('ends pageload span when the page goes to background', async ({ getLocalTestUrl, page }) => {
+  if (shouldSkipTracingTest()) {
+    sentryTest.skip();
+  }
+  const url = await getLocalTestUrl({ testDir: __dirname });
+
+  const spanPromise = waitForV2Spans(page, spans => !!spans.find(span => getSpanOp(span) === 'pageload'));
+
+  await page.goto(url);
+  await page.locator('#go-background').click();
+
+  const pageloadSpan = (await spanPromise).find(span => getSpanOp(span) === 'pageload');
+
+  expect(pageloadSpan?.status).toBe('error'); // a cancelled span previously mapped to status error with message cancelled.
+  expect(pageloadSpan?.attributes?.['sentry.op']?.value).toBe('pageload');
+  expect(pageloadSpan?.attributes?.['sentry.cancellation_reason']?.value).toBe('document.hidden');
+});
