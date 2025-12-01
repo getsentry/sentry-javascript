919389f38ee460fe2e13fdf76e8be348c1310cd2
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-01 17:07:49 +0100

    more integration tests

---
+++ dev-packages/browser-integration-tests/suites/span-first/backgroundtab-pageload/init.js
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/browser';
+
+window.Sentry = Sentry;
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  traceLifecycle: 'stream',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.spanStreamingIntegration()],
+  tracePropagationTargets: ['http://sentry-test-site.example'],
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+});
