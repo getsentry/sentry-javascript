3294f62c3b74ffd2a9477ed7c343920ab35ade1f
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-02 16:10:05 +0100

    rewrite httpContext integration to use processSpan client hook

--- packages/core/src/spans/captureSpan.ts
+++ packages/core/src/spans/captureSpan.ts
@@ -1,4 +1,3 @@
-import { type RawAttributes, isAttributeObject } from '../attributes';
 import type { Client } from '../client';
 import { getClient, getGlobalScope } from '../currentScopes';
 import { DEBUG_BUILD } from '../debug-build';
@@ -16,10 +15,12 @@ import {
   SEMANTIC_ATTRIBUTE_USER_USERNAME,
 } from '../semanticAttributes';
 import { getCapturedScopesOnSpan } from '../tracing/utils';
+import type { SerializedAttributes } from '../types-hoist/attributes';
 import type { Span, SpanV2JSON } from '../types-hoist/span';
 import { mergeScopeData } from '../utils/applyScopeDataToEvent';
 import { debug } from '../utils/debug-logger';
 import { INTERNAL_getSegmentSpan, spanToV2JSON } from '../utils/spanUtils';
+import { safeSetSpanAttributes } from './spanFirstUtils';
 
 /**
  * Captures a span and returns it to the caller, to be enqueued for sending.
@@ -36,13 +37,18 @@ export function captureSpan(span: Span, client = getClient()): void {
   const { isolationScope: spanIsolationScope, scope: spanScope } = getCapturedScopesOnSpan(span);
   const finalScopeData = getFinalScopeData(spanIsolationScope, spanScope);
 
-  const originalAttributeKeys = Object.keys(serializedSegmentSpan.attributes ?? {});
+  const originalAttributes = serializedSegmentSpan.attributes ?? {};
 
-  applyCommonSpanAttributes(span, serializedSegmentSpan, client, finalScopeData, originalAttributeKeys);
+  applyCommonSpanAttributes(span, serializedSegmentSpan, client, finalScopeData, originalAttributes);
 
   if (span === segmentSpan) {
-    applyScopeToSegmentSpan(span, finalScopeData, originalAttributeKeys);
+    applyScopeToSegmentSpan(span, finalScopeData, originalAttributes);
   }
+
+  // Allow integrations to add additional data to span. Pass in a serialized
+  // span to avoid having to potentially serialize the span in every integration
+  // (for improved performance).
+  client.emit('processSpan', span, { readOnlySpan: spanToV2JSON(span) });
 
   // Wondering where we apply the beforeSendSpan callback?
   // We apply it directly before sending the span,
@@ -55,11 +61,15 @@ export function captureSpan(span: Span, client = getClient()): void {
   client.emit('enqueueSpan', span);
 }
 
-function applyScopeToSegmentSpan(segmentSpan: Span, scopeData: ScopeData, originalAttributeKeys: string[]): void {
+function applyScopeToSegmentSpan(
+  segmentSpan: Span,
+  scopeData: ScopeData,
+  originalAttributes: SerializedAttributes,
+): void {
   // TODO: Apply all scope data from auto instrumentation (contexts, request) to segment span
   const { attributes } = scopeData;
   if (attributes) {
-    setAttributesIfNotPresent(segmentSpan, originalAttributeKeys, attributes);
+    safeSetSpanAttributes(segmentSpan, attributes, originalAttributes);
   }
 }
 
@@ -68,28 +78,32 @@ function applyCommonSpanAttributes(
   serializedSegmentSpan: SpanV2JSON,
   client: Client,
   scopeData: ScopeData,
-  originalAttributeKeys: string[],
+  originalAttributes: SerializedAttributes,
 ): void {
   const sdk = client.getSdkMetadata();
   const { release, environment, sendDefaultPii } = client.getOptions();
 
   // avoid overwriting any previously set attributes (from users or potentially our SDK instrumentation)
-  setAttributesIfNotPresent(span, originalAttributeKeys, {
-    [SEMANTIC_ATTRIBUTE_SENTRY_RELEASE]: release,
-    [SEMANTIC_ATTRIBUTE_SENTRY_ENVIRONMENT]: environment,
-    [SEMANTIC_ATTRIBUTE_SENTRY_SEGMENT_NAME]: serializedSegmentSpan.name,
-    [SEMANTIC_ATTRIBUTE_SENTRY_SEGMENT_ID]: serializedSegmentSpan.span_id,
-    [SEMANTIC_ATTRIBUTE_SENTRY_SDK_NAME]: sdk?.sdk?.name,
-    [SEMANTIC_ATTRIBUTE_SENTRY_SDK_VERSION]: sdk?.sdk?.version,
-    ...(sendDefaultPii
-      ? {
-          [SEMANTIC_ATTRIBUTE_USER_ID]: scopeData.user?.id,
-          [SEMANTIC_ATTRIBUTE_USER_EMAIL]: scopeData.user?.email,
-          [SEMANTIC_ATTRIBUTE_USER_IP_ADDRESS]: scopeData.user?.ip_address ?? undefined,
-          [SEMANTIC_ATTRIBUTE_USER_USERNAME]: scopeData.user?.username,
-        }
-      : {}),
-  });
+  safeSetSpanAttributes(
+    span,
+    {
+      [SEMANTIC_ATTRIBUTE_SENTRY_RELEASE]: release,
+      [SEMANTIC_ATTRIBUTE_SENTRY_ENVIRONMENT]: environment,
+      [SEMANTIC_ATTRIBUTE_SENTRY_SEGMENT_NAME]: serializedSegmentSpan.name,
+      [SEMANTIC_ATTRIBUTE_SENTRY_SEGMENT_ID]: serializedSegmentSpan.span_id,
+      [SEMANTIC_ATTRIBUTE_SENTRY_SDK_NAME]: sdk?.sdk?.name,
+      [SEMANTIC_ATTRIBUTE_SENTRY_SDK_VERSION]: sdk?.sdk?.version,
+      ...(sendDefaultPii
+        ? {
+            [SEMANTIC_ATTRIBUTE_USER_ID]: scopeData.user?.id,
+            [SEMANTIC_ATTRIBUTE_USER_EMAIL]: scopeData.user?.email,
+            [SEMANTIC_ATTRIBUTE_USER_IP_ADDRESS]: scopeData.user?.ip_address ?? undefined,
+            [SEMANTIC_ATTRIBUTE_USER_USERNAME]: scopeData.user?.username,
+          }
+        : {}),
+    },
+    originalAttributes,
+  );
 }
 
 // TODO: Extract this to a helper in core. It's used in multiple places.
@@ -102,36 +116,4 @@ function getFinalScopeData(isolationScope: Scope | undefined, scope: Scope | und
     mergeScopeData(finalScopeData, scope.getScopeData());
   }
   return finalScopeData;
-}
-
-function setAttributesIfNotPresent(
-  span: Span,
-  originalAttributeKeys: string[],
-  newAttributes: RawAttributes<Record<string, unknown>>,
-): void {
-  Object.keys(newAttributes).forEach(key => {
-    if (!originalAttributeKeys.includes(key)) {
-      setAttributeOnSpanWithMaybeUnit(span, key, newAttributes[key]);
-    }
-  });
-}
-
-function setAttributeOnSpanWithMaybeUnit(span: Span, attributeKey: string, attributeValue: unknown): void {
-  if (isAttributeObject(attributeValue)) {
-    const { value, unit } = attributeValue;
-
-    if (isSupportedAttributeType(value)) {
-      span.setAttribute(attributeKey, value);
-    }
-
-    if (unit) {
-      span.setAttribute(`${attributeKey}.unit`, unit);
-    }
-  } else if (isSupportedAttributeType(attributeValue)) {
-    span.setAttribute(attributeKey, attributeValue);
-  }
-}
-
-function isSupportedAttributeType(value: unknown): value is Parameters<Span['setAttribute']>[1] {
-  return ['string', 'number', 'boolean'].includes(typeof value) || Array.isArray(value);
 }
