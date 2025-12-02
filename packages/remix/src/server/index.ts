0c8a7870efc4ae822575217a7cf756313d545138
JPeer264 <jan.peer@sentry.io>
2025-12-02 13:44:13 +0100

    feat: add info why barrel files need eslint disable

--- packages/remix/src/server/index.ts
+++ packages/remix/src/server/index.ts
@@ -1,3 +1,5 @@
+// import/export got a false positive, and affects most of our index barrel files
+// can be removed once following issue is fixed: https://github.com/import-js/eslint-plugin-import/issues/703
 /* eslint-disable import/export */
 // We need to explicitly export @sentry/node as they end up under `default` in ESM builds
 // See: https://github.com/getsentry/sentry-javascript/issues/8474
--- packages/solidstart/src/solidrouter.client.ts
+++ packages/solidstart/src/solidrouter.client.ts
@@ -1,2 +1,4 @@
+// import/export got a false positive, and affects most of our index barrel files
+// can be removed once following issue is fixed: https://github.com/import-js/eslint-plugin-import/issues/703
 /* eslint-disable import/export */
 export * from './client/solidrouter';
