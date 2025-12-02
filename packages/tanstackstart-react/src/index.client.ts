2cdbe7c721f9517d8dcfae139743d8ba5ab54ea0
JPeer264 <jan.peer@sentry.io>
2025-12-02 13:51:16 +0100

    feat: update files after rebase

--- packages/tanstackstart-react/src/index.client.ts
+++ packages/tanstackstart-react/src/index.client.ts
@@ -1,4 +1,6 @@
 // TODO: For now these are empty re-exports, but we may add actual implementations here
 // so we keep this to be future proof
 export * from './client';
+// nothing gets exported yet from there
+// eslint-disable-next-line import/export
 export * from './common';
