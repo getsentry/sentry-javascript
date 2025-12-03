e27cb507e81ceb725275441f1d9b3c0b9d3c27ed
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-12-03 17:58:49 +0100

    ref: don't relax it too much

--- dev-packages/e2e-tests/test-applications/nuxt-3/tests/middleware.test.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-3/tests/middleware.test.ts
@@ -125,7 +125,7 @@ test.describe('Server Middleware Instrumentation', () => {
           // This is a timing problem, sometimes Nitro can capture the error first, and sometimes it can't
           // If nitro captures the error first, the type will be 'chained'
           // If Sentry captures the error first, the type will be 'auto.middleware.nuxt'
-          // type: 'auto.middleware.nuxt',
+          type: expect.stringMatching(/^(auto\.middleware\.nuxt|chained)$/),
         }),
       }),
     );
