d4b97f470714beb8319be5f310f6a2345d8c5cb5
Nicolas Hrubec <nico.hrubec@sentry.io>
2025-12-03 08:14:51 +0100

    yarn fix

--- dev-packages/e2e-tests/test-applications/tanstackstart-react/src/routes/api.error.ts
+++ dev-packages/e2e-tests/test-applications/tanstackstart-react/src/routes/api.error.ts
@@ -1,18 +1,18 @@
 import * as Sentry from '@sentry/tanstackstart-react';
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/api/error')({
-    server: {
-        handlers: {
-            GET: async () => {
-                // This will throw a server-side error that Sentry should catch
-                try {
-                    throw new Error('Sentry API Route Test Error');
-                } catch (error) {
-                    Sentry.captureException(error);
-                    throw error;
-                }
-            },
-        },
+  server: {
+    handlers: {
+      GET: async () => {
+        // This will throw a server-side error that Sentry should catch
+        try {
+          throw new Error('Sentry API Route Test Error');
+        } catch (error) {
+          Sentry.captureException(error);
+          throw error;
+        }
+      },
     },
+  },
 });
