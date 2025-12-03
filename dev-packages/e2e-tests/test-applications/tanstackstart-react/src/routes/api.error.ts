b8f16a39843a2261a558ad9a3f3a5dd9a49482ee
Nicolas Hrubec <nico.hrubec@sentry.io>
2025-12-03 08:17:26 +0100

    remove comment

--- dev-packages/e2e-tests/test-applications/tanstackstart-react/src/routes/api.error.ts
+++ dev-packages/e2e-tests/test-applications/tanstackstart-react/src/routes/api.error.ts
@@ -5,7 +5,6 @@ export const Route = createFileRoute('/api/error')({
   server: {
     handlers: {
       GET: async () => {
-        // This will throw a server-side error that Sentry should catch
         try {
           throw new Error('Sentry API Route Test Error');
         } catch (error) {
