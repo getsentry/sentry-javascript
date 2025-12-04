571a9614b7754be971c9e25f29079a5ebeebda5c
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-04 16:36:41 +0100

    initial StreamingSpanExporter implementation for otel

--- packages/opentelemetry/src/spanExporter.ts
+++ packages/opentelemetry/src/spanExporter.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-lines */
 import type { Span } from '@opentelemetry/api';
 import { SpanKind } from '@opentelemetry/api';
-import type { ReadableSpan } from '@opentelemetry/sdk-trace-base';
+import type { ReadableSpan, SpanExporter } from '@opentelemetry/sdk-trace-base';
 import { ATTR_HTTP_RESPONSE_STATUS_CODE, SEMATTRS_HTTP_STATUS_CODE } from '@opentelemetry/semantic-conventions';
 import type {
   SpanAttributes,
@@ -46,10 +46,16 @@ interface FinishedSpanBucket {
   spans: Set<ReadableSpan>;
 }
 
+export interface ISentrySpanExporter {
+  export(span: ReadableSpan): void;
+  flush(): void;
+  clear(): void;
+}
+
 /**
  * A Sentry-specific exporter that converts OpenTelemetry Spans to Sentry Spans & Transactions.
  */
-export class SentrySpanExporter {
+export class SentrySpanExporter implements ISentrySpanExporter {
   /*
    * A quick explanation on the buckets: We do bucketing of finished spans for efficiency. This span exporter is
    * accumulating spans until a root span is encountered and then it flushes all the spans that are descendants of that
