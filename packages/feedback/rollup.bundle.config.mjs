37fdbd384a5a66a01b7582bcce28a5fa9eae77d8
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-12-04 20:12:42 +0100

    fix: alias preact for feedback bundling

--- packages/feedback/rollup.bundle.config.mjs
+++ packages/feedback/rollup.bundle.config.mjs
@@ -1,5 +1,15 @@
 import { makeBaseBundleConfig, makeBundleConfigVariants } from '@sentry-internal/rollup-utils';
 
+// Alias react to preact/compat since this package uses Preact for rendering
+const preactAlias = {
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react/jsx-runtime': 'preact/jsx-runtime',
+    },
+  },
+};
+
 export default [
   // The core `feedback` bundle is built in the browser package
   // Sub-bundles are built here
@@ -10,6 +20,7 @@ export default [
       jsVersion: 'es6',
       licenseTitle: '@sentry-internal/feedback',
       outputFileBase: () => 'bundles/feedback-screenshot',
+      packageSpecificConfig: preactAlias,
     }),
   ),
   ...makeBundleConfigVariants(
@@ -19,6 +30,7 @@ export default [
       jsVersion: 'es6',
       licenseTitle: '@sentry-internal/feedback',
       outputFileBase: () => 'bundles/feedback-modal',
+      packageSpecificConfig: preactAlias,
     }),
   ),
 ];
