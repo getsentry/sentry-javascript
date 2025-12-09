77be21301e70e315b3b0101f001c754eb78339d2
Burak Yigit Kaya <byk@sentry.io>
2025-12-09 23:47:27 +0000

    bump nextjs size

--- .size-limit.js
+++ .size-limit.js
@@ -213,7 +213,7 @@ module.exports = [
     import: createImport('init'),
     ignore: ['next/router', 'next/constants'],
     gzip: true,
-    limit: '46 KB',
+    limit: '47 KB',
   },
   // SvelteKit SDK (ESM)
   {
