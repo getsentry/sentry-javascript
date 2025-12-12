c6751b3588b005ebb15c377e2b1eb1c3b509b59c
Charly Gomez <charly.gomez@sentry.io>
2025-12-12 15:47:36 +0100

    lint

--- packages/nextjs/src/server/index.ts
+++ packages/nextjs/src/server/index.ts
@@ -1,7 +1,6 @@
 // import/export got a false positive, and affects most of our index barrel files
 // can be removed once following issue is fixed: https://github.com/import-js/eslint-plugin-import/issues/703
 /* eslint-disable import/export */
-import { context } from '@opentelemetry/api';
 import {
   ATTR_HTTP_ROUTE,
   ATTR_URL_QUERY,
