f3fbcd902a9f6e1b04b174afd95d284ce063d4c3
Sigrid <32902192+s1gr1d@users.noreply.github.com>
2025-12-17 11:03:41 +0100

    feat(replay): Add Request body with `attachRawBodyFromRequest` option (#18501)
    Previously, we only got the body from the `fetch` options. However, some
    people use the `Request` object which can also contain a body. As this
    body is a `ReadableStream`, we could only read that async.
    
    With `attachRawBodyFromRequest`, the `Request` object is patched to
    include the raw body as a `Symbol` so it can be read synchronously.
    
    Linear ticket (internal):
    https://linear.app/getsentry/issue/JS-1255/investigate-sdk-issues-causing-all-requests-to-default-to-get

---
+++ dev-packages/browser-integration-tests/suites/replay/attachRawBodyFromRequest/init.js
@@ -0,0 +1,22 @@
+import * as Sentry from '@sentry/browser';
+
+window.Sentry = Sentry;
+window.Replay = Sentry.replayIntegration({
+  flushMinDelay: 200,
+  flushMaxDelay: 200,
+  minReplayDuration: 0,
+
+  networkDetailAllowUrls: ['http://sentry-test.io/foo'],
+  networkCaptureBodies: true,
+  attachRawBodyFromRequest: true,
+});
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  sampleRate: 1,
+  // We ensure to sample for errors, so by default nothing is sent
+  replaysSessionSampleRate: 0.0,
+  replaysOnErrorSampleRate: 1.0,
+
+  integrations: [window.Replay],
+});
