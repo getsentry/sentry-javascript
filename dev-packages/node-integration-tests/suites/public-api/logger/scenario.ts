b70a54604512350b59c7cdb95beddb96c42c6a95
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-17 18:23:37 +0100

    possibly fix logs integration test for 3.8

--- dev-packages/node-integration-tests/suites/public-api/logger/scenario.ts
+++ dev-packages/node-integration-tests/suites/public-api/logger/scenario.ts
@@ -11,7 +11,7 @@ Sentry.init({
 
 async function run(): Promise<void> {
   // only log attribute
-  Sentry.logger.info('log_before_any_scope', { log_attr: 'log_attr_1' });
+  Sentry.logger.info('log_before_any_scope', { log_attr: 'log_attr_1' }, {});
 
   Sentry.getGlobalScope().setAttribute('global_scope_attr', true);
 
@@ -19,26 +19,26 @@ async function run(): Promise<void> {
   Sentry.getGlobalScope().setAttributes({ array_attr: [1, 2, 3] });
 
   // global scope, log attribute
-  Sentry.logger.info('log_after_global_scope', { log_attr: 'log_attr_2' });
+  Sentry.logger.info('log_after_global_scope', { log_attr: 'log_attr_2' }, {});
 
   Sentry.withIsolationScope(isolationScope => {
     isolationScope.setAttribute('isolation_scope_1_attr', { value: 100, unit: 'millisecond' });
 
     // global scope, isolation scope, log attribute
-    Sentry.logger.info('log_with_isolation_scope', { log_attr: 'log_attr_3' });
+    Sentry.logger.info('log_with_isolation_scope', { log_attr: 'log_attr_3' }, {});
 
     Sentry.withScope(scope => {
       scope.setAttribute('scope_attr', { value: 200, unit: 'millisecond' });
 
       // global scope, isolation scope, current scope attribute, log attribute
-      Sentry.logger.info('log_with_scope', { log_attr: 'log_attr_4' });
+      Sentry.logger.info('log_with_scope', { log_attr: 'log_attr_4' }, {});
     });
 
     Sentry.withScope(scope2 => {
       scope2.setAttribute('scope_2_attr', { value: 300, unit: 'millisecond' });
 
       // global scope, isolation scope, current scope attribute, log attribute
-      Sentry.logger.info('log_with_scope_2', { log_attr: 'log_attr_5' });
+      Sentry.logger.info('log_with_scope_2', { log_attr: 'log_attr_5' }, {});
     });
   });
 
