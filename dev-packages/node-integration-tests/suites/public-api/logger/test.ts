438df0fdc865388145a20a665cee266aa80cab6d
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-17 15:55:48 +0100

    node integration tests assert on equals, not objectContaining

--- dev-packages/node-integration-tests/suites/public-api/logger/test.ts
+++ dev-packages/node-integration-tests/suites/public-api/logger/test.ts
@@ -33,73 +33,75 @@ describe('logs', () => {
   test('captures logs with scope and log attributes', async () => {
     const runner = createRunner(__dirname, 'scenario.ts')
       .expect({
-        log: {
-          items: [
-            {
-              timestamp: expect.any(Number),
-              level: 'info',
-              body: 'log_before_any_scope',
-              severity_number: 9,
-              trace_id: expect.any(String),
-              attributes: {
-                ...commonAttributes,
-                log_attr: { value: 'log_attr_1', type: 'string' },
+        log: logsContainer => {
+          expect(logsContainer).toEqual({
+            items: [
+              {
+                timestamp: expect.any(Number),
+                level: 'info',
+                body: 'log_before_any_scope',
+                severity_number: 9,
+                trace_id: expect.any(String),
+                attributes: {
+                  ...commonAttributes,
+                  log_attr: { value: 'log_attr_1', type: 'string' },
+                },
               },
-            },
-            {
-              timestamp: expect.any(Number),
-              level: 'info',
-              body: 'log_after_global_scope',
-              severity_number: 9,
-              trace_id: expect.any(String),
-              attributes: {
-                ...commonAttributes,
-                global_scope_attr: { value: true, type: 'boolean' },
-                log_attr: { value: 'log_attr_2', type: 'string' },
+              {
+                timestamp: expect.any(Number),
+                level: 'info',
+                body: 'log_after_global_scope',
+                severity_number: 9,
+                trace_id: expect.any(String),
+                attributes: {
+                  ...commonAttributes,
+                  global_scope_attr: { value: true, type: 'boolean' },
+                  log_attr: { value: 'log_attr_2', type: 'string' },
+                },
               },
-            },
-            {
-              timestamp: expect.any(Number),
-              level: 'info',
-              body: 'log_with_isolation_scope',
-              severity_number: 9,
-              trace_id: expect.any(String),
-              attributes: {
-                ...commonAttributes,
-                global_scope_attr: { value: true, type: 'boolean' },
-                isolation_scope_1_attr: { value: 100, unit: 'millisecond', type: 'integer' },
-                log_attr: { value: 'log_attr_3', type: 'string' },
+              {
+                timestamp: expect.any(Number),
+                level: 'info',
+                body: 'log_with_isolation_scope',
+                severity_number: 9,
+                trace_id: expect.any(String),
+                attributes: {
+                  ...commonAttributes,
+                  global_scope_attr: { value: true, type: 'boolean' },
+                  isolation_scope_1_attr: { value: 100, unit: 'millisecond', type: 'integer' },
+                  log_attr: { value: 'log_attr_3', type: 'string' },
+                },
               },
-            },
-            {
-              timestamp: expect.any(Number),
-              level: 'info',
-              body: 'log_with_scope',
-              severity_number: 9,
-              trace_id: expect.any(String),
-              attributes: {
-                ...commonAttributes,
-                global_scope_attr: { value: true, type: 'boolean' },
-                isolation_scope_1_attr: { value: 100, unit: 'millisecond', type: 'integer' },
-                scope_attr: { value: 200, unit: 'millisecond', type: 'integer' },
-                log_attr: { value: 'log_attr_4', type: 'string' },
+              {
+                timestamp: expect.any(Number),
+                level: 'info',
+                body: 'log_with_scope',
+                severity_number: 9,
+                trace_id: expect.any(String),
+                attributes: {
+                  ...commonAttributes,
+                  global_scope_attr: { value: true, type: 'boolean' },
+                  isolation_scope_1_attr: { value: 100, unit: 'millisecond', type: 'integer' },
+                  scope_attr: { value: 200, unit: 'millisecond', type: 'integer' },
+                  log_attr: { value: 'log_attr_4', type: 'string' },
+                },
               },
-            },
-            {
-              timestamp: expect.any(Number),
-              level: 'info',
-              body: 'log_with_scope_2',
-              severity_number: 9,
-              trace_id: expect.any(String),
-              attributes: {
-                ...commonAttributes,
-                global_scope_attr: { value: true, type: 'boolean' },
-                isolation_scope_1_attr: { value: 100, unit: 'millisecond', type: 'integer' },
-                scope_2_attr: { value: 300, unit: 'millisecond', type: 'integer' },
-                log_attr: { value: 'log_attr_5', type: 'string' },
+              {
+                timestamp: expect.any(Number),
+                level: 'info',
+                body: 'log_with_scope_2',
+                severity_number: 9,
+                trace_id: expect.any(String),
+                attributes: {
+                  ...commonAttributes,
+                  global_scope_attr: { value: true, type: 'boolean' },
+                  isolation_scope_1_attr: { value: 100, unit: 'millisecond', type: 'integer' },
+                  scope_2_attr: { value: 300, unit: 'millisecond', type: 'integer' },
+                  log_attr: { value: 'log_attr_5', type: 'string' },
+                },
               },
-            },
-          ],
+            ],
+          });
         },
       })
       .start();
