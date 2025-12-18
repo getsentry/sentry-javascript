acb01af460afbe1d7ff5629045290aa4578ccf0d
Jan Peer Stöcklmair <jan.peer@sentry.io>
2025-12-18 10:54:47 +0100

    Update dev-packages/browser-integration-tests/suites/errors/fetch-enhance-messages-report-only/test.ts
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>

--- dev-packages/browser-integration-tests/suites/errors/fetch-enhance-messages-report-only/test.ts
+++ dev-packages/browser-integration-tests/suites/errors/fetch-enhance-messages-report-only/test.ts
@@ -3,7 +3,7 @@ import { sentryTest } from '../../../utils/fixtures';
 import { envelopeRequestParser, waitForErrorRequest } from '../../../utils/helpers';
 
 sentryTest(
-  'enhanceFetchErrorMessages: report-only: enhances error for Sentry while preserving original @firefox',
+  'enhanceFetchErrorMessages: report-only: enhances error for Sentry while preserving original',
   async ({ getLocalTestUrl, page, browserName }) => {
     const url = await getLocalTestUrl({ testDir: __dirname });
     const reqPromise = waitForErrorRequest(page);
