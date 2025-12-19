827a54b490a4004e06f8bd4dba56edbdda8d11f0
Lukas Stracke <lukas.stracke@sentry.io>
2025-12-19 09:26:49 +0100

    chore(size-limit): Add size checks for metrics and logs

--- .size-limit.js
+++ .size-limit.js
@@ -112,6 +112,27 @@ module.exports = [
     gzip: true,
     limit: '35 KB',
   },
+  {
+    name: '@sentry/browser (incl. Metrics)',
+    path: 'packages/browser/build/npm/esm/prod/index.js',
+    import: createImport('init', 'metrics', 'logger'),
+    gzip: true,
+    limit: '28 KB',
+  },
+  {
+    name: '@sentry/browser (incl. Logs)',
+    path: 'packages/browser/build/npm/esm/prod/index.js',
+    import: createImport('init', 'metrics', 'logger'),
+    gzip: true,
+    limit: '28 KB',
+  },
+  {
+    name: '@sentry/browser (incl. Metrics & Logs)',
+    path: 'packages/browser/build/npm/esm/prod/index.js',
+    import: createImport('init', 'metrics', 'logger'),
+    gzip: true,
+    limit: '30 KB',
+  },
   // React SDK (ESM)
   {
     name: '@sentry/react',
