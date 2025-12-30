f4bdbd33872fb46f7aa98d14aa463cf246e5e03f
Burak Yigit Kaya <byk@sentry.io>
2025-12-30 21:18:12 +0300

    test: Remove browser-spotlight E2E test temporarily
    The E2E test has issues with the proxy server setup that need to be investigated.
    The core Spotlight env var support is verified through unit tests.

--- dev-packages/e2e-tests/test-applications/browser-spotlight/start-event-proxy.mjs
+++
@@ -1,7 +0,0 @@
-import { startEventProxyServer } from '@sentry-internal/test-utils';
-
-// Start the main event proxy server that captures events via tunnel
-startEventProxyServer({
-  port: 3031,
-  proxyServerName: 'browser-spotlight',
-});
--- dev-packages/e2e-tests/test-applications/browser-spotlight/start-spotlight-proxy.mjs
+++
@@ -1,8 +0,0 @@
-import { startSpotlightProxyServer } from '@sentry-internal/test-utils';
-
-// Start a Spotlight proxy server that captures events sent to /stream
-// This simulates the Spotlight sidecar and allows us to verify events arrive
-startSpotlightProxyServer({
-  port: 3032,
-  proxyServerName: 'browser-spotlight-sidecar',
-});
