42b310669d43e6f274f7f2505f85de7fb0ffcb88
s1gr1d <32902192+s1gr1d@users.noreply.github.com>
2025-12-30 14:03:50 +0100

    specific request and response handlers

--- packages/hono/src/middleware/middleware.cloudflare.ts
+++ packages/hono/src/middleware/middleware.cloudflare.ts
@@ -1,66 +1,24 @@
 import { withSentry } from '@sentry/cloudflare';
-import {
-  type BaseTransportOptions,
-  continueTrace,
-  debug,
-  getActiveSpan,
-  getClient,
-  getRootSpan,
-  type Options,
-  updateSpanName,
-  winterCGRequestToRequestData,
-  withIsolationScope,
-} from '@sentry/core';
+import { type BaseTransportOptions, debug, type Options } from '@sentry/core';
 import type { Context, Hono, MiddlewareHandler } from 'hono';
-import { routePath } from 'hono/route';
-import { hasFetchEvent } from '../utils/hono-context';
+import { requestHandler, responseHandler } from '../shared/middlewareHandlers';
 
 export interface HonoOptions extends Options<BaseTransportOptions> {
   context?: Context;
 }
 
-export const sentry = (
-  app: Hono,
-  options: HonoOptions | undefined = {},
-  // callback?: (sentry: Toucan) => void,
-): MiddlewareHandler => {
+export const sentry = (app: Hono, options: HonoOptions | undefined = {}): MiddlewareHandler => {
   const isDebug = options.debug;
-  isDebug && debug.log('- - - - - - - - call sentry middleware - - - - - - - - - -');
+
+  isDebug && debug.log('Initialized Sentry Hono middleware (Cloudflare)');
 
   withSentry(() => options, app);
 
   return async (context, next) => {
-    isDebug && debug.log('- - - - - - - - - - -  new request - - - - - - - - - - -');
+    requestHandler(context);
 
-    return withIsolationScope(async isolationScope =>
-      continueTrace(
-        {
-          sentryTrace: context.req.raw.headers.get('sentry-trace') ?? '',
-          baggage: context.req.raw.headers.get('baggage'),
-        },
-        async () => {
-          isolationScope.setSDKProcessingMetadata({
-            normalizedRequest: winterCGRequestToRequestData(
-              hasFetchEvent(context) ? context.event.request : context.req.raw,
-            ),
-          });
+    await next(); // Handler runs in between Request above ⤴ and Response below ⤵
 
-          await next(); // Handler runs in between. Before is Request ⤴ and afterward is Response ⤵
-
-          const activeSpan = getActiveSpan();
-          if (activeSpan) {
-            activeSpan.updateName(`${context.req.method} ${routePath(context)}`);
-            updateSpanName(getRootSpan(activeSpan), `${context.req.method} ${routePath(context)}`);
-          }
-
-          isolationScope.setTransactionName(`${context.req.method} ${routePath(context)}`);
-
-          if (context.error) {
-            isDebug && debug.log('captureException...');
-            getClient()?.captureException(context.error);
-          }
-        },
-      ),
-    );
+    responseHandler(context);
   };
 };
---
+++ packages/hono/src/shared/middlewareHandlers.ts
@@ -0,0 +1,43 @@
+import { getIsolationScope } from '@sentry/cloudflare';
+import {
+  getActiveSpan,
+  getClient,
+  getDefaultIsolationScope,
+  getRootSpan,
+  updateSpanName,
+  winterCGRequestToRequestData,
+} from '@sentry/core';
+import type { Context } from 'hono';
+import { routePath } from 'hono/dist/types/helper/route/index';
+import { hasFetchEvent } from '../utils/hono-context';
+
+/**
+ *
+ */
+export function requestHandler(context: Context): void {
+  const defaultScope = getDefaultIsolationScope();
+  const currentIsolationScope = getIsolationScope();
+
+  const isolationScope = defaultScope === currentIsolationScope ? defaultScope : currentIsolationScope;
+
+  isolationScope.setSDKProcessingMetadata({
+    normalizedRequest: winterCGRequestToRequestData(hasFetchEvent(context) ? context.event.request : context.req.raw),
+  });
+}
+
+/**
+ *
+ */
+export function responseHandler(context: Context): void {
+  const activeSpan = getActiveSpan();
+  if (activeSpan) {
+    activeSpan.updateName(`${context.req.method} ${routePath(context)}`);
+    updateSpanName(getRootSpan(activeSpan), `${context.req.method} ${routePath(context)}`);
+  }
+
+  getIsolationScope().setTransactionName(`${context.req.method} ${routePath(context)}`);
+
+  if (context.error) {
+    getClient()?.captureException(context.error);
+  }
+}
