d718dfdbbeb31b75245555b88bc60af95adc473f
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-12-31 13:45:25 +0200

    feat: init nitro SDK

---
+++ packages/nitro/src/module.ts
@@ -0,0 +1,13 @@
+import type { NitroModule } from 'nitro/types';
+
+/**
+ * Creates a Nitro module to setup the Sentry SDK.
+ */
+export function createNitroModule(): NitroModule {
+  return {
+    name: 'sentry',
+    setup: nitro => {
+      // TODO: Setup the Sentry SDK.
+    },
+  };
+}
