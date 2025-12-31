cd76308da6409095d1d29cec253325f8c04b7bf0
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2025-12-31 13:45:25 +0200

    feat: init nitro SDK

---
+++ packages/nitro/src/config.ts
@@ -0,0 +1,32 @@
+import type { NitroConfig } from 'nitro/types';
+import { createNitroModule } from './module';
+
+type SentryNitroOptions = {
+  // TODO: Add options
+};
+
+/**
+ * Modifies the passed in Nitro configuration with automatic build-time instrumentation.
+ *
+ * @param config A Nitro configuration object, as usually exported in `nitro.config.ts` or `nitro.config.mjs`.
+ * @returns The modified config to be exported
+ */
+export function withSentryConfig(config: NitroConfig, moduleOptions?: SentryNitroOptions): NitroConfig {
+  setupSentryNitroModule(config, moduleOptions);
+
+  return config;
+}
+
+/**
+ * Sets up the Sentry Nitro module, useful for meta framework integrations.
+ */
+export function setupSentryNitroModule(
+  config: NitroConfig,
+  moduleOptions?: SentryNitroOptions,
+  serverConfigFile?: string,
+): NitroConfig {
+  config.modules = config.modules || [];
+  config.modules.push(createNitroModule());
+
+  return config;
+}
