538066abb3d92e2922eaf72e410848dc728ff22b
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-01-02 16:44:18 +0100

    Use proxy

--- packages/tanstackstart-react/src/server/middleware.ts
+++ packages/tanstackstart-react/src/server/middleware.ts
@@ -3,11 +3,7 @@ import { SEMANTIC_ATTRIBUTE_SENTRY_OP, SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN, startSp
 const SENTRY_WRAPPED = Symbol.for('sentry.middleware.wrapped');
 
 type TanStackMiddleware = {
-  options: { type: string; server: (...args: unknown[]) => unknown };
-  middleware: (...args: unknown[]) => unknown;
-  inputValidator: (...args: unknown[]) => unknown;
-  client: (...args: unknown[]) => unknown;
-  server: (...args: unknown[]) => unknown;
+  options: { server: (...args: unknown[]) => unknown };
 };
 
 type MiddlewareWrapperOptions = {
@@ -39,41 +35,28 @@ export function wrapMiddlewareWithSentry<T extends TanStackMiddleware>(
   middleware: T,
   options: MiddlewareWrapperOptions,
 ): T {
-  // Check for double-wrapping
-  if ((middleware as unknown as Record<symbol, unknown>)[SENTRY_WRAPPED]) {
+  if ((middleware as Record<symbol, unknown>)[SENTRY_WRAPPED]) {
     return middleware;
   }
 
-  const originalServerFn = middleware.options.server;
-
-  // Wrap the options.server function (this is what TanStack Start actually calls)
-  const wrappedServerFn = function (this: unknown, ...args: unknown[]): unknown {
-    return startSpan(
-      {
-        op: 'middleware.tanstackstart',
-        name: options.name,
-        attributes: {
-          [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'manual.middleware.tanstackstart',
-          [SEMANTIC_ATTRIBUTE_SENTRY_OP]: 'middleware.tanstackstart',
+  middleware.options.server = new Proxy(middleware.options.server, {
+    apply: (target, thisArg, args) => {
+      return startSpan(
+        {
+          op: 'middleware.tanstackstart',
+          name: options.name,
+          attributes: {
+            [SEMANTIC_ATTRIBUTE_SENTRY_ORIGIN]: 'manual.middleware.tanstackstart',
+            [SEMANTIC_ATTRIBUTE_SENTRY_OP]: 'middleware.tanstackstart',
+          },
         },
-      },
-      () => originalServerFn.apply(this, args),
-    );
-  };
-
-  // Create a new middleware object with the wrapped server function
-  const wrappedMiddleware = {
-    ...middleware,
-    options: {
-      ...middleware.options,
-      server: wrappedServerFn,
+        () => target.apply(thisArg, args),
+      );
     },
-  };
+  });
 
-  // Mark the whole object as wrapped
-  Object.defineProperty(wrappedMiddleware, SENTRY_WRAPPED, { value: true });
-
-  return wrappedMiddleware as T;
+  Object.defineProperty(middleware, SENTRY_WRAPPED, { value: true });
+  return middleware;
 }
 
 /**
