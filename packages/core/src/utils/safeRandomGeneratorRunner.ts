708280b445640afdb7c6a1223461eda4748cc8b9
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-01-05 15:33:50 +0200

    feat: implement a safe random runner

---
+++ packages/core/src/utils/safeRandomGeneratorRunner.ts
@@ -0,0 +1,41 @@
+import { GLOBAL_OBJ } from './worldwide';
+
+type SafeRandomContextRunner = <T>(callback: () => T) => T;
+
+let RESOLVED_RUNNER: SafeRandomContextRunner | undefined;
+
+/**
+ * Simple wrapper that allows SDKs to *secretly* set context wrapper to generate safe random IDs in cache components contexts
+ */
+export function runInRandomSafeContext<T>(cb: () => T): T {
+  // Skips future symbol lookups if we've already resolved the runner once
+  if (RESOLVED_RUNNER) {
+    return RESOLVED_RUNNER(cb);
+  }
+
+  const sym = Symbol.for('__SENTRY_SAFE_RANDOM_ID_WRAPPER__');
+  const globalWithSymbol: typeof GLOBAL_OBJ & { [sym]?: SafeRandomContextRunner } = GLOBAL_OBJ;
+  if (!(sym in globalWithSymbol) || typeof globalWithSymbol[sym] !== 'function') {
+    return cb();
+  }
+
+  RESOLVED_RUNNER = globalWithSymbol[sym];
+
+  return globalWithSymbol[sym](cb);
+}
+
+/**
+ * Returns the current date and time wrapped in a safe context runner.
+ * @returns number The current date and time.
+ */
+export function safeDateNow(): number {
+  return runInRandomSafeContext(() => Date.now());
+}
+
+/**
+ * Returns a random number between 0 and 1 wrapped in a safe context runner.
+ * @returns number A random number between 0 and 1.
+ */
+export function safeMathRandom(): number {
+  return runInRandomSafeContext(() => Math.random());
+}
