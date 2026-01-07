a35268daa3cd530029f73d446bece4a16b4b6a09
s1gr1d <32902192+s1gr1d@users.noreply.github.com>
2026-01-07 14:56:23 +0100

    add tests for server-side; modify bash script

--- dev-packages/e2e-tests/test-applications/nuxt-4/tests/environment.test.ts
+++ dev-packages/e2e-tests/test-applications/nuxt-4/tests/environment.test.ts
@@ -3,7 +3,7 @@ import { waitForError, waitForTransaction } from '@sentry-internal/test-utils';
 import { isDevMode } from './isDevMode';
 
 test.describe('environment detection', async () => {
-  test('sets correct environment for application errors', async ({ page }) => {
+  test('sets correct environment for client-side errors', async ({ page }) => {
     const errorPromise = waitForError('nuxt-4', async errorEvent => {
       return errorEvent?.exception?.values?.[0]?.value === 'Error thrown from Nuxt-4 E2E test app';
     });
@@ -21,7 +21,7 @@ test.describe('environment detection', async () => {
     }
   });
 
-  test('sets correct environment for application transactions', async ({ page }) => {
+  test('sets correct environment for client-side transactions', async ({ page }) => {
     const transactionPromise = waitForTransaction('nuxt-4', async transactionEvent => {
       return transactionEvent.transaction === '/test-param/:param()';
     });
@@ -29,6 +29,44 @@ test.describe('environment detection', async () => {
     await page.goto(`/test-param/1234`);
 
     const transaction = await transactionPromise;
+
+    if (isDevMode) {
+      expect(transaction.environment).toBe('development');
+    } else {
+      expect(transaction.environment).toBe('production');
+    }
+  });
+
+  test('sets correct environment for server-side errors', async ({ page }) => {
+    const errorPromise = waitForError('nuxt-4', async errorEvent => {
+      return errorEvent?.exception?.values?.[0]?.value === 'Nuxt 4 Server error';
+    });
+
+    await page.goto(`/fetch-server-routes`, isDevMode ? { waitUntil: 'networkidle' } : {});
+    await page.getByText('Fetch Server API Error', { exact: true }).click();
+
+    const error = await errorPromise;
+
+    expect(error.transaction).toBe('GET /api/server-error');
+
+    if (isDevMode) {
+      expect(error.environment).toBe('development');
+    } else {
+      expect(error.environment).toBe('production');
+    }
+  });
+
+  test('sets correct environment for server-side transactions', async ({ page }) => {
+    const transactionPromise = waitForTransaction('nuxt-4', async transactionEvent => {
+      return transactionEvent.transaction === 'GET /api/nitro-fetch';
+    });
+
+    await page.goto(`/fetch-server-routes`, isDevMode ? { waitUntil: 'networkidle' } : {});
+    await page.getByText('Fetch Nitro $fetch', { exact: true }).click();
+
+    const transaction = await transactionPromise;
+
+    expect(transaction.contexts.trace.op).toBe('http.server');
 
     if (isDevMode) {
       expect(transaction.environment).toBe('development');
