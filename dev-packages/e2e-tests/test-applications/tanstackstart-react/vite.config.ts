53e9099a2947aece39dc368e1f39018fb63e825c
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-01-08 09:04:43 +0100

    add vite wrapper to e2e tests

--- dev-packages/e2e-tests/test-applications/tanstackstart-react/vite.config.ts
+++ dev-packages/e2e-tests/test-applications/tanstackstart-react/vite.config.ts
@@ -3,16 +3,27 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react-swc';
 import { nitro } from 'nitro/vite';
+import { wrapConfigWithSentry } from '@sentry/tanstackstart-react';
 
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    nitro(),
-    // react's vite plugin must come after start's vite plugin
-    viteReact(),
-  ],
-});
+export default defineConfig(
+  wrapConfigWithSentry(
+    {
+      server: {
+        port: 3000,
+      },
+      plugins: [
+        tsConfigPaths(),
+        tanstackStart(),
+        nitro(),
+        // react's vite plugin must come after start's vite plugin
+        viteReact(),
+      ],
+    },
+    {
+      org: process.env.E2E_TEST_SENTRY_ORG_SLUG,
+      project: process.env.E2E_TEST_SENTRY_PROJECT,
+      authToken: process.env.E2E_TEST_AUTH_TOKEN,
+      debug: true,
+    },
+  ),
+);
