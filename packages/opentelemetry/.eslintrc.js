2240f9d4033826a5df2654cc3d9fc9650d3ce9ce
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-01-08 18:47:03 +0200

    chore: bring back the eslint rule

--- packages/opentelemetry/.eslintrc.js
+++ packages/opentelemetry/.eslintrc.js
@@ -3,4 +3,15 @@ module.exports = {
     node: true,
   },
   extends: ['../../.eslintrc.js'],
+  rules: {
+    '@sentry-internal/sdk/no-unsafe-random-apis': 'error',
+  },
+  overrides: [
+    {
+      files: ['test/**/*.ts', 'test/**/*.tsx'],
+      rules: {
+        '@sentry-internal/sdk/no-unsafe-random-apis': 'off',
+      },
+    },
+  ],
 };
