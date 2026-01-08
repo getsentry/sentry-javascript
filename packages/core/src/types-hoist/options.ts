026bea91233c2ffced571ba2bd69cd6925aa4668
JPeer264 <jan.peer@sentry.io>
2026-01-08 11:53:06 +0100

    feat(core): Add more types to BaseWinterTCOptions

--- packages/core/src/types-hoist/options.ts
+++ packages/core/src/types-hoist/options.ts
@@ -1,3 +1,5 @@
+import type { Instrumentation } from '@opentelemetry/instrumentation';
+import type { SpanProcessor } from '@opentelemetry/sdk-trace-base';
 import type { CaptureContext } from '../scope';
 import type { Breadcrumb, BreadcrumbHint } from './breadcrumb';
 import type { ErrorEvent, EventHint, TransactionEvent } from './event';
@@ -65,6 +67,58 @@ export interface BaseWinterTCOptions {
    * * The `SentrySampler`
    */
   skipOpenTelemetrySetup?: boolean;
+
+  /**
+   * If set to `false`, the SDK will not automatically detect the `serverName`.
+   *
+   * This is useful if you are using the SDK in a CLI app or Electron where the
+   * hostname might be considered PII.
+   *
+   * @default true
+   */
+  includeServerName?: boolean;
+
+  /**
+   * By default, the SDK will try to identify problems with your instrumentation setup and warn you about it.
+   * If you want to disable these warnings, set this to `true`.
+   */
+  disableInstrumentationWarnings?: boolean;
+
+  /**
+   * Controls how many milliseconds to wait before shutting down. The default is 2 seconds. Setting this too low can cause
+   * problems for sending events from command line applications. Setting it too
+   * high can cause the application to block for users with network connectivity
+   * problems.
+   */
+  shutdownTimeout?: number;
+
+  /**
+   * Configures in which interval client reports will be flushed. Defaults to `60_000` (milliseconds).
+   */
+  clientReportFlushInterval?: number;
+
+  /**
+   * Provide an array of OpenTelemetry Instrumentations that should be registered.
+   *
+   * Use this option if you want to register OpenTelemetry instrumentation that the Sentry SDK does not yet have support for.
+   */
+  openTelemetryInstrumentations?: Instrumentation[];
+
+  /**
+   * Provide an array of additional OpenTelemetry SpanProcessors that should be registered.
+   */
+  openTelemetrySpanProcessors?: SpanProcessor[];
+
+  /**
+   * The max. duration in seconds that the SDK will wait for parent spans to be finished before discarding a span.
+   * The SDK will automatically clean up spans that have no finished parent after this duration.
+   * This is necessary to prevent memory leaks in case of parent spans that are never finished or otherwise dropped/missing.
+   * However, if you have very long-running spans in your application, a shorter duration might cause spans to be discarded too early.
+   * In this case, you can increase this duration to a value that fits your expected data.
+   *
+   * Defaults to 300 seconds (5 minutes).
+   */
+  maxSpanWaitDuration?: number;
 
   /**
    * Callback that is executed when a fatal global error occurs.
--- packages/node-core/src/types.ts
+++ packages/node-core/src/types.ts
@@ -1,6 +1,5 @@
 import type { Span as WriteableSpan } from '@opentelemetry/api';
-import type { Instrumentation } from '@opentelemetry/instrumentation';
-import type { ReadableSpan, SpanProcessor } from '@opentelemetry/sdk-trace-base';
+import type { ReadableSpan } from '@opentelemetry/sdk-trace-base';
 import type { BaseWinterTCOptions, ClientOptions, Options, SamplingContext, Scope, Span } from '@sentry/core';
 import type { NodeTransportOptions } from './transports';
 
@@ -48,44 +47,11 @@ export interface BaseNodeOptions extends BaseWinterTCOptions {
   profileLifecycle?: 'manual' | 'trace';
 
   /**
-   * If set to `false`, the SDK will not automatically detect the `serverName`.
-   *
-   * This is useful if you are using the SDK in a CLI app or Electron where the
-   * hostname might be considered PII.
-   *
-   * @default true
-   */
-  includeServerName?: boolean;
-
-  /**
    * Include local variables with stack traces.
    *
    * Requires the `LocalVariables` integration.
    */
   includeLocalVariables?: boolean;
-
-  /**
-   * Provide an array of OpenTelemetry Instrumentations that should be registered.
-   *
-   * Use this option if you want to register OpenTelemetry instrumentation that the Sentry SDK does not yet have support for.
-   */
-  openTelemetryInstrumentations?: Instrumentation[];
-
-  /**
-   * Provide an array of additional OpenTelemetry SpanProcessors that should be registered.
-   */
-  openTelemetrySpanProcessors?: SpanProcessor[];
-
-  /**
-   * The max. duration in seconds that the SDK will wait for parent spans to be finished before discarding a span.
-   * The SDK will automatically clean up spans that have no finished parent after this duration.
-   * This is necessary to prevent memory leaks in case of parent spans that are never finished or otherwise dropped/missing.
-   * However, if you have very long-running spans in your application, a shorter duration might cause spans to be discarded too early.
-   * In this case, you can increase this duration to a value that fits your expected data.
-   *
-   * Defaults to 300 seconds (5 minutes).
-   */
-  maxSpanWaitDuration?: number;
 
   /**
    * Whether to register ESM loader hooks to automatically instrument libraries.
@@ -96,25 +62,6 @@ export interface BaseNodeOptions extends BaseWinterTCOptions {
    * Defaults to `true`.
    */
   registerEsmLoaderHooks?: boolean;
-
-  /**
-   * Configures in which interval client reports will be flushed. Defaults to `60_000` (milliseconds).
-   */
-  clientReportFlushInterval?: number;
-
-  /**
-   * By default, the SDK will try to identify problems with your instrumentation setup and warn you about it.
-   * If you want to disable these warnings, set this to `true`.
-   */
-  disableInstrumentationWarnings?: boolean;
-
-  /**
-   * Controls how many milliseconds to wait before shutting down. The default is 2 seconds. Setting this too low can cause
-   * problems for sending events from command line applications. Setting it too
-   * high can cause the application to block for users with network connectivity
-   * problems.
-   */
-  shutdownTimeout?: number;
 }
 
 /**
