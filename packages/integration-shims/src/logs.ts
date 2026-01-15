a531f551f977f69b74861f6a6e026f9e0ebc1738
Charly Gomez <charly.gomez@sentry.io>
2026-01-15 16:48:05 +0100

    add shim package for logs

---
+++ packages/integration-shims/src/logs.ts
@@ -0,0 +1,55 @@
+import type { Integration, ParameterizedString } from '@sentry/core';
+import { consoleSandbox, defineIntegration } from '@sentry/core';
+import { FAKE_FUNCTION } from './common';
+
+/**
+ * This is a shim for the logger namespace.
+ * It is needed in order for the CDN bundles to continue working when users add/remove logs
+ * from it, without changing their config. This is necessary for the loader mechanism.
+ */
+function logShim(_message: unknown, _attributes?: unknown): void {
+  consoleSandbox(() => {
+    // eslint-disable-next-line no-console
+    console.warn('You are using Sentry.logger.* even though this bundle does not include logs.');
+  });
+}
+
+/**
+ * This is a shim for the logger.fmt template literal function.
+ * It is needed in order for the CDN bundles to continue working when users add/remove logs
+ * from it, without changing their config. This is necessary for the loader mechanism.
+ */
+function fmtShim(_strings: TemplateStringsArray, ..._values: unknown[]): ParameterizedString {
+  consoleSandbox(() => {
+    // eslint-disable-next-line no-console
+    console.warn('You are using Sentry.logger.fmt even though this bundle does not include logs.');
+  });
+  return '' as ParameterizedString;
+}
+
+export const loggerShim = {
+  trace: logShim,
+  debug: logShim,
+  info: logShim,
+  warn: logShim,
+  error: logShim,
+  fatal: logShim,
+  fmt: fmtShim,
+};
+
+/**
+ * This is a shim for the consoleLoggingIntegration.
+ * It is needed in order for the CDN bundles to continue working when users add/remove logs
+ * from it, without changing their config. This is necessary for the loader mechanism.
+ */
+export const consoleLoggingIntegrationShim = defineIntegration((_options?: unknown) => {
+  consoleSandbox(() => {
+    // eslint-disable-next-line no-console
+    console.warn('You are using consoleLoggingIntegration() even though this bundle does not include logs.');
+  });
+
+  return {
+    name: 'ConsoleLogs',
+    setup: FAKE_FUNCTION,
+  };
+}) as () => Integration;
