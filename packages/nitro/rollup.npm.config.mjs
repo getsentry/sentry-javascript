4e07ce136562488c3c4e78c82062bdb38e84192a
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-01-27 09:30:50 -0500

    fix: ensure runtime plugins are present in the dist

--- packages/nitro/rollup.npm.config.mjs
+++ packages/nitro/rollup.npm.config.mjs
@@ -3,7 +3,7 @@ import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollu
 export default [
   ...makeNPMConfigVariants(
     makeBaseNPMConfig({
-      entrypoints: ['src/index.ts'],
+      entrypoints: ['src/index.ts', 'src/runtime/plugins/server.ts'],
       packageSpecificConfig: {
         external: [/^nitro/],
       },
