abedb42eb4de98a1d0e68b0de62e50733fca1460
Lukas Stracke <lukas.stracke@sentry.io>
2026-01-28 15:48:50 +0100

    remove stacktrace from tests (browser compat)

--- dev-packages/browser-integration-tests/suites/public-api/captureException/aggregateError-custom/test.ts
+++ dev-packages/browser-integration-tests/suites/public-api/captureException/aggregateError-custom/test.ts
@@ -11,60 +11,30 @@ sentryTest('captures custom AggregateErrors', async ({ getLocalTestUrl, page }) 
 
   // Verify the embedded errors come first
   expect(eventData.exception?.values).toEqual([
-    {
+    expect.objectContaining({
       mechanism: { exception_id: 4, handled: true, parent_id: 0, source: 'errors[1]', type: 'chained' },
-      stacktrace: {
-        frames: [
-          { colno: 12, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 14 },
-          { colno: 5, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 10 },
-        ],
-      },
       type: 'Error',
       value: 'error 2',
-    },
-    {
+    }),
+    expect.objectContaining({
       mechanism: { exception_id: 3, handled: true, parent_id: 2, source: 'cause', type: 'chained' },
-      stacktrace: {
-        frames: [
-          { colno: 12, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 14 },
-          { colno: 10, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 9 },
-        ],
-      },
       type: 'Error',
       value: 'error 1 cause',
-    },
-    {
+    }),
+    expect.objectContaining({
       mechanism: { exception_id: 2, handled: true, parent_id: 0, source: 'errors[0]', type: 'chained' },
-      stacktrace: {
-        frames: [
-          { colno: 12, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 14 },
-          { colno: 50, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 8 },
-        ],
-      },
       type: 'Error',
       value: 'error 1',
-    },
-    {
+    }),
+    expect.objectContaining({
       mechanism: { exception_id: 1, handled: true, parent_id: 0, source: 'cause', type: 'chained' },
-      stacktrace: {
-        frames: [
-          { colno: 12, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 14 },
-          { colno: 10, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 11 },
-        ],
-      },
       type: 'Error',
       value: 'aggregate cause',
-    },
-    {
+    }),
+    expect.objectContaining({
       mechanism: { exception_id: 0, handled: true, type: 'generic', is_exception_group: true },
-      stacktrace: {
-        frames: [
-          { colno: 12, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 14 },
-          { colno: 24, filename: 'http://sentry-test.io/subject.bundle.js', function: '?', in_app: true, lineno: 8 },
-        ],
-      },
       type: 'CustomAggregateError',
       value: 'custom aggregate error',
-    },
+    }),
   ]);
 });
