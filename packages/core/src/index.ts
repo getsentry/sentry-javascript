a34aee7f50cdac1feebda90ecce40bf18d9449f6
Lukas Stracke <lukas.stracke@sentry.io>
2026-02-02 14:14:52 +0100

    feat(core): Add span v2 and envelope type definitions (#19100)
    This PR introduces span v2 types as defined in our [develop
    spec](https://develop.sentry.dev/sdk/telemetry/spans/span-protocol/):
    
    * Envelope types:
    * `SpanV2Envelope`, `SpanV2EnvelopeHeaders`, `SpanContainerItem`,
    `SpanContainerItemHeaders`
    * Span v2 types:
    * `SpanV2JSON` the equivalent to today's `SpanJSON`. Users will interact
    with spans in this format in `beforeSendSpan`. SDK integrations will use
    this format in `processSpan` (and related) hooks.
    * `SerializedSpan` the final, serialized format for v2 spans, sent in
    the envelope container item.
    
    Closes #19101 (added automatically)
    
    ref #17836

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -394,6 +394,7 @@ export type {
   ProfileChunkEnvelope,
   ProfileChunkItem,
   SpanEnvelope,
+  SpanV2Envelope,
   SpanItem,
   LogEnvelope,
   MetricEnvelope,
@@ -461,6 +462,9 @@ export type {
   SpanJSON,
   SpanContextData,
   TraceFlag,
+  StreamedSpanJSON,
+  SerializedSpanContainer,
+  SerializedSpan,
 } from './types-hoist/span';
 export type { SpanStatus } from './types-hoist/spanStatus';
 export type { Log, LogSeverityLevel } from './types-hoist/log';
