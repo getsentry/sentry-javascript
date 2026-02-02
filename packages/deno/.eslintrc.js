dffcccfc146725d00b6bd49a94bec723c3455550
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-02-02 13:41:42 -0500

    chore: cleanup eslint

--- packages/deno/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['../../.eslintrc.js'],
-  ignorePatterns: ['lib.deno.d.ts', 'scripts/*.mjs', 'build-types/**', 'build-test/**', 'build/**'],
-  rules: {
-    '@sentry-internal/sdk/no-class-field-initializers': 'off',
-  },
-};
