587b5575115d775552887c569d36446cb617e3e7
Lukas Stracke <lukas.stracke@sentry.io>
2026-02-04 17:00:17 +0100

    feat(core): Add `StreamedSpanEnvelope` creation function (#19153)
    Adds a utility to create a span v2 envelope from a `SerializedSpan`
    array + tests.
    
    Note: I think here, the "v2" naming makes more sense than the
    `StreamSpan` patter we use for user-facing functionality. This function
    should never be called by users, and the envelope is type `span` with
    content type `span.v2+json`
    
    ref #17836

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -392,7 +392,7 @@ export type {
   ProfileChunkEnvelope,
   ProfileChunkItem,
   SpanEnvelope,
-  SpanV2Envelope,
+  StreamedSpanEnvelope,
   SpanItem,
   LogEnvelope,
   MetricEnvelope,
@@ -461,8 +461,6 @@ export type {
   SpanContextData,
   TraceFlag,
   StreamedSpanJSON,
-  SerializedSpanContainer,
-  SerializedSpan,
 } from './types-hoist/span';
 export type { SpanStatus } from './types-hoist/spanStatus';
 export type { Log, LogSeverityLevel } from './types-hoist/log';
