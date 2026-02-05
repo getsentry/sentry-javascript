afaff66ef2219e98cfe44362968286e60f88b37c
JPeer264 <jan.peer@sentry.io>
2026-02-05 09:13:39 +0100

    test: Verify if only one session exists

--- dev-packages/browser-integration-tests/suites/sessions/single-mode/test.ts
+++ dev-packages/browser-integration-tests/suites/sessions/single-mode/test.ts
@@ -31,6 +31,16 @@ sentryTest(
       timeout: 4000,
     });
 
+    const manualSessionsPromise = getMultipleSentryEnvelopeRequests<SessionContext>(page, 10, {
+      envelopeType: 'session',
+      timeout: 4000,
+    });
+
+    const eventsPromise = getMultipleSentryEnvelopeRequests<SessionContext>(page, 10, {
+      envelopeType: 'event',
+      timeout: 4000,
+    });
+
     await page.waitForSelector('#navigate');
 
     await page.locator('#navigate').click();
@@ -41,5 +51,22 @@ sentryTest(
 
     expect(sessions.length).toBe(1);
     expect(sessions[0].init).toBe(true);
+
+    // teardown and verify if nothing else got sent
+    await page.locator('#manual-session').click();
+
+    const newSessions = (await manualSessionsPromise).filter(session => session.init);
+    const events = await eventsPromise;
+
+    expect(newSessions.length).toBe(2);
+    expect(newSessions[0].init).toBe(true);
+    expect(newSessions[1].init).toBe(true);
+    expect(newSessions[1].sid).not.toBe(newSessions[0].sid);
+    expect(events).toEqual([
+      expect.objectContaining({
+        level: 'error',
+        message: 'Test error from manual session',
+      }),
+    ]);
   },
 );
