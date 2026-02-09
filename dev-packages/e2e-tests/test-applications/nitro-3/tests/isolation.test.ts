29fc9f76e13020152dc388e8508eeca29af23754
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-02-09 16:14:08 -0500

    feat: use vite mode for better test coverage

--- dev-packages/e2e-tests/test-applications/nitro-3/tests/isolation.test.ts
+++ dev-packages/e2e-tests/test-applications/nitro-3/tests/isolation.test.ts
@@ -3,14 +3,14 @@ import { waitForError, waitForTransaction } from '@sentry-internal/test-utils';
 
 test('Isolation scope prevents tag leaking between requests', async ({ request }) => {
   const transactionEventPromise = waitForTransaction('nitro-3', event => {
-    return event?.transaction === 'GET /test-isolation/1';
+    return event?.transaction === 'GET /api/test-isolation/1';
   });
 
   const errorPromise = waitForError('nitro-3', event => {
-    return !event.type && event.exception?.values?.some(v => v.value === 'Isolation test error');
+    return !event.type && !!event.exception?.values?.some(v => v.value === 'Isolation test error');
   });
 
-  await request.get('/test-isolation/1').catch(() => {
+  await request.get('/api/test-isolation/1').catch(() => {
     // noop - route throws
   });
 
