36c96b652739a32eeb908a667504349d19072f00
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-02-12 13:03:55 -0500

    fix: set headers before they get frozen

--- packages/nitro/src/runtime/plugins/server.ts
+++ packages/nitro/src/runtime/plugins/server.ts
@@ -1,16 +1,11 @@
 import { definePlugin } from 'nitro';
 import { captureErrorHook } from '../hooks/captureErrorHook';
 import { captureTracingEvents } from '../hooks/captureTracingEvents';
-import { setServerTimingHeaders } from '../hooks/setServerTimingHeaders';
 
 export default definePlugin(nitroApp => {
   // FIXME: Nitro hooks are not typed it seems
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   nitroApp.hooks.hook('error', captureErrorHook);
-
-  // FIXME: Nitro hooks are not typed it seems
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  nitroApp.hooks.hook('response', setServerTimingHeaders);
 
   captureTracingEvents();
 });
