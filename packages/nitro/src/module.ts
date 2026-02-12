572a8305d0631387dd3d93b1559a0e9e98aac5ad
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-02-12 12:04:20 -0500

    feat: sourcemaps upload and debug ID injection

--- packages/nitro/src/module.ts
+++ packages/nitro/src/module.ts
@@ -1,14 +1,17 @@
 import type { NitroModule } from 'nitro/types';
+import type { SentryNitroOptions } from './config';
 import { instrumentServer } from './instruments/instrumentServer';
+import { setupSourceMaps } from './sourceMaps';
 
 /**
  * Creates a Nitro module to setup the Sentry SDK.
  */
-export function createNitroModule(): NitroModule {
+export function createNitroModule(sentryOptions?: SentryNitroOptions): NitroModule {
   return {
     name: 'sentry',
     setup: nitro => {
       instrumentServer(nitro);
+      setupSourceMaps(nitro, sentryOptions);
     },
   };
 }
