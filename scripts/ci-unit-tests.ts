7cb8127b76096b05246e528ddd534b7d1966eede
Charly Gomez <charly.gomez@sentry.io>
2026-02-16 16:49:02 +0100

    .

--- scripts/ci-unit-tests.ts
+++ scripts/ci-unit-tests.ts
@@ -89,7 +89,7 @@ function runAllTests(ignorePackages: Set<string>): void {
     .map(dep => `--exclude="${dep}"`)
     .join(' ');
 
-  run(`yarn test ${excludeFlags}`);
+  run(`yarn test:unit ${excludeFlags}`);
 }
 
 /**
