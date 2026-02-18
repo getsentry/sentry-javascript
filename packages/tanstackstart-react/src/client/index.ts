c56c38ba80a61ce27badf49cf6d5d0aa611f3a93
Lazar Nikolov <hello@nikolovlazar.com>
2026-02-18 13:23:20 -0500

    fix(tanstackstart-react): linting error in client/index.ts

--- packages/tanstackstart-react/src/client/index.ts
+++ packages/tanstackstart-react/src/client/index.ts
@@ -1,11 +1,11 @@
 // import/export got a false positive, and affects most of our index barrel files
 // can be removed once following issue is fixed: https://github.com/import-js/eslint-plugin-import/issues/703
 /* eslint-disable import/export */
-import type { TanStackMiddlewareBase } from "../common/types";
+import type { TanStackMiddlewareBase } from '../common/types';
 
-export * from "@sentry/react";
+export * from '@sentry/react';
 
-export { init } from "./sdk";
+export { init } from './sdk';
 
 /**
  * No-op stub for client-side builds.
@@ -22,7 +22,7 @@ export function wrapMiddlewaresWithSentry<T extends TanStackMiddlewareBase>(
  * The actual implementation is server-only, but this stub is needed to prevent rendering errors.
  */
 export const sentryGlobalRequestMiddleware: TanStackMiddlewareBase = {
-  "~types": undefined,
+  '~types': undefined,
   options: {},
 };
 
@@ -31,6 +31,6 @@ export const sentryGlobalRequestMiddleware: TanStackMiddlewareBase = {
  * The actual implementation is server-only, but this stub is needed to prevent rendering errors.
  */
 export const sentryGlobalFunctionMiddleware: TanStackMiddlewareBase = {
-  "~types": undefined,
+  '~types': undefined,
   options: {},
 };
