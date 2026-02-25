b12c65d1f1d93a2d92211f0d4c56e79463d76c0f
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-02-25 15:41:21 +0100

    update

--- packages/nestjs/src/helpers.ts
+++ packages/nestjs/src/helpers.ts
@@ -1,7 +1,7 @@
 /**
  * Determines if the exception is an expected NestJS control flow error.
- * - HttpException have getStatus and getResponse methods
- * - RpcException have getError and initMessage methods
+ * - HttpException have getStatus and getResponse methods: https://github.com/nestjs/nest/blob/master/packages/microservices/exceptions/rpc-exception.ts
+ * - RpcException have getError and initMessage methods: https://github.com/nestjs/nest/blob/master/packages/common/exceptions/http.exception.ts
  *
  * We cannot use `instanceof HttpException` here because this file is imported
  * from the main entry point (via decorators.ts), and importing @nestjs/common at that
