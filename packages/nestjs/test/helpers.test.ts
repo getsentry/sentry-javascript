250510c640a0b912075008d68f4ec9f326ef33a3
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-02-25 15:37:08 +0100

    cut shit

--- packages/nestjs/test/helpers.test.ts
+++ packages/nestjs/test/helpers.test.ts
@@ -7,10 +7,6 @@ describe('isExpectedError', () => {
     expect(isExpectedError(new HttpException('Bad Request', HttpStatus.BAD_REQUEST))).toBe(true);
   });
 
-  it('should return true for HttpException with 500 status', () => {
-    expect(isExpectedError(new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR))).toBe(true);
-  });
-
   it('should return true for RpcException-like objects', () => {
     const rpcLike = {
       getError: () => 'some error',
@@ -25,29 +21,5 @@ describe('isExpectedError', () => {
 
   it('should return false for object with status property', () => {
     expect(isExpectedError({ status: 502, message: 'Bad Gateway' })).toBe(false);
-  });
-
-  it('should return false for object with error property', () => {
-    expect(isExpectedError({ error: 'something went wrong' })).toBe(false);
-  });
-
-  it('should return false for object with only getError method', () => {
-    expect(isExpectedError({ getError: () => 'error' })).toBe(false);
-  });
-
-  it('should return false for object with only getStatus method', () => {
-    expect(isExpectedError({ getStatus: () => 500 })).toBe(false);
-  });
-
-  it('should return false for null', () => {
-    expect(isExpectedError(null)).toBe(false);
-  });
-
-  it('should return false for undefined', () => {
-    expect(isExpectedError(undefined)).toBe(false);
-  });
-
-  it('should return false for string', () => {
-    expect(isExpectedError('error')).toBe(false);
   });
 });
