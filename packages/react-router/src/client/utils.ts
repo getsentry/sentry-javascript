00a791a6dd77f1736aa17d420713773cd2520734
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-03-02 17:57:10 +0100

    remove comment

--- packages/react-router/src/client/utils.ts
+++ packages/react-router/src/client/utils.ts
@@ -2,7 +2,6 @@
  * Resolves a navigate argument to a string path.
  *
  * React Router's navigate() accepts a string, number, or To object ({ pathname, search, hash }).
- * This function extracts the pathname from object arguments to avoid "[object Object]" in transaction names.
  */
 export function resolveNavigateArg(target: unknown): string {
   if (typeof target === 'object' && target !== null && 'pathname' in target) {
