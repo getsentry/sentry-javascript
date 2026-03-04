9dcfb1319c9b75c17ff20d7023115aa36d59da68
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-03-04 16:57:06 +0100

    Add test that verifies that exceptions are not auto-captured

--- dev-packages/e2e-tests/test-applications/nestjs-websockets/tests/errors.test.ts
+++ dev-packages/e2e-tests/test-applications/nestjs-websockets/tests/errors.test.ts
@@ -21,3 +21,35 @@ test('Captures manually reported error in WebSocket gateway handler', async ({ b
 
   socket.disconnect();
 });
+
+// There is no good mechanism to verify that an event was NOT sent to Sentry.
+// The idea here is that we first send a message that triggers an exception which won't be auto-captured,
+// and then send a message that triggers a manually captured error which will be sent to Sentry.
+// If the manually captured error arrives, we can deduce that the first exception was not sent,
+// because Socket.IO guarantees message ordering: https://socket.io/docs/v4/delivery-guarantees
+test('Does not automatically capture exceptions in WebSocket gateway handler', async ({ baseURL }) => {
+  let errorEventOccurred = false;
+
+  waitForError('nestjs-websockets', event => {
+    if (!event.type && event.exception?.values?.[0]?.value === 'This is an exception in a WebSocket handler') {
+      errorEventOccurred = true;
+    }
+
+    return false;
+  });
+
+  const manualCapturePromise = waitForError('nestjs-websockets', event => {
+    return event.exception?.values?.[0]?.value === 'Manually captured WebSocket error';
+  });
+
+  const socket = io(baseURL!);
+  await new Promise<void>(resolve => socket.on('connect', resolve));
+
+  socket.emit('test-exception', {});
+  socket.emit('test-manual-capture', {});
+  await manualCapturePromise;
+
+  expect(errorEventOccurred).toBe(false);
+
+  socket.disconnect();
+});
