15170d03ccc35ac01a1284d4c337d1eb8b9952b2
Jan Peer Stöcklmair <jan.peer@sentry.io>
2026-03-05 14:35:36 +0100

    feat(effect): Add client/server entrypoints without functionality (#19649)
    That adds now the functionality to use the `Sentry.effectLayer`
    properly. **But** it doesn't do anything, which means right now, to keep
    the PRs small, it returns an empty layer.
    
    Following can be used without any Sentry functionality:
    
    ```js
    const MainLive = HttpLive.pipe(Layer.provide(Sentry.effectLayer({
      dsn: "",
      tracesSampleRate: 1.0,
      debug: true,
    })))
    
    MainLive.pipe(Layer.launch, NodeRuntime.runMain)
    ```

---
+++ packages/effect/src/index.client.ts
@@ -0,0 +1,4 @@
+export * from '@sentry/browser';
+
+export { effectLayer } from './client/index';
+export type { EffectClientLayerOptions } from './client/index';
---
+++ packages/effect/src/index.server.ts
@@ -0,0 +1,4 @@
+export * from '@sentry/node-core/light';
+
+export { effectLayer } from './server/index';
+export type { EffectServerLayerOptions } from './server/index';
