90184af647c3f74b5b8a689e88b70933e3c9fe11
JPeer264 <jan.peer@sentry.io>
2026-03-05 13:01:44 +0100

    fixup! feat(effect): Add client/server entrypoints without functionality

--- packages/effect/src/index.types.ts
+++ packages/effect/src/index.types.ts
@@ -3,12 +3,24 @@
 // We export everything from both the client part of the SDK and from the server part.
 // Some of the exports collide, which is not allowed, unless we redefine the colliding
 // exports in this file - which we do below.
+import type { Client, Integration, Options, StackParser } from '@sentry/core';
 import type * as EffectLayer from 'effect/Layer';
-import type { EffectClientLayerOptions } from './index.client';
-import type { EffectServerLayerOptions } from './index.server';
+import type * as clientSdk from './index.client';
+import type * as serverSdk from './index.server';
 
-export * from './client/index';
-export * from './server/index';
+export * from './index.client';
+export * from './index.server';
 
-/** Creates an Effect Layer that initializes Sentry and integrates tracing, logging, and metrics. */
-export declare function effectLayer(options: EffectClientLayerOptions | EffectServerLayerOptions): EffectLayer.Layer<never, never, never>;
+export type { EffectClientLayerOptions } from './index.client';
+export type { EffectServerLayerOptions } from './index.server';
+
+export declare function effectLayer(
+  options: clientSdk.EffectClientLayerOptions | serverSdk.EffectServerLayerOptions,
+): EffectLayer.Layer<never, never, never>;
+
+export declare function init(options: Options | clientSdk.BrowserOptions | serverSdk.NodeOptions): Client | undefined;
+export declare const linkedErrorsIntegration: typeof clientSdk.linkedErrorsIntegration;
+export declare const contextLinesIntegration: typeof clientSdk.contextLinesIntegration;
+export declare const getDefaultIntegrations: (options: Options) => Integration[];
+export declare const defaultStackParser: StackParser;
+export declare const logger: typeof clientSdk.logger | typeof serverSdk.logger;
