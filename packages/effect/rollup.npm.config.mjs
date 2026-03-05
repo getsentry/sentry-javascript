d3f9e90305085170f875060ddff188e734e5efe7
Jan Peer Stöcklmair <jan.peer@sentry.io>
2026-03-05 14:35:36 +0100

    feat(effect): Add client/server entrypoints without functionality (#19649)
    That adds now the functionality to use the `Sentry.effectLayer`
    properly. **But** it doesn't do anything, which means right now, to keep
    the PRs small, it returns an empty layer.
    
    Following can be used without any Sentry functionality:
    
    ```js
    const MainLive = HttpLive.pipe(Layer.provide(Sentry.effectLayer({
      dsn: "",
      tracesSampleRate: 1.0,
      debug: true,
    })))
    
    MainLive.pipe(Layer.launch, NodeRuntime.runMain)
    ```

--- packages/effect/rollup.npm.config.mjs
+++ packages/effect/rollup.npm.config.mjs
@@ -1,11 +1,25 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollup-utils';
 
-export default makeNPMConfigVariants(
-  makeBaseNPMConfig({
-    packageSpecificConfig: {
-      output: {
-        preserveModulesRoot: 'src',
-      },
+const baseConfig = makeBaseNPMConfig({
+  entrypoints: ['src/index.server.ts', 'src/index.client.ts'],
+  packageSpecificConfig: {
+    output: {
+      preserveModulesRoot: 'src',
     },
-  }),
-);
+  },
+});
+
+const defaultExternal = baseConfig.external || [];
+baseConfig.external = id => {
+  if (defaultExternal.includes(id)) {
+    return true;
+  }
+
+  if (id === 'effect' || id.startsWith('effect/') || id.startsWith('@sentry/')) {
+    return true;
+  }
+
+  return false;
+};
+
+export default makeNPMConfigVariants(baseConfig);
