7e9e7c2eff2bcbc6ff794cac2a663e9e05e4ccfb
JPeer264 <jan.peer@sentry.io>
2026-03-05 09:38:34 +0100

    fixup! test(effect): Add a smoke test so CI doesn't fail

--- packages/effect/.eslintrc.js
+++ packages/effect/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['vite.config.ts', 'vitest.config.ts'],
+      parserOptions: {
+        project: ['tsconfig.vite.json'],
+      },
+    },
+  ],
   extends: ['../../.eslintrc.js'],
 };
