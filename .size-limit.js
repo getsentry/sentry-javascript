9e2f8ebe43d14ce31aef95b7deaee9b7cddaf0c1
Lukas Stracke <lukas.stracke@sentry.io>
2026-03-06 13:33:13 +0100

    test(browser): Add span streaming integration tests (#19581)
    This PR adds browser integration to test testing span streaming:
    
    - Added test helpers:
      - `waitForStreamedSpan`: Returns a promise of a single matching span
    - `waitForStreamedSpans`: Returns a promise of all spans in an array
    whenever the callback returns true
    - `waitForStreamedSpanEnvelope`: Returns an entire streamed span (v2)
    envelope (including headers)
    - `observeStreamedSpan`: Can be used to observe sent span envelopes
    without blocking the test if no envelopes are sent (good for testing
    that spans are _not_ sent)
    - `getSpanOp`: Small helper to easily get the op of a span which we
    almost always need for the `waitFor*` function callbacks
    
    Added 50+ tests, mostly converted from transaction integration tests
    around spans from `browserTracingIntegration`:
    - tests asserting the entire span v2 envelope payloads of manually
    started, pageload and navigation span trees
    - tests for trace linking and trace lifetime
    - tests for spans coming from browserTracingIntegration (fetch, xhr,
    long animation frame, long tasks)
    
    Also, this PR fixes two bugs discovered through tests:
    - negatively sampled spans were still sent (because non-recording spans
    go through the same span life cycle)
    - cancelled spans received status `error` instead of `ok`. We want them
    to have status `ok` but an attribute detailing the cancellation reason.
    
    Lastly, I discovered a problem with timing data on fetch and XHR spans.
    Will try to fix as a follow-up. Tracked in #19613
    
    ref #17836

--- .size-limit.js
+++ .size-limit.js
@@ -148,7 +148,7 @@ module.exports = [
     import: createImport('init', 'ErrorBoundary', 'reactRouterV6BrowserTracingIntegration'),
     ignore: ['react/jsx-runtime'],
     gzip: true,
-    limit: '45 KB',
+    limit: '46 KB',
   },
   // Vue SDK (ESM)
   {
---
+++ dev-packages/browser-integration-tests/suites/tracing/request/xhr-streamed/test.ts
@@ -0,0 +1,43 @@
+import { expect } from '@playwright/test';
+import { sentryTest } from '../../../../utils/fixtures';
+import { shouldSkipTracingTest, testingCdnBundle } from '../../../../utils/helpers';
+import { getSpanOp, waitForStreamedSpans } from '../../../../utils/spanUtils';
+
+sentryTest('creates spans for XHR requests', async ({ getLocalTestUrl, page }) => {
+  sentryTest.skip(shouldSkipTracingTest() || testingCdnBundle());
+
+  await page.route('http://sentry-test-site.example/*', route => route.fulfill({ body: 'ok' }));
+
+  const url = await getLocalTestUrl({ testDir: __dirname });
+
+  const spansPromise = waitForStreamedSpans(
+    page,
+    spans => spans.filter(s => getSpanOp(s) === 'http.client').length >= 3,
+  );
+
+  await page.goto(url);
+
+  const allSpans = await spansPromise;
+  const pageloadSpan = allSpans.find(s => getSpanOp(s) === 'pageload');
+  const requestSpans = allSpans.filter(s => getSpanOp(s) === 'http.client');
+
+  expect(requestSpans).toHaveLength(3);
+
+  requestSpans.forEach((span, index) =>
+    expect(span).toMatchObject({
+      name: `GET http://sentry-test-site.example/${index}`,
+      parent_span_id: pageloadSpan?.span_id,
+      span_id: expect.stringMatching(/[a-f\d]{16}/),
+      start_timestamp: expect.any(Number),
+      end_timestamp: expect.any(Number),
+      trace_id: pageloadSpan?.trace_id,
+      attributes: expect.objectContaining({
+        'http.method': { type: 'string', value: 'GET' },
+        'http.url': { type: 'string', value: `http://sentry-test-site.example/${index}` },
+        url: { type: 'string', value: `http://sentry-test-site.example/${index}` },
+        'server.address': { type: 'string', value: 'sentry-test-site.example' },
+        type: { type: 'string', value: 'xhr' },
+      }),
+    }),
+  );
+});
