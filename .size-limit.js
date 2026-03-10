3b1e3540d7d9679e5543c71aa68d6bb68e61bf6b
RulaKhaled <rola.abuhasna@sentry.io>
2026-03-10 11:36:24 +0100

    bump size

--- .size-limit.js
+++ .size-limit.js
@@ -326,7 +326,7 @@ module.exports = [
     import: createImport('init'),
     ignore: [...builtinModules, ...nodePrefixedBuiltinModules],
     gzip: true,
-    limit: '175 KB',
+    limit: '176 KB',
   },
   {
     name: '@sentry/node - without tracing',
