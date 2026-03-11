d3901e3b332cd3379fe54e012e463e92fef83d38
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2026-03-11 09:21:15 +0100

    feat(nestjs): Instrument `@nestjs/schedule` decorators (#19735)
    Instruments the `@Cron`, `@Interval` and `@Timeout` decorators from
    `@nestjs/schedule`
    ([npm](https://www.npmjs.com/package/@nestjs/schedule)) to capture
    errors and fork isolation scopes to prevent leakage into subsequent http
    requests.
    
    So far we only had a manual `@SentryCron` decorator that users could
    apply to get checkins and exceptions from crons. `@SentryCron` is now
    reduced to only send check-ins if applied (no exception capture anymore
    since this is handled by the auto-instrumentation).
    
    Closes https://github.com/getsentry/sentry-javascript/issues/19704

--- dev-packages/e2e-tests/test-applications/nestjs-basic/src/app.module.ts
+++ dev-packages/e2e-tests/test-applications/nestjs-basic/src/app.module.ts
@@ -6,12 +6,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExampleGlobalFilter } from './example-global.filter';
 import { ExampleMiddleware } from './example.middleware';
+import { ScheduleService } from './schedule.service';
 
 @Module({
   imports: [SentryModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
+    ScheduleService,
     {
       provide: APP_FILTER,
       useClass: SentryGlobalFilter,
--- packages/nestjs/src/integrations/nest.ts
+++ packages/nestjs/src/integrations/nest.ts
@@ -3,6 +3,7 @@ import { defineIntegration } from '@sentry/core';
 import { generateInstrumentOnce } from '@sentry/node';
 import { SentryNestEventInstrumentation } from './sentry-nest-event-instrumentation';
 import { SentryNestInstrumentation } from './sentry-nest-instrumentation';
+import { SentryNestScheduleInstrumentation } from './sentry-nest-schedule-instrumentation';
 
 const INTEGRATION_NAME = 'Nest';
 
@@ -18,11 +19,16 @@ const instrumentNestEvent = generateInstrumentOnce(`${INTEGRATION_NAME}.Event`, 
   return new SentryNestEventInstrumentation();
 });
 
+const instrumentNestSchedule = generateInstrumentOnce(`${INTEGRATION_NAME}.Schedule`, () => {
+  return new SentryNestScheduleInstrumentation();
+});
+
 export const instrumentNest = Object.assign(
   (): void => {
     instrumentNestCore();
     instrumentNestCommon();
     instrumentNestEvent();
+    instrumentNestSchedule();
   },
   { id: INTEGRATION_NAME },
 );
