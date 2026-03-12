d2490bf0682fa8cb22a2769a2021507cabaaad49
JPeer264 <jan.peer@sentry.io>
2026-03-12 13:51:02 +0100

    test(astro,cloudflare): Add an E2E test for Astro 6 on Cloudflare

---
+++ dev-packages/e2e-tests/test-applications/astro-6-cf-workers/sentry.server.config.js
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/cloudflare';
+import handler from "@astrojs/cloudflare/entrypoints/server";
+
+export default Sentry.withSentry((env) => ({
+  dsn: env.E2E_TEST_DSN,
+  environment: 'qa',
+  tracesSampleRate: 1.0,
+  tunnel: 'http://localhost:3031/', // proxy server
+  debug: true,
+}), handler);
