0f8ea1181c34c07ba45d77e4284000a49a775585
Lukas Stracke <lukas.stracke@sentry.io>
2026-03-13 16:36:29 +0100

    add span name update tests

---
+++ dev-packages/node-integration-tests/suites/public-api/startSpan/updateName-method-streamed/test.ts
@@ -0,0 +1,26 @@
+import { SEMANTIC_ATTRIBUTE_SENTRY_SOURCE } from '@sentry/node';
+import { afterAll, expect, test } from 'vitest';
+import { cleanupChildProcesses, createRunner } from '../../../../utils/runner';
+
+afterAll(() => {
+  cleanupChildProcesses();
+});
+
+test('updates the span name when calling `span.updateName` (streamed)', async () => {
+  await createRunner(__dirname, 'scenario.ts')
+    .expect({
+      span: {
+        items: [
+          {
+            name: 'new name',
+            is_segment: true,
+            attributes: {
+              [SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: { type: 'string', value: 'url' },
+            },
+          },
+        ],
+      },
+    })
+    .start()
+    .completed();
+});
