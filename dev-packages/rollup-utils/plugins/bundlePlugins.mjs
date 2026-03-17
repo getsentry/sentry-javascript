cfd1c4848d5fd2ef438554e02e42316b1cbfc639
Daniel Griesser <daniel.griesser.86@gmail.com>
2026-03-17 12:35:32 +0100

    chore: remove autoresearch scaffolding files, drop unsafe_Function terser option

--- dev-packages/rollup-utils/plugins/bundlePlugins.mjs
+++ dev-packages/rollup-utils/plugins/bundlePlugins.mjs
@@ -151,7 +151,6 @@ export function makeTerserPlugin() {
       pure_getters: true,
       unsafe_arrows: true,
       unsafe_methods: true,
-      unsafe_Function: true,
     },
     output: {
       comments: false,
