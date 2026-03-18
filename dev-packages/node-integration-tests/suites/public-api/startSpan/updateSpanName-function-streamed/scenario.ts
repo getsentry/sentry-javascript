5963170f2f95b3e7f91bc2c89fd80f7c2012f00c
Lukas Stracke <lukas.stracke@sentry.io>
2026-03-18 17:35:09 +0100

    test(node,node-core): Add span streaming integration tests (#19806)
    Extends our Node (core) integration test runner API to expect `span`
    envelopes with both an API to test against span envelope headers as well
    as the container. In addition, this adds a couple of integration tests
    testing manually started spans, span name updates and span
    relationships. Luckily, all of the span relationship logic is
    independent from span streaming so the tests all pass. I still believe
    it's valuable to have a `span` version of them.

---
+++ dev-packages/node-integration-tests/suites/public-api/startSpan/updateSpanName-function-streamed/scenario.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/node';
+import { loggingTransport } from '@sentry-internal/node-integration-tests';
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  release: '1.0',
+  tracesSampleRate: 1.0,
+  traceLifecycle: 'stream',
+  transport: loggingTransport,
+});
+
+Sentry.startSpan(
+  { name: 'test_span', attributes: { [Sentry.SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: 'url' } },
+  (span: Sentry.Span) => {
+    Sentry.updateSpanName(span, 'new name');
+  },
+);
+
+void Sentry.flush();
