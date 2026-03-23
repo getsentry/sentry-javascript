3fafdb4fcce32d317dc6159db8f5dfcf10df2baf
Sigrid <32902192+s1gr1d@users.noreply.github.com>
2026-03-23 11:03:15 +0100

    ref(nuxt): Extract handler patching to extra plugin for Nitro v2/v3 (#19915)
    `h3App` is called `h3` in Nitro v3. Additionally, the type imports have
    changed.
    
    This PR is just moving some code around (not changing any logic). Those
    are the made changes:
    - Installing `nitro` as a `devDependency` -> the new import in Nitro v3
    (instead of `nitropack`)
    - Created extra plugin that just does the handler patching (to not have
    to duplicate everthing
    - Extracted the patching function as an utility function which accepts a
    generic type (as type imports differ in Nitro versions)
    
    
    Currently, the `handler.server.ts` file (for Nuxt v5) is unused as we
    don't have a reliable way yet to tell whether Nitro v2 or v3 is running.
    In theory we could do something like this below but this only checks for
    Nuxt and not Nitro.
    ```ts
        // Checking for compatibilityVersion 5 in Nuxt, does not mean that Nitro v3 is installed for sure.
        if (nuxt.options.future.compatibilityVersion < 5) {
          addServerPlugin(moduleDirResolver.resolve('./runtime/plugins/handler.server'));
        } else {
          addServerPlugin(moduleDirResolver.resolve('./runtime/plugins/handler-legacy.server'));
        }
    ```
    
    Closes https://github.com/getsentry/sentry-javascript/issues/19913

---
+++ packages/nuxt/src/runtime/plugins/handler.server.ts
@@ -0,0 +1,13 @@
+import type { EventHandler } from 'nitro/h3';
+import type { NitroAppPlugin, NitroApp } from 'nitro/types';
+import { patchEventHandler } from '../utils/patchEventHandler';
+
+/**
+ * This plugin patches the h3 event handler for Nuxt v5+ (Nitro v3+).
+ */
+export default ((nitroApp: NitroApp) => {
+  if (nitroApp?.h3?.handler) {
+    // oxlint-disable-next-line @typescript-eslint/unbound-method
+    nitroApp.h3.handler = patchEventHandler<EventHandler>(nitroApp.h3.handler);
+  }
+}) satisfies NitroAppPlugin;
