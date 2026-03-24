ab0356976dae7924c73b273620c3a60beb6851db
isaacs <i@izs.me>
2026-03-24 15:54:08 -0700

    core: add getDefaultExport method
    This was implemented for the portable Express integration, but others
    will need the same functionality, so make it a reusable util.

---
+++ packages/core/test/lib/utils/get-default-export.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest';
+import { getDefaultExport } from '../../../src/utils/get-default-export';
+
+describe('getDefaultExport', () => {
+  it('returns the default export if there is one', () => {
+    const mod = {
+      default: () => {},
+    };
+    expect(getDefaultExport(mod)).toBe(mod.default);
+  });
+  it('returns the module export if no default', () => {
+    const mod = {};
+    expect(getDefaultExport(mod)).toBe(mod);
+  });
+  it('returns the module if a function and not plain object', () => {
+    const mod = Object.assign(function () {}, {
+      default: () => {},
+    });
+    expect(getDefaultExport(mod)).toBe(mod);
+  });
+  it('returns the module if a default is falsey', () => {
+    const mod = Object.assign(function () {}, {
+      default: false,
+    });
+    expect(getDefaultExport(mod)).toBe(mod);
+  });
+});
