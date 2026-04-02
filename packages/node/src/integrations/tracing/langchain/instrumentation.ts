6721f303666f86e91fd86fd53ca516c65f361a93
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-04-02 11:28:37 +0200

    please

--- packages/node/src/integrations/tracing/langchain/instrumentation.ts
+++ packages/node/src/integrations/tracing/langchain/instrumentation.ts
@@ -264,6 +264,10 @@ export class SentryLangChainInstrumentation extends InstrumentationBase<LangChai
       if (typeof proto.embedQuery !== 'function' || typeof proto.embedDocuments !== 'function') {
         continue;
       }
+      if (proto.__sentry_patched__) {
+        continue;
+      }
+      proto.__sentry_patched__ = true;
 
       instrumentLangChainEmbeddings(proto, options);
     }
