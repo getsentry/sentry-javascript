45fa8101cc71b0d6f60493ec0a639e82bb873511
Jan Peer Stöcklmair <jan.peer@sentry.io>
2026-04-09 20:24:28 +0200

    feat(core): Export a reusable function to add tracing headers (#20076)
    This PR is an extraction of #19991 
    
    It basically exports `getTracingHeadersForFetchRequest`, which was
    previously only exported for testing, but offers a great functionality
    if you want to add tracing headers to a request. I renamed it as
    `addTracingHeadersToFetchRequest` sounded a little misleading, as it
    didn't really add headers to the request, as it returned the extracted
    headers from the request (or init, if there are any).
    
    ### Open question
    
    I added `@hidden` and `@internal` to it, not sure if this is an approach
    we follow. I'm ok to remove it from the jsdoc

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -147,7 +147,7 @@ export { profiler } from './profiling';
 // eslint thinks the entire function is deprecated (while only one overload is actually deprecated)
 // Therefore:
 // eslint-disable-next-line deprecation/deprecation
-export { instrumentFetchRequest } from './fetch';
+export { instrumentFetchRequest, _INTERNAL_getTracingHeadersForFetchRequest } from './fetch';
 export { trpcMiddleware } from './trpc';
 export { wrapMcpServerWithSentry } from './integrations/mcp-server';
 export { captureFeedback } from './feedback';
