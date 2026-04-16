647e65aa1096094424279b77d6456f6d69222614
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-04-16 08:24:45 -0400

    chore: nitro hooks are typed properly now

--- packages/nitro/src/runtime/plugins/server.ts
+++ packages/nitro/src/runtime/plugins/server.ts
@@ -3,8 +3,6 @@ import { captureErrorHook } from '../hooks/captureErrorHook';
 import { captureTracingEvents } from '../hooks/captureTracingEvents';
 
 export default definePlugin(nitroApp => {
-  // FIXME: Nitro hooks are not typed it seems
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   nitroApp.hooks.hook('error', captureErrorHook);
 
   captureTracingEvents();
