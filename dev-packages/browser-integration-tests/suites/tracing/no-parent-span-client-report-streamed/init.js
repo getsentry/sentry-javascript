c5f0f562db438500a85ae9b38df9344428bf4982
Lukas Stracke <lukas.stracke@sentry.io>
2026-04-16 17:21:39 +0200

    format

--- dev-packages/browser-integration-tests/suites/tracing/no-parent-span-client-report-streamed/init.js
+++ dev-packages/browser-integration-tests/suites/tracing/no-parent-span-client-report-streamed/init.js
@@ -4,7 +4,10 @@ window.Sentry = Sentry;
 
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
-  integrations: [Sentry.browserTracingIntegration({ instrumentPageLoad: false, instrumentNavigation: false }), Sentry.spanStreamingIntegration()],
+  integrations: [
+    Sentry.browserTracingIntegration({ instrumentPageLoad: false, instrumentNavigation: false }),
+    Sentry.spanStreamingIntegration(),
+  ],
   tracesSampleRate: 1,
   sendClientReports: true,
 });
--- dev-packages/browser-integration-tests/suites/tracing/no-parent-span-client-report/subject.js
+++ dev-packages/browser-integration-tests/suites/tracing/no-parent-span-client-report/subject.js
@@ -1,2 +1,1 @@
 fetch('http://sentry-test-site.example/api/test');
-
