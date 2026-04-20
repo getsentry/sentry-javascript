9a47eda41dae946078d3313edd6d20689d1a407a
Sigrid <32902192+s1gr1d@users.noreply.github.com>
2026-04-20 13:21:01 +0200

    Apply suggestion from @nicohrubec
    Co-authored-by: Nicolas Hrubec <nicolas.hrubec@outlook.com>

--- dev-packages/node-integration-tests/suites/tracing/no-parent-span-client-report/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/no-parent-span-client-report/test.ts
@@ -1,7 +1,7 @@
 import { afterAll, describe, expect } from 'vitest';
 import { cleanupChildProcesses, createEsmAndCjsTests } from '../../../utils/runner';
 
-describe('no_parent_span client report (streaming)', () => {
+describe('no_parent_span client report', () => {
   afterAll(() => {
     cleanupChildProcesses();
   });
