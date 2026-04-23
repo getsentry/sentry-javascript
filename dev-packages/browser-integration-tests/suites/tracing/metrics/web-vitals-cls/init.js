a2a1befc59371c315047b885852c02702c9a1c09
Francesco Gringl-Novy <francesco.novy@sentry.io>
2026-04-23 17:09:45 +0200

    chore(test): Reduce unneeded `idleTimeout` test config (#20467)
    After looking a bit at what the slowest browser-integration-tests are, I
    found an easy-to-fix pattern in our tests - we had _a lot_ of tests
    there that set some arbitrary `idleTimeout` to the
    `browserIntegrationTest`. This massively made these tests slower as it
    takes much longer to flush any pageload span. Many test runtimes reduced
    from 10-20s to 1-5s due to this change. A few places remain where this
    seems to be necessary, but there are way fewer now.
    
    FYI 1000ms is the default, we also had some defining this for some
    reason, I also removed those to be more consistent.

--- dev-packages/browser-integration-tests/suites/tracing/metrics/web-vitals-cls/init.js
+++ dev-packages/browser-integration-tests/suites/tracing/metrics/web-vitals-cls/init.js
@@ -6,10 +6,7 @@ Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   integrations: [
     Sentry.browserTracingIntegration({
-      enableLongTask: false,
-      enableLongAnimationFrame: true,
-      instrumentPageLoad: false,
-      enableInp: false,
+      idleTimeout: 5000,
     }),
   ],
   tracesSampleRate: 1,
