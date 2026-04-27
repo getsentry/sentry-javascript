68c1aa8fe87d40520242c722543549b9cc215615
Andrei <168741329+andreiborza@users.noreply.github.com>
2026-04-27 16:05:26 +0900

    fix(v8/profiling-node): Restore install script to build binaries from source (#20517)
    This PR adds the `install` script back compiles bindings from source if
    precompiled binaries are not found.
    
    We removed this in `8.55.1` when we fixed our CI to run on this legacy
    branch but v8 does not ship with a precompiled Node 24 binary. Users on
    Node 24 crash with `Cannot find module` errors.
    
    It also fixes deprecation warnings for `spawnSync` calls and an
    interpolation error in the profiling integration console warning.
    
    Closes: #20501

--- packages/profiling-node/src/integration.ts
+++ packages/profiling-node/src/integration.ts
@@ -430,7 +430,7 @@ export const _nodeProfilingIntegration = ((): ProfilingIntegration<NodeClient> =
     consoleSandbox(() => {
       // eslint-disable-next-line no-console
       console.warn(
-        `[Sentry Profiling] You are using a Node.js version that does not have prebuilt binaries (${NODE_VERSION}).`,
+        `[Sentry Profiling] You are using a Node.js version that does not have prebuilt binaries (${NODE_VERSION.major}.${NODE_VERSION.minor}.${NODE_VERSION.patch}).`,
         'The @sentry/profiling-node package only has prebuilt support for the following LTS versions of Node.js: 16, 18, 20, 22.',
         'To use the @sentry/profiling-node package with this version of Node.js, you will need to compile the native addon from source.',
         'See: https://github.com/getsentry/sentry-javascript/tree/develop/packages/profiling-node#building-the-package-from-source',
