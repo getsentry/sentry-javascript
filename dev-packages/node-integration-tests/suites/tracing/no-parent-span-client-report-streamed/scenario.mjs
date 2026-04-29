8732e32c8b4af7ab8a7768b930e73061ec50ccb6
Charly Gomez <charly.gomez1310@gmail.com>
2026-04-29 10:20:00 +0200

    just fetch for now

--- dev-packages/node-integration-tests/suites/tracing/no-parent-span-client-report-streamed/scenario.mjs
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/node';
-import http from 'http';
-
-const req = http.get('http://127.0.0.1:9999/external', { timeout: 5000 }, () => {});
-req.on('timeout', () => req.destroy());
-req.on('error', () => {
-  void Sentry.flush();
-});
