a4e888fa9c70e20c961c3e6d44613b3ec7c662c7
Francesco Gringl-Novy <francesco.novy@sentry.io>
2026-04-29 10:21:35 +0200

    fix(opentelemetry): Add conditional browser export to avoid node deps (#20556)
    We accidentally added a `node` dependency to an export from
    `opentelemetry` package, leading to problems for users using this in a
    browser environment.
    
    This PR adds conditional exports to the opentelemetry package, where for
    `browser` targets we have stubs for the node-only thing. This should
    generally work the same as before, but stop failing builds in browser
    envs.
    
    I had to adjust browser integration tests for this a bit, as they did
    some unnecessary aliasing which prevented webpack from using normal
    conditional exports. We are simply doing less now and doing regular
    dependency resolution which should work as expected (hopefully).

---
+++ packages/opentelemetry/src/exports.ts
@@ -0,0 +1,56 @@
+export { SEMANTIC_ATTRIBUTE_SENTRY_GRAPHQL_OPERATION } from './semanticAttributes';
+
+export { getRequestSpanData } from './utils/getRequestSpanData';
+
+export type { OpenTelemetryClient } from './types';
+export { wrapClientClass } from './custom/client';
+
+export { getSpanKind } from './utils/getSpanKind';
+
+export { getScopesFromContext } from './utils/contextData';
+
+export {
+  spanHasAttributes,
+  spanHasEvents,
+  spanHasKind,
+  spanHasName,
+  spanHasParentId,
+  spanHasStatus,
+} from './utils/spanTypes';
+
+// Re-export this for backwards compatibility (this used to be a different implementation)
+export { getDynamicSamplingContextFromSpan } from '@sentry/core';
+
+export { isSentryRequestSpan } from './utils/isSentryRequest';
+
+export { enhanceDscWithOpenTelemetryRootSpanName } from './utils/enhanceDscWithOpenTelemetryRootSpanName';
+
+export { getActiveSpan } from './utils/getActiveSpan';
+export {
+  startSpan,
+  startSpanManual,
+  startInactiveSpan,
+  withActiveSpan,
+  continueTrace,
+  getTraceContextForScope,
+} from './trace';
+
+export { suppressTracing } from './utils/suppressTracing';
+
+export { setupEventContextTrace } from './setupEventContextTrace';
+
+export { setOpenTelemetryContextAsyncContextStrategy } from './asyncContextStrategy';
+export { wrapContextManagerClass } from './contextManager';
+
+export { SentryPropagator, shouldPropagateTraceForUrl } from './propagator';
+export { SentrySpanProcessor } from './spanProcessor';
+export { SentrySampler, wrapSamplingDecision } from './sampler';
+
+export { openTelemetrySetupCheck } from './utils/setupCheck';
+
+export { getSentryResource } from './resource';
+
+export { withStreamedSpan } from '@sentry/core';
+
+// Legacy
+export { getClient } from '@sentry/core';
