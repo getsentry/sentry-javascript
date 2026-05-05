12c92553089291e93c7af03e3bbdaff08fe73d03
Francesco Gringl-Novy <francesco.novy@sentry.io>
2026-05-05 09:54:34 +0200

    test(node): Unflake mongodb test

--- dev-packages/node-integration-tests/suites/tracing/mongodb/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/mongodb/test.ts
@@ -1,5 +1,7 @@
+import type { TransactionEvent } from '@sentry/core';
 import { MongoMemoryServer } from 'mongodb-memory-server-global';
 import { afterAll, beforeAll, describe, expect, test } from 'vitest';
+import { assertSentryTransaction } from '../../../utils/assertions';
 import { cleanupChildProcesses, createRunner } from '../../../utils/runner';
 
 describe('MongoDB experimental Test', () => {
@@ -17,160 +19,166 @@ describe('MongoDB experimental Test', () => {
     cleanupChildProcesses();
   });
 
-  const EXPECTED_TRANSACTION = {
-    transaction: 'Test Transaction',
-    spans: [
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': '$cmd',
-          'db.operation': 'isMaster',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement':
-            '{"ismaster":"?","client":{"driver":{"name":"?","version":"?"},"os":{"type":"?","name":"?","architecture":"?","version":"?"},"platform":"?"},"compression":[],"helloOk":"?"}',
-          'otel.kind': 'CLIENT',
-        },
-        description:
+  const EXPECTED_SPAN_MATCHERS = [
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': '$cmd',
+        'db.operation': 'isMaster',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement':
           '{"ismaster":"?","client":{"driver":{"name":"?","version":"?"},"os":{"type":"?","name":"?","architecture":"?","version":"?"},"platform":"?"},"compression":[],"helloOk":"?"}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': '$cmd',
-          'db.operation': 'isMaster',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement':
-            '{"ismaster":"?","client":{"driver":{"name":"?","version":"?"},"os":{"type":"?","name":"?","architecture":"?","version":"?"},"platform":"?"},"compression":[],"helloOk":"?"}',
-          'otel.kind': 'CLIENT',
-        },
-        description:
+        'otel.kind': 'CLIENT',
+      },
+      description:
+        '{"ismaster":"?","client":{"driver":{"name":"?","version":"?"},"os":{"type":"?","name":"?","architecture":"?","version":"?"},"platform":"?"},"compression":[],"helloOk":"?"}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': '$cmd',
+        'db.operation': 'isMaster',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement':
           '{"ismaster":"?","client":{"driver":{"name":"?","version":"?"},"os":{"type":"?","name":"?","architecture":"?","version":"?"},"platform":"?"},"compression":[],"helloOk":"?"}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': 'movies',
-          'db.operation': 'insert',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement': '{"title":"?","_id":{"_bsontype":"?","id":"?"}}',
-          'otel.kind': 'CLIENT',
-        },
-        description: '{"title":"?","_id":{"_bsontype":"?","id":"?"}}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': 'movies',
-          'db.operation': 'find',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement': '{"title":"?"}',
-          'otel.kind': 'CLIENT',
-        },
-        description: '{"title":"?"}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': 'movies',
-          'db.operation': 'update',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement': '{"title":"?"}',
-          'otel.kind': 'CLIENT',
-        },
-        description: '{"title":"?"}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': 'movies',
-          'db.operation': 'find',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement': '{"title":"?"}',
-          'otel.kind': 'CLIENT',
-        },
-        description: '{"title":"?"}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': 'movies',
-          'db.operation': 'find',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement': '{"title":"?"}',
-          'otel.kind': 'CLIENT',
-        },
-        description: '{"title":"?"}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-      expect.objectContaining({
-        data: {
-          'sentry.origin': 'auto.db.otel.mongo',
-          'sentry.op': 'db',
-          'db.system': 'mongodb',
-          'db.name': 'admin',
-          'db.mongodb.collection': '$cmd',
-          'db.connection_string': expect.any(String),
-          'net.peer.name': expect.any(String),
-          'net.peer.port': expect.any(Number),
-          'db.statement': '{"endSessions":[{"id":{"_bsontype":"?","sub_type":"?","position":"?","buffer":"?"}}]}',
-          'otel.kind': 'CLIENT',
-        },
-        description: '{"endSessions":[{"id":{"_bsontype":"?","sub_type":"?","position":"?","buffer":"?"}}]}',
-        op: 'db',
-        origin: 'auto.db.otel.mongo',
-      }),
-    ],
-  };
+        'otel.kind': 'CLIENT',
+      },
+      description:
+        '{"ismaster":"?","client":{"driver":{"name":"?","version":"?"},"os":{"type":"?","name":"?","architecture":"?","version":"?"},"platform":"?"},"compression":[],"helloOk":"?"}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': 'movies',
+        'db.operation': 'insert',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement': '{"title":"?","_id":{"_bsontype":"?","id":"?"}}',
+        'otel.kind': 'CLIENT',
+      },
+      description: '{"title":"?","_id":{"_bsontype":"?","id":"?"}}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': 'movies',
+        'db.operation': 'find',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement': '{"title":"?"}',
+        'otel.kind': 'CLIENT',
+      },
+      description: '{"title":"?"}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': 'movies',
+        'db.operation': 'update',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement': '{"title":"?"}',
+        'otel.kind': 'CLIENT',
+      },
+      description: '{"title":"?"}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': 'movies',
+        'db.operation': 'find',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement': '{"title":"?"}',
+        'otel.kind': 'CLIENT',
+      },
+      description: '{"title":"?"}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': 'movies',
+        'db.operation': 'find',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement': '{"title":"?"}',
+        'otel.kind': 'CLIENT',
+      },
+      description: '{"title":"?"}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+    expect.objectContaining({
+      data: {
+        'sentry.origin': 'auto.db.otel.mongo',
+        'sentry.op': 'db',
+        'db.system': 'mongodb',
+        'db.name': 'admin',
+        'db.mongodb.collection': '$cmd',
+        'db.connection_string': expect.any(String),
+        'net.peer.name': expect.any(String),
+        'net.peer.port': expect.any(Number),
+        'db.statement': '{"endSessions":[{"id":{"_bsontype":"?","sub_type":"?","position":"?","buffer":"?"}}]}',
+        'otel.kind': 'CLIENT',
+      },
+      description: '{"endSessions":[{"id":{"_bsontype":"?","sub_type":"?","position":"?","buffer":"?"}}]}',
+      op: 'db',
+      origin: 'auto.db.otel.mongo',
+    }),
+  ];
 
   test('CJS - should auto-instrument `mongodb` package.', async () => {
-    await createRunner(__dirname, 'scenario.js').expect({ transaction: EXPECTED_TRANSACTION }).start().completed();
+    await createRunner(__dirname, 'scenario.js')
+      .expect({
+        transaction: (txn: TransactionEvent) => {
+          assertSentryTransaction(txn, { transaction: 'Test Transaction' });
+          expect(txn.spans).toHaveLength(EXPECTED_SPAN_MATCHERS.length);
+          expect(txn.spans).toEqual(expect.arrayContaining(EXPECTED_SPAN_MATCHERS));
+        },
+      })
+      .start()
+      .completed();
   });
 });
