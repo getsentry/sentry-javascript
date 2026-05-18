9c062a623a13da5b3d0c977fe2c246210a34f07b
Sigrid <32902192+s1gr1d@users.noreply.github.com>
2026-05-18 14:54:43 +0200

    chore: Add compatibility function for `sendDefaultPii` (#20967)
    Adds a function to bridge from `sendDefaultPii` to `dataCollection`.
    
    Related to https://github.com/getsentry/sentry-javascript/pull/20965
    
    Closes https://github.com/getsentry/sentry-javascript/issues/20925

--- packages/core/src/shared-exports.ts
+++ packages/core/src/shared-exports.ts
@@ -65,6 +65,7 @@ export {
   _INTERNAL_shouldSkipAiProviderWrapping,
   _INTERNAL_clearAiProviderSkips,
 } from './utils/ai/providerSkip';
+export { defaultPiiToCollectionOptions } from './utils/data-collection/defaultPiiToCollectionOptions';
 export { envToBool } from './utils/envToBool';
 export { applyScopeDataToEvent, mergeScopeData, getCombinedScopeData } from './utils/scopeData';
 export { prepareEvent } from './utils/prepareEvent';
