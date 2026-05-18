c01a8e38a8ff5621e2e4bd79cf50b794d88e79c9
Francesco Gringl-Novy <francesco.novy@sentry.io>
2026-05-18 15:08:05 +0200

    fix ember-vite app

--- dev-packages/e2e-tests/test-applications/ember-vite/playwright.config.ts
+++ dev-packages/e2e-tests/test-applications/ember-vite/playwright.config.ts
@@ -1,44 +1,10 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+import { getPlaywrightConfig } from '@sentry-internal/test-utils';
 
-// Fix urls not resolving to localhost on Node v17+
-import { setDefaultResultOrder } from 'dns';
-setDefaultResultOrder('ipv4first');
-
-const emberPort = 4020;
-const eventProxyPort = 3031;
-
-const config: PlaywrightTestConfig = {
-  testDir: './tests',
-  timeout: 30_000,
-  expect: {
-    timeout: 10000,
-  },
-  fullyParallel: false,
-  workers: 1,
-  forbidOnly: !!process.env.CI,
-  retries: 0,
-  reporter: process.env.CI ? [['list'], ['junit', { outputFile: 'results.junit.xml' }]] : 'list',
-  use: {
-    baseURL: `http://localhost:${emberPort}`,
-    trace: 'on-first-retry',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
-  webServer: [
-    {
-      command: 'pnpm ts-node-script start-event-proxy.ts',
-      port: eventProxyPort,
-    },
-    {
-      command: `pnpm start`,
-      port: emberPort,
-    },
-  ],
-};
+const config = getPlaywrightConfig({
+  startCommand: 'pnpm start',
+  eventProxyFile: 'start-event-proxy.mjs',
+  eventProxyPort: 3031,
+  port: 3030,
+});
 
 export default config;
