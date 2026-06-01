f58a6ed18605e19747c1d949753935957a120d61
isaacs <i@izs.me>
2026-06-01 16:26:05 -0700

    feat(orchestrion): move into server-utils
    Also, fix the `import ... from 'node:module'` in the import-hook.mjs,
    which was broken.

---
+++ packages/server-utils/src/orchestrion/index.ts
@@ -0,0 +1,3 @@
+export { setupOrchestrion } from './setup';
+export type { SetupOrchestrionOptions } from './setup';
+export { mysqlChannelIntegration } from '../integrations/tracing-channel/mysql';
