32d9d49e4888c6e6f30d116df4c778504d379afc
Charly Gomez <charly.gomez1310@gmail.com>
2026-06-03 16:58:09 +0200

    fix(react): Detect React Router v6/v7 navigations in a layout effect to propagate the correct trace (#21326)
    Fixes React Router v6/v7 propagating the stale pageload trace for
    requests in route mount effects
    
    closes https://github.com/getsentry/sentry-javascript/issues/21322

--- dev-packages/e2e-tests/test-applications/react-router-6/src/index.tsx
+++ dev-packages/e2e-tests/test-applications/react-router-6/src/index.tsx
@@ -11,6 +11,7 @@ import {
   useNavigationType,
 } from 'react-router-dom';
 import Index from './pages/Index';
+import Products from './pages/Products';
 import SSE from './pages/SSE';
 import User from './pages/User';
 
@@ -51,6 +52,7 @@ root.render(
     <SentryRoutes>
       <Route path="/" element={<Index />} />
       <Route path="/user/:id" element={<User />} />
+      <Route path="/products" element={<Products />} />
       <Route path="/sse" element={<SSE />} />
     </SentryRoutes>
   </BrowserRouter>,
