12bd9a79ae0a457387f4c072cf52ca65ef99d51b
isaacs <i@izs.me>
2026-06-04 13:22:04 -0700

    chore: isolate the bundler-plugins package for merge
    - Remove all contents that are not going to be ported to the
      sentry-javascript monorepo.
    - Move the integration tests into a suitable location.

--- packages/babel-plugin-component-annotate/src/index.ts
+++
@@ -1,4 +0,0 @@
-export {
-  default,
-  experimentalComponentNameAnnotatePlugin,
-} from "@sentry/bundler-plugins/babel-plugin";
--- packages/playground/src/hello-world.js
+++
@@ -1,3 +0,0 @@
-export function helloWorld() {
-  console.log("Hello world!");
-}
