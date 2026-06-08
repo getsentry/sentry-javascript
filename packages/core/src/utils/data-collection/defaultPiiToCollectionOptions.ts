5b81b4123ce96d5d41e9bd5a10f7a748dc35994c
Sigrid <32902192+s1gr1d@users.noreply.github.com>
2026-06-08 10:42:54 +0200

    feat(core): Change default of `dataCollection.userInfo` to `true` (#21348)
    Changes the default of `dataColelction.userInfo` to `true`.
    
    Closes https://github.com/getsentry/sentry-javascript/issues/21345

--- packages/core/src/utils/data-collection/defaultPiiToCollectionOptions.ts
+++ packages/core/src/utils/data-collection/defaultPiiToCollectionOptions.ts
@@ -3,6 +3,10 @@ import type { ResolvedDataCollection } from '../../types/datacollection';
 
 /**
  * Helper function that maps the `sendDefaultPii` boolean flag to the corresponding `DataCollection` configuration.
+ * Used as a backward-compatibility bridge when `dataCollection` is not set by the user.
+ *
+ * TODO(v11): Remove this function along with `sendDefaultPii`. Once `dataCollection` is the only API,
+ * the DEFAULTS in `resolveDataCollectionOptions` (including `userInfo: true`) will always apply.
  */
 export function defaultPiiToCollectionOptions(sendDefaultPii?: boolean): ResolvedDataCollection {
   return sendDefaultPii === true
