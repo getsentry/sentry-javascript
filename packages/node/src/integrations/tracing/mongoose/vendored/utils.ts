2ab1d677963b25cb3219993c105c55add66536a7
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-06-11 15:25:29 -0400

    ref(node): Swap mongoose vendored span lifecycle to Sentry APIs
    Replace the OpenTelemetry tracing APIs in the vendored mongoose
    instrumentation with Sentry primitives (startInactiveSpan, getActiveSpan,
    span.setStatus) and bake the span origin in directly. Also drops the
    config paths that are dead in Sentry's context (dbStatementSerializer,
    requireParentSpan, suppressInternalInstrumentation) and the
    SemconvStability dual-emission machinery, keeping only the OLD attribute
    set. The OpenTelemetry instrumentation base/module-patching layer is kept.

--- packages/node/src/integrations/tracing/mongoose/vendored/utils.ts
+++ packages/node/src/integrations/tracing/mongoose/vendored/utils.ts
@@ -17,13 +17,13 @@
  * - Vendored from: https://github.com/open-telemetry/opentelemetry-js-contrib/tree/15ef7506553f631ea4181391e0c5725a56f0d082/packages/instrumentation-mongoose
  * - Upstream version: @opentelemetry/instrumentation-mongoose@0.64.0
  * - Types vendored from mongoose as simplified interfaces
+ * - Refactored to use Sentry's span APIs instead of OpenTelemetry tracing APIs
  */
 /* eslint-disable */
 
-import { Attributes, SpanStatusCode, diag, Span } from '@opentelemetry/api';
+import type { Span, SpanAttributes } from '@sentry/core';
+import { SPAN_STATUS_ERROR } from '@sentry/core';
 import type { Collection } from './mongoose-types';
-import { MongooseResponseCustomAttributesFunction } from './types';
-import { safeExecuteInTheMiddle, SemconvStability } from '@opentelemetry/instrumentation';
 import {
   ATTR_DB_MONGODB_COLLECTION,
   ATTR_DB_NAME,
@@ -31,90 +31,32 @@ import {
   ATTR_NET_PEER_NAME,
   ATTR_NET_PEER_PORT,
 } from './semconv';
-import {
-  ATTR_DB_COLLECTION_NAME,
-  ATTR_DB_NAMESPACE,
-  ATTR_SERVER_ADDRESS,
-  ATTR_SERVER_PORT,
-} from '@opentelemetry/semantic-conventions';
 
-export function getAttributesFromCollection(
-  collection: Collection,
-  dbSemconvStability: SemconvStability,
-  netSemconvStability: SemconvStability,
-): Attributes {
-  const attrs: Attributes = {};
-
-  if (dbSemconvStability & SemconvStability.OLD) {
-    attrs[ATTR_DB_MONGODB_COLLECTION] = collection.name;
-    attrs[ATTR_DB_NAME] = collection.conn.name;
-    attrs[ATTR_DB_USER] = collection.conn.user;
-  }
-  if (dbSemconvStability & SemconvStability.STABLE) {
-    attrs[ATTR_DB_COLLECTION_NAME] = collection.name;
-    attrs[ATTR_DB_NAMESPACE] = collection.conn.name;
-  }
-
-  if (netSemconvStability & SemconvStability.OLD) {
-    attrs[ATTR_NET_PEER_NAME] = collection.conn.host;
-    attrs[ATTR_NET_PEER_PORT] = collection.conn.port;
-  }
-  if (netSemconvStability & SemconvStability.STABLE) {
-    attrs[ATTR_SERVER_ADDRESS] = collection.conn.host;
-    attrs[ATTR_SERVER_PORT] = collection.conn.port;
-  }
-
-  return attrs;
+export function getAttributesFromCollection(collection: Collection): SpanAttributes {
+  return {
+    [ATTR_DB_MONGODB_COLLECTION]: collection.name,
+    [ATTR_DB_NAME]: collection.conn.name,
+    [ATTR_DB_USER]: collection.conn.user,
+    [ATTR_NET_PEER_NAME]: collection.conn.host,
+    [ATTR_NET_PEER_PORT]: collection.conn.port,
+  };
 }
 
-function setErrorStatus(span: Span, error: any = {}) {
-  span.recordException(error);
-
+function setErrorStatus(span: Span, error: any = {}): void {
   span.setStatus({
-    code: SpanStatusCode.ERROR,
+    code: SPAN_STATUS_ERROR,
     message: `${error.message} ${error.code ? `\nMongoose Error Code: ${error.code}` : ''}`,
   });
 }
 
-function applyResponseHook(
-  span: Span,
-  response: any,
-  responseHook?: MongooseResponseCustomAttributesFunction,
-  moduleVersion: string | undefined = undefined,
-) {
-  if (!responseHook) {
-    return;
-  }
-
-  safeExecuteInTheMiddle(
-    () => responseHook(span, { moduleVersion, response }),
-    e => {
-      if (e) {
-        diag.error('mongoose instrumentation: responseHook error', e);
-      }
-    },
-    true,
-  );
-}
-
-export function handlePromiseResponse(
-  execResponse: any,
-  span: Span,
-  responseHook?: MongooseResponseCustomAttributesFunction,
-  moduleVersion: string | undefined = undefined,
-): any {
+export function handlePromiseResponse(execResponse: any, span: Span): any {
   if (!(execResponse instanceof Promise)) {
-    applyResponseHook(span, execResponse, responseHook, moduleVersion);
     span.end();
     return execResponse;
   }
 
   return execResponse
-    .then(response => {
-      applyResponseHook(span, response, responseHook, moduleVersion);
-      return response;
-    })
-    .catch(err => {
+    .catch((err: any) => {
       setErrorStatus(span, err);
       throw err;
     })
@@ -127,8 +69,6 @@ export function handleCallbackResponse(
   originalThis: any,
   span: Span,
   args: IArguments,
-  responseHook?: MongooseResponseCustomAttributesFunction,
-  moduleVersion: string | undefined = undefined,
 ) {
   let callbackArgumentIndex = 0;
   if (args.length === 2) {
@@ -140,8 +80,6 @@ export function handleCallbackResponse(
   args[callbackArgumentIndex] = (err: Error, response: any): any => {
     if (err) {
       setErrorStatus(span, err);
-    } else {
-      applyResponseHook(span, response, responseHook, moduleVersion);
     }
 
     span.end();
