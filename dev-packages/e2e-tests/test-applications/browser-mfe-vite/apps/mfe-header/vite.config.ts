52986e7928fbda895e7834ced3f4b35bd7d9f7d0
gioboa <giorgiob.boa@gmail.com>
2026-06-17 15:13:17 +0200

    chore: use official @module-federation/vite

--- dev-packages/e2e-tests/test-applications/browser-mfe-vite/apps/mfe-header/vite.config.ts
+++ dev-packages/e2e-tests/test-applications/browser-mfe-vite/apps/mfe-header/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import federation from '@originjs/vite-plugin-federation';
+import { federation } from '@module-federation/vite';
 
 export default defineConfig({
   plugins: [
