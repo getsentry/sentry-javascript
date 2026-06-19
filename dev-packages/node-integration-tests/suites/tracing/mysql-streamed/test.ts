753ab1e5b107df2f2e7a153e9cd82d68f6f65125
Lukas Stracke <lukas.stracke@sentry.io>
2026-06-19 13:08:49 +0200

    streamline mysql tests, explain error status

--- dev-packages/node-integration-tests/suites/tracing/mysql-streamed/test.ts
+++ dev-packages/node-integration-tests/suites/tracing/mysql-streamed/test.ts
@@ -18,158 +18,102 @@ describe('mysql auto instrumentation (streamed)', () => {
 
     expect(dbSpans.length).toBe(2);
 
+    const COMMON_ATTRIBUTES = {
+      'db.connection_string': {
+        type: 'string',
+        value: expect.stringMatching(/^jdbc:mysql:\/\/localhost:.*/),
+      },
+      'db.system': {
+        type: 'string',
+        value: 'mysql',
+      },
+      'db.user': {
+        type: 'string',
+        value: 'root',
+      },
+      'net.peer.name': {
+        type: 'string',
+        value: 'localhost',
+      },
+      'net.peer.port': {
+        type: 'integer',
+        value: expect.any(Number),
+      },
+      'otel.kind': {
+        type: 'string',
+        value: 'CLIENT',
+      },
+      'sentry.environment': {
+        type: 'string',
+        value: 'production',
+      },
+      'sentry.op': {
+        type: 'string',
+        value: 'db',
+      },
+      'sentry.release': {
+        type: 'string',
+        value: '1.0',
+      },
+      'sentry.sdk.name': {
+        type: 'string',
+        value: 'sentry.javascript.node',
+      },
+      'sentry.sdk.version': {
+        type: 'string',
+        value: expect.any(String),
+      },
+      'sentry.segment.id': {
+        type: 'string',
+        value: expect.stringMatching(/^[\da-f]{16}$/),
+      },
+      'sentry.segment.name': {
+        type: 'string',
+        value: 'Test Transaction',
+      },
+      'sentry.source': {
+        type: 'string',
+        value: 'task',
+      },
+      'sentry.span.source': {
+        type: 'string',
+        value: 'task',
+      },
+    };
+
+    const COMMON_SPAN_PROPS = {
+      end_timestamp: expect.any(Number),
+      is_segment: false,
+      parent_span_id: expect.stringMatching(/^[\da-f]{16}$/),
+      span_id: expect.stringMatching(/^[\da-f]{16}$/),
+      start_timestamp: expect.any(Number),
+      // all db spans have an error status because we don't have an actual mysql DB server running for these tests
+      status: 'error',
+      trace_id: expect.stringMatching(/^[\da-f]{32}$/),
+    };
+
     expect(dbSpans).toEqual([
       {
         attributes: {
-          'db.connection_string': {
-            type: 'string',
-            value: expect.stringMatching(/^jdbc:mysql:\/\/localhost:.*/),
-          },
+          ...COMMON_ATTRIBUTES,
           'db.statement': {
             type: 'string',
             value: 'SELECT 1 + 1 AS solution',
           },
-          'db.system': {
-            type: 'string',
-            value: 'mysql',
-          },
-          'db.user': {
-            type: 'string',
-            value: 'root',
-          },
-          'net.peer.name': {
-            type: 'string',
-            value: 'localhost',
-          },
-          'net.peer.port': {
-            type: 'integer',
-            value: expect.any(Number),
-          },
-          'otel.kind': {
-            type: 'string',
-            value: 'CLIENT',
-          },
-          'sentry.environment': {
-            type: 'string',
-            value: 'production',
-          },
-          'sentry.op': {
-            type: 'string',
-            value: 'db',
-          },
-          'sentry.release': {
-            type: 'string',
-            value: '1.0',
-          },
-          'sentry.sdk.name': {
-            type: 'string',
-            value: 'sentry.javascript.node',
-          },
-          'sentry.sdk.version': {
-            type: 'string',
-            value: expect.any(String),
-          },
-          'sentry.segment.id': {
-            type: 'string',
-            value: expect.stringMatching(/^[\da-f]{16}$/),
-          },
-          'sentry.segment.name': {
-            type: 'string',
-            value: 'Test Transaction',
-          },
-          'sentry.source': {
-            type: 'string',
-            value: 'task',
-          },
-          'sentry.span.source': {
-            type: 'string',
-            value: 'task',
-          },
         },
-        end_timestamp: expect.any(Number),
-        is_segment: false,
         name: 'SELECT 1 + 1 AS solution',
-        parent_span_id: expect.stringMatching(/^[\da-f]{16}$/),
-        span_id: expect.stringMatching(/^[\da-f]{16}$/),
-        start_timestamp: expect.any(Number),
-        status: 'error',
-        trace_id: expect.stringMatching(/^[\da-f]{32}$/),
+        ...COMMON_SPAN_PROPS,
       },
       {
         attributes: {
-          'db.connection_string': {
-            type: 'string',
-            value: expect.stringMatching(/^jdbc:mysql:\/\/localhost:.*/),
-          },
+          ...COMMON_ATTRIBUTES,
           'db.statement': {
             type: 'string',
             value: 'SELECT NOW()',
           },
-          'db.system': {
-            type: 'string',
-            value: 'mysql',
-          },
-          'db.user': {
-            type: 'string',
-            value: 'root',
-          },
-          'net.peer.name': {
-            type: 'string',
-            value: 'localhost',
-          },
-          'net.peer.port': {
-            type: 'integer',
-            value: expect.any(Number),
-          },
-          'otel.kind': {
-            type: 'string',
-            value: 'CLIENT',
-          },
-          'sentry.environment': {
-            type: 'string',
-            value: 'production',
-          },
-          'sentry.op': {
-            type: 'string',
-            value: 'db',
-          },
-          'sentry.release': {
-            type: 'string',
-            value: '1.0',
-          },
-          'sentry.sdk.name': {
-            type: 'string',
-            value: 'sentry.javascript.node',
-          },
-          'sentry.sdk.version': {
-            type: 'string',
-            value: expect.any(String),
-          },
-          'sentry.segment.id': {
-            type: 'string',
-            value: expect.stringMatching(/^[\da-f]{16}$/),
-          },
-          'sentry.segment.name': {
-            type: 'string',
-            value: 'Test Transaction',
-          },
-          'sentry.source': {
-            type: 'string',
-            value: 'task',
-          },
-          'sentry.span.source': {
-            type: 'string',
-            value: 'task',
-          },
         },
-        end_timestamp: expect.any(Number),
-        is_segment: false,
         name: 'SELECT NOW()',
-        parent_span_id: expect.stringMatching(/^[\da-f]{16}$/),
-        span_id: expect.stringMatching(/^[\da-f]{16}$/),
-        start_timestamp: expect.any(Number),
-        status: 'error',
-        trace_id: expect.stringMatching(/^[\da-f]{32}$/),
+        ...COMMON_SPAN_PROPS,
       },
     ]);
   };
