ce4eca5cd479308382c74c9902fe362a13dbaa28
suzunn <slymnuzn@hotmail.com>
2026-06-19 16:07:57 +0300

    fix: enforce cloudflare nodejs compat flag

--- packages/cloudflare/src/async.ts
+++ packages/cloudflare/src/async.ts
@@ -8,7 +8,7 @@ import { getDefaultCurrentScope, getDefaultIsolationScope, setAsyncContextStrate
  * Sets the async context strategy to use AsyncLocalStorage.
  *
  * AsyncLocalStorage is only available in the cloudflare workers runtime if you set
- * compatibility_flags = ["nodejs_compat"] or compatibility_flags = ["nodejs_als"]
+ * compatibility_flags = ["nodejs_compat"]
  *
  * @internal Only exported to be used in higher-level Sentry packages
  * @hidden Only exported to be used in higher-level Sentry packages
