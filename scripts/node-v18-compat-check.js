b0365659688280eb724aa5dab2963ccad45b9c9f
Tim Fish <tim@timfish.uk>
2026-06-19 16:49:57 +0100

    Fix

--- scripts/node-v18-compat-check.js
+++ scripts/node-v18-compat-check.js
@@ -2,6 +2,6 @@
 
 // Verify the Node SDK can be loaded without errors on Node v18.0.0 (the minimum supported version).
 // This catches accidental use of Node APIs that don't exist in v18.0.0.
-require('./packages/node/build/cjs/index.js');
+require('@sentry/node');
 
 console.log('Node v18.0.0 compatibility check passed');
