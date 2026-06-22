0b32c8e670ddc2ba69ca9e2867c96d3462d37813
Tim Fish <tim@timfish.uk>
2026-06-22 12:51:31 +0100

    Simplify

--- packages/node/src/integrations/fs/index.ts
+++ packages/node/src/integrations/fs/index.ts
@@ -1,9 +1,8 @@
-import { defineIntegration } from '@sentry/core';
-import { enableFsInstrumentation } from './vendored/instrumentation';
+import { defineIntegration } from "@sentry/core";
+import { enableFsInstrumentation } from "./vendored/instrumentation";
+import type { FsInstrumentationConfig } from "./vendored/types";
 
-const INTEGRATION_NAME = 'FileSystem';
-
-let _isEnabled = false;
+const INTEGRATION_NAME = "FileSystem";
 
 /**
  * This integration will create spans for `fs` API operations, like reading and writing files.
@@ -14,37 +13,11 @@ let _isEnabled = false;
  *
  * @param options Configuration for this integration.
  */
-export const fsIntegration = defineIntegration(
-  (
-    options: {
-      /**
-       * Whether to enable the plugin.
-       * @default true
-       */
-      enabled?: boolean;
-      /**
-       * Setting this option to `true` will include any filepath arguments from your `fs` API calls as span attributes.
-       *
-       * Defaults to `false`.
-       */
-      recordFilePaths?: boolean;
-
-      /**
-       * Setting this option to `true` will include the error messages of failed `fs` API calls as a span attribute.
-       *
-       * Defaults to `false`.
-       */
-      recordErrorMessagesAsSpanAttributes?: boolean;
-    } = {},
-  ) => {
-    return {
-      name: INTEGRATION_NAME,
-      setupOnce() {
-        if (options.enabled === false) return;
-        if (_isEnabled) return;
-        _isEnabled = true;
-        enableFsInstrumentation(options);
-      },
-    };
-  },
-);
+export const fsIntegration = defineIntegration((options: FsInstrumentationConfig = {}) => {
+  return {
+    name: INTEGRATION_NAME,
+    setupOnce() {
+      enableFsInstrumentation(options);
+    },
+  };
+});
