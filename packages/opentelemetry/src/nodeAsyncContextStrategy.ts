7d9ba652cbd47c1904339c1a6b23333ab2d71986
Francesco Novy <francesco.novy@sentry.io>
2026-06-26 13:51:39 +0200

    fixes

--- packages/opentelemetry/src/nodeAsyncContextStrategy.ts
+++ packages/opentelemetry/src/nodeAsyncContextStrategy.ts
@@ -25,7 +25,7 @@ export function setNodeOpenTelemetryContextAsyncContextStrategy(options?: { skip
  * In the default case, we build the local storage instance ourselves here.
  * The default asyncLocalStorageContextManager will then use this internally.
  */
-function getDefaultAsyncLocalStorageFactory() {
+function getDefaultAsyncLocalStorageFactory(): () => TracingChannelBinding {
   const defaultAsyncLocalStorage = new AsyncLocalStorage<api.Context>();
 
   return () => {
@@ -41,16 +41,18 @@ function getDefaultAsyncLocalStorageFactory() {
  * this may not be available yet, if this is called before the Otel ContextManager was setup
  * in this case, we need to return undefined and retry later, hoping that the setup works by then
  */
-function getCustomAsyncLocalStorageFactory() {
+function getCustomAsyncLocalStorageFactory(): () => TracingChannelBinding | undefined {
   return () => {
     try {
       const contextManager = (api.context as unknown as ContextApi)._getContextManager();
       const asyncLocalStorage = contextManager?.getAsyncLocalStorageLookup().asyncLocalStorage;
 
-      return {
-        asyncLocalStorage,
-        getStoreWithActiveSpan: span => api.trace.setSpan(api.context.active(), span as api.Span),
-      } satisfies TracingChannelBinding;
+      return asyncLocalStorage
+        ? ({
+            asyncLocalStorage,
+            getStoreWithActiveSpan: span => api.trace.setSpan(api.context.active(), span as api.Span),
+          } satisfies TracingChannelBinding)
+        : undefined;
     } catch {
       return undefined;
     }
