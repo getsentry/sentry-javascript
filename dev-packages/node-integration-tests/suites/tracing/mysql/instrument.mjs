66dd2154149a88d60a5b1fd44ad646296806c4ba
Francesco Novy <francesco.novy@sentry.io>
2026-07-01 15:27:24 +0200

    test(node): Collapse mysql span streaming tests into same suite

--- dev-packages/node-integration-tests/suites/tracing/mysql/instrument.mjs
+++ dev-packages/node-integration-tests/suites/tracing/mysql/instrument.mjs
@@ -1,9 +1,14 @@
 import * as Sentry from '@sentry/node';
 import { loggingTransport } from '@sentry-internal/node-integration-tests';
 
+if (process.env.ORCHESTRION === 'true') {
+  Sentry.experimentalUseDiagnosticsChannelInjection();
+}
+
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   release: '1.0',
   tracesSampleRate: 1.0,
   transport: loggingTransport,
+  traceLifecycle: process.env.STREAMED === 'true' ? 'stream' : undefined,
 });
