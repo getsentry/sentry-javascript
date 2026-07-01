7b57b84f48340efca98c0b1c99a674addf2295fa
Francesco Novy <francesco.novy@sentry.io>
2026-07-01 14:33:28 +0200

    tests

--- dev-packages/node-integration-tests/suites/express/tracing/instrument-filterStatusCode.mjs
+++ dev-packages/node-integration-tests/suites/express/tracing/instrument-filterStatusCode.mjs
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/node';
 import { loggingTransport } from '@sentry-internal/node-integration-tests';
 
+if (process.env.USE_ORCHESTRION === 'true') {
+  Sentry.experimentalUseDiagnosticsChannelInjection();
+}
 Sentry.init({
   dsn: 'https://public@dsn.ingest.sentry.io/1337',
   release: '1.0',
