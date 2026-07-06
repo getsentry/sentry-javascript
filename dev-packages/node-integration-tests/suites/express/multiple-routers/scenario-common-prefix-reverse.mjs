0e5a9251a7f9f7e81a14917659677998a8378f2e
Francesco Gringl-Novy <francesco.novy@sentry.io>
2026-07-06 11:30:58 +0200

    test(node): Refactor remaining express integrations tests to new runner (#21949)
    This migrates the remaining node integration tests for express to the
    new runner format (so they also auto-run for orchestrion etc). I opted
    to run them cjs only to keep things faster, this should be fine for now
    imho.
    
    Tests have been collapsed how it makes sense. Test logic itself should
    not have changed.

---
+++ dev-packages/node-integration-tests/suites/express/multiple-routers/scenario-common-prefix-reverse.mjs
@@ -0,0 +1,24 @@
+import * as Sentry from '@sentry/node';
+import { startExpressServerAndSendPortToRunner } from '@sentry-internal/node-integration-tests';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+const APIv1 = express.Router();
+
+APIv1.get('/user/:userId', function (_req, res) {
+  Sentry.captureMessage('Custom Message');
+  res.send('Success');
+});
+
+const root = express.Router();
+
+app.use('/api/v1', APIv1);
+app.use('/api', root);
+
+Sentry.setupExpressErrorHandler(app);
+
+startExpressServerAndSendPortToRunner(app);
