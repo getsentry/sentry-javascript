1fe8abc14797409ad43729911cda5da92fd257e9
Lukas Stracke <lukas.stracke@sentry.io>
2026-07-14 14:49:30 +0200

    fix(browser): Propagate trace data from active span if http.client span is not recording

---
+++ dev-packages/browser-integration-tests/suites/tracing/ignoreSpans-streamed/continued-trace-http-client/init.js
@@ -0,0 +1,11 @@
+import * as Sentry from '@sentry/browser';
+
+window.Sentry = Sentry;
+
+Sentry.init({
+  dsn: 'https://public@dsn.ingest.sentry.io/1337',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.spanStreamingIntegration()],
+  ignoreSpans: [{ op: 'http.client' }],
+  tracePropagationTargets: ['sentry-test-external.io'],
+  tracesSampleRate: 0,
+});
