5208cbefb8be5c7edc2136a5bc0e1f6c7b8a9850
Tim Fish <tim@timfish.uk>
2026-07-16 20:43:31 +0100

    bump size limit

--- .size-limit.js
+++ .size-limit.js
@@ -400,7 +400,7 @@ module.exports = [
     import: createImport('init', 'experimentalUseDiagnosticsChannelInjection'),
     ignore: [...builtinModules, ...nodePrefixedBuiltinModules],
     gzip: true,
-    limit: '142 KB',
+    limit: '145 KB',
     disablePlugins: ['@size-limit/esbuild'],
   },
   {
