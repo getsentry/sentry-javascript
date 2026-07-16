63e61efdffdafe120468460b10bd21f1e34d0003
isaacs <i@izs.me>
2026-07-16 16:43:08 -0700

    fixup! feat(mongoose): implement orchestrion mongoose integration

--- .size-limit.js
+++ .size-limit.js
@@ -400,7 +400,7 @@ module.exports = [
     import: createImport('init', 'experimentalUseDiagnosticsChannelInjection'),
     ignore: [...builtinModules, ...nodePrefixedBuiltinModules],
     gzip: true,
-    limit: '148 KB',
+    limit: '150 KB',
     disablePlugins: ['@size-limit/esbuild'],
   },
   {
