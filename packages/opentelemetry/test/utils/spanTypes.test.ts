348851a952d1b4060e8e71c6f7583c72874b6859
Francesco Novy <francesco.novy@sentry.io>
2026-07-24 11:12:34 +0200

    fix tests

--- packages/opentelemetry/test/utils/spanTypes.test.ts
+++ packages/opentelemetry/test/utils/spanTypes.test.ts
@@ -1,6 +1,6 @@
 import type { Span } from '@opentelemetry/api';
 import { describe, expect, it } from 'vitest';
-import { spanHasAttributes, spanHasEvents, spanHasKind, spanHasParentId } from '../../src/utils/spanTypes';
+import { spanHasAttributes } from '../../src/utils/spanTypes';
 
 describe('spanTypes', () => {
   describe('spanHasAttributes', () => {
@@ -16,59 +16,6 @@ describe('spanTypes', () => {
 
       if (actual) {
         expect(castSpan.attributes).toBeDefined();
-      }
-    });
-  });
-
-  describe('spanHasKind', () => {
-    it.each([
-      [{}, false],
-      [{ kind: null }, false],
-      [{ kind: 0 }, true],
-      [{ kind: 5 }, true],
-      [{ kind: 'TEST_KIND' }, false],
-    ])('works with %j', (span, expected) => {
-      const castSpan = span as unknown as Span;
-      const actual = spanHasKind(castSpan);
-
-      expect(actual).toBe(expected);
-
-      if (actual) {
-        expect(castSpan.kind).toBeDefined();
-      }
-    });
-  });
-
-  describe('spanHasParentId', () => {
-    it.each([
-      [{}, false],
-      [{ parentSpanId: null }, false],
-      [{ parentSpanId: 'TEST_PARENT_ID' }, true],
-    ])('works with %j', (span, expected) => {
-      const castSpan = span as unknown as Span;
-      const actual = spanHasParentId(castSpan);
-
-      expect(actual).toBe(expected);
-
-      if (actual) {
-        expect(castSpan.parentSpanId).toBeDefined();
-      }
-    });
-  });
-
-  describe('spanHasEvents', () => {
-    it.each([
-      [{}, false],
-      [{ events: null }, false],
-      [{ events: [] }, true],
-    ])('works with %j', (span, expected) => {
-      const castSpan = span as unknown as Span;
-      const actual = spanHasEvents(castSpan);
-
-      expect(actual).toBe(expected);
-
-      if (actual) {
-        expect(castSpan.events).toBeDefined();
       }
     });
   });
