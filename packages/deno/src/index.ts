b001079630ac0042b1f9e8d2036513aaef60fd9a
isaacs <i@izs.me>
2026-07-23 18:22:40 -0700

    feat(deno): add lruMemoizer integration (#22453)

--- packages/deno/src/index.ts
+++ packages/deno/src/index.ts
@@ -121,6 +121,7 @@ export {
   genericPoolChannelIntegration,
   knexChannelIntegration,
   koaChannelIntegration,
+  lruMemoizerChannelIntegration,
   mongodbChannelIntegration,
   mongooseChannelIntegration,
   mysqlChannelIntegration,
