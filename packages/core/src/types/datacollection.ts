8596b15ebf54be113a97f6ac3945792cca56132d
Sigrid <32902192+s1gr1d@users.noreply.github.com>
2026-07-28 15:16:56 +0200

    feat(core)!: Remove `queryParams` in favor of `urlQueryParams` (#22711)
    Migration docs will be added all at once for all `dataCollection`
    changes.
    
    Closes https://github.com/getsentry/sentry-javascript/issues/22311

--- packages/core/src/types/datacollection.ts
+++ packages/core/src/types/datacollection.ts
@@ -44,9 +44,6 @@ export interface DataCollection {
    */
   httpBodies?: HttpBodyCollectionTarget[];
 
-  /** @deprecated Use `urlQueryParams` instead. */
-  queryParams?: CollectBehavior;
-
   /**
    * Controls URL query parameter collection and sensitive value filtering.
    * @default true
@@ -112,8 +109,7 @@ export interface DataCollection {
 /**
  * Fully resolved `DataCollection` with all defaults applied.
  */
-// todo(v11): change `Omit<DataCollection, 'queryParams'>` to just `DataCollection`
-export type ResolvedDataCollection = Required<Omit<DataCollection, 'queryParams'>> & {
+export type ResolvedDataCollection = Required<DataCollection> & {
   httpHeaders: Required<NonNullable<DataCollection['httpHeaders']>>;
   graphQL: Required<NonNullable<DataCollection['graphQL']>>;
   genAI: Required<NonNullable<DataCollection['genAI']>>;
