93d6d0018b675a02a1f257f2e207b02e76403b77
s1gr1d <32902192+s1gr1d@users.noreply.github.com>
2026-07-29 13:49:49 +0200

    copy solidstart

---
+++ dev-packages/e2e-tests/test-applications/solidstart-2/tests/errors.server.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test';
+import { waitForError } from '@sentry-internal/test-utils';
+
+test.describe('server-side errors', () => {
+  test('captures server action error', async ({ page }) => {
+    const errorEventPromise = waitForError('solidstart', errorEvent => {
+      return errorEvent?.exception?.values?.[0]?.value === 'Error thrown from Solid Start E2E test app server route';
+    });
+
+    await page.goto(`/server-error`);
+
+    const error = await errorEventPromise;
+
+    expect(error).toMatchObject({
+      exception: {
+        values: [
+          {
+            type: 'Error',
+            value: 'Error thrown from Solid Start E2E test app server route',
+            mechanism: {
+              type: 'auto.function.solidstart',
+              handled: false,
+            },
+          },
+        ],
+      },
+      transaction: 'GET /server-error',
+    });
+  });
+});
---
+++ dev-packages/e2e-tests/test-applications/solidstart-2/tests/performance.client.test.ts
@@ -0,0 +1,119 @@
+import { expect, test } from '@playwright/test';
+import { waitForTransaction } from '@sentry-internal/test-utils';
+
+test('sends a pageload transaction', async ({ page }) => {
+  const transactionPromise = waitForTransaction('solidstart', async transactionEvent => {
+    return transactionEvent?.transaction === '/' && transactionEvent.contexts?.trace?.op === 'pageload';
+  });
+
+  await page.goto('/');
+  const pageloadTransaction = await transactionPromise;
+
+  expect(pageloadTransaction).toMatchObject({
+    contexts: {
+      trace: {
+        op: 'pageload',
+        origin: 'auto.pageload.browser',
+        data: {
+          'sentry.source': 'route',
+          'url.template': '/',
+          'url.path': '/',
+          'url.full': expect.stringMatching(/^https?:\/\/localhost:\d+\/$/),
+        },
+      },
+    },
+    transaction: '/',
+    transaction_info: {
+      source: 'route',
+    },
+  });
+});
+
+test('sends a navigation transaction with parametrized route', async ({ page }) => {
+  const transactionPromise = waitForTransaction('solidstart', async transactionEvent => {
+    return transactionEvent?.transaction === '/users/:id' && transactionEvent.contexts?.trace?.op === 'navigation';
+  });
+
+  await page.goto(`/`);
+  await page.locator('#navLink').click();
+  const navigationTransaction = await transactionPromise;
+
+  expect(navigationTransaction).toMatchObject({
+    contexts: {
+      trace: {
+        op: 'navigation',
+        origin: 'auto.navigation.solidstart.solidrouter',
+        data: {
+          'sentry.source': 'route',
+          'url.template': '/users/:id',
+          'url.path': '/users/5',
+          'url.full': expect.stringMatching(/^https?:\/\/localhost:\d+\/users\/5$/),
+        },
+      },
+    },
+    transaction: '/users/:id',
+    transaction_info: {
+      source: 'route',
+    },
+  });
+});
+
+test('updates the transaction when using the back button', async ({ page }) => {
+  // Solid Router sends a `-1` navigation when using the back button.
+  // The sentry solidRouterBrowserTracingIntegration tries to update such
+  // transactions with the proper name once the `useLocation` hook triggers.
+  const navigationTxnPromise = waitForTransaction('solidstart', async transactionEvent => {
+    return transactionEvent?.transaction === '/users/:id' && transactionEvent.contexts?.trace?.op === 'navigation';
+  });
+
+  await page.goto(`/back-navigation`);
+  await page.locator('#navLink').click();
+  const navigationTxn = await navigationTxnPromise;
+
+  expect(navigationTxn).toMatchObject({
+    contexts: {
+      trace: {
+        op: 'navigation',
+        origin: 'auto.navigation.solidstart.solidrouter',
+        data: {
+          'sentry.source': 'route',
+          'url.template': '/users/:id',
+          'url.path': '/users/6',
+          'url.full': expect.stringMatching(/^https?:\/\/localhost:\d+\/users\/6$/),
+        },
+      },
+    },
+    transaction: '/users/:id',
+    transaction_info: {
+      source: 'route',
+    },
+  });
+
+  const backNavigationTxnPromise = waitForTransaction('solidstart', async transactionEvent => {
+    return (
+      transactionEvent?.transaction === '/back-navigation' && transactionEvent.contexts?.trace?.op === 'navigation'
+    );
+  });
+
+  await page.goBack();
+  const backNavigationTxn = await backNavigationTxnPromise;
+
+  expect(backNavigationTxn).toMatchObject({
+    contexts: {
+      trace: {
+        op: 'navigation',
+        origin: 'auto.navigation.solidstart.solidrouter',
+        data: {
+          'sentry.source': 'route',
+          'url.template': '/back-navigation',
+          'url.path': '/back-navigation',
+          'url.full': expect.stringMatching(/^https?:\/\/localhost:\d+\/back-navigation$/),
+        },
+      },
+    },
+    transaction: '/back-navigation',
+    transaction_info: {
+      source: 'route',
+    },
+  });
+});
