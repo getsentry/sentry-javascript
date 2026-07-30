43b4301acd845f72176d6d4adec3f81efe521f81
Charly Gomez <charly.gomez1310@gmail.com>
2026-07-30 11:49:48 +0200

    Revert "test(e2e/astro): Run orchestrion e2e in dev mode too"
    This reverts commit f0e65b40d6273915ada765b89d6bc87594661864.

--- dev-packages/e2e-tests/test-applications/astro-7-orchestrion/playwright.config.mjs
+++ dev-packages/e2e-tests/test-applications/astro-7-orchestrion/playwright.config.mjs
@@ -1,13 +1,7 @@
 import { getPlaywrightConfig } from '@sentry-internal/test-utils';
 
-const testEnv = process.env.TEST_ENV;
-
-if (!testEnv) {
-  throw new Error('No test env defined');
-}
-
 const config = getPlaywrightConfig({
-  startCommand: testEnv === 'development' ? 'pnpm dev --port 3030' : 'pnpm start',
+  startCommand: 'pnpm start',
 });
 
 export default {
