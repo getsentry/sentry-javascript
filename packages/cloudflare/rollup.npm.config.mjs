06464c237b4705b6182d1fb739d0c0322ca6f375
JPeer264 <jan.peer@sentry.io>
2026-08-03 09:25:12 +0200

    fixup! ref(cloudflare): Ensure wrapRequestHandler stays free of node specifcs

--- packages/cloudflare/rollup.npm.config.mjs
+++ packages/cloudflare/rollup.npm.config.mjs
@@ -2,7 +2,7 @@ import { makeBaseNPMConfig, makeNPMConfigVariants } from '@sentry-internal/rollu
 
 export default makeNPMConfigVariants(
   makeBaseNPMConfig({
-    entrypoints: ['src/index.ts', 'src/nodejs_compat/index.ts', 'src/vite/index.ts'],
+    entrypoints: ['src/index.ts', 'src/request.ts', 'src/nodejs_compat/index.ts', 'src/vite/index.ts'],
   }),
   { splitDevProd: true },
 );
