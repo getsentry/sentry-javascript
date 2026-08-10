cc13ae7b8472a0f801eb3e144282990f0ab9dc06
Francesco Novy <francesco.novy@sentry.io>
2026-08-10 13:19:16 +0200

    fixes fixes

--- packages/remix/src/server/integrations/tracing-channel.ts
+++ packages/remix/src/server/integrations/tracing-channel.ts
@@ -2,6 +2,7 @@ import * as diagnosticsChannel from 'node:diagnostics_channel';
 import type { Span, SpanAttributes } from '@sentry/core';
 import {
   getActiveSpan,
+  getSpanStatusFromHttpCode,
   isObjectLike,
   isURLObjectRelative,
   parseStringToURLObject,
@@ -107,6 +108,9 @@ function setResponseStatus(span: Span, result: unknown): void {
     // oxlint-disable-next-line typescript/no-deprecated
     span.setAttribute(HTTP_STATUS_CODE, status);
     span.setAttribute(HTTP_RESPONSE_STATUS_CODE, status);
+
+    const spanStatus = getSpanStatusFromHttpCode(status);
+    span.setStatus(spanStatus);
   }
 }
 
