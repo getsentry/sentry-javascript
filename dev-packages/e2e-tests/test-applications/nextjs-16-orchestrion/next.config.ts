6706f7da8daff5ea15bd5dde1fff0b610e8ee926
Francesco Novy <francesco.novy@sentry.io>
2026-08-12 14:36:15 +0200

    test: Remove unneeded orchestrion test apps

--- dev-packages/e2e-tests/test-applications/nextjs-16-orchestrion/next.config.ts
+++
@@ -1,8 +0,0 @@
-import { withSentryConfig } from '@sentry/nextjs';
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {};
-
-export default withSentryConfig(nextConfig, {
-  silent: true,
-});
