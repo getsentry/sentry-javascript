640b46f8fdc19d55a3c9a7710ca1662c5d9954b9
Francesco Novy <francesco.novy@sentry.io>
2026-08-24 09:39:59 +0200

    remove vercel ai

--- packages/cloudflare/src/sdk.ts
+++ packages/cloudflare/src/sdk.ts
@@ -1,21 +1,12 @@
 import type { Integration } from '@sentry/core';
 import { getBaseDefaultIntegrations, initWithDefaultIntegrations } from './baseSdk';
 import type { CloudflareClient, CloudflareOptions } from './client';
-import { vercelAIIntegration } from '@sentry/server-utils/orchestrion';
 
 /**
  * Get the default integrations for the Cloudflare SDK.
  */
 export function getDefaultIntegrations(options: CloudflareOptions): Integration[] {
-  return [
-    ...getBaseDefaultIntegrations(options),
-    // Note: For now we add this directly here, in order for this to be here for vercel AI v7
-    // Generally, we auto-inject integrations based on what orchestrion is using
-    // however, for things with native channel support (like ai v7) we do not know about this
-    // we'll fix this in a follow up, but for the time being vercelAIIntegration is added here
-    // TODO: Remove this once we auto-inject integrations for native channels as well
-    vercelAIIntegration(),
-  ];
+  return getBaseDefaultIntegrations(options);
 }
 
 /**
