410aec1dd86104daab74712bd7759f4777e2ea9d
Francesco Novy <francesco.novy@sentry.io>
2026-08-25 10:12:02 +0200

    no deprecate

--- packages/node/src/integrations/tracing/index.ts
+++ packages/node/src/integrations/tracing/index.ts
@@ -1,9 +1,6 @@
 import type { Integration } from '@sentry/core';
 import { getTracingIntegrations } from '@sentry/server-utils';
 
-/**
- * @deprecated Use getTracingIntegrations instead.
- */
 export function getAutoPerformanceIntegrations(): Integration[] {
   return getTracingIntegrations();
 }
