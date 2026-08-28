aa47997549e143fbe1816d9ffd6eb9af9e7c9504
Francesco Novy <francesco.novy@sentry.io>
2026-08-28 10:18:47 +0200

    fix lint

--- packages/core/src/utils/browser.ts
+++ packages/core/src/utils/browser.ts
@@ -2,10 +2,6 @@ import { GLOBAL_OBJ } from './worldwide';
 
 const WINDOW = GLOBAL_OBJ as unknown as Window;
 
-type SimpleNode = {
-  parentNode: SimpleNode;
-} | null;
-
 /**
  * A safe form of location.href
  */
