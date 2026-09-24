25a1d20eca86000f8083322ec65cf001f9a1a9c3
Abdelrahman Awad <abdelrahman.awad@sentry.io>
2026-09-24 17:20:19 -0400

    test(nuxt): Cover route resolution through the route provider

---
+++ dev-packages/e2e-tests/test-applications/nuxt-5/app/pages/route-provider/[id].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import * as Sentry from '@sentry/nuxt';
+import { onMounted, ref } from 'vue';
+
+const route = ref<string>();
+
+onMounted(() => {
+  route.value = Sentry.resolveCurrentRoute() ?? 'unresolved';
+});
+</script>
+
+<template>
+  <div id="resolved-route">{{ route }}</div>
+</template>
